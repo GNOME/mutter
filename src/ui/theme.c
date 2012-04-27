a6737ef1291a47b86e6dbc20c4b363a45733e624
Jasper St. Pierre <jstpierre@mecheye.net>
2012-04-27 01:16:57 -0400

    theme: Make meta_frame_style_draw_with_style private

--- src/ui/theme.c
+++ src/ui/theme.c
@@ -4104,7 +4104,7 @@ button_rect (MetaButtonType           type,
     }
 }
 
-void
+static void
 meta_frame_style_draw_with_style (MetaFrameStyle          *style,
                                   GtkStyleContext         *style_gtk,
                                   cairo_t                 *cr,
