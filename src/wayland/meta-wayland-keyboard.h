a474608954347c1add635591e837cc2809d8f99c
Giovanni Campagna <gcampagn@redhat.com>
2013-09-04 15:01:11 +0200

    wayland: reimplement keyboard state handling properly
    We can't rely on clutter's xkb_state, because that's updated
    when events are pulled from the kernel, not when we see them.
    Instead, use the new clutter API to get the full modifier state
    from the event (which, as a side effect, also works when clutter
    is using the X11 backend for running nested).
    
    https://bugzilla.gnome.org/show_bug.cgi?id=706963

--- src/wayland/meta-wayland-keyboard.h
+++ src/wayland/meta-wayland-keyboard.h
@@ -50,8 +50,8 @@
 
 struct _MetaWaylandKeyboardGrabInterface
 {
-  void (*key) (MetaWaylandKeyboardGrab * grab, uint32_t time,
-               uint32_t key, uint32_t state);
+  gboolean (*key) (MetaWaylandKeyboardGrab * grab, uint32_t time,
+		   uint32_t key, uint32_t state);
   void (*modifiers) (MetaWaylandKeyboardGrab * grab, uint32_t serial,
                      uint32_t mods_depressed, uint32_t mods_latched,
                      uint32_t mods_locked, uint32_t group);
@@ -111,9 +111,7 @@ struct _MetaWaylandKeyboard
   struct wl_display *display;
 
   struct xkb_context *xkb_context;
-  struct xkb_state *xkb_state;
   gboolean is_evdev;
-
   MetaWaylandXkbInfo xkb_info;
   struct xkb_rule_names xkb_names;
 
