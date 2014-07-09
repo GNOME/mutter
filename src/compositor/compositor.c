GNOME/gnome-shell

Switch to using ClutterStage::after-paint
The experimental API clutter_stage_set_paint_callback() is replaced
with an ::after-paint signal as of Clutter 1.19.5.

src/compositor/compositor.c
@@ -528,10 +528,8 @@ meta_compositor_manage (MetaCompositor *compositor)
         }
     }
 
-  clutter_stage_set_paint_callback (CLUTTER_STAGE (compositor->stage),
-                                    after_stage_paint,
-                                    compositor,
-                                    NULL);
+  g_signal_connect (CLUTTER_STAGE (compositor->stage), "after-paint",
+                    G_CALLBACK (after_stage_paint), compositor);
 
   clutter_stage_set_sync_delay (CLUTTER_STAGE (compositor->stage), META_SYNC_DELAY);
 

GNOME/mutter

src/compositor/compositor.c
@@ -668,10 +668,8 @@ meta_compositor_manage_screen (MetaCompositor *compositor,
 
   info->stage = clutter_stage_new ();
 
-  clutter_stage_set_paint_callback (CLUTTER_STAGE (info->stage),
-                                    after_stage_paint,
-                                    info,
-                                    NULL);
+  g_signal_connect (CLUTTER_STAGE (info->stage), "after-paint",
+                    G_CALLBACK (after_stage_paint), info);
 
   clutter_stage_set_sync_delay (CLUTTER_STAGE (info->stage), META_SYNC_DELAY);
 