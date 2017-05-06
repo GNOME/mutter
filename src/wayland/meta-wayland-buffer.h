3a293f02251718ee6186a164d973414074bcd42a
Miguel A. Vico <mvicomoya@nvidia.com>
2017-05-05 18:41:51 -0700

    wayland: Realize synchronized subsurface buffers
    Clients using EGLStream-backed buffers will expect the stream to be
    functional after wl_surface::attach(). That means the compositor-side
    stream must be created and a consumer attached to it.
    
    To resolve the above, this change realizes buffers even when the attach
    operation is deferred for synchronized subsurfaces.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=782575

--- src/wayland/meta-wayland-buffer.h
+++ src/wayland/meta-wayland-buffer.h
@@ -68,6 +68,8 @@ G_DECLARE_FINAL_TYPE (MetaWaylandBuffer, meta_wayland_buffer,
                       META, WAYLAND_BUFFER, GObject);
 
 MetaWaylandBuffer *     meta_wayland_buffer_from_resource       (struct wl_resource    *resource);
+gboolean                meta_wayland_buffer_is_realized         (MetaWaylandBuffer     *buffer);
+gboolean                meta_wayland_buffer_realize             (MetaWaylandBuffer     *buffer);
 gboolean                meta_wayland_buffer_attach              (MetaWaylandBuffer     *buffer,
                                                                  GError               **error);
 CoglTexture *           meta_wayland_buffer_get_texture         (MetaWaylandBuffer     *buffer);
