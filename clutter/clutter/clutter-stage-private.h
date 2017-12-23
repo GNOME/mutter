766c8696aa0381153b3f84dccc981892bb6eedb0
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-12-22 21:17:03 -0600

    clutter/stage: Add private API to get max view scale for rect
    Move the same code from ClutterActor as this is something we can
    reuse elsewhere.

--- clutter/clutter/clutter-stage-private.h
+++ clutter/clutter/clutter-stage-private.h
@@ -124,6 +124,9 @@ gboolean                _clutter_stage_update_state     (ClutterStage      *stag
 
 void                    _clutter_stage_set_scale_factor (ClutterStage      *stage,
                                                          int                factor);
+gboolean                _clutter_stage_get_max_view_scale_factor_for_rect (ClutterStage *stage,
+                                                                           ClutterRect  *rect,
+                                                                           float        *view_scale);
 
 void            _clutter_stage_presented                (ClutterStage      *stage,
                                                          CoglFrameEvent     frame_event,
--- clutter/clutter/clutter-stage.c
+++ clutter/clutter/clutter-stage.c
@@ -4822,6 +4822,22 @@ clutter_stage_capture (ClutterStage          *stage,
 }
 
 static void
+init_rect_from_cairo_rect (ClutterRect           *rect,
+                           cairo_rectangle_int_t *cairo_rect)
+{
+  *rect = (ClutterRect) {
+    .origin = {
+      .x = cairo_rect->x,
+      .y = cairo_rect->y
+    },
+    .size = {
+      .width = cairo_rect->width,
+      .height = cairo_rect->height
+    }
+  };
+}
+
+static void
 capture_view_into (ClutterStage          *stage,
                    gboolean               paint,
                    ClutterStageView      *view,
@@ -4978,4 +4994,34 @@ void
 clutter_stage_update_resource_scales (ClutterStage *stage)
 {
   _clutter_actor_queue_update_resource_scale_recursive (CLUTTER_ACTOR (stage));
+}
+
+gboolean
+_clutter_stage_get_max_view_scale_factor_for_rect (ClutterStage *stage,
+                                                   ClutterRect  *rect,
+                                                   float        *view_scale)
+{
+  ClutterStagePrivate *priv = stage->priv;
+  GList *l;
+  float scale;
+
+  scale = 0.0;
+  for (l = _clutter_stage_window_get_views (priv->impl); l; l = l->next)
+    {
+      ClutterStageView *view = l->data;
+      cairo_rectangle_int_t view_layout;
+      ClutterRect view_rect;
+
+      clutter_stage_view_get_layout (view, &view_layout);
+      init_rect_from_cairo_rect (&view_rect, &view_layout);
+
+      if (clutter_rect_intersection (&view_rect, rect, NULL))
+        scale = MAX (clutter_stage_view_get_scale (view), scale);
+    }
+
+  if (scale == 0.0)
+    return FALSE;
+
+  *view_scale = scale;
+  return TRUE;
 }
