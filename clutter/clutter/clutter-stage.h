0d3094993d173abd2239bab1723c95068d83f32f
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-12-23 04:36:09 +0100

    clutter/stage: Add function to get the scale for a capture area
    In the shell we need to know what scaling will be applied to the
    stage capture texture in order to properly know what will be its
    size.

--- clutter/clutter/clutter-stage.h
+++ clutter/clutter/clutter-stage.h
@@ -259,6 +259,11 @@ void            clutter_stage_skip_sync_delay                   (ClutterStage   
 #endif
 
 CLUTTER_AVAILABLE_IN_MUTTER
+gboolean clutter_stage_capture_get_scale (ClutterStage          *stage,
+                                          cairo_rectangle_int_t *rect,
+                                          gfloat                *scale);
+
+CLUTTER_AVAILABLE_IN_MUTTER
 gboolean clutter_stage_capture (ClutterStage          *stage,
                                 gboolean               paint,
                                 cairo_rectangle_int_t *rect,
