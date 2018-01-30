e93b2e9f6fa978b62114ee0e3cd97c63930c7389
Daniel van Vugt <daniel.van.vugt@canonical.com>
2018-01-30 17:47:17 +0800

    First prototype of the new object (untested)

---
+++ src/backends/native/meta-framebuffer-kms.c
@@ -0,0 +1,171 @@
+/*
+ * Copyright © 2018 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Author: Daniel van Vugt <daniel.van.vugt@canonical.com>
+ */
+
+#include "meta-framebuffer-kms.h"
+#include <xf86drm.h>
+#include <xf86drmMode.h>
+#include <drm_fourcc.h>
+
+#define INVALID_FB_ID 0U
+
+G_DEFINE_TYPE (MetaFramebufferKms, meta_framebuffer_kms, G_TYPE_OBJECT)
+
+static void
+meta_framebuffer_kms_init (MetaFramebufferKms *framebuffer_kms)
+{
+  framebuffer_kms->drm_fd = -1;
+  framebuffer_kms->fb_id = INVALID_FB_ID;
+}
+
+static void
+release_private (MetaFramebufferKms *framebuffer_kms)
+{
+  drmModeRmFB (framebuffer_kms->drm_fd, framebuffer_kms->fb_id);
+  framebuffer_kms->fb_id = INVALID_FB_ID;
+
+  gbm_surface_release_buffer (framebuffer_kms->gbm_surface,
+                              framebuffer_kms->gbm_bo);
+  framebuffer_kms->gbm_bo = NULL;
+}
+
+static void
+meta_framebuffer_kms_finalize (GObject *object)
+{
+  MetaFramebufferKms *framebuffer_kms = META_FRAMEBUFFER_KMS (object);
+
+  if (framebuffer_kms->gbm_bo)
+    release_private (framebuffer_kms);
+
+  /* We don't own these: */
+  framebuffer_kms->gbm_surface = NULL;
+  framebuffer_kms->drm_fd = -1;
+
+  G_OBJECT_CLASS (meta_framebuffer_kms_parent_class)->finalize (object);
+}
+
+static void
+meta_framebuffer_kms_class_init (MetaFramebufferKmsClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = meta_framebuffer_kms_finalize;
+}
+
+void
+meta_framebuffer_kms_set_drm_fd (MetaFramebufferKms *framebuffer_kms,
+                                 int drm_fd)
+{
+  g_return_if_fail (framebuffer_kms->gbm_bo == NULL);
+  g_return_if_fail (framebuffer_kms->gbm_surface == NULL);
+
+  framebuffer_kms->drm_fd = drm_fd;
+}
+
+void
+meta_framebuffer_kms_set_gbm_surface (MetaFramebufferKms *framebuffer_kms,
+                                      struct gbm_surface *gbm_surface)
+{
+  g_return_if_fail (framebuffer_kms->gbm_bo == NULL);
+  g_return_if_fail (framebuffer_kms->drm_fd >= 0);
+
+  framebuffer_kms->gbm_surface = gbm_surface;
+}
+
+gboolean
+meta_framebuffer_kms_acquire_swapped_buffer (MetaFramebufferKms *framebuffer_kms)
+{
+  uint32_t handles[4] = {0, 0, 0, 0};
+  uint32_t strides[4] = {0, 0, 0, 0};
+  uint32_t offsets[4] = {0, 0, 0, 0};
+  uint64_t modifiers[4] = {0, 0, 0, 0};
+  uint32_t width, height, format;
+  struct gbm_bo *bo;
+  int i;
+
+  g_return_val_if_fail (framebuffer_kms->gbm_bo == NULL, FALSE);
+  g_return_val_if_fail (framebuffer_kms->gbm_surface != NULL, FALSE);
+  g_return_val_if_fail (framebuffer_kms->drm_fd >= 0, FALSE);
+
+  bo = gbm_surface_lock_front_buffer (framebuffer_kms->gbm_surface);
+  g_assert (bo != NULL);
+
+  for (i = 0; i < gbm_bo_get_plane_count (bo); ++i)
+    {
+      strides[i] = gbm_bo_get_stride_for_plane (bo, i);
+      handles[i] = gbm_bo_get_handle_for_plane (bo, i).u32;
+      offsets[i] = gbm_bo_get_offset (bo, i);
+      modifiers[i] = gbm_bo_get_modifier (bo);
+    }
+
+  width = gbm_bo_get_width (bo);
+  height = gbm_bo_get_height (bo);
+  format = gbm_bo_get_format (bo);
+
+  if (modifiers[0] != DRM_FORMAT_MOD_INVALID)
+    {
+      if (drmModeAddFB2WithModifiers (framebuffer_kms->drm_fd,
+                                      width, height, format,
+                                      handles, strides, offsets, modifiers,
+                                      &framebuffer_kms->fb_id,
+                                      DRM_MODE_FB_MODIFIERS))
+        {
+          g_warning ("drmModeAddFB2WithModifiers failed: %m");
+          gbm_surface_release_buffer (framebuffer_kms->gbm_surface, bo);
+          return FALSE;
+        }
+    }
+  else if (drmModeAddFB2 (framebuffer_kms->drm_fd, width, height, format,
+                          handles, strides, offsets,
+                          &framebuffer_kms->fb_id, 0))
+    {
+      /* TODO: Configurable depth and bpp in future? */
+      if (drmModeAddFB (framebuffer_kms->drm_fd, width, height, 24, 32,
+                        strides[0], handles[0], &framebuffer_kms->fb_id))
+        {
+          g_warning ("drmModeAddFB: %m");
+          gbm_surface_release_buffer (framebuffer_kms->gbm_surface, bo);
+          return FALSE;
+        }
+    }
+
+  framebuffer_kms->gbm_bo = bo;
+  return TRUE;
+}
+
+uint32_t
+meta_framebuffer_kms_get_fb_id (const MetaFramebufferKms *framebuffer_kms)
+{
+  g_return_val_if_fail (framebuffer_kms->gbm_bo != NULL, INVALID_FB_ID);
+  g_return_val_if_fail (framebuffer_kms->gbm_surface != NULL, INVALID_FB_ID);
+  g_return_val_if_fail (framebuffer_kms->drm_fd >= 0, INVALID_FB_ID);
+
+  return framebuffer_kms->fb_id;
+}
+
+void
+meta_framebuffer_kms_release_swapped_buffer (MetaFramebufferKms *framebuffer_kms)
+{
+  g_return_if_fail (framebuffer_kms->gbm_bo != NULL);
+  g_return_if_fail (framebuffer_kms->gbm_surface != NULL);
+  g_return_if_fail (framebuffer_kms->drm_fd >= 0);
+
+  release_private (framebuffer_kms);
+}
---
+++ src/backends/native/meta-framebuffer-kms.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2018 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Author: Daniel van Vugt <daniel.van.vugt@canonical.com>
+ */
+
+#ifndef META_FRAMEBUFFER_KMS_H
+#define META_FRAMEBUFFER_KMS_H
+
+#include <glib-object.h>
+#include <gbm.h>
+
+struct _MetaFramebufferKms
+{
+  GObject parent;
+
+  /* Contextual information we don't own (and assume lives longer than us since
+     there's no refcount on these) */
+  int drm_fd;
+  struct gbm_surface *gbm_surface;
+
+  /* Members we own and will destroy when refcount reaches zero */
+  struct gbm_bo *gbm_bo;
+  uint32_t fb_id;
+};
+
+#define META_TYPE_FRAMEBUFFER_KMS (meta_framebuffer_kms_get_type ())
+G_DECLARE_FINAL_TYPE (MetaFramebufferKms, meta_framebuffer_kms, META, FRAMEBUFFER_KMS, GObject)
+
+void meta_framebuffer_kms_set_drm_fd (MetaFramebufferKms *fb_kms,
+                                      int drm_fd);
+void meta_framebuffer_kms_set_gbm_surface (MetaFramebufferKms *fb_kms,
+                                           struct gbm_surface *gbm_surface);
+
+gboolean meta_framebuffer_kms_acquire_swapped_buffer (MetaFramebufferKms *fb_kms);
+uint32_t meta_framebuffer_kms_get_fb_id (const MetaFramebufferKms *fb_kms);
+void meta_framebuffer_kms_release_swapped_buffer (MetaFramebufferKms *fb_kms);
+
+#endif /* META_FRAMEBUFFER_KMS_H */
