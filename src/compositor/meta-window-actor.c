c21d12c4acf27b883a23616698f569bd7bafacd2
Ray Strode <rstrode@redhat.com>
2018-03-19 14:31:16 -0400

    compositor: recompute shape when surface is changed
    Anytime the surface is changed, we need to rebuild our masks and
    recalculate our shapes since the new surface may have a different
    size.
    
    If the actor gets redrawn for any reason before that computation
    happens then it will misrender.
    
    This commit makes sure we do the recomputation right away, before
    any painting might occur.

--- src/compositor/meta-window-actor.c
+++ src/compositor/meta-window-actor.c
@@ -296,6 +296,7 @@ surface_size_changed (MetaSurfaceActor *actor,
   MetaWindowActor *self = META_WINDOW_ACTOR (user_data);
 
   meta_window_actor_update_shape (self);
+  meta_window_actor_handle_updates (self);
 }
 
 static void
