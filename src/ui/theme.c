deede62474effa9ff660aae57569877781ccad2b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-04-03 20:34:26 -0500

    theme: support :variant in GTK_THEME

--- src/ui/theme.c
+++ src/ui/theme.c
@@ -1046,10 +1046,23 @@ meta_theme_create_style_info (GdkScreen   *screen,
 {
   MetaStyleInfo *style_info;
   GtkCssProvider *provider;
+  const char *env_theme;
   char *theme_name;
 
-  if (g_getenv ("GTK_THEME"))
-    theme_name = g_strdup (g_getenv ("GTK_THEME"));
+  env_theme = g_getenv ("GTK_THEME");
+  if (env_theme && *env_theme)
+    {
+      const char *variant_p;
+      variant_p = strrchr (env_theme, ':');
+      if (variant_p)
+        {
+          theme_name = g_strndup (env_theme, variant_p - env_theme);
+          if (*(variant_p + 1))
+            variant = variant_p + 1;
+        }
+      else
+        theme_name = g_strdup (env_theme);
+    }
   else
     g_object_get (gtk_settings_get_for_screen (screen),
                   "gtk-theme-name", &theme_name,
