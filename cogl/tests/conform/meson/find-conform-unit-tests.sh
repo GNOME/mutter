8b4ff01769facf6e1f331c530a519215d0fec4ff
Jonas Ådahl <jadahl@gmail.com>
2018-05-22 15:55:35 +0200

    Add meson build support
    This commit adds meson build support to mutter. It takes a step away
    from the three separate code bases with three different autotools setups
    into a single meson build system. There are still places that can be
    unified better, for example by removing various "config.h" style files
    from cogl and clutter, centralizing debug C flags and other configurable
    macros, and similar artifacts that are there only because they were once
    separate code bases.
    
    There are some differences between the autotools setup and the new
    meson. Here are a few:
    
    The meson setup doesn't generate wrapper scripts for various cogl and
    clutter test cases. What these tests did was more or less generate a
    tiny script that called an executable with a test name as the argument.
    To run particular tests, just run the test executable with the name of
    the test as the argument.
    
    The meson setup doesn't install test files anymore. The autotools test
    suite was designed towards working with installed tests, but it didn't
    really still, and now with meson, it doesn't install anything at all,
    but instead makes sure that everything runs with the uninstalled input
    files, binaries and libraries when running the test suite. Installable
    tests may come later.
    
    Tests from cogl, clutter and mutter are run on 'meson test'. In
    autotools, only cogl and clutter tests were run on 'make check'.

---
+++ cogl/tests/conform/meson/find-conform-unit-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+inputfile="$1"
+outputfile="$2"
+
+echo > "$outputfile"
+
+sed -n -e 's/^ \{1,\}ADD_TEST *( *\([a-zA-Z0-9_]\{1,\}\).*/\1/p' "$1" | while read test; do
+  echo $test >> $outputfile
+done
