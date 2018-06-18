a8db59908f1e40394a29b1e357a02e7998f51ad8
Olivier Fourdan <ofourdan@redhat.com>
2018-06-18 10:39:11 +0000

    window/wayland: Always update monitor for non-user ops
    meta_window_wayland_update_main_monitor() would skip the monitor update
    if the difference in scale between the old and the new monitor would
    cause another monitor change.
    
    While this is suitable when the monitor change results from a user
    interactively moving the surface between monitors of different scales,
    this can leave dangling pointers to freed monitors when this is
    triggered by a change of monitor configuration.
    
    Make sure we update the monitor unconditionally if not from a user
    operation.
    
    Closes: https://gitlab.gnome.org/GNOME/mutter/issues/189
    
    (cherry picked from commit a3da4b8d5bd217c0262fd9361036877d155a300f)

--- src/wayland/meta-window-wayland.c
+++ src/wayland/meta-window-wayland.c
@@ -419,6 +419,12 @@ meta_window_wayland_update_main_monitor (MetaWindow *window,
       return;
     }
 
+  if (!user_op)
+    {
+      window->monitor = to;
+      return;
+    }
+
   from_scale = meta_logical_monitor_get_scale (from);
   to_scale = meta_logical_monitor_get_scale (to);
 
