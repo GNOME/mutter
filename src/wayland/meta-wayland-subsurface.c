3456736f28cc1c76e8f817733ca2b07df2992426
Robert Mader <robert.mader@posteo.de>
2018-06-22 16:54:55 +0200

    Implement wp_viewporter
    To do this, it's necessary to change some hard-coded calculations
    in many places. This will benefit us when we implement transfor-
    mations, which would require the same.

--- src/wayland/meta-wayland-subsurface.c
+++ src/wayland/meta-wayland-subsurface.c
@@ -165,8 +165,8 @@ meta_wayland_subsurface_union_geometry (MetaWaylandSubsurface *subsurface,
   geometry = (MetaRectangle) {
     .x = surface->offset_x + surface->sub.x,
     .y = surface->offset_y + surface->sub.y,
-    .width = cogl_texture_get_width (texture) / surface->scale,
-    .height = cogl_texture_get_height (texture) / surface->scale,
+    .width = meta_wayland_surface_get_width (surface),
+    .height = meta_wayland_surface_get_height (surface),
   };
 
   meta_rectangle_union (out_geometry, &geometry, out_geometry);
