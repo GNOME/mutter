20181052678a7ba89ea50f8b315a8bafcbc8cc57
Jonas Ådahl <jadahl@gmail.com>
2018-07-12 22:20:20 +0200

    plugin: Rename the .so file from plugin.so to libplugin.so
    This is the filename convention you get when you define a shared module
    in meson, and since there is no particular reason to not include the
    "lib" prefix, lets make it easier to port it over. While at it,
    de-duplicate the retrieval of the plugin name.

--- src/tests/test-runner.c
+++ src/tests/test-runner.c
@@ -840,7 +840,7 @@ main (int argc, char **argv)
     }
   g_option_context_free (ctx);
 
-  meta_plugin_manager_load ("default");
+  meta_plugin_manager_load (test_get_plugin_name ());
   meta_wayland_override_display_name ("mutter-test-display");
 
   meta_init ();
