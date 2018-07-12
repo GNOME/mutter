e0dc136835d19ccb69ac1ca324827d19c4ebc6ba
Jonas Ådahl <jadahl@gmail.com>
2018-07-12 10:25:16 +0200

    tests: Move window-shown verification to test-runner
    Previously we relied on the test-client to make sure that a window was
    shown. For X11, we did not need to do anything, but for Wayland we had
    to make sure we had drawn the first frame, otherwise mutter wouldn't
    have a buffer making the window not showable.
    
    Doing it this way doesn't work anymore however, since the 'after-paint'
    event will be emitted even if we didn't actually paint anything. This is
    the case with current Gtk under Wayland, where we won't draw until the
    compositor has configured the surface. In effect, this mean we'll get a
    dummy after-paint emission before the first frame is actually painted.
    
    Instead, move the verification that a "show" command has completed by
    having the test-runner wait for a "shown" signal on the window, which is
    emitted in the end of meta_window_show(). This requires an additional
    call to gdk_display_sync() in the test-client after creating the window,
    to make sure that the window creation vents has been received in the
    compositor.

--- src/tests/test-runner.c
+++ src/tests/test-runner.c
@@ -327,6 +327,39 @@ test_case_check_xserver_stacking (TestCase *test,
   return *error == NULL;
 }
 
+typedef struct _WaitForShownData
+{
+  GMainLoop *loop;
+  MetaWindow *window;
+  guint shown_handler_id;
+} WaitForShownData;
+
+static void
+on_window_shown (MetaWindow       *window,
+                 WaitForShownData *data)
+{
+  g_main_loop_quit (data->loop);
+}
+
+static gboolean
+test_case_wait_for_showing_before_redraw (gpointer user_data)
+{
+  WaitForShownData *data = user_data;
+
+  if (meta_window_is_hidden (data->window))
+    {
+      data->shown_handler_id = g_signal_connect (data->window, "shown",
+                                                 G_CALLBACK (on_window_shown),
+                                                 data);
+    }
+  else
+    {
+      g_main_loop_quit (data->loop);
+    }
+
+  return FALSE;
+}
+
 static gboolean
 test_case_do (TestCase *test,
               int       argc,
@@ -407,8 +440,37 @@ test_case_do (TestCase *test,
                            NULL))
         return FALSE;
     }
-  else if (strcmp (argv[0], "show") == 0 ||
-           strcmp (argv[0], "hide") == 0 ||
+  else if (strcmp (argv[0], "show") == 0)
+    {
+      if (argc != 2)
+        BAD_COMMAND("usage: %s <client-id>/<window-id>", argv[0]);
+
+      TestClient *client;
+      const char *window_id;
+      if (!test_case_parse_window_id (test, argv[1], &client, &window_id, error))
+        return FALSE;
+
+      if (!test_client_do (client, error, argv[0], window_id, NULL))
+        return FALSE;
+
+      MetaWindow *window = test_client_find_window (client, window_id, error);
+      if (!window)
+        return FALSE;
+
+      WaitForShownData data = {
+        .loop = g_main_loop_new (NULL, FALSE),
+        .window = window,
+      };
+      meta_later_add (META_LATER_BEFORE_REDRAW,
+                      test_case_wait_for_showing_before_redraw,
+                      &data,
+                      NULL);
+      g_main_loop_run (data.loop);
+      if (data.shown_handler_id)
+        g_signal_handler_disconnect (window, data.shown_handler_id);
+      g_main_loop_unref (data.loop);
+    }
+  else if (strcmp (argv[0], "hide") == 0 ||
            strcmp (argv[0], "activate") == 0 ||
            strcmp (argv[0], "raise") == 0 ||
            strcmp (argv[0], "lower") == 0 ||
