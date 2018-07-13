3b802ee09a5decbdef53e1db130534c59f75ce32
Olivier Fourdan <ofourdan@redhat.com>
2018-07-13 16:40:31 +0200

    tests: Add "set_parent_exported" command
    Add a "set_parent_exported" command to test the xdg-foreign support, so
    that we can add a test which uses the GDK API for exported window:
    
      `gdk_wayland_window_set_transient_for_exported()`
    
    That will allow to detect if transient is applied correctly between
    foreign windows and detect possible crashes such as issue !174.
    
    Related: https://gitlab.gnome.org/GNOME/mutter/issues/174

--- src/tests/test-client.c
+++ src/tests/test-client.c
@@ -20,6 +20,7 @@
 #include <gio/gunixinputstream.h>
 #include <gtk/gtk.h>
 #include <gdk/gdkx.h>
+#include <gdk/gdkwayland.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,6 +31,18 @@ static gboolean wayland;
 GHashTable *windows;
 
 static void read_next_line (GDataInputStream *in);
+
+static void
+window_export_handle_cb (GdkWindow  *window,
+                         const char *handle_str,
+                         gpointer    user_data)
+{
+  GdkWindow *gwindow = gtk_widget_get_window (GTK_WIDGET (user_data));
+
+  if (!gdk_wayland_window_set_transient_for_exported (gwindow, (gchar *) handle_str))
+    g_print ("Fail to set transient_for exported window handle %s", handle_str);
+  gdk_window_set_modal_hint (gwindow, TRUE);
+}
 
 static GtkWidget *
 lookup_window (const char *window_id)
@@ -130,7 +143,7 @@ process_line (const char *line)
     {
       if (argc != 3)
         {
-          g_print ("usage: menu <window-id> <parent-id>");
+          g_print ("usage: set_parent <window-id> <parent-id>");
           goto out;
         }
 
@@ -150,6 +163,32 @@ process_line (const char *line)
 
       gtk_window_set_transient_for (GTK_WINDOW (window),
                                     GTK_WINDOW (parent_window));
+    }
+  else if (strcmp (argv[0], "set_parent_exported") == 0)
+    {
+      if (argc != 3)
+        {
+          g_print ("usage: set_parent_exported <window-id> <parent-id>");
+          goto out;
+        }
+
+      GtkWidget *window = lookup_window (argv[1]);
+      if (!window)
+        {
+          g_print ("unknown window %s", argv[1]);
+          goto out;
+        }
+
+      GtkWidget *parent_window = lookup_window (argv[2]);
+      if (!parent_window)
+        {
+          g_print ("unknown parent window %s", argv[2]);
+          goto out;
+        }
+
+      GdkWindow *parent_gwindow = gtk_widget_get_window (parent_window);
+      if (!gdk_wayland_window_export_handle (parent_gwindow, window_export_handle_cb, window, NULL))
+        g_print ("Fail to export handle for window id %s", argv[2]);
     }
   else if (strcmp (argv[0], "show") == 0)
     {
