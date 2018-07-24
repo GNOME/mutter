GNOME/mutter

commit a3da4b8d5bd217c0262fd9361036877d155a300f

window/wayland: Always update monitor for non-user ops
meta_window_wayland_update_main_monitor() would skip the monitor update
if the difference in scale between the old and the new monitor would
cause another monitor change.

While this is suitable when the monitor change results from a user
interactively moving the surface between monitors of different scales,
this can leave dangling pointers to freed monitors when this is
triggered by a change of monitor configuration.

Make sure we update the monitor unconditionally if not from a user
operation.

Closes: https://gitlab.gnome.org/GNOME/mutter/issues/189

src/wayland/meta-window-wayland.c
@@ -413,6 +413,12 @@ meta_window_wayland_update_main_monitor (MetaWindow *window,
       return;
     }
 
+  if (!user_op)
+    {
+      window->monitor = to;
+      return;
+    }
+
   from_scale = meta_logical_monitor_get_scale (from);
   to_scale = meta_logical_monitor_get_scale (to);
 

commit e51b7ce7b271cc69fd9a2d1818b60a03a48fe904

window: Force window monitor update only on monitor change
Forcing the update of the window monitor for all non-user driven actions
leads to a small regression when a window is moved programmatically
between monitors of different scales, it may end up being scaled
according to the larger scale of one monitor whereas it's positionned
mostly on the other monitor.

To avoid this, we need to distinguish between the case where the monitor
update results from a monitor change (in which case we need to always
update the window monitor), the case where the monitor change results
from a user interactive change and all the other cases where the change
results from a non-interactive move but the old monitor is still valid.

For this purpose, change the `user_op` boolean value passed to the
various update monitor functions to a flag that can take different
values.

Fixes: commit a3da4b8

src/core/window-private.h
@@ -140,6 +140,13 @@ typedef enum
   META_EDGE_CONSTRAINT_MONITOR = 2,
 } MetaEdgeConstraint;
 
+typedef enum
+{
+  META_WINDOW_UPDATE_MONITOR_NONE        = 0,
+  META_WINDOW_UPDATE_MONITOR_USER_ACTION = 1 << 0,
+  META_WINDOW_UPDATE_MONITOR_CHANGED     = 1 << 1,
+} MetaWindowUpdateMonitorFlags;
+
 struct _MetaWindow
 {
   GObject parent_instance;
@@ -548,8 +555,8 @@ struct _MetaWindowClass
                                   cairo_surface_t **icon,
                                   cairo_surface_t **mini_icon);
   uint32_t (*get_client_pid)     (MetaWindow *window);
-  void (*update_main_monitor)    (MetaWindow *window,
-                                  gboolean    user_op);
+  void (*update_main_monitor)    (MetaWindow                  *window,
+                                  MetaWindowUpdateMonitorFlags flags);
   void (*main_monitor_changed)   (MetaWindow *window,
                                   const MetaLogicalMonitor *old);
   void (*force_restore_shortcuts) (MetaWindow         *window,
@@ -766,8 +773,8 @@ void meta_window_activate_full (MetaWindow     *window,
 MetaLogicalMonitor * meta_window_calculate_main_logical_monitor (MetaWindow *window);
 
 MetaLogicalMonitor * meta_window_get_main_logical_monitor (MetaWindow *window);
-void meta_window_update_monitor (MetaWindow *window,
-                                 gboolean    user_op);
+void meta_window_update_monitor (MetaWindow                  *window,
+                                 MetaWindowUpdateMonitorFlags flags);
 
 void meta_window_set_urgent (MetaWindow *window,
                              gboolean    urgent);

src/core/window.c
@@ -3823,7 +3823,7 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
 
   if (window->override_redirect || window->type == META_WINDOW_DESKTOP)
     {
-      meta_window_update_monitor (window, FALSE);
+      meta_window_update_monitor (window, META_WINDOW_UPDATE_MONITOR_CHANGED);
       return;
     }
 
@@ -3858,19 +3858,19 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
     }
   else
     {
-      meta_window_update_monitor (window, FALSE);
+      meta_window_update_monitor (window, META_WINDOW_UPDATE_MONITOR_CHANGED);
     }
 }
 
 void
-meta_window_update_monitor (MetaWindow *window,
-                            gboolean    user_op)
+meta_window_update_monitor (MetaWindow                  *window,
+                            MetaWindowUpdateMonitorFlags flags)
 {
   MetaWorkspaceManager *workspace_manager = window->display->workspace_manager;
   const MetaLogicalMonitor *old;
 
   old = window->monitor;
-  META_WINDOW_GET_CLASS (window)->update_main_monitor (window, user_op);
+  META_WINDOW_GET_CLASS (window)->update_main_monitor (window, flags);
   if (old != window->monitor)
     {
       meta_window_on_all_workspaces_changed (window);
@@ -3884,7 +3884,8 @@ meta_window_update_monitor (MetaWindow *window,
        * That should be handled by explicitly moving the window before changing the
        * workspace.
        */
-      if (meta_prefs_get_workspaces_only_on_primary () && user_op &&
+      if (meta_prefs_get_workspaces_only_on_primary () &&
+          (flags & META_WINDOW_UPDATE_MONITOR_USER_ACTION) &&
           meta_window_is_on_primary_monitor (window)  &&
           workspace_manager->active_workspace != window->workspace)
         meta_window_change_workspace (window, workspace_manager->active_workspace);
@@ -3927,6 +3928,7 @@ meta_window_move_resize_internal (MetaWindow          *window,
   MetaRectangle unconstrained_rect;
   MetaRectangle constrained_rect;
   MetaMoveResizeResultFlags result = 0;
+  MetaWindowUpdateMonitorFlags update_monitor_flags;
   gboolean moved_or_resized = FALSE;
 
   g_return_if_fail (!window->override_redirect);
@@ -4028,13 +4030,18 @@ meta_window_move_resize_internal (MetaWindow          *window,
                                             did_placement);
     }
 
+  if (flags & META_MOVE_RESIZE_USER_ACTION)
+      update_monitor_flags = META_WINDOW_UPDATE_MONITOR_USER_ACTION;
+  else
+      update_monitor_flags = META_WINDOW_UPDATE_MONITOR_NONE;
+
   if (window->monitor)
     {
       guint old_output_winsys_id;
 
       old_output_winsys_id = window->monitor->winsys_id;
 
-      meta_window_update_monitor (window, flags & META_MOVE_RESIZE_USER_ACTION);
+      meta_window_update_monitor (window, update_monitor_flags);
 
       if (old_output_winsys_id != window->monitor->winsys_id &&
           flags & META_MOVE_RESIZE_MOVE_ACTION && flags & META_MOVE_RESIZE_USER_ACTION)
@@ -4042,7 +4049,7 @@ meta_window_move_resize_internal (MetaWindow          *window,
     }
   else
     {
-      meta_window_update_monitor (window, flags & META_MOVE_RESIZE_USER_ACTION);
+      meta_window_update_monitor (window, update_monitor_flags);
     }
 
   if ((result & META_MOVE_RESIZE_RESULT_FRAME_SHAPE_CHANGED) && window->frame_bounds)

src/wayland/meta-wayland-shell-surface.c
@@ -83,7 +83,7 @@ meta_wayland_shell_surface_set_window (MetaWaylandShellSurface *shell_surface,
     meta_wayland_surface_role_get_surface (surface_role);
 
   meta_wayland_surface_set_window (surface, window);
-  meta_window_update_monitor (window, FALSE);
+  meta_window_update_monitor (window, META_WINDOW_UPDATE_MONITOR_NONE);
 }
 
 void

src/wayland/meta-window-wayland.c
@@ -371,8 +371,8 @@ scale_rect_size (MetaRectangle *rect,
 }
 
 static void
-meta_window_wayland_update_main_monitor (MetaWindow *window,
-                                         gboolean    user_op)
+meta_window_wayland_update_main_monitor (MetaWindow                  *window,
+                                         MetaWindowUpdateMonitorFlags flags)
 {
   MetaBackend *backend = meta_get_backend ();
   MetaMonitorManager *monitor_manager =
@@ -392,7 +392,7 @@ meta_window_wayland_update_main_monitor (MetaWindow *window,
   toplevel_window = meta_wayland_surface_get_toplevel_window (window->surface);
   if (toplevel_window != window)
     {
-      meta_window_update_monitor (toplevel_window, user_op);
+      meta_window_update_monitor (toplevel_window, flags);
       window->monitor = toplevel_window->monitor;
       return;
     }
@@ -413,7 +413,7 @@ meta_window_wayland_update_main_monitor (MetaWindow *window,
       return;
     }
 
-  if (!user_op)
+  if (flags & META_WINDOW_UPDATE_MONITOR_CHANGED)
     {
       window->monitor = to;
       return;

src/x11/window-x11.c
@@ -1501,8 +1501,8 @@ meta_window_x11_update_icon (MetaWindow       *window,
 }
 
 static void
-meta_window_x11_update_main_monitor (MetaWindow *window,
-                                     gboolean    user_op)
+meta_window_x11_update_main_monitor (MetaWindow                  *window,
+                                     MetaWindowUpdateMonitorFlags flags)
 {
   window->monitor = meta_window_calculate_main_logical_monitor (window);
 }
@@ -3339,7 +3339,7 @@ meta_window_x11_configure_notify (MetaWindow      *window,
   priv->client_rect = window->rect;
   window->buffer_rect = window->rect;
 
-  meta_window_update_monitor (window, FALSE);
+  meta_window_update_monitor (window, META_WINDOW_UPDATE_MONITOR_NONE);
 
   /* Whether an override-redirect window is considered fullscreen depends
    * on its geometry.