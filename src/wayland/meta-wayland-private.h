bc5e76c76f7cb427cd00b54f98617409fe290d09
Jonas Ådahl <jadahl@gmail.com>
2018-08-23 15:49:17 +0200

    wayland: Make MetaWaylandCompositor a GObject
    This makes it possible to use GObject features such as data attachments.
    
    https://gitlab.gnome.org/GNOME/mutter/issues/276

--- src/wayland/meta-wayland-private.h
+++ src/wayland/meta-wayland-private.h
@@ -61,6 +61,8 @@ typedef struct
 
 struct _MetaWaylandCompositor
 {
+  GObject parent;
+
   struct wl_display *wayland_display;
   char *display_name;
   GHashTable *outputs;
@@ -73,5 +75,9 @@ struct _MetaWaylandCompositor
 
   GHashTable *scheduled_surface_associations;
 };
+
+#define META_TYPE_WAYLAND_COMPOSITOR (meta_wayland_compositor_get_type ())
+G_DECLARE_FINAL_TYPE (MetaWaylandCompositor, meta_wayland_compositor,
+                      META, WAYLAND_COMPOSITOR, GObject)
 
 #endif /* META_WAYLAND_PRIVATE_H */
