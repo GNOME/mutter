83b7b17b64b3e42a87f524ac3504be81789ec0fc
Jonas Ådahl <jadahl@gmail.com>
2018-08-24 12:56:31 +0200

    window: Force update monitor on hot plugs
    Commit a3da4b8d5bd217c0262fd9361036877d155a300f changed updating of
    window monitors to always use take affect when it was done from a
    non-user operation. This could cause feed back loops when a non-user
    driven operation would trigger the changing of a monitor, which itself
    would trigger changing of the monitor again due to a window scale
    change.
    
    The reason for the change, was that when the window monitor changed due
    to a hot plug, if it didn't actually change, eventually the window
    monitor pointer would be pointing to freed memory.
    
    Instead of force updating the monitor on all non-user operations, just
    do it on hot plugs. This allows for the feedback loop preventing logic
    to still do what its supposed to do, without risking dangling pointers
    on hot plugs.
    
    Related: https://gitlab.gnome.org/GNOME/mutter/issues/189
    Closes: https://gitlab.gnome.org/GNOME/mutter/issues/192
    
    (cherry picked from commit 8d3e053059cd202e740076caedfc8e3ac149066a)

--- src/core/window.c
+++ src/core/window.c
@@ -3805,7 +3805,7 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
   if (window->override_redirect || window->type == META_WINDOW_DESKTOP)
     {
       meta_window_update_monitor (window,
-                                  META_WINDOW_UPDATE_MONITOR_FLAGS_NONE);
+                                  META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE);
       return;
     }
 
@@ -3841,7 +3841,7 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
   else
     {
       meta_window_update_monitor (window,
-                                  META_WINDOW_UPDATE_MONITOR_FLAGS_NONE);
+                                  META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE);
     }
 }
 
