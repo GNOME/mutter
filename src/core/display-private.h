dacfe8bcaa2c2115e52f037d0530f25e61f9d619
Olivier Fourdan <ofourdan@redhat.com>
2018-09-21 17:49:36 +0200

    window: Add window id
    Generate a unique 64bit window-id which is unrelated to any windowing
    backend.

--- src/core/display-private.h
+++ src/core/display-private.h
@@ -427,4 +427,8 @@ void meta_display_queue_check_fullscreen (MetaDisplay *display);
 MetaWindow *meta_display_get_pointer_window (MetaDisplay *display,
                                              MetaWindow  *not_this_one);
 
+MetaWindow *meta_display_get_window_from_id (MetaDisplay *display,
+                                             uint64_t     win_id);
+uint64_t    meta_display_generate_window_id (MetaDisplay *display);
+
 #endif
