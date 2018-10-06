6b146e2c556f69aff1fe64d8e1166a0db4bfb249
Andrew Gaul <andrew@gaul.org>
2018-10-05 17:06:58 -0700

    Wire up middle-emulation to libinput
    This allows emulating middle click via simultaneous left and right
    click.  Fixes #238.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/256

--- src/meta/prefs.h
+++ src/meta/prefs.h
@@ -65,6 +65,7 @@
  * @META_PREF_AUTO_MAXIMIZE: auto-maximize
  * @META_PREF_CENTER_NEW_WINDOWS: center new windows
  * @META_PREF_DRAG_THRESHOLD: drag threshold
+ * @META_PREF_MIDDLE_EMULATION: middle emulation
  */
 
 /* Keep in sync with GSettings schemas! */
@@ -103,6 +104,7 @@ typedef enum
   META_PREF_AUTO_MAXIMIZE,
   META_PREF_CENTER_NEW_WINDOWS,
   META_PREF_DRAG_THRESHOLD,
+  META_PREF_MIDDLE_EMULATION,
 } MetaPreference;
 
 typedef void (* MetaPrefsChangedFunc) (MetaPreference pref,
@@ -176,6 +178,9 @@ gboolean                    meta_prefs_get_edge_tiling        (void);
 
 META_EXPORT
 gboolean                    meta_prefs_get_auto_maximize      (void);
+
+META_EXPORT
+gboolean                    meta_prefs_get_middle_emulation   (void);
 
 META_EXPORT
 gboolean                    meta_prefs_get_center_new_windows (void);
