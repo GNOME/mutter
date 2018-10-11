fdc8b74f04ba37a78f6f5b727ba4831f14f2ef32
Emilio Pozuelo Monfort <emilio.pozuelo@collabora.co.uk>
2018-10-11 16:12:53 +0200

    meta-gpu-kms: add identifier
    We need a device unique id in order to uniquely identify an output,
    which will use a (device, connector) pair. We use a guint16 to
    easily fit both device and connector into a glong.

--- src/backends/native/meta-gpu-kms.c
+++ src/backends/native/meta-gpu-kms.c
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (C) 2017 Red Hat
+ * Copyright (C) 2018 DisplayLink (UK) Ltd.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -57,6 +58,7 @@ struct _MetaGpuKms
 {
   MetaGpu parent;
 
+  uint16_t id;
   int fd;
   char *file_path;
   GSource *source;
@@ -372,6 +374,12 @@ int
 meta_gpu_kms_get_fd (MetaGpuKms *gpu_kms)
 {
   return gpu_kms->fd;
+}
+
+uint16_t
+meta_gpu_kms_get_id (MetaGpuKms *gpu_kms)
+{
+  return gpu_kms->id;
 }
 
 const char *
@@ -885,7 +893,10 @@ meta_gpu_kms_finalize (GObject *object)
 static void
 meta_gpu_kms_init (MetaGpuKms *gpu_kms)
 {
+  static uint16_t id = 0;
+
   gpu_kms->fd = -1;
+  gpu_kms->id = ++id;
 }
 
 static void
