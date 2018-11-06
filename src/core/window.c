GNOME/mutter

commit 8d3e053059cd202e740076caedfc8e3ac149066a

window: Force update monitor on hot plugs
Commit a3da4b8d5bd217c0262fd9361036877d155a300f changed updating of
window monitors to always use take affect when it was done from a
non-user operation. This could cause feed back loops when a non-user
driven operation would trigger the changing of a monitor, which itself
would trigger changing of the monitor again due to a window scale
change.

The reason for the change, was that when the window monitor changed due
to a hot plug, if it didn't actually change, eventually the window
monitor pointer would be pointing to freed memory.

Instead of force updating the monitor on all non-user operations, just
do it on hot plugs. This allows for the feedback loop preventing logic
to still do what its supposed to do, without risking dangling pointers
on hot plugs.

Related: https://gitlab.gnome.org/GNOME/mutter/issues/189
Closes: https://gitlab.gnome.org/GNOME/mutter/issues/192

src/core/window.c
@@ -3826,7 +3826,7 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
   if (window->override_redirect || window->type == META_WINDOW_DESKTOP)
     {
       meta_window_update_monitor (window,
-                                  META_WINDOW_UPDATE_MONITOR_FLAGS_NONE);
+                                  META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE);
       return;
     }
 
@@ -3862,7 +3862,7 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
   else
     {
       meta_window_update_monitor (window,
-                                  META_WINDOW_UPDATE_MONITOR_FLAGS_NONE);
+                                  META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE);
     }
 }
 

src/core/window-private.h
@@ -125,6 +125,7 @@ typedef enum _MetaWindowUpdateMonitorFlags
 {
   META_WINDOW_UPDATE_MONITOR_FLAGS_NONE = 0,
   META_WINDOW_UPDATE_MONITOR_FLAGS_USER_OP = 1 << 0,
+  META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE = 1 << 1,
 } MetaWindowUpdateMonitorFlags;
 
 typedef struct _MetaPlacementRule

src/wayland/meta-window-wayland.c
@@ -413,7 +413,7 @@ meta_window_wayland_update_main_monitor (MetaWindow                   *window,
       return;
     }
 
-  if (flags & META_WINDOW_UPDATE_MONITOR_FLAGS_USER_OP)
+  if (flags & META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE)
     {
       window->monitor = to;
       return;

commit f522c2651b5e038017d46eb53a9f66af2d26121d

window: Really force update monitor on hot plugs
Commit 8d3e05305 ("window: Force update monitor on hot plugs") added the
flag `META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE` passed to
`update_monitor()` from `update_for_monitors_changed()`.

However, `update_for_monitors_changed()` may choose to call another code
 path to `move_between_rects()` and `meta_window_move_resize_internal()`
eventually.

As `meta_window_move_resize_internal()` does not use the "force" flag,
we may still end up in case where the window->monitor is left unchanged.

To avoid that problem, make sure we call `update_monitor()`
unconditionally with the appropriate flag to force the monitor update in
`update_for_monitors_changed()`.

Fixes: 8d3e05305 ("window: Force update monitor on hot plugs")
Closes: https://gitlab.gnome.org/GNOME/mutter/issues/189

src/core/window.c
@@ -3868,11 +3868,11 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
                                       &old->rect,
                                       &new->rect);
     }
-  else
-    {
-      meta_window_update_monitor (window,
-                                  META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE);
-    }
+
+  /* meta_window_move_resize_internal() may not have updated the monitor,
+   * make sure we do it now... */
+  meta_window_update_monitor (window,
+                              META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE);
 }
 
 void