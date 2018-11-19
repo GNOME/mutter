5ca0572df985cbbec45359966b4bcf9bbe9596d9
Carlos Garnacho <carlosg@gnome.org>
2018-11-19 17:34:18 +0100

    wayland: Add wayland MetaSelectionSource implementation
    This object represents a Wayland selection owner. In order to invert the
    FD direction (we hand an output fd, but want an inpu fd), create an
    intermediate pipe so we can then create a GInputStream on top of it.

---
+++ src/wayland/meta-wayland-selection-source-private.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Author: Carlos Garnacho <carlosg@gnome.org>
+ */
+
+#ifndef META_WAYLAND_SELECTION_SOURCE_H
+#define META_WAYLAND_SELECTION_SOURCE_H
+
+#include <wayland-server.h>
+
+#include "core/meta-selection-source.h"
+
+#define META_TYPE_WAYLAND_SELECTION_SOURCE (meta_wayland_selection_source_get_type ())
+
+G_DECLARE_FINAL_TYPE (MetaWaylandSelectionSource,
+                      meta_wayland_selection_source,
+                      META, WAYLAND_SELECTION_SOURCE,
+                      MetaSelectionSource)
+
+typedef void (* MetaWaylandSendFunc) (struct wl_resource *resource,
+                                      const char         *mimetype,
+                                      int                 fd);
+typedef void (* MetaWaylandCancelFunc) (struct wl_resource *resource);
+
+MetaSelectionSource * meta_wayland_selection_source_new (struct wl_resource    *resource,
+                                                         GList                 *mime_types,
+                                                         MetaWaylandSendFunc    send_func,
+                                                         MetaWaylandCancelFunc  cancel_func);
+
+#endif /* META_WAYLAND_SELECTION_SOURCE_H */
