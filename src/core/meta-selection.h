32d642848dbe7f5f8f25f5f272ec893e6703b843
Carlos Garnacho <carlosg@gnome.org>
2018-11-19 16:45:20 +0100

    core: Add MetaSelection and MetaSelectionSource
    MetaSelectionSource represents a primary/clipboard/dnd selection owner,
    it is an abstract type so wayland/x11/etc implementations can be provided.
    These 3 selections are managed by the MetaSelection object, the current
    selection owners will be set there, and signals will be emitted so the
    previous selection owner can clean itself up.
    
    The actual data transfer is done through the meta_selection_transfer_async()
    call, which will take a GOutputStream and create a corresponding
    GInputStream from the MetaSelectionSource in order to splice them.

---
+++ src/core/meta-selection.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2018 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Author: Carlos Garnacho <carlosg@gnome.org>
+ */
+#ifndef META_SELECTION_H
+#define META_SELECTION_H
+
+#include <gio/gio.h>
+
+#include "core/meta-selection-source.h"
+#include "meta/display.h"
+
+#define META_TYPE_SELECTION (meta_selection_get_type ())
+
+G_DECLARE_FINAL_TYPE (MetaSelection,
+                      meta_selection,
+                      META, SELECTION,
+                      GObject)
+
+MetaSelection *
+         meta_selection_new                  (MetaDisplay *display);
+
+void     meta_selection_set_owner            (MetaSelection        *selection,
+                                              MetaSelectionType     selection_type,
+                                              MetaSelectionSource  *owner);
+void     meta_selection_unset_owner          (MetaSelection        *selection,
+                                              MetaSelectionType     selection_type,
+                                              MetaSelectionSource  *owner);
+
+GList *  meta_selection_get_mimetypes        (MetaSelection        *selection,
+                                              MetaSelectionType     selection_type);
+
+void     meta_selection_transfer_async       (MetaSelection        *selection,
+                                              MetaSelectionType     selection_type,
+                                              const gchar          *mimetype,
+                                              gssize                size,
+                                              GOutputStream        *output,
+                                              GCancellable         *cancellable,
+                                              GAsyncReadyCallback   callback,
+                                              gpointer              user_data);
+gboolean meta_selection_transfer_finish      (MetaSelection        *selection,
+                                              GAsyncResult         *result,
+                                              GError              **error);
+
+#endif /* META_SELECTION_H */
