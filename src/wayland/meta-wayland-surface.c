ad9e5b94c64b78d3f97fd4d8f83b7fd608aca6bf
Robert Mader <robert.mader@posteo.de>
2018-11-19 18:37:29 +0100

    wayland/meta-wayland-surface: add support for buffer-transforms
    Add support for buffer-transforms. Also includes some preparation work
    for wp_viewporter.

--- src/wayland/meta-wayland-surface.c
+++ src/wayland/meta-wayland-surface.c
@@ -32,7 +32,6 @@
 #include "clutter/wayland/clutter-wayland-compositor.h"
 #include "clutter/wayland/clutter-wayland-surface.h"
 #include "cogl/cogl-wayland-server.h"
-#include "compositor/meta-shaped-texture-private.h"
 #include "compositor/meta-surface-actor-wayland.h"
 #include "compositor/meta-surface-actor.h"
 #include "compositor/meta-window-actor-private.h"
@@ -247,12 +246,167 @@ meta_wayland_surface_assign_role (MetaWaylandSurface *surface,
     }
 }
 
+static int
+get_buffer_width (MetaWaylandSurface *surface)
+{
+  MetaWaylandBuffer *buffer = meta_wayland_surface_get_buffer (surface);
+
+  if (buffer)
+    {
+      CoglTexture *texture = meta_wayland_buffer_get_texture (buffer);
+      return cogl_texture_get_width (texture);
+    }
+  else
+    {
+      return 0;
+    }
+}
+
+static int
+get_buffer_height (MetaWaylandSurface *surface)
+{
+  MetaWaylandBuffer *buffer = meta_wayland_surface_get_buffer (surface);
+
+  if (buffer)
+    {
+      CoglTexture *texture = meta_wayland_buffer_get_texture (buffer);
+      return cogl_texture_get_height (texture);
+    }
+  else
+    {
+      return 0;
+    }
+}
+
+static void
+transformed_coord (int width, int height, MetaShapedTextureTransform transform,
+                   int scale, float sx, float sy, float *bx, float *by)
+{
+  switch (transform)
+    {
+      default:
+      case META_SHAPED_TEXTURE_TRANSFORM_NORMAL:
+        *bx = sx;
+        *by = sy;
+        break;
+      case META_SHAPED_TEXTURE_TRANSFORM_FLIPPED:
+        *bx = width - sx;
+        *by = sy;
+        break;
+      case META_SHAPED_TEXTURE_TRANSFORM_90:
+        *bx = width - sy;
+        *by = sx;
+        break;
+      case META_SHAPED_TEXTURE_TRANSFORM_FLIPPED_90:
+        *bx = width - sy;
+        *by = height - sx;
+        break;
+      case META_SHAPED_TEXTURE_TRANSFORM_180:
+        *bx = width - sx;
+        *by = height - sy;
+        break;
+      case META_SHAPED_TEXTURE_TRANSFORM_FLIPPED_180:
+        *bx = sx;
+        *by = height - sy;
+        break;
+      case META_SHAPED_TEXTURE_TRANSFORM_270:
+        *bx = sy;
+        *by = height - sx;
+        break;
+      case META_SHAPED_TEXTURE_TRANSFORM_FLIPPED_270:
+        *bx = sy;
+        *by = sx;
+        break;
+    }
+
+  *bx *= scale;
+  *by *= scale;
+}
+
+static cairo_rectangle_int_t
+transformed_rect (int width, int height, MetaShapedTextureTransform transform,
+                  int scale, cairo_rectangle_int_t rect)
+{
+  float x1, x2, y1, y2;
+  cairo_rectangle_int_t ret;
+
+  transformed_coord(width, height, transform, scale, rect.x, rect.y, &x1, &y1);
+  transformed_coord(width, height, transform, scale, rect.x + rect.width,
+                    rect.y + rect.height, &x2, &y2);
+
+  if (x1 <= x2)
+    {
+      ret.x = x1;
+      ret.width = x2 - x1;
+    }
+  else
+    {
+      ret.x = x2;
+      ret.width = x1 - x2;
+    }
+
+  if (y1 <= y2)
+    {
+      ret.y = y1;
+      ret.height = y2;
+    }
+  else
+    {
+      ret.y = y2;
+      ret.height = y1 - y2;
+    }
+
+  return ret;
+}
+
+static cairo_region_t *
+surface_to_buffer_region (MetaWaylandSurface *surface, cairo_region_t *region)
+{
+  int n_rects, i;
+  cairo_rectangle_int_t *rects;
+  cairo_rectangle_int_t surface_rect;
+  cairo_region_t *scaled_region;
+
+  if (surface->scale == 1 &&
+      surface->transform == META_SHAPED_TEXTURE_TRANSFORM_NORMAL)
+    return cairo_region_copy (region);
+
+  n_rects = cairo_region_num_rectangles (region);
+
+  rects = g_malloc (sizeof(cairo_rectangle_int_t) * n_rects);
+
+  for (i = 0; i < n_rects; i++)
+    {
+      cairo_region_get_rectangle (region, i, &rects[i]);
+      rects[i] = transformed_rect(get_buffer_width (surface) / surface->scale,
+                                  get_buffer_height (surface) / surface->scale,
+                                  surface->transform,
+                                  surface->scale,
+                                  rects[i]);
+    }
+
+  scaled_region = cairo_region_create_rectangles (rects, n_rects);
+
+  /* Intersect the scaled region to make sure no rounding errors made
+   * it to big */
+  surface_rect = (cairo_rectangle_int_t) {
+    .width  = get_buffer_width (surface),
+    .height = get_buffer_height (surface),
+  };
+  cairo_region_intersect_rectangle (scaled_region, &surface_rect);
+
+  g_free (rects);
+
+
+  return scaled_region;
+}
+
 static void
 surface_process_damage (MetaWaylandSurface *surface,
                         cairo_region_t     *surface_region,
                         cairo_region_t     *buffer_region)
 {
-  MetaWaylandBuffer *buffer = surface->buffer_ref.buffer;
+  MetaWaylandBuffer *buffer = meta_wayland_surface_get_buffer (surface);
   cairo_rectangle_int_t surface_rect;
   cairo_region_t *scaled_region;
   int i, n_rectangles;
@@ -275,7 +429,7 @@ surface_process_damage (MetaWaylandSurface *surface,
 
   /* The damage region must be in the same coordinate space as the buffer,
    * i.e. scaled with surface->scale. */
-  scaled_region = meta_region_scale (surface_region, surface->scale);
+  scaled_region = surface_to_buffer_region (surface, surface_region);
 
   /* Now add the buffer damage on top of the scaled damage region, as buffer
    * damage is already in that scale. */
@@ -352,7 +506,7 @@ meta_wayland_surface_ref_buffer_use_count (MetaWaylandSurface *surface)
 void
 meta_wayland_surface_unref_buffer_use_count (MetaWaylandSurface *surface)
 {
-  MetaWaylandBuffer *buffer = surface->buffer_ref.buffer;
+  MetaWaylandBuffer *buffer = meta_wayland_surface_get_buffer (surface);
 
   g_return_if_fail (surface->buffer_ref.use_count != 0);
 
@@ -380,7 +534,6 @@ pending_state_init (MetaWaylandPendingState *state)
   state->buffer_destroy_handler_id = 0;
   state->dx = 0;
   state->dy = 0;
-  state->scale = 0;
 
   state->input_region = NULL;
   state->input_region_set = FALSE;
@@ -394,6 +547,11 @@ pending_state_init (MetaWaylandPendingState *state)
   state->has_new_geometry = FALSE;
   state->has_new_min_size = FALSE;
   state->has_new_max_size = FALSE;
+
+  state->has_new_scale = FALSE;
+  state->has_new_transform = FALSE;
+  state->scale = 1;
+  state->transform = META_SHAPED_TEXTURE_TRANSFORM_NORMAL;
 }
 
 static void
@@ -498,8 +656,17 @@ merge_pending_state (MetaWaylandPendingState *from,
       to->has_new_max_size = TRUE;
     }
 
-  if (from->scale > 0)
-    to->scale = from->scale;
+  if (from->has_new_scale)
+    {
+      to->scale = from->scale;
+      to->has_new_scale = TRUE;
+    }
+
+  if (from->has_new_transform)
+    {
+      to->transform = from->transform;
+      to->has_new_transform = TRUE;
+    }
 
   if (to->buffer && to->buffer_destroy_handler_id == 0)
     {
@@ -673,8 +840,34 @@ meta_wayland_surface_apply_pending_state (MetaWaylandSurface      *surface,
                               !wl_shm_buffer_get (pending->buffer->resource));
     }
 
-  if (pending->scale > 0)
-    surface->scale = pending->scale;
+  if (pending->has_new_scale)
+    {
+      MetaSurfaceActor *actor = meta_wayland_surface_get_actor (surface);
+
+      surface->scale = pending->scale;
+
+      if (actor)
+        {
+          MetaShapedTexture *stex = meta_surface_actor_get_texture (actor);
+          meta_shaped_texture_invalidate_size (stex);
+        }
+    }
+
+  if (pending->has_new_transform)
+    {
+      MetaSurfaceActor *actor = meta_wayland_surface_get_actor (surface);
+
+      if (actor)
+        {
+          MetaShapedTexture *stex;
+
+          surface->transform = pending->transform;
+
+          stex = meta_surface_actor_get_texture (actor);
+          meta_shaped_texture_set_transform (stex,
+                                             surface->transform);
+        }
+    }
 
   if (meta_wayland_surface_get_actor (surface) &&
       (!cairo_region_is_empty (pending->surface_damage) ||
@@ -921,23 +1114,75 @@ wl_surface_commit (struct wl_client *client,
 }
 
 static void
-wl_surface_set_buffer_transform (struct wl_client *client,
-                                 struct wl_resource *resource,
-                                 int32_t transform)
-{
-  g_warning ("TODO: support set_buffer_transform request");
-}
-
-static void
 wl_surface_set_buffer_scale (struct wl_client *client,
                              struct wl_resource *resource,
                              int scale)
 {
   MetaWaylandSurface *surface = wl_resource_get_user_data (resource);
   if (scale > 0)
-    surface->pending->scale = scale;
+    {
+      if (scale != surface->scale)
+        {
+          surface->pending->scale = scale;
+          surface->pending->has_new_scale = TRUE;
+        }
+      else
+        {
+          surface->pending->has_new_scale = FALSE;
+        }
+    }
   else
-    g_warning ("Trying to set invalid buffer_scale of %d\n", scale);
+    {
+      g_warning ("Trying to set invalid buffer_scale of %d\n", scale);
+    }
+}
+
+static void
+wl_surface_set_buffer_transform (struct wl_client *client,
+                                 struct wl_resource *resource,
+                                 int32_t transform)
+{
+  MetaWaylandSurface *surface;
+  MetaWaylandPendingState *pending;
+
+  surface = wl_resource_get_user_data (resource);
+  pending = surface->pending;
+
+  if (surface->transform != (MetaShapedTextureTransform) transform)
+    {
+      switch (transform)
+        {
+          case 0:
+            pending->transform = META_SHAPED_TEXTURE_TRANSFORM_NORMAL;
+            break;
+          case 1:
+            pending->transform = META_SHAPED_TEXTURE_TRANSFORM_90;
+            break;
+          case 2:
+            pending->transform = META_SHAPED_TEXTURE_TRANSFORM_180;
+            break;
+          case 3:
+            pending->transform = META_SHAPED_TEXTURE_TRANSFORM_270;
+            break;
+          case 4:
+            pending->transform = META_SHAPED_TEXTURE_TRANSFORM_FLIPPED;
+            break;
+          case 5:
+            pending->transform = META_SHAPED_TEXTURE_TRANSFORM_FLIPPED_90;
+            break;
+          case 6:
+            pending->transform = META_SHAPED_TEXTURE_TRANSFORM_FLIPPED_180;
+            break;
+          case 7:
+            pending->transform = META_SHAPED_TEXTURE_TRANSFORM_FLIPPED_270;
+            break;
+          default:
+            g_warning ("Trying to set invalid buffer_transform of %d\n",
+                       transform);
+            return;
+        }
+      pending->has_new_transform = TRUE;
+    }
 }
 
 static void
@@ -1741,36 +1986,34 @@ meta_wayland_surface_notify_geometry_changed (MetaWaylandSurface *surface)
   g_signal_emit (surface, surface_signals[SURFACE_GEOMETRY_CHANGED], 0);
 }
 
+static int
+surface_get_dimension (MetaWaylandSurface *surface, gboolean getWidth)
+{
+  switch (surface->transform)
+    {
+      default:
+        break;
+      case META_SHAPED_TEXTURE_TRANSFORM_90:
+      case META_SHAPED_TEXTURE_TRANSFORM_270:
+      case META_SHAPED_TEXTURE_TRANSFORM_FLIPPED_90:
+      case META_SHAPED_TEXTURE_TRANSFORM_FLIPPED_270:
+        getWidth = !getWidth;
+        break;
+    }
+
+  int ret;
+  ret = getWidth ? get_buffer_width (surface) : get_buffer_height (surface);
+  return ret > 0 ? ret / surface->scale : 0;
+}
+
 int
 meta_wayland_surface_get_width (MetaWaylandSurface *surface)
 {
-  MetaWaylandBuffer *buffer;
-
-  buffer = surface->buffer_ref.buffer;
-  if (buffer)
-    {
-      CoglTexture *texture = meta_wayland_buffer_get_texture (buffer);
-      return cogl_texture_get_width (texture) / surface->scale;
-    }
-  else
-    {
-      return 0;
-    }
+  return surface_get_dimension(surface, TRUE);
 }
 
 int
 meta_wayland_surface_get_height (MetaWaylandSurface *surface)
 {
-  MetaWaylandBuffer *buffer;
-
-  buffer = surface->buffer_ref.buffer;
-  if (buffer)
-    {
-      CoglTexture *texture = meta_wayland_buffer_get_texture (buffer);
-      return cogl_texture_get_height (texture) / surface->scale;
-    }
-  else
-    {
-      return 0;
-    }
+  return surface_get_dimension(surface, FALSE);
 }
