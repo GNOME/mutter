17c3fb244f5cc6289c60850e08ab25f93de213c3
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-11-24 03:35:51 +0000

    monitor-manager-xrandr: Use monitor to calculate scaling
    In Xrandr we were caching the available scaling modes that were computed just
    for the current mode, for each monitor, while we can actually reuse the
    default implementation, by just passing the proper scaling constraint.
    
    In monitor we need then to properly filter these values, by only accepting
    integer scaling factors that would allow to have a minimal logical monitor
    size.
    
    Fixes https://gitlab.gnome.org/GNOME/mutter/issues/407
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/336

--- src/backends/meta-monitor.c
+++ src/backends/meta-monitor.c
@@ -1570,6 +1570,16 @@ is_logical_size_large_enough (gint width, gint height)
          height >= MINIMUM_LOGICAL_HEIGHT;
 }
 
+static gboolean
+is_scale_valid_for_size (float width,
+                         float height,
+                         float scale)
+{
+  return scale >= MINIMUM_SCALE_FACTOR &&
+         scale <= MAXIMUM_SCALE_FACTOR &&
+         is_logical_size_large_enough (floorf (width/scale), floorf (width/scale));
+}
+
 gboolean
 meta_monitor_mode_should_be_advertised (MetaMonitorMode *monitor_mode)
 {
@@ -1593,21 +1603,17 @@ get_closest_scale_factor_for_resolution (float width,
   gboolean found_one;
 
   best_scale = 0;
-  scaled_w = width / scale;
-  scaled_h = height / scale;
 
-  if (scale < MINIMUM_SCALE_FACTOR ||
-      scale > MAXIMUM_SCALE_FACTOR ||
-      !is_logical_size_large_enough (floorf (scaled_w), floorf (scaled_h)))
+  if (!is_scale_valid_for_size (width, height, scale))
     goto out;
 
-  if (floorf (scaled_w) == scaled_w && floorf (scaled_h) == scaled_h)
+  if (fmodf (width, scale) == 0.0 && fmodf (height, scale) == 0.0)
     return scale;
 
   i = 0;
   found_one = FALSE;
   limit_exceeded = FALSE;
-  base_scaled_w = floorf (scaled_w);
+  base_scaled_w = floorf (width / scale);
 
   do
     {
@@ -1668,16 +1674,18 @@ meta_monitor_calculate_supported_scales (MetaMonitor                 *monitor,
           float scale;
           float scale_value = i + j * SCALE_FACTORS_STEPS;
 
-          if ((constraints & META_MONITOR_SCALES_CONSTRAINT_NO_FRAC) &&
-              fmodf (scale_value, 1.0) != 0.0)
+          if (constraints & META_MONITOR_SCALES_CONSTRAINT_NO_FRAC)
             {
-              continue;
+              if (fmodf (scale_value, 1.0) != 0.0 ||
+                  !is_scale_valid_for_size (width, height, scale_value))
+                continue;
+
+              scale = scale_value;
             }
-
-          scale = get_closest_scale_factor_for_resolution (width,
-                                                           height,
-                                                           scale_value);
-
+          else
+            scale = get_closest_scale_factor_for_resolution (width,
+                                                             height,
+                                                             scale_value);
           if (scale > 0.0f)
             g_array_append_val (supported_scales, scale);
         }
