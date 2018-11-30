8f9ee59a5ad7567cc61f0564f30a93dac518fec4
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-11-29 23:04:17 -0500

    monitor-manager: Check if all monitor scales are matching in global mode
    When global scaling is set we need to ensure that all the requested scale
    configurations are matching, otherwise we'd end up in a mixed setup that
    we don't support in this scenario.
    
    Fixes #407

--- src/backends/meta-monitor-manager.c
+++ src/backends/meta-monitor-manager.c
@@ -92,6 +92,10 @@ meta_monitor_manager_is_config_complete (MetaMonitorManager *manager,
 static MetaMonitor *
 meta_monitor_manager_get_active_monitor (MetaMonitorManager *manager);
 
+static gboolean
+is_global_scale_matching_in_config (MetaMonitorsConfig *config,
+                                    float               scale);
+
 MetaBackend *
 meta_monitor_manager_get_backend (MetaMonitorManager *manager)
 {
@@ -1502,6 +1506,43 @@ meta_monitor_manager_is_scale_supported (MetaMonitorManager          *manager,
 }
 
 static gboolean
+is_global_scale_matching_in_config (MetaMonitorsConfig *config,
+                                    float               scale)
+{
+  GList *l;
+
+  for (l = config->logical_monitor_configs; l; l = l->next)
+    {
+      MetaLogicalMonitorConfig *logical_monitor_config = l->data;
+
+      if (logical_monitor_config->scale != scale)
+        return FALSE;
+    }
+
+  return TRUE;
+}
+
+static gboolean
+meta_monitor_manager_is_scale_supported_for_config (MetaMonitorManager *manager,
+                                                    MetaMonitorsConfig *config,
+                                                    MetaMonitor        *monitor,
+                                                    MetaMonitorMode    *monitor_mode,
+                                                    float               scale)
+{
+  if (meta_monitor_manager_is_scale_supported (manager, config->layout_mode,
+                                               monitor, monitor_mode, scale))
+    {
+      if (meta_monitor_manager_get_capabilities (manager) &
+          META_MONITOR_MANAGER_CAPABILITY_GLOBAL_SCALE_REQUIRED)
+        return is_global_scale_matching_in_config (config, scale);
+
+      return TRUE;
+    }
+
+  return FALSE;
+}
+
+static gboolean
 meta_monitor_manager_is_config_applicable (MetaMonitorManager *manager,
                                            MetaMonitorsConfig *config,
                                            GError            **error)
@@ -1539,11 +1580,11 @@ meta_monitor_manager_is_config_applicable (MetaMonitorManager *manager,
               return FALSE;
             }
 
-          if (!meta_monitor_manager_is_scale_supported (manager,
-                                                        config->layout_mode,
-                                                        monitor,
-                                                        monitor_mode,
-                                                        scale))
+          if (!meta_monitor_manager_is_scale_supported_for_config (manager,
+                                                                   config,
+                                                                   monitor,
+                                                                   monitor_mode,
+                                                                   scale))
             {
               g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
                            "Scale not supported by backend");
