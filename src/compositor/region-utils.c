8e3ad76533c6a1b4777e529729af4e22284e5861
Robert Mader <robert.mader@posteo.de>
2018-11-30 18:02:35 +0100

    region-utils: Add API to crop and scale an integer region

--- src/compositor/region-utils.c
+++ src/compositor/region-utils.c
@@ -408,3 +408,42 @@ meta_region_transform (cairo_region_t       *region,
 
   return transformed_region;
 }
+
+cairo_region_t *
+meta_region_crop_and_scale (cairo_region_t *region,
+                            ClutterRect    *src_rect,
+                            int             dst_width,
+                            int             dst_height)
+{
+  int n_rects, i;
+  cairo_rectangle_int_t *rects;
+  cairo_region_t *viewport_region;
+
+  if (src_rect->origin.x == 0 &&
+      src_rect->origin.y == 0 &&
+      src_rect->size.width == dst_width &&
+      src_rect->size.height == dst_height)
+    {
+      return cairo_region_copy (region);
+    }
+
+  n_rects = cairo_region_num_rectangles (region);
+
+  rects = g_new0 (cairo_rectangle_int_t, n_rects);
+  for (i = 0; i < n_rects; i++)
+    {
+      cairo_region_get_rectangle (region, i, &rects[i]);
+
+      meta_rectangle_crop_and_scale (&rects[i],
+                                     src_rect,
+                                     dst_width,
+                                     dst_height,
+                                     &rects[i]);
+    }
+
+  viewport_region = cairo_region_create_rectangles (rects, n_rects);
+
+  g_free (rects);
+
+  return viewport_region;
+}
