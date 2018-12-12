01c763470e5ed18c05215e92583b8e888ba6a49a
Olivier Fourdan <ofourdan@redhat.com>
2018-12-12 13:05:21 +0100

    window: Expose the client type in the API
    We already have the enum exposed, but no accessor function.
    
    Add `meta_window_get_client_type()` which returns the
    `MetaWindowClientType` of a window.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/306
    (cherry picked from commit 7a5e0c78244b31052edfe3ea4ce1a1f48c94f93f)

--- src/meta/window.h
+++ src/meta/window.h
@@ -262,4 +262,6 @@ void     meta_window_shove_titlebar_onscreen (MetaWindow *window);
 
 uint64_t meta_window_get_id (MetaWindow *window);
 
+MetaWindowClientType meta_window_get_client_type (MetaWindow *window);
+
 #endif
