a646e972c2cd0806acd84aa0a6bf545e8c3d607f
Daniel van Vugt <daniel.van.vugt@canonical.com>
2018-12-20 15:50:54 +0800

    clutter-stage-cogl: Remove magic numbers
    Instead of 0Hz falling back to 60Hz, use `$CLUTTER_DEFAULT_FPS` instead,
    which is also 60Hz by default.
    
    And instead of stupid refresh rates >1MHz falling back to 60Hz, just
    honour them instead by rendering unthrottled (same as `sync_delay < 0`).
    Although I wouldn't actually expect this path to ever be needed in
    reality - it just ensures an infinite `while` loop never happens.

--- clutter/clutter/cogl/clutter-stage-cogl.c
+++ clutter/clutter/cogl/clutter-stage-cogl.c
@@ -175,12 +175,15 @@ clutter_stage_cogl_schedule_update (ClutterStageWindow *stage_window,
     }
 
   refresh_rate = stage_cogl->refresh_rate;
-  if (refresh_rate == 0.0)
-    refresh_rate = 60.0;
+  if (refresh_rate <= 0.0)
+    refresh_rate = clutter_get_default_frame_rate ();
 
   refresh_interval = (gint64) (0.5 + 1000000 / refresh_rate);
   if (refresh_interval == 0)
-    refresh_interval = 16667; /* 1/60th second */
+    {
+      stage_cogl->update_time = now;
+      return;
+    }
 
   stage_cogl->update_time = stage_cogl->last_presentation_time + 1000 * sync_delay;
 
