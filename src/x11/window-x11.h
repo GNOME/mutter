c70b078ec18e6b63e1f1c2d5186a40a85c40a39b
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2018-12-22 23:46:15 -0200

    window-x11: Cleanup header and shuffle function locations
    The functions meta_window_x11_create_sync_request_alarm() and
    meta_window_x11_destroy_sync_request_alarm() are not used outside
    MetaWindowX11 now, and don't need to be exposed in window-x11.h
    
    Remove them from window-x11.h and shuffle these functions to
    before their first usage, to avoid having to add their prototypes.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/372

--- src/x11/window-x11.h
+++ src/x11/window-x11.h
@@ -53,8 +53,6 @@ void meta_window_x11_set_net_wm_state            (MetaWindow *window);
 void meta_window_x11_set_wm_state                (MetaWindow *window);
 void meta_window_x11_set_allowed_actions_hint    (MetaWindow *window);
 
-void meta_window_x11_create_sync_request_alarm   (MetaWindow *window);
-void meta_window_x11_destroy_sync_request_alarm  (MetaWindow *window);
 void meta_window_x11_update_sync_request_counter (MetaWindow *window,
                                                   gint64      new_counter_value);
 
