6e487b879a9b67ef918f546c7faf401be810a5ec
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2018-12-23 12:24:26 -0200

    Add MetaWindowContent
    MetaWindowContent is a GObject class that implements ClutterContent.
    It draws the contents of the window, unshadowed in the case of server
    side decorated windows on X11, as a composition of the contents of
    the subsurfaces.

---
+++ src/compositor/meta-window-content-private.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2018 Endless, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Written by:
+ *     Georges Basile Stavracas Neto <gbsneto@gnome.org>
+ */
+
+#ifndef META_WINDOW_CONTENT_PRIVATE_H
+#define META_WINDOW_CONTENT_PRIVATE_H
+
+#include "meta/meta-window-content.h"
+#include "meta/meta-window-actor.h"
+
+MetaWindowContent* meta_window_content_new (MetaWindowActor *window_actor);
+
+#endif /* META_WINDOW_CONTENT_PRIVATE_H */
--- src/meta/meta-window-actor.h
+++ src/meta/meta-window-actor.h
@@ -51,6 +51,9 @@ META_EXPORT
 cairo_surface_t * meta_window_actor_get_image (MetaWindowActor       *self,
                                                cairo_rectangle_int_t *clip);
 
+META_EXPORT
+ClutterContent *meta_window_actor_get_content (MetaWindowActor *self);
+
 typedef enum
 {
   META_SHADOW_MODE_AUTO,
