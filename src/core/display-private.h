de9ed77ebb12d6b883228f2892e8b1fd68e99d78
Carlos Garnacho <carlosg@gnome.org>
2018-12-30 18:25:08 +0100

    x11: Separate X11 focus handling into MetaX11Display method
    Updating the MetaWindow focus and the X Window focus is interrelated but
    independent. Call one after the other in the places we handle window focus
    changes.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/420

--- src/core/display-private.h
+++ src/core/display-private.h
@@ -140,14 +140,6 @@ struct _MetaDisplay
    */
   guint allow_terminal_deactivation : 1;
 
-  /* If true, server->focus_serial refers to us changing the focus; in
-   * this case, we can ignore focus events that have exactly focus_serial,
-   * since we take care to make another request immediately afterwards.
-   * But if focus is being changed by another client, we have to accept
-   * multiple events with the same serial.
-   */
-  guint focused_by_us : 1;
-
   /*< private-ish >*/
   GHashTable *stamps;
   GHashTable *wayland_windows;
@@ -371,10 +363,7 @@ gboolean meta_display_modifiers_accelerator_activate (MetaDisplay *display);
 
 void meta_display_sync_wayland_input_focus (MetaDisplay *display);
 void meta_display_update_focus_window (MetaDisplay *display,
-                                       MetaWindow  *window,
-                                       Window       xwindow,
-                                       gulong       serial,
-                                       gboolean     focused_by_us);
+                                       MetaWindow  *window);
 
 void meta_display_sanity_check_timestamps (MetaDisplay *display,
                                            guint32      timestamp);
