b8e6e74fdc21a67c436ee3c515622608755b78b7
Carlos Garnacho <carlosg@gnome.org>
2018-12-30 13:55:52 +0100

    x11: Move X11 calls to map/unmap a MetaWindow to MetaWindowX11
    Add 2 vmethods so that MetaWindowX11 may handle the X11 calls itself.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/420

--- src/x11/window-x11.c
+++ src/x11/window-x11.c
@@ -1707,6 +1707,27 @@ meta_window_x11_are_updates_frozen (MetaWindow *window)
 }
 
 static void
+meta_window_x11_map (MetaWindow *window)
+{
+  MetaX11Display *x11_display = window->display->x11_display;
+
+  meta_x11_error_trap_push (x11_display);
+  XMapWindow (x11_display->xdisplay, window->xwindow);
+  meta_x11_error_trap_pop (x11_display);
+}
+
+static void
+meta_window_x11_unmap (MetaWindow *window)
+{
+  MetaX11Display *x11_display = window->display->x11_display;
+
+  meta_x11_error_trap_push (x11_display);
+  XUnmapWindow (x11_display->xdisplay, window->xwindow);
+  meta_x11_error_trap_pop (x11_display);
+  window->unmaps_pending ++;
+}
+
+static void
 meta_window_x11_class_init (MetaWindowX11Class *klass)
 {
   MetaWindowClass *window_class = META_WINDOW_CLASS (klass);
@@ -1733,6 +1754,8 @@ meta_window_x11_class_init (MetaWindowX11Class *klass)
   window_class->is_stackable = meta_window_x11_is_stackable;
   window_class->can_ping = meta_window_x11_can_ping;
   window_class->are_updates_frozen = meta_window_x11_are_updates_frozen;
+  window_class->map = meta_window_x11_map;
+  window_class->unmap = meta_window_x11_unmap;
 }
 
 void
