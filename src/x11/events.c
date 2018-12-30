3be68bad3a41f3b1df62c9165c5edba9206b1f39
Carlos Garnacho <carlosg@gnome.org>
2018-12-30 12:58:31 +0100

    x11: Move focus sentinel to MetaX11Display
    This focus sentinel is a mechanism to avoid some X11-specific race
    conditions in focus-follows-pointer, using X11 mechanisms. Move it to
    MetaX11Display altogether.

--- src/x11/events.c
+++ src/x11/events.c
@@ -882,7 +882,7 @@ handle_input_xevent (MetaX11Display *x11_display,
           enter_event->mode != XINotifyGrab &&
           enter_event->mode != XINotifyUngrab &&
           enter_event->detail != XINotifyInferior &&
-          meta_display_focus_sentinel_clear (display))
+          meta_x11_display_focus_sentinel_clear (x11_display))
         {
           meta_window_handle_enter (window,
                                     enter_event->time,
@@ -1525,7 +1525,7 @@ handle_other_xevent (MetaX11Display *x11_display,
             if (event->xproperty.atom ==
                 x11_display->atom__MUTTER_SENTINEL)
               {
-                meta_display_decrement_focus_sentinel (display);
+                meta_x11_display_decrement_focus_sentinel (x11_display);
               }
           }
       }
