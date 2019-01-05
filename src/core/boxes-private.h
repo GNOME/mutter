5e59b9cbbb746adefff449aec6a19d923b982179
Robert Mader <robert.mader@posteo.de>
2019-01-05 16:39:55 +0100

    boxes: Add API to crop and scale a MetaRectangle
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/323

--- src/core/boxes-private.h
+++ src/core/boxes-private.h
@@ -252,4 +252,10 @@ void meta_rectangle_transform (const MetaRectangle  *rect,
                                int                   height,
                                MetaRectangle        *dest);
 
+void meta_rectangle_crop_and_scale (const MetaRectangle *rect,
+                                    ClutterRect         *src_rect,
+                                    int                  dst_width,
+                                    int                  dst_height,
+                                    MetaRectangle       *dest);
+
 #endif /* META_BOXES_PRIVATE_H */
