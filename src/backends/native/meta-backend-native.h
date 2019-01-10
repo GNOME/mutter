97d48ab7f45f937f499711f665191fa6f61c2e58
Jonas Ådahl <jadahl@gmail.com>
2019-01-10 11:30:02 +0100

    backends/native: Add udev abstraction layer
    To be used to signal devices added, hotplugs and other udev events.
    Currently the only event emitted is when a device is added.
    
    https://gitlab.gnome.org/GNOME/mutter/issues/548
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/525

--- src/backends/native/meta-backend-native.h
+++ src/backends/native/meta-backend-native.h
@@ -28,6 +28,7 @@
 #include "backends/meta-backend-private.h"
 #include "backends/native/meta-clutter-backend-native.h"
 #include "backends/native/meta-launcher.h"
+#include "backends/native/meta-udev.h"
 
 #define META_TYPE_BACKEND_NATIVE (meta_backend_native_get_type ())
 G_DECLARE_FINAL_TYPE (MetaBackendNative, meta_backend_native,
@@ -40,5 +41,7 @@ void meta_backend_native_pause (MetaBackendNative *backend_native);
 void meta_backend_native_resume (MetaBackendNative *backend_native);
 
 MetaLauncher * meta_backend_native_get_launcher (MetaBackendNative *native);
+
+MetaUdev * meta_backend_native_get_udev (MetaBackendNative *native);
 
 #endif /* META_BACKEND_NATIVE_H */
