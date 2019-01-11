d526283ab94f4b424bb62d1ccf83695cd4a11f04
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-01-11 10:18:27 -0600

    compositor: Make it a GObject
    This means we can later use GObject features like signals, subclassing
    etc.
    
    Bump glib_req version as per g_clear_handle_id usage.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/385

--- src/meta/compositor.h
+++ src/meta/compositor.h
@@ -28,6 +28,10 @@
 #include <meta/window.h>
 #include <meta/workspace.h>
 
+#define META_TYPE_COMPOSITOR (meta_compositor_get_type ())
+META_EXPORT
+G_DECLARE_FINAL_TYPE (MetaCompositor, meta_compositor, META, COMPOSITOR, GObject)
+
 /**
  * MetaCompEffect:
  * @META_COMP_EFFECT_CREATE: The window is newly created
