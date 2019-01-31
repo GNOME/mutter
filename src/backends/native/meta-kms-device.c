03cc83b31e5ddbd0e77de5d93f4a365d4f46ab87
Jonas Ådahl <jadahl@gmail.com>
2019-01-31 18:48:19 +0100

    kms: Add plane representation
    A plane is one of three possible: primary, overlay and cursor. Each
    plane can have various properties, such as possible rotations, formats
    etc. Each plane can also be used with a set of CRTCs.
    
    A primary plane is the "backdrop" of a CRTC, i.e. the primary output for
    the composited frame that covers the whole CRTC. In general, mutter
    composites to a stage view frame onto a framebuffer that is then put on
    the primary plane.
    
    An overlay plane is a rectangular area that can be displayed on top of
    the primary plane. Eventually it will be used to place non-fullscreen
    surfaces, potentially avoiding stage redraws.
    
    A cursor plane is a plane placed on top of all the other planes, usually
    used to put the mouse cursor sprite.
    
    Initially, we only fetch the rotation properties, and we so far
    blacklist all rotations except ones that ends up with the same
    dimensions as with no rotations. This is because non-180° rotations
    doesn't work yet due to incorrect buffer modifiers. To make it possible
    to use non-180° rotations, changes necessary include among other things
    finding compatible modifiers using atomic modesetting. Until then,
    simply blacklist the ones we know doesn't work.
    
    https://gitlab.gnome.org/GNOME/mutter/issues/548
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/525

--- src/backends/native/meta-kms-device.c
+++ src/backends/native/meta-kms-device.c
@@ -38,6 +38,7 @@ struct _MetaKmsDevice
   char *path;
 
   GList *crtcs;
+  GList *planes;
 };
 
 G_DEFINE_TYPE (MetaKmsDevice, meta_kms_device, G_TYPE_OBJECT);
@@ -73,6 +74,7 @@ typedef struct _CreateImplDeviceData
 
   MetaKmsImplDevice *out_impl_device;
   GList *out_crtcs;
+  GList *out_planes;
 } CreateImplDeviceData;
 
 static gboolean
@@ -87,6 +89,7 @@ create_impl_device_in_impl (MetaKmsImpl  *impl,
 
   data->out_impl_device = impl_device;
   data->out_crtcs = meta_kms_impl_device_copy_crtcs (impl_device);
+  data->out_planes = meta_kms_impl_device_copy_planes (impl_device);
 
   return TRUE;
 }
@@ -127,6 +130,7 @@ meta_kms_device_new (MetaKms            *kms,
   device->flags = flags;
   device->path = g_strdup (path);
   device->crtcs = data.out_crtcs;
+  device->planes = data.out_planes;
 
   return device;
 }
@@ -166,6 +170,7 @@ meta_kms_device_finalize (GObject *object)
   GError *error = NULL;
 
   g_list_free (device->crtcs);
+  g_list_free (device->planes);
 
   data = (FreeImplDeviceData) {
     .impl_device = device->impl_device,
