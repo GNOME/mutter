c57daa16dc96ea23b2c97651bd0dd4c7e5cb0245
verdre <verdre@v0yd.nl>
2019-02-03 20:48:40 +0100

    window: Don't treat new touches like TOUCH_UPDATE events during grab ops
    Treating TOUCH_BEGIN events the same way as TOUCH_UPDATE events while a
    window is being dragged or resized causes the window to jump when a new
    touch is added. Instead, simply check if the new sequence is the pointer
    emulating sequence and don't handle the event if it isn't.

--- src/core/window.c
+++ src/core/window.c
@@ -6467,6 +6467,12 @@ meta_window_handle_mouse_grab_op_event  (MetaWindow         *window,
 
   switch (event->type)
     {
+    case CLUTTER_TOUCH_BEGIN:
+      if (!meta_display_is_pointer_emulating_sequence (window->display, sequence))
+        return FALSE;
+
+      return TRUE;
+
     case CLUTTER_BUTTON_PRESS:
       {
         ClutterModifierType grab_mods = meta_display_get_window_grab_modifiers (window->display);
@@ -6499,17 +6505,11 @@ meta_window_handle_mouse_grab_op_event  (MetaWindow         *window,
 
       return TRUE;
 
-    case CLUTTER_TOUCH_BEGIN:
-      /* This will only catch the keybinding and menu cases, just deal with this
-       * like a CLUTTER_TOUCH_UPDATE rather than a CLUTTER_BUTTON_PRESS, and
-       * wait until CLUTTER_TOUCH_END to undo the grab, just so the window
-       * doesn't warp below the finger and remain there.
-       */
     case CLUTTER_TOUCH_UPDATE:
       if (!meta_display_is_pointer_emulating_sequence (window->display, sequence))
         return FALSE;
 
-      /* Fall through */
+    /* Fall through */
     case CLUTTER_MOTION:
       modifier_state = clutter_event_get_state (event);
       clutter_event_get_coords (event, &x, &y);
