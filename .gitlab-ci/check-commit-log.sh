90b205a132b8cd006d0f754294a5b8a1cc087325
Jonas Ådahl <jadahl@gmail.com>
2019-02-05 18:19:43 +0100

    gitlab-ci.yml: Add check for issue or MR URL
    This adds a pipeline stage for merge requests that checks that the
    commit message contains an URL to either a issue or a merge request.
    This means that for merge requests without corresponding issues will
    always fail initially, as the merge request URL is not known until after
    it is created. This is still arguably better than accidentally merging
    merge requests without URLs.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/440

---
+++ .gitlab-ci/check-commit-log.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+test -z "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" && { echo Cannot review non-merge request; exit 1; }
+
+echo git remote -v:
+git remote -v
+
+echo target branch commit: $(git show -s --format=format:%H $CI_MERGE_REQUEST_TARGET_BRANCH_NAME)
+echo head commit: $(git show -s --format=format:%H HEAD)
+
+git fetch $CI_MERGE_REQUEST_PROJECT_URL.git $CI_MERGE_REQUEST_TARGET_BRANCH_NAME
+
+branch_point=$(git merge-base HEAD FETCH_HEAD)
+echo branch point: $branch_point
+
+commits=$(git log --format='format:%H' $branch_point..$CI_COMMIT_SHA)
+echo commits: $commits
+
+test -z "$commits" && { echo Commit range empty; exit 1; }
+
+for commit in $commits; do
+  git show -s --format='format:%b' $commit | grep -qe "\($CI_MERGE_REQUEST_PROJECT_URL/\(issues\|merge_requests\)/[0-9]\+\|https://bugzilla.gnome.org/show_bug.cgi?id=[0-9]\+\)" ||
+    { echo "Missing merge request or issue URL on commit $(echo $commit | cut -c -8)"; exit 1; }
+done
