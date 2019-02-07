5241d3fc8775753de52d6b4677660c3197cf7dda
Adam Jackson <ajax@redhat.com>
2019-02-07 09:41:03 -0500

    clutter: Remove deprecated/clutter-util.h

--- clutter/clutter/clutter-util.c
+++ clutter/clutter/clutter-util.c
@@ -39,29 +39,6 @@
 #include "clutter-interval.h"
 #include "clutter-private.h"
 
-#include "deprecated/clutter-util.h"
-
-/**
- * clutter_util_next_p2:
- * @a: Value to get the next power
- *
- * Calculates the nearest power of two, greater than or equal to @a.
- *
- * Return value: The nearest power of two, greater or equal to @a.
- *
- * Deprecated: 1.2
- */
-gint
-clutter_util_next_p2 (gint a)
-{
-  int rval = 1;
-
-  while (rval < a)
-    rval <<= 1;
-
-  return rval;
-}
-
 /* Help macros to scale from OpenGL <-1,1> coordinates system to
  * window coordinates ranging [0,window-size]
  */
