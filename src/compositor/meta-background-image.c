28e2637731b93a1bd008095034f119c23e3af369
Jonas Dreßler <verdre@v0yd.nl>
2019-02-08 09:05:14 +1100

    background: Add function to get color information about wallpaper-area
    Allow desktop environments showing text, icons or other information on
    top of the background image to determine the characteristics of the area
    used, so they can ensure the content has enough contrast.
    
    The function takes the monitor index, the starting point and the size of
    the area to analyze as arguments and calculates the area's mean
    luminance, the variance of the luminance and the mean acutance.
    
    To do this in an efficient way, we cache parts of the background images
    luminance information during loading the image. The size of this cache
    is limited and the cached data only consists of 3/4 of the actual number
    of pixels in the area.
    
    The weights of the primary colors were taken from and the calculation
    code was heavily inspired by elementary's wingpanel, see
    get_background_color_information() in wingpanel-interface/Utils.vala.
    (https://github.com/elementary/wingpanel/blob/master/wingpanel-interface/Utils.vala)

--- src/compositor/meta-background-image.c
+++ src/compositor/meta-background-image.c
@@ -67,6 +67,8 @@ struct _MetaBackgroundImage
   gboolean in_cache;
   gboolean loaded;
   CoglTexture *texture;
+  uint8_t *pixel_lums;
+  uint pixel_lums_height;
 };
 
 G_DEFINE_TYPE (MetaBackgroundImageCache, meta_background_image_cache, G_TYPE_OBJECT);
@@ -120,6 +122,11 @@ meta_background_image_cache_get_default (void)
   return cache;
 }
 
+/* The maximum size of the image-part we cache in kilobytes starting from the upper left corner.
+ * We use it to calculate luminance and acutance values for requested areas of the image.
+ */
+#define MAX_CACHED_SIZE 1000
+
 static void
 load_file (GTask               *task,
            MetaBackgroundImage *image,
@@ -129,6 +136,11 @@ load_file (GTask               *task,
   GError *error = NULL;
   GdkPixbuf *pixbuf;
   GFileInputStream *stream;
+  uint image_width, image_height, n_channels, rowstride;
+  uint small_line_size, two_lines_size, n_two_lines, n_points, n_lines;
+  uint pixel_count = 0;
+  guchar *pixels;
+  uint8_t *pixel_lums;
 
   stream = g_file_read (image->file, NULL, &error);
   if (stream == NULL)
@@ -145,6 +157,69 @@ load_file (GTask               *task,
       g_task_return_error (task, error);
       return;
     }
+
+  image_width = gdk_pixbuf_get_width (pixbuf);
+  image_height = gdk_pixbuf_get_height (pixbuf);
+  n_channels = gdk_pixbuf_get_n_channels (pixbuf);
+  rowstride = gdk_pixbuf_get_rowstride (pixbuf);
+  pixels = gdk_pixbuf_get_pixels (pixbuf);
+
+  /* To save memory, we only store 3/4 of the image and not the full RGB data,
+   * but only the luminance of every pixel. Also, the size of the cached data is
+   * limited by MAX_CACHED_SIZE.
+   * In particular, we store the value of every second pixel for all even rows,
+   * and the value of every pixel for all uneven rows. This allows calculating the
+   * acutance for 1/4 of all pixels, which is still enough to not miss any small
+   * patterns in the image.
+   *
+   * For example, the cached data of a 10x6 image looks like this:
+   *  ----------
+   * | x x x x x|
+   * |xxxxxxxxxx|
+   * | x x x x x|
+   * |xxxxxxxxxx|
+   * | x x x x x|
+   * |          |
+   *  ----------
+   */
+
+  small_line_size = floor (image_width / 2);
+  two_lines_size = small_line_size + image_width;
+  n_two_lines = floor ((MAX_CACHED_SIZE * 1000 - small_line_size) / two_lines_size);
+
+  /* We always append another every-second-pixel line to the end */
+  if (n_two_lines * 2 + 1 > image_height)
+    n_two_lines = floor ((image_height - 1) / 2);
+
+  n_points =  n_two_lines * two_lines_size + small_line_size;
+  n_lines = n_two_lines * 2 + 1;
+
+  pixel_lums = g_malloc (sizeof(uint8_t) * n_points);
+
+  for (uint y = 0; y < n_lines; y++)
+    for (uint x = 0; x < image_width; x++)
+      {
+        /* Skip every second (even) column if the row number is even */
+        if (y % 2 != 1 && x % 2 != 1)
+          continue;
+
+        uint i = y * rowstride + x * n_channels;
+
+        uint8_t r = pixels[i];
+        uint8_t g = pixels[i + 1];
+        uint8_t b = pixels[i + 2];
+
+        /* The weight of the different colors is taken from elementary's wingpanel,
+         * see meta_background_image_get_color_info().
+         */
+        uint8_t pixel = (0.3 * r + 0.59 * g + 0.11 * b) ;
+
+        pixel_lums[pixel_count] = pixel;
+        pixel_count++;
+      }
+
+  image->pixel_lums = pixel_lums;
+  image->pixel_lums_height = n_lines;
 
   g_task_return_pointer (task, pixbuf, (GDestroyNotify) g_object_unref);
 }
@@ -302,6 +377,8 @@ meta_background_image_finalize (GObject *object)
     cogl_object_unref (image->texture);
   if (image->file)
     g_object_unref (image->file);
+  if (image->pixel_lums)
+    g_free (image->pixel_lums);
 
   G_OBJECT_CLASS (meta_background_image_parent_class)->finalize (object);
 }
@@ -367,4 +444,103 @@ meta_background_image_get_texture (MetaBackgroundImage *image)
   g_return_val_if_fail (META_IS_BACKGROUND_IMAGE (image), NULL);
 
   return image->texture;
+}
+
+/**
+ * meta_background_image_get_color_info:
+ * @image: A #MetaBackgroundImage
+ * @image_area: The area of the image to analyze as #cairo_rectangle_int_t
+ * @mean_luminance: (out): The mean luminance as #float
+ * @luminance_variance: (out): Variance of the luminance as #float
+ * @mean_acutance: (out): The mean acutance as #float
+ *
+ * Gets color information about a specified area of a background image.
+ * Calculates the mean luminance, variance of the luminance and the mean
+ * acutance of the area.
+ * This only works if the requested area is inside the cached part of the
+ * image, the size of this part is limited to MAX_CACHED_SIZE.
+ *
+ * Return value: %TRUE if the calculation was successful, %FALSE if the area
+ *  is not completely cached or if the given input was invalid.
+ **/
+gboolean
+meta_background_image_get_color_info (MetaBackgroundImage   *image,
+                                      cairo_rectangle_int_t *image_area,
+                                      float                 *mean_luminance,
+                                      float                 *luminance_variance,
+                                      float                 *mean_acutance)
+{
+  uint texture_width;
+  unsigned long int acutance_mean = 0, luminance_mean = 0, luminance_mean_squares = 0;
+  uint8_t pixel = 0;
+  uint x = 0, y = 0, luminance_values_count = 0, acutance_values_count = 0;
+
+  g_return_val_if_fail (META_IS_BACKGROUND_IMAGE (image), FALSE);
+
+  texture_width = cogl_texture_get_width (image->texture);
+
+  uint small_line_size = floor (texture_width / 2);
+  uint two_lines_size = small_line_size + texture_width;
+
+  if (image_area->width == 0 ||
+      image_area->x + image_area->width > texture_width ||
+      image_area->x < 0)
+    return FALSE;
+
+  if (image_area->height == 0 ||
+      image_area->y + image_area->height > image->pixel_lums_height ||
+      image_area->y < 0)
+    return FALSE;
+
+  /* Code to calculate luminance and acutance is from elementary's wingpanel,
+   * see wingpanel-interface/Utils.vala - get_background_color_information():
+   * https://github.com/elementary/wingpanel/blob/master/wingpanel-interface/Utils.vala
+   */
+  for (y = image_area->y; y < image_area->y + image_area->height; y++)
+    for (x = image_area->x; x < image_area->x + image_area->width; x++)
+      {
+        /* For uneven row numbers we have every pixel stored, for the others every second pixel */
+        if (y % 2 == 1 || x % 2 == 1)
+          {
+            uint i = floor (y / 2) * two_lines_size;
+            i += (y % 2 == 1) ? small_line_size + x :
+                                floor (x / 2);
+
+            pixel = image->pixel_lums[i];
+            luminance_mean += pixel;
+            luminance_mean_squares += pixel * pixel;
+            luminance_values_count++;
+
+            /* Only use uneven rows where we store every pixel for calculating acutance.
+             * Also skip the edge rows and colums since we look at the four pixels adjacent
+             * to the current pixel.
+             */
+            if (y % 2 == 1 && x % 2 == 1 &&
+                y > image_area->y && y < image_area->y + image_area->height - 1 &&
+                x > image_area->x && x < image_area->x + image_area->width - 1)
+              {
+                uint i_above = i - x - small_line_size + (int) (x / 2);
+                uint i_below = i - x + texture_width + (int) (x / 2);
+
+                float acutance =
+                  (pixel * 4) -
+                  (
+                    image->pixel_lums[i - 1] +
+                    image->pixel_lums[i + 1] +
+                    image->pixel_lums[i_above] +
+                    image->pixel_lums[i_below]
+                  );
+
+                acutance_mean += abs (acutance);
+                acutance_values_count++;
+              }
+          }
+      }
+
+  *mean_luminance = (float) luminance_mean / luminance_values_count;
+  *luminance_variance = ((float) luminance_mean_squares / luminance_values_count) - (*mean_luminance * *mean_luminance);
+  if (acutance_values_count > 0)
+    *mean_acutance = (float) acutance_mean / acutance_values_count;
+
+  return TRUE;
 }
