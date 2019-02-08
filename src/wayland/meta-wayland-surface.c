7a4518ae0b64e0a2c7f43364e0f948853ae9bd44
Jonas Ådahl <jadahl@gmail.com>
2019-02-08 11:46:48 +0100

    wayland: Move surface texture ownership to MetaWaylandSurface
    Prior to this commit, MetaWaylandSurface held a reference to
    MetaWaylandBuffer, who owned the texture drawn by the surface. When
    switching buffer, the texture change with it.
    
    This is problematic when dealing with SHM buffer damage management, as
    when having one texture per buffer, damaged regions uploaded to one,
    will not follow along to the next one attached. It also wasted GPU
    memory as there would be one texture per buffer, instead of one one
    texture per surface.
    
    Instead, move the texture ownership to MetaWaylandSurface, and have the
    SHM buffer damage management update the surface texture. This ensures
    damage is processed properly, and that we won't end up with stale
    texture content when doing partial texture uploads. If the same SHM
    buffer is attached to multiple surfaces, each surface will get their own
    copy, and damage is tracked and uploaded separately.
    
    Non-SHM types of buffers still has their own texture reference, as the
    texture is just a representation of the GPU memory associated with the
    buffer. When such a buffer is attached to a surface, instead the surface
    just gets a reference to that texture, instead of a separately allocated
    one.
    
    Fixes: https://gitlab.gnome.org/GNOME/mutter/issues/199

--- src/wayland/meta-wayland-surface.c
+++ src/wayland/meta-wayland-surface.c
@@ -245,14 +245,9 @@ get_buffer_width (MetaWaylandSurface *surface)
   MetaWaylandBuffer *buffer = meta_wayland_surface_get_buffer (surface);
 
   if (buffer)
-    {
-      CoglTexture *texture = meta_wayland_buffer_get_texture (buffer);
-      return cogl_texture_get_width (texture);
-    }
+    return cogl_texture_get_width (surface->texture);
   else
-    {
-      return 0;
-    }
+    return 0;
 }
 
 static int
@@ -261,14 +256,9 @@ get_buffer_height (MetaWaylandSurface *surface)
   MetaWaylandBuffer *buffer = meta_wayland_surface_get_buffer (surface);
 
   if (buffer)
-    {
-      CoglTexture *texture = meta_wayland_buffer_get_texture (buffer);
-      return cogl_texture_get_height (texture);
-    }
+    return cogl_texture_get_height (surface->texture);
   else
-    {
-      return 0;
-    }
+    return 0;
 }
 
 static void
@@ -341,8 +331,8 @@ surface_process_damage (MetaWaylandSurface *surface,
 
   cairo_region_intersect_rectangle (buffer_region, &buffer_rect);
 
-  /* First update the buffer. */
-  meta_wayland_buffer_process_damage (buffer, buffer_region);
+  /* First update the surface texture. */
+  meta_wayland_buffer_process_damage (buffer, surface->texture, buffer_region);
 
   if (meta_wayland_surface_get_actor (surface))
     {
@@ -694,8 +684,6 @@ meta_wayland_surface_apply_pending_state (MetaWaylandSurface      *surface,
 
   if (pending->newly_attached)
     {
-      gboolean switched_buffer;
-
       if (!surface->buffer_ref.buffer && surface->window)
         meta_window_queue (surface->window, META_QUEUE_CALC_SHOWING);
 
@@ -707,8 +695,7 @@ meta_wayland_surface_apply_pending_state (MetaWaylandSurface      *surface,
       if (surface->buffer_held)
         meta_wayland_surface_unref_buffer_use_count (surface);
 
-      switched_buffer = g_set_object (&surface->buffer_ref.buffer,
-                                      pending->buffer);
+      g_set_object (&surface->buffer_ref.buffer, pending->buffer);
 
       if (pending->buffer)
         meta_wayland_surface_ref_buffer_use_count (surface);
@@ -716,19 +703,23 @@ meta_wayland_surface_apply_pending_state (MetaWaylandSurface      *surface,
       if (pending->buffer)
         {
           GError *error = NULL;
+          gboolean changed_texture;
 
-          if (!meta_wayland_buffer_attach (pending->buffer, &error))
+          if (!meta_wayland_buffer_attach (pending->buffer,
+                                           &surface->texture,
+                                           &changed_texture,
+                                           &error))
             {
               g_warning ("Could not import pending buffer: %s", error->message);
               wl_resource_post_error (surface->resource, WL_DISPLAY_ERROR_NO_MEMORY,
-                                      "Failed to create a texture for surface %i: %s",
+                                      "Failed to attach buffer to surface %i: %s",
                                       wl_resource_get_id (surface->resource),
                                       error->message);
               g_error_free (error);
               goto cleanup;
             }
 
-          if (switched_buffer && meta_wayland_surface_get_actor (surface))
+          if (changed_texture && meta_wayland_surface_get_actor (surface))
             {
               MetaShapedTexture *stex;
               CoglTexture *texture;
@@ -736,7 +727,7 @@ meta_wayland_surface_apply_pending_state (MetaWaylandSurface      *surface,
               gboolean is_y_inverted;
 
               stex = meta_surface_actor_get_texture (meta_wayland_surface_get_actor (surface));
-              texture = meta_wayland_buffer_get_texture (pending->buffer);
+              texture = surface->texture;
               snippet = meta_wayland_buffer_create_snippet (pending->buffer);
               is_y_inverted = meta_wayland_buffer_is_y_inverted (pending->buffer);
 
@@ -1330,6 +1321,7 @@ wl_surface_destructor (struct wl_resource *resource)
 
   if (surface->buffer_held)
     meta_wayland_surface_unref_buffer_use_count (surface);
+  g_clear_pointer (&surface->texture, cogl_object_unref);
   g_clear_object (&surface->buffer_ref.buffer);
 
   g_clear_object (&surface->pending);
@@ -1849,6 +1841,12 @@ meta_wayland_surface_is_shortcuts_inhibited (MetaWaylandSurface *surface,
     return FALSE;
 
   return g_hash_table_contains (surface->shortcut_inhibited_seats, seat);
+}
+
+CoglTexture *
+meta_wayland_surface_get_texture (MetaWaylandSurface *surface)
+{
+  return surface->texture;
 }
 
 MetaSurfaceActor *
