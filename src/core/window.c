5efb11acadd9e344d15fd2e6ce1f6180a541975b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-02-14 13:57:22 +0100

    window: Warn if try to focus unmanaging windows
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/307
    (cherry picked from commit e14613e74e8c9bb8e08dc7a94e35a86c8baf7a01)

--- src/core/window.c
+++ src/core/window.c
@@ -8562,6 +8562,8 @@ meta_window_shortcuts_inhibited (MetaWindow         *window,
 gboolean
 meta_window_is_focusable (MetaWindow *window)
 {
+  g_return_val_if_fail (!window->unmanaging, FALSE);
+
   return META_WINDOW_GET_CLASS (window)->is_focusable (window);
 }
 
