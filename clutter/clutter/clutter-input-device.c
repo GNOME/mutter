0abb53444d85c8984b9bd9150bf9f79613ae15a8
Jonas Dreßler <verdre@v0yd.nl>
2019-02-14 23:37:04 +0100

    events: Move counting of touch sequences out of gesture-tracker
    To support pointer emulating sequences on Wayland, we count touch
    sequences and flag the first sequence as pointer-emulating.
    
    Since the gesture-tracker stored a list of sequences anyway, we used the
    tracker for this. Now, after the gesture-tracking refactoring, the
    tracker doesn't have to store the list anymore in Wayland sessions.
    
    Instead we just use the number of touch sequences of the
    ClutterInputDevice to find the pointer emulating sequence.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/424

--- clutter/clutter/clutter-input-device.c
+++ clutter/clutter/clutter-input-device.c
@@ -1474,6 +1474,25 @@ clutter_input_device_get_axis_value (ClutterInputDevice *device,
 }
 
 /**
+ * clutter_input_device_get_n_sequences:
+ * @device: a #ClutterInputDevice
+ *
+ * Retrieves the number of sequences that are active on the
+ * @device right now.
+ *
+ * Return value: the current number of sequences on the device
+ *
+ * Since: ?
+ */
+guint
+clutter_input_device_get_n_sequences (ClutterInputDevice *device)
+{
+  g_return_val_if_fail (CLUTTER_IS_INPUT_DEVICE (device), 0);
+
+  return g_hash_table_size (device->touch_sequences_info);
+}
+
+/**
  * clutter_input_device_get_n_axes:
  * @device: a #ClutterInputDevice
  *
