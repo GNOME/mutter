ba5f81a32a88f2ef40b2dd46fbd36dc41fe5e23a
Robert Mader <robert.mader@posteo.de>
2019-03-11 14:33:38 +0100

    wayland/surface: Post error on invalid scale
    to follow the spec and make faulty clients fail hard.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/647

--- src/wayland/meta-wayland-surface.c
+++ src/wayland/meta-wayland-surface.c
@@ -1072,10 +1072,17 @@ wl_surface_set_buffer_scale (struct wl_client *client,
                              int scale)
 {
   MetaWaylandSurface *surface = wl_resource_get_user_data (resource);
-  if (scale > 0)
-    surface->pending->scale = scale;
-  else
-    g_warning ("Trying to set invalid buffer_scale of %d\n", scale);
+
+  if (scale <= 0)
+    {
+      wl_resource_post_error (resource,
+                              WL_SURFACE_ERROR_INVALID_SCALE,
+                              "Trying to set invalid buffer_scale of %d\n",
+                              scale);
+      return;
+    }
+
+  surface->pending->scale = scale;
 }
 
 static void
