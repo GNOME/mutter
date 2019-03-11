e282afc554d5b058bef6fa77cbd83e2d1c190b59
Jonas Ådahl <jadahl@gmail.com>
2019-03-11 11:13:01 +0100

    backend/native: Move some KMS utilities to its own file
    They are not strictly related to any of the KMS objects, and should be
    reusable without adding a dependency on the non-meta-kms-* files in
    meta-kms-*.
    
    https://gitlab.gnome.org/GNOME/mutter/issues/548
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/525

--- src/backends/native/meta-crtc-kms.c
+++ src/backends/native/meta-crtc-kms.c
@@ -24,18 +24,10 @@
 
 #include "backends/native/meta-crtc-kms.h"
 
-#include <drm_fourcc.h>
-#include <drm_mode.h>
-
 #include "backends/meta-backend-private.h"
 #include "backends/native/meta-gpu-kms.h"
 #include "backends/native/meta-kms-device.h"
 #include "backends/native/meta-kms-plane.h"
-
-/* added in libdrm 2.4.95 */
-#ifndef DRM_FORMAT_INVALID
-#define DRM_FORMAT_INVALID 0
-#endif
 
 #define ALL_TRANSFORMS_MASK ((1 << META_MONITOR_N_TRANSFORMS) - 1)
 
@@ -48,39 +40,6 @@ typedef struct _MetaCrtcKms
 
   MetaKmsPlane *primary_plane;
 } MetaCrtcKms;
-
-/**
- * meta_drm_format_to_string:
- * @tmp: temporary buffer
- * @drm_format: DRM fourcc pixel format
- *
- * Returns a pointer to a string naming the given pixel format,
- * usually a pointer to the temporary buffer but not always.
- * Invalid formats may return nonsense names.
- *
- * When calling this, allocate one MetaDrmFormatBuf on the stack to
- * be used as the temporary buffer.
- */
-const char *
-meta_drm_format_to_string (MetaDrmFormatBuf *tmp,
-                           uint32_t          drm_format)
-{
-  int i;
-
-  if (drm_format == DRM_FORMAT_INVALID)
-    return "INVALID";
-
-  G_STATIC_ASSERT (sizeof (tmp->s) == 5);
-  for (i = 0; i < 4; i++)
-    {
-      char c = (drm_format >> (i * 8)) & 0xff;
-      tmp->s[i] = g_ascii_isgraph (c) ? c : '.';
-    }
-
-  tmp->s[i] = 0;
-
-  return tmp->s;
-}
 
 gboolean
 meta_crtc_kms_is_transform_handled (MetaCrtc             *crtc,
