d906d2992dcedf7ab89af97fcc277bcfb0a9052e
Carlos Garnacho <carlosg@gnome.org>
2019-03-29 14:46:58 +0100

    clutter: Remove support for foreign stages
    We have no use for this.

--- clutter/clutter/x11/clutter-x11.h
+++ clutter/clutter/x11/clutter-x11.h
@@ -106,9 +106,6 @@ void     clutter_x11_set_display         (Display * xdpy);
 
 CLUTTER_EXPORT
 Window       clutter_x11_get_stage_window  (ClutterStage *stage);
-CLUTTER_EXPORT
-gboolean     clutter_x11_set_stage_foreign (ClutterStage *stage,
-                                            Window        xwindow);
 
 CLUTTER_EXPORT
 void         clutter_x11_add_filter    (ClutterX11FilterFunc func,
