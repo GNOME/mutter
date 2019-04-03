64cab0f63005813c8d6de83f907a06d1dd3616e2
Pekka Paalanen <pekka.paalanen@collabora.com>
2019-04-03 13:11:45 +0300

    renderer/native: Make EGL initialization failure not fatal
    The failure to initialize EGL does not necessarily mean the KMS device cannot
    be used. The device could still be used as a "secondary GPU" with the CPU copy
    mode.
    
    If meta_renderer_native_create_renderer_gpu_data () fails,
    meta_renderer_native_get_gpu_data () will return NULL, which may cause crashes.
    This patch removes most of the failures, but does not fix the NULL dereferences
    that will still happen if creating gpu data fails.
    
    This patch reorders create_renderer_gpu_data_gbm () so that it fails hard only
    if GBM device cannot be created, and otherwise always returns an initialized
    gpu data structure. Users of the gpu data structure are responsible for
    checking egl_display validity.
    
    Fixes: https://gitlab.gnome.org/GNOME/mutter/issues/542
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/521

--- src/backends/native/meta-renderer-native.c
+++ src/backends/native/meta-renderer-native.c
@@ -3798,19 +3798,6 @@ create_renderer_gpu_data_gbm (MetaRendererNative  *renderer_native,
   int kms_fd;
   MetaRendererNativeGpuData *renderer_gpu_data;
 
-  if (!meta_egl_has_extensions (egl, EGL_NO_DISPLAY, NULL,
-                                "EGL_MESA_platform_gbm",
-                                NULL) &&
-      !meta_egl_has_extensions (egl, EGL_NO_DISPLAY, NULL,
-                                "EGL_KHR_platform_gbm",
-                                NULL))
-    {
-      g_set_error (error, G_IO_ERROR,
-                   G_IO_ERROR_FAILED,
-                   "Missing extension for GBM renderer: EGL_KHR_platform_gbm");
-      return NULL;
-    }
-
   kms_fd = meta_gpu_kms_get_fd (gpu_kms);
 
   gbm_device = gbm_create_device (kms_fd);
@@ -3822,22 +3809,40 @@ create_renderer_gpu_data_gbm (MetaRendererNative  *renderer_native,
       return NULL;
     }
 
+  renderer_gpu_data = meta_create_renderer_native_gpu_data (gpu_kms);
+  renderer_gpu_data->renderer_native = renderer_native;
+  renderer_gpu_data->gbm.device = gbm_device;
+  renderer_gpu_data->mode = META_RENDERER_NATIVE_MODE_GBM;
+  renderer_gpu_data->egl_display = EGL_NO_DISPLAY;
+
+  if (!meta_egl_has_extensions (egl, EGL_NO_DISPLAY, NULL,
+                                "EGL_MESA_platform_gbm",
+                                NULL) &&
+      !meta_egl_has_extensions (egl, EGL_NO_DISPLAY, NULL,
+                                "EGL_KHR_platform_gbm",
+                                NULL))
+    {
+      g_set_error (error, G_IO_ERROR,
+                   G_IO_ERROR_FAILED,
+                   "Missing extension for GBM renderer: EGL_KHR_platform_gbm");
+      init_secondary_gpu_data_cpu (renderer_gpu_data);
+      return renderer_gpu_data;
+    }
+
   egl_display = meta_egl_get_platform_display (egl,
                                                EGL_PLATFORM_GBM_KHR,
                                                gbm_device, NULL, error);
   if (egl_display == EGL_NO_DISPLAY)
     {
-      gbm_device_destroy (gbm_device);
-      return NULL;
+      init_secondary_gpu_data_cpu (renderer_gpu_data);
+      return renderer_gpu_data;
     }
 
   if (!meta_egl_initialize (egl, egl_display, error))
-    return NULL;
-
-  renderer_gpu_data = meta_create_renderer_native_gpu_data (gpu_kms);
-  renderer_gpu_data->renderer_native = renderer_native;
-  renderer_gpu_data->gbm.device = gbm_device;
-  renderer_gpu_data->mode = META_RENDERER_NATIVE_MODE_GBM;
+    {
+      init_secondary_gpu_data_cpu (renderer_gpu_data);
+      return renderer_gpu_data;
+    }
   renderer_gpu_data->egl_display = egl_display;
 
   init_secondary_gpu_data (renderer_gpu_data);
@@ -4041,7 +4046,7 @@ meta_renderer_native_create_renderer_gpu_data (MetaRendererNative  *renderer_nat
                                                GError             **error)
 {
   MetaRendererNativeGpuData *renderer_gpu_data;
-  GError *gbm_error = NULL;
+  g_autoptr (GError) gbm_error = NULL;
 #ifdef HAVE_EGL_DEVICE
   GError *egl_device_error = NULL;
 #endif
@@ -4059,9 +4064,19 @@ meta_renderer_native_create_renderer_gpu_data (MetaRendererNative  *renderer_nat
     return renderer_gpu_data;
 #endif
 
+  /* May set gbm_error even if "succeeds", because failure to
+   * initialize EGL is not fatal at this point.
+   */
   renderer_gpu_data = create_renderer_gpu_data_gbm (renderer_native,
                                                     gpu_kms,
                                                     &gbm_error);
+  if (gbm_error)
+    {
+      g_debug ("GPU %s GBM-mode initialization cancelled with: %s",
+               meta_gpu_kms_get_file_path (gpu_kms),
+               gbm_error->message);
+    }
+
   if (renderer_gpu_data)
     {
 #ifdef HAVE_EGL_DEVICE
@@ -4083,7 +4098,6 @@ meta_renderer_native_create_renderer_gpu_data (MetaRendererNative  *renderer_nat
 #endif
   );
 
-  g_error_free (gbm_error);
 #ifdef HAVE_EGL_DEVICE
   g_error_free (egl_device_error);
 #endif
