2878dee9474a6a4f2bb084c73070418bccdac30f
Takao Fujiwara <tfujiwar@redhat.com>
2019-04-04 18:47:08 +0900

    clutter: Enable negative offsets in delete surrounding text
    The input method can assign a negative value to
    clutter_input_method_delete_surrounding() to move the cursor to the left.
    But Wayland protocol accepts positive values in delete_surrounding() and
    GTK converts the values to the negative ones in
    text_input_delete_surrounding_text_apply().
    
    https://gitlab.gnome.org/GNOME/mutter/issues/539

--- src/wayland/meta-wayland-text-input.c
+++ src/wayland/meta-wayland-text-input.c
@@ -166,17 +166,19 @@ meta_wayland_text_input_focus_defer_done (ClutterInputFocus *focus)
 
 static void
 meta_wayland_text_input_focus_delete_surrounding (ClutterInputFocus *focus,
-                                                  guint              cursor,
+                                                  gint               offset,
                                                   guint              len)
 {
   MetaWaylandTextInput *text_input;
   struct wl_resource *resource;
+  guint before_length = offset <= 0 ? -offset : offset;
+  guint after_length = len >= before_length ? (len - before_length) : len + before_length;
 
   text_input = META_WAYLAND_TEXT_INPUT_FOCUS (focus)->text_input;
 
   wl_resource_for_each (resource, &text_input->focus_resource_list)
     {
-      zwp_text_input_v3_send_delete_surrounding_text (resource, cursor, len);
+      zwp_text_input_v3_send_delete_surrounding_text (resource, before_length, after_length);
     }
 
   meta_wayland_text_input_focus_defer_done (focus);
