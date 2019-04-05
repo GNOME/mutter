af269b2fb96d81be3752b224eeae76c41a2cbd34
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-04-04 23:36:06 -0400

    cogl/tests: Use less verbose run-tests.sh on single tests run by meson
    Use less verbose output when run-tests.sh is running only a test and we're
    requested for less verbose output.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/442

--- cogl/tests/run-tests.sh
+++ cogl/tests/run-tests.sh
@@ -32,13 +32,15 @@ EXIT=0
 MISSING_FEATURE="WARNING: Missing required feature";
 KNOWN_FAILURE="WARNING: Test is known to fail";
 
-echo "Key:"
-echo "ok = Test passed"
-echo "n/a = Driver is missing a feature required for the test"
-echo "FAIL = Unexpected failure"
-echo "FIXME = Test failed, but it was an expected failure"
-echo "PASS! = Unexpected pass"
-echo ""
+if [ -z "$RUN_TESTS_QUIET" ]; then
+  echo "Key:"
+  echo "ok = Test passed"
+  echo "n/a = Driver is missing a feature required for the test"
+  echo "FAIL = Unexpected failure"
+  echo "FIXME = Test failed, but it was an expected failure"
+  echo "PASS! = Unexpected pass"
+  echo ""
+fi
 
 get_status()
 {
@@ -88,6 +90,11 @@ run_test()
   fi
 }
 
+if [ -z "$UNIT_TESTS" ]; then
+  echo Missing unit-tests file or names
+  exit 1
+fi
+
 TITLE_FORMAT="%35s"
 printf "$TITLE_FORMAT" "Test"
 
@@ -101,15 +108,15 @@ if test "$HAVE_GLES2" -eq 1; then
 fi
 
 echo ""
-echo ""
-
-if [ -z "$UNIT_TESTS" ]; then
-  echo Missing unit-tests file or names
-  exit 1
-fi
 
 if [ -f "$UNIT_TESTS" ]; then
   UNIT_TESTS="$(cat $UNIT_TESTS)"
+fi
+
+if [ -n "$RUN_TESTS_QUIET" ] && [ "$(echo "$UNIT_TESTS" | wc -w )" -eq 1 ]; then
+  SINGLE_TEST=true
+else
+  echo ""
 fi
 
 for test in $UNIT_TESTS
@@ -147,7 +154,8 @@ do
     run_test "$test" gles2_npot
   fi
 
-  printf $TITLE_FORMAT "$test:"
+  printf $TITLE_FORMAT
+  [ "$SINGLE_TEST" != true ] && printf "$test:"
   if test "$HAVE_GL" -eq 1; then
     printf "$GL_FORMAT" \
       "$(get_status "$gl_glsl_result")" \
