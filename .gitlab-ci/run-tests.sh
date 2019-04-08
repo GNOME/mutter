ee58c1b4ab160bb2d66926c4e1f30aa1a8792b9e
Niels De Graef <niels.degraef@barco.com>
2019-04-08 14:34:13 +0200

    ci: Generate a cover report for the test suite
    Based on @ebassi's work from https://gitlab.gnome.org/GNOME/glib/merge_requests/769
    
    This MR allows us to see the succeeded/failed tests each time the CI
    pipeline is run.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/528

---
+++ .gitlab-ci/run-tests.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+set +e
+
+mkdir -m 700 $XDG_RUNTIME_DIR
+glib-compile-schemas $GSETTINGS_SCHEMA_DIR
+
+dbus-run-session -- \
+    xvfb-run -s '+iglx -noreset' \
+        meson test -C build --no-rebuild -t 10 --wrap catchsegv
+
+exit_code=$?
+
+python3 .gitlab-ci/meson-junit-report.py \
+        --project-name=mutter \
+        --job-id "${CI_JOB_NAME}" \
+        --output "build/${CI_JOB_NAME}-report.xml" \
+        build/meson-logs/testlog.json
+
+exit $exit_code
