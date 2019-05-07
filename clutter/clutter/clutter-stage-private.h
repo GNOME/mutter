e9517550af86e9b37f014c11d68825be387712d5
Daniel van Vugt <daniel.van.vugt@canonical.com>
2019-05-07 13:02:55 +0800

    clutter/stage: Add an API for shallow relayouts
    Where "shallow" means you can specify any actor instead of every relayout
    having to start from the stage.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/575

--- clutter/clutter/clutter-stage-private.h
+++ clutter/clutter/clutter-stage-private.h
@@ -134,6 +134,9 @@ void            _clutter_stage_presented                (ClutterStage      *stag
 
 GList *         _clutter_stage_peek_stage_views         (ClutterStage *stage);
 
+void            _clutter_stage_queue_actor_relayout     (ClutterStage *stage,
+                                                         ClutterActor *actor);
+
 G_END_DECLS
 
 #endif /* __CLUTTER_STAGE_PRIVATE_H__ */
