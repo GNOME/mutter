162f7601b07a504a602905bc9bedad6fcc161665
Carlos Garnacho <carlosg@gnome.org>
2019-05-24 20:52:17 +0200

    wayland: Rename xwayland init/shutdown functions
    The start/stop verbs will be reused later when we can start and
    stop the X server. Rename these functions to init/shutdown, and
    init_xserver() to start_xserver().

--- src/wayland/meta-xwayland.c
+++ src/wayland/meta-xwayland.c
@@ -542,7 +542,7 @@ on_displayfd_ready (int          fd,
 }
 
 static gboolean
-meta_xwayland_init_xserver (MetaXWaylandManager *manager)
+meta_xwayland_start_xserver (MetaXWaylandManager *manager)
 {
   int xwayland_client_fd[2];
   int displayfd[2];
@@ -616,8 +616,8 @@ meta_xwayland_init_xserver (MetaXWaylandManager *manager)
 }
 
 gboolean
-meta_xwayland_start (MetaXWaylandManager *manager,
-                     struct wl_display   *wl_display)
+meta_xwayland_init (MetaXWaylandManager *manager,
+                    struct wl_display   *wl_display)
 {
   if (!choose_xdisplay (manager))
     return FALSE;
@@ -626,7 +626,7 @@ meta_xwayland_start (MetaXWaylandManager *manager,
     return FALSE;
 
   manager->wayland_display = wl_display;
-  return meta_xwayland_init_xserver (manager);
+  return meta_xwayland_start_xserver (manager);
 }
 
 static void
@@ -652,7 +652,7 @@ meta_xwayland_complete_init (MetaDisplay *display)
 }
 
 void
-meta_xwayland_stop (MetaXWaylandManager *manager)
+meta_xwayland_shutdown (MetaXWaylandManager *manager)
 {
   char path[256];
 
