10bce387cd3c637b43fe089b9e6490da491bfee8
Pekka Paalanen <pekka.paalanen@collabora.com>
2019-06-11 11:16:39 +0300

    renderer/native: Add tracing for 2nd GPU copies
    These traces allow seeing how long the copy operations stall in libmutter, and
    which copy operations actually get used.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/615

--- src/backends/native/meta-renderer-native.c
+++ src/backends/native/meta-renderer-native.c
@@ -68,6 +68,7 @@
 #include "meta-marshal.h"
 #include "cogl/cogl.h"
 #include "cogl/cogl-framebuffer.h"
+#include "cogl/cogl-trace.h"
 #include "core/boxes-private.h"
 
 #ifndef EGL_DRM_MASTER_FD_EXT
@@ -2084,6 +2085,9 @@ copy_shared_framebuffer_gpu (CoglOnscreen                        *onscreen,
   MetaDrmBufferGbm *buffer_gbm;
   struct gbm_bo *bo;
 
+  COGL_TRACE_BEGIN_SCOPED (CopySharedFramebufferSecondaryGpu,
+                           "FB Copy (secondary GPU)");
+
   if (!meta_egl_make_current (egl,
                               renderer_gpu_data->egl_display,
                               secondary_gpu_state->egl_surface,
@@ -2177,6 +2181,9 @@ copy_shared_framebuffer_primary_gpu (CoglOnscreen                        *onscre
   CoglOffscreen *cogl_fbo;
   CoglError *cerror = NULL;
   int ret;
+
+  COGL_TRACE_BEGIN_SCOPED (CopySharedFramebufferPrimaryGpu,
+                           "FB Copy (primary GPU)");
 
   dumb_fb = secondary_gpu_get_next_dumb_buffer (secondary_gpu_state);
 
@@ -2336,6 +2343,9 @@ copy_shared_framebuffer_cpu (CoglOnscreen                        *onscreen,
   CoglPixelFormat cogl_format;
   gboolean ret;
   MetaDrmBufferDumb *buffer_dumb;
+
+  COGL_TRACE_BEGIN_SCOPED (CopySharedFramebufferCpu,
+                           "FB Copy (CPU)");
 
   dumb_fb = secondary_gpu_get_next_dumb_buffer (secondary_gpu_state);
 
