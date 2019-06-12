e2bd9fa9f8194941ac1621472caffa7cd3649eba
Niels De Graef <niels.degraef@barco.com>
2019-06-12 15:14:15 +0200

    cogl: Replace G_8 with R_8 pixel format
    Cogl doesn't use `COGL_PIXEL_FORMAT_G_8` anywhere, and it has differing
    implementations depending on the driver (Mesa averages it out over the
    diferent components, while NVIDIA stores it in the R component).
    
    Furthermore, by having this new pixel format, we can use this for
    single-component uploads, which will be useful when dealing with
    non-trivial textures (such as YUV-based textures).

--- cogl/cogl/driver/gl/gles/cogl-driver-gles.c
+++ cogl/cogl/driver/gl/gles/cogl-driver-gles.c
@@ -86,9 +86,10 @@ _cogl_driver_pixel_format_to_gl (CoglContext     *context,
       glformat = GL_ALPHA;
       gltype = GL_UNSIGNED_BYTE;
       break;
-    case COGL_PIXEL_FORMAT_G_8:
-      glintformat = GL_LUMINANCE;
-      glformat = GL_LUMINANCE;
+    case COGL_PIXEL_FORMAT_R_8:
+      /* GLES doesn't have GL_RED, so use GL_ALPHA and use a swizzle later */
+      glintformat = GL_ALPHA;
+      glformat = GL_ALPHA;
       gltype = GL_UNSIGNED_BYTE;
       break;
 
