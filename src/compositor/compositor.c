f1837b785bd03086baf4c218e3a56b5430d0bc63
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-06-13 10:39:31 +0200

    compositor: Replace tabs with spaces
    Some old code was still using tabs instead of spaces, so replace the leftover
    tabs with 8 spaces each.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/660

--- src/compositor/compositor.c
+++ src/compositor/compositor.c
@@ -844,7 +844,7 @@ meta_compositor_filter_keybinding (MetaCompositor *compositor,
 
 void
 meta_compositor_show_window (MetaCompositor *compositor,
-			     MetaWindow	    *window,
+                             MetaWindow     *window,
                              MetaCompEffect  effect)
 {
   MetaWindowActor *window_actor = META_WINDOW_ACTOR (meta_window_get_compositor_private (window));
@@ -1032,7 +1032,7 @@ on_top_window_actor_destroyed (MetaWindowActor *window_actor,
 
 void
 meta_compositor_sync_stack (MetaCompositor  *compositor,
-			    GList	    *stack)
+                            GList           *stack)
 {
   GList *old_stack;
 
@@ -1132,8 +1132,8 @@ meta_compositor_sync_stack (MetaCompositor  *compositor,
 
 void
 meta_compositor_sync_window_geometry (MetaCompositor *compositor,
-				      MetaWindow *window,
-                                      gboolean did_placement)
+                                      MetaWindow     *window,
+                                      gboolean        did_placement)
 {
   MetaWindowActor *window_actor = META_WINDOW_ACTOR (meta_window_get_compositor_private (window));
   meta_window_actor_sync_actor_geometry (window_actor, did_placement);
@@ -1533,7 +1533,7 @@ void
 meta_compositor_show_window_menu_for_rect (MetaCompositor     *compositor,
                                            MetaWindow         *window,
                                            MetaWindowMenuType  menu,
-					   MetaRectangle      *rect)
+                                           MetaRectangle      *rect)
 {
   meta_plugin_manager_show_window_menu_for_rect (compositor->plugin_mgr, window, menu, rect);
 }
