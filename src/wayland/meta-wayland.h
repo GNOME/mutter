c8d45ae169b7ce3ed487e05a97a5922d3b5b0133
Jonas Ådahl <jadahl@gmail.com>
2019-06-19 13:15:04 +0200

    wayland: Don't export non-public API not used by tests
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/628

--- src/wayland/meta-wayland.h
+++ src/wayland/meta-wayland.h
@@ -31,11 +31,10 @@
 META_EXPORT_TEST
 void                    meta_wayland_override_display_name (const char *display_name);
 
-META_EXPORT_TEST
 void                    meta_wayland_pre_clutter_init           (void);
-META_EXPORT_TEST
+
 void                    meta_wayland_init                       (void);
-META_EXPORT_TEST
+
 void                    meta_wayland_finalize                   (void);
 
 /* We maintain a singleton MetaWaylandCompositor which can be got at via this
@@ -43,51 +42,45 @@ void                    meta_wayland_finalize                   (void);
 META_EXPORT_TEST
 MetaWaylandCompositor  *meta_wayland_compositor_get_default     (void);
 
-META_EXPORT_TEST
 void                    meta_wayland_compositor_update          (MetaWaylandCompositor *compositor,
                                                                  const ClutterEvent    *event);
-META_EXPORT_TEST
+
 gboolean                meta_wayland_compositor_handle_event    (MetaWaylandCompositor *compositor,
                                                                  const ClutterEvent    *event);
-META_EXPORT_TEST
+
 void                    meta_wayland_compositor_update_key_state (MetaWaylandCompositor *compositor,
                                                                  char                  *key_vector,
                                                                   int                    key_vector_len,
                                                                   int                    offset);
-META_EXPORT_TEST
+
 void                    meta_wayland_compositor_repick          (MetaWaylandCompositor *compositor);
 
-META_EXPORT_TEST
 void                    meta_wayland_compositor_set_input_focus (MetaWaylandCompositor *compositor,
                                                                  MetaWindow            *window);
 
-META_EXPORT_TEST
 void                    meta_wayland_compositor_paint_finished  (MetaWaylandCompositor *compositor);
 
-META_EXPORT_TEST
 void                    meta_wayland_compositor_destroy_frame_callbacks (MetaWaylandCompositor *compositor,
                                                                          MetaWaylandSurface    *surface);
 
 META_EXPORT_TEST
 const char             *meta_wayland_get_wayland_display_name   (MetaWaylandCompositor *compositor);
+
 META_EXPORT_TEST
 const char             *meta_wayland_get_xwayland_display_name  (MetaWaylandCompositor *compositor);
 
-META_EXPORT_TEST
 void                    meta_wayland_compositor_restore_shortcuts      (MetaWaylandCompositor *compositor,
                                                                         ClutterInputDevice    *source);
-META_EXPORT_TEST
+
 gboolean                meta_wayland_compositor_is_shortcuts_inhibited (MetaWaylandCompositor *compositor,
                                                                         ClutterInputDevice    *source);
 
-META_EXPORT_TEST
 void                    meta_wayland_compositor_flush_clients (MetaWaylandCompositor *compositor);
 
-META_EXPORT_TEST
 void                    meta_wayland_compositor_schedule_surface_association (MetaWaylandCompositor *compositor,
                                                                               int                    id,
                                                                               MetaWindow            *window);
-META_EXPORT_TEST
+
 void                    meta_wayland_compositor_notify_surface_id (MetaWaylandCompositor *compositor,
                                                                    int                    id,
                                                                    MetaWaylandSurface    *surface);
