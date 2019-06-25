9005abf206a2b5ca5385e63888cd57d875fbdd7e
Jonas Dreßler <verdre@v0yd.nl>
2019-06-26 01:09:18 +0200

    clutter/x11: Don't set the stage on crossing events during implicit grabs
    On x11 we emulate pointer events from touch events as long as there's
    only one touchpoint on screen, this obviously leads to x11 sending us
    crossing events triggered by the emulated pointer. Now if we get a leave
    event and set the stage of the ClutterInputDevice to NULL, new touch
    events will be discarded by clutters backend because the core pointer
    doesn't have a stage associated. This means Mutter completely loses
    state of a touchpoint as soon as it crosses a shell actor.
    
    As a solution, don't update the stage of the input devices on crossing
    events that happen during the pointer is implicitly grabbed.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/423

--- clutter/clutter/x11/clutter-device-manager-xi2.c
+++ clutter/clutter/x11/clutter-device-manager-xi2.c
@@ -1920,7 +1920,8 @@ clutter_device_manager_xi2_translate_event (ClutterEventTranslator *translator,
             event->crossing.time = xev->time;
             translate_coords (stage_x11, xev->event_x, xev->event_y, &event->crossing.x, &event->crossing.y);
 
-            _clutter_input_device_set_stage (device, stage);
+            if (xev->mode == XINotifyWhileGrabbed)
+              _clutter_input_device_set_stage (device, stage);
           }
         else
           {
@@ -1943,7 +1944,8 @@ clutter_device_manager_xi2_translate_event (ClutterEventTranslator *translator,
             event->crossing.time = xev->time;
             translate_coords (stage_x11, xev->event_x, xev->event_y, &event->crossing.x, &event->crossing.y);
 
-            _clutter_input_device_set_stage (device, NULL);
+            if (xev->mode == XINotifyWhileGrabbed)
+              _clutter_input_device_set_stage (device, NULL);
           }
 
         _clutter_input_device_reset_scroll_info (source_device);
