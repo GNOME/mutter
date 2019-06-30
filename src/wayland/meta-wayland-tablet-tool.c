18d224cc5389dc0d7c714da41a4774d51d6afe5b
Robert Mader <robert.mader@posteo.de>
2019-06-30 15:18:46 +0200

    wayland: Implement subsurface.place_below() for parents
    Flatten the subsurface actor tree, making all surface actors children
    of the window actor.
    Save the subsurface state in a GNode tree in MetaWaylandSurface, where
    each surface holds two nodes, one branch, which can be the tree root
    or be attached to a parent surfaces branch, and a leaf, which is
    used to save the position relative to child branch nodes.
    
    Each time a surface is added or reordered in the tree, unparent all
    surface actors from the window actor, traverse all leaves of the
    tree and readd the corresponding surface actors back to the window
    actor.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/664

--- src/wayland/meta-wayland-tablet-tool.c
+++ src/wayland/meta-wayland-tablet-tool.c
@@ -993,14 +993,18 @@ static gboolean
 tablet_tool_can_grab_surface (MetaWaylandTabletTool *tool,
                               MetaWaylandSurface    *surface)
 {
-  GList *l;
+  GNode *n;
 
   if (tool->focus_surface == surface)
     return TRUE;
 
-  for (l = surface->subsurfaces; l; l = l->next)
+  meta_wayland_surface_ensure_subsurface_node (surface);
+  for (n = g_node_first_child (surface->subsurface_node); n; n = g_node_next_sibling (n))
     {
-      MetaWaylandSurface *subsurface = l->data;
+      MetaWaylandSurface *subsurface = n->data;
+
+      if (subsurface == surface)
+        continue;
 
       if (tablet_tool_can_grab_surface (tool, subsurface))
         return TRUE;
