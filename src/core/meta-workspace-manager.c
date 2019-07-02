fbbbb754abe50cbfcaf2190c17d97089bf0b37d9
Florian Müllner <florian.muellner@gmail.com>
2019-07-02 18:30:56 +0000

    Apply suggestion to src/core/meta-workspace-manager.c

--- src/core/meta-workspace-manager.c
+++ src/core/meta-workspace-manager.c
@@ -177,7 +177,7 @@ meta_workspace_manager_class_init (MetaWorkspaceManagerClass *klass)
                                                      "Layout rows",
                                                      "Number of rows in layout",
                                                      -1, G_MAXINT, -1,
-                                                     G_PARAM_READABLE));
+                                                     G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
 
   g_object_class_install_property (object_class,
                                    PROP_N_WORKSPACES,
