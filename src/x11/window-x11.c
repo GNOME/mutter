86c24026e459a744bf82a4cd84ffea995533c158
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-07-03 12:04:08 +0200

    window-x11: Use any focusable window as fallback delayed focus window
    As per commit f71151a5 we focus an input window if no take-focus-window accepts
    it. This might lead to an infinite loop if there are various focusable but
    non-input windows in the stack.
    
    When the current focus window is unmanaging and we're try to focus a
    WM_TAKE_FOCUS window, we try to give the focus to the first focusable input
    window in the stack.
    
    However, if an application (such as the Java ones) only uses non-input
    WM_TAKE_FOCUS windows, are not requesting these ones to get the focus. This
    might lead to a state where no window is focused, or a wrong one is.
    
    So, instead of only eventually focus the first input window found, try before to
    request to all the take-focus windows that are in the stack between the
    destroyed one and the input one to set the input focus, waiting for an
    incremental delay.
    
    Also, reduce the default timeout value, as the previous one might lead to a
    too long wait.
    
    Added a metatest verifying this situation.
    
    Closes: https://gitlab.gnome.org/GNOME/mutter/issues/660
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/669

--- src/x11/window-x11.c
+++ src/x11/window-x11.c
@@ -50,7 +50,7 @@
 #include "x11/window-props.h"
 #include "x11/xprops.h"
 
-#define TAKE_FOCUS_FALLBACK_DELAY_MS 250
+#define TAKE_FOCUS_FALLBACK_DELAY_MS 150
 
 enum _MetaGtkEdgeConstraints
 {
@@ -814,7 +814,8 @@ focus_window_delayed_timeout (gpointer user_data)
 
 static void
 meta_window_x11_maybe_focus_delayed (MetaWindow *window,
-                                     guint32     timestamp)
+                                     guint32     timestamp,
+                                     guint       interval)
 {
   MetaWindowX11DelayedFocusData *data;
 
@@ -832,8 +833,8 @@ meta_window_x11_maybe_focus_delayed (MetaWindow *window,
                               G_CALLBACK (meta_window_x11_delayed_focus_data_free),
                               data);
 
-  data->timeout_id = g_timeout_add (TAKE_FOCUS_FALLBACK_DELAY_MS,
-                                    focus_window_delayed_timeout, data);
+  data->timeout_id = g_timeout_add (interval, focus_window_delayed_timeout,
+                                    data);
 }
 
 static void
@@ -894,30 +895,56 @@ meta_window_x11_focus (MetaWindow *window,
               if (window->display->focus_window != NULL &&
                   window->display->focus_window->unmanaging)
                 {
-                  MetaWindow *focus_window = window;
                   MetaWorkspace *workspace = window->workspace;
                   MetaStack *stack = workspace->display->stack;
+                  g_autoptr (GList) focusables =
+                    meta_stack_list_focusable_windows (stack, workspace);
+                  GList *l;
+                  guint i;
 
-                  while (TRUE)
+                  for (l = focusables; l;)
                     {
-                      focus_window = meta_stack_get_default_focus_window (stack,
-                                                                          workspace,
-                                                                          focus_window);
-                      if (!focus_window)
-                        break;
+                      GList *next = l->next;
+                      MetaWindow *focus_window = l->data;
+                      gboolean remove = TRUE;
 
-                      if (meta_window_is_focusable (focus_window))
-                        break;
+                      if (focus_window != window)
+                        {
+                          if (focus_window->input)
+                            remove = FALSE;
+                          else if (focus_window->shaded && focus_window->frame)
+                            remove = FALSE;
+                        }
 
-                      if (focus_window->shaded && focus_window->frame)
-                        break;
+                      if (remove)
+                        focusables = g_list_delete_link (focusables, l);
+
+                      l = next;
                     }
 
                   meta_display_unset_input_focus (window->display, timestamp);
 
-                  if (focus_window)
-                    meta_window_x11_maybe_focus_delayed (focus_window,
-                                                         timestamp);
+                  /* Go through all the focusable windows and try to focus them
+                   * in order, waiting for a proportional delay. The first one
+                   * that replies to the call (in case of take focus windows)
+                   * changing the focused window, will stop the others' delayed
+                   * requests.
+                   */
+                  for (l = g_list_last (focusables), i = 1;
+                       l != NULL;
+                       l = l->prev, ++i)
+                    {
+                      MetaWindow *focus_window = l->data;
+                      guint interval = TAKE_FOCUS_FALLBACK_DELAY_MS * i;
+
+                      meta_topic (META_DEBUG_FOCUS,
+                                  "Requesting delayed focus to %s %s\n",
+                                  focus_window->desc, focus_window->res_name);
+
+                      meta_window_x11_maybe_focus_delayed (focus_window,
+                                                           timestamp,
+                                                           interval);
+                    }
                 }
             }
 
