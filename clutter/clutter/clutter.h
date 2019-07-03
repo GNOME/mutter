144b1f729fbc95f9c2d659e1ecf3078a44763169
Carlos Garnacho <carlosg@gnome.org>
2019-07-03 13:43:25 +0200

    tests: Move clutter-test-utils.[ch] to src/tests
    And add the necessary glue so those initialize a X11 clutter backend.
    This should get Clutter tests that are dependent on windowing to work
    again.

--- clutter/clutter/clutter.h
+++ clutter/clutter/clutter.h
@@ -103,7 +103,6 @@
 #include "clutter-stage-manager.h"
 #include "clutter-stage-view.h"
 #include "clutter-tap-action.h"
-#include "clutter-test-utils.h"
 #include "clutter-texture.h"
 #include "clutter-text.h"
 #include "clutter-timeline.h"
