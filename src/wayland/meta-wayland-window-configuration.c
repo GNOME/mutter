fb59751f55124a11540969ffea7b738fdca412e9
Jonas Ådahl <jadahl@gmail.com>
2019-07-05 18:10:14 +0200

    wayland: Rework asynchronous window configuration
    This changes how asynchronous window configuration works. Prior to this
    commit, it worked by MetaWindowWayland remembering the last
    configuration it sent, then when the Wayland client got back to it, it
    tried to figure out whether it was a acknowledgment of the configuration
    or not, and finish the move. This failed the client had acknowledged a
    configuration older than the last one sent, and it had hacks to somewhat
    deal with wl_shell's lack of configuration serial numbers.
    
    This commits scraps that and makes the MetaWindowWayland take ownership
    of sent configurations, including generating serial numbers. The
    wl_shell implementation is changed to emulate serial numbers (assuming
    each commit acknowledges the last sent configure event). Each
    configuration sent to the client is kept around until the client one. At
    this point, the position used for that particular configuration is used
    when applying the acknowledged state, meaning cases where we have
    already sent a new configuration when the client acknowledges a previous
    one, we'll still use the correct position for the window.

---
+++ src/wayland/meta-wayland-window-configuration.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2019 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ */
+
+#include "config.h"
+
+#include "wayland/meta-wayland-window-configuration.h"
+
+static uint32_t global_serial_counter = 0;
+
+MetaWaylandWindowConfiguration *
+meta_wayland_window_configuration_new (int x,
+                                       int y,
+                                       int width,
+                                       int height)
+{
+  MetaWaylandWindowConfiguration *configuration;
+
+  configuration = g_new0 (MetaWaylandWindowConfiguration, 1);
+  *configuration = (MetaWaylandWindowConfiguration) {
+    .serial = ++global_serial_counter,
+
+    .has_position = TRUE,
+    .x = x,
+    .y = y,
+
+    .has_size = TRUE,
+    .width = width,
+    .height = height,
+  };
+
+  return configuration;
+}
+
+MetaWaylandWindowConfiguration *
+meta_wayland_window_configuration_new_empty (void)
+{
+  MetaWaylandWindowConfiguration *configuration;
+
+  configuration = g_new0 (MetaWaylandWindowConfiguration, 1);
+  *configuration = (MetaWaylandWindowConfiguration) {
+    .serial = ++global_serial_counter,
+  };
+
+  return configuration;
+}
+
+void
+meta_wayland_window_configuration_free (MetaWaylandWindowConfiguration *configuration)
+{
+  g_free (configuration);
+}
