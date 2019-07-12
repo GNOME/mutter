ee69380d5046769244f8ceaaefb298928cf9dd61
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2019-07-12 14:33:17 -0300

    window-actor: Handle geometry scale
    Geometry scale is applied to each surface individually, using
    Clutter scales, and not only this breaks subsurfaces, it also
    pollutes the toolkit and makes the actor tree slightly too
    fragile. If GNOME Shell mistakenly tries to set the actor scale
    of any of these surfaces, for example, various artifacts might
    happen.
    
    Move geometry scale handling to MetaWindowActor. It is applied
    as an additional scale operation in the transform matrix, so
    that the Clutter-managed scale properties are left untouched.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/409

--- src/compositor/meta-surface-actor.c
+++ src/compositor/meta-surface-actor.c
@@ -22,6 +22,7 @@
 #include "clutter/clutter.h"
 #include "compositor/meta-cullable.h"
 #include "compositor/meta-shaped-texture-private.h"
+#include "compositor/meta-window-actor-private.h"
 #include "compositor/region-utils.h"
 #include "meta/meta-shaped-texture.h"
 
@@ -246,18 +247,20 @@ meta_surface_actor_cull_out (MetaCullable   *cullable,
 
   if (opacity == 0xff)
     {
+      MetaWindowActor *window_actor;
+      MetaWindow *window;
       cairo_region_t *scaled_opaque_region;
       cairo_region_t *opaque_region;
-      double geometry_scale;
+      int geometry_scale;
 
       opaque_region = meta_shaped_texture_get_opaque_region (priv->texture);
 
       if (!opaque_region)
         return;
 
-      clutter_actor_get_scale (CLUTTER_ACTOR (surface_actor),
-                               &geometry_scale,
-                               NULL);
+      window = meta_surface_actor_get_window (surface_actor);
+      window_actor = meta_window_actor_from_window (window);
+      geometry_scale = meta_window_actor_get_geometry_scale (window_actor);
       scaled_opaque_region = meta_region_scale (opaque_region, geometry_scale);
 
       if (unobscured_region)
