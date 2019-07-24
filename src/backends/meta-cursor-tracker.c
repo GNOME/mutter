ec81f60d76206271cbbb0f7e1a076a8d1f9ecea3
Niels De Graef <nielsdegraef@gmail.com>
2019-07-24 07:42:07 +0200

    cursor-tracker: Use our own marshal for cursor-moved
    By putting `NULL` as the C marshaller in `g_signal_new`, you
    automatically get `g_cclosure_marshaller_generic`, which will try to
    process its arguments and return value with the help of libffi and
    GValue.
    
    Using `glib-genmarshal` and valist_marshallers, we can prevent this so
    that we need less instructions for each signal emission.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/697

--- src/backends/meta-cursor-tracker.c
+++ src/backends/meta-cursor-tracker.c
@@ -39,6 +39,7 @@
 #include "backends/x11/cm/meta-cursor-sprite-xfixes.h"
 #include "cogl/cogl.h"
 #include "clutter/clutter.h"
+#include "meta-marshal.h"
 #include "meta/main.h"
 #include "meta/meta-x11-errors.h"
 #include "meta/util.h"
@@ -170,10 +171,14 @@ meta_cursor_tracker_class_init (MetaCursorTrackerClass *klass)
                                         G_TYPE_FROM_CLASS (klass),
                                         G_SIGNAL_RUN_LAST,
                                         0,
-                                        NULL, NULL, NULL,
+                                        NULL, NULL,
+                                        meta_marshal_VOID__FLOAT_FLOAT,
                                         G_TYPE_NONE, 2,
                                         G_TYPE_FLOAT,
                                         G_TYPE_FLOAT);
+  g_signal_set_va_marshaller (signals[CURSOR_MOVED],
+                              G_TYPE_FROM_CLASS (klass),
+                              meta_marshal_VOID__FLOAT_FLOATv);
 
   signals[VISIBILITY_CHANGED] = g_signal_new ("visibility-changed",
                                               G_TYPE_FROM_CLASS (klass),
