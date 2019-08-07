1130b19b522feff62f56fb9dce3a5a88b04cfae2
Daniel van Vugt <daniel.van.vugt@canonical.com>
2019-08-07 10:24:35 +0800

    clutter/stage-cogl: Don't return update_time=-1 when pending_swaps
    As a protection against duplicate/early update times, it has already
    been replaced by commit 35aa2781 and commit 4faeb127.
    
    Removing the old check prevents a common cause of frame skips:
    
      clutter_stage_cogl_schedule_update()
      clutter_stage_cogl_get_update_time() == -1
    
    Closes: https://gitlab.gnome.org/GNOME/gnome-shell/issues/1411
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/719

--- clutter/clutter/cogl/clutter-stage-cogl.c
+++ clutter/clutter/cogl/clutter-stage-cogl.c
@@ -243,9 +243,6 @@ clutter_stage_cogl_get_update_time (ClutterStageWindow *stage_window)
 {
   ClutterStageCogl *stage_cogl = CLUTTER_STAGE_COGL (stage_window);
 
-  if (stage_cogl->pending_swaps)
-    return -1; /* in the future, indefinite */
-
   return stage_cogl->update_time;
 }
 
