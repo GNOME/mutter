8b74f8a715c72eee4b887d74652e155f5bb429bf
Jonas Ådahl <jadahl@gmail.com>
2019-08-17 10:00:46 +0200

    window-actor: Move shapes, shadows and unredirection to X11 sub types
    Move out updating of various shapes (input, opaque, shape) indirectly
    from X11 to the corresponding X11 sub types of MetaWindowActor and
    MetaSurfaceActor.
    
    Also move fullscreen window unredirection code with it. We want to
    effectively do something similar for MetaCompositorServer, but it will
    work differently enough not to share too much logic.
    
    While it would have been nice to move things piece by piece, things were
    too intertwined to make it feasible.
    
    This has the side effect fixing accidentally and arbitrarily adding
    server side shadow to Wayland surfaces.
    
    Fixes: https://gitlab.gnome.org/GNOME/mutter/issues/727
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/734

--- src/compositor/meta-window-actor-x11.h
+++ src/compositor/meta-window-actor-x11.h
@@ -31,4 +31,17 @@ G_DECLARE_FINAL_TYPE (MetaWindowActorX11,
                       META, WINDOW_ACTOR_X11,
                       MetaWindowActor)
 
+void meta_window_actor_x11_process_x11_damage (MetaWindowActorX11 *actor_x11,
+                                               XDamageNotifyEvent *event);
+
+gboolean meta_window_actor_x11_should_unredirect (MetaWindowActorX11 *actor_x11);
+
+void meta_window_actor_x11_set_unredirected (MetaWindowActorX11 *actor_x11,
+                                             gboolean            unredirected);
+
+void meta_window_actor_x11_update_shape (MetaWindowActorX11 *actor_x11);
+
+void meta_window_actor_x11_process_damage (MetaWindowActorX11 *actor_x11,
+                                           XDamageNotifyEvent *event);
+
 #endif /* META_WINDOW_ACTOR_X11_H */
