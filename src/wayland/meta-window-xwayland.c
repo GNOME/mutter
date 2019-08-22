9e2e960de65b84b1138fedbf527fb5ba3f425708
Hans de Goede <hdegoede@redhat.com>
2019-08-22 11:17:42 +0200

    window-xwayland: Add Xwayland fullscreen games workaround
    This is a workaround for X11 games which use xrandr to change the resolution
    in combination with NET_WM_STATE_FULLSCREEN when going fullscreen.
    
    Newer versions of Xwayland support the xrandr part of this by supporting
    "fake" xrandr resolution changes in combination with using WPviewport to
    scale the app's window (at the fake resolution) to fill the entire monitor.
    
    Apps using xrandr in combination with NET_WM_STATE_FULLSCREEN expect the
    fullscreen window to have the size of the (fake) xrandr resolution since
    when running on regular Xorg the resolution will actually be changed and
    after that going fullscreen through NET_WM_STATE_FULLSCREEN will size
    the window to be equal to the new resolution.
    
    On older Xwayland versions meta_window_xwayland_get_randr_monitor_resolution
    will always return the Windows' monitor width and height, so then this is
    a no-op.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/739

--- src/wayland/meta-window-xwayland.c
+++ src/wayland/meta-window-xwayland.c
@@ -25,6 +25,8 @@
 
 #include <X11/extensions/Xrandr.h>
 
+#include "meta/meta-x11-errors.h"
+
 enum
 {
   PROP_0,
@@ -126,6 +128,71 @@ free_output:
   return success;
 }
 
+/* Is this window likely to be the window of a (fullscreen) game? */
+static gboolean
+meta_window_xwayland_likely_is_game (MetaWindow *window)
+{
+  MetaX11Display *x11_display = window->display->x11_display;
+  XClassHint class_hint;
+  Status success;
+
+  /* most games / gaming libs set a full set of hints including setting
+   * _NET_WM_BYPASS_COMPOSITOR to _NET_WM_BYPASS_COMPOSITOR_HINT_ON, so
+   * we check this first
+   */
+  if (window->bypass_compositor == _NET_WM_BYPASS_COMPOSITOR_HINT_ON)
+    return TRUE;
+
+  /* Some other games / gaming libs (e.g. OGRE) set as little hints as possible,
+   * these do not even set the WM_CLASS hints, which is somewhat unusual.
+   */
+  meta_x11_error_trap_push (x11_display);
+  success = XGetClassHint (x11_display->xdisplay, window->xwindow, &class_hint);
+  meta_x11_error_trap_pop (x11_display);
+
+  if (success)
+    {
+      XFree (class_hint.res_name);
+      XFree (class_hint.res_class);
+    }
+
+  /* If the WM_CLASS hints were *not* set it may very well be a game. */
+  return !success;
+}
+
+/* This is a workaround for X11 games which use xrandr to change the resolution
+ * in combination with NET_WM_STATE_FULLSCREEN when going fullscreen.
+ *
+ * Newer versions of Xwayland support the xrandr part of this by supporting
+ * "fake" xrandr resolution changes in combination with using WPviewport to
+ * scale the app's window (at the fake resolution) to fill the entire monitor.
+ *
+ * Apps using xrandr in combination with NET_WM_STATE_FULLSCREEN expect the
+ * fullscreen window to have the size of the (fake) xrandr resolution since
+ * when running on regular Xorg the resolution will actually be changed and
+ * after that going fullscreen through NET_WM_STATE_FULLSCREEN will size
+ * the window to be equal to the new resolution.
+ *
+ * We need to emulate this behavior for these games to work correctly, so
+ * when Xwayland is used, we query the Window's monitor fake xrandr
+ * resolution and "fullscreen" to that size.
+ */
+static void
+meta_window_xwayland_adjust_fullscreen_monitor_rect (MetaWindow *window,
+                                                     MetaRectangle *monitor_rect)
+{
+  int width, height;
+
+  if (!meta_window_xwayland_likely_is_game (window))
+    return;
+
+  if (!meta_window_xwayland_get_randr_monitor_resolution (window, &width, &height))
+    return;
+
+  monitor_rect->width = width;
+  monitor_rect->height = height;
+}
+
 static void
 meta_window_xwayland_force_restore_shortcuts (MetaWindow         *window,
                                               ClutterInputDevice *source)
@@ -188,6 +255,7 @@ meta_window_xwayland_class_init (MetaWindowXwaylandClass *klass)
   MetaWindowClass *window_class = META_WINDOW_CLASS (klass);
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
+  window_class->adjust_fullscreen_monitor_rect = meta_window_xwayland_adjust_fullscreen_monitor_rect;
   window_class->force_restore_shortcuts = meta_window_xwayland_force_restore_shortcuts;
   window_class->shortcuts_inhibited = meta_window_xwayland_shortcuts_inhibited;
 
