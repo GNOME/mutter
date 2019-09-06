525539aa93e6e9cd5c824d3c420a61e386fef190
Rémi Bernon <rbernon@codeweavers.com>
2019-09-06 18:30:22 +0200

    Revert "core: Fix multiple reparent requests handling"
    This reverts commit 8f242f8bf00607e4b9d78fcb1b3b9a0bc60a9715.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/781

--- src/x11/events.c
+++ src/x11/events.c
@@ -1393,8 +1393,7 @@ handle_other_xevent (MetaX11Display *x11_display,
           window = meta_window_x11_new (display, event->xmap.window,
                                         FALSE, META_COMP_EFFECT_CREATE);
         }
-      else if (window && window->restore_focus_on_map &&
-               window->reparents_pending == 0)
+      else if (window && window->restore_focus_on_map)
         {
           meta_window_focus (window,
                              meta_display_get_current_time_roundtrip (display));
@@ -1438,8 +1437,6 @@ handle_other_xevent (MetaX11Display *x11_display,
       break;
     case ReparentNotify:
       {
-        if (window->reparents_pending > 0)
-          window->reparents_pending -= 1;
         if (event->xreparent.event == x11_display->xroot)
           meta_stack_tracker_reparent_event (display->stack_tracker,
                                              &event->xreparent);
