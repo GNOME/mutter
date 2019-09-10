18cab97c417097b6ecad0f4127c4f55a5ecc9792
Marco Trevisan (Treviño) <mail@3v1n0.net>
2019-09-10 02:22:07 +0200

    clutter/actor: Remove deprecated internal child support
    Clutter had support for internal children in its early revisions, but they
    were deprecated for long time (commit f41061b8df, more than 7 years ago) and
    no one is using them in both clutter and in gnome-shell.
    
    So remove any alternative code path that uses internal children.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/816

--- clutter/clutter/deprecated/clutter-container.h
+++ clutter/clutter/deprecated/clutter-container.h
@@ -70,11 +70,6 @@ void            clutter_container_foreach                       (ClutterContaine
                                                                  ClutterCallback   callback,
                                                                  gpointer          user_data);
 
-CLUTTER_DEPRECATED
-void            clutter_container_foreach_with_internals        (ClutterContainer *container,
-                                                                 ClutterCallback   callback,
-                                                                 gpointer          user_data);
-
 CLUTTER_DEPRECATED_FOR(clutter_actor_set_child_above_sibling)
 void            clutter_container_raise_child                   (ClutterContainer *container,
                                                                  ClutterActor     *actor,
