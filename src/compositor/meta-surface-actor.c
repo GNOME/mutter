e46a29cf6337970fd607bf33378f536eae4956f7
Jonas Ådahl <jadahl@gmail.com>
2019-10-08 22:52:50 +0200

    surface-actor: Remove get_window() API and vfunc
    It was unused.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/835

--- src/compositor/meta-surface-actor.c
+++ src/compositor/meta-surface-actor.c
@@ -520,12 +520,6 @@ meta_surface_actor_set_frozen (MetaSurfaceActor *self,
     }
 }
 
-MetaWindow *
-meta_surface_actor_get_window (MetaSurfaceActor *self)
-{
-  return META_SURFACE_ACTOR_GET_CLASS (self)->get_window (self);
-}
-
 void
 meta_surface_actor_set_transform (MetaSurfaceActor     *self,
                                   MetaMonitorTransform  transform)
