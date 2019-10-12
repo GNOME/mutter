2054a6c9af1264c7a2ac9757069e21700fa4ad92
Alexander Mikhaylenko <exalm7659@gmail.com>
2019-10-12 23:55:26 +0500

    window: Make default focus window on each workspace appear focused
    Makes workspace transitions in gnome-shell look more seamless, since
    both outgoing and incoming workspace have focused windows.
    
    This is only done for click focus mode, since it's not known which
    window would be focused for the other modes.
    
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/850

--- src/core/window.c
+++ src/core/window.c
@@ -251,6 +251,10 @@ prefs_changed_callback (MetaPreference pref,
       meta_window_recalc_features (window);
       meta_window_queue (window, META_QUEUE_MOVE_RESIZE);
     }
+  else if (pref == META_PREF_FOCUS_MODE)
+    {
+      meta_window_appears_focused_changed (window);
+    }
 }
 
 static void
@@ -4860,6 +4864,9 @@ set_workspace_state (MetaWindow    *window,
         }
     }
 
+  if (!window->constructing)
+    meta_window_appears_focused_changed (window);
+
   /* queue a move_resize since changing workspaces may change
    * the relevant struts
    */
@@ -5159,7 +5166,7 @@ meta_window_change_workspace_by_index (MetaWindow *window,
     meta_window_change_workspace (window, workspace);
 }
 
-static void
+void
 meta_window_appears_focused_changed (MetaWindow *window)
 {
   set_net_wm_state (window);
@@ -7234,7 +7241,17 @@ meta_window_get_frame (MetaWindow *window)
 gboolean
 meta_window_appears_focused (MetaWindow *window)
 {
-  return window->has_focus || (window->attached_focus_window != NULL);
+  MetaWorkspaceManager *workspace_manager;
+  MetaWorkspace *workspace;
+  MetaWindow *default_window = NULL;
+
+  workspace_manager = window->display->workspace_manager;
+  workspace = meta_window_get_workspace (window);
+
+  if (workspace && workspace != workspace_manager->active_workspace)
+    default_window = meta_workspace_get_default_focus_window (workspace);
+
+  return window->has_focus || (window->attached_focus_window != NULL) || (window == default_window);
 }
 
 gboolean
