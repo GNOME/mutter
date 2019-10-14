b4976b1541fe0e64126a96a5803b2f504595963e
Jonas Ådahl <jadahl@gmail.com>
2019-10-14 11:32:54 +0200

    x11/window-controls: Pass around MetaX11Display directly
    Instead of passing around an X11 Display pointer that is retrieved from
    the default Gdk backend, then finding the MetaX11Display from said X11
    Display, pass the MetaX11Display directly.

--- src/x11/meta-x11-display-private.h
+++ src/x11/meta-x11-display-private.h
@@ -253,6 +253,8 @@ void meta_x11_display_set_input_focus (MetaX11Display *x11_display,
                                        gboolean        focus_frame,
                                        uint32_t        timestamp);
 
+MetaDisplay * meta_x11_display_get_display (MetaX11Display *x11_display);
+
 const gchar * meta_x11_get_display_name (void);
 
 #endif /* META_X11_DISPLAY_PRIVATE_H */
