e2ecc6f716ef6fbd0b21a123f991b627e2ef7f6a
Adam Jackson <ajax@redhat.com>
2019-10-17 16:53:29 -0400

    cogl: Remove unused COGL_FEATURE_ID_DEPTH_RANGE
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/866

--- cogl/cogl/cogl-depth-state.h
+++ cogl/cogl/cogl-depth-state.h
@@ -225,13 +225,6 @@ cogl_depth_state_get_test_function (CoglDepthState *state);
  * mapped too although the range must still lye within the range [0,
  * 1].
  *
- * If your driver does not support this feature (for example you are
- * using GLES 1 drivers) then if you don't use the default range
- * values you will get an error reported when calling
- * cogl_pipeline_set_depth_state (). You can check ahead of time for
- * the %COGL_FEATURE_ID_DEPTH_RANGE feature with
- * cogl_has_feature() to know if this function will succeed.
- *
  * By default normalized device coordinate depth values are mapped to
  * the full range of depth buffer values, [0, 1].
  *
