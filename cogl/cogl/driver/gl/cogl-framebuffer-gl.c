0e4c23c9851fc1b1228872dc4669d27ed9d259d7
Adam Jackson <ajax@redhat.com>
2019-10-17 19:47:56 -0400

    cogl: Remove unused depth texture API
    https://gitlab.gnome.org/GNOME/mutter/merge_requests/866

--- cogl/cogl/driver/gl/cogl-framebuffer-gl.c
+++ cogl/cogl/driver/gl/cogl-framebuffer-gl.c
@@ -439,20 +439,6 @@ _cogl_framebuffer_gl_flush_state (CoglFramebuffer *draw_buffer,
 }
 
 static CoglTexture *
-create_depth_texture (CoglContext *ctx,
-                      int width,
-                      int height)
-{
-  CoglTexture2D *depth_texture =
-    cogl_texture_2d_new_with_size (ctx, width, height);
-
-  cogl_texture_set_components (COGL_TEXTURE (depth_texture),
-                               COGL_TEXTURE_COMPONENTS_DEPTH);
-
-  return COGL_TEXTURE (depth_texture);
-}
-
-static CoglTexture *
 attach_depth_texture (CoglContext *ctx,
                       CoglTexture *depth_texture,
                       CoglOffscreenAllocateFlags flags)
@@ -783,24 +769,6 @@ _cogl_offscreen_gl_allocate (CoglOffscreen *offscreen,
                                 &level_width,
                                 &level_height,
                                 NULL);
-
-  if (fb->config.depth_texture_enabled &&
-      offscreen->depth_texture == NULL)
-    {
-      offscreen->depth_texture =
-        create_depth_texture (ctx,
-                              level_width,
-                              level_height);
-
-      if (!cogl_texture_allocate (offscreen->depth_texture, error))
-        {
-          cogl_object_unref (offscreen->depth_texture);
-          offscreen->depth_texture = NULL;
-          return FALSE;
-        }
-
-      _cogl_texture_associate_framebuffer (offscreen->depth_texture, fb);
-    }
 
   /* XXX: The framebuffer_object spec isn't clear in defining whether attaching
    * a texture as a renderbuffer with mipmap filtering enabled while the
