62c8cafe2ceed8dce8160d7422768e7d9a476f7c
Carlos Garnacho <carlosg@gnome.org>
2019-10-20 13:14:09 +0200

    cogl: Drop unused struct field
    We only ever stored that the current clip uses the stencil buffer, but
    never read it back. It doesn't seem like we'll have an use for it.

--- cogl/cogl/driver/gl/cogl-clip-stack-gl.c
+++ cogl/cogl/driver/gl/cogl-clip-stack-gl.c
@@ -525,7 +525,6 @@ _cogl_clip_stack_gl_flush (CoglClipStack *stack,
     {
       COGL_NOTE (CLIPPING, "Flushed empty clip stack");
 
-      ctx->current_clip_stack_uses_stencil = FALSE;
       GE (ctx, glDisable (GL_SCISSOR_TEST));
       return;
     }
@@ -668,6 +667,4 @@ _cogl_clip_stack_gl_flush (CoglClipStack *stack,
      setting up the stencil buffer */
   if (using_clip_planes)
     enable_clip_planes (ctx);
-
-  ctx->current_clip_stack_uses_stencil = using_stencil_buffer;
 }
