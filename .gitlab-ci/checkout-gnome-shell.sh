4ba3c4537e30cdd500286f57b9716e168ad54250
Florian Müllner <fmuellner@gnome.org>
2019-11-07 00:05:12 +0100

    ci: Fix checking out gnome-shell on stable branches
    For stable branches, we currently only check out the correct shell
    branch for merge requests. For the regular pipeline, our code to
    determine the current mutter branch fails because CI runs on a
    temporary "pipeline/12345" branch that doesn't exist for gnome-shell.
    
    Switching to the correct gitlab environment variable fixes that.
    
    https://gitlab.gnome.org/GNOME/gnome-shell/merge_requests/811

--- .gitlab-ci/checkout-gnome-shell.sh
+++ .gitlab-ci/checkout-gnome-shell.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-mutter_branch=$(git describe --contains --all HEAD)
 gnome_shell_target=
 
 git clone https://gitlab.gnome.org/GNOME/gnome-shell.git
@@ -26,8 +25,7 @@ if [ "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" ]; then
 fi
 
 if [ -z "$gnome_shell_target" ]; then
-  gnome_shell_target=$(git branch -r -l origin/$mutter_branch)
-  gnome_shell_target=${gnome_shell_target:-$(git branch -r -l ${mutter_branch#remotes/})}
+  gnome_shell_target=$(git branch -r -l origin/$CI_COMMIT_REF_NAME)
   gnome_shell_target=${gnome_shell_target:-origin/master}
   echo Using $gnome_shell_target instead
 fi
