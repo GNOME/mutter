cb0cc42651dcc6366dc88b37ff38eac1eef99e35
Jonas Dreßler <verdre@v0yd.nl>
2020-03-03 21:50:45 +0100

    clutter: Also remove event sequences on TOUCH_CANCEL events
    We don't want to leave event sequences around when touches are
    cancelled, so also remove those from the ClutterInputDevice's hashtable.
    
    https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1275

--- clutter/clutter/clutter-main.c
+++ clutter/clutter/clutter-main.c
@@ -2049,7 +2049,8 @@ _clutter_process_event_details (ClutterActor        *stage,
 
                   emit_touch_event (event, device);
 
-                  if (event->type == CLUTTER_TOUCH_END)
+                  if (event->type == CLUTTER_TOUCH_END ||
+                      event->type == CLUTTER_TOUCH_CANCEL)
                     _clutter_input_device_remove_event_sequence (device, event);
 
                   break;
@@ -2084,7 +2085,8 @@ _clutter_process_event_details (ClutterActor        *stage,
 
           emit_touch_event (event, device);
 
-          if (event->type == CLUTTER_TOUCH_END)
+          if (event->type == CLUTTER_TOUCH_END ||
+              event->type == CLUTTER_TOUCH_CANCEL)
             _clutter_input_device_remove_event_sequence (device, event);
 
           break;
