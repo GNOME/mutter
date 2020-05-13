a416bed34cfeeba37671f61c96d1b68581d3246d
Carlos Garnacho <carlosg@gnome.org>
2020-05-13 16:39:44 +0200

    wayland: Send primary offer to all data devices from the same client
    Make the data device track the keyboard focus, and use that list to
    forward the primary selection to all data devices from the same
    client.
    
    https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1253
    
    (cherry-pick of commit b45d5ef3f5a497703fd2c77b7088249cbf3ef367)
    
    https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1262

--- src/wayland/meta-wayland-data-device.h
+++ src/wayland/meta-wayland-data-device.h
@@ -65,6 +65,7 @@ struct _MetaWaylandDataDevice
   struct wl_list resource_list;
   struct wl_list focus_resource_list;
   struct wl_list primary_resource_list;
+  struct wl_list primary_focus_resource_list;
   MetaWaylandDragGrab *current_grab;
   struct wl_client *focus_client;
 
