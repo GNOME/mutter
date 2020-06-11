GNOME/mutter

commit 1ce9e379d99a8e15b3d043013062104f9b569574

crtc: Make implementations inherit MetaCrtc
Just as with MetaOutput, instead of the home baked "inheritance" system,
using a gpointer and a GDestroyNotify function to keep the what
effectively is sub type details, make MetaCrtc an abstract derivable
type, and make the implementations inherit it.

https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1287

src/backends/native/meta-crtc-kms.c
@@ -34,40 +34,36 @@
 
 #define ALL_TRANSFORMS_MASK ((1 << META_MONITOR_N_TRANSFORMS) - 1)
 
-typedef struct _MetaCrtcKms
+struct _MetaCrtcKms
 {
   MetaKmsCrtc *kms_crtc;
 
   MetaKmsPlane *primary_plane;
 
   gpointer cursor_renderer_private;
-} MetaCrtcKms;
+};
 
 static GQuark kms_crtc_crtc_kms_quark;
 
-gpointer
-meta_crtc_kms_get_cursor_renderer_private (MetaCrtc *crtc)
-{
-  MetaCrtcKms *crtc_kms = crtc->driver_private;
+G_DEFINE_TYPE (MetaCrtcKms, meta_crtc_kms, META_TYPE_CRTC)
 
+gpointer
+meta_crtc_kms_get_cursor_renderer_private (MetaCrtcKms *crtc_kms)
+{
   return crtc_kms->cursor_renderer_private;
 }
 
 void
-meta_crtc_kms_set_cursor_renderer_private (MetaCrtc *crtc,
-                                           gpointer  cursor_renderer_private)
+meta_crtc_kms_set_cursor_renderer_private (MetaCrtcKms *crtc_kms,
+                                           gpointer     cursor_renderer_private)
 {
-  MetaCrtcKms *crtc_kms = crtc->driver_private;
-
   crtc_kms->cursor_renderer_private = cursor_renderer_private;
 }
 
 gboolean
-meta_crtc_kms_is_transform_handled (MetaCrtc             *crtc,
+meta_crtc_kms_is_transform_handled (MetaCrtcKms          *crtc_kms,
                                     MetaMonitorTransform  transform)
 {
-  MetaCrtcKms *crtc_kms = crtc->driver_private;
-
   if (!crtc_kms->primary_plane)
     return FALSE;
 
@@ -76,19 +72,19 @@ meta_crtc_kms_is_transform_handled (MetaCrtc             *crtc,
 }
 
 void
-meta_crtc_kms_apply_transform (MetaCrtc               *crtc,
+meta_crtc_kms_apply_transform (MetaCrtcKms            *crtc_kms,
                                MetaKmsPlaneAssignment *kms_plane_assignment)
 {
-  MetaCrtcKms *crtc_kms = crtc->driver_private;
+  MetaCrtc *crtc = META_CRTC (crtc_kms);
   const MetaCrtcConfig *crtc_config;
   MetaMonitorTransform hw_transform;
 
   crtc_config = meta_crtc_get_config (crtc);
 
   hw_transform = crtc_config->transform;
-  if (!meta_crtc_kms_is_transform_handled (crtc, hw_transform))
+  if (!meta_crtc_kms_is_transform_handled (crtc_kms, hw_transform))
     hw_transform = META_MONITOR_TRANSFORM_NORMAL;
-  if (!meta_crtc_kms_is_transform_handled (crtc, hw_transform))
+  if (!meta_crtc_kms_is_transform_handled (crtc_kms, hw_transform))
     return;
 
   meta_kms_plane_update_set_rotation (crtc_kms->primary_plane,
@@ -97,10 +93,11 @@ meta_crtc_kms_apply_transform (MetaCrtc               *crtc,
 }
 
 void
-meta_crtc_kms_assign_primary_plane (MetaCrtc      *crtc,
+meta_crtc_kms_assign_primary_plane (MetaCrtcKms   *crtc_kms,
                                     uint32_t       fb_id,
                                     MetaKmsUpdate *kms_update)
 {
+  MetaCrtc *crtc = META_CRTC (crtc_kms);
   const MetaCrtcConfig *crtc_config;
   MetaFixed16Rectangle src_rect;
   MetaFixed16Rectangle dst_rect;
@@ -127,7 +124,7 @@ meta_crtc_kms_assign_primary_plane (MetaCrtc      *crtc,
 
   flags = META_KMS_ASSIGN_PLANE_FLAG_NONE;
 
-  kms_crtc = meta_crtc_kms_get_kms_crtc (crtc);
+  kms_crtc = meta_crtc_kms_get_kms_crtc (crtc_kms);
   kms_device = meta_kms_crtc_get_device (kms_crtc);
   primary_kms_plane = meta_kms_device_get_primary_plane_for (kms_device,
                                                              kms_crtc);
@@ -138,7 +135,7 @@ meta_crtc_kms_assign_primary_plane (MetaCrtc      *crtc,
                                                    src_rect,
                                                    dst_rect,
                                                    flags);
-  meta_crtc_kms_apply_transform (crtc, plane_assignment);
+  meta_crtc_kms_apply_transform (crtc_kms, plane_assignment);
 }
 
 static GList *
@@ -167,9 +164,10 @@ generate_crtc_connector_list (MetaGpu  *gpu,
 }
 
 void
-meta_crtc_kms_set_mode (MetaCrtc      *crtc,
+meta_crtc_kms_set_mode (MetaCrtcKms   *crtc_kms,
                         MetaKmsUpdate *kms_update)
 {
+  MetaCrtc *crtc = META_CRTC (crtc_kms);
   MetaGpu *gpu = meta_crtc_get_gpu (crtc);
   GList *connectors;
   drmModeModeInfo *mode;
@@ -194,34 +192,32 @@ meta_crtc_kms_set_mode (MetaCrtc      *crtc,
     }
 
   meta_kms_update_mode_set (kms_update,
-                            meta_crtc_kms_get_kms_crtc (crtc),
+                            meta_crtc_kms_get_kms_crtc (crtc_kms),
                             g_steal_pointer (&connectors),
                             mode);
 }
 
 void
-meta_crtc_kms_page_flip (MetaCrtc                      *crtc,
+meta_crtc_kms_page_flip (MetaCrtcKms                   *crtc_kms,
                          const MetaKmsPageFlipFeedback *page_flip_feedback,
                          gpointer                       user_data,
                          MetaKmsUpdate                 *kms_update)
 {
   meta_kms_update_page_flip (kms_update,
-                             meta_crtc_kms_get_kms_crtc (crtc),
+                             meta_crtc_kms_get_kms_crtc (crtc_kms),
                              page_flip_feedback,
                              user_data);
 }
 
 MetaKmsCrtc *
-meta_crtc_kms_get_kms_crtc (MetaCrtc *crtc)
+meta_crtc_kms_get_kms_crtc (MetaCrtcKms *crtc_kms)
 {
-  MetaCrtcKms *crtc_kms = crtc->driver_private;
-
   return crtc_kms->kms_crtc;
 }
 
 /**
  * meta_crtc_kms_get_modifiers:
- * @crtc: a #MetaCrtc object that has to be a #MetaCrtcKms
+ * @crtc_kms: a #MetaCrtc object that has to be a #MetaCrtcKms
  * @format: a DRM pixel format
  *
  * Returns a pointer to a #GArray containing all the supported
@@ -234,84 +230,67 @@ meta_crtc_kms_get_kms_crtc (MetaCrtc *crtc)
  * supported.
  */
 GArray *
-meta_crtc_kms_get_modifiers (MetaCrtc *crtc,
-                             uint32_t  format)
+meta_crtc_kms_get_modifiers (MetaCrtcKms *crtc_kms,
+                             uint32_t     format)
 {
-  MetaCrtcKms *crtc_kms = crtc->driver_private;
-
   return meta_kms_plane_get_modifiers_for_format (crtc_kms->primary_plane,
                                                   format);
 }
 
 /**
  * meta_crtc_kms_copy_drm_format_list:
- * @crtc: a #MetaCrtc object that has to be a #MetaCrtcKms
+ * @crtc_kms: a #MetaCrtc object that has to be a #MetaCrtcKms
  *
  * Returns a new #GArray that the caller must destroy. The array
  * contains all the DRM pixel formats the CRTC supports on
  * its primary plane. The array element type is uint32_t.
  */
 GArray *
-meta_crtc_kms_copy_drm_format_list (MetaCrtc *crtc)
+meta_crtc_kms_copy_drm_format_list (MetaCrtcKms *crtc_kms)
 {
-  MetaCrtcKms *crtc_kms = crtc->driver_private;
-
   return meta_kms_plane_copy_drm_format_list (crtc_kms->primary_plane);
 }
 
 /**
  * meta_crtc_kms_supports_format:
- * @crtc: a #MetaCrtc object that has to be a #MetaCrtcKms
+ * @crtc_kms: a #MetaCrtcKms
  * @drm_format: a DRM pixel format
  *
  * Returns true if the CRTC supports the format on its primary plane.
  */
 gboolean
-meta_crtc_kms_supports_format (MetaCrtc *crtc,
-                               uint32_t  drm_format)
+meta_crtc_kms_supports_format (MetaCrtcKms *crtc_kms,
+                               uint32_t     drm_format)
 {
-  MetaCrtcKms *crtc_kms = crtc->driver_private;
-
   return meta_kms_plane_is_format_supported (crtc_kms->primary_plane,
                                              drm_format);
 }
 
-MetaCrtc *
+MetaCrtcKms *
 meta_crtc_kms_from_kms_crtc (MetaKmsCrtc *kms_crtc)
 {
   return g_object_get_qdata (G_OBJECT (kms_crtc), kms_crtc_crtc_kms_quark);
 }
 
-static void
-meta_crtc_destroy_notify (MetaCrtc *crtc)
-{
-  g_free (crtc->driver_private);
-}
-
-MetaCrtc *
-meta_create_kms_crtc (MetaGpuKms  *gpu_kms,
-                      MetaKmsCrtc *kms_crtc)
+MetaCrtcKms *
+meta_crtc_kms_new (MetaGpuKms  *gpu_kms,
+                   MetaKmsCrtc *kms_crtc)
 {
   MetaGpu *gpu = META_GPU (gpu_kms);
   MetaKmsDevice *kms_device;
-  MetaCrtc *crtc;
   MetaCrtcKms *crtc_kms;
   MetaKmsPlane *primary_plane;
 
   kms_device = meta_gpu_kms_get_kms_device (gpu_kms);
   primary_plane = meta_kms_device_get_primary_plane_for (kms_device,
                                                          kms_crtc);
-  crtc = g_object_new (META_TYPE_CRTC,
-                       "id", meta_kms_crtc_get_id (kms_crtc),
-                       "gpu", gpu,
-                       NULL);
+  crtc_kms = g_object_new (META_TYPE_CRTC_KMS,
+                           "id", meta_kms_crtc_get_id (kms_crtc),
+                           "gpu", gpu,
+                           NULL);
 
-  crtc_kms = g_new0 (MetaCrtcKms, 1);
   crtc_kms->kms_crtc = kms_crtc;
   crtc_kms->primary_plane = primary_plane;
-
-  crtc->driver_private = crtc_kms;
-  crtc->driver_notify = (GDestroyNotify) meta_crtc_destroy_notify;
 
   if (!kms_crtc_crtc_kms_quark)
     {
@@ -319,7 +298,17 @@ meta_create_kms_crtc (MetaGpuKms  *gpu_kms,
         g_quark_from_static_string ("meta-kms-crtc-crtc-kms-quark");
     }
 
-  g_object_set_qdata (G_OBJECT (kms_crtc), kms_crtc_crtc_kms_quark, crtc);
+  g_object_set_qdata (G_OBJECT (kms_crtc), kms_crtc_crtc_kms_quark, crtc_kms);
 
-  return crtc;
+  return crtc_kms;
+}
+
+static void
+meta_crtc_kms_init (MetaCrtcKms *crtc_kms)
+{
+}
+
+static void
+meta_crtc_kms_class_init (MetaCrtcKmsClass *klass)
+{
 }

src/backends/meta-crtc.c
@@ -47,7 +47,7 @@ typedef struct _MetaCrtcPrivate
   MetaCrtcConfig *config;
 } MetaCrtcPrivate;
 
-G_DEFINE_TYPE_WITH_PRIVATE (MetaCrtc, meta_crtc, G_TYPE_OBJECT)
+G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (MetaCrtc, meta_crtc, G_TYPE_OBJECT)
 
 G_DEFINE_TYPE (MetaCrtcMode, meta_crtc_mode, G_TYPE_OBJECT)
 
@@ -165,9 +165,6 @@ meta_crtc_finalize (GObject *object)
 {
   MetaCrtc *crtc = META_CRTC (object);
   MetaCrtcPrivate *priv = meta_crtc_get_instance_private (crtc);
-
-  if (crtc->driver_notify)
-    crtc->driver_notify (crtc);
 
   g_clear_pointer (&priv->config, g_free);
 

src/backends/meta-crtc.h
@@ -56,14 +56,6 @@ typedef struct _MetaCrtcConfig
   MetaCrtcMode *mode;
 } MetaCrtcConfig;
 
-struct _MetaCrtc
-{
-  GObject parent;
-
-  gpointer driver_private;
-  GDestroyNotify driver_notify;
-};
-
 struct _MetaCrtcMode
 {
   GObject parent;
@@ -82,7 +74,13 @@ struct _MetaCrtcMode
 };
 
 #define META_TYPE_CRTC (meta_crtc_get_type ())
-META_EXPORT_TEST G_DECLARE_FINAL_TYPE (MetaCrtc, meta_crtc, META, CRTC, GObject)
+META_EXPORT_TEST
+G_DECLARE_DERIVABLE_TYPE (MetaCrtc, meta_crtc, META, CRTC, GObject)
+
+struct _MetaCrtcClass
+{
+  GObjectClass parent_class;
+};
 
 #define META_TYPE_CRTC_MODE (meta_crtc_mode_get_type ())
 META_EXPORT_TEST G_DECLARE_FINAL_TYPE (MetaCrtcMode, meta_crtc_mode, META, CRTC_MODE, GObject)

src/backends/meta-monitor-manager-dummy.c
@@ -60,8 +60,13 @@ struct _MetaOutputDummy
   float scale;
 };
 
-G_DEFINE_TYPE (MetaOutputDummy, meta_output_dummy, META_TYPE_OUTPUT)
+struct _MetaCrtcDummy
+{
+  MetaCrtc parent;
+};
 
+G_DEFINE_TYPE (MetaOutputDummy, meta_output_dummy, META_TYPE_OUTPUT)
+G_DEFINE_TYPE (MetaCrtcDummy, meta_crtc_dummy, META_TYPE_CRTC)
 G_DEFINE_TYPE (MetaMonitorManagerDummy, meta_monitor_manager_dummy, META_TYPE_MONITOR_MANAGER);
 
 struct _MetaGpuDummy
@@ -198,7 +203,7 @@ append_monitor (MetaMonitorManager *manager,
     }
   *modes = g_list_concat (*modes, new_modes);
 
-  crtc = g_object_new (META_TYPE_CRTC,
+  crtc = g_object_new (META_TYPE_CRTC_DUMMY,
                        "id", g_list_length (*crtcs) + 1,
                        "gpu", gpu,
                        NULL);
@@ -284,7 +289,7 @@ append_tiled_monitor (MetaMonitorManager *manager,
     {
       MetaCrtc *crtc;
 
-      crtc = g_object_new (META_TYPE_CRTC,
+      crtc = g_object_new (META_TYPE_CRTC_DUMMY,
                            "id", g_list_length (*crtcs) + i + 1,
                            "gpu", gpu,
                            NULL);
@@ -791,5 +796,15 @@ meta_output_dummy_init (MetaOutputDummy *output_dummy)
 
 static void
 meta_output_dummy_class_init (MetaOutputDummyClass *klass)
+{
+}
+
+static void
+meta_crtc_dummy_init (MetaCrtcDummy *crtc_dummy)
+{
+}
+
+static void
+meta_crtc_dummy_class_init (MetaCrtcDummyClass *klass)
 {
 }

src/backends/meta-monitor-manager-dummy.h
@@ -23,6 +23,7 @@
 #ifndef META_MONITOR_MANAGER_DUMMY_H
 #define META_MONITOR_MANAGER_DUMMY_H
 
+#include "backends/meta-crtc.h"
 #include "backends/meta-gpu.h"
 #include "backends/meta-monitor-manager-private.h"
 #include "backends/meta-output.h"
@@ -31,6 +32,11 @@
 G_DECLARE_FINAL_TYPE (MetaOutputDummy, meta_output_dummy,
                       META, OUTPUT_DUMMY,
                       MetaOutput)
+
+#define META_TYPE_CRTC_DUMMY (meta_crtc_dummy_get_type ())
+G_DECLARE_FINAL_TYPE (MetaCrtcDummy, meta_crtc_dummy,
+                      META, CRTC_DUMMY,
+                      MetaCrtc)
 
 #define META_TYPE_MONITOR_MANAGER_DUMMY (meta_monitor_manager_dummy_get_type ())
 G_DECLARE_FINAL_TYPE (MetaMonitorManagerDummy, meta_monitor_manager_dummy,

src/backends/native/meta-crtc-kms.h
@@ -31,47 +31,52 @@
 #include "backends/native/meta-gpu-kms.h"
 #include "backends/native/meta-kms-crtc.h"
 
-gpointer meta_crtc_kms_get_cursor_renderer_private (MetaCrtc *crtc);
+#define META_TYPE_CRTC_KMS (meta_crtc_kms_get_type ())
+G_DECLARE_FINAL_TYPE (MetaCrtcKms, meta_crtc_kms,
+                      META, CRTC_KMS,
+                      MetaCrtc)
 
-void meta_crtc_kms_set_cursor_renderer_private (MetaCrtc *crtc,
-                                                gpointer  cursor_renderer_private);
+gpointer meta_crtc_kms_get_cursor_renderer_private (MetaCrtcKms *crtc_kms);
 
-gboolean meta_crtc_kms_is_transform_handled (MetaCrtc             *crtc,
+void meta_crtc_kms_set_cursor_renderer_private (MetaCrtcKms *crtc_kms,
+                                                gpointer     cursor_renderer_private);
+
+gboolean meta_crtc_kms_is_transform_handled (MetaCrtcKms          *crtc_kms,
                                              MetaMonitorTransform  transform);
 
-void meta_crtc_kms_apply_transform (MetaCrtc               *crtc,
+void meta_crtc_kms_apply_transform (MetaCrtcKms            *crtc_kms,
                                     MetaKmsPlaneAssignment *kms_plane_assignment);
 
-void meta_crtc_kms_assign_primary_plane (MetaCrtc      *crtc,
+void meta_crtc_kms_assign_primary_plane (MetaCrtcKms   *crtc_kms,
                                          uint32_t       fb_id,
                                          MetaKmsUpdate *kms_update);
 
-void meta_crtc_kms_set_mode (MetaCrtc      *crtc,
+void meta_crtc_kms_set_mode (MetaCrtcKms   *crtc_kms,
                              MetaKmsUpdate *kms_update);
 
-void meta_crtc_kms_page_flip (MetaCrtc                      *crtc,
+void meta_crtc_kms_page_flip (MetaCrtcKms                   *crtc_kms,
                               const MetaKmsPageFlipFeedback *page_flip_feedback,
                               gpointer                       user_data,
                               MetaKmsUpdate                 *kms_update);
 
-void meta_crtc_kms_set_is_underscanning (MetaCrtc *crtc,
-                                         gboolean  is_underscanning);
+void meta_crtc_kms_set_is_underscanning (MetaCrtcKms *crtc_kms,
+                                         gboolean     is_underscanning);
 
-MetaKmsCrtc * meta_crtc_kms_get_kms_crtc (MetaCrtc *crtc);
+MetaKmsCrtc * meta_crtc_kms_get_kms_crtc (MetaCrtcKms *crtc_kms);
 
-GArray * meta_crtc_kms_get_modifiers (MetaCrtc *crtc,
-                                      uint32_t  format);
+GArray * meta_crtc_kms_get_modifiers (MetaCrtcKms *crtc_kms,
+                                      uint32_t     format);
 
 GArray *
-meta_crtc_kms_copy_drm_format_list (MetaCrtc *crtc);
+meta_crtc_kms_copy_drm_format_list (MetaCrtcKms *crtc_kms);
 
 gboolean
-meta_crtc_kms_supports_format (MetaCrtc *crtc,
-                               uint32_t  drm_format);
+meta_crtc_kms_supports_format (MetaCrtcKms *crtc_kms,
+                               uint32_t     drm_format);
 
-MetaCrtc * meta_crtc_kms_from_kms_crtc (MetaKmsCrtc *kms_crtc);
+MetaCrtcKms * meta_crtc_kms_from_kms_crtc (MetaKmsCrtc *kms_crtc);
 
-MetaCrtc * meta_create_kms_crtc (MetaGpuKms  *gpu_kms,
+MetaCrtcKms * meta_crtc_kms_new (MetaGpuKms  *gpu_kms,
                                  MetaKmsCrtc *kms_crtc);
 
 #endif /* META_CRTC_KMS_H */

src/backends/native/meta-cursor-renderer-native.c
@@ -251,13 +251,14 @@ calculate_crtc_cursor_hotspot (MetaCursorSprite *cursor_sprite,
 static void
 set_crtc_cursor (MetaCursorRendererNative *native,
                  MetaKmsUpdate            *kms_update,
-                 MetaCrtc                 *crtc,
+                 MetaCrtcKms              *crtc_kms,
                  int                       x,
                  int                       y,
                  MetaCursorSprite         *cursor_sprite)
 {
   MetaCursorRendererNativePrivate *priv =
     meta_cursor_renderer_native_get_instance_private (native);
+  MetaCrtc *crtc = META_CRTC (crtc_kms);
   MetaCursorNativePrivate *cursor_priv = get_cursor_priv (cursor_sprite);
   MetaGpuKms *gpu_kms = META_GPU_KMS (meta_crtc_get_gpu (crtc));
   MetaCursorRendererNativeGpuData *cursor_renderer_gpu_data =
@@ -283,7 +284,7 @@ set_crtc_cursor (MetaCursorRendererNative *native,
   else
     bo = get_active_cursor_sprite_gbm_bo (cursor_gpu_state);
 
-  kms_crtc = meta_crtc_kms_get_kms_crtc (crtc);
+  kms_crtc = meta_crtc_kms_get_kms_crtc (crtc_kms);
   kms_device = meta_kms_crtc_get_device (kms_crtc);
   cursor_plane = meta_kms_device_get_cursor_plane_for (kms_device, kms_crtc);
   g_return_if_fail (cursor_plane);
@@ -306,7 +307,7 @@ set_crtc_cursor (MetaCursorRendererNative *native,
   };
 
   flags = META_KMS_ASSIGN_PLANE_FLAG_NONE;
-  crtc_bo = meta_crtc_kms_get_cursor_renderer_private (crtc);
+  crtc_bo = meta_crtc_kms_get_cursor_renderer_private (crtc_kms);
   if (!priv->hw_state_invalidated && bo == crtc_bo)
     flags |= META_KMS_ASSIGN_PLANE_FLAG_FB_UNCHANGED;
 
@@ -325,7 +326,7 @@ set_crtc_cursor (MetaCursorRendererNative *native,
                                                 cursor_hotspot_x,
                                                 cursor_hotspot_y);
 
-  meta_crtc_kms_set_cursor_renderer_private (crtc, bo);
+  meta_crtc_kms_set_cursor_renderer_private (crtc_kms, bo);
 
   if (cursor_gpu_state->pending_bo_state == META_CURSOR_GBM_BO_STATE_SET)
     {
@@ -338,7 +339,7 @@ set_crtc_cursor (MetaCursorRendererNative *native,
 static void
 unset_crtc_cursor (MetaCursorRendererNative *native,
                    MetaKmsUpdate            *kms_update,
-                   MetaCrtc                 *crtc)
+                   MetaCrtcKms              *crtc_kms)
 {
   MetaCursorRendererNativePrivate *priv =
     meta_cursor_renderer_native_get_instance_private (native);
@@ -347,18 +348,18 @@ unset_crtc_cursor (MetaCursorRendererNative *native,
   MetaKmsPlane *cursor_plane;
   struct gbm_bo *crtc_bo;
 
-  crtc_bo = meta_crtc_kms_get_cursor_renderer_private (crtc);
+  crtc_bo = meta_crtc_kms_get_cursor_renderer_private (crtc_kms);
   if (!priv->hw_state_invalidated && !crtc_bo)
     return;
 
-  kms_crtc = meta_crtc_kms_get_kms_crtc (crtc);
+  kms_crtc = meta_crtc_kms_get_kms_crtc (crtc_kms);
   kms_device = meta_kms_crtc_get_device (kms_crtc);
   cursor_plane = meta_kms_device_get_cursor_plane_for (kms_device, kms_crtc);
 
   if (cursor_plane)
     meta_kms_update_unassign_plane (kms_update, kms_crtc, cursor_plane);
 
-  meta_crtc_kms_set_cursor_renderer_private (crtc, NULL);
+  meta_crtc_kms_set_cursor_renderer_private (crtc_kms, NULL);
 }
 
 static float
@@ -484,7 +485,7 @@ update_monitor_crtc_cursor (MetaMonitor         *monitor,
 
       set_crtc_cursor (data->in_cursor_renderer_native,
                        data->in_kms_update,
-                       crtc,
+                       META_CRTC_KMS (crtc),
                        cursor_rect.x,
                        cursor_rect.y,
                        data->in_cursor_sprite);
@@ -495,7 +496,7 @@ update_monitor_crtc_cursor (MetaMonitor         *monitor,
     {
       unset_crtc_cursor (data->in_cursor_renderer_native,
                          data->in_kms_update,
-                         crtc);
+                         META_CRTC_KMS (crtc));
     }
 
   return TRUE;
@@ -505,7 +506,8 @@ static void
 disable_hw_cursor_for_crtc (MetaKmsCrtc  *kms_crtc,
                             const GError *error)
 {
-  MetaCrtc *crtc = meta_crtc_kms_from_kms_crtc (kms_crtc);
+  MetaCrtcKms *crtc_kms = meta_crtc_kms_from_kms_crtc (kms_crtc);
+  MetaCrtc *crtc = META_CRTC (crtc_kms);
   MetaGpuKms *gpu_kms = META_GPU_KMS (meta_crtc_get_gpu (crtc));
   MetaCursorRendererNativeGpuData *cursor_renderer_gpu_data =
     meta_cursor_renderer_native_gpu_data_from_gpu (gpu_kms);
@@ -695,7 +697,8 @@ has_cursor_plane (MetaLogicalMonitor *logical_monitor,
                   gpointer            user_data)
 {
   gboolean *has_cursor_planes = user_data;
-  MetaKmsCrtc *kms_crtc = meta_crtc_kms_get_kms_crtc (crtc);
+  MetaCrtcKms *crtc_kms = META_CRTC_KMS (crtc);
+  MetaKmsCrtc *kms_crtc = meta_crtc_kms_get_kms_crtc (crtc_kms);
   MetaKmsDevice *kms_device = meta_kms_crtc_get_device (kms_crtc);
 
   *has_cursor_planes &= !!meta_kms_device_get_cursor_plane_for (kms_device,
@@ -1053,12 +1056,12 @@ unset_crtc_cursor_renderer_privates (MetaGpu       *gpu,
 
   for (l = meta_gpu_get_crtcs (gpu); l; l = l->next)
     {
-      MetaCrtc *crtc = l->data;
+      MetaCrtcKms *crtc_kms = META_CRTC_KMS (l->data);
       struct gbm_bo *crtc_bo;
 
-      crtc_bo = meta_crtc_kms_get_cursor_renderer_private (crtc);
+      crtc_bo = meta_crtc_kms_get_cursor_renderer_private (crtc_kms);
       if (bo == crtc_bo)
-        meta_crtc_kms_set_cursor_renderer_private (crtc, NULL);
+        meta_crtc_kms_set_cursor_renderer_private (crtc_kms, NULL);
     }
 }
 

src/backends/native/meta-gpu-kms.c
@@ -481,11 +481,11 @@ init_crtcs (MetaGpuKms *gpu_kms)
   for (l = meta_kms_device_get_crtcs (kms_device); l; l = l->next)
     {
       MetaKmsCrtc *kms_crtc = l->data;
-      MetaCrtc *crtc;
+      MetaCrtcKms *crtc_kms;
 
-      crtc = meta_create_kms_crtc (gpu_kms, kms_crtc);
+      crtc_kms = meta_crtc_kms_new (gpu_kms, kms_crtc);
 
-      crtcs = g_list_append (crtcs, crtc);
+      crtcs = g_list_append (crtcs, crtc_kms);
     }
 
   meta_gpu_take_crtcs (gpu, crtcs);

commit 19b467aa01798ef530cec6aeb5f3c30600426106

crtc-kms: Include MetaCrtc in MetaCrtcKms struct
Otherwise it can't be a GObject inheriting MetaCrtc. This fixes warning
logged as

"invalid uninstantiatable type '(null)' in cast to 'MetaCrtcKms'"

Fixes: 1ce9e379d99a8e15b3d043013062104f9b569574

https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1308

src/backends/native/meta-crtc-kms.c
@@ -37,6 +37,8 @@
 
 struct _MetaCrtcKms
 {
+  MetaCrtc parent;
+
   MetaKmsCrtc *kms_crtc;
 
   MetaKmsPlane *primary_plane;