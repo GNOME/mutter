f335df56b664794876ad5dd418d5da35d96bf8ca
Florian Müllner <fmuellner@gnome.org>
2020-12-11 14:30:05 +0100

    ci: Improve checkout script output
    We are currently not very good at communicating what's going in,
    in particular for non-merge request pipelines where the output
    is usually just "Using origin/master instead" (instead of what?).
    
    Improve this by consistently communicate what we are looking for,
    whether we found it, and what we end up doing.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1630>

--- .gitlab-ci/checkout-gnome-shell.sh
+++ .gitlab-ci/checkout-gnome-shell.sh
@@ -2,10 +2,11 @@
 
 gnome_shell_target=
 
-git clone https://gitlab.gnome.org/GNOME/gnome-shell.git
-
-if [ $? -ne 0 ]; then
-  echo Checkout failed
+echo -n Cloning into gnome-shell ...
+if git clone --quiet https://gitlab.gnome.org/GNOME/gnome-shell.git; then
+  echo \ done
+else
+  echo \ failed
   exit 1
 fi
 
@@ -15,19 +16,27 @@ if [ "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" ]; then
   merge_request_remote=${CI_MERGE_REQUEST_SOURCE_PROJECT_URL//mutter/gnome-shell}
   merge_request_branch=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
 
-  echo Looking for $merge_request_branch on remote ...
+  echo -n Looking for $merge_request_branch on remote ...
   if git fetch -q $merge_request_remote $merge_request_branch 2>/dev/null; then
+    echo \ found
     gnome_shell_target=FETCH_HEAD
   else
+    echo \ not found
     gnome_shell_target=origin/$CI_MERGE_REQUEST_TARGET_BRANCH_NAME
     echo Using $gnome_shell_target instead
   fi
 fi
 
 if [ -z "$gnome_shell_target" ]; then
+  echo -n Looking for $CI_COMMIT_REF_NAME on remote ...
   gnome_shell_target=$(git branch -r -l origin/$CI_COMMIT_REF_NAME)
-  gnome_shell_target=${gnome_shell_target:-origin/master}
-  echo Using $gnome_shell_target instead
+  if [ "$gnome_shell_target" ]; then
+    echo \ found
+  else
+    echo \ not found
+    gnome_shell_target=origin/master
+    echo Using $gnome_shell_target instead
+  fi
 fi
 
 git checkout -q $gnome_shell_target
