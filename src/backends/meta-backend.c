c4a73e795020722eda3e2bec0c16d96f9f37333b
Jonas Ådahl <jadahl@gmail.com>
2021-04-10 00:45:01 +0200

    backend: Cleanup renderer on shutdown
    This removes left-over views, and related resources.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1822>

--- src/backends/meta-backend.c
+++ src/backends/meta-backend.c
@@ -244,6 +244,7 @@ meta_backend_dispose (GObject *object)
   g_clear_pointer (&priv->default_seat, clutter_seat_destroy);
   g_clear_pointer (&priv->stage, clutter_actor_destroy);
   g_clear_pointer (&priv->clutter_backend, clutter_backend_destroy);
+  g_clear_object (&priv->renderer);
   g_clear_list (&priv->gpus, g_object_unref);
 
   G_OBJECT_CLASS (meta_backend_parent_class)->dispose (object);
