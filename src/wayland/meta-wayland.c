d4bdd8b56f259da9292b97292f91f497d3d934f6
Fernando Monteiro <fr02monteiro@gmail.com>
2021-09-16 23:34:24 +0100

    wayland: Remove Gtk primary selection protocol
    This has been replaced for primary selection protocol from
    wayland-protocols.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2184>

--- src/wayland/meta-wayland.c
+++ src/wayland/meta-wayland.c
@@ -606,7 +606,6 @@ meta_wayland_compositor_new (MetaContext *context)
   meta_wayland_outputs_init (compositor);
   meta_wayland_data_device_manager_init (compositor);
   meta_wayland_data_device_primary_manager_init (compositor);
-  meta_wayland_data_device_primary_legacy_manager_init (compositor);
   meta_wayland_subsurfaces_init (compositor);
   meta_wayland_shell_init (compositor);
   meta_wayland_pointer_gestures_init (compositor);
