GNOME/mutter

commit a51ad8f932cb55bdd8a62fc5d8d10af7fc62b0a0

core/window: Store/load window dimensions before/after fullscreen
We will use a dedicated variable when transitioning to/from fullscreen state
and leave the previously used 'saved_rect' exclusively for transitioning
between floating and maximized state.

https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/801

src/core/window.c
@@ -3471,7 +3471,7 @@ meta_window_make_fullscreen_internal (MetaWindow  *window)
           meta_window_unshade (window, timestamp);
         }
 
-      meta_window_save_rect (window);
+      window->saved_rect_fullscreen = window->rect;
 
       window->fullscreen = TRUE;
 
@@ -3530,7 +3530,7 @@ meta_window_unmake_fullscreen (MetaWindow  *window)
                   "Unfullscreening %s", window->desc);
 
       window->fullscreen = FALSE;
-      target_rect = window->saved_rect;
+      target_rect = window->saved_rect_fullscreen;
 
       meta_window_frame_size_changed (window);
       meta_window_get_frame_rect (window, &old_frame_rect);
@@ -4074,7 +4074,7 @@ meta_window_move_resize_internal (MetaWindow          *window,
   temporary_rect = window->rect;
   if (flags & (META_MOVE_RESIZE_MOVE_ACTION | META_MOVE_RESIZE_RESIZE_ACTION) &&
       !(flags & META_MOVE_RESIZE_WAYLAND_FINISH_MOVE_RESIZE) &&
-      (unconstrained_rect.width > 0 && unconstrained_rect.height > 0) &&
+      !(flags & (META_MOVE_RESIZE_UNMAXIMIZE | META_MOVE_RESIZE_UNFULLSCREEN)) &&
       window->monitor)
     {
       MetaRectangle old_rect;

src/core/window-private.h
@@ -497,6 +497,9 @@ struct _MetaWindow
   /* The geometry to restore when we unmaximize. */
   MetaRectangle saved_rect;
 
+  /* The geometry to restore when we unfullscreen. */
+  MetaRectangle saved_rect_fullscreen;
+
   /* This is the geometry the window will have if no constraints have
    * applied. We use this whenever we are moving implicitly (for example,
    * if we move to avoid a panel, we can snap back to this position if

commit 13f35ab0640631566f52c80583b09fe4e3dc3685

window: Initialize saved_rect_fullscreen too
We save the window rect before going fullscreen to a dedicated variable,
so we can go back to the correct dimension. We also have a dedicated
variable for returning from other window states, e.g. maximized, and
this one we initialized when creating the MetaWindow. This meant that we
could always rely on this being up to date on X11 windows that were
mapped maximized or fullscreen.

What the commit that introduced the saved rect dedicated for going
unfullscreen missed was to initialize the new saved rectangle too when
creating the MetaWindow. This resulted in windows mapped as fullscreen
often ending up misbehaving when unfullscreening, as mutter would tell
them to unfullscreen to 0x0.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/1786
Fixes: a51ad8f932cb55bdd8a62fc5d8d10af7fc62b0a0
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2210>

src/core/window.c
@@ -1061,6 +1061,7 @@ _meta_window_shared_new (MetaDisplay         *display,
 
   /* And this is our unmaximized size */
   window->saved_rect = window->rect;
+  window->saved_rect_fullscreen = window->rect;
   window->unconstrained_rect = window->rect;
 
   window->depth = attrs->depth;