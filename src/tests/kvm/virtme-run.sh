daa945175badd6d979465c9cb5763cadb68717d8
Jonas Ådahl <jadahl@gmail.com>
2022-01-25 16:37:05 +0100

    virtme-run: Forward some more environment variables
    XDG_DATA_DIRS and LD_LIBRARY_PATH is needed to make sure non-system link
    directories are respected, e.g. when using jhbuild.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2159>

--- src/tests/kvm/virtme-run.sh
+++ src/tests/kvm/virtme-run.sh
@@ -12,12 +12,18 @@ TEST_BUILD_DIR="$5"
 TEST_RESULT_FILE=$(mktemp -p "$TEST_BUILD_DIR" -t test-result-XXXXXX)
 echo 1 > "$TEST_RESULT_FILE"
 
+VIRTME_ENV="\
+HOME=$HOME \
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
+XDG_DATA_DIRS=$XDG_DATA_DIRS \
+"
+
 virtme-run \
   --memory=256M \
   --rw \
   --pwd \
   --kimg "$IMAGE" \
-  --script-sh "sh -c \"env HOME=$HOME $DIRNAME/run-kvm-test.sh \\\"$WRAPPER\\\" \\\"$WRAPPER_ARGS\\\" \\\"$TEST_EXECUTABLE\\\" \\\"$TEST_RESULT_FILE\\\"\""
+  --script-sh "sh -c \"env $VIRTME_ENV $DIRNAME/run-kvm-test.sh \\\"$WRAPPER\\\" \\\"$WRAPPER_ARGS\\\" \\\"$TEST_EXECUTABLE\\\" \\\"$TEST_RESULT_FILE\\\"\""
 
 TEST_RESULT="$(cat "$TEST_RESULT_FILE")"
 rm "$TEST_RESULT_FILE"
