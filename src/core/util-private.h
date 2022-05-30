a9740074caa81f5a699b6e897dbfa80f34ca2c01
Jonas Ådahl <jadahl@gmail.com>
2022-05-30 23:34:17 +0200

    util: Remove left-over declaration
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2619>

--- src/core/util-private.h
+++ src/core/util-private.h
@@ -37,7 +37,6 @@
 void     meta_set_verbose (gboolean setting);
 void     meta_set_debugging (gboolean setting);
 
-void     meta_set_replace_current_wm (gboolean setting);
 void     meta_set_is_wayland_compositor (gboolean setting);
 
 char *   meta_generate_random_id (GRand *rand,
