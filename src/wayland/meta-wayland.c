GNOME/gnome-shell

commit 8df2a1452cf48b724fd3f3e0b72e5175553febe5

wayland: Notify actively of xwayland window/surface associations
Instead of scheduling a meta_later, keep track of the unassociated
windows, and look for matches as soon as the MetaWaylandSurface is
created on our side.

This will ensure the surface is given the Xwayland role before receiving
the first wl_surface.commit.

src/wayland/meta-wayland.c
@@ -46,6 +46,7 @@
 #include "meta-wayland-inhibit-shortcuts.h"
 #include "meta-wayland-inhibit-shortcuts-dialog.h"
 #include "meta-xwayland-grab-keyboard.h"
+#include "meta-xwayland.h"
 
 static MetaWaylandCompositor _meta_wayland_compositor;
 static char *_display_name_override;
@@ -306,6 +307,8 @@ meta_wayland_compositor_init (MetaWaylandCompositor *compositor)
 {
   memset (compositor, 0, sizeof (MetaWaylandCompositor));
   wl_list_init (&compositor->frame_callbacks);
+
+  compositor->scheduled_surface_associations = g_hash_table_new (NULL, NULL);
 }
 
 void
@@ -481,4 +484,63 @@ void
 meta_wayland_compositor_flush_clients (MetaWaylandCompositor *compositor)
 {
   wl_display_flush_clients (compositor->wayland_display);
+}
+
+static void on_scheduled_association_unmanaged (MetaWindow *window,
+                                                gpointer    user_data);
+
+static void
+meta_wayland_compositor_remove_surface_association (MetaWaylandCompositor *compositor,
+                                                    int                    id)
+{
+  MetaWindow *window;
+
+  window = g_hash_table_lookup (compositor->scheduled_surface_associations,
+                                GINT_TO_POINTER (id));
+  if (window)
+    {
+      g_signal_handlers_disconnect_by_func (window,
+                                            on_scheduled_association_unmanaged,
+                                            GINT_TO_POINTER (id));
+      g_hash_table_remove (compositor->scheduled_surface_associations,
+                           GINT_TO_POINTER (id));
+    }
+}
+
+static void
+on_scheduled_association_unmanaged (MetaWindow *window,
+                                    gpointer    user_data)
+{
+  MetaWaylandCompositor *compositor = meta_wayland_compositor_get_default ();
+
+  meta_wayland_compositor_remove_surface_association (compositor,
+                                                      GPOINTER_TO_INT (user_data));
+}
+
+void
+meta_wayland_compositor_schedule_surface_association (MetaWaylandCompositor *compositor,
+                                                      int                    id,
+                                                      MetaWindow            *window)
+{
+  g_signal_connect (window, "unmanaged",
+                    G_CALLBACK (on_scheduled_association_unmanaged),
+                    GINT_TO_POINTER (id));
+  g_hash_table_insert (compositor->scheduled_surface_associations,
+                       GINT_TO_POINTER (id), window);
+}
+
+void
+meta_wayland_compositor_notify_surface_id (MetaWaylandCompositor *compositor,
+                                           int                    id,
+                                           MetaWaylandSurface    *surface)
+{
+  MetaWindow *window;
+
+  window = g_hash_table_lookup (compositor->scheduled_surface_associations,
+                                GINT_TO_POINTER (id));
+  if (window)
+    {
+      meta_xwayland_associate_window_with_surface (window, surface);
+      meta_wayland_compositor_remove_surface_association (compositor, id);
+    }
 }

src/wayland/meta-wayland-private.h
@@ -72,6 +72,8 @@ struct _MetaWaylandCompositor
 
   MetaWaylandSeat *seat;
   MetaWaylandTabletManager *tablet_manager;
+
+  GHashTable *scheduled_surface_associations;
 };
 
 #endif /* META_WAYLAND_PRIVATE_H */

src/wayland/meta-wayland-surface.c
@@ -1322,6 +1322,8 @@ meta_wayland_surface_create (MetaWaylandCompositor *compositor,
   surface->outputs_to_destroy_notify_id = g_hash_table_new (NULL, NULL);
   surface->shortcut_inhibited_seats = g_hash_table_new (NULL, NULL);
 
+  meta_wayland_compositor_notify_surface_id (compositor, id, surface);
+
   return surface;
 }
 

src/wayland/meta-wayland.h
@@ -65,5 +65,12 @@ gboolean                meta_wayland_compositor_is_shortcuts_inhibited (MetaWayl
 
 void                    meta_wayland_compositor_flush_clients (MetaWaylandCompositor *compositor);
 
+void                    meta_wayland_compositor_schedule_surface_association (MetaWaylandCompositor *compositor,
+                                                                              int                    id,
+                                                                              MetaWindow            *window);
+void                    meta_wayland_compositor_notify_surface_id (MetaWaylandCompositor *compositor,
+                                                                   int                    id,
+                                                                   MetaWaylandSurface    *surface);
+
 #endif
 

src/wayland/meta-xwayland.c
@@ -60,9 +60,9 @@ G_DEFINE_TYPE (MetaWaylandSurfaceRoleXWayland,
                meta_wayland_surface_role_xwayland,
                META_TYPE_WAYLAND_ACTOR_SURFACE)
 
-static void
-associate_window_with_surface (MetaWindow         *window,
-                               MetaWaylandSurface *surface)
+void
+meta_xwayland_associate_window_with_surface (MetaWindow          *window,
+                                             MetaWaylandSurface  *surface)
 {
   MetaDisplay *display = window->display;
 
@@ -110,56 +110,11 @@ associate_window_with_surface_id (MetaXWaylandManager *manager,
   if (resource)
     {
       MetaWaylandSurface *surface = wl_resource_get_user_data (resource);
-      associate_window_with_surface (window, surface);
+      meta_xwayland_associate_window_with_surface (window, surface);
       return TRUE;
     }
   else
     return FALSE;
-}
-
-typedef struct {
-  MetaXWaylandManager *manager;
-  MetaWindow *window;
-  guint32 surface_id;
-  guint later_id;
-} AssociateWindowWithSurfaceOp;
-
-static void associate_window_with_surface_window_unmanaged (MetaWindow                   *window,
-                                                            AssociateWindowWithSurfaceOp *op);
-static void
-associate_window_with_surface_op_free (AssociateWindowWithSurfaceOp *op)
-{
-  if (op->later_id != 0)
-    meta_later_remove (op->later_id);
-  g_signal_handlers_disconnect_by_func (op->window,
-                                        (gpointer) associate_window_with_surface_window_unmanaged,
-                                        op);
-  g_free (op);
-}
-
-static void
-associate_window_with_surface_window_unmanaged (MetaWindow                   *window,
-                                                AssociateWindowWithSurfaceOp *op)
-{
-  associate_window_with_surface_op_free (op);
-}
-
-static gboolean
-associate_window_with_surface_later (gpointer user_data)
-{
-  AssociateWindowWithSurfaceOp *op = user_data;
-
-  op->later_id = 0;
-
-  if (!associate_window_with_surface_id (op->manager, op->window, op->surface_id))
-    {
-      /* Not here? Oh well... nothing we can do */
-      g_warning ("Unknown surface ID %d (from window %s)", op->surface_id, op->window->desc);
-    }
-
-  associate_window_with_surface_op_free (op);
-
-  return G_SOURCE_REMOVE;
 }
 
 void
@@ -171,21 +126,11 @@ meta_xwayland_handle_wl_surface_id (MetaWindow *window,
 
   if (!associate_window_with_surface_id (manager, window, surface_id))
     {
-      /* No surface ID yet... it should arrive after the next
-       * iteration through the loop, so queue a later and see
-       * what happens.
+      /* No surface ID yet, schedule this association for whenever the
+       * surface is made known.
        */
-      AssociateWindowWithSurfaceOp *op = g_new0 (AssociateWindowWithSurfaceOp, 1);
-      op->manager = manager;
-      op->window = window;
-      op->surface_id = surface_id;
-      op->later_id = meta_later_add (META_LATER_BEFORE_REDRAW,
-                                     associate_window_with_surface_later,
-                                     op,
-                                     NULL);
-
-      g_signal_connect (op->window, "unmanaged",
-                        G_CALLBACK (associate_window_with_surface_window_unmanaged), op);
+      meta_wayland_compositor_schedule_surface_association (compositor,
+                                                            surface_id, window);
     }
 }
 

src/wayland/meta-xwayland.h
@@ -41,4 +41,8 @@ void
 meta_xwayland_handle_xwayland_grab (MetaWindow *window,
                                     gboolean    allow);
 
+void
+meta_xwayland_associate_window_with_surface (MetaWindow          *window,
+                                             MetaWaylandSurface  *surface);
+
 #endif /* META_XWAYLAND_H */

GNOME/mutter

commit 4ddefb9a69223bdf962906f035edfe3c962bbc32

wayland: Clean up scheduled_surface_associations hash table
Fixes memory leak:

==995170== 192 (96 direct, 96 indirect) bytes in 1 blocks are definitely lost in loss record 14,260 of 15,641
==995170==    at 0x483F7B5: malloc (vg_replace_malloc.c:381)
==995170==    by 0x4B21178: g_malloc (gmem.c:125)
==995170==    by 0x4B395C0: g_slice_alloc (gslice.c:1072)
==995170==    by 0x4B0766D: g_hash_table_new_full (ghash.c:1071)
==995170==    by 0x4A3F3A4: meta_wayland_compositor_init (meta-wayland.c:477)
==995170==    by 0x4E1F509: g_type_create_instance (gtype.c:1929)
==995170==    by 0x4E03DFC: g_object_new_internal (gobject.c:2011)
==995170==    by 0x4E0538C: g_object_new_with_properties (gobject.c:2181)
==995170==    by 0x4E05D40: g_object_new (gobject.c:1821)
==995170==    by 0x4A3F8C4: meta_wayland_compositor_new (meta-wayland.c:590)
==995170==    by 0x49C7FA7: meta_context_start (meta-context.c:412)
==995170==    by 0x10F065: main (mutter.c:148)

Fixes: 8df2a1452cf4 ("wayland: Notify actively of xwayland window/surface associations")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2527>

src/wayland/meta-wayland.c
@@ -448,6 +448,8 @@ meta_wayland_compositor_finalize (GObject *object)
   MetaBackend *backend = meta_context_get_backend (compositor->context);
   ClutterActor *stage = meta_backend_get_stage (backend);
 
+  g_hash_table_destroy (compositor->scheduled_surface_associations);
+
   g_signal_handlers_disconnect_by_func (stage, on_after_update, compositor);
   g_signal_handlers_disconnect_by_func (stage, on_presented, compositor);
 