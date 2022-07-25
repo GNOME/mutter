GNOME/mutter

commit 2ce3a050f02c3424f35c09cfb90cf80121a9bdb7

wayland: Wire up presentation-time machinery
- add surfaces to the presentation list,
- move their feedbacks to the map in on-after-update,
- fire the feedbacks in on_presented().

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1484>

src/wayland/meta-wayland.c
@@ -230,6 +230,52 @@ on_after_update (ClutterStage          *stage,
     }
 }
 
+static MetaWaylandOutput *
+get_output_for_stage_view (MetaWaylandCompositor *compositor,
+                           ClutterStageView      *stage_view)
+{
+  MetaCrtc *crtc;
+  MetaOutput *output;
+  MetaMonitor *monitor;
+  MetaLogicalMonitor *logical_monitor;
+
+  crtc = meta_renderer_view_get_crtc (META_RENDERER_VIEW (stage_view));
+
+  /*
+   * All outputs occupy the same region of the screen, as their contents are
+   * the same, so pick the first one.
+   */
+  output = meta_crtc_get_outputs (crtc)->data;
+
+  monitor = meta_output_get_monitor (output);
+  logical_monitor = meta_monitor_get_logical_monitor (monitor);
+  return g_hash_table_lookup (compositor->outputs, &logical_monitor->winsys_id);
+}
+
+static void
+on_presented (ClutterStage          *stage,
+              ClutterStageView      *stage_view,
+              ClutterFrameInfo      *frame_info,
+              MetaWaylandCompositor *compositor)
+{
+  MetaWaylandPresentationFeedback *feedback, *next;
+  struct wl_list *feedbacks;
+  MetaWaylandOutput *output;
+
+  feedbacks =
+    meta_wayland_presentation_time_ensure_feedbacks (&compositor->presentation_time,
+                                                     stage_view);
+
+  output = get_output_for_stage_view (compositor, stage_view);
+
+  wl_list_for_each_safe (feedback, next, feedbacks, link)
+    {
+      meta_wayland_presentation_feedback_present (feedback,
+                                                  frame_info,
+                                                  output);
+    }
+}
+
 /**
  * meta_wayland_compositor_handle_event:
  * @compositor: the #MetaWaylandCompositor instance
@@ -438,6 +484,8 @@ meta_wayland_compositor_setup (MetaWaylandCompositor *compositor)
 
   g_signal_connect (stage, "after-update",
                     G_CALLBACK (on_after_update), compositor);
+  g_signal_connect (stage, "presented",
+                    G_CALLBACK (on_presented), compositor);
 
   if (!wl_global_create (compositor->wayland_display,
 			 &wl_compositor_interface,

src/wayland/meta-wayland-presentation-time.c
@@ -26,6 +26,7 @@
 
 #include <glib.h>
 
+#include "compositor/meta-surface-actor-wayland.h"
 #include "wayland/meta-wayland-private.h"
 #include "wayland/meta-wayland-surface.h"
 #include "wayland/meta-wayland-outputs.h"
@@ -112,10 +113,8 @@ wp_presentation_bind (struct wl_client *client,
 }
 
 static void
-destroy_feedback_list (gpointer data)
+discard_feedbacks (struct wl_list *feedbacks)
 {
-  struct wl_list *feedbacks = data;
-
   while (!wl_list_empty (feedbacks))
     {
       MetaWaylandPresentationFeedback *feedback =
@@ -123,7 +122,67 @@ destroy_feedback_list (gpointer data)
 
       meta_wayland_presentation_feedback_discard (feedback);
     }
+}
 
+static void
+on_after_paint (ClutterStage          *stage,
+                ClutterStageView      *stage_view,
+                MetaWaylandCompositor *compositor)
+{
+  struct wl_list *feedbacks;
+  GList *l;
+
+  /*
+   * We just painted this stage view, which means that all feedbacks that didn't
+   * fire (e.g. due to page flip failing) are now obsolete and should be
+   * discarded.
+   */
+  feedbacks =
+    meta_wayland_presentation_time_ensure_feedbacks (&compositor->presentation_time,
+                                                     stage_view);
+  discard_feedbacks (feedbacks);
+
+  l = compositor->presentation_time.feedback_surfaces;
+  while (l)
+    {
+      GList *l_cur = l;
+      MetaWaylandSurface *surface = l->data;
+      MetaSurfaceActor *actor;
+      ClutterStageView *surface_primary_view;
+
+      l = l->next;
+
+      actor = meta_wayland_surface_get_actor (surface);
+      if (!actor)
+        continue;
+
+      surface_primary_view =
+        meta_surface_actor_wayland_get_current_primary_view (actor, stage);
+      if (stage_view != surface_primary_view)
+        continue;
+
+      if (!wl_list_empty (&surface->presentation_time.feedback_list))
+        {
+          /* Add feedbacks to the list to be fired on presentation. */
+          wl_list_insert_list (feedbacks,
+                               &surface->presentation_time.feedback_list);
+          wl_list_init (&surface->presentation_time.feedback_list);
+
+          surface->presentation_time.needs_sequence_update = TRUE;
+        }
+
+      compositor->presentation_time.feedback_surfaces =
+        g_list_delete_link (compositor->presentation_time.feedback_surfaces,
+                            l_cur);
+    }
+}
+
+static void
+destroy_feedback_list (gpointer data)
+{
+  struct wl_list *feedbacks = data;
+
+  discard_feedbacks (feedbacks);
   g_free (feedbacks);
 }
 
@@ -141,12 +200,16 @@ meta_wayland_init_presentation_time (MetaWaylandCompositor *compositor)
   MetaBackend *backend = compositor->backend;
   MetaMonitorManager *monitor_manager =
     meta_backend_get_monitor_manager (backend);
+  ClutterActor *stage = meta_backend_get_stage (backend);
 
   compositor->presentation_time.feedbacks =
     g_hash_table_new_full (NULL, NULL, NULL, destroy_feedback_list);
 
   g_signal_connect (monitor_manager, "monitors-changed-internal",
                     G_CALLBACK (on_monitors_changed), compositor);
+
+  g_signal_connect (stage, "after-paint",
+                    G_CALLBACK (on_after_paint), compositor);
 
   if (wl_global_create (compositor->wayland_display,
                         &wp_presentation_interface,

src/wayland/meta-wayland-surface.c
@@ -799,6 +799,10 @@ meta_wayland_surface_apply_state (MetaWaylandSurface      *surface,
                        &state->presentation_feedback_list);
   wl_list_init (&state->presentation_feedback_list);
 
+  if (!wl_list_empty (&surface->presentation_time.feedback_list))
+    meta_wayland_compositor_add_presentation_feedback_surface (surface->compositor,
+                                                               surface);
+
   if (surface->role)
     {
       meta_wayland_surface_role_apply_state (surface->role, state);
@@ -1390,6 +1394,8 @@ wl_surface_destructor (struct wl_resource *resource)
     cairo_region_destroy (surface->input_region);
 
   meta_wayland_compositor_remove_frame_callback_surface (compositor, surface);
+  meta_wayland_compositor_remove_presentation_feedback_surface (compositor,
+                                                                surface);
 
   g_hash_table_foreach (surface->outputs,
                         surface_output_disconnect_signals,

commit 39fd32c362dcc0e277f5794d0534aee224343ebd

wayland: Disconnect signals when Wayland compositor object is finalized
Fixes potential use-after-free during mutter shutdown, e.g.:

==993876== Invalid read of size 8
==993876==    at 0x4A4FCA3: meta_wayland_presentation_time_ensure_feedbacks (meta-wayland-presentation-time.c:373)
==993876==    by 0x4A3F07F: on_presented (meta-wayland.c:282)
==993876==    by 0x661B7E9: ??? (in /usr/lib/x86_64-linux-gnu/libffi.so.8.1.0)
==993876==    by 0x661A922: ??? (in /usr/lib/x86_64-linux-gnu/libffi.so.8.1.0)
==993876==    by 0x4DFF4BC: g_cclosure_marshal_generic_va (gclosure.c:1648)
==993876==    by 0x4DFE948: _g_closure_invoke_va (gclosure.c:893)
==993876==    by 0x4E17498: g_signal_emit_valist (gsignal.c:3406)
==993876==    by 0x4E176BE: g_signal_emit (gsignal.c:3553)
==993876==    by 0x51D9DB5: clutter_stage_view_notify_presented (clutter-stage-view.c:1226)
==993876==    by 0x499ACD2: frame_cb (meta-stage-view.c:83)
==993876==    by 0x499ACD2: frame_cb (meta-stage-view.c:43)
==993876==    by 0x50CAA41: notify_event (cogl-onscreen.c:175)
==993876==    by 0x50CAA41: _cogl_onscreen_notify_complete (cogl-onscreen.c:545)
==993876==    by 0x4A877F5: meta_onscreen_native_notify_frame_complete (meta-onscreen-native.c:211)
==993876==  Address 0x24b7be58 is 296 bytes inside a block of size 344 free'd
==993876==    at 0x484217B: free (vg_replace_malloc.c:872)
==993876==    by 0x4E1F88B: g_type_free_instance (gtype.c:2001)
==993876==    by 0x49C793C: meta_context_dispose (meta-context.c:675)
==993876==    by 0x4E037E0: g_object_unref (gobject.c:3636)
==993876==    by 0x4E037E0: g_object_unref (gobject.c:3553)
==993876==    by 0x10F145: glib_autoptr_clear_GObject (gobject-autocleanups.h:27)
==993876==    by 0x10F145: glib_autoptr_clear_MetaContext (meta-context.h:32)
==993876==    by 0x10F145: glib_autoptr_cleanup_MetaContext (meta-context.h:32)
==993876==    by 0x10F145: main (mutter.c:126)
==993876==  Block was alloc'd at
==993876==    at 0x483F7B5: malloc (vg_replace_malloc.c:381)
==993876==    by 0x4B21178: g_malloc (gmem.c:125)
==993876==    by 0x4B395C0: g_slice_alloc (gslice.c:1072)
==993876==    by 0x4B39C29: g_slice_alloc0 (gslice.c:1098)
==993876==    by 0x4E1F544: g_type_create_instance (gtype.c:1901)
==993876==    by 0x4E03DFC: g_object_new_internal (gobject.c:2011)
==993876==    by 0x4E0538C: g_object_new_with_properties (gobject.c:2181)
==993876==    by 0x4E05D40: g_object_new (gobject.c:1821)
==993876==    by 0x4A3F864: meta_wayland_compositor_new (meta-wayland.c:585)
==993876==    by 0x49C7FA7: meta_context_start (meta-context.c:412)
==993876==    by 0x10F065: main (mutter.c:148)

Fixes: 2ce3a050f02c ("wayland: Wire up presentation-time machinery")

src/wayland/meta-wayland.c
@@ -445,6 +445,11 @@ static void
 meta_wayland_compositor_finalize (GObject *object)
 {
   MetaWaylandCompositor *compositor = META_WAYLAND_COMPOSITOR (object);
+  MetaBackend *backend = meta_context_get_backend (compositor->context);
+  ClutterActor *stage = meta_backend_get_stage (backend);
+
+  g_signal_handlers_disconnect_by_func (stage, on_after_update, compositor);
+  g_signal_handlers_disconnect_by_func (stage, on_presented, compositor);
 
   g_clear_object (&compositor->dma_buf_manager);
 