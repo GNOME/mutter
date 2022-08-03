97c5638a35e8b5d0d01582994fb524a2998b3ce6
Jonas Ådahl <jadahl@gmail.com>
2022-08-03 18:57:10 +0200

    tests/runner: Add 'resize_monitor' metatest command
    This will allow tests to change monitor resolution. The first argument
    is the monitor ID; there is always one monitor added by default, and it
    has the id 0. It's currently not possible to add more monitors, so
    passing '0' is the only valid way to resize monitors.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2554>

--- src/backends/meta-virtual-monitor.h
+++ src/backends/meta-virtual-monitor.h
@@ -70,6 +70,7 @@ void meta_virtual_monitor_info_free (MetaVirtualMonitorInfo *info);
 
 MetaCrtc * meta_virtual_monitor_get_crtc (MetaVirtualMonitor *virtual_monitor);
 
+META_EXPORT_TEST
 MetaCrtcMode * meta_virtual_monitor_get_crtc_mode (MetaVirtualMonitor *virtual_monitor);
 
 META_EXPORT_TEST
