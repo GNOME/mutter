GNOME/mutter

commit baef39e603f232f5ce3c2ce79c443383cd6a1f67

color-device: Add API to update gamma LUTs
This will generate a gamma LUT from the profile and apply it to the
monitor.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2165>

src/backends/meta-color-device.c
@@ -1085,3 +1085,36 @@ meta_color_device_get_assigned_profile (MetaColorDevice *color_device)
 {
   return color_device->assigned_profile;
 }
+
+void
+meta_color_device_update_gamma (MetaColorDevice *color_device,
+                                unsigned int     temperature)
+{
+  MetaColorProfile *color_profile;
+  MetaMonitor *monitor;
+  size_t lut_size;
+  g_autoptr (MetaGammaLut) lut = NULL;
+
+  color_profile = meta_color_device_get_assigned_profile (color_device);
+  if (!color_profile)
+    return;
+
+  monitor = color_device->monitor;
+  if (!meta_monitor_is_active (monitor))
+    return;
+
+  meta_topic (META_DEBUG_COLOR,
+              "Updating device '%s' (%s) using color profile '%s' "
+              "and temperature %uK",
+              meta_color_device_get_id (color_device),
+              meta_monitor_get_connector (monitor),
+              meta_color_profile_get_id (color_profile),
+              temperature);
+
+  lut_size = meta_monitor_get_gamma_lut_size (monitor);
+  lut = meta_color_profile_generate_gamma_lut (color_profile,
+                                               temperature,
+                                               lut_size);
+
+  meta_monitor_set_gamma_lut (monitor, lut);
+}

src/backends/meta-color-device.h
@@ -62,4 +62,7 @@ MetaColorProfile * meta_color_device_generate_profile_finish (MetaColorDevice  *
 META_EXPORT_TEST
 gboolean meta_color_device_is_ready (MetaColorDevice *color_device);
 
+void meta_color_device_update_gamma (MetaColorDevice *color_device,
+                                     unsigned int     temperature);
+
 #endif /* META_COLOR_DEVICE_H */

commit 255cd9a9fff5d928eb63eb3464b5d4c27a2a74e4

color-device: Don't attempt to set GAMMA_LUT if LUT size is zero
This might happen on e.g. virtual machines. If we don't skip trying,
we'll end up crashing because we'll fail to create a look up table since
the size must be greater than zero to make any sense.

Fixes: baef39e603f232f5ce3c2ce79c443383cd6a1f67
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/2415
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2611>

src/backends/meta-color-device.c
@@ -1171,7 +1171,6 @@ meta_color_device_update (MetaColorDevice *color_device,
   MetaColorProfile *color_profile;
   MetaMonitor *monitor;
   size_t lut_size;
-  g_autoptr (MetaGammaLut) lut = NULL;
 
   color_profile = meta_color_device_get_assigned_profile (color_device);
   if (!color_profile)
@@ -1206,11 +1205,16 @@ meta_color_device_update (MetaColorDevice *color_device,
     }
 
   lut_size = meta_monitor_get_gamma_lut_size (monitor);
-  lut = meta_color_profile_generate_gamma_lut (color_profile,
-                                               temperature,
-                                               lut_size);
+  if (lut_size > 0)
+    {
+      g_autoptr (MetaGammaLut) lut = NULL;
 
-  meta_monitor_set_gamma_lut (monitor, lut);
+      lut = meta_color_profile_generate_gamma_lut (color_profile,
+                                                   temperature,
+                                                   lut_size);
+
+      meta_monitor_set_gamma_lut (monitor, lut);
+    }
 
   g_signal_emit (color_device, signals[UPDATED], 0);
 }

src/backends/meta-color-profile.c
@@ -488,7 +488,7 @@ meta_color_profile_generate_gamma_lut (MetaColorProfile *color_profile,
                                        unsigned int      temperature,
                                        size_t            lut_size)
 {
-  g_return_val_if_fail (lut_size > 0, NULL);
+  g_assert (lut_size > 0);
 
   if (color_profile->calibration->has_vcgt)
     {