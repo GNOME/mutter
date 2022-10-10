8420692d3e03fff832f55a9cdb385720fc16bc4a
Jonas Ådahl <jadahl@gmail.com>
2022-10-11 01:08:46 +0200

    cogl: Add soft-float software implementation
    The implementation comes from mesa, which came from Berkeley SoftFloat
    3e Library.
    
    The software float implementation will be used to convert from and to
    half floating point pixel format bitmaps, when no the CPU isn't capable
    of the conversion.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3441>

---
+++ cogl/cogl/cogl-soft-float.h
@@ -0,0 +1,71 @@
+/*
+ * License for Berkeley SoftFloat Release 3e
+ *
+ * John R. Hauser
+ * 2018 January 20
+ *
+ * The following applies to the whole of SoftFloat Release 3e as well as to
+ * each source file individually.
+ *
+ * Copyright 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 The Regents of the
+ * University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions, and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions, and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the University nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS", AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * The functions listed in this file are modified versions of the ones
+ * from the Berkeley SoftFloat 3e Library.
+ */
+
+#ifndef COGL_SOFT_FLOAT_H
+#define COGL_SOFT_FLOAT_H
+
+#include <glib.h>
+#include <stdint.h>
+
+double cogl_double_add_rtz (double a,
+                            double b);
+
+double cogl_double_sub_rtz (double a,
+                            double b);
+
+double cogl_double_mul_rtz (double a,
+                            double b);
+
+double cogl_double_fma_rtz (double a,
+                            double b,
+                            double c);
+
+float cogl_float_fma_rtz (float a,
+                          float b,
+                          float c);
+
+float cogl_double_to_f32 (double   x,
+                          gboolean rtz);
+
+uint16_t cogl_float_to_half_rtz_slow (float x);
+
+#endif  /* COGL_SOFT_FLOAT_H */
