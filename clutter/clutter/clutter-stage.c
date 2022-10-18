GNOME/mutter

commit 9c79c723411625e62904a77c65a83afcc1fdbb5b

clutter: Only allow grabs to be created on realized actors
The bare minimum that we can ask to an actor before creating a grab
on it is that it is realized (and thus, attached to the stage). Bail
out if that is not the case when creating a grab.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2669>

clutter/clutter/clutter-stage.c
@@ -3790,6 +3790,7 @@ clutter_stage_grab (ClutterStage *stage,
 
   g_return_val_if_fail (CLUTTER_IS_STAGE (stage), NULL);
   g_return_val_if_fail (CLUTTER_IS_ACTOR (actor), NULL);
+  g_return_val_if_fail (clutter_actor_is_realized (actor), NULL);
 
   priv = stage->priv;
 

commit 714f01048eac3cb8c8f578132afd49bd164f2ada

clutter: Demote the new clutter_stage_grab() precondition check
There do indeed seem to be places in our own code that trigger grabs on
actors before they are realized. It was not the intention to change the
practical preconditions for GNOME 43, so make it an even lower minimum
that every caller ought to match: That the actor is attached to the stage.

Further constraining of these preconditions will have to wait until
branching for new development.

Fixes: 9c79c7234 (clutter: Only allow grabs to be created on realized actors)
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2670>

clutter/clutter/clutter-stage.c
@@ -3790,7 +3790,9 @@ clutter_stage_grab (ClutterStage *stage,
 
   g_return_val_if_fail (CLUTTER_IS_STAGE (stage), NULL);
   g_return_val_if_fail (CLUTTER_IS_ACTOR (actor), NULL);
-  g_return_val_if_fail (clutter_actor_is_realized (actor), NULL);
+  g_return_val_if_fail (stage ==
+                        (ClutterStage *) _clutter_actor_get_stage_internal (actor),
+                        NULL);
 
   priv = stage->priv;
 