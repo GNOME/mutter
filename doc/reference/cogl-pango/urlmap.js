9a5289cf58fc258bcbbbbd32482c134812b8a6b6
Bilal Elmoussaoui <belmouss@redhat.com>
2022-11-22 13:00:29 +0100

    docs/cogl-pango: Update urlmap
    Drop unnused dependencies
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2708>

--- doc/reference/cogl-pango/urlmap.js
+++ doc/reference/cogl-pango/urlmap.js
@@ -3,13 +3,7 @@
 
 // A map between namespaces and base URLs for their online documentation
 baseURLs = [
-    [ 'GLib', 'https://docs.gtk.org/glib/' ],
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
-    [ 'Gio', 'https://docs.gtk.org/gio/' ],
-    [ 'Atk', 'https://docs.gtk.org/atk/' ],
-    [ 'Gdk', 'https://docs.gtk.org/gdk3/' ],
-    [ 'Gtk', 'https://docs.gtk.org/gtk3/' ],
     [ 'Pango', 'https://docs.gtk.org/Pango/' ],
-    [ 'PangoCairo', 'https://docs.gtk.org/PangoCairo/' ],
-    [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
+    [ 'Cogl', 'https://gnome.pages.gitlab.gnome.org/mutter/cogl/' ],
 ]
