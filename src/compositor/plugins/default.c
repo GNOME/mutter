GNOME/mutter

commit 562024f746b5bcae7fcdd9cc2adac7a94f726110

plugin/default: Don't load properties when getting locale proxy
This cuts away between 7 and 40 ms from the startup of tests.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2678>

src/compositor/plugins/default.c
@@ -424,7 +424,9 @@ init_keymap (MetaDefaultPlugin *self)
   g_autofree char *x11_variant = NULL;
 
   proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
-                                         G_DBUS_PROXY_FLAGS_NONE,
+                                         (G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES |
+                                          G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS |
+                                          G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START),
                                          NULL,
                                          "org.freedesktop.locale1",
                                          "/org/freedesktop/locale1",

commit eb7abdacd8eee3b05a0c21c4df5a029ab8ba6915

plugin/default: Autostart localed when querying keyboard layout
Otherwise it's required that systemd-localed is already running, which
is unlikely, since it will auto-terminate by default.

Fixes: 562024f746b5bcae7fcdd9cc2adac7a94f726110
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2710>

src/compositor/plugins/default.c
@@ -425,8 +425,7 @@ init_keymap (MetaDefaultPlugin *self)
 
   proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
                                          (G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES |
-                                          G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS |
-                                          G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START),
+                                          G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS),
                                          NULL,
                                          "org.freedesktop.locale1",
                                          "/org/freedesktop/locale1",