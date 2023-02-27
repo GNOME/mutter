GNOME/mutter

commit 2d8fa26c8e07fbedda5f1266c4897d8dbbb9b65c

core: Pass "frame action" grab operations as an "unconstrained" grab op
The frame_action boolean is only used by constraints.c code, in order to
determine whether a moving window should be able to move past the top
bar or not.

We can avoid the special casing by passing this information as a
META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED flag passed with the grab op.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2683>

src/core/display.c
@@ -1670,6 +1670,8 @@ meta_display_notify_window_created (MetaDisplay  *display,
 static MetaCursor
 meta_cursor_for_grab_op (MetaGrabOp op)
 {
+  op &= ~(META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED);
+
   switch (op)
     {
     case META_GRAB_OP_RESIZING_SE:
@@ -1880,6 +1882,9 @@ meta_display_begin_grab_op (MetaDisplay *display,
       return FALSE;
     }
 
+  if (!frame_action)
+    op |= META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED;
+
   event_route = get_event_route_from_grab_op (op);
 
   if (event_route == META_EVENT_ROUTE_WINDOW_OP)
@@ -1957,7 +1962,6 @@ meta_display_begin_grab_op (MetaDisplay *display,
   display->grab_latest_motion_x = root_x;
   display->grab_latest_motion_y = root_y;
   display->grab_last_edge_resistance_flags = META_EDGE_RESISTANCE_DEFAULT;
-  display->grab_frame_action = frame_action;
 
   meta_display_update_cursor (display);
 
@@ -2047,7 +2051,6 @@ meta_display_end_grab_op (MetaDisplay *display,
   display->grab_latest_motion_x = 0;
   display->grab_latest_motion_y = 0;
   display->grab_last_edge_resistance_flags = META_EDGE_RESISTANCE_DEFAULT;
-  display->grab_frame_action = FALSE;
 
   meta_display_update_cursor (display);
 
@@ -2563,6 +2566,8 @@ MetaGravity
 meta_resize_gravity_from_grab_op (MetaGrabOp op)
 {
   MetaGravity gravity;
+
+  op &= ~(META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED);
 
   gravity = -1;
   switch (op)

src/core/constraints.c
@@ -1769,7 +1769,8 @@ constrain_titlebar_visible (MetaWindow         *window,
    * clicking on the frame to start the move.
    */
   unconstrained_user_action =
-    info->is_user_action && !window->display->grab_frame_action;
+    info->is_user_action &&
+    (window->display->grab_op & META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED) != 0;
 
   /* Exit early if we know the constraint won't apply--note that this constraint
    * is only meant for normal windows (e.g. we don't want docks to be shoved

src/core/display-private.h
@@ -172,7 +172,6 @@ struct _MetaDisplay
   int         grab_latest_motion_y;
   guint       grab_have_pointer : 1;
   guint       grab_have_keyboard : 1;
-  guint       grab_frame_action : 1;
   MetaRectangle grab_initial_window_pos;
   int         grab_initial_x, grab_initial_y;  /* These are only relevant for */
   gboolean    grab_threshold_movement_reached; /* raise_on_click == FALSE.    */

src/meta/meta-enums.h
@@ -110,6 +110,7 @@ enum
 {
   META_GRAB_OP_WINDOW_FLAG_KEYBOARD = 0x0100,
   META_GRAB_OP_WINDOW_FLAG_UNKNOWN  = 0x0200,
+  META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED = 0x0400,
   META_GRAB_OP_WINDOW_DIR_WEST      = 0x1000,
   META_GRAB_OP_WINDOW_DIR_EAST      = 0x2000,
   META_GRAB_OP_WINDOW_DIR_SOUTH     = 0x4000,
@@ -119,6 +120,7 @@ enum
   /* WGO = "window grab op". shorthand for below */
   _WGO_K = META_GRAB_OP_WINDOW_FLAG_KEYBOARD,
   _WGO_U = META_GRAB_OP_WINDOW_FLAG_UNKNOWN,
+  _WGO_C = META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED,
   _WGO_W = META_GRAB_OP_WINDOW_DIR_WEST,
   _WGO_E = META_GRAB_OP_WINDOW_DIR_EAST,
   _WGO_S = META_GRAB_OP_WINDOW_DIR_SOUTH,
@@ -133,6 +135,7 @@ typedef enum
   META_GRAB_OP_WINDOW_BASE,
 
   META_GRAB_OP_MOVING                     = META_GRAB_OP_WINDOW_BASE,
+  META_GRAB_OP_MOVING_UNCONSTRAINED       = META_GRAB_OP_WINDOW_BASE | _WGO_C,
   META_GRAB_OP_RESIZING_NW                = META_GRAB_OP_WINDOW_BASE | _WGO_N | _WGO_W,
   META_GRAB_OP_RESIZING_N                 = META_GRAB_OP_WINDOW_BASE | _WGO_N,
   META_GRAB_OP_RESIZING_NE                = META_GRAB_OP_WINDOW_BASE | _WGO_N | _WGO_E,

commit 5bdc08099e951e3907b20740bab1d5d941172c31

compositor: Fix handling of keyboard-driven window resize
The introduction of the META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED
flag threw off some checks around keyboard-driven resize. This
was partly because there were some == checks that did not account
for that flag maybe being enabled, but also the handling
of META_GRAB_OP_KEYBOARD_RESIZING_UNKNOWN into a definite
resize direction was maybe unsetting that flag. Fix both things
at the same time.

Fixes: 2d8fa26c8e ("core: Pass "frame action" grab operations as an "unconstrained" grab op")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/2629
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2871>

src/compositor/meta-window-drag.c
@@ -722,10 +722,14 @@ process_keyboard_resize_grab_op_change (MetaWindowDrag  *window_drag,
                                         MetaWindow      *window,
                                         ClutterKeyEvent *event)
 {
+  MetaGrabOp op, unconstrained;
   gboolean handled;
 
+  op = (window_drag->grab_op & ~META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED);
+  unconstrained = (window_drag->grab_op & META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED);
+
   handled = FALSE;
-  switch (window_drag->grab_op)
+  switch (op)
     {
     case META_GRAB_OP_KEYBOARD_RESIZING_UNKNOWN:
       switch (event->keyval)
@@ -827,6 +831,8 @@ process_keyboard_resize_grab_op_change (MetaWindowDrag  *window_drag,
       g_assert_not_reached ();
       break;
     }
+
+  window_drag->grab_op |= unconstrained;
 
   if (handled)
     {
@@ -1091,7 +1097,8 @@ process_key_event (MetaWindowDrag  *window_drag,
 
   if (window_drag->grab_op & META_GRAB_OP_WINDOW_FLAG_KEYBOARD)
     {
-      if (window_drag->grab_op == META_GRAB_OP_KEYBOARD_MOVING)
+      if ((window_drag->grab_op & META_GRAB_OP_KEYBOARD_MOVING) ==
+          META_GRAB_OP_KEYBOARD_MOVING)
         {
           meta_topic (META_DEBUG_KEYBINDINGS,
                       "Processing event for keyboard move");
@@ -1454,9 +1461,12 @@ update_resize (MetaWindowDrag          *window_drag,
   if (dx == 0 && dy == 0)
     return;
 
-  if (window_drag->grab_op == META_GRAB_OP_KEYBOARD_RESIZING_UNKNOWN)
+  if ((window_drag->grab_op & META_GRAB_OP_KEYBOARD_RESIZING_UNKNOWN) ==
+      META_GRAB_OP_KEYBOARD_RESIZING_UNKNOWN)
     {
-      MetaGrabOp op = META_GRAB_OP_WINDOW_BASE | META_GRAB_OP_WINDOW_FLAG_KEYBOARD;
+      MetaGrabOp op = META_GRAB_OP_WINDOW_BASE |
+        META_GRAB_OP_WINDOW_FLAG_KEYBOARD |
+        (window_drag->grab_op & META_GRAB_OP_WINDOW_FLAG_UNCONSTRAINED);
 
       if (dx > 0)
         op |= META_GRAB_OP_WINDOW_DIR_EAST;

src/core/display.c
@@ -1198,7 +1198,9 @@ meta_grab_op_is_keyboard (MetaGrabOp op)
 gboolean
 meta_grab_op_is_resizing (MetaGrabOp op)
 {
-  return (op & META_GRAB_OP_WINDOW_DIR_MASK) != 0 || op == META_GRAB_OP_KEYBOARD_RESIZING_UNKNOWN;
+  return (op & META_GRAB_OP_WINDOW_DIR_MASK) != 0 ||
+    (op & META_GRAB_OP_KEYBOARD_RESIZING_UNKNOWN) ==
+    META_GRAB_OP_KEYBOARD_RESIZING_UNKNOWN;
 }
 
 gboolean