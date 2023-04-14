GNOME/mutter

commit e66f4396ee0b8c84c9d2d186d65608f2f4f43a58

x11: Avoid GDK API in X11 selections
Use MetaX11Display's Display, and XInternAtom/XGetAtomName to
convert between Atoms and strings.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2836>

src/x11/meta-selection-source-x11.c
@@ -21,8 +21,6 @@
 
 #include "config.h"
 
-#include <gdk/gdkx.h>
-
 #include "x11/meta-x11-selection-input-stream-private.h"
 #include "x11/meta-selection-source-x11-private.h"
 
@@ -77,6 +75,7 @@ meta_selection_source_x11_read_async (MetaSelectionSource *source,
                                       gpointer             user_data)
 {
   MetaSelectionSourceX11 *source_x11 = META_SELECTION_SOURCE_X11 (source);
+  Display *xdisplay = meta_x11_display_get_xdisplay (source_x11->x11_display);
   GTask *task;
 
   task = g_task_new (source, cancellable, callback, user_data);
@@ -92,7 +91,7 @@ meta_selection_source_x11_read_async (MetaSelectionSource *source,
     mimetype = "UTF8_STRING";
 
   meta_x11_selection_input_stream_new_async (source_x11->x11_display,
-                                             gdk_x11_get_xatom_name (source_x11->xselection),
+                                             XGetAtomName (xdisplay, source_x11->xselection),
                                              mimetype,
                                              source_x11->timestamp,
                                              G_PRIORITY_DEFAULT,
@@ -143,6 +142,7 @@ static GList *
 atoms_to_mimetypes (MetaX11Display *display,
                     GBytes         *bytes)
 {
+  Display *xdisplay = meta_x11_display_get_xdisplay (display);
   GList *mimetypes = NULL;
   const Atom *atoms;
   gsize size;
@@ -157,7 +157,7 @@ atoms_to_mimetypes (MetaX11Display *display,
     {
       const gchar *mimetype;
 
-      mimetype = gdk_x11_get_xatom_name (atoms[i]);
+      mimetype = XGetAtomName (xdisplay, atoms[i]);
       mimetypes = g_list_prepend (mimetypes, g_strdup (mimetype));
 
       utf8_text_plain_found |= strcmp (mimetype, "text/plain;charset=utf-8") == 0;
@@ -236,6 +236,7 @@ meta_selection_source_x11_new_async (MetaX11Display      *x11_display,
                                      GAsyncReadyCallback  callback,
                                      gpointer             user_data)
 {
+  Display *xdisplay = meta_x11_display_get_xdisplay (x11_display);
   MetaSelectionSourceX11 *source;
   GTask *task;
 
@@ -250,7 +251,7 @@ meta_selection_source_x11_new_async (MetaX11Display      *x11_display,
   g_task_set_task_data (task, source, g_object_unref);
 
   meta_x11_selection_input_stream_new_async (x11_display,
-                                             gdk_x11_get_xatom_name (xselection),
+                                             XGetAtomName (xdisplay, xselection),
                                              "TARGETS",
                                              timestamp,
                                              G_PRIORITY_DEFAULT,

src/x11/meta-x11-selection-input-stream.c
@@ -23,8 +23,6 @@
 
 #include "meta-x11-selection-input-stream-private.h"
 
-#include <gdk/gdkx.h>
-
 #include "meta/meta-x11-errors.h"
 #include "x11/meta-x11-display-private.h"
 
@@ -489,7 +487,7 @@ meta_x11_selection_input_stream_xevent (MetaX11SelectionInputStream *stream,
             bytes = get_selection_property (priv->x11_display, xwindow,
                                             xevent->xselection.property,
                                             &priv->xtype, &priv->format);
-            priv->type = gdk_x11_get_xatom_name (priv->xtype);
+            priv->type = XGetAtomName (xdisplay, priv->xtype);
 
             g_task_return_pointer (task, g_object_ref (stream), g_object_unref);
 

src/x11/meta-x11-selection.c
@@ -21,8 +21,6 @@
 
 #include "config.h"
 
-#include <gdk/gdkx.h>
-
 #include "core/meta-selection-private.h"
 #include "meta/meta-selection-source-memory.h"
 #include "x11/meta-selection-source-x11-private.h"
@@ -125,10 +123,11 @@ mimetypes_to_bytes (GList   *mimetypes,
 }
 
 static void
-send_selection_notify (XSelectionRequestEvent *request_event,
+send_selection_notify (MetaX11Display         *x11_display,
+                       XSelectionRequestEvent *request_event,
                        gboolean                accepted)
 {
-  Display *xdisplay = GDK_DISPLAY_XDISPLAY (gdk_display_get_default ());
+  Display *xdisplay = meta_x11_display_get_xdisplay (x11_display);
   XSelectionEvent event;
 
   memset(&event, 0, sizeof (XSelectionEvent));
@@ -184,12 +183,13 @@ meta_x11_selection_find_target (MetaX11Display     *x11_display,
                                 MetaSelectionType   selection_type,
                                 Atom                selection_atom)
 {
+  Display *xdisplay = meta_x11_display_get_xdisplay (x11_display);
   GList* mimetypes = NULL;
   const gchar *atom_name;
   char *retval;
 
   mimetypes = meta_selection_get_mimetypes (selection, selection_type);
-  atom_name = gdk_x11_get_xatom_name (selection_atom);
+  atom_name = XGetAtomName (xdisplay, selection_atom);
 
   if (g_list_find_custom (mimetypes, atom_name, (GCompareFunc) g_strcmp0))
     {
@@ -222,6 +222,7 @@ meta_x11_selection_handle_selection_request (MetaX11Display *x11_display,
                                              XEvent         *xevent)
 {
   MetaDisplay *display = meta_x11_display_get_display (x11_display);
+  Display *xdisplay = meta_x11_display_get_xdisplay (x11_display);
   XSelectionRequestEvent *event = (XSelectionRequestEvent *) xevent;
   MetaSelectionType selection_type;
   MetaSelection *selection;
@@ -235,7 +236,7 @@ meta_x11_selection_handle_selection_request (MetaX11Display *x11_display,
 
   selection = meta_display_get_selection (display);
 
-  if (event->target == gdk_x11_get_xatom_by_name ("TARGETS"))
+  if (event->target == XInternAtom (xdisplay, "TARGETS", False))
     {
       GBytes *bytes;
 
@@ -243,14 +244,14 @@ meta_x11_selection_handle_selection_request (MetaX11Display *x11_display,
 
       if (!mimetypes)
         {
-          send_selection_notify (event, FALSE);
+          send_selection_notify (x11_display, event, FALSE);
           return FALSE;
         }
 
       output = meta_x11_selection_output_stream_new (x11_display, event->requestor,
-                                                     gdk_x11_get_xatom_name (event->selection),
-                                                     gdk_x11_get_xatom_name (event->target),
-                                                     gdk_x11_get_xatom_name (event->property),
+                                                     XGetAtomName (xdisplay, event->selection),
+                                                     XGetAtomName (xdisplay, event->target),
+                                                     XGetAtomName (xdisplay, event->property),
                                                      "ATOM", 32, event->time);
 
       bytes = mimetypes_to_bytes (mimetypes, x11_display->xdisplay);
@@ -265,12 +266,12 @@ meta_x11_selection_handle_selection_request (MetaX11Display *x11_display,
       g_bytes_unref (bytes);
       return TRUE;
     }
-  else if (event->target == gdk_x11_get_xatom_by_name ("DELETE"))
+  else if (event->target == XInternAtom (xdisplay, "DELETE", False))
     {
       /* DnD only, this is just handled through other means on our non-x11
        * sources, so just go with it.
        */
-      send_selection_notify (event, TRUE);
+      send_selection_notify (x11_display, event, TRUE);
     }
   else
     {
@@ -283,10 +284,10 @@ meta_x11_selection_handle_selection_request (MetaX11Display *x11_display,
         {
           output = meta_x11_selection_output_stream_new (x11_display,
                                                          event->requestor,
-                                                         gdk_x11_get_xatom_name (event->selection),
-                                                         gdk_x11_get_xatom_name (event->target),
-                                                         gdk_x11_get_xatom_name (event->property),
-                                                         gdk_x11_get_xatom_name (event->target),
+                                                         XGetAtomName (xdisplay, event->selection),
+                                                         XGetAtomName (xdisplay, event->target),
+                                                         XGetAtomName (xdisplay, event->property),
+                                                         XGetAtomName (xdisplay, event->target),
                                                          8, event->time);
 
           meta_selection_transfer_async (selection,
@@ -301,7 +302,7 @@ meta_x11_selection_handle_selection_request (MetaX11Display *x11_display,
         }
       else
         {
-          send_selection_notify (event, FALSE);
+          send_selection_notify (x11_display, event, FALSE);
         }
     }
 

commit 01ae1d32eefedb613032f5b5b980a614831d3391

x11: Remove unused member variables from MetaX11SelectionInputStream
The private format and type member variables were not being used by any
of the callers, so they can simply be removed. This also fixes a leak of
type which was introduced when switching from gdk_x11_get_xatom_name()
to XGetAtomName().

Fixes: e66f4396e ("x11: Avoid GDK API in X11 selections")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2957>

src/x11/meta-selection-source-x11.c
@@ -48,7 +48,7 @@ stream_new_cb (GObject      *source,
   GInputStream *stream;
   GError *error = NULL;
 
-  stream = meta_x11_selection_input_stream_new_finish (res, NULL, NULL, &error);
+  stream = meta_x11_selection_input_stream_new_finish (res, &error);
 
   if (stream)
     g_task_return_pointer (task, stream, g_object_unref);
@@ -217,7 +217,7 @@ get_mimetypes_cb (GObject      *source,
   GInputStream *stream;
   GError *error = NULL;
 
-  stream = meta_x11_selection_input_stream_new_finish (res, NULL, NULL, &error);
+  stream = meta_x11_selection_input_stream_new_finish (res, &error);
   if (error)
     {
       g_task_return_error (task, error);

src/x11/meta-x11-selection-input-stream-private.h
@@ -41,8 +41,6 @@ void           meta_x11_selection_input_stream_new_async     (MetaX11Display    
                                                               GAsyncReadyCallback         callback,
                                                               gpointer                    user_data);
 GInputStream * meta_x11_selection_input_stream_new_finish    (GAsyncResult               *result,
-                                                              const char                **type,
-                                                              int                        *format,
                                                               GError                    **error);
 
 gboolean       meta_x11_selection_input_stream_xevent        (MetaX11SelectionInputStream *stream,

src/x11/meta-x11-selection-input-stream.c
@@ -41,9 +41,7 @@ struct MetaX11SelectionInputStreamPrivate
   Atom xselection;
   Atom xtarget;
   Atom xproperty;
-  const char *type;
   Atom xtype;
-  int format;
 
   GTask *pending_task;
   uint8_t *pending_data;
@@ -342,8 +340,7 @@ static GBytes *
 get_selection_property (MetaX11Display *x11_display,
                         Window          owner,
                         Atom            property,
-                        Atom           *ret_type,
-                        gint           *ret_format)
+                        Atom           *ret_type)
 {
   gulong nitems;
   gulong nbytes;
@@ -386,7 +383,6 @@ get_selection_property (MetaX11Display *x11_display,
         }
 
       *ret_type = prop_type;
-      *ret_format = prop_format;
 
       return g_bytes_new_with_free_func (data,
                                          length,
@@ -399,7 +395,6 @@ err:
     XFree (data);
 
   *ret_type = None;
-  *ret_format = 0;
 
   return NULL;
 }
@@ -414,7 +409,6 @@ meta_x11_selection_input_stream_xevent (MetaX11SelectionInputStream *stream,
   Window xwindow;
   GBytes *bytes;
   Atom type;
-  gint format;
   char *target;
 
   xdisplay = priv->x11_display->xdisplay;
@@ -434,7 +428,7 @@ meta_x11_selection_input_stream_xevent (MetaX11SelectionInputStream *stream,
 
       bytes = get_selection_property (priv->x11_display, xwindow,
                                       xevent->xproperty.atom,
-                                      &type, &format);
+                                      &type);
 
       if (bytes == NULL)
         {
@@ -486,8 +480,7 @@ meta_x11_selection_input_stream_xevent (MetaX11SelectionInputStream *stream,
           {
             bytes = get_selection_property (priv->x11_display, xwindow,
                                             xevent->xselection.property,
-                                            &priv->xtype, &priv->format);
-            priv->type = XGetAtomName (xdisplay, priv->xtype);
+                                            &priv->xtype);
 
             g_task_return_pointer (task, g_object_ref (stream), g_object_unref);
 
@@ -575,12 +568,9 @@ meta_x11_selection_input_stream_new_async (MetaX11Display      *x11_display,
 
 GInputStream *
 meta_x11_selection_input_stream_new_finish (GAsyncResult  *result,
-                                            const char   **type,
-                                            int           *format,
                                             GError       **error)
 {
   MetaX11SelectionInputStream *stream;
-  MetaX11SelectionInputStreamPrivate *priv;
   GTask *task;
 
   g_return_val_if_fail (g_task_is_valid (result, NULL), NULL);
@@ -591,13 +581,6 @@ meta_x11_selection_input_stream_new_finish (GAsyncResult  *result,
   stream = g_task_propagate_pointer (task, error);
   if (!stream)
     return NULL;
-
-  priv = meta_x11_selection_input_stream_get_instance_private (stream);
-
-  if (type)
-    *type = priv->type;
-  if (format)
-    *format = priv->format;
 
   return G_INPUT_STREAM (stream);
 }