GNOME/mutter

commit 9a2c8b2592a417051a262a98ddad7f912fe783bd

window: Add suspend state
The 'suspend state' is meant to track whether a window is likely to be
visible any time soon. The hueristics for this are as follows:

 * If a window is hidden, it will enter the 'hidden' state.
 * If a window is visible, and unobscured, it will enter the 'active'
   state.
 * If a window is visible, but obscured by another window, it will enter
   the 'hidden' state.
 * If there is a mapped clone of a window, it will enter the 'active'
   state.
 * If the window has been in the 'hidden' state for 3 seconds, it will
   enter the 'suspended' state.

This will eventually be communicated to Wayland clients so that they can
change their behaviour to e.g. save power.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3019>

src/compositor/meta-window-actor.c
@@ -54,6 +54,8 @@ typedef struct _MetaWindowActorPrivate
 
   MetaSurfaceActor *surface;
 
+  GPtrArray *surface_actors;
+
   int geometry_scale;
 
   /*
@@ -117,6 +119,11 @@ static MetaSurfaceActor * meta_window_actor_real_get_scanout_candidate (MetaWind
 
 static void meta_window_actor_real_assign_surface_actor (MetaWindowActor  *self,
                                                          MetaSurfaceActor *surface_actor);
+
+static void on_cloned (ClutterActor *actor,
+                       ClutterClone *clone);
+static void on_decloned (ClutterActor *actor,
+                         ClutterClone *clone);
 
 static void screen_cast_window_iface_init (MetaScreenCastWindowInterface *iface);
 
@@ -218,7 +225,13 @@ meta_window_actor_init (MetaWindowActor *self)
   MetaWindowActorPrivate *priv =
     meta_window_actor_get_instance_private (self);
 
+  priv->surface_actors = g_ptr_array_new ();
   priv->geometry_scale = 1;
+
+  g_signal_connect (self, "cloned",
+                    G_CALLBACK (on_cloned), NULL);
+  g_signal_connect (self, "decloned",
+                    G_CALLBACK (on_decloned), NULL);
 }
 
 static void
@@ -357,6 +370,8 @@ meta_window_actor_real_assign_surface_actor (MetaWindowActor  *self,
   g_clear_object (&priv->surface);
   priv->surface = g_object_ref_sink (surface_actor);
 
+  meta_window_actor_add_surface_actor (self, surface_actor);
+
   if (meta_window_actor_is_frozen (self))
     meta_window_actor_set_frozen (self, TRUE);
   else
@@ -369,6 +384,102 @@ meta_window_actor_assign_surface_actor (MetaWindowActor  *self,
 {
   META_WINDOW_ACTOR_GET_CLASS (self)->assign_surface_actor (self,
                                                             surface_actor);
+}
+
+static void
+is_surface_actor_obscured_changed (MetaSurfaceActor *surface_actor,
+                                   GParamSpec       *pspec,
+                                   MetaWindowActor  *window_actor)
+{
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+
+  if (meta_surface_actor_is_obscured (surface_actor))
+    meta_window_uninhibit_suspend_state (priv->window);
+  else
+    meta_window_inhibit_suspend_state (priv->window);
+}
+
+static void
+disconnect_surface_actor_from (MetaSurfaceActor *surface_actor,
+                               MetaWindowActor  *window_actor)
+{
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+
+  g_signal_handlers_disconnect_by_func (surface_actor,
+                                        is_surface_actor_obscured_changed,
+                                        window_actor);
+  if (!meta_surface_actor_is_obscured (surface_actor))
+    meta_window_uninhibit_suspend_state (priv->window);
+}
+
+void
+meta_window_actor_add_surface_actor (MetaWindowActor  *window_actor,
+                                     MetaSurfaceActor *surface_actor)
+{
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+
+  g_signal_connect (surface_actor,
+                    "notify::is-obscured",
+                    G_CALLBACK (is_surface_actor_obscured_changed),
+                    window_actor);
+  if (!meta_surface_actor_is_obscured (surface_actor))
+    meta_window_inhibit_suspend_state (priv->window);
+  g_ptr_array_add (priv->surface_actors, surface_actor);
+}
+
+void
+meta_window_actor_remove_surface_actor (MetaWindowActor  *window_actor,
+                                        MetaSurfaceActor *surface_actor)
+{
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+
+  disconnect_surface_actor_from (surface_actor, window_actor);
+  g_ptr_array_remove (priv->surface_actors, surface_actor);
+}
+
+static void
+on_clone_notify_mapped (ClutterClone    *clone,
+                        GParamSpec      *pspec,
+                        MetaWindowActor *window_actor)
+{
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+
+  if (clutter_actor_is_mapped (CLUTTER_ACTOR (clone)))
+    meta_window_inhibit_suspend_state (priv->window);
+  else
+    meta_window_uninhibit_suspend_state (priv->window);
+}
+
+static void
+on_cloned (ClutterActor *actor,
+           ClutterClone *clone)
+{
+  MetaWindowActor *window_actor = META_WINDOW_ACTOR (actor);
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+
+  g_signal_connect (clone, "notify::mapped",
+                    G_CALLBACK (on_clone_notify_mapped), actor);
+  if (clutter_actor_is_mapped (CLUTTER_ACTOR (clone)))
+    meta_window_inhibit_suspend_state (priv->window);
+}
+
+static void
+on_decloned (ClutterActor *actor,
+             ClutterClone *clone)
+{
+  MetaWindowActor *window_actor = META_WINDOW_ACTOR (actor);
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+
+  g_signal_handlers_disconnect_by_func (clone, on_clone_notify_mapped, actor);
+  if (clutter_actor_is_mapped (CLUTTER_ACTOR (clone)))
+    meta_window_uninhibit_suspend_state (priv->window);
 }
 
 static void
@@ -453,6 +564,10 @@ meta_window_actor_dispose (GObject *object)
 
   priv->disposed = TRUE;
 
+  g_ptr_array_foreach (priv->surface_actors,
+                       (GFunc) disconnect_surface_actor_from,
+                       self);
+  g_clear_pointer (&priv->surface_actors, g_ptr_array_unref);
   g_clear_signal_handler (&priv->stage_views_changed_id, self);
 
   meta_compositor_remove_window_actor (compositor, self);

src/compositor/meta-compositor-view-native.c
@@ -161,7 +161,7 @@ find_scanout_candidate (MetaCompositorView  *compositor_view,
       return FALSE;
     }
 
-  if (meta_surface_actor_is_obscured (surface_actor))
+  if (meta_surface_actor_is_effectively_obscured (surface_actor))
     {
       meta_topic (META_DEBUG_RENDER,
                   "No direct scanout candidate: surface-actor is obscured");

src/compositor/meta-surface-actor.c
@@ -27,6 +27,17 @@
 #include "compositor/region-utils.h"
 #include "meta/meta-shaped-texture.h"
 
+enum
+{
+  PROP_0,
+
+  PROP_IS_OBSCURED,
+
+  N_PROPS
+};
+
+static GParamSpec *obj_props[N_PROPS];
+
 typedef struct _MetaSurfaceActorPrivate
 {
   MetaShapedTexture *texture;
@@ -35,6 +46,7 @@ typedef struct _MetaSurfaceActorPrivate
 
   /* MetaCullable regions, see that documentation for more details */
   cairo_region_t *unobscured_region;
+  gboolean is_obscured;
 
   /* Freeze/thaw accounting */
   cairo_region_t *pending_damage;
@@ -78,6 +90,29 @@ effective_unobscured_region (MetaSurfaceActor *surface_actor)
 }
 
 static void
+update_is_obscured (MetaSurfaceActor *surface_actor)
+{
+  MetaSurfaceActorPrivate *priv =
+    meta_surface_actor_get_instance_private (surface_actor);
+  cairo_region_t *unobscured_region;
+  gboolean is_obscured;
+
+  unobscured_region = priv->unobscured_region;
+
+  if (unobscured_region)
+    is_obscured = cairo_region_is_empty (unobscured_region);
+  else
+    is_obscured = FALSE;
+
+  if (priv->is_obscured == is_obscured)
+    return;
+
+  priv->is_obscured = is_obscured;
+  g_object_notify_by_pspec (G_OBJECT (surface_actor),
+                            obj_props[PROP_IS_OBSCURED]);
+}
+
+static void
 set_unobscured_region (MetaSurfaceActor *surface_actor,
                        cairo_region_t   *unobscured_region)
 {
@@ -109,6 +144,8 @@ set_unobscured_region (MetaSurfaceActor *surface_actor,
           cairo_region_intersect_rectangle (priv->unobscured_region, &bounds);
         }
     }
+
+  update_is_obscured (surface_actor);
 }
 
 static void
@@ -191,6 +228,27 @@ meta_surface_actor_get_paint_volume (ClutterActor       *actor,
 }
 
 static void
+meta_surface_actor_get_property (GObject      *object,
+                                 guint         prop_id,
+                                 GValue       *value,
+                                 GParamSpec   *pspec)
+{
+  MetaSurfaceActor *surface_actor = META_SURFACE_ACTOR (object);
+  MetaSurfaceActorPrivate *priv =
+    meta_surface_actor_get_instance_private (surface_actor);
+
+  switch (prop_id)
+    {
+    case PROP_IS_OBSCURED:
+      g_value_set_boolean (value, priv->is_obscured);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+static void
 meta_surface_actor_dispose (GObject *object)
 {
   MetaSurfaceActor *self = META_SURFACE_ACTOR (object);
@@ -212,8 +270,18 @@ meta_surface_actor_class_init (MetaSurfaceActorClass *klass)
   ClutterActorClass *actor_class = CLUTTER_ACTOR_CLASS (klass);
 
   object_class->dispose = meta_surface_actor_dispose;
+  object_class->get_property = meta_surface_actor_get_property;
+
   actor_class->pick = meta_surface_actor_pick;
   actor_class->get_paint_volume = meta_surface_actor_get_paint_volume;
+
+  obj_props[PROP_IS_OBSCURED] =
+    g_param_spec_boolean ("is-obscured",
+                          "is obscured",
+                          "If the surface actor is fully obscured",
+                          TRUE,
+                          G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+  g_object_class_install_properties (object_class, N_PROPS, obj_props);
 
   signals[REPAINT_SCHEDULED] = g_signal_new ("repaint-scheduled",
                                              G_TYPE_FROM_CLASS (object_class),
@@ -303,6 +371,7 @@ meta_surface_actor_init (MetaSurfaceActor *self)
   MetaSurfaceActorPrivate *priv =
     meta_surface_actor_get_instance_private (self);
 
+  priv->is_obscured = TRUE;
   priv->texture = meta_shaped_texture_new ();
   g_signal_connect_object (priv->texture, "size-changed",
                            G_CALLBACK (texture_size_changed), self, 0);
@@ -381,14 +450,22 @@ meta_surface_actor_update_area (MetaSurfaceActor *self,
 gboolean
 meta_surface_actor_is_obscured (MetaSurfaceActor *self)
 {
-  cairo_region_t *unobscured_region;
+  MetaSurfaceActorPrivate *priv =
+    meta_surface_actor_get_instance_private (self);
 
-  unobscured_region = effective_unobscured_region (self);
+  return priv->is_obscured;
+}
 
-  if (unobscured_region)
-    return cairo_region_is_empty (unobscured_region);
-  else
+gboolean
+meta_surface_actor_is_effectively_obscured (MetaSurfaceActor *surface_actor)
+{
+  MetaSurfaceActorPrivate *priv =
+    meta_surface_actor_get_instance_private (surface_actor);
+
+  if (clutter_actor_has_mapped_clones (CLUTTER_ACTOR (surface_actor)))
     return FALSE;
+  else
+    return priv->is_obscured;
 }
 
 gboolean

src/compositor/meta-surface-actor.h
@@ -35,6 +35,9 @@ void meta_surface_actor_update_area (MetaSurfaceActor *self,
                                      int               height);
 
 gboolean meta_surface_actor_is_obscured (MetaSurfaceActor *self);
+
+gboolean meta_surface_actor_is_effectively_obscured (MetaSurfaceActor *self);
+
 gboolean meta_surface_actor_is_obscured_on_stage_view (MetaSurfaceActor *self,
                                                        ClutterStageView *stage_view,
                                                        float            *unobscurred_fraction);

src/compositor/meta-window-actor-private.h
@@ -111,3 +111,9 @@ gboolean meta_window_actor_is_opaque (MetaWindowActor *self);
 void meta_window_actor_update_regions (MetaWindowActor *self);
 
 gboolean meta_window_actor_can_freeze_commits (MetaWindowActor *self);
+
+void meta_window_actor_add_surface_actor (MetaWindowActor  *window_actor,
+                                          MetaSurfaceActor *surface_actor);
+
+void meta_window_actor_remove_surface_actor (MetaWindowActor  *window_actor,
+                                             MetaSurfaceActor *surface_actor);

src/compositor/meta-window-actor-wayland.c
@@ -199,6 +199,9 @@ set_surface_actor_index (GNode    *node,
   ClutterActor *container = traverse_data->surface_container;
   ClutterActor *surface_actor =
     CLUTTER_ACTOR (meta_wayland_surface_get_actor (surface));
+  MetaSurfaceContainerActorWayland *surface_container =
+    META_SURFACE_CONTAINER_ACTOR_WAYLAND (container);
+  MetaWindowActor *window_actor = surface_container->window_actor;
 
   if (clutter_actor_contains (container, surface_actor))
     {
@@ -212,6 +215,8 @@ set_surface_actor_index (GNode    *node,
     }
   else
     {
+      meta_window_actor_add_surface_actor (window_actor,
+                                           META_SURFACE_ACTOR (surface_actor));
       clutter_actor_insert_child_at_index (container,
                                            surface_actor,
                                            traverse_data->index);
@@ -250,6 +255,9 @@ meta_window_actor_wayland_rebuild_surface_tree (MetaWindowActor *actor)
 
       if (!g_list_find (surface_actors, child_actor))
         {
+          MetaSurfaceActor *surface_actor = META_SURFACE_ACTOR (child_actor);
+
+          meta_window_actor_remove_surface_actor (actor, surface_actor);
           clutter_actor_remove_child (CLUTTER_ACTOR (self->surface_container),
                                       child_actor);
         }

src/compositor/meta-window-actor-x11.c
@@ -355,7 +355,7 @@ meta_window_actor_x11_queue_frame_drawn (MetaWindowActor *actor,
       surface = meta_window_actor_get_surface (META_WINDOW_ACTOR (actor_x11));
 
       if (surface)
-        is_obscured = meta_surface_actor_is_obscured (surface);
+        is_obscured = meta_surface_actor_is_effectively_obscured (surface);
       else
         is_obscured = FALSE;
 

src/core/window-private.h
@@ -119,6 +119,13 @@ typedef enum _MetaWindowUpdateMonitorFlags
   META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE = 1 << 1,
 } MetaWindowUpdateMonitorFlags;
 
+typedef enum _MetaWindowSuspendState
+{
+  META_WINDOW_SUSPEND_STATE_ACTIVE = 1,
+  META_WINDOW_SUSPEND_STATE_HIDDEN,
+  META_WINDOW_SUSPEND_STATE_SUSPENDED,
+} MetaWindowSuspendState;
+
 typedef struct _MetaPlacementRule
 {
   MetaRectangle anchor_rect;
@@ -887,3 +894,9 @@ void meta_window_set_frame_xwindow (MetaWindow *window,
 
 void meta_window_maybe_apply_size_hints (MetaWindow    *window,
                                          MetaRectangle *target_rect);
+
+void meta_window_inhibit_suspend_state (MetaWindow *window);
+
+void meta_window_uninhibit_suspend_state (MetaWindow *window);
+
+gboolean meta_window_is_suspended (MetaWindow *window);

commit 87b7b073d3811c4bc51d0f100a8e1e81aa1be0a5

window-actor: Don't update suspend state if no window
If the window is gone, don't try to update it. This might happen on
unmap animations with active clones.

Fixes: 9a2c8b2592 ("window: Add suspend state")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/2984
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3213>

src/compositor/meta-window-actor.c
@@ -478,7 +478,8 @@ on_decloned (ClutterActor *actor,
     meta_window_actor_get_instance_private (window_actor);
 
   g_signal_handlers_disconnect_by_func (clone, on_clone_notify_mapped, actor);
-  if (clutter_actor_is_mapped (CLUTTER_ACTOR (clone)))
+  if (clutter_actor_is_mapped (CLUTTER_ACTOR (clone)) &&
+      priv->window)
     meta_window_uninhibit_suspend_state (priv->window);
 }
 