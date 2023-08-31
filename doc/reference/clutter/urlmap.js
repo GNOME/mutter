a7231ab2bea02eba1cc186428d467e3e1713d9f8
Bilal Elmoussaoui <belmouss@redhat.com>
2023-08-31 10:11:57 +0200

    docs: Update url maps
    To get working items links
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3227>

--- doc/reference/clutter/urlmap.js
+++ doc/reference/clutter/urlmap.js
@@ -7,9 +7,6 @@ baseURLs = [
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
     [ 'Gio', 'https://docs.gtk.org/gio/' ],
     [ 'Atk', 'https://docs.gtk.org/atk/' ],
-    [ 'Gdk', 'https://docs.gtk.org/gdk3/' ],
-    [ 'Gtk', 'https://docs.gtk.org/gtk3/' ],
-    [ 'Pango', 'https://docs.gtk.org/Pango/' ],
-    [ 'PangoCairo', 'https://docs.gtk.org/PangoCairo/' ],
-    [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
+    [ 'Mtk', 'https://gnome.pages.gitlab.gnome.org/mutter/mtk/' ],
+    [ 'Cogl', 'https://gnome.pages.gitlab.gnome.org/mutter/cogl/' ],
 ]
