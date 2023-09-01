GNOME/mutter

commit a3c62bf8aa7a383eda42aaa3a6ee63704d405bed

wayland/idle-inhibit: Add state tracking to fix races
This changes how state is tracked by introducing an explicit state. We
need this since we use asynchronous calls to the out of process
component that handles actual inhibitation, including idleness.

This means if inhibitations changes rapidly, we might end up with an
incorrect state if we e.g. try to uninhibit while we're currently trying
to inhibit.

This is done by adding a state variable that accounts for the pending
state, as well as the active state, with a function that looks at the
current conditions to derive what state we should be in, and what state
we are in, to decide what the next action should be.

For example, if we're trying to inhibit, but now wants to uninhibit,
we'll wait for the inhibit call to complete, recheck what we want, which
would result in an async uninhibit call being made.

Fixes: 388b534062 ("wayland: Implement idle inhibit protocol")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3219>

src/wayland/meta-wayland-idle-inhibit.c
@@ -30,18 +30,49 @@
 
 #include "idle-inhibit-unstable-v1-server-protocol.h"
 
+typedef enum _IdleState
+{
+  IDLE_STATE_UNINHIBITED,
+  IDLE_STATE_INHIBITING,
+  IDLE_STATE_INHIBITED,
+  IDLE_STATE_UNINHIBITING,
+} IdleState;
+
 struct _MetaWaylandIdleInhibitor
 {
-  MetaWaylandSurface *surface;
   GDBusProxy *session_proxy;
-  uint32_t cookie;
+  struct wl_resource *resource;
+
+  MetaSurfaceActor *actor;
   gulong is_obscured_changed_handler;
-  gboolean idle_inhibited;
+  gulong actor_destroyed_handler_id;
+
+  MetaWaylandSurface *surface;
   gulong surface_destroy_handler_id;
-  GCancellable *cancellable;
+  gulong actor_changed_handler_id;
+
+  uint32_t cookie;
+  IdleState state;
 };
 
 typedef struct _MetaWaylandIdleInhibitor MetaWaylandIdleInhibitor;
+
+static void update_inhibitation (MetaWaylandIdleInhibitor *inhibitor);
+
+static void
+meta_wayland_inhibitor_free (MetaWaylandIdleInhibitor *inhibitor)
+{
+  g_clear_signal_handler (&inhibitor->is_obscured_changed_handler,
+                          inhibitor->actor);
+  g_clear_signal_handler (&inhibitor->actor_destroyed_handler_id,
+                          inhibitor->actor);
+  g_clear_signal_handler (&inhibitor->actor_changed_handler_id,
+                          inhibitor->surface);
+  g_clear_signal_handler (&inhibitor->surface_destroy_handler_id,
+                          inhibitor->surface);
+
+  g_free (inhibitor);
+}
 
 static void
 inhibit_completed (GObject      *source,
@@ -60,10 +91,13 @@ inhibit_completed (GObject      *source,
       return;
     }
 
-  g_variant_get (ret, "(u)", &inhibitor->cookie);
-  inhibitor->idle_inhibited = TRUE;
-}
+  g_warn_if_fail (inhibitor->state == IDLE_STATE_INHIBITING);
 
+  g_variant_get (ret, "(u)", &inhibitor->cookie);
+  inhibitor->state = IDLE_STATE_INHIBITED;
+
+  update_inhibitation (inhibitor);
+}
 
 static void
 uninhibit_completed (GObject      *source,
@@ -81,47 +115,86 @@ uninhibit_completed (GObject      *source,
         g_warning ("Failed to uninhibit: %s", error->message);
       return;
     }
-  if (inhibitor)
-    inhibitor->idle_inhibited = FALSE;
+
+  if (!inhibitor)
+    return;
+
+  g_warn_if_fail (inhibitor->state == IDLE_STATE_UNINHIBITING);
+  inhibitor->state = IDLE_STATE_UNINHIBITED;
+
+  update_inhibitation (inhibitor);
 }
 
 static void
 update_inhibitation (MetaWaylandIdleInhibitor *inhibitor)
 {
-  MetaSurfaceActor *actor;
+  gboolean should_inhibit;
 
   if (!inhibitor->session_proxy)
     return;
 
-  if (inhibitor->surface)
-    return;
-
-  actor = meta_wayland_surface_get_actor (inhibitor->surface);
-
-  if (!meta_surface_actor_is_effectively_obscured (actor))
+  if (!inhibitor->surface ||
+      !inhibitor->resource)
     {
-      if (!inhibitor->idle_inhibited)
-        {
-          g_dbus_proxy_call (G_DBUS_PROXY (inhibitor->session_proxy),
-                             "Inhibit",
-                             g_variant_new ("(ss)", "mutter", "idle-inhibit"),
-                             G_DBUS_CALL_FLAGS_NONE,
-                             -1,
-                             inhibitor->cancellable,
-                             inhibit_completed,
-                             inhibitor);
-        }
+      should_inhibit = FALSE;
     }
-  else if (inhibitor->idle_inhibited)
+  else
+    {
+      MetaSurfaceActor *actor;
+
+      actor = meta_wayland_surface_get_actor (inhibitor->surface);
+      if (meta_surface_actor_is_effectively_obscured (actor))
+        should_inhibit = FALSE;
+      else
+        should_inhibit = TRUE;
+    }
+
+  switch (inhibitor->state)
+    {
+    case IDLE_STATE_UNINHIBITED:
+      if (!inhibitor->resource)
+        {
+          meta_wayland_inhibitor_free (inhibitor);
+          return;
+        }
+
+      if (!should_inhibit)
+        return;
+
+      break;
+    case IDLE_STATE_INHIBITED:
+      if (should_inhibit)
+        return;
+      break;
+    case IDLE_STATE_INHIBITING:
+    case IDLE_STATE_UNINHIBITING:
+      /* Update inhibitation after current asynchronous call completes. */
+      return;
+    }
+
+  if (should_inhibit)
+    {
+      g_dbus_proxy_call (G_DBUS_PROXY (inhibitor->session_proxy),
+                         "Inhibit",
+                         g_variant_new ("(ss)", "mutter", "idle-inhibit"),
+                         G_DBUS_CALL_FLAGS_NONE,
+                         -1,
+                         NULL,
+                         inhibit_completed,
+                         inhibitor);
+      inhibitor->state = IDLE_STATE_INHIBITING;
+    }
+  else
     {
       g_dbus_proxy_call (G_DBUS_PROXY (inhibitor->session_proxy),
                          "UnInhibit",
                          g_variant_new ("(u)", inhibitor->cookie),
                          G_DBUS_CALL_FLAGS_NONE,
                          -1,
-                         inhibitor->cancellable,
+                         NULL,
                          uninhibit_completed,
                          inhibitor);
+      inhibitor->state = IDLE_STATE_UNINHIBITING;
     }
 }
 
@@ -170,39 +243,69 @@ idle_inhibitor_destructor (struct wl_resource *resource)
 {
   MetaWaylandIdleInhibitor *inhibitor = wl_resource_get_user_data (resource);
 
-  if (inhibitor->surface)
-    g_clear_signal_handler (&inhibitor->is_obscured_changed_handler,
-                            meta_wayland_surface_get_actor (inhibitor->surface));
-
-  /* Cancel any already pending calls */
-  g_cancellable_cancel (inhibitor->cancellable);
-  /* Uninhibit when the inhibitor is destroyed */
-  if (inhibitor->session_proxy && inhibitor->idle_inhibited)
+  switch (inhibitor->state)
     {
-      g_dbus_proxy_call (G_DBUS_PROXY (inhibitor->session_proxy),
-                         "UnInhibit",
-                         g_variant_new ("(u)", inhibitor->cookie),
-                         G_DBUS_CALL_FLAGS_NONE,
-                         -1,
-                         NULL,
-                         uninhibit_completed,
-                         NULL);
+    case IDLE_STATE_UNINHIBITED:
+      meta_wayland_inhibitor_free (inhibitor);
+      return;
+    case IDLE_STATE_INHIBITED:
+    case IDLE_STATE_INHIBITING:
+    case IDLE_STATE_UNINHIBITING:
+      inhibitor->resource = NULL;
+      break;
     }
 
-  if (inhibitor->surface)
-    {
-      g_clear_signal_handler (&inhibitor->surface_destroy_handler_id,
-                              inhibitor->surface);
-    }
-
-  g_free (inhibitor);
+  update_inhibitation (inhibitor);
 }
 
 static void
 on_surface_destroyed (MetaWaylandSurface       *surface,
                       MetaWaylandIdleInhibitor *inhibitor)
 {
+  g_clear_signal_handler (&inhibitor->is_obscured_changed_handler,
+                          inhibitor->actor);
+  g_clear_signal_handler (&inhibitor->actor_destroyed_handler_id,
+                          inhibitor->actor);
+  inhibitor->actor = NULL;
+  g_clear_signal_handler (&inhibitor->actor_changed_handler_id,
+                          inhibitor->surface);
+  g_clear_signal_handler (&inhibitor->surface_destroy_handler_id,
+                          inhibitor->surface);
   inhibitor->surface = NULL;
+}
+
+static void
+on_actor_destroyed (MetaSurfaceActor         *actor,
+                    MetaWaylandIdleInhibitor *inhibitor)
+{
+  g_warn_if_fail (actor == inhibitor->actor);
+
+  g_clear_signal_handler (&inhibitor->is_obscured_changed_handler, actor);
+  g_clear_signal_handler (&inhibitor->actor_destroyed_handler_id, actor);
+  inhibitor->actor = NULL;
+}
+
+static void
+attach_actor (MetaWaylandIdleInhibitor *inhibitor)
+{
+  inhibitor->actor = meta_wayland_surface_get_actor (inhibitor->surface);
+  inhibitor->is_obscured_changed_handler =
+    g_signal_connect (inhibitor->actor, "notify::is-obscured",
+                      G_CALLBACK (is_obscured_changed), inhibitor);
+  inhibitor->actor_destroyed_handler_id =
+    g_signal_connect (inhibitor->actor, "destroy",
+                      G_CALLBACK (on_actor_destroyed), inhibitor);
+}
+
+static void
+on_actor_changed (MetaWaylandSurface       *surface,
+                  MetaWaylandIdleInhibitor *inhibitor)
+{
+  g_clear_signal_handler (&inhibitor->is_obscured_changed_handler,
+                          inhibitor->surface);
+  g_clear_signal_handler (&inhibitor->actor_destroyed_handler_id,
+                          inhibitor->surface);
+  attach_actor (inhibitor);
 }
 
 static const struct zwp_idle_inhibitor_v1_interface meta_wayland_idle_inhibitor_interface =
@@ -227,12 +330,16 @@ idle_inhibit_manager_create_inhibitor (struct wl_client   *client,
 
   inhibitor = g_new0 (MetaWaylandIdleInhibitor, 1);
   inhibitor->surface = surface;
+  inhibitor->resource = inhibitor_resource;
 
-  inhibitor->is_obscured_changed_handler =
-    g_signal_connect (meta_wayland_surface_get_actor (surface),
-                      "notify::is-obscured",
-                      G_CALLBACK (is_obscured_changed),
-                      inhibitor);
+  attach_actor (inhibitor);
+
+  inhibitor->actor_changed_handler_id =
+    g_signal_connect (surface, "actor-changed",
+                      G_CALLBACK (on_actor_changed), inhibitor);
+  inhibitor->surface_destroy_handler_id =
+    g_signal_connect (surface, "destroy",
+                      G_CALLBACK (on_surface_destroyed), inhibitor);
 
   g_dbus_proxy_new_for_bus (G_BUS_TYPE_SESSION,
                             G_DBUS_PROXY_FLAGS_NONE,
@@ -240,7 +347,7 @@ idle_inhibit_manager_create_inhibitor (struct wl_client   *client,
                             "org.freedesktop.ScreenSaver",
                             "/org/freedesktop/ScreenSaver",
                             "org.freedesktop.ScreenSaver",
-                            inhibitor->cancellable,
+                            NULL,
                             inhibitor_proxy_completed,
                             inhibitor);
 
@@ -248,11 +355,6 @@ idle_inhibit_manager_create_inhibitor (struct wl_client   *client,
                                   &meta_wayland_idle_inhibitor_interface,
                                   inhibitor,
                                   idle_inhibitor_destructor);
-
-  inhibitor->surface_destroy_handler_id =
-    g_signal_connect (surface, "destroy",
-                      G_CALLBACK (on_surface_destroyed),
-                      inhibitor);
 }
 
 

commit 88fef1d02133e596951e7de438565175b55bdc15

wayland/idle-inhibit: Handle immediate inhibitor destruction
If the inhibitor object was destroyed immediately, the proxy
construction completing would try to update the inhibitation state, but
this didn't work since it was already freed. Fix this by adding an
'initializing' state that keeps track of this.

Fixes: a3c62bf8aa ("wayland/idle-inhibit: Add state tracking to fix races")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/2998
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3234>

src/wayland/meta-wayland-idle-inhibit.c
@@ -32,6 +32,7 @@
 
 typedef enum _IdleState
 {
+  IDLE_STATE_INITIALIZING,
   IDLE_STATE_UNINHIBITED,
   IDLE_STATE_INHIBITING,
   IDLE_STATE_INHIBITED,
@@ -151,6 +152,7 @@ update_inhibitation (MetaWaylandIdleInhibitor *inhibitor)
 
   switch (inhibitor->state)
     {
+    case IDLE_STATE_INITIALIZING:
     case IDLE_STATE_UNINHIBITED:
       if (!inhibitor->resource)
         {
@@ -227,6 +229,7 @@ inhibitor_proxy_completed (GObject      *source,
     }
 
   inhibitor->session_proxy = proxy;
+  inhibitor->state = IDLE_STATE_UNINHIBITED;
 
   update_inhibitation (inhibitor);
 }
@@ -248,6 +251,7 @@ idle_inhibitor_destructor (struct wl_resource *resource)
     case IDLE_STATE_UNINHIBITED:
       meta_wayland_inhibitor_free (inhibitor);
       return;
+    case IDLE_STATE_INITIALIZING:
     case IDLE_STATE_INHIBITED:
     case IDLE_STATE_INHIBITING:
     case IDLE_STATE_UNINHIBITING:

src/tests/meson.build
@@ -563,6 +563,7 @@ if have_native_tests
       'depends': [
         test_client,
         test_client_executables.get('buffer-transform'),
+        test_client_executables.get('idle-inhibit'),
         test_client_executables.get('invalid-subsurfaces'),
         test_client_executables.get('invalid-xdg-shell-actions'),
         test_client_executables.get('single-pixel-buffer'),

src/tests/wayland-test-clients/idle-inhibit.c
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2023 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <glib.h>
+#include <wayland-client.h>
+
+#include "wayland-test-client-utils.h"
+
+#include "idle-inhibit-unstable-v1-client-protocol.h"
+
+static WaylandDisplay *display;
+struct zwp_idle_inhibit_manager_v1 *idle_inhibit_manager;
+
+static void
+handle_registry_global (void               *user_data,
+                        struct wl_registry *registry,
+                        uint32_t            id,
+                        const char         *interface,
+                        uint32_t            version)
+{
+  if (strcmp (interface, "zwp_idle_inhibit_manager_v1") == 0)
+    {
+      idle_inhibit_manager = wl_registry_bind (registry, id,
+                                               &zwp_idle_inhibit_manager_v1_interface,
+                                               1);
+    }
+}
+
+static void
+handle_registry_global_remove (void               *user_data,
+                               struct wl_registry *registry,
+                               uint32_t            name)
+{
+}
+
+static const struct wl_registry_listener registry_listener = {
+  handle_registry_global,
+  handle_registry_global_remove
+};
+
+int
+main (int    argc,
+      char **argv)
+{
+  struct wl_registry *registry;
+  WaylandSurface *surface;
+  struct zwp_idle_inhibitor_v1 *inhibitor;
+
+  display = wayland_display_new (WAYLAND_DISPLAY_CAPABILITY_TEST_DRIVER);
+  registry = wl_display_get_registry (display->display);
+  wl_registry_add_listener (registry, &registry_listener, NULL);
+  wl_display_roundtrip (display->display);
+
+  surface = wayland_surface_new (display,
+                                 "idle-inhibit-client",
+                                 20, 20, 0x11223344);
+
+  inhibitor =
+    zwp_idle_inhibit_manager_v1_create_inhibitor (idle_inhibit_manager,
+                                                  surface->wl_surface);
+  zwp_idle_inhibitor_v1_destroy (inhibitor);
+
+  wl_display_roundtrip (display->display);
+
+  return EXIT_SUCCESS;
+}

src/tests/wayland-test-clients/meson.build
@@ -55,6 +55,9 @@ wayland_test_clients = [
     'name': 'fullscreen',
   },
   {
+    'name': 'idle-inhibit',
+  },
+  {
     'name': 'kms-cursor-hotplug-helper',
     'extra_deps': [
       wayland_cursor_dep,

src/tests/wayland-unit-tests.c
@@ -839,6 +839,32 @@ wayland_registry_filter (void)
   g_assert_false (client3_saw_global);
 }
 
+static gboolean
+set_true (gpointer user_data)
+{
+  gboolean *done = user_data;
+
+  *done = TRUE;
+
+  return G_SOURCE_REMOVE;
+}
+
+static void
+wayland_idle_inhibit_instant_destroy (void)
+{
+  MetaWaylandTestClient *wayland_test_client;
+  gboolean done;
+
+  wayland_test_client =
+    meta_wayland_test_client_new (test_context, "idle-inhibit");
+  meta_wayland_test_client_finish (wayland_test_client);
+
+  done = FALSE;
+  g_timeout_add_seconds (1, set_true, &done);
+  while (!done)
+    g_main_context_iteration (NULL, TRUE);
+}
+
 static void
 on_before_tests (void)
 {
@@ -886,6 +912,8 @@ init_tests (void)
                    xdg_foreign_set_parent_of);
   g_test_add_func ("/wayland/registry/filter",
                    wayland_registry_filter);
+  g_test_add_func ("/wayland/idle-inhibit/instant-destroy",
+                   wayland_idle_inhibit_instant_destroy);
 }
 
 int