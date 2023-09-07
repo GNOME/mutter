GNOME/mutter

commit a8c62251f8a7ab94182362da0fd18114d50562af

clutter: Port stage crossing events to new constructors
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3153>

clutter/clutter/clutter-stage.c
@@ -3162,35 +3162,6 @@ find_common_root_actor (ClutterStage *stage,
   return CLUTTER_ACTOR (stage);
 }
 
-static ClutterEvent *
-create_crossing_event (ClutterStage         *stage,
-                       ClutterInputDevice   *device,
-                       ClutterEventSequence *sequence,
-                       ClutterInputDevice   *source_device,
-                       ClutterEventType      event_type,
-                       ClutterEventFlags     flags,
-                       ClutterActor         *source,
-                       ClutterActor         *related,
-                       graphene_point_t      coords,
-                       uint32_t              time_ms)
-{
-  ClutterEvent *event;
-
-  event = clutter_event_new (event_type);
-  event->crossing.time = time_ms;
-  event->crossing.flags = flags;
-  event->crossing.stage = stage;
-  event->crossing.x = coords.x;
-  event->crossing.y = coords.y;
-  event->crossing.source = source;
-  event->crossing.related = related;
-  event->crossing.sequence = sequence;
-  clutter_event_set_device (event, device);
-  clutter_event_set_source_device (event, source_device);
-
-  return event;
-}
-
 static inline void
 add_actor_to_event_emission_chain (GArray            *chain,
                                    ClutterActor      *actor,
@@ -3379,16 +3350,14 @@ sync_crossings_on_implicit_grab_end (ClutterStage       *self,
       topmost = parent;
     }
 
-  crossing = create_crossing_event (self,
-                                    entry->device,
-                                    entry->sequence,
-                                    NULL,
-                                    CLUTTER_ENTER,
-                                    CLUTTER_EVENT_FLAG_GRAB_NOTIFY,
-                                    entry->current_actor,
-                                    NULL,
-                                    entry->coords,
-                                    CLUTTER_CURRENT_TIME);
+  crossing = clutter_event_crossing_new (CLUTTER_ENTER,
+                                         CLUTTER_EVENT_FLAG_GRAB_NOTIFY,
+                                         CLUTTER_CURRENT_TIME,
+                                         entry->device,
+                                         entry->sequence,
+                                         entry->coords,
+                                         entry->current_actor,
+                                         NULL);
 
   if (!_clutter_event_process_filters (crossing, deepmost))
     {
@@ -3465,13 +3434,14 @@ clutter_stage_update_device (ClutterStage         *stage,
        */
       if (old_actor && emit_crossing)
         {
-          event = create_crossing_event (stage,
-                                         device, sequence,
-                                         source_device,
-                                         CLUTTER_LEAVE,
-                                         CLUTTER_EVENT_NONE,
-                                         old_actor, new_actor,
-                                         point, time_ms);
+          event = clutter_event_crossing_new (CLUTTER_LEAVE,
+                                              CLUTTER_EVENT_NONE,
+                                              ms2us (time_ms),
+                                              device,
+                                              sequence,
+                                              point,
+                                              old_actor,
+                                              new_actor);
           if (!_clutter_event_process_filters (event, old_actor))
             {
               clutter_stage_emit_crossing_event (stage,
@@ -3485,13 +3455,14 @@ clutter_stage_update_device (ClutterStage         *stage,
 
       if (new_actor && emit_crossing)
         {
-          event = create_crossing_event (stage,
-                                         device, sequence,
-                                         source_device,
-                                         CLUTTER_ENTER,
-                                         CLUTTER_EVENT_NONE,
-                                         new_actor, old_actor,
-                                         point, time_ms);
+          event = clutter_event_crossing_new (CLUTTER_ENTER,
+                                              CLUTTER_EVENT_NONE,
+                                              ms2us (time_ms),
+                                              device,
+                                              sequence,
+                                              point,
+                                              new_actor,
+                                              old_actor);
           if (!_clutter_event_process_filters (event, new_actor))
             {
               clutter_stage_emit_crossing_event (stage,
@@ -3751,17 +3722,15 @@ clutter_stage_notify_grab_on_pointer_entry (ClutterStage       *stage,
       if (entry->implicit_grab_actor)
         deepmost = find_common_root_actor (stage, entry->implicit_grab_actor, deepmost);
 
-      event = create_crossing_event (stage,
-                                     entry->device,
-                                     entry->sequence,
-                                     NULL,
-                                     event_type,
-                                     CLUTTER_EVENT_FLAG_GRAB_NOTIFY,
-                                     entry->current_actor,
-                                     event_type == CLUTTER_LEAVE ?
-                                     grab_actor : old_grab_actor,
-                                     entry->coords,
-                                     CLUTTER_CURRENT_TIME);
+      event = clutter_event_crossing_new (event_type,
+                                          CLUTTER_EVENT_FLAG_GRAB_NOTIFY,
+                                          CLUTTER_CURRENT_TIME,
+                                          entry->device,
+                                          entry->sequence,
+                                          entry->coords,
+                                          entry->current_actor,
+                                          event_type == CLUTTER_LEAVE ?
+                                          grab_actor : old_grab_actor);
       if (!_clutter_event_process_filters (event, entry->current_actor))
         {
           clutter_stage_emit_crossing_event (stage,

commit 89e11e8335ffc738dc1fe7e1c7f823bd58706dc8

clutter: Fix source device in crossing ClutterEvents
This used to be the HW device that triggered the crossing (i.e.
the mouse moving the pointer, etc), or the logical device if the
crossing event happened through other means than input device
events, e.g. relayouts.

The move to ClutterEvent constructors went a bit too far in
the simplifications and broke these expectations for input-generated
crossing events.

Make this event constructor behave like the other events: receive
a source device, and figure out the corresponding logical device from
there. Also pass the source device as it'd be expected, in the
input-induced crossing event generation paths.

Fixes: a8c62251f8a ("clutter: Port stage crossing events to new constructors")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/2981
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3256>

clutter/clutter/clutter-event.c
@@ -2047,17 +2047,31 @@ ClutterEvent *
 clutter_event_crossing_new (ClutterEventType      type,
                             ClutterEventFlags     flags,
                             int64_t               timestamp_us,
-                            ClutterInputDevice   *device,
+                            ClutterInputDevice   *source_device,
                             ClutterEventSequence *sequence,
                             graphene_point_t      coords,
                             ClutterActor         *source,
                             ClutterActor         *related)
 {
+  ClutterInputDevice *device;
   ClutterEvent *event;
 
   g_return_val_if_fail (type == CLUTTER_ENTER ||
                         type == CLUTTER_LEAVE, NULL);
-  g_return_val_if_fail (CLUTTER_IS_INPUT_DEVICE (device), NULL);
+  g_return_val_if_fail (CLUTTER_IS_INPUT_DEVICE (source_device), NULL);
+
+  if (!!(clutter_input_device_get_capabilities (source_device) &
+         CLUTTER_INPUT_CAPABILITY_TABLET_TOOL))
+    {
+      device = source_device;
+    }
+  else
+    {
+      ClutterSeat *seat;
+
+      seat = clutter_input_device_get_seat (source_device);
+      device = clutter_seat_get_pointer (seat);
+    }
 
   event = clutter_event_new (type);
 
@@ -2069,6 +2083,7 @@ clutter_event_crossing_new (ClutterEventType      type,
   event->crossing.source = source;
   event->crossing.related = related;
   g_set_object (&event->crossing.device, device);
+  g_set_object (&event->crossing.source_device, source_device);
 
   return event;
 }

clutter/clutter/clutter-stage.c
@@ -3476,6 +3476,9 @@ clutter_stage_update_device (ClutterStage         *stage,
   old_actor = clutter_stage_get_device_actor (stage, device, sequence);
   device_actor_changed = new_actor != old_actor;
 
+  if (!source_device)
+    source_device = device;
+
   clutter_stage_update_device_entry (stage,
                                      device, sequence,
                                      point,
@@ -3519,7 +3522,7 @@ clutter_stage_update_device (ClutterStage         *stage,
           event = clutter_event_crossing_new (CLUTTER_LEAVE,
                                               CLUTTER_EVENT_NONE,
                                               ms2us (time_ms),
-                                              device,
+                                              source_device,
                                               sequence,
                                               point,
                                               old_actor,
@@ -3540,7 +3543,7 @@ clutter_stage_update_device (ClutterStage         *stage,
           event = clutter_event_crossing_new (CLUTTER_ENTER,
                                               CLUTTER_EVENT_NONE,
                                               ms2us (time_ms),
-                                              device,
+                                              source_device,
                                               sequence,
                                               point,
                                               new_actor,