GNOME/mutter

commit ed068200358463389164df4e0fabc7507254cf9c

kms/impl-device/atomic: Fix stack overflow
Intel has started to advertise big gamma LUT sizes on some hardware
because the hardware supports segmented LUTs. This means they have a lot
more precision at certain segments then others. The uAPI can't expose
this functionality meaningfully so they chose to expose a huge number of
TAPs to sample from to their segmented LUT.

This increase in uAPI LUT size resulted in stack overflows because we
allocated the LUT on the stack. This commit moves it to the heap
instead.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3064
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3322>

src/backends/native/meta-kms-impl-device-atomic.c
@@ -675,9 +675,11 @@ process_crtc_color_updates (MetaKmsImplDevice  *impl_device,
   if (color_update->gamma.has_update)
     {
       MetaGammaLut *gamma = color_update->gamma.state;
-      struct drm_color_lut drm_color_lut[gamma->size];
+      g_autofree struct drm_color_lut *drm_color_lut = NULL;
       int i;
       uint32_t color_lut_blob_id;
+
+      drm_color_lut = g_new (struct drm_color_lut, gamma->size);
 
       for (i = 0; i < gamma->size; i++)
         {

commit 3d318e18b527244d3c8ff62455fa6f95be1e3d2b

kms/impl-device/atomic: Fix blob size
Fixes: ed0682003 ("kms/impl-device/atomic: Fix stack overflow")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3323>

src/backends/native/meta-kms-impl-device-atomic.c
@@ -676,10 +676,12 @@ process_crtc_color_updates (MetaKmsImplDevice  *impl_device,
     {
       MetaGammaLut *gamma = color_update->gamma.state;
       g_autofree struct drm_color_lut *drm_color_lut = NULL;
+      size_t color_lut_size;
       int i;
       uint32_t color_lut_blob_id;
 
-      drm_color_lut = g_new (struct drm_color_lut, gamma->size);
+      color_lut_size = sizeof (struct drm_color_lut) * gamma->size;
+      drm_color_lut = g_malloc (color_lut_size);
 
       for (i = 0; i < gamma->size; i++)
         {
@@ -691,7 +693,7 @@ process_crtc_color_updates (MetaKmsImplDevice  *impl_device,
       color_lut_blob_id = store_new_blob (impl_device,
                                           blob_ids,
                                           drm_color_lut,
-                                          sizeof drm_color_lut,
+                                          color_lut_size,
                                           error);
       if (!color_lut_blob_id)
         return FALSE;