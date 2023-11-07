GNOME/mutter

commit 9b9e12edb25d14ac6194744464b1d5c217c84485

cogl: Port Node/Pipeline/PipelineLayer away from CoglObject
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3193>

cogl/cogl/driver/gl/cogl-pipeline-fragend-glsl.c
@@ -101,7 +101,7 @@ typedef struct
   CoglPipelineCacheEntry *cache_entry;
 } CoglPipelineFragendShaderState;
 
-static CoglUserDataKey shader_state_key;
+static GQuark shader_state_key = 0;
 
 static void
 ensure_layer_generated (CoglPipeline *pipeline,
@@ -120,23 +120,41 @@ shader_state_new (int n_layers,
 
   return shader_state;
 }
+typedef struct
+{
+  CoglPipelineFragendShaderState *shader_state;
+  CoglPipeline *instance;
+} CoglPipelineFragendShaderStateCache;
+
+static GQuark
+get_cache_key (void)
+{
+  if (G_UNLIKELY (shader_state_key == 0))
+    shader_state_key = g_quark_from_static_string ("shader-state-key");
+
+  return shader_state_key;
+}
 
 static CoglPipelineFragendShaderState *
 get_shader_state (CoglPipeline *pipeline)
 {
-  return cogl_object_get_user_data (COGL_OBJECT (pipeline), &shader_state_key);
+  CoglPipelineFragendShaderStateCache *cache;
+  cache = g_object_get_qdata (G_OBJECT (pipeline), get_cache_key ());
+  if (cache)
+    return cache->shader_state;
+  return NULL;
 }
 
 static void
-destroy_shader_state (void *user_data,
-                      void *instance)
+destroy_shader_state (void *user_data)
 {
-  CoglPipelineFragendShaderState *shader_state = user_data;
+  CoglPipelineFragendShaderStateCache *cache = user_data;
+  CoglPipelineFragendShaderState *shader_state = cache->shader_state;
 
   _COGL_GET_CONTEXT (ctx, NO_RETVAL);
 
   if (shader_state->cache_entry &&
-      shader_state->cache_entry->pipeline != instance)
+      shader_state->cache_entry->pipeline != cache->instance)
     shader_state->cache_entry->usage_count--;
 
   if (--shader_state->ref_count == 0)
@@ -147,6 +165,7 @@ destroy_shader_state (void *user_data,
       g_free (shader_state->unit_state);
 
       g_free (shader_state);
+      g_free (cache);
     }
 }
 
@@ -163,20 +182,23 @@ set_shader_state (CoglPipeline *pipeline, CoglPipelineFragendShaderState *shader
           shader_state->cache_entry->pipeline != pipeline)
         shader_state->cache_entry->usage_count++;
     }
+  CoglPipelineFragendShaderStateCache *cache = g_new0 (CoglPipelineFragendShaderStateCache, 1);
+  cache->instance = pipeline;
+  cache->shader_state = shader_state;
 
-  _cogl_object_set_user_data (COGL_OBJECT (pipeline),
-                              &shader_state_key,
-                              shader_state,
-                              destroy_shader_state);
+  g_object_set_qdata_full (G_OBJECT (pipeline),
+                           get_cache_key (),
+                           cache,
+                           destroy_shader_state);
 }
 
 static void
 dirty_shader_state (CoglPipeline *pipeline)
 {
-  cogl_object_set_user_data (COGL_OBJECT (pipeline),
-                             &shader_state_key,
-                             NULL,
-                             NULL);
+  g_object_set_qdata_full (G_OBJECT (pipeline),
+                           get_cache_key (),
+                           NULL,
+                           NULL);
 }
 
 GLuint

cogl/cogl/driver/gl/cogl-pipeline-progend-glsl.c
@@ -140,12 +140,31 @@ typedef struct
   CoglPipelineCacheEntry *cache_entry;
 } CoglPipelineProgramState;
 
-static CoglUserDataKey program_state_key;
+static GQuark program_state_key = 0;
+
+typedef struct
+{
+  CoglPipelineProgramState *program_state;
+  CoglPipeline *instance;
+} CoglPipelineProgramStateCache;
+
+static GQuark
+get_cache_key (void)
+{
+  if (G_UNLIKELY (program_state_key == 0))
+    program_state_key = g_quark_from_static_string ("program-state-progend-key");
+
+  return program_state_key;
+}
 
 static CoglPipelineProgramState *
 get_program_state (CoglPipeline *pipeline)
 {
-  return cogl_object_get_user_data (COGL_OBJECT (pipeline), &program_state_key);
+  CoglPipelineProgramStateCache *cache;
+  cache = g_object_get_qdata (G_OBJECT (pipeline), get_cache_key ());
+  if (cache)
+    return cache->program_state;
+  return NULL;
 }
 
 #define UNIFORM_LOCATION_UNKNOWN -2
@@ -246,10 +265,10 @@ program_state_new (int n_layers,
 }
 
 static void
-destroy_program_state (void *user_data,
-                       void *instance)
+destroy_program_state (void *user_data)
 {
-  CoglPipelineProgramState *program_state = user_data;
+  CoglPipelineProgramStateCache *cache = user_data;
+  CoglPipelineProgramState *program_state = cache->program_state;
 
   _COGL_GET_CONTEXT (ctx, NO_RETVAL);
 
@@ -257,11 +276,11 @@ destroy_program_state (void *user_data,
      it so that if same address gets used again for a new pipeline
      then we won't think it's the same pipeline and avoid updating the
      uniforms */
-  if (program_state->last_used_for_pipeline == instance)
+  if (program_state->last_used_for_pipeline == cache->instance)
     program_state->last_used_for_pipeline = NULL;
 
   if (program_state->cache_entry &&
-      program_state->cache_entry->pipeline != instance)
+      program_state->cache_entry->pipeline != cache->instance)
     program_state->cache_entry->usage_count--;
 
   if (--program_state->ref_count == 0)
@@ -280,6 +299,7 @@ destroy_program_state (void *user_data,
         g_array_free (program_state->uniform_locations, TRUE);
 
       g_free (program_state);
+      g_free (cache);
     }
 }
 
@@ -298,19 +318,23 @@ set_program_state (CoglPipeline *pipeline,
         program_state->cache_entry->usage_count++;
     }
 
-  _cogl_object_set_user_data (COGL_OBJECT (pipeline),
-                              &program_state_key,
-                              program_state,
-                              destroy_program_state);
+  CoglPipelineProgramStateCache *cache = g_new0 (CoglPipelineProgramStateCache, 1);
+  cache->instance = pipeline;
+  cache->program_state = program_state;
+
+  g_object_set_qdata_full (G_OBJECT (pipeline),
+                           get_cache_key (),
+                           cache,
+                           destroy_program_state);
 }
 
 static void
 dirty_program_state (CoglPipeline *pipeline)
 {
-  cogl_object_set_user_data (COGL_OBJECT (pipeline),
-                             &program_state_key,
-                             NULL,
-                             NULL);
+  g_object_set_qdata_full (G_OBJECT (pipeline),
+                           get_cache_key (),
+                           NULL,
+                           NULL);
 }
 
 static void
@@ -660,7 +684,7 @@ _cogl_shader_compile_real (CoglShader   *shader,
 
       if (shader->compilation_pipeline)
         {
-          cogl_object_unref (shader->compilation_pipeline);
+          g_object_unref (shader->compilation_pipeline);
           shader->compilation_pipeline = NULL;
         }
     }
@@ -690,7 +714,7 @@ _cogl_shader_compile_real (CoglShader   *shader,
                                                  NULL);
   GE (ctx, glCompileShader (shader->gl_handle));
 
-  shader->compilation_pipeline = cogl_object_ref (pipeline);
+  shader->compilation_pipeline = g_object_ref (pipeline);
 
   GE (ctx, glGetShaderiv (shader->gl_handle, GL_COMPILE_STATUS, &status));
   if (!status)

cogl/cogl/driver/gl/cogl-pipeline-vertend-glsl.c
@@ -59,7 +59,7 @@ struct _CoglPipelineVertendShaderState
   CoglPipelineCacheEntry *cache_entry;
 };
 
-static CoglUserDataKey shader_state_key;
+static GQuark shader_state_key = 0;
 
 static CoglPipelineVertendShaderState *
 shader_state_new (CoglPipelineCacheEntry *cache_entry)
@@ -73,10 +73,29 @@ shader_state_new (CoglPipelineCacheEntry *cache_entry)
   return shader_state;
 }
 
+typedef struct
+{
+  CoglPipelineVertendShaderState *shader_state;
+  CoglPipeline *instance;
+} CoglPipelineVertendShaderStateCache;
+
+static GQuark
+get_cache_key (void)
+{
+  if (G_UNLIKELY (shader_state_key == 0))
+    shader_state_key = g_quark_from_static_string ("shader-vertend-state-key");
+
+  return shader_state_key;
+}
+
 static CoglPipelineVertendShaderState *
 get_shader_state (CoglPipeline *pipeline)
 {
-  return cogl_object_get_user_data (COGL_OBJECT (pipeline), &shader_state_key);
+  CoglPipelineVertendShaderStateCache * cache;
+  cache = g_object_get_qdata (G_OBJECT (pipeline), get_cache_key ());
+  if (cache)
+    return cache->shader_state;
+  return NULL;
 }
 
 CoglPipelineVertendShaderState *
@@ -86,15 +105,15 @@ cogl_pipeline_vertend_glsl_get_shader_state (CoglPipeline *pipeline)
 }
 
 static void
-destroy_shader_state (void *user_data,
-                      void *instance)
+destroy_shader_state (void *user_data)
 {
-  CoglPipelineVertendShaderState *shader_state = user_data;
+  CoglPipelineVertendShaderStateCache *cache = user_data;
+  CoglPipelineVertendShaderState *shader_state = cache->shader_state;
 
   _COGL_GET_CONTEXT (ctx, NO_RETVAL);
 
   if (shader_state->cache_entry &&
-      shader_state->cache_entry->pipeline != instance)
+      shader_state->cache_entry->pipeline != cache->instance)
     shader_state->cache_entry->usage_count--;
 
   if (--shader_state->ref_count == 0)
@@ -103,6 +122,7 @@ destroy_shader_state (void *user_data,
         GE( ctx, glDeleteShader (shader_state->gl_shader) );
 
       g_free (shader_state);
+      g_free (cache);
     }
 }
 
@@ -121,19 +141,22 @@ set_shader_state (CoglPipeline *pipeline,
         shader_state->cache_entry->usage_count++;
     }
 
-  _cogl_object_set_user_data (COGL_OBJECT (pipeline),
-                              &shader_state_key,
-                              shader_state,
-                              destroy_shader_state);
+  CoglPipelineVertendShaderStateCache *cache = g_new0 (CoglPipelineVertendShaderStateCache, 1);
+  cache->instance = pipeline;
+  cache->shader_state = shader_state;
+  g_object_set_qdata_full (G_OBJECT (pipeline),
+                           get_cache_key (),
+                           cache,
+                           destroy_shader_state);
 }
 
 static void
 dirty_shader_state (CoglPipeline *pipeline)
 {
-  cogl_object_set_user_data (COGL_OBJECT (pipeline),
-                             &shader_state_key,
-                             NULL,
-                             NULL);
+  g_object_set_qdata_full (G_OBJECT (pipeline),
+                           get_cache_key (),
+                           NULL,
+                           NULL);
 }
 
 static gboolean

commit 61de3131981c60c4935337586e88698b8ddc46f6

cogl: Unconditionally free pipeline shader/program state cache struct
destroy_shader_state()/destroy_program_state() are called when the qdata
pointer to the state cache structure gets overwritten, but were only
conditionally destroying it. This lead to leaks when called with a
shader/program state ref count > 1.

Fixes: 9b9e12edb ("cogl: Port Node/Pipeline/PipelineLayer away from CoglObject")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3141
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3366>

cogl/cogl/driver/gl/cogl-pipeline-fragend-glsl.c
@@ -164,8 +164,9 @@ destroy_shader_state (void *user_data)
       g_free (shader_state->unit_state);
 
       g_free (shader_state);
-      g_free (cache);
     }
+
+  g_free (cache);
 }
 
 static void

cogl/cogl/driver/gl/cogl-pipeline-progend-glsl.c
@@ -298,8 +298,9 @@ destroy_program_state (void *user_data)
         g_array_free (program_state->uniform_locations, TRUE);
 
       g_free (program_state);
-      g_free (cache);
     }
+
+  g_free (cache);
 }
 
 static void

cogl/cogl/driver/gl/cogl-pipeline-vertend-glsl.c
@@ -121,8 +121,9 @@ destroy_shader_state (void *user_data)
         GE( ctx, glDeleteShader (shader_state->gl_shader) );
 
       g_free (shader_state);
-      g_free (cache);
     }
+
+  g_free (cache);
 }
 
 static void