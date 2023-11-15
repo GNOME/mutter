GNOME/mutter

commit 8f4ab53bd6c116fded360a683d9d1dc0559a2fdb

window-actor/wayland: Ensure to use allocation for black background check
The surface actors may not have a valid allocation when running the
test. The preferred height, which `clutter_actor_get_size()` returns
in that case, can be wrong in certain cases, making us not add the black
background when it's actually needed.

Query the allocation instead, even at the expense of additional
relayouts.

While on it, sneak it some small cleanups.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3024
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3310>

src/compositor/meta-window-actor-wayland.c
@@ -498,24 +498,23 @@ maybe_configure_black_background (MetaWindowActorWayland *self,
   clutter_actor_iter_init (&iter, CLUTTER_ACTOR (self->surface_container));
   while (clutter_actor_iter_next (&iter, &child))
     {
-      float child_width, child_height;
+      ClutterActorBox actor_box;
 
-      clutter_actor_get_size (child, &child_width, &child_height);
+      if (!clutter_actor_is_mapped (child))
+        continue;
 
-      if (META_IS_SURFACE_ACTOR (child) &&
-          meta_surface_actor_is_opaque (META_SURFACE_ACTOR (child)) &&
-          G_APPROX_VALUE (clutter_actor_get_x (child), 0,
+      clutter_actor_get_allocation_box (child, &actor_box);
+      if (meta_surface_actor_is_opaque (META_SURFACE_ACTOR (child)) &&
+          G_APPROX_VALUE (actor_box.x1, 0, CLUTTER_COORDINATE_EPSILON) &&
+          G_APPROX_VALUE (actor_box.y1, 0, CLUTTER_COORDINATE_EPSILON) &&
+          G_APPROX_VALUE (actor_box.x2, fullscreen_layout.width,
                           CLUTTER_COORDINATE_EPSILON) &&
-          G_APPROX_VALUE (clutter_actor_get_y (child), 0,
-                          CLUTTER_COORDINATE_EPSILON) &&
-          G_APPROX_VALUE (child_width, fullscreen_layout.width,
-                          CLUTTER_COORDINATE_EPSILON) &&
-          G_APPROX_VALUE (child_height, fullscreen_layout.height,
+          G_APPROX_VALUE (actor_box.y2, fullscreen_layout.height,
                           CLUTTER_COORDINATE_EPSILON))
         return FALSE;
 
-      max_width = MAX (max_width, child_width);
-      max_height = MAX (max_height, child_height);
+      max_width = MAX (max_width, actor_box.x2 - actor_box.x1);
+      max_height = MAX (max_height, actor_box.y2 - actor_box.y1);
     }
 
   *surfaces_width = max_width;

commit 976bcef3d5981d1d923eba7867bf3da0e4b4c929

window-actor/wayland: Sync geometry only when window is mapped
In some cases the window is not mapped when the geometry changes.
Without the mapped window the surfaces are not mapped either and don't
have a sensible allocation.

This patch makes sure we abort syncing the geometry if the window is not
mapped and also make sure we sync geometry when the actor eventually
does get mapped.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3152
Fixes: 8f4ab53bd ("window-actor/wayland: Ensure to use allocation for black background check")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3401>

src/compositor/meta-window-actor-wayland.c
@@ -536,6 +536,9 @@ meta_window_actor_wayland_sync_geometry (MetaWindowActor    *actor,
   if (window->unmanaging)
     return;
 
+  if (!clutter_actor_is_mapped (CLUTTER_ACTOR (actor)))
+    return;
+
   if (maybe_configure_black_background (self,
                                         &surfaces_width, &surfaces_height,
                                         &background_width, &background_height))
@@ -601,9 +604,25 @@ meta_window_actor_wayland_constructed (GObject *object)
 }
 
 static void
+meta_window_actor_wayland_map (ClutterActor *self)
+{
+  ClutterActorClass *parent_class =
+    CLUTTER_ACTOR_CLASS (meta_window_actor_wayland_parent_class);
+  MetaWindowActor *window_actor = META_WINDOW_ACTOR (self);
+  MetaWindow *window = meta_window_actor_get_meta_window (window_actor);
+  MtkRectangle actor_rect;
+
+  meta_window_get_buffer_rect (window, &actor_rect);
+  meta_window_actor_wayland_sync_geometry (window_actor, &actor_rect);
+
+  parent_class->map (self);
+}
+
+static void
 meta_window_actor_wayland_class_init (MetaWindowActorWaylandClass *klass)
 {
   MetaWindowActorClass *window_actor_class = META_WINDOW_ACTOR_CLASS (klass);
+  ClutterActorClass *clutter_actor_class = CLUTTER_ACTOR_CLASS (klass);
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
   window_actor_class->get_scanout_candidate = meta_window_actor_wayland_get_scanout_candidate;
@@ -618,6 +637,8 @@ meta_window_actor_wayland_class_init (MetaWindowActorWaylandClass *klass)
   window_actor_class->can_freeze_commits = meta_window_actor_wayland_can_freeze_commits;
   window_actor_class->sync_geometry = meta_window_actor_wayland_sync_geometry;
   window_actor_class->is_single_surface_actor = meta_window_actor_wayland_is_single_surface_actor;
+
+  clutter_actor_class->map = meta_window_actor_wayland_map;
 
   object_class->constructed = meta_window_actor_wayland_constructed;
   object_class->dispose = meta_window_actor_wayland_dispose;