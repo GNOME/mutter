2bcf6607d30c97b9de2901ffb1bdf9d63148ad30
Carlos Garnacho <carlosg@gnome.org>
2023-11-18 11:10:03 +0100

    compositor: Do not sync input focus
    The wayland bits do already take care of logically unsetting
    their focus in the presence of extraneous grabs, there is no
    need to trigger this from the compositor.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3420>

--- src/compositor/meta-compositor-server.c
+++ src/compositor/meta-compositor-server.c
@@ -41,30 +41,9 @@ meta_compositor_server_monotonic_to_high_res_xserver_time (MetaCompositor *compo
   return meta_translate_to_high_res_xserver_time (monotonic_time_us);
 }
 
-#ifdef HAVE_WAYLAND
-static MetaWaylandCompositor *
-wayland_compositor_from_display (MetaDisplay *display)
-{
-  MetaContext *context = meta_display_get_context (display);
-
-  return meta_context_get_wayland_compositor (context);
-}
-#endif
-
 static void
 meta_compositor_server_grab_begin (MetaCompositor *compositor)
 {
-  MetaDisplay *display = meta_compositor_get_display (compositor);
-#ifdef HAVE_WAYLAND
-  MetaWaylandCompositor *wayland_compositor =
-    wayland_compositor_from_display (display);
-#endif
-
-#ifdef HAVE_WAYLAND
-  meta_wayland_compositor_sync_focus (wayland_compositor);
-#endif
-  meta_display_cancel_touch (display);
-
 #ifdef HAVE_WAYLAND
   meta_dnd_wayland_handle_begin_modal (compositor);
 #endif
@@ -74,12 +53,7 @@ static void
 meta_compositor_server_grab_end (MetaCompositor *compositor)
 {
 #ifdef HAVE_WAYLAND
-  MetaDisplay *display = meta_compositor_get_display (compositor);;
-  MetaWaylandCompositor *wayland_compositor =
-    wayland_compositor_from_display (display);
-
   meta_dnd_wayland_handle_end_modal (compositor);
-  meta_wayland_compositor_sync_focus (wayland_compositor);
 #endif
 }
 
