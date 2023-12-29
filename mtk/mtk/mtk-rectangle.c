GNOME/mutter

commit a93471eb905f49e9bdcee32ef3c0929e517fc104

mtk: Move Rectangle.to_graphene_rect from Meta
Also drops the clutter equivalent

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3128>

mtk/mtk/mtk-rectangle.c
@@ -276,3 +276,24 @@ mtk_rectangle_contains_rect (const MtkRectangle *outer_rect,
     inner_rect->x + inner_rect->width  <= outer_rect->x + outer_rect->width &&
     inner_rect->y + inner_rect->height <= outer_rect->y + outer_rect->height;
 }
+
+/**
+ * mtk_rectangle_to_graphene_rect:
+ * @rect: A rectangle
+ *
+ * Returns: Return a graphene_rect_t created from `rect`
+ */
+graphene_rect_t
+mtk_rectangle_to_graphene_rect (MtkRectangle *rect)
+{
+  return (graphene_rect_t) {
+           .origin = {
+             .x = rect->x,
+             .y = rect->y
+           },
+           .size = {
+             .width = rect->width,
+             .height = rect->height
+           }
+  };
+}

mtk/mtk/mtk-rectangle.h
@@ -23,6 +23,7 @@
 
 #include <cairo.h>
 #include <glib-object.h>
+#include <graphene.h>
 
 #include "mtk/mtk-macros.h"
 
@@ -117,4 +118,7 @@ gboolean mtk_rectangle_could_fit_rect (const MtkRectangle *outer_rect,
 MTK_EXPORT
 gboolean mtk_rectangle_contains_rect (const MtkRectangle *outer_rect,
                                       const MtkRectangle *inner_rect);
+
+MTK_EXPORT
+graphene_rect_t mtk_rectangle_to_graphene_rect (MtkRectangle *rect);
 

clutter/clutter/clutter-private.h
@@ -201,9 +201,6 @@ void  _clutter_util_fully_transform_vertices (const graphene_matrix_t  *modelvie
                                               graphene_point3d_t       *vertices_out,
                                               int                       n_vertices);
 
-CLUTTER_EXPORT
-void _clutter_util_rect_from_rectangle (const MtkRectangle          *src,
-                                        graphene_rect_t             *dest);
 
 CLUTTER_EXPORT
 void _clutter_util_rectangle_int_extents (const graphene_rect_t *src,

clutter/clutter/clutter-stage.c
@@ -2689,7 +2689,7 @@ clutter_stage_get_capture_final_size (ClutterStage *stage,
       g_autoptr (GList) views = NULL;
       GList *l;
 
-      _clutter_util_rect_from_rectangle (rect, &capture_rect);
+      capture_rect = mtk_rectangle_to_graphene_rect (rect);
       views = clutter_stage_get_views_for_rect (stage, &capture_rect);
 
       if (!views)
@@ -2977,7 +2977,7 @@ clutter_stage_get_views_for_rect (ClutterStage          *stage,
       graphene_rect_t view_rect;
 
       clutter_stage_view_get_layout (view, &view_layout);
-      _clutter_util_rect_from_rectangle (&view_layout, &view_rect);
+      view_rect = mtk_rectangle_to_graphene_rect (&view_layout);
 
       if (graphene_rect_intersection (&view_rect, rect, NULL))
         views_for_rect = g_list_prepend (views_for_rect, view);

clutter/clutter/clutter-util.c
@@ -120,22 +120,6 @@ _clutter_util_fully_transform_vertices (const graphene_matrix_t  *modelview,
 }
 
 void
-_clutter_util_rect_from_rectangle (const MtkRectangle *src,
-                                   graphene_rect_t    *dest)
-{
-  *dest = (graphene_rect_t) {
-    .origin = {
-      .x = src->x,
-      .y = src->y
-    },
-    .size = {
-      .width = src->width,
-      .height = src->height
-    }
-  };
-}
-
-void
 _clutter_util_rectangle_int_extents (const graphene_rect_t *src,
                                      MtkRectangle          *dest)
 {

mtk/meson.build
@@ -29,6 +29,7 @@ mtk_pkg_deps = [
   glib_dep,
   gobject_dep,
   gio_dep,
+  graphene_dep,
 ]
 
 mtk_deps = [

mtk/mtk/meson.build
@@ -55,6 +55,7 @@ if have_introspection
     includes: [
       'GObject-2.0',
       'cairo-1.0',
+      'Graphene-1.0',
     ],
     extra_args: mtk_introspection_args + ['--c-include=mtk/mtk.h'],
     install_dir_gir: pkglibdir,

src/backends/meta-cursor-renderer.c
@@ -179,7 +179,7 @@ meta_cursor_renderer_after_paint (ClutterStage       *stage,
       rect = meta_cursor_renderer_calculate_rect (renderer,
                                                   priv->displayed_cursor);
       clutter_stage_view_get_layout (stage_view, &view_layout);
-      view_rect = meta_rectangle_to_graphene_rect (&view_layout);
+      view_rect = mtk_rectangle_to_graphene_rect (&view_layout);
       if (graphene_rect_intersection (&rect, &view_rect, NULL))
         {
           meta_cursor_renderer_emit_painted (renderer,
@@ -392,7 +392,7 @@ find_highest_logical_monitor_scale (MetaBackend      *backend,
     {
       MetaLogicalMonitor *logical_monitor = l->data;
       graphene_rect_t logical_monitor_rect =
-        meta_rectangle_to_graphene_rect (&logical_monitor->rect);
+        mtk_rectangle_to_graphene_rect (&logical_monitor->rect);
 
       if (!graphene_rect_intersection (&cursor_rect,
                                        &logical_monitor_rect,

commit a4181a67302c01f99f22e3115729fc4b0c7fb871

mtk: Mark mtk_rectangle_to_graphene_rect in-variable as const
To silence occasional compiler warnings.

Fixes: a93471eb90 ("mtk: Move Rectangle.to_graphene_rect from Meta")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3474>

mtk/mtk/mtk-rectangle.c
@@ -286,7 +286,7 @@ mtk_rectangle_contains_rect (const MtkRectangle *outer_rect,
  * Returns: Return a graphene_rect_t created from `rect`
  */
 graphene_rect_t
-mtk_rectangle_to_graphene_rect (MtkRectangle *rect)
+mtk_rectangle_to_graphene_rect (const MtkRectangle *rect)
 {
   return (graphene_rect_t) {
            .origin = {

mtk/mtk/mtk-rectangle.h
@@ -117,7 +117,7 @@ gboolean mtk_rectangle_contains_rect (const MtkRectangle *outer_rect,
                                       const MtkRectangle *inner_rect);
 
 MTK_EXPORT
-graphene_rect_t mtk_rectangle_to_graphene_rect (MtkRectangle *rect);
+graphene_rect_t mtk_rectangle_to_graphene_rect (const MtkRectangle *rect);
 
 MTK_EXPORT
 void mtk_rectangle_from_graphene_rect (const graphene_rect_t *rect,