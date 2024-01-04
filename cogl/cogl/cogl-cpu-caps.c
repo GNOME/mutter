GNOME/mutter

commit 3bf6de60bbea642745394b50c907e00a598af69a

cogl: Add CPU capability detection helper
This will be needed to support the fast half float implementations.
Copied from mesa and adapted.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3441>

cogl/cogl/cogl-cpu-caps.c
@@ -1,0 +1,83 @@
+/*
+ * Cogl
+ *
+ * A Low Level GPU Graphics and Utilities API
+ *
+ * Copyright (C) 2022 Red Hat Inc
+ * Copyright 2008 Dennis Smit
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
+
+/* CPU capability discovery code comes from lp_cpu_detect.c in mesa. */
+
+#include "config.h"
+
+#include "cogl/cogl-cpu-caps.h"
+
+#include <stdint.h>
+
+CoglCpuCaps cogl_cpu_caps;
+
+static inline uint64_t
+xgetbv (void)
+{
+#if defined(__GNUC__)
+  uint32_t eax, edx;
+
+  __asm __volatile (
+    ".byte 0x0f, 0x01, 0xd0" /* xgetbv isn't supported on gcc < 4.4 */
+    : "=a"(eax),
+      "=d"(edx)
+    : "c"(0)
+  );
+
+  return ((uint64_t) edx << 32) | eax;
+#else
+  return 0;
+#endif
+}
+
+void
+cogl_init_cpu_caps (void)
+{
+#ifdef HAVE_CPUID
+  uint32_t regs[4];
+
+  cpuid(0x00000000, regs);
+
+  if (regs[0] >= 0x00000001)
+    {
+      uint32_t regs2[4];
+      gboolean has_avx;
+
+      cpuid (0x00000001, regs2);
+
+      has_avx = (((regs2[2] >> 28) & 1) && /* AVX */
+                 ((regs2[2] >> 27) & 1) && /* OSXSAVE */
+                 ((xgetbv() & 6) == 6));   /* XMM & YMM */
+      if (((regs2[2] >> 29) & 1) && has_avx)
+        cpu_caps |= COGL_CPU_CAP_F16C;
+    }
+#endif
+}

cogl/cogl/cogl-cpu-caps.h
@@ -1,0 +1,54 @@
+/*
+ * Cogl
+ *
+ * A Low Level GPU Graphics and Utilities API
+ *
+ * Copyright (C) 2022 Red Hat Inc
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
+
+#ifndef COGL_CPU_CAPS_H
+#define COGL_CPU_CAPS_H
+
+#include "cogl/cogl-types.h"
+
+#include <glib.h>
+
+typedef enum _CoglCpuCaps
+{
+  COGL_CPU_CAP_F16C = 1 << 0,
+} CoglCpuCaps;
+
+COGL_EXPORT
+CoglCpuCaps cogl_cpu_caps;
+
+void cogl_init_cpu_caps (void);
+
+static inline gboolean
+cogl_cpu_has_cap (CoglCpuCaps cap)
+{
+  return !!(cogl_cpu_caps & cap);
+}
+
+#endif /* COGL_CPU_CAPS_H */

cogl/cogl/cogl.c
@@ -35,6 +35,7 @@
 #include <stdlib.h>
 
 #include "cogl/cogl-i18n-private.h"
+#include "cogl/cogl-cpu-caps.h"
 #include "cogl/cogl-debug.h"
 #include "cogl/cogl-graphene.h"
 #include "cogl/cogl-util.h"
@@ -176,6 +177,7 @@ _cogl_init (void)
   if (initialized == FALSE)
     {
       _cogl_debug_check_environment ();
+      cogl_init_cpu_caps ();
       initialized = TRUE;
     }
 }

cogl/cogl/meson.build
@@ -172,6 +172,8 @@ cogl_sources = [
   'cogl-color.c',
   'cogl-context-private.h',
   'cogl-context.c',
+  'cogl-cpu-caps.c',
+  'cogl-cpu-caps.h',
   'cogl-debug-options.h',
   'cogl-debug.c',
   'cogl-debug.h',

commit 985e93f9faff6765e4a02ac21da1035689f8d4df

cogl/cpu-caps: Build cpuid and xgetbv only for x86_64
This also adds the cpuid functionality inline and checks for GCC ASM
flag output support.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3226
Fixes: 3bf6de60b ("cogl: Add CPU capability detection helper")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3488>

cogl/cogl/cogl-cpu-caps.c
@@ -39,10 +39,11 @@
 
 CoglCpuCaps cogl_cpu_caps;
 
+#ifdef __x86_64
 static inline uint64_t
 xgetbv (void)
 {
-#if defined(__GNUC__)
+#ifdef __GCC_ASM_FLAG_OUTPUTS__
   uint32_t eax, edx;
 
   __asm __volatile (
@@ -58,13 +59,35 @@ xgetbv (void)
 #endif
 }
 
+static inline void
+cpuid (uint32_t  ax,
+       uint32_t *p)
+{
+#ifdef __GCC_ASM_FLAG_OUTPUTS__
+   __asm __volatile (
+     "cpuid\n\t"
+     : "=a" (p[0]),
+       "=b" (p[1]),
+       "=c" (p[2]),
+       "=d" (p[3])
+     : "0" (ax)
+   );
+#else
+   p[0] = 0;
+   p[1] = 0;
+   p[2] = 0;
+   p[3] = 0;
+#endif
+}
+#endif
+
 void
 cogl_init_cpu_caps (void)
 {
-#ifdef HAVE_CPUID
+#ifdef __x86_64
   uint32_t regs[4];
 
-  cpuid(0x00000000, regs);
+  cpuid (0x00000000, regs);
 
   if (regs[0] >= 0x00000001)
     {
@@ -75,9 +98,9 @@ cogl_init_cpu_caps (void)
 
       has_avx = (((regs2[2] >> 28) & 1) && /* AVX */
                  ((regs2[2] >> 27) & 1) && /* OSXSAVE */
-                 ((xgetbv() & 6) == 6));   /* XMM & YMM */
+                 ((xgetbv () & 6) == 6));   /* XMM & YMM */
       if (((regs2[2] >> 29) & 1) && has_avx)
-        cpu_caps |= COGL_CPU_CAP_F16C;
+        cogl_cpu_caps |= COGL_CPU_CAP_F16C;
     }
 #endif
 }