GNOME/mutter

commit 4b9d1271a0a1a4d6378302b7b7b225622177c18c

backends/native: Add PLANE_PROP_HOTSPOT_[X,Y]
Add META_KMS_PLANE_PROP_HOTSPOT_[X,Y] properties
to the MetaKmsPlaneProp enumeration, and
properly initialise them.

Also, add a convenience method in meta-kms-plane
(i.e., `meta_kms_plane_supports_cursor_hotspot`)
to check whether a plane supports hotspot
property setting.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3337>

src/backends/native/meta-kms-cursor-manager.c
@@ -405,14 +405,17 @@ maybe_update_cursor_plane (MetaKmsCursorManagerImpl  *cursor_manager_impl,
         .height = round (cursor_rect.size.height),
       };
 
-      plane_assignment = meta_kms_update_assign_plane (update,
-                                                       crtc, cursor_plane,
-                                                       buffer,
-                                                       src_rect, dst_rect,
-                                                       assign_plane_flags);
-      meta_kms_plane_assignment_set_cursor_hotspot (plane_assignment,
-                                                    (int) roundf (hotspot->x),
-                                                    (int) roundf (hotspot->y));
+      if (meta_kms_plane_supports_cursor_hotspot (cursor_plane))
+        {
+          plane_assignment = meta_kms_update_assign_plane (update,
+                                                           crtc, cursor_plane,
+                                                           buffer,
+                                                           src_rect, dst_rect,
+                                                           assign_plane_flags);
+          meta_kms_plane_assignment_set_cursor_hotspot (plane_assignment,
+                                                        (int) roundf (hotspot->x),
+                                                        (int) roundf (hotspot->y));
+        }
     }
   else
     {

src/backends/native/meta-kms-plane-private.h
@@ -40,6 +40,8 @@ typedef enum _MetaKmsPlaneProp
   META_KMS_PLANE_PROP_CRTC_ID,
   META_KMS_PLANE_PROP_FB_DAMAGE_CLIPS_ID,
   META_KMS_PLANE_PROP_IN_FENCE_FD,
+  META_KMS_PLANE_PROP_HOTSPOT_X,
+  META_KMS_PLANE_PROP_HOTSPOT_Y,
   META_KMS_PLANE_N_PROPS
 } MetaKmsPlaneProp;
 

src/backends/native/meta-kms-plane.c
@@ -26,6 +26,8 @@
 #include "backends/meta-monitor-transform.h"
 #include "backends/native/meta-kms-crtc.h"
 #include "backends/native/meta-kms-impl-device.h"
+#include "backends/native/meta-kms-impl-device-atomic.h"
+#include "backends/native/meta-kms-device-private.h"
 #include "backends/native/meta-kms-update-private.h"
 
 typedef struct _MetaKmsPlanePropTable
@@ -187,6 +189,23 @@ meta_kms_plane_is_transform_handled (MetaKmsPlane         *plane,
     }
 
   return FALSE;
+}
+
+gboolean
+meta_kms_plane_supports_cursor_hotspot (MetaKmsPlane *plane)
+{
+  MetaKmsImplDevice *impl_device =
+    meta_kms_device_get_impl_device (plane->device);
+
+  if (META_IS_KMS_IMPL_DEVICE_ATOMIC (impl_device))
+    {
+      return (meta_kms_plane_get_prop_id (plane, META_KMS_PLANE_PROP_HOTSPOT_X) &&
+              meta_kms_plane_get_prop_id (plane, META_KMS_PLANE_PROP_HOTSPOT_Y));
+    }
+  else
+    {
+      return TRUE;
+    }
 }
 
 GArray *
@@ -503,6 +522,16 @@ init_properties (MetaKmsPlane            *plane,
       [META_KMS_PLANE_PROP_IN_FENCE_FD] =
         {
           .name = "IN_FENCE_FD",
+          .type = DRM_MODE_PROP_SIGNED_RANGE,
+        },
+      [META_KMS_PLANE_PROP_HOTSPOT_X] =
+        {
+          .name = "HOTSPOT_X",
+          .type = DRM_MODE_PROP_SIGNED_RANGE,
+        },
+      [META_KMS_PLANE_PROP_HOTSPOT_Y] =
+        {
+          .name = "HOTSPOT_Y",
           .type = DRM_MODE_PROP_SIGNED_RANGE,
         },
     },

src/backends/native/meta-kms-plane.h
@@ -47,6 +47,8 @@ MetaKmsPlaneType meta_kms_plane_get_plane_type (MetaKmsPlane *plane);
 gboolean meta_kms_plane_is_transform_handled (MetaKmsPlane         *plane,
                                               MetaMonitorTransform  transform);
 
+gboolean meta_kms_plane_supports_cursor_hotspot (MetaKmsPlane *plane);
+
 GArray * meta_kms_plane_get_modifiers_for_format (MetaKmsPlane *plane,
                                                   uint32_t      format);
 

commit 1a0d00467d817888c3c9a9b51591922096a2e0f9

backends/native: Fix atomic cursor plane assign
Make atomic cursor plane assignment not depending on
cursor hotspot support.

Fixes: 4b9d1271a ("backends/native: Add PLANE_PROP_HOTSPOT_[X,Y]")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3238
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3503>

src/backends/native/meta-kms-cursor-manager.c
@@ -405,13 +405,14 @@ maybe_update_cursor_plane (MetaKmsCursorManagerImpl  *cursor_manager_impl,
         .height = round (cursor_rect.size.height),
       };
 
+      plane_assignment = meta_kms_update_assign_plane (update,
+                                                       crtc, cursor_plane,
+                                                       buffer,
+                                                       src_rect, dst_rect,
+                                                       assign_plane_flags);
+
       if (meta_kms_plane_supports_cursor_hotspot (cursor_plane))
         {
-          plane_assignment = meta_kms_update_assign_plane (update,
-                                                           crtc, cursor_plane,
-                                                           buffer,
-                                                           src_rect, dst_rect,
-                                                           assign_plane_flags);
           meta_kms_plane_assignment_set_cursor_hotspot (plane_assignment,
                                                         (int) roundf (hotspot->x),
                                                         (int) roundf (hotspot->y));