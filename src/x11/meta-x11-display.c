GNOME/mutter

commit bc9cd123e95e61321a21c9baf8040fda912c2744

window: Move xwindow to WindowX11
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3211>

src/x11/meta-x11-display.c
@@ -64,6 +64,7 @@
 #include "x11/group-props.h"
 #include "x11/meta-x11-selection-private.h"
 #include "x11/window-props.h"
+#include "x11/window-x11.h"
 #include "x11/xprops.h"
 
 #ifdef HAVE_XWAYLAND
@@ -2017,7 +2018,7 @@ meta_x11_display_update_active_window_hint (MetaX11Display *x11_display)
                                                    x11_display->focus_xwindow);
 
   if (focus_window)
-    data[0] = focus_window->xwindow;
+    data[0] = meta_window_x11_get_xwindow (focus_window);
   else
     data[0] = None;
 
@@ -2101,7 +2102,7 @@ meta_x11_display_set_input_focus (MetaX11Display *x11_display,
       if (window->frame && !meta_window_is_focusable (window))
         xwindow = window->frame->xwindow;
       else
-        xwindow = window->xwindow;
+        xwindow = meta_window_x11_get_xwindow (window);
 
 #ifdef HAVE_X11
       if (!meta_is_wayland_compositor ())

src/core/frame.c
@@ -31,6 +31,7 @@
 #include "core/keybindings-private.h"
 #include "meta/meta-x11-errors.h"
 #include "x11/meta-x11-display-private.h"
+#include "x11/window-x11.h"
 #include "x11/window-props.h"
 
 #include <X11/Xatom.h>
@@ -49,7 +50,7 @@ meta_window_ensure_frame (MetaWindow *window)
   meta_x11_error_trap_push (x11_display);
 
   XChangeProperty (x11_display->xdisplay,
-                   window->xwindow,
+                   meta_window_x11_get_xwindow (window),
                    x11_display->atom__MUTTER_NEEDS_FRAME,
                    XA_CARDINAL,
                    32, PropModeReplace, (guchar*) data, 1);
@@ -124,10 +125,10 @@ meta_window_set_frame_xwindow (MetaWindow *window,
     }
 
   meta_stack_tracker_record_remove (window->display->stack_tracker,
-                                    window->xwindow,
+                                    meta_window_x11_get_xwindow (window),
                                     XNextRequest (x11_display->xdisplay));
   XReparentWindow (x11_display->xdisplay,
-                   window->xwindow,
+                   meta_window_x11_get_xwindow (window),
                    frame->xwindow,
                    frame->child_x,
                    frame->child_y);
@@ -203,12 +204,12 @@ meta_window_destroy_frame (MetaWindow *window)
       if (!window->unmanaging)
         {
           meta_stack_tracker_record_add (window->display->stack_tracker,
-                                         window->xwindow,
+                                         meta_window_x11_get_xwindow (window),
                                          XNextRequest (x11_display->xdisplay));
         }
 
       XReparentWindow (x11_display->xdisplay,
-                       window->xwindow,
+                       meta_window_x11_get_xwindow (window),
                        x11_display->xroot,
                        /* Using anything other than client root window coordinates
                         * coordinates here means we'll need to ensure a configure
@@ -223,7 +224,7 @@ meta_window_destroy_frame (MetaWindow *window)
     XShapeSelectInput (x11_display->xdisplay, frame->xwindow, NoEventMask);
 
   XDeleteProperty (x11_display->xdisplay,
-                   window->xwindow,
+                   meta_window_x11_get_xwindow (window),
                    x11_display->atom__MUTTER_NEEDS_FRAME);
 
   meta_x11_error_trap_pop (x11_display);

src/core/keybindings.c
@@ -1208,7 +1208,7 @@ meta_change_button_grab (MetaKeyBindingManager *keys,
   if (window->frame)
     xwindow = window->frame->xwindow;
   else
-    xwindow = window->xwindow;
+    xwindow = meta_window_x11_get_xwindow (window);
 
   /* GrabModeSync means freeze until XAllowEvents */
   if (grab)
@@ -1605,7 +1605,7 @@ meta_window_grab_keys (MetaWindow  *window)
       || window->override_redirect)
     {
       if (window->keys_grabbed)
-        change_window_keygrabs (keys, window->xwindow, FALSE);
+        change_window_keygrabs (keys, meta_window_x11_get_xwindow (window), FALSE);
       window->keys_grabbed = FALSE;
       return;
     }
@@ -1613,7 +1613,7 @@ meta_window_grab_keys (MetaWindow  *window)
   if (window->keys_grabbed)
     {
       if (window->frame && !window->grab_on_frame)
-        change_window_keygrabs (keys, window->xwindow, FALSE);
+        change_window_keygrabs (keys, meta_window_x11_get_xwindow (window), FALSE);
       else if (window->frame == NULL &&
                window->grab_on_frame)
         ; /* continue to regrab on client window */
@@ -1641,7 +1641,7 @@ meta_window_ungrab_keys (MetaWindow  *window)
           window->frame != NULL)
         change_window_keygrabs (keys, window->frame->xwindow, FALSE);
       else if (!window->grab_on_frame)
-        change_window_keygrabs (keys, window->xwindow, FALSE);
+        change_window_keygrabs (keys, meta_window_x11_get_xwindow (window), FALSE);
 
       window->keys_grabbed = FALSE;
     }

src/core/stack-tracker.c
@@ -45,6 +45,7 @@
 #include "meta/meta-x11-errors.h"
 #include "meta/util.h"
 #include "x11/meta-x11-display-private.h"
+#include "x11/window-x11.h"
 
 /* The complexity here comes from resolving two competing factors:
  *
@@ -581,7 +582,6 @@ on_stack_changed (MetaStack        *stack,
   for (l = sorted; l; l = l->next)
     {
       MetaWindow *w = l->data;
-      uint64_t top_level_window;
       uint64_t stack_id;
 
       if (w->unmanaging)
@@ -590,13 +590,13 @@ on_stack_changed (MetaStack        *stack,
       meta_topic (META_DEBUG_STACK, "  %u:%d - %s ",
 		  w->layer, w->stack_position, w->desc);
 
-      if (w->frame)
-	top_level_window = w->frame->xwindow;
-      else
-	top_level_window = w->xwindow;
-
       if (w->client_type == META_WINDOW_CLIENT_TYPE_X11)
-        stack_id = top_level_window;
+        {
+          if (w->frame)
+	          stack_id = w->frame->xwindow;
+          else
+	          stack_id = meta_window_x11_get_xwindow (w);
+        }
       else
         stack_id = w->stamp;
 
@@ -1047,7 +1047,7 @@ meta_stack_tracker_sync_stack (MetaStackTracker *tracker)
            * see window-prop.c:reload_net_wm_user_time_window() for registration.)
            */
           if (meta_window &&
-              ((Window)window == meta_window->xwindow ||
+              ((Window)window == meta_window_x11_get_xwindow (meta_window) ||
                (meta_window->frame && (Window)window == meta_window->frame->xwindow)))
             meta_windows = g_list_prepend (meta_windows, meta_window);
         }

src/core/stack.c
@@ -40,6 +40,7 @@
 #include "meta/prefs.h"
 #include "meta/workspace.h"
 #include "x11/meta-x11-display-private.h"
+#include "x11/window-x11.h"
 
 #define WINDOW_TRANSIENT_FOR_WHOLE_GROUP(w)        \
   (meta_window_has_transient_type (w) && w->transient_for == NULL)

src/core/window-private.h
@@ -175,7 +175,6 @@ struct _MetaWindow
   MetaLogicalMonitor *highest_scale_monitor;
   MetaWorkspace *workspace;
   MetaWindowClientType client_type;
-  Window xwindow;
   /* may be NULL! not all windows get decorated */
   MetaFrame *frame;
   int depth;

src/core/window.c
@@ -213,7 +213,6 @@ enum
   PROP_IS_ALIVE,
   PROP_DISPLAY,
   PROP_EFFECT,
-  PROP_XWINDOW,
   PROP_SUSPEND_STATE,
 
   PROP_LAST,
@@ -434,9 +433,6 @@ meta_window_get_property(GObject         *object,
     case PROP_EFFECT:
       g_value_set_int (value, win->pending_compositor_effect);
       break;
-    case PROP_XWINDOW:
-      g_value_set_ulong (value, win->xwindow);
-      break;
     case PROP_SUSPEND_STATE:
       g_value_set_enum (value, priv->suspend_state);
       break;
@@ -461,9 +457,6 @@ meta_window_set_property(GObject         *object,
       break;
     case PROP_EFFECT:
       win->pending_compositor_effect = g_value_get_int (value);
-      break;
-    case PROP_XWINDOW:
-      win->xwindow = g_value_get_ulong (value);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -601,11 +594,6 @@ meta_window_class_init (MetaWindowClass *klass)
                       META_COMP_EFFECT_NONE,
                       META_COMP_EFFECT_NONE,
                       G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
-
-  obj_props[PROP_XWINDOW] =
-    g_param_spec_ulong ("xwindow", NULL, NULL,
-                        0, G_MAXULONG, 0,
-                        G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
 
   /**
    * MetaWindow::suspend-state: (skip)
@@ -943,12 +931,12 @@ meta_window_update_desc (MetaWindow *window)
   g_clear_pointer (&window->desc, g_free);
 
   if (window->client_type == META_WINDOW_CLIENT_TYPE_X11)
-    window->desc = g_strdup_printf ("0x%lx", window->xwindow);
+    window->desc = g_strdup_printf ("0x%lx", meta_window_x11_get_xwindow (window));
   else
     {
-      guint64 small_stamp = window->stamp - G_GUINT64_CONSTANT(0x100000000);
+      guint64 small_stamp = window->stamp - G_GUINT64_CONSTANT (0x100000000);
 
-      window->desc = g_strdup_printf ("W%" G_GUINT64_FORMAT , small_stamp);
+      window->desc = g_strdup_printf ("W%" G_GUINT64_FORMAT, small_stamp);
     }
 }
 
@@ -6465,17 +6453,6 @@ MetaDisplay *
 meta_window_get_display (MetaWindow *window)
 {
   return window->display;
-}
-
-/**
- * meta_window_get_xwindow: (skip)
- * @window: a #MetaWindow
- *
- */
-Window
-meta_window_get_xwindow (MetaWindow *window)
-{
-  return window->xwindow;
 }
 
 MetaWindowType

src/meta/window.h
@@ -141,9 +141,6 @@ META_EXPORT
 MetaDisplay *meta_window_get_display (MetaWindow *window);
 
 META_EXPORT
-Window meta_window_get_xwindow (MetaWindow *window);
-
-META_EXPORT
 MetaWindowType meta_window_get_window_type (MetaWindow *window);
 
 META_EXPORT

commit 09b7cd9f4a6162c0c6104ed68883dbcdc4e4a710

x11/display: Don't try to retrieve xwindow of wayland windows
Trying to get the xwindow of a wayland only window would fail when
casting to a x11 window. Which happens as
meta_x11_display_set_input_focus is called whenever the focused
window changes, whether it is a wayland or x11 one

Fixes: bc9cd123e ("window: Move xwindow to WindowX11")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3506>

src/x11/meta-x11-display.c
@@ -2088,10 +2088,14 @@ meta_x11_display_set_input_focus (MetaX11Display *x11_display,
                                   MetaWindow     *window,
                                   uint32_t        timestamp)
 {
-  Window xwindow;
+  Window xwindow = x11_display->no_focus_window;
   gulong serial;
+#ifdef HAVE_X11
+  MetaDisplay *display = x11_display->display;
+  ClutterStage *stage = CLUTTER_STAGE (meta_get_stage_for_display (display));
+#endif
 
-  if (window)
+  if (window && META_IS_WINDOW_X11 (window))
     {
       /* For output-only windows, focus the frame.
        * This seems to result in the client window getting key events
@@ -2106,33 +2110,19 @@ meta_x11_display_set_input_focus (MetaX11Display *x11_display,
 
 #ifdef HAVE_X11
       if (!meta_is_wayland_compositor ())
-        {
-          MetaDisplay *display = x11_display->display;
-          ClutterStage *stage = CLUTTER_STAGE (meta_get_stage_for_display (display));
-
-          clutter_stage_set_key_focus (stage, NULL);
-        }
-#endif
+        clutter_stage_set_key_focus (stage, NULL);
     }
-  else
+  else if (!meta_is_wayland_compositor () &&
+           stage_has_focus_actor (x11_display))
     {
-#ifdef HAVE_X11
       /* If we expect keyboard focus (e.g. there is a focused actor, keep
        * focus on the stage window, otherwise focus the no focus window.
        */
-      if (!meta_is_wayland_compositor () &&
-          stage_has_focus_actor (x11_display))
-        {
-          MetaDisplay *display = x11_display->display;
-          ClutterStage *stage = CLUTTER_STAGE (meta_get_stage_for_display (display));
-          xwindow = meta_x11_get_stage_window (stage);
-        }
-      else
-#endif
-        {
-          xwindow = x11_display->no_focus_window;
-        }
+      xwindow = meta_x11_get_stage_window (stage);
     }
+#else
+    }
+#endif
 
   meta_topic (META_DEBUG_FOCUS, "Setting X11 input focus for window %s to 0x%lx",
               window ? window->desc : "none", xwindow);