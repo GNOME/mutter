GNOME/mutter

commit f4c6fa949debc1f11e73de9228415f4b72e02286

clutter: Make clutter_actor_create_texture_paint_node public
Since StDrawingArea in gnome-shell is the only user of ClutterCanvas,
it is possible to move ClutterCanvas completely out of Mutter to
gnome-shell. This allows to remove another Cairo dependency from
Mutter.

This patch makes clutter_actor_create_texture_paint_node() function
public to be used by StDrawingArea in gnome-shell that replaces
ClutterCanvas.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3507>

clutter/clutter/clutter-actor.c
@@ -18385,7 +18385,7 @@ clutter_actor_bind_model_with_properties (ClutterActor *self,
   clutter_actor_bind_model (self, model, bind_child_with_properties, clos, bind_closure_free);
 }
 
-/*< private >
+/*
  * clutter_actor_create_texture_paint_node:
  * @self: a #ClutterActor
  * @texture: a #CoglTexture

clutter/clutter/clutter-actor-private.h
@@ -249,9 +249,6 @@ void                            clutter_actor_clear_stage_views_recursive       
 
 float                           clutter_actor_get_real_resource_scale                   (ClutterActor *actor);
 
-ClutterPaintNode *              clutter_actor_create_texture_paint_node                 (ClutterActor *self,
-                                                                                         CoglTexture  *texture);
-
 void clutter_actor_finish_layout (ClutterActor *self,
                                   int           phase);
 

clutter/clutter/clutter-actor.h
@@ -308,6 +308,9 @@ CLUTTER_EXPORT
 void                            clutter_actor_continue_paint                    (ClutterActor                *self,
                                                                                  ClutterPaintContext         *paint_context);
 CLUTTER_EXPORT
+ClutterPaintNode *              clutter_actor_create_texture_paint_node         (ClutterActor                *self,
+                                                                                 CoglTexture                 *texture);
+CLUTTER_EXPORT
 void                            clutter_actor_pick                              (ClutterActor                *actor,
                                                                                  ClutterPickContext          *pick_context);
 CLUTTER_EXPORT

commit e8116ba4f0be6a198627f6c4f3d1a859ec853c04

clutter/actor: Double asterisk is required for GObject Introspection
Like GtkDoc and Doxygen before it.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3252

Fixes: f4c6fa949debc1f11e73de9228415f4b72e02286
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3527>

clutter/clutter/clutter-actor.c
@@ -18385,7 +18385,7 @@ clutter_actor_bind_model_with_properties (ClutterActor *self,
   clutter_actor_bind_model (self, model, bind_child_with_properties, clos, bind_closure_free);
 }
 
-/*
+/**
  * clutter_actor_create_texture_paint_node:
  * @self: a #ClutterActor
  * @texture: a #CoglTexture