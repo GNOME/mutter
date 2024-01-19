af4de2a68171eb2ea73c33319eb3895f7284515c
Sebastian Wick <sebastian.wick@redhat.com>
2024-01-19 19:07:41 +0100

    output/native: Remove unused function declarations
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3535>

--- src/backends/native/meta-output-kms.h
+++ src/backends/native/meta-output-kms.h
@@ -30,10 +30,6 @@ G_DECLARE_FINAL_TYPE (MetaOutputKms, meta_output_kms,
                       META, OUTPUT_KMS,
                       MetaOutputNative)
 
-void meta_output_kms_set_power_save_mode (MetaOutputKms *output_kms,
-                                          uint64_t       dpms_state,
-                                          MetaKmsUpdate *kms_update);
-
 void meta_output_kms_set_underscan (MetaOutputKms *output_kms,
                                     MetaKmsUpdate *kms_update);
 
@@ -41,9 +37,6 @@ void meta_output_kms_set_max_bpc (MetaOutputKms *output_kms,
                                   MetaKmsUpdate *kms_update);
 
 gboolean meta_output_kms_is_privacy_screen_invalid (MetaOutputKms *output_kms);
-
-void meta_output_kms_set_privacy_screen (MetaOutputKms *output_kms,
-                                         MetaKmsUpdate *kms_update);
 
 gboolean meta_output_kms_can_clone (MetaOutputKms *output_kms,
                                     MetaOutputKms *other_output_kms);
