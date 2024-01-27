GNOME/mutter

commit 7716b62fa2ae04f432b4429c70f81b01cb85691a

clutter: Separate ClutterInputFocus event processing and filtering
Currently, we let the same function handle key event filtering as they
are passed to the IM, and the IM events resulting in actions like text
commit or preedit changes.

Split these two aspects into filter/process functions, and port
ClutterText to it. MetaWaylandTextInput still handles everything in
a single place, but that will be split in later commits.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3044>

src/wayland/meta-wayland-text-input.c
@@ -801,6 +801,21 @@ meta_wayland_text_input_init (MetaWaylandCompositor *compositor)
 }
 
 gboolean
+meta_wayland_text_input_update (MetaWaylandTextInput *text_input,
+                                const ClutterEvent   *event)
+{
+  if (!text_input->surface ||
+      !clutter_input_focus_is_focused (text_input->input_focus))
+    return FALSE;
+
+  if (event->type == CLUTTER_KEY_PRESS ||
+      event->type == CLUTTER_KEY_RELEASE)
+    return clutter_input_focus_filter_event (text_input->input_focus, event);
+
+  return FALSE;
+}
+
+gboolean
 meta_wayland_text_input_handle_event (MetaWaylandTextInput *text_input,
                                       const ClutterEvent   *event)
 {
@@ -815,7 +830,7 @@ meta_wayland_text_input_handle_event (MetaWaylandTextInput *text_input,
       clutter_event_get_flags (event) & CLUTTER_EVENT_FLAG_INPUT_METHOD)
     meta_wayland_text_input_focus_flush_done (text_input->input_focus);
 
-  retval = clutter_input_focus_filter_event (text_input->input_focus, event);
+  retval = clutter_input_focus_process_event (text_input->input_focus, event);
 
   if (event->type == CLUTTER_BUTTON_PRESS ||
       event->type == CLUTTER_TOUCH_BEGIN)

clutter/clutter/clutter-input-focus.c
@@ -190,7 +190,22 @@ clutter_input_focus_filter_event (ClutterInputFocus  *focus,
     {
       return clutter_input_method_filter_key_event (priv->im, &event->key);
     }
-  else if (event->type == CLUTTER_IM_COMMIT)
+
+  return FALSE;
+}
+
+gboolean
+clutter_input_focus_process_event (ClutterInputFocus  *focus,
+                                   const ClutterEvent *event)
+{
+  ClutterInputFocusPrivate *priv;
+
+  g_return_val_if_fail (CLUTTER_IS_INPUT_FOCUS (focus), FALSE);
+  g_return_val_if_fail (clutter_input_focus_is_focused (focus), FALSE);
+
+  priv = clutter_input_focus_get_instance_private (focus);
+
+  if (event->type == CLUTTER_IM_COMMIT)
     {
       clutter_input_focus_commit (focus, event->im.text);
       return TRUE;

clutter/clutter/clutter-input-focus.h
@@ -75,6 +75,11 @@ void clutter_input_focus_set_content_purpose (ClutterInputFocus          *focus,
 CLUTTER_EXPORT
 gboolean clutter_input_focus_filter_event (ClutterInputFocus  *focus,
                                            const ClutterEvent *event);
+
+CLUTTER_EXPORT
+gboolean clutter_input_focus_process_event (ClutterInputFocus  *focus,
+                                            const ClutterEvent *event);
+
 CLUTTER_EXPORT
 void clutter_input_focus_set_can_show_preedit (ClutterInputFocus *focus,
                                                gboolean           can_show_preedit);

clutter/clutter/clutter-text.c
@@ -3080,7 +3080,7 @@ clutter_text_event (ClutterActor *self,
        event->type == CLUTTER_IM_DELETE ||
        event->type == CLUTTER_IM_PREEDIT))
     {
-      return clutter_input_focus_filter_event (priv->input_focus, event);
+      return clutter_input_focus_process_event (priv->input_focus, event);
     }
 
   return CLUTTER_EVENT_PROPAGATE;

src/wayland/meta-wayland-seat.c
@@ -384,7 +384,7 @@ meta_wayland_seat_handle_event (MetaWaylandSeat *seat,
       break;
     case CLUTTER_KEY_PRESS:
     case CLUTTER_KEY_RELEASE:
-      if (meta_wayland_text_input_handle_event (seat->text_input, event))
+      if (meta_wayland_text_input_update (seat->text_input, event))
         return TRUE;
 
       if (meta_wayland_seat_has_keyboard (seat))

src/wayland/meta-wayland-text-input.h
@@ -41,6 +41,9 @@ gboolean meta_wayland_text_input_init (MetaWaylandCompositor *compositor);
 void meta_wayland_text_input_set_focus (MetaWaylandTextInput *text_input,
 					MetaWaylandSurface   *surface);
 
+gboolean meta_wayland_text_input_update (MetaWaylandTextInput *text_input,
+                                         const ClutterEvent   *event);
+
 gboolean meta_wayland_text_input_handle_event (MetaWaylandTextInput *text_input,
                                                const ClutterEvent   *event);
 

commit 381048cf902d8b46186119eea03f9f14e962661b

wayland: Restore IM state flushing before handling key events
The code that maybe flushed IM state before processing a key event
became ineffective at commit 7716b62fa2, since the handle_event()
method on MetaWaylandTextInput won't handle key events, only IM
events and touch/button press events causing IM state to be
committed. Basically, the events that directly change the IM state.

Move this ineffective code to the the filter_event() method handling
the key presses in order to let the IM maybe filter them, and handle
them so that any key event that is let through (both key events
previously injected by the IM, and key events that the IM chooses to
ignore) will ensure that the pending IM state is flushed before the
key event is handled and emitted to the client.

This brings back lost guarantees of orderly event emission when IMs
alternate key events and IM actions.

Fixes: 7716b62fa2 ("clutter: Separate ClutterInputFocus event processing and filtering")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3090
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3536>

src/wayland/meta-wayland-text-input.c
@@ -875,7 +875,15 @@ meta_wayland_text_input_update (MetaWaylandTextInput *text_input,
 
   if (event_type == CLUTTER_KEY_PRESS ||
       event_type == CLUTTER_KEY_RELEASE)
-    return clutter_input_focus_filter_event (text_input->input_focus, event);
+    {
+      gboolean filtered = FALSE;
+
+      filtered = clutter_input_focus_filter_event (text_input->input_focus, event);
+      if (!filtered)
+        meta_wayland_text_input_focus_flush_done (text_input->input_focus);
+
+      return filtered;
+    }
 
   return FALSE;
 }
@@ -892,11 +900,6 @@ meta_wayland_text_input_handle_event (MetaWaylandTextInput *text_input,
     return FALSE;
 
   event_type = clutter_event_type (event);
-
-  if ((event_type == CLUTTER_KEY_PRESS ||
-       event_type == CLUTTER_KEY_RELEASE) &&
-      clutter_event_get_flags (event) & CLUTTER_EVENT_FLAG_INPUT_METHOD)
-    meta_wayland_text_input_focus_flush_done (text_input->input_focus);
 
   retval = clutter_input_focus_process_event (text_input->input_focus, event);
 