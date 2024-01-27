GNOME/mutter

commit 947c636275de5d66f02ff7a5f0be8201f9fb65cc

backends/native: Swap BTN_STYLUS and BTN_STYLUS2
BTN_STYLUS is the lower one and traditionally (read: in X) maps to
middle button (2), BTN_STYLUS2 is the upper one and traditionally maps
to right button (3).

This is also what GTK does and our desktop actions too map MIDDLE to
BTN_STYLUS and RIGHT to BTN_STYLUS2.

See also gtk!6168

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3541>

src/backends/native/meta-seat-impl.c
@@ -824,12 +824,12 @@ meta_seat_impl_notify_button_in_impl (MetaSeatImpl       *seat_impl,
 
     case BTN_RIGHT:
     case BTN_STYLUS:
-      button_nr = CLUTTER_BUTTON_SECONDARY;
+      button_nr = CLUTTER_BUTTON_MIDDLE;
       break;
 
     case BTN_MIDDLE:
     case BTN_STYLUS2:
-      button_nr = CLUTTER_BUTTON_MIDDLE;
+      button_nr = CLUTTER_BUTTON_SECONDARY;
       break;
 
     case BTN_STYLUS3:

commit 3528b54378b60fdb7692dcd849c61dccfeeb805f

backends/native: Fix clutter button number assignment to pointer evcodes
Commit 947c636275 meant to swap BTN_STYLUS* buttons, not BTN_MIDDLE/RIGHT
as used by pointing devices. This was also missed during review.

Fixes: 947c636275 ("backends/native: Swap BTN_STYLUS and BTN_STYLUS2")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3550>

src/backends/native/meta-seat-impl.c
@@ -822,12 +822,12 @@ meta_seat_impl_notify_button_in_impl (MetaSeatImpl       *seat_impl,
       button_nr = CLUTTER_BUTTON_PRIMARY;
       break;
 
-    case BTN_RIGHT:
+    case BTN_MIDDLE:
     case BTN_STYLUS:
       button_nr = CLUTTER_BUTTON_MIDDLE;
       break;
 
-    case BTN_MIDDLE:
+    case BTN_RIGHT:
     case BTN_STYLUS2:
       button_nr = CLUTTER_BUTTON_SECONDARY;
       break;