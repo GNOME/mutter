0a46ca67ec27b24eae3f71a2295c4c62f8dbb51c
Bilal Elmoussaoui <belmouss@redhat.com>
2024-01-31 16:43:22 +0100

    core: Include a small subset of xcursor in-tree
    For a Wayland only build, we would like to avoid linking against
    libXcursor which on it turn, links back to some of the X11 deps.
    In order to achieve that, we include a small subset of xcursor.
    
    In case Mutter is built with X11 or with both Wayland & X11, we link
    against libXcursor and don't make use of the in-tree implementation.
    
    This patch mimics what GTK 4 do by shipping an in-tree copy of xcursor.
    Especially that libwayland-cursor does not provide an alternative to
    xcursor itself.
    
    Helps #2272
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3607>

---
+++ src/third_party/xcursor/xcursor.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2002 Keith Packard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * A slightly modified version of XCursor used with Wayland only builds.
+ */
+#pragma once
+
+typedef unsigned int XcursorPixel;
+
+typedef struct _XcursorImage {
+  unsigned int version; /* version of the image data */
+  unsigned int size; /* nominal size for matching */
+  unsigned int width; /* actual width */
+  unsigned int height; /* actual height */
+  unsigned int xhot; /* hot spot x (must be inside image) */
+  unsigned int yhot; /* hot spot y (must be inside image) */
+  unsigned int delay; /* animation delay to next frame (ms) */
+  XcursorPixel *pixels; /* pointer to pixels */
+} XcursorImage;
+
+typedef struct _XcursorImages {
+  int nimage;  /* number of images */
+  XcursorImage **images; /* array of XcursorImage pointers */
+  char *name; /* name used to load images */
+} XcursorImages;
+
+void XcursorImagesDestroy (XcursorImages *images);
+
+XcursorImages * XcursorImagesCreate (int size);
+
+XcursorImages * XcursorLibraryLoadImages (const char *library,
+                                          const char *theme,
+                                          int         size);
+
+XcursorImage * XcursorImageCreate (int width,
+                                   int height);
