GNOME/mutter

commit 551101c65cda0e8a3c3a609baef263d39a32a651

compositor-x11: Move synchronization to before-update
The synchronization must happen no matter the painting, as it in itself
might result in reported damage, making the stage actually painted. Thus
move it out of the "pre-paint" handler, to something explicitly not tied
to the painting itself - ClutterStage::before-update.

https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1285

src/compositor/meta-compositor-x11.c
@@ -39,6 +39,8 @@ struct _MetaCompositorX11
 
   Window output;
 
+  gulong before_update_handler_id;
+
   gboolean frame_has_updated_xsurfaces;
   gboolean have_x11_sync_object;
 
@@ -291,15 +293,10 @@ out:
 }
 
 static void
-meta_compositor_x11_pre_paint (MetaCompositor *compositor)
+on_before_update (ClutterStage   *stage,
+                  MetaCompositor *compositor)
 {
   MetaCompositorX11 *compositor_x11 = META_COMPOSITOR_X11 (compositor);
-  MetaCompositorClass *parent_class;
-
-  maybe_unredirect_top_window (compositor_x11);
-
-  parent_class = META_COMPOSITOR_CLASS (meta_compositor_x11_parent_class);
-  parent_class->pre_paint (compositor);
 
   if (compositor_x11->frame_has_updated_xsurfaces)
     {
@@ -330,6 +327,18 @@ meta_compositor_x11_pre_paint (MetaCompositor *compositor)
       else
         XSync (display->x11_display->xdisplay, False);
     }
+}
+
+static void
+meta_compositor_x11_pre_paint (MetaCompositor *compositor)
+{
+  MetaCompositorX11 *compositor_x11 = META_COMPOSITOR_X11 (compositor);
+  MetaCompositorClass *parent_class;
+
+  maybe_unredirect_top_window (compositor_x11);
+
+  parent_class = META_COMPOSITOR_CLASS (meta_compositor_x11_parent_class);
+  parent_class->pre_paint (compositor);
 }
 
 static void
@@ -381,15 +390,33 @@ meta_compositor_x11_new (MetaDisplay *display,
 }
 
 static void
+meta_compositor_x11_constructed (GObject *object)
+{
+  MetaCompositorX11 *compositor_x11 = META_COMPOSITOR_X11 (object);
+  MetaCompositor *compositor = META_COMPOSITOR (compositor_x11);
+  ClutterStage *stage = meta_compositor_get_stage (compositor);
+
+  compositor_x11->before_update_handler_id =
+    g_signal_connect (stage, "before-update",
+                      G_CALLBACK (on_before_update), compositor);
+
+  G_OBJECT_CLASS (meta_compositor_x11_parent_class)->constructed (object);
+}
+
+static void
 meta_compositor_x11_dispose (GObject *object)
 {
   MetaCompositorX11 *compositor_x11 = META_COMPOSITOR_X11 (object);
+  MetaCompositor *compositor = META_COMPOSITOR (compositor_x11);
+  ClutterStage *stage = meta_compositor_get_stage (compositor);
 
   if (compositor_x11->have_x11_sync_object)
     {
       meta_sync_ring_destroy ();
       compositor_x11->have_x11_sync_object = FALSE;
     }
+
+  g_clear_signal_handler (&compositor_x11->before_update_handler_id, stage);
 
   G_OBJECT_CLASS (meta_compositor_x11_parent_class)->dispose (object);
 }
@@ -405,6 +432,7 @@ meta_compositor_x11_class_init (MetaCompositorX11Class *klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
   MetaCompositorClass *compositor_class = META_COMPOSITOR_CLASS (klass);
 
+  object_class->constructed = meta_compositor_x11_constructed;
   object_class->dispose = meta_compositor_x11_dispose;
 
   compositor_class->manage = meta_compositor_x11_manage;

commit f606a4424a5afc71672566b15f56971bfb7fa4db

compositor/x11: Sync again at the end of before_paint
The existing comment tells us this is necessary:

> there may be drawing between the last damage event and the
> XDamageSubtract() that needs to be flushed as well.

But the commit message for 551101c65cda also tells us that
synchronization is necessary before-update. Assuming both are correct
then it needs to be done in both places.

I did try optimizing out the second sync to only do it if damage
arrived during the update, but that doesn't seem to be the issue.
The damage event is arriving before the update starts and it's some
secondary changes within the damage region running late that need
flushing. So this means the client is reporting damage more frequently
than the frame rate and we're ignoring the secondary damage reports
for efficiency (XDamageReportBoundingBox), which is still a good thing.

Fixes: 551101c65cda ("compositor-x11: Move synchronization to before-update")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/2880
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3590>

src/compositor/meta-compositor-x11.c
@@ -325,10 +325,7 @@ out:
 }
 
 static void
-on_before_update (ClutterStage     *stage,
-                  ClutterStageView *stage_view,
-                  ClutterFrame     *frame,
-                  MetaCompositor   *compositor)
+maybe_do_sync (MetaCompositor *compositor)
 {
   MetaCompositorX11 *compositor_x11 = META_COMPOSITOR_X11 (compositor);
 
@@ -364,6 +361,15 @@ on_before_update (ClutterStage     *stage,
 }
 
 static void
+on_before_update (ClutterStage     *stage,
+                  ClutterStageView *stage_view,
+                  ClutterFrame     *frame,
+                  MetaCompositor   *compositor)
+{
+  maybe_do_sync (compositor);
+}
+
+static void
 on_after_update (ClutterStage     *stage,
                  ClutterStageView *stage_view,
                  ClutterFrame     *frame,
@@ -391,6 +397,22 @@ meta_compositor_x11_before_paint (MetaCompositor     *compositor,
 
   parent_class = META_COMPOSITOR_CLASS (meta_compositor_x11_parent_class);
   parent_class->before_paint (compositor, compositor_view);
+
+  /* We must sync after MetaCompositor's before_paint because that's the final
+   * time XDamageSubtract may happen before painting (when it calls
+   * meta_window_actor_x11_before_paint -> handle_updates ->
+   * meta_surface_actor_x11_handle_updates). If a client was to redraw between
+   * the last damage event and XDamageSubtract, and the bounding box of the
+   * region didn't grow, then we will not receive a new damage report for it
+   * (because XDamageReportBoundingBox). Then if we haven't synchronized again
+   * and the same region doesn't change on subsequent frames, we have lost some
+   * part of the update from the client. So to ensure the correct pixels get
+   * composited we must sync at least once between XDamageSubtract and
+   * compositing, which is here. More related documentation can be found in
+   * maybe_do_sync.
+   */
+
+  maybe_do_sync (compositor);
 }
 
 static void