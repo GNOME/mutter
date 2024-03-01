GNOME/mutter

commit d48129f5ee5e49a8743b9fa1d8230f9fc99467ef

wayland: Fix pointer cursor during Wayland grabs
With Wayland popups and drag-and-drop using grabs, we
should let window cursors prevail when there is one
in effect.

Also, resort always to the actor as known by the
stage. This fixes the cursor lookup right after crossing
events induced by grabs, e.g. right clicking on the
gtk4-demo textview without motion would keep the I-beam
cursor, now results on the right actor/cursor for the
menu being picked.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3420>

src/backends/meta-cursor-tracker.c
@@ -109,12 +109,9 @@ update_displayed_cursor (MetaCursorTracker *tracker)
 {
   MetaCursorTrackerPrivate *priv =
     meta_cursor_tracker_get_instance_private (tracker);
-  MetaContext *context = meta_backend_get_context (priv->backend);
-  MetaDisplay *display = meta_context_get_display (context);
   MetaCursorSprite *cursor = NULL;
 
-  if (display && meta_display_windows_are_interactable (display) &&
-      priv->has_window_cursor)
+  if (priv->has_window_cursor)
     cursor = priv->window_cursor;
   else
     cursor = priv->root_cursor;

src/wayland/meta-wayland-pointer.c
@@ -560,20 +560,12 @@ repick_for_event (MetaWaylandPointer *pointer,
 {
   ClutterActor *actor;
   MetaWaylandSurface *surface;
+  MetaBackend *backend = backend_from_pointer (pointer);
+  ClutterStage *stage = CLUTTER_STAGE (meta_backend_get_stage (backend));
 
-  if (clutter_event_type (for_event) == CLUTTER_LEAVE)
-    {
-      actor = clutter_event_get_related (for_event);
-    }
-  else
-    {
-      MetaBackend *backend = backend_from_pointer (pointer);
-      ClutterStage *stage = CLUTTER_STAGE (meta_backend_get_stage (backend));
-
-      actor = clutter_stage_get_device_actor (stage,
-                                              clutter_event_get_device (for_event),
-                                              clutter_event_get_event_sequence (for_event));
-    }
+  actor = clutter_stage_get_device_actor (stage,
+                                          clutter_event_get_device (for_event),
+                                          clutter_event_get_event_sequence (for_event));
 
   if (META_IS_SURFACE_ACTOR_WAYLAND (actor))
     {

commit 525ed1166c498ea031c0819d3532c85ad5926343

wayland/pointer: Unset current surface during window drags
Commit d48129f5ee broke the displaying of our own drag cursor during window
drags, as now the window cursor is always used, even during stage grabs
(window grabs are just a kind of stage grab).

To fix it, while not regressing on the intention of the other commit, unset
the MetaWaylandPointer surface in case a window drag is active (instead of
all kinds of grabs) by checking via meta_display_is_grab().

Fixes: d48129f5ee ("wayland: Fix pointer cursor during Wayland grabs")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3316
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3624>

src/backends/meta-cursor-tracker.c
@@ -109,9 +109,11 @@ update_displayed_cursor (MetaCursorTracker *tracker)
 {
   MetaCursorTrackerPrivate *priv =
     meta_cursor_tracker_get_instance_private (tracker);
+  MetaContext *context = meta_backend_get_context (priv->backend);
+  MetaDisplay *display = meta_context_get_display (context);
   MetaCursorSprite *cursor = NULL;
 
-  if (priv->has_window_cursor)
+  if (display && !meta_display_is_grabbed (display) && priv->has_window_cursor)
     cursor = priv->window_cursor;
   else
     cursor = priv->root_cursor;