GNOME/mutter

commit 627ab247210b6ec3e186c27091c8a8926323a942

surface-actor: Add meta_surface_actor_schedule_update()
This function allows scheduling a stage update in the context of a
surface actor and emit the "update-scheduled" signal. This signal is
similar to "repaint-scheduled", but can be used to be notified of
updates that do not necessarily result in a repaint.

With variable refresh rate, any update potentially affecting the
frame pacing of a surface actor needs to be handled differently
depending on whether that surface actor drives the refresh rate or
not.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1154>

src/compositor/meta-surface-actor.c
@@ -62,6 +62,7 @@ G_DEFINE_ABSTRACT_TYPE_WITH_CODE (MetaSurfaceActor, meta_surface_actor, CLUTTER_
 enum
 {
   REPAINT_SCHEDULED,
+  UPDATE_SCHEDULED,
   SIZE_CHANGED,
 
   LAST_SIGNAL,
@@ -293,6 +294,14 @@ meta_surface_actor_class_init (MetaSurfaceActorClass *klass)
                                              NULL, NULL, NULL,
                                              G_TYPE_NONE, 0);
 
+  signals[UPDATE_SCHEDULED] = g_signal_new ("update-scheduled",
+                                            G_TYPE_FROM_CLASS (object_class),
+                                            G_SIGNAL_RUN_LAST,
+                                            0,
+                                            NULL, NULL, NULL,
+                                            G_TYPE_NONE, 0);
+
+
   signals[SIZE_CHANGED] = g_signal_new ("size-changed",
                                         G_TYPE_FROM_CLASS (object_class),
                                         G_SIGNAL_RUN_LAST,
@@ -391,6 +400,17 @@ meta_surface_actor_get_texture (MetaSurfaceActor *self)
     meta_surface_actor_get_instance_private (self);
 
   return priv->texture;
+}
+
+void
+meta_surface_actor_schedule_update (MetaSurfaceActor *self)
+{
+  ClutterStage *stage =
+    CLUTTER_STAGE (clutter_actor_get_stage (CLUTTER_ACTOR (self)));
+
+  clutter_stage_schedule_update (stage);
+
+  g_signal_emit (self, signals[UPDATE_SCHEDULED], 0);
 }
 
 void

src/compositor/meta-surface-actor.h
@@ -49,6 +49,8 @@ void meta_surface_actor_set_opaque_region (MetaSurfaceActor *self,
                                            MtkRegion        *region);
 MtkRegion * meta_surface_actor_get_opaque_region (MetaSurfaceActor *self);
 
+void meta_surface_actor_schedule_update (MetaSurfaceActor *self);
+
 void meta_surface_actor_process_damage (MetaSurfaceActor *actor,
                                         int x, int y, int width, int height);
 

commit e8b43d3b1bb19ca487ef15a6668c0c436eb56df5

surface-actor: Add NULL check for stage
In some cases Wayland surface actors are not decendents of the stage,
notably subsurfaces without a parent surface. The code changes in the
mentioned commits dropped the previously existing check for that - readd
it.

Fixes: 627ab24721 (surface-actor: Add meta_surface_actor_schedule_update())
Fixes: 7eb99ddef7 (wayland/surface: Schedule updates in the context of the surface actor)

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3323
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3633>

src/compositor/meta-surface-actor.c
@@ -405,8 +405,11 @@ meta_surface_actor_get_texture (MetaSurfaceActor *self)
 void
 meta_surface_actor_schedule_update (MetaSurfaceActor *self)
 {
-  ClutterStage *stage =
-    CLUTTER_STAGE (clutter_actor_get_stage (CLUTTER_ACTOR (self)));
+  ClutterStage *stage;
+
+  stage = CLUTTER_STAGE (clutter_actor_get_stage (CLUTTER_ACTOR (self)));
+  if (!stage)
+    return;
 
   clutter_stage_schedule_update (stage);
 