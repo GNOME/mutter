GNOME/gnome-shell

commit 8f3da9f68a10ff142c5150f06236368449ab2512

Use meta_window_set_user_time for setting user time consistently
This is a followup to making user-time a GObject property, this
way we get notifications.

src/core/display.c
@@ -5111,9 +5111,9 @@ sanity_check_timestamps (MetaDisplay *display,
               meta_warning ("%s appears to be one of the offending windows "
                             "with a timestamp of %u.  Working around...\n",
                             window->desc, window->net_wm_user_time);
-              window->net_wm_user_time = timestamp;
+              meta_window_set_user_time (window, timestamp);
             }
-          
+
           tmp = tmp->next;
         }
 

GNOME/mutter

commit fb890ead963466bab3c75a2be5dab74911de6a79

display: Force window user time update to happen
`meta_window_set_user_time()` will not update the window's
user time if it timestamp in the argument is before the
currently saved timestamp. However, when trying to work around
problematic timestamps, this is exactly what needs to be done.

So force the update to happen by setting the "is user time set?"
flag to false.

Fixes: 8f3da9f68a10ff ("Use meta_window_set_user_time for setting user time consistently")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3027>

src/core/display.c
@@ -2494,6 +2494,7 @@ meta_display_sanity_check_timestamps (MetaDisplay *display,
               meta_warning ("%s appears to be one of the offending windows "
                             "with a timestamp of %u.  Working around...",
                             window->desc, window->net_wm_user_time);
+              window->net_wm_user_time_set = FALSE;
               meta_window_set_user_time (window, timestamp);
             }
 