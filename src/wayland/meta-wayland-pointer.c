GNOME/mutter

commit 93a9e7f3f4d09b55796a851e308d1f599cf34a7c

core: Move code ignoring events on windows with modals to Wayland
This is Wayland specific code, handle it directly in MetaWaylandPointer.
This also fixes issues with the crossing event itself managing to reach
the window occluded by modals.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3420>

src/wayland/meta-wayland-pointer.c
@@ -573,6 +573,9 @@ repick_for_event (MetaWaylandPointer *pointer,
         META_SURFACE_ACTOR_WAYLAND (actor);
 
       surface = meta_surface_actor_wayland_get_surface (actor_wayland);
+
+      if (meta_window_has_modals (meta_wayland_surface_get_window (surface)))
+        surface = NULL;
     }
   else
     {

src/core/events.c
@@ -475,7 +475,6 @@ meta_display_handle_event (MetaDisplay        *display,
        *   trigger ::captured-event handlers along the way.
        */
       bypass_clutter = !IS_GESTURE_EVENT (event_type);
-      bypass_wayland = meta_window_has_modals (window);
 
       meta_window_handle_ungrabbed_event (window, event);
 

commit 1cfc1aa92bfaa7e7b160c4b5c7f30cd7fe48e054

wayland: Check surface before looking up modals
Near window destruction, there might be cases where the surface
actor does no longer have a surface, yet it's still in the stage
and eligible for picking. In that situation looking for modal
dialogs attached to this surface will evidently fail, so avoid
this check on a NULL surface.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3332
Fixes: 93a9e7f3f ("core: Move code ignoring events on windows with modals to Wayland")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3660>

src/wayland/meta-wayland-pointer.c
@@ -603,7 +603,7 @@ repick_for_event (MetaWaylandPointer *pointer,
 
       surface = meta_surface_actor_wayland_get_surface (actor_wayland);
 
-      if (meta_window_has_modals (meta_wayland_surface_get_window (surface)))
+      if (surface && meta_window_has_modals (meta_wayland_surface_get_window (surface)))
         surface = NULL;
     }
   else