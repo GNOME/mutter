57e16cf010d64776ce44ad92d31e71ddab13640d
Jonas Ådahl <jadahl@gmail.com>
2024-04-14 22:11:14 +0200

    window/wayland: Constrain sporadic client resizes immediately
    When a client resizes on its own, make sure the new size is passed
    through the window constraints machinery directly, to trigger any
    potential window management rule that might apply.
    
    Fix a couple of tests to make use of this behavior by introducing a new
    'wait_size' command that waits until a window has been resized to a
    expected size.
    
    This replaces the fix introduced in 0e736af301 ("window: Ensure
    constraints after a Wayland client resize").
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3700>

--- src/tests/test-runner.c
+++ src/tests/test-runner.c
@@ -840,6 +840,35 @@ test_case_do (TestCase    *test,
       if (!test_case_wait (test, error))
         return FALSE;
     }
+  else if (strcmp (argv[0], "wait_size") == 0)
+    {
+      MetaTestClient *client;
+      const char *window_id;
+      MetaWindow *window;
+      int width, height;
+
+      if (argc != 4)
+        BAD_COMMAND("usage: %s <client-id>/<window-id> <width> <height>", argv[0]);
+
+      if (!test_case_parse_window_id (test, argv[1], &client, &window_id, error))
+        return FALSE;
+
+      window = meta_test_client_find_window (client, window_id, error);
+
+      width = atoi (argv[2]);
+      height = atoi (argv[3]);
+
+      while (TRUE)
+        {
+          MtkRectangle rect;
+
+          meta_window_get_frame_rect (window, &rect);
+          if (rect.width == width && rect.height == height)
+            break;
+
+          g_main_context_iteration (NULL, TRUE);
+        }
+    }
   else if (strcmp (argv[0], "dispatch") == 0)
     {
       if (argc != 1)
