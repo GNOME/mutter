GNOME/mutter

commit 40950f99b3a7c5bbabd2942dee95ed91587610b1

drm-buffer-gbm: Do not call ensure_fb_id from lock_front
When preparing a frame for scanout on a secondary GPU, calling
meta_drm_buffer_gbm_ensure_fb_id for the primary GPU device is
unnecessary and potentially harmful.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3389
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3674>

src/backends/native/meta-onscreen-native.c
@@ -1372,6 +1372,17 @@ meta_onscreen_native_swap_buffers_with_damage (CoglOnscreen  *onscreen,
 #endif
     }
 
+  if (!meta_drm_buffer_ensure_fb_id (onscreen_native->gbm.next_fb, &error))
+    {
+      g_warning ("Failed to ensure KMS FB ID on %s: %s",
+                 meta_device_file_get_path (render_device_file),
+                 error->message);
+
+      frame_info->flags |= COGL_FRAME_INFO_FLAG_SYMBOLIC;
+      meta_onscreen_native_notify_frame_complete (onscreen);
+      return;
+    }
+
   /*
    * If we changed EGL context, cogl will have the wrong idea about what is
    * current, making it fail to set it when it needs to. Avoid that by making

src/backends/native/meta-drm-buffer-gbm.c
@@ -186,7 +186,7 @@ lock_front_buffer (MetaDrmBufferGbm  *buffer_gbm,
       return FALSE;
     }
 
-  return meta_drm_buffer_gbm_ensure_fb_id (META_DRM_BUFFER (buffer_gbm), error);
+  return TRUE;
 }
 
 MetaDrmBufferGbm *

commit d53da38198947754bd7e9cc264d6073caeb2447b

onscreen/native: Avoid dereferencing gbm.next_fb when not in MODE_GBM
Such as with MODE_EGL_DEVICE.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3436
Fixes: 40950f99b3 ("drm-buffer-gbm: Do not call ensure_fb_id from...")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3708>

src/backends/native/meta-onscreen-native.c
@@ -1384,6 +1384,17 @@ meta_onscreen_native_swap_buffers_with_damage (CoglOnscreen  *onscreen,
         g_set_object (&onscreen_native->gbm.next_fb, secondary_gpu_fb);
       else
         g_set_object (&onscreen_native->gbm.next_fb, primary_gpu_fb);
+
+      if (!meta_drm_buffer_ensure_fb_id (onscreen_native->gbm.next_fb, &error))
+        {
+          g_warning ("Failed to ensure KMS FB ID on %s: %s",
+                     meta_device_file_get_path (render_device_file),
+                     error->message);
+
+          frame_info->flags |= COGL_FRAME_INFO_FLAG_SYMBOLIC;
+          meta_onscreen_native_notify_frame_complete (onscreen);
+          return;
+        }
       break;
     case META_RENDERER_NATIVE_MODE_SURFACELESS:
       break;
@@ -1391,17 +1402,6 @@ meta_onscreen_native_swap_buffers_with_damage (CoglOnscreen  *onscreen,
     case META_RENDERER_NATIVE_MODE_EGL_DEVICE:
       break;
 #endif
-    }
-
-  if (!meta_drm_buffer_ensure_fb_id (onscreen_native->gbm.next_fb, &error))
-    {
-      g_warning ("Failed to ensure KMS FB ID on %s: %s",
-                 meta_device_file_get_path (render_device_file),
-                 error->message);
-
-      frame_info->flags |= COGL_FRAME_INFO_FLAG_SYMBOLIC;
-      meta_onscreen_native_notify_frame_complete (onscreen);
-      return;
     }
 
   /*