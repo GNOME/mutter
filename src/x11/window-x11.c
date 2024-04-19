GNOME/mutter

commit d991961ae2a5c8cf2e58ff1072239f4902b0f767

x11: Use input region from frame window for decorated windows
Previously the input region was ignored for decorated windows, which
since the introduction of the frames client meant that the entire shadow
region of the frame window was considered interactive.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/2706
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3031>
(cherry picked from commit 6274bb3518dfe39ea223a3220ea15976879faf24)

src/x11/window-x11.c
@@ -2118,6 +2118,10 @@ meta_window_x11_constructed (GObject *object)
   window->hidden = FALSE;
   priv->border_width = attrs.border_width;
 
+  g_signal_connect (window, "notify::decorated",
+                    G_CALLBACK (meta_window_x11_update_input_region),
+                    window);
+
   G_OBJECT_CLASS (meta_window_x11_parent_class)->constructed (object);
 }
 
@@ -2428,16 +2432,21 @@ meta_window_x11_update_input_region (MetaWindow *window)
   cairo_region_t *region = NULL;
   MetaWindowX11 *window_x11 = META_WINDOW_X11 (window);
   MetaWindowX11Private *priv = meta_window_x11_get_instance_private (window_x11);
+  Window xwindow;
 
-  /* Decorated windows don't have an input region, because
-     we don't shape the frame to match the client windows
-     (so the events are blocked by the frame anyway)
-  */
   if (window->decorated)
     {
-      if (window->input_region)
-        meta_window_set_input_region (window, NULL);
-      return;
+      if (!window->frame)
+        {
+          if (window->input_region)
+            meta_window_set_input_region (window, NULL);
+          return;
+        }
+      xwindow = window->frame->xwindow;
+    }
+  else
+    {
+      xwindow = window->xwindow;
     }
 
   if (META_X11_DISPLAY_HAS_SHAPE (x11_display))
@@ -2449,7 +2458,7 @@ meta_window_x11_update_input_region (MetaWindow *window)
 
       meta_x11_error_trap_push (x11_display);
       rects = XShapeGetRectangles (x11_display->xdisplay,
-                                   window->xwindow,
+                                   xwindow,
                                    ShapeInput,
                                    &n_rects,
                                    &ordering);
@@ -2503,8 +2512,8 @@ meta_window_x11_update_input_region (MetaWindow *window)
 
       client_area.x = 0;
       client_area.y = 0;
-      client_area.width = priv->client_rect.width;
-      client_area.height = priv->client_rect.height;
+      client_area.width = window->buffer_rect.width;
+      client_area.height = window->buffer_rect.height;
 
       /* The shape we get back from the client may have coordinates
        * outside of the frame. The X SHAPE Extension requires that

src/core/frame.c
@@ -34,6 +34,7 @@
 #include "x11/window-props.h"
 
 #include <X11/Xatom.h>
+#include <X11/extensions/shape.h>
 
 #define EVENT_MASK (SubstructureRedirectMask |                     \
                     StructureNotifyMask | SubstructureNotifyMask | \
@@ -106,6 +107,9 @@ meta_window_set_frame_xwindow (MetaWindow *window,
   attrs.event_mask = EVENT_MASK;
   XChangeWindowAttributes (x11_display->xdisplay,
 			   frame->xwindow, CWEventMask, &attrs);
+
+  if (META_X11_DISPLAY_HAS_SHAPE (x11_display))
+    XShapeSelectInput (x11_display->xdisplay, frame->xwindow, ShapeNotifyMask);
 
   meta_x11_display_register_x_window (x11_display, &frame->xwindow, window);
 
@@ -214,6 +218,9 @@ meta_window_destroy_frame (MetaWindow *window)
                        window->frame->rect.y + borders.invisible.top);
       window->reparents_pending += 1;
     }
+
+  if (META_X11_DISPLAY_HAS_SHAPE (x11_display))
+    XShapeSelectInput (x11_display->xdisplay, frame->xwindow, NoEventMask);
 
   XDeleteProperty (x11_display->xdisplay,
                    window->xwindow,

commit 6bd920b355d12395dbf00b99374fc1f23783caaf

x11/window: Use correct bounding rect to determine NULL input region
When updating the input region we check whether the input shape reported
by XShape matches the bounding region of a window to determine when it
was not set by the client. We would then use a NULL input region instead
which always matches the full size of the window. The code however was
using the client rect for this comparison, which does not include the
window frame. Since d991961a the frame is considered part of the input
region.

This meant that for SSD windows where the input region would match the
bounding region, we would not detect that and fail to set the input
region to NULL, but instead set it to the reported input shape.

Usually this would not be the case due to the GTK frame window having
shadows and a resize region, but in the presence of an issue that causes
GTK to wrongly detect _GTK_FRAME_EXTENTS as not supported, GTK would not
draw shadows or set an input shape. And due to GTK not updating its
input shape, there would be no further calls to
meta_window_x11_update_input_region() after the initial one.
The input region would therefore remain at the fixed size from the
initial call. This was causing windows to become click-through outside
of the region corresponding to their initial size after being resized.

Fixes: d991961ae ("x11: Use input region from frame window for decorated windows")
Related: https://gitlab.gnome.org/GNOME/gtk/-/issues/6558
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3404
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3697>

src/x11/window-x11.c
@@ -2458,8 +2458,8 @@ meta_window_x11_update_input_region (MetaWindow *window)
       else if (n_rects == 1 &&
                (rects[0].x == 0 &&
                 rects[0].y == 0 &&
-                rects[0].width == priv->client_rect.width &&
-                rects[0].height == priv->client_rect.height))
+                rects[0].width == window->buffer_rect.width &&
+                rects[0].height == window->buffer_rect.height))
         {
           /* This is the bounding region case. Keep the
            * region as NULL. */