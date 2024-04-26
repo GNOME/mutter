GNOME/mutter

commit 5dfed8a4319e8b7bac78fb9bf67f69254ddb0b52

wayland: Preserve serial for all pressed keys
The current code checking keyboard serials for popup/grab
validation is a bit simple, tracking one key press exclusively.
This may break expectations if a client uses a serial
corresponding to a previous key that is still pressed.

Keep track of the serials corresponding to all pressed keys,
and ensure these are reset across focus changes, since the
validity of those serials is already outdated. The code does
still keep track of a single (last) key release serial, since
the validity lifetime is somewhat underdefined with those if
we keep track of multiple keys simultaneously.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3267
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3644>

src/wayland/meta-wayland-keyboard.c
@@ -74,13 +74,9 @@ struct _MetaWaylandKeyboard
   struct wl_listener focus_surface_listener;
   uint32_t focus_serial;
 
-  uint32_t key_down_keycode;
-  uint32_t key_down_serial;
-
-  uint32_t key_up_keycode;
-  uint32_t key_up_serial;
-
   struct wl_array pressed_keys;
+  GHashTable *key_down_serials;
+  uint32_t last_key_up_serial;
 
   MetaWaylandXkbInfo xkb_info;
   enum xkb_state_component mods_changed;
@@ -285,13 +281,16 @@ meta_wayland_keyboard_broadcast_key (MetaWaylandKeyboard *keyboard,
 
       if (state)
         {
-          keyboard->key_down_serial = serial;
-          keyboard->key_down_keycode = key;
+          g_hash_table_insert (keyboard->key_down_serials,
+                               GUINT_TO_POINTER (key),
+                               GUINT_TO_POINTER (serial));
+          keyboard->last_key_up_serial = 0;
         }
       else
         {
-          keyboard->key_up_serial = serial;
-          keyboard->key_up_keycode = key;
+          g_hash_table_remove (keyboard->key_down_serials,
+                               GUINT_TO_POINTER (key));
+          keyboard->last_key_up_serial = serial;
         }
 
       wl_resource_for_each (resource, &keyboard->focus_resource_list)
@@ -548,6 +547,8 @@ meta_wayland_keyboard_enable (MetaWaylandKeyboard *keyboard)
 
   wl_array_init (&keyboard->pressed_keys);
 
+  keyboard->key_down_serials = g_hash_table_new (NULL, NULL);
+
   g_signal_connect (keyboard->settings, "changed",
                     G_CALLBACK (settings_changed), keyboard);
 
@@ -587,6 +588,9 @@ meta_wayland_keyboard_disable (MetaWaylandKeyboard *keyboard)
   wl_list_init (&keyboard->resource_list);
   wl_list_remove (&keyboard->focus_resource_list);
   wl_list_init (&keyboard->focus_resource_list);
+
+  g_clear_pointer (&keyboard->key_down_serials, g_hash_table_unref);
+  keyboard->last_key_up_serial = 0;
 
   wl_array_release (&keyboard->pressed_keys);
 
@@ -791,6 +795,8 @@ meta_wayland_keyboard_set_focus (MetaWaylandKeyboard *keyboard,
 
       wl_list_remove (&keyboard->focus_surface_listener.link);
       keyboard->focus_surface = NULL;
+      g_hash_table_remove_all (keyboard->key_down_serials);
+      keyboard->last_key_up_serial = 0;
     }
 
   if (surface != NULL)
@@ -891,9 +897,20 @@ gboolean
 meta_wayland_keyboard_can_popup (MetaWaylandKeyboard *keyboard,
                                  uint32_t             serial)
 {
-  return (keyboard->key_down_serial == serial ||
-          ((keyboard->key_down_keycode == keyboard->key_up_keycode) &&
-           keyboard->key_up_serial == serial));
+  GHashTableIter iter;
+  gpointer value;
+
+  if (keyboard->last_key_up_serial == serial)
+    return TRUE;
+
+  g_hash_table_iter_init (&iter, keyboard->key_down_serials);
+  while (g_hash_table_iter_next (&iter, NULL, &value))
+    {
+      if (GPOINTER_TO_UINT (value) == serial)
+        return TRUE;
+    }
+
+  return FALSE;
 }
 
 static void

commit 243890a6886262717447afe0c6d1c329e86643c9

wayland: Do not forget immediately of key press serials on key release
Prior to commit 5dfed8a431, the MetaWaylandKeyboard would always remember
the last key press serial, and consider it valid after the key was released,
as long as no other key presses/releases happened in between.

That commit improved things so that MetaWaylandKeyboard can track multiple
keys being pressed simultaneously, but also changed so that the serial for
a key press is immediately forgotten after the key press event was received.
This may break in situations like testing or keyboard macros where key
press and release is handled in a quick sucession, so the client reaction
to the key press (e.g. popping up a menu) might arrive too late.

Add a sort of spiritual successor to this handling, and make keyboard
press serials corresponding to the last key up forgotten at the next
key press/release received.

Fixes: 5dfed8a431 ("wayland: Preserve serial for all pressed keys")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3458
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3721>

src/wayland/meta-wayland-keyboard.c
@@ -77,6 +77,7 @@ struct _MetaWaylandKeyboard
   struct wl_array pressed_keys;
   GHashTable *key_down_serials;
   uint32_t last_key_up_serial;
+  uint32_t last_key_up;
 
   MetaWaylandXkbInfo xkb_info;
   enum xkb_state_component mods_changed;
@@ -279,6 +280,13 @@ meta_wayland_keyboard_broadcast_key (MetaWaylandKeyboard *keyboard,
 
       serial = meta_wayland_input_device_next_serial (input_device);
 
+      if (keyboard->last_key_up)
+        {
+          g_hash_table_remove (keyboard->key_down_serials,
+                               GUINT_TO_POINTER (keyboard->last_key_up));
+          keyboard->last_key_up = 0;
+        }
+
       if (state)
         {
           g_hash_table_insert (keyboard->key_down_serials,
@@ -288,9 +296,8 @@ meta_wayland_keyboard_broadcast_key (MetaWaylandKeyboard *keyboard,
         }
       else
         {
-          g_hash_table_remove (keyboard->key_down_serials,
-                               GUINT_TO_POINTER (key));
           keyboard->last_key_up_serial = serial;
+          keyboard->last_key_up = key;
         }
 
       wl_resource_for_each (resource, &keyboard->focus_resource_list)