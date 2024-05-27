73c65007d50e12c4a428e5e6d751d37f73bbfae5
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-05-27 21:12:44 +0200

    backends/stage-impl: Clamp the redraw clips to the framebuffer size
    When computing the redraw clips rectangles we were using graphene
    extents rounding (via "grow" Mtk rounding strategy) to be sure that
    each area is properly covering the unscaled space.
    
    However graphene's extents rounding has not any concept of maximum
    allocation space and thus it may lead to generate rectangles that don't
    fully fit in the framebuffer area.
    
    Assuming of having a framebuffer 3840x2160 scaled @ 2,2018349170684814453125
    
    An example is this rounding rectangle:
      969x1734 at 12x3 (in the framebuffer scaled space)
    
    With the current code it becomes once re-scaled:
      2135x3819 at 26x6 (in the framebuffer unscaled (e.g. monitor) space
    
    And clearly this rectangle doesn't properly fit in the framebuffer,
    since its x2 would be positioned at 2135+26 (=2161, so a pixel outside).
    
    This is not something that is generally problematic for the receiver
    code, since most KMS drivers handle this properly (even though it's still
    conceptually wrong to damage an area bigger than the plane size), but it
    becomes a problem once such rectangle also get transformed, because due
    to the nature of meta_rectangle_transform() the rectangle will also be
    offset.
    
    Following the previous example, applying a 90° rotation, it will become:
      3819x2135 at 6x-1 (in the monitor space)
    
    And now this is really a problem, because we're ending up sending to the
    driver a swap region with offscreen coordinates, and that's something that
    most driver do not really support, causing the KMS commit to fail with:
    
      Failed to post KMS update: drmModeAtomicCommit: Invalid argument
    
    So, when scaling the regions, also clamp the rectangles so that they
    will fit into the framebuffer area.
    
    In the scale_offset_and_clamp_region() case we also need to rescale the
    framebuffer go to back into framebuffer space.
    
    Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3509
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5203>

--- src/backends/meta-stage-impl.c
+++ src/backends/meta-stage-impl.c
@@ -324,10 +324,12 @@ static MtkRegion *
 offset_scale_and_clamp_region (const MtkRegion *region,
                                int              offset_x,
                                int              offset_y,
-                               float            scale)
+                               float            scale,
+                               graphene_size_t *max_size)
 {
   int n_rects, i;
   MtkRectangle *rects;
+  graphene_rect_t fb_rect;
   g_autofree MtkRectangle *freeme = NULL;
 
   n_rects = mtk_region_num_rectangles (region);
@@ -340,6 +342,8 @@ offset_scale_and_clamp_region (const MtkRegion *region,
   else
     rects = freeme = g_new (MtkRectangle, n_rects);
 
+  fb_rect = GRAPHENE_RECT_INIT (0, 0, max_size->width, max_size->height);
+
   for (i = 0; i < n_rects; i++)
     {
       MtkRectangle *rect = &rects[i];
@@ -350,6 +354,7 @@ offset_scale_and_clamp_region (const MtkRegion *region,
       tmp = mtk_rectangle_to_graphene_rect (rect);
       graphene_rect_offset (&tmp, offset_x, offset_y);
       graphene_rect_scale (&tmp, scale, scale, &tmp);
+      graphene_rect_intersection (&tmp, &fb_rect, &tmp);
       mtk_rectangle_from_graphene_rect (&tmp, MTK_ROUNDING_STRATEGY_GROW,
                                         rect);
     }
@@ -361,10 +366,12 @@ static MtkRegion *
 scale_offset_and_clamp_region (const MtkRegion *region,
                                float            scale,
                                int              offset_x,
-                               int              offset_y)
+                               int              offset_y,
+                               graphene_size_t *max_size)
 {
   int n_rects, i;
   MtkRectangle *rects;
+  graphene_rect_t fb_rect;
   g_autofree MtkRectangle *freeme = NULL;
 
   n_rects = mtk_region_num_rectangles (region);
@@ -377,6 +384,10 @@ scale_offset_and_clamp_region (const MtkRegion *region,
   else
     rects = freeme = g_new (MtkRectangle, n_rects);
 
+  fb_rect = GRAPHENE_RECT_INIT (0, 0, max_size->width, max_size->height);
+  graphene_rect_scale (&fb_rect, scale, scale, &fb_rect);
+  graphene_rect_offset (&fb_rect, offset_x, offset_y);
+
   for (i = 0; i < n_rects; i++)
     {
       MtkRectangle *rect = &rects[i];
@@ -387,6 +398,7 @@ scale_offset_and_clamp_region (const MtkRegion *region,
       tmp = mtk_rectangle_to_graphene_rect (rect);
       graphene_rect_scale (&tmp, scale, scale, &tmp);
       graphene_rect_offset (&tmp, offset_x, offset_y);
+      graphene_rect_intersection (&tmp, &fb_rect, &tmp);
       mtk_rectangle_from_graphene_rect (&tmp,
                                         MTK_ROUNDING_STRATEGY_GROW,
                                         rect);
@@ -556,7 +568,11 @@ meta_stage_impl_redraw_view_primary (MetaStageImpl    *stage_impl,
       fb_clip_region = offset_scale_and_clamp_region (redraw_clip,
                                                       -view_rect.x,
                                                       -view_rect.y,
-                                                      fb_scale);
+                                                      fb_scale,
+                                                      &(graphene_size_t) {
+                                                        .width = fb_width,
+                                                        .height = fb_height,
+                                                      });
 
       if (G_UNLIKELY (paint_debug_flags & CLUTTER_DEBUG_PAINT_DAMAGE_REGION))
         {
@@ -564,7 +580,11 @@ meta_stage_impl_redraw_view_primary (MetaStageImpl    *stage_impl,
             scale_offset_and_clamp_region (fb_clip_region,
                                            1.0f / fb_scale,
                                            view_rect.x,
-                                           view_rect.y);
+                                           view_rect.y,
+                                           &(graphene_size_t) {
+                                            .width = fb_width,
+                                            .height = fb_height,
+                                           });
         }
     }
   else
@@ -645,7 +665,11 @@ meta_stage_impl_redraw_view_primary (MetaStageImpl    *stage_impl,
       redraw_clip = scale_offset_and_clamp_region (fb_clip_region,
                                                    1.0f / fb_scale,
                                                    view_rect.x,
-                                                   view_rect.y);
+                                                   view_rect.y,
+                                                   &(graphene_size_t) {
+                                                    .width = fb_width,
+                                                    .height = fb_height,
+                                                   });
     }
 
   if (paint_debug_flags & CLUTTER_DEBUG_PAINT_DAMAGE_REGION)
@@ -712,7 +736,11 @@ meta_stage_impl_redraw_view_primary (MetaStageImpl    *stage_impl,
         scale_offset_and_clamp_region (swap_region,
                                        1.0f / fb_scale,
                                        view_rect.x,
-                                       view_rect.y);
+                                       view_rect.y,
+                                       &(graphene_size_t) {
+                                        .width = fb_width,
+                                        .height = fb_height,
+                                       });
 
       mtk_region_subtract (swap_region_in_stage_space, queued_redraw_clip);
 
