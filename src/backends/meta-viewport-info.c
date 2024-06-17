a48a149c6145592395dc3647b4b86da67f33f70d
Carlos Garnacho <carlosg@gnome.org>
2024-06-17 20:33:37 +0200

    backends: Use floating point checks in MetaViewportInfo
    In these checks, it's an important detail to preserve subpixel information
    in order to correctly determine whether the coordinates are inside or
    outside a view.
    
    Otherwise, small enough motion towards the left/top might get rounded
    to 0, be seen as "inside the view", and the pointer coordinates be allowed
    to escape the viewport constraints.
    
    This was figured out by Pascal Nowack before me, with a difference of
    minutes. Credit where credit is due.
    
    Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3530
    Fixes: 6c972546f1 ("mtk: Add Rectangle.contains_point")
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3820>

--- src/backends/meta-viewport-info.c
+++ src/backends/meta-viewport-info.c
@@ -100,7 +100,7 @@ meta_viewport_info_get_view_at (MetaViewportInfo *viewport_info,
     {
       ViewInfo *info = &g_array_index (viewport_info->views, ViewInfo, i);
 
-      if (mtk_rectangle_contains_point (&info->rect, x, y))
+      if (mtk_rectangle_contains_pointf (&info->rect, x, y))
         return i;
     }
 
