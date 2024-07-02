GNOME/mutter

commit b9ba34ac6f347d5e10805b627688158f488334ca

wayland/activation: Fix signal callback signature
Parameter notify signals have a GParamSpec as second parameter.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3523
Fixes: c166b3fc5 ("wayland/activation: Apply xdg-activation request when window gets mapped")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3798>

src/wayland/meta-wayland-activation.c
@@ -378,8 +378,9 @@ maybe_activate (MetaWaylandActivation *activation,
 }
 
 static void
-complete_pending_activate (MetaWaylandActivation *activation,
-                           MetaWindow            *window)
+complete_pending_activate (MetaWindow            *window,
+                           GParamSpec            *pspec,
+                           MetaWaylandActivation *activation)
 {
   g_autofree char *token_str = NULL;
 
@@ -409,12 +410,12 @@ activation_activate (struct wl_client   *client,
   g_signal_handlers_disconnect_by_func (window,
                                         complete_pending_activate,
                                         activation);
-  g_signal_connect_swapped (window, "notify::mapped",
-                            G_CALLBACK (complete_pending_activate),
-                            activation);
-  g_signal_connect_swapped (window, "unmanaged",
-                            G_CALLBACK (complete_pending_activate),
-                            activation);
+  g_signal_connect (window, "notify::mapped",
+                    G_CALLBACK (complete_pending_activate),
+                    activation);
+  g_signal_connect (window, "unmanaged",
+                    G_CALLBACK (complete_pending_activate),
+                    activation);
   g_hash_table_insert (activation->pending_activations,
                        window, g_strdup (token_str));
 }

commit 949eb8b638d213120fd5c78f7abe9e208918e72e

wayland: Use separate signal handlers to track pending activations
We cannot use a function with the same signature for signals with different
arguments, if we want to rely on the user data parameter. Separate into
two signal handlers calling the same function inside.

Fixes: b9ba34ac6f ("wayland/activation: Fix signal callback signature")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3860>

src/wayland/meta-wayland-activation.c
@@ -378,9 +378,8 @@ maybe_activate (MetaWaylandActivation *activation,
 }
 
 static void
-complete_pending_activate (MetaWindow            *window,
-                           GParamSpec            *pspec,
-                           MetaWaylandActivation *activation)
+complete_pending_activate (MetaWaylandActivation *activation,
+                           MetaWindow            *window)
 {
   g_autoptr (GPtrArray) requests = NULL;
   size_t i;
@@ -388,8 +387,7 @@ complete_pending_activate (MetaWindow            *window,
   if (!window)
     return;
 
-  g_signal_handlers_disconnect_by_func (window, complete_pending_activate,
-                                        activation);
+  g_signal_handlers_disconnect_by_data (window, activation);
 
   if (!g_hash_table_steal_extended (activation->pending_activations,
                                     window,
@@ -399,6 +397,21 @@ complete_pending_activate (MetaWindow            *window,
 
   for (i = 0; i < requests->len; i++)
     maybe_activate (activation, window, requests->pdata[i]);
+}
+
+static void
+on_window_mapped_notify (MetaWindow            *window,
+                         GParamSpec            *pspec,
+                         MetaWaylandActivation *activation)
+{
+  complete_pending_activate (activation, window);
+}
+
+static void
+on_window_unmanaged (MetaWindow            *window,
+                     MetaWaylandActivation *activation)
+{
+  complete_pending_activate (activation, window);
 }
 
 static void
@@ -419,10 +432,10 @@ add_pending_activate (MetaWaylandActivation *activation,
       requests = g_ptr_array_new_null_terminated (0, g_free, TRUE);
 
       g_signal_connect (window, "notify::mapped",
-                        G_CALLBACK (complete_pending_activate),
+                        G_CALLBACK (on_window_mapped_notify),
                         activation);
       g_signal_connect (window, "unmanaged",
-                        G_CALLBACK (complete_pending_activate),
+                        G_CALLBACK (on_window_unmanaged),
                         activation);
     }
 