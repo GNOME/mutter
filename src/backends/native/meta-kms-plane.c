GNOME/mutter

commit 0ca933baecf06afa494657b6ffe3f3c172d5ff17

backend/native: Adds support for SIZE_HINTS Cursor Plane Property
Reads exposed size hints for the given cursor plane. Chooses nearest
minimum cursor size out of the hints with respect to the user chosen
cursor size from the UI. Allocates optimized Hardare cursor size,
hence drm buffer

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3165>

src/backends/native/meta-kms-plane.c
@@ -56,9 +56,17 @@ struct _MetaKmsPlane
    */
   GHashTable *formats_modifiers;
 
+  MetaKmsPlaneCursorSizeHints size_hints;
+
   MetaKmsPlanePropTable prop_table;
 
   MetaKmsDevice *device;
+};
+
+/* Shall be removed once available on libdrm.*/
+struct drm_plane_size_hint {
+  __u16 width;
+  __u16 height;
 };
 
 G_DEFINE_TYPE (MetaKmsPlane, meta_kms_plane, G_TYPE_OBJECT)
@@ -81,6 +89,12 @@ MetaKmsPlaneType
 meta_kms_plane_get_plane_type (MetaKmsPlane *plane)
 {
   return plane->type;
+}
+
+const MetaKmsPlaneCursorSizeHints *
+meta_kms_plane_get_cursor_size_hints (MetaKmsPlane *plane)
+{
+  return &plane->size_hints;
 }
 
 uint32_t
@@ -349,6 +363,48 @@ update_formats (MetaKmsPlane      *plane,
 }
 
 static void
+update_cursor_size_hints (MetaKmsPlane      *plane,
+                          MetaKmsImplDevice *impl_device)
+
+{
+  MetaKmsProp *prop;
+  drmModePropertyBlobPtr size_hints_blob = NULL;
+  struct drm_plane_size_hint *size_hints_ptr;
+  uint32_t blob_id, i, num_of_size_hints;
+  int fd;
+  MetaKmsPlaneType type = meta_kms_plane_get_plane_type (plane);
+
+  if (type != META_KMS_PLANE_TYPE_CURSOR)
+    return;
+  prop = &plane->prop_table.props[META_KMS_PLANE_PROP_SIZE_HINTS];
+  if(!prop)
+    return;
+
+  blob_id = prop->value;
+  if (blob_id == 0)
+    return;
+
+  fd = meta_kms_impl_device_get_fd (impl_device);
+  size_hints_blob = drmModeGetPropertyBlob (fd, blob_id);
+  if (!size_hints_blob)
+    return;
+
+  plane->size_hints.has_size_hints = TRUE;
+  size_hints_ptr = size_hints_blob->data;
+
+  num_of_size_hints = size_hints_blob->length / sizeof (struct drm_plane_size_hint);
+  plane->size_hints.cursor_width = g_new0 (uint64_t, num_of_size_hints);
+  plane->size_hints.cursor_height = g_new0 (uint64_t, num_of_size_hints);
+  plane->size_hints.num_of_size_hints = num_of_size_hints;
+
+  for (i = 0; i < num_of_size_hints; i++)
+    {
+      plane->size_hints.cursor_width [i] = size_hints_ptr[i].width;
+      plane->size_hints.cursor_height [i] = size_hints_ptr[i].height;
+    }
+}
+
+static void
 set_formats_from_array (MetaKmsPlane   *plane,
                         const uint32_t *formats,
                         size_t          n_formats)
@@ -426,6 +482,7 @@ meta_kms_plane_read_state (MetaKmsPlane            *plane,
                                           META_KMS_PLANE_N_PROPS);
 
   update_formats (plane, impl_device);
+  update_cursor_size_hints (plane, impl_device);
   update_rotations (plane);
   update_legacy_formats (plane, drm_plane);
 
@@ -534,6 +591,11 @@ init_properties (MetaKmsPlane            *plane,
           .name = "HOTSPOT_Y",
           .type = DRM_MODE_PROP_SIGNED_RANGE,
         },
+      [META_KMS_PLANE_PROP_SIZE_HINTS] =
+        {
+          .name = "SIZE_HINTS",
+          .type = DRM_MODE_PROP_BLOB,
+        },
     },
     .rotation_bitmask = {
       [META_KMS_PLANE_ROTATION_BIT_ROTATE_0] =
@@ -627,6 +689,8 @@ meta_kms_plane_finalize (GObject *object)
   MetaKmsPlane *plane = META_KMS_PLANE (object);
 
   g_hash_table_destroy (plane->formats_modifiers);
+  g_clear_pointer (&plane->size_hints.cursor_width, g_free);
+  g_clear_pointer (&plane->size_hints.cursor_height, g_free);
 
   G_OBJECT_CLASS (meta_kms_plane_parent_class)->finalize (object);
 }

src/backends/native/meta-cursor-renderer-native.c
@@ -591,6 +591,98 @@ calculate_crtc_cursor_hotspot (MetaCursorSprite     *cursor_sprite,
 }
 
 static gboolean
+get_optimal_cursor_size (MetaCrtcKms *crtc_kms,
+                         int          required_width,
+                         int          required_height,
+                         uint64_t    *out_cursor_width,
+                         uint64_t    *out_cursor_height)
+{
+  MetaGpu *gpu = meta_crtc_get_gpu (META_CRTC (crtc_kms));
+  MetaGpuKms *gpu_kms = META_GPU_KMS (gpu);
+  MetaCursorRendererNativeGpuData *cursor_renderer_gpu_data;
+  MetaKmsPlane *kms_plane;
+  const MetaKmsPlaneCursorSizeHints *size_hints;
+  size_t i;
+
+  cursor_renderer_gpu_data =
+    meta_cursor_renderer_native_gpu_data_from_gpu (gpu_kms);
+
+  if (!cursor_renderer_gpu_data)
+    return FALSE;
+
+  kms_plane = meta_crtc_kms_get_assigned_cursor_plane (crtc_kms);
+  if (!kms_plane)
+    return FALSE;
+
+  size_hints = meta_kms_plane_get_cursor_size_hints (kms_plane);
+
+  for (i = 0; i < size_hints->num_of_size_hints; i++)
+    {
+      if (size_hints->cursor_width[i] >= required_width &&
+          size_hints->cursor_height[i] >= required_height)
+        {
+          *out_cursor_width = size_hints->cursor_width[i];
+          *out_cursor_height = size_hints->cursor_height[i];
+          return TRUE;
+        }
+    }
+
+  if (!size_hints->has_size_hints &&
+      cursor_renderer_gpu_data->cursor_width >= required_width &&
+      cursor_renderer_gpu_data->cursor_height >= required_height)
+    {
+      *out_cursor_width = cursor_renderer_gpu_data->cursor_width;
+      *out_cursor_height = cursor_renderer_gpu_data->cursor_height;
+      return TRUE;
+    }
+
+  return FALSE;
+}
+
+static gboolean
+supports_exact_cursor_size (MetaCrtcKms *crtc_kms,
+                            int          required_width,
+                            int          required_height)
+{
+  MetaGpu *gpu = meta_crtc_get_gpu (META_CRTC (crtc_kms));
+  MetaGpuKms *gpu_kms = META_GPU_KMS (gpu);
+  MetaCursorRendererNativeGpuData *cursor_renderer_gpu_data;
+  MetaKmsPlane *kms_plane;
+  const MetaKmsPlaneCursorSizeHints *size_hints;
+  size_t i;
+
+  cursor_renderer_gpu_data =
+    meta_cursor_renderer_native_gpu_data_from_gpu (gpu_kms);
+
+  if (!cursor_renderer_gpu_data)
+    return FALSE;
+
+  kms_plane = meta_crtc_kms_get_assigned_cursor_plane (crtc_kms);
+  if (!kms_plane)
+    return FALSE;
+
+  size_hints = meta_kms_plane_get_cursor_size_hints (kms_plane);
+
+  for (i = 0; i < size_hints->num_of_size_hints; i++)
+    {
+      if (size_hints->cursor_width[i] == required_width &&
+          size_hints->cursor_height[i] == required_height)
+        {
+          return TRUE;
+        }
+    }
+
+  if (!size_hints->has_size_hints &&
+      cursor_renderer_gpu_data->cursor_width == required_width &&
+      cursor_renderer_gpu_data->cursor_height == required_height)
+    {
+      return TRUE;
+    }
+
+  return FALSE;
+}
+
+static gboolean
 load_cursor_sprite_gbm_buffer_for_crtc (MetaCursorRendererNative *native,
                                         MetaCrtcKms              *crtc_kms,
                                         MetaCursorSprite         *cursor_sprite,
@@ -614,23 +706,15 @@ load_cursor_sprite_gbm_buffer_for_crtc (MetaCursorRendererNative *native,
   MetaKmsCrtc *kms_crtc;
   uint64_t cursor_width, cursor_height;
   g_autoptr (MetaDrmBuffer) buffer = NULL;
-  MetaCursorRendererNativeGpuData *cursor_renderer_gpu_data;
   g_autoptr (MetaDeviceFile) device_file = NULL;
   g_autoptr (GError) error = NULL;
   graphene_point_t hotspot;
 
-  cursor_renderer_gpu_data =
-    meta_cursor_renderer_native_gpu_data_from_gpu (gpu_kms);
-  if (!cursor_renderer_gpu_data)
-    return FALSE;
-
-  cursor_width = (uint64_t) cursor_renderer_gpu_data->cursor_width;
-  cursor_height = (uint64_t) cursor_renderer_gpu_data->cursor_height;
-
-  if (width > cursor_width || height > cursor_height)
+  if (!get_optimal_cursor_size (crtc_kms,
+                                width, height,
+                                &cursor_width, &cursor_height))
     {
-      meta_warning ("Invalid theme cursor size (must be at most %ux%u)",
-                    (unsigned int)cursor_width, (unsigned int)cursor_height);
+      meta_warning ("Can't handle cursor size %ux%u)", width, height);
       return FALSE;
     }
 
@@ -834,7 +918,6 @@ realize_cursor_sprite_from_wl_buffer_for_crtc (MetaCursorRenderer      *renderer
   MetaGpu *gpu = meta_crtc_get_gpu (META_CRTC (crtc_kms));
   MetaGpuKms *gpu_kms = META_GPU_KMS (gpu);
   MetaCursorRendererNativeGpuData *cursor_renderer_gpu_data;
-  uint64_t cursor_width, cursor_height;
   CoglTexture *texture;
   uint width, height;
   MetaWaylandBuffer *buffer;
@@ -950,16 +1033,13 @@ realize_cursor_sprite_from_wl_buffer_for_crtc (MetaCursorRenderer      *renderer
        * access to the data, but it's not possible if the buffer is in GPU
        * memory (and possibly tiled too), so if we don't get the right size, we
        * fallback to GL. */
-      cursor_width = (uint64_t) cursor_renderer_gpu_data->cursor_width;
-      cursor_height = (uint64_t) cursor_renderer_gpu_data->cursor_height;
-
       texture = meta_cursor_sprite_get_cogl_texture (cursor_sprite);
       width = cogl_texture_get_width (texture);
       height = cogl_texture_get_height (texture);
 
-      if (width != cursor_width || height != cursor_height)
+      if (!supports_exact_cursor_size (crtc_kms, width, height))
         {
-          meta_warning ("Invalid cursor size (must be 64x64), falling back to software (GL) cursors");
+          meta_warning ("Invalid cursor size %ux%u, falling back to SW GL cursors)", width, height);
           return FALSE;
         }
 

src/backends/native/meta-kms-plane-private.h
@@ -42,6 +42,7 @@ typedef enum _MetaKmsPlaneProp
   META_KMS_PLANE_PROP_IN_FENCE_FD,
   META_KMS_PLANE_PROP_HOTSPOT_X,
   META_KMS_PLANE_PROP_HOTSPOT_Y,
+  META_KMS_PLANE_PROP_SIZE_HINTS,
   META_KMS_PLANE_N_PROPS
 } MetaKmsPlaneProp;
 

src/backends/native/meta-kms-plane.h
@@ -31,6 +31,14 @@ enum _MetaKmsPlaneType
   META_KMS_PLANE_TYPE_OVERLAY,
 };
 
+typedef struct _MetaKmsPlaneCursorSizeHints
+{
+  gboolean has_size_hints;
+  uint64_t num_of_size_hints;
+  uint64_t *cursor_width;
+  uint64_t *cursor_height;
+} MetaKmsPlaneCursorSizeHints;
+
 #define META_TYPE_KMS_PLANE meta_kms_plane_get_type ()
 G_DECLARE_FINAL_TYPE (MetaKmsPlane, meta_kms_plane,
                       META, KMS_PLANE, GObject)
@@ -43,6 +51,9 @@ uint32_t meta_kms_plane_get_id (MetaKmsPlane *plane);
 
 META_EXPORT_TEST
 MetaKmsPlaneType meta_kms_plane_get_plane_type (MetaKmsPlane *plane);
+
+const MetaKmsPlaneCursorSizeHints *
+meta_kms_plane_get_cursor_size_hints (MetaKmsPlane *plane);
 
 gboolean meta_kms_plane_is_transform_handled (MetaKmsPlane         *plane,
                                               MetaMonitorTransform  transform);

commit 92d60dbb089efa04419eed5c910738a60dde77a1

backend/native: Define struct drm_plane_size_hint only if libdrm doesn't
Fixes error building against libdrm >= 2.4.122:

../src/backends/native/meta-kms-plane.c:67:8: error: redefinition of ‘struct drm_plane_size_hint’
   67 | struct drm_plane_size_hint {
      |        ^~~~~~~~~~~~~~~~~~~
In file included from /usr/local/include/libdrm/drm.h:1025,
                 from /usr/local/include/xf86drm.h:40,
                 from ../src/backends/native/meta-kms-plane-private.h:20,
                 from ../src/backends/native/meta-kms-plane.c:21:
/usr/local/include/libdrm/drm_mode.h:866:8: note: originally defined here
  866 | struct drm_plane_size_hint {
      |        ^~~~~~~~~~~~~~~~~~~

Suggested by Jonas Ådahl.

v2:
* Use has_type. (Sebastian Wick)

v3: (jadahl)
* Bump meson requirement to 1.3.0 for compiler.has_type()

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3556
Fixes: 0ca933baecf0 ("backend/native: Adds support for SIZE_HINTS Cursor Plane Property")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3848>

config.h.meson
@@ -138,3 +138,6 @@
 
 /* Supports eventfd */
 #mesondefine HAVE_EVENTFD
+
+/* libdrm defines struct drm_plane_size_hint */
+#mesondefine HAVE_DRM_PLANE_SIZE_HINT

meson.build
@@ -1,6 +1,6 @@
 project('mutter', 'c',
   version: '47.alpha',
-  meson_version: '>= 1.1.0',
+  meson_version: '>= 1.3.0',
   license: 'GPLv2+'
 )
 
@@ -271,6 +271,11 @@ endif
 
 if have_wayland or have_native_backend
   libdrm_dep = dependency('libdrm', version: libdrm_req)
+  have_drm_plane_size_hint = cc.has_type('struct drm_plane_size_hint',
+                                         dependencies: libdrm_dep,
+                                         prefix: '#include <drm_mode.h>')
+else
+  have_drm_plane_size_hint = false
 endif
 
 have_egl_device = get_option('egl_device')
@@ -567,6 +572,7 @@ cdata.set('HAVE_PANGO_FT2', have_pango_ft2)
 cdata.set('HAVE_TIMERFD', have_timerfd)
 cdata.set('HAVE_MALLOC_TRIM', have_malloc_trim)
 cdata.set('HAVE_EVENTFD', have_eventfd)
+cdata.set('HAVE_DRM_PLANE_SIZE_HINT', have_drm_plane_size_hint)
 
 if have_x11_client
   xkb_base = xkeyboard_config_dep.get_variable('xkb_base')

src/backends/native/meta-kms-plane.c
@@ -63,11 +63,15 @@ struct _MetaKmsPlane
   MetaKmsDevice *device;
 };
 
+#ifndef HAVE_DRM_PLANE_SIZE_HINT
+
 /* Shall be removed once available on libdrm.*/
 struct drm_plane_size_hint {
   __u16 width;
   __u16 height;
 };
+
+#endif
 
 G_DEFINE_TYPE (MetaKmsPlane, meta_kms_plane, G_TYPE_OBJECT)
 