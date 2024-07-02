GNOME/mutter

commit ec5444f541a5126de253e0462b68e8805ed09033

wayland/actor-surface: Don't sync actor state for frozen actors
This ensures that sub-surfaces remain visible during the fade-out
animation of their window.

Fixes: be4bf8da9ca8 ("wayland/surface: Keep applied sub-surface branch node linked up")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3508
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3840>

(cherry picked from commit fab78d951a5f462f9f69511b8e790bb56024d972)

src/wayland/meta-wayland-actor-surface.c
@@ -289,6 +289,11 @@ meta_wayland_actor_surface_sync_actor_state (MetaWaylandActorSurface *actor_surf
 {
   MetaWaylandActorSurfaceClass *actor_surface_class =
     META_WAYLAND_ACTOR_SURFACE_GET_CLASS (actor_surface);
+  MetaWaylandActorSurfacePrivate *priv =
+    meta_wayland_actor_surface_get_instance_private (actor_surface);
+
+  if (priv->actor && meta_surface_actor_is_frozen (priv->actor))
+    return;
 
   actor_surface_class->sync_actor_state (actor_surface);
 }

commit 041a404ba03b0d66db003f983b8cd6d27852445f

wayland/actor-surface: Don't bail for Xwayland surfaces
Thawing Xwayland surfaces don't hit meta_window_actor_wayland_set_frozen,
so we need to sync actor state for them here.

v2:
* Guard META_IS_XWAYLAND_SURFACE by HAVE_XWAYLAND, fixes Wayland-only
  build.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3557
Fixes: ec5444f541a5 ("wayland/actor-surface: Don't sync actor state for frozen actors")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3862>

src/wayland/meta-wayland-actor-surface.c
@@ -286,8 +286,13 @@ meta_wayland_actor_surface_sync_actor_state (MetaWaylandActorSurface *actor_surf
   MetaWaylandActorSurfacePrivate *priv =
     meta_wayland_actor_surface_get_instance_private (actor_surface);
 
-  if (priv->actor && meta_surface_actor_is_frozen (priv->actor))
-    return;
+#ifdef HAVE_XWAYLAND
+  if (!META_IS_XWAYLAND_SURFACE (actor_surface))
+#endif
+    {
+      if (priv->actor && meta_surface_actor_is_frozen (priv->actor))
+        return;
+    }
 
   actor_surface_class->sync_actor_state (actor_surface);
 }