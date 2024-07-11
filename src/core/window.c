d7daf50d623c5fb53b8524f8c7330a9e7056a8b5
Jonas Ådahl <jadahl@gmail.com>
2024-07-11 14:33:01 +0200

    window: Move required condition into main if statement in show()
    No logical changes.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3879>

--- src/core/window.c
+++ src/core/window.c
@@ -2283,15 +2283,13 @@ meta_window_show (MetaWindow *window)
 
   if (focus_window &&
       window->showing_for_first_time &&
+      !meta_window_is_ancestor_of_transient (focus_window, window) &&
       ((!place_on_top_on_map && !takes_focus_on_map) ||
        window_would_be_covered_by_always_above_window (window)))
     {
-      if (!meta_window_is_ancestor_of_transient (focus_window, window))
-        {
-          needs_stacking_adjustment = TRUE;
-          if (!window->placed)
-            place_flags |= META_PLACE_FLAG_DENIED_FOCUS_AND_NOT_TRANSIENT;
-        }
+      needs_stacking_adjustment = TRUE;
+      if (!window->placed)
+        place_flags |= META_PLACE_FLAG_DENIED_FOCUS_AND_NOT_TRANSIENT;
     }
 
   if (!window->placed)
