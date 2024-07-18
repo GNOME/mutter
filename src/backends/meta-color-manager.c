f5ce2ddf3ca62268213a3060fcab64611f2ca878
Sebastian Wick <sebastian.wick@redhat.com>
2024-07-18 14:29:14 +0200

    color-manager: Create color devices also for virtual monitors
    We will need color devices on all monitors soon.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3904>

--- src/backends/meta-color-manager.c
+++ src/backends/meta-color-manager.c
@@ -174,9 +174,6 @@ update_devices (MetaColorManager *color_manager)
       g_autofree char *stolen_monitor_id = NULL;
       MetaColorDevice *color_device;
 
-      if (meta_monitor_is_virtual (monitor))
-        continue;
-
       monitor_id = generate_monitor_id (monitor);
 
       if (priv->devices &&
