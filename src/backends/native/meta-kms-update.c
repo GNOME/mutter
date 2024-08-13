GNOME/mutter

commit 27ed0697663901474ac9ebbb56596290462e9b3a

kms/impl-device: Add deadline based KMS commit scheduling
This makes it possible to post KMS updates that will always defer until
just before the scanout deadline. This is useful to allow queuing cursor
updates where we don't want to post them to KMS immediately, but rather
wait until as late as possible to get lower latency.

We cannot delay primary plane compositions however, and this is due to
how the kernel may prioritize GPU work - not until a pipeline gets
attached to a atomic commit will it in some drivers get bumped to high
priority. This means we still need to post any update that depends on
OpenGL pipelines as soon as possible.

To avoid working on compositing, then getting stomped on the feet by the
deadline scheduler, the deadline timer is disarmed whenever there is a
frame currently being painted. This will still allow new cursor updates
to arrive during composition, but will delay the actual KMS commit until
the primary plane update has been posted.

Still, even for cursor-only we still need higher than default timing
capabilities, thus the deadline scheduler depends on the KMS thread
getting real-time scheduling priority. When the thread isn't realtime
scheduled, the KMS thread instead asks the main thread to "flush" the
commit as part of the regular frame update. A flushing update means one
that isn't set to always defer and has a latching CRTC.

The verbose KMS debug logging makes the processing take too long, making
us more likely to miss the deadline. Avoid this by increasing the
evasion length when debug logging is enabled. Not the best, but better
than changing the behavior completely.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/2777>

src/backends/native/meta-kms-update.c
@@ -36,6 +36,9 @@ struct _MetaKmsUpdate
 
   gboolean is_sealed;
 
+  gboolean is_latchable;
+  MetaKmsCrtc *latch_crtc;
+
   GList *mode_sets;
   GList *plane_assignments;
   GList *connector_updates;
@@ -199,7 +202,7 @@ meta_kms_mode_set_free (MetaKmsModeSet *mode_set)
   g_free (mode_set);
 }
 
-static void
+void
 meta_kms_page_flip_listener_unref (MetaKmsPageFlipListener *listener)
 {
   MetaKmsDevice *device;
@@ -248,6 +251,27 @@ meta_kms_update_drop_plane_assignment (MetaKmsUpdate *update,
   drop_plane_assignment (update, plane, NULL);
 }
 
+static void
+update_latch_crtc (MetaKmsUpdate *update,
+                   MetaKmsCrtc   *crtc)
+{
+  if (update->is_latchable)
+    {
+      if (update->latch_crtc)
+        {
+          if (update->latch_crtc != crtc)
+            {
+              update->is_latchable = FALSE;
+              update->latch_crtc = NULL;
+            }
+        }
+      else
+        {
+          update->latch_crtc = crtc;
+        }
+    }
+}
+
 MetaKmsPlaneAssignment *
 meta_kms_update_assign_plane (MetaKmsUpdate          *update,
                               MetaKmsCrtc            *crtc,
@@ -286,6 +310,8 @@ meta_kms_update_assign_plane (MetaKmsUpdate          *update,
   update->plane_assignments = g_list_prepend (update->plane_assignments,
                                               plane_assignment);
 
+  update_latch_crtc (update, crtc);
+
   return plane_assignment;
 }
 
@@ -309,6 +335,8 @@ meta_kms_update_unassign_plane (MetaKmsUpdate *update,
 
   update->plane_assignments = g_list_prepend (update->plane_assignments,
                                               plane_assignment);
+
+  update_latch_crtc (update, crtc);
 
   return plane_assignment;
 }
@@ -490,6 +518,8 @@ meta_kms_update_set_crtc_gamma (MetaKmsUpdate      *update,
   color_update = ensure_color_update (update, crtc);
   color_update->gamma.state = gamma_update;
   color_update->gamma.has_update = TRUE;
+
+  update_latch_crtc (update, crtc);
 }
 
 static void
@@ -1016,6 +1046,7 @@ meta_kms_update_new (MetaKmsDevice *device)
 
   update = g_new0 (MetaKmsUpdate, 1);
   update->device = device;
+  update->is_latchable = TRUE;
 
   return update;
 }
@@ -1048,4 +1079,33 @@ meta_kms_update_realize (MetaKmsUpdate     *update,
 {
   update->impl_device = impl_device;
   meta_kms_impl_device_hold_fd (impl_device);
+}
+
+void
+meta_kms_update_set_flushing (MetaKmsUpdate *update,
+                              MetaKmsCrtc   *crtc)
+{
+  update_latch_crtc (update, crtc);
+}
+
+gboolean
+meta_kms_update_is_flushing (MetaKmsUpdate *update,
+                             MetaKmsCrtc   *crtc)
+{
+  return update->latch_crtc == crtc;
+}
+
+MetaKmsCrtc *
+meta_kms_update_get_latch_crtc (MetaKmsUpdate *update)
+{
+  return update->latch_crtc;
+}
+
+gboolean
+meta_kms_update_is_empty (MetaKmsUpdate *update)
+{
+  return (!update->mode_sets &&
+          !update->plane_assignments &&
+          !update->connector_updates &&
+          !update->crtc_color_updates);
 }

src/backends/native/meta-kms-crtc-private.h
@@ -56,4 +56,9 @@ uint64_t meta_kms_crtc_get_prop_drm_value (MetaKmsCrtc     *crtc,
                                            MetaKmsCrtcProp  prop,
                                            uint64_t         value);
 
+gboolean meta_kms_crtc_determine_deadline (MetaKmsCrtc  *crtc,
+                                           int64_t      *out_next_deadline_us,
+                                           int64_t      *out_next_presentation_us,
+                                           GError      **error);
+
 #endif /* META_KMS_CRTC_PRIVATE_H */

src/backends/native/meta-kms-crtc.c
@@ -28,6 +28,10 @@
 #include "backends/native/meta-kms-impl-device-simple.h"
 #include "backends/native/meta-kms-mode.h"
 #include "backends/native/meta-kms-update-private.h"
+#include "backends/native/meta-kms-utils.h"
+
+#define DEADLINE_EVASION_US 500
+#define DEADLINE_EVASION_WITH_KMS_TOPIC_US 1000
 
 typedef struct _MetaKmsCrtcPropTable
 {
@@ -486,4 +490,93 @@ meta_kms_crtc_class_init (MetaKmsCrtcClass *klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
   object_class->finalize = meta_kms_crtc_finalize;
+}
+
+static drmVBlankSeqType
+get_crtc_type_bitmask (MetaKmsCrtc *crtc)
+{
+  if (crtc->idx > 1)
+    {
+      return ((crtc->idx << DRM_VBLANK_HIGH_CRTC_SHIFT) &
+              DRM_VBLANK_HIGH_CRTC_MASK);
+    }
+  else if (crtc->idx > 0)
+    {
+      return DRM_VBLANK_SECONDARY;
+    }
+  else
+    {
+      return 0;
+    }
+}
+
+gboolean
+meta_kms_crtc_determine_deadline (MetaKmsCrtc  *crtc,
+                                  int64_t      *out_next_deadline_us,
+                                  int64_t      *out_next_presentation_us,
+                                  GError      **error)
+{
+  MetaKmsImplDevice *impl_device;
+  int fd;
+  drmVBlank vblank;
+  int ret;
+  int64_t next_presentation_us;
+  int64_t next_deadline_us;
+  drmModeModeInfo *drm_mode;
+  int64_t vblank_duration_us;
+  int64_t deadline_evasion_us;
+
+  if (!crtc->current_state.is_drm_mode_valid)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND,
+                   "Mode invalid");
+      return FALSE;
+    }
+
+  impl_device = meta_kms_device_get_impl_device (crtc->device);
+  fd = meta_kms_impl_device_get_fd (impl_device);
+
+  vblank = (drmVBlank) {
+    .request.type = DRM_VBLANK_RELATIVE | get_crtc_type_bitmask (crtc),
+    .request.sequence = 0,
+    .request.signal = 0,
+  };
+
+  ret = drmWaitVBlank (fd, &vblank);
+  if (ret != 0)
+    {
+      g_set_error (error, G_IO_ERROR, g_io_error_from_errno (-ret),
+                   "drmWaitVBlank failed: %s", g_strerror (-ret));
+      return FALSE;
+    }
+
+  drm_mode = &crtc->current_state.drm_mode;
+  next_presentation_us =
+    s2us (vblank.reply.tval_sec) + vblank.reply.tval_usec + 0.5 +
+    G_USEC_PER_SEC / meta_calculate_drm_mode_refresh_rate (drm_mode);
+
+  /*
+   *                         1
+   * time per pixel = -----------------
+   *                   Pixel clock (Hz)
+   *
+   * number of pixels = vdisplay * htotal
+   *
+   * time spent scanning out = time per pixel * number of pixels
+   *
+   */
+
+  if (meta_is_topic_enabled (META_DEBUG_KMS))
+    deadline_evasion_us = DEADLINE_EVASION_WITH_KMS_TOPIC_US;
+  else
+    deadline_evasion_us = DEADLINE_EVASION_US;
+
+  vblank_duration_us = meta_calculate_drm_mode_vblank_duration_us (drm_mode);
+  next_deadline_us = next_presentation_us - (vblank_duration_us +
+                                             deadline_evasion_us);
+
+  *out_next_presentation_us = next_presentation_us;
+  *out_next_deadline_us = next_deadline_us;
+
+  return TRUE;
 }

src/backends/native/meta-kms-device-private.h
@@ -40,4 +40,7 @@ MetaKmsCrtc * meta_kms_device_find_crtc_in_impl (MetaKmsDevice *device,
 MetaKmsConnector * meta_kms_device_find_connector_in_impl (MetaKmsDevice *device,
                                                            uint32_t       connector_id);
 
+void meta_kms_device_set_needs_flush (MetaKmsDevice *device,
+                                      MetaKmsCrtc   *crtc);
+
 #endif /* META_KMS_DEVICE_PRIVATE_H */

src/backends/native/meta-kms-device.c
@@ -41,6 +41,15 @@
 #include "backends/native/meta-kms-private.h"
 #include "backends/native/meta-kms-update-private.h"
 
+enum
+{
+  CRTC_NEEDS_FLUSH,
+
+  N_SIGNALS
+};
+
+static int signals[N_SIGNALS];
+
 struct _MetaKmsDevice
 {
   GObject parent;
@@ -61,6 +70,9 @@ struct _MetaKmsDevice
   MetaKmsDeviceCaps caps;
 
   GList *fallback_modes;
+
+  GHashTable *needs_flush_crtcs;
+  GMutex needs_flush_mutex;
 };
 
 G_DEFINE_TYPE (MetaKmsDevice, meta_kms_device, G_TYPE_OBJECT);
@@ -326,11 +338,8 @@ process_async_update_in_impl (MetaThreadImpl  *thread_impl,
   MetaKmsUpdate *update = data->update;
   MetaKmsDevice *device = meta_kms_update_get_device (update);
   MetaKmsImplDevice *impl_device = meta_kms_device_get_impl_device (device);
-  MetaKmsFeedback *feedback;
 
-  feedback = meta_kms_impl_device_process_update (impl_device, update,
-                                                  data->flags);
-  meta_kms_feedback_unref (feedback);
+  meta_kms_impl_device_handle_update (impl_device, update, data->flags);
 
   return GINT_TO_POINTER (TRUE);
 }
@@ -355,6 +364,72 @@ meta_kms_device_post_update (MetaKmsDevice       *device,
                               process_async_update_in_impl,
                               data, g_free,
                               NULL, NULL);
+}
+
+static gpointer
+await_flush_in_impl (MetaThreadImpl  *thread_impl,
+                     gpointer         user_data,
+                     GError         **error)
+{
+  MetaKmsCrtc *crtc = META_KMS_CRTC (user_data);
+  MetaKmsDevice *device = meta_kms_crtc_get_device (crtc);
+  MetaKmsImplDevice *impl_device = meta_kms_device_get_impl_device (device);
+
+  meta_kms_impl_device_await_flush (impl_device, crtc);
+
+  return NULL;
+}
+
+void
+meta_kms_device_await_flush (MetaKmsDevice *device,
+                             MetaKmsCrtc   *crtc)
+{
+  MetaKms *kms = meta_kms_device_get_kms (device);
+
+  meta_thread_post_impl_task (META_THREAD (kms),
+                              await_flush_in_impl,
+                              crtc, NULL,
+                              NULL, NULL);
+}
+
+static void
+emit_crtc_needs_flush_in_main (MetaThread *thread,
+                               gpointer    user_data)
+{
+  MetaKmsCrtc *crtc = user_data;
+
+  g_signal_emit (meta_kms_crtc_get_device (crtc),
+                 signals[CRTC_NEEDS_FLUSH], 0, crtc);
+}
+
+void
+meta_kms_device_set_needs_flush (MetaKmsDevice *device,
+                                 MetaKmsCrtc   *crtc)
+{
+  gboolean needs_flush;
+
+  g_mutex_lock (&device->needs_flush_mutex);
+  needs_flush = g_hash_table_add (device->needs_flush_crtcs, crtc);
+  g_mutex_unlock (&device->needs_flush_mutex);
+
+  if (needs_flush)
+    {
+      meta_kms_queue_callback (meta_kms_device_get_kms (device),
+                               NULL, emit_crtc_needs_flush_in_main, crtc, NULL);
+    }
+}
+
+gboolean
+meta_kms_device_handle_flush (MetaKmsDevice *device,
+                              MetaKmsCrtc   *crtc)
+{
+  gboolean needs_flush;
+
+  g_mutex_lock (&device->needs_flush_mutex);
+  needs_flush = g_hash_table_remove (device->needs_flush_crtcs, crtc);
+  g_mutex_unlock (&device->needs_flush_mutex);
+
+  return needs_flush;
 }
 
 void
@@ -631,12 +706,17 @@ meta_kms_device_finalize (GObject *object)
                                    NULL);
     }
 
+  g_mutex_clear (&device->needs_flush_mutex);
+  g_hash_table_unref (device->needs_flush_crtcs);
+
   G_OBJECT_CLASS (meta_kms_device_parent_class)->finalize (object);
 }
 
 static void
 meta_kms_device_init (MetaKmsDevice *device)
 {
+  device->needs_flush_crtcs = g_hash_table_new (NULL, NULL);
+  g_mutex_init (&device->needs_flush_mutex);
 }
 
 static void
@@ -645,4 +725,13 @@ meta_kms_device_class_init (MetaKmsDeviceClass *klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
   object_class->finalize = meta_kms_device_finalize;
+
+  signals[CRTC_NEEDS_FLUSH] =
+    g_signal_new ("crtc-needs-flush",
+                  G_TYPE_FROM_CLASS (klass),
+                  G_SIGNAL_RUN_LAST,
+                  0,
+                  NULL, NULL, NULL,
+                  G_TYPE_NONE, 1,
+                  META_TYPE_KMS_CRTC);
 }

src/backends/native/meta-kms-device.h
@@ -83,6 +83,12 @@ void meta_kms_device_post_update (MetaKmsDevice     *device,
                                   MetaKmsUpdate     *update,
                                   MetaKmsUpdateFlag  flags);
 
+void meta_kms_device_await_flush (MetaKmsDevice *device,
+                                  MetaKmsCrtc   *crtc);
+
+gboolean meta_kms_device_handle_flush (MetaKmsDevice *device,
+                                       MetaKmsCrtc   *crtc);
+
 META_EXPORT_TEST
 void meta_kms_device_disable (MetaKmsDevice *device);
 

src/backends/native/meta-kms-impl-device.h
@@ -177,6 +177,16 @@ MetaKmsFeedback * meta_kms_impl_device_process_update (MetaKmsImplDevice *impl_d
                                                        MetaKmsUpdateFlag  flags)
   G_GNUC_WARN_UNUSED_RESULT;
 
+void meta_kms_impl_device_handle_update (MetaKmsImplDevice *impl_device,
+                                         MetaKmsUpdate     *update,
+                                         MetaKmsUpdateFlag  flags);
+
+void meta_kms_impl_device_await_flush (MetaKmsImplDevice *impl_device,
+                                       MetaKmsCrtc       *crtc);
+
+void meta_kms_impl_device_schedule_process (MetaKmsImplDevice *impl_device,
+                                            MetaKmsCrtc       *crtc);
+
 void meta_kms_impl_device_handle_page_flip_callback (MetaKmsImplDevice   *impl_device,
                                                      MetaKmsPageFlipData *page_flip_data);
 

commit 4c91616ff2982e91cee139b560165cb2d12bf365

kms/update: Don't count trivial custom page flips as empty updates
This includes most frames when using EGL_DEVICE.

While it would be nice to skip all update processing in this case,
we can't go quite that far because EGL_DEVICE is still waiting on
page flip callbacks.

Fixes: 27ed069766 ("kms/impl-device: Add deadline based KMS commit scheduling")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3196
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3939>

src/backends/native/meta-kms-update.c
@@ -1207,5 +1207,6 @@ meta_kms_update_is_empty (MetaKmsUpdate *update)
           !update->plane_assignments &&
           !update->connector_updates &&
           !update->crtc_updates &&
-          !update->crtc_color_updates);
+          !update->crtc_color_updates &&
+          !update->custom_page_flip);
 }