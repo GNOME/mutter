GNOME/mutter

commit 076b3664a379c3123f207ed28af9617b2226b9d4

monitor-config-store: Store and load layout mode for each configuration
Store and load the layout mode for each logical monitor configuration in
monitors.xml by introducing a new <layoutmode> element. The value of the
element can be either "logical" or "physical". The layout mode is also
made part of the monitor configuration key.

Right now this isn't doing a lot:

When no <layoutmode> is found on the config (this is the case with all
existing configs), we'll keep using the layout mode expected by the system,
without updating the config file.

When changing an existing, or introducing a new configuration, we'll now
store the current layout mode with the config though, and load it again
on the next start of mutter. This is still not problematic as long as
mutters expected layout mode doesn't change (eg. by turning on/off
"scale-monitor-framebuffers").

When the expected layout mode of mutter switches between
restarts, the monitor config is now still loaded but remains unused,
and mutter will create (and store) a new one with the other layout mode.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3596>

src/backends/meta-monitor-config-store.c
@@ -131,6 +131,7 @@ typedef enum
   STATE_UNKNOWN,
   STATE_MONITORS,
   STATE_CONFIGURATION,
+  STATE_LAYOUT_MODE,
   STATE_LOGICAL_MONITOR,
   STATE_LOGICAL_MONITOR_X,
   STATE_LOGICAL_MONITOR_Y,
@@ -172,6 +173,8 @@ typedef struct
 
   ParserState monitor_spec_parent_state;
 
+  gboolean is_current_layout_mode_valid;
+  MetaLogicalMonitorLayoutMode current_layout_mode;
   GList *current_logical_monitor_configs;
   MetaMonitorSpec *current_monitor_spec;
   gboolean current_transform_flipped;
@@ -279,6 +282,7 @@ handle_start_element (GMarkupParseContext  *context,
         if (g_str_equal (element_name, "configuration"))
           {
             parser->state = STATE_CONFIGURATION;
+            parser->is_current_layout_mode_valid = FALSE;
           }
         else if (g_str_equal (element_name, "policy"))
           {
@@ -319,6 +323,10 @@ handle_start_element (GMarkupParseContext  *context,
 
             parser->state = STATE_LOGICAL_MONITOR;
           }
+        else if (g_str_equal (element_name, "layoutmode"))
+          {
+            parser->state = STATE_LAYOUT_MODE;
+          }
         else if (g_str_equal (element_name, "disabled"))
           {
             parser->state = STATE_DISABLED;
@@ -329,6 +337,13 @@ handle_start_element (GMarkupParseContext  *context,
                                    "configuration", STATE_CONFIGURATION);
           }
 
+        return;
+      }
+
+    case STATE_LAYOUT_MODE:
+      {
+        g_set_error (error, G_MARKUP_ERROR, G_MARKUP_ERROR_UNKNOWN_ELEMENT,
+                     "Unexpected element '%s'", element_name);
         return;
       }
 
@@ -884,6 +899,12 @@ handle_end_element (GMarkupParseContext  *context,
         return;
       }
 
+    case STATE_LAYOUT_MODE:
+      {
+        parser->state = STATE_CONFIGURATION;
+        return;
+      }
+
     case STATE_DISABLED:
       {
         g_assert (g_str_equal (element_name, "disabled"));
@@ -897,12 +918,13 @@ handle_end_element (GMarkupParseContext  *context,
         MetaMonitorConfigStore *store = parser->config_store;
         MetaMonitorsConfig *config;
         GList *l;
-        MetaLogicalMonitorLayoutMode layout_mode;
+        MetaLogicalMonitorLayoutMode layout_mode = parser->current_layout_mode;
         MetaMonitorsConfigFlag config_flags = META_MONITORS_CONFIG_FLAG_NONE;
 
         g_assert (g_str_equal (element_name, "configuration"));
 
-        layout_mode = meta_monitor_manager_get_default_layout_mode (store->monitor_manager);
+        if (!parser->is_current_layout_mode_valid)
+          layout_mode = meta_monitor_manager_get_default_layout_mode (store->monitor_manager);
 
         for (l = parser->current_logical_monitor_configs; l; l = l->next)
           {
@@ -1171,6 +1193,27 @@ handle_text (GMarkupParseContext *context,
         if (!is_all_whitespace (text, text_len))
           g_set_error (error, G_MARKUP_ERROR, G_MARKUP_ERROR_INVALID_CONTENT,
                        "Unexpected content at this point");
+        return;
+      }
+
+    case STATE_LAYOUT_MODE:
+      {
+        if (text_equals (text, text_len, "logical"))
+          {
+            parser->current_layout_mode = META_LOGICAL_MONITOR_LAYOUT_MODE_LOGICAL;
+            parser->is_current_layout_mode_valid = TRUE;
+          }
+        else if (text_equals (text, text_len, "physical"))
+          {
+            parser->current_layout_mode = META_LOGICAL_MONITOR_LAYOUT_MODE_PHYSICAL;
+            parser->is_current_layout_mode_valid = TRUE;
+          }
+        else
+          {
+            g_set_error (error, G_MARKUP_ERROR, G_MARKUP_ERROR_INVALID_CONTENT,
+                         "Invalid layout mode %.*s", (int)text_len, text);
+          }
+
         return;
       }
 
@@ -1687,6 +1730,16 @@ generate_config_xml (MetaMonitorConfigStore *config_store)
         continue;
 
       g_string_append (buffer, "  <configuration>\n");
+
+      switch (config->layout_mode)
+        {
+        case META_LOGICAL_MONITOR_LAYOUT_MODE_LOGICAL:
+          g_string_append (buffer, "    <layout_mode>logical</layout_mode>\n");
+          break;
+        case META_LOGICAL_MONITOR_LAYOUT_MODE_PHYSICAL:
+          g_string_append (buffer, "    <layout_mode>physical</layout_mode>\n");
+          break;
+        }
 
       for (l = config->logical_monitor_configs; l; l = l->next)
         {

src/backends/meta-monitor-config-manager.c
@@ -493,7 +493,8 @@ meta_create_monitors_config_key_for_current_state (MetaMonitorManager *monitor_m
 
   config_key = g_new0 (MetaMonitorsConfigKey, 1);
   *config_key = (MetaMonitorsConfigKey) {
-    .monitor_specs = monitor_specs
+    .monitor_specs = monitor_specs,
+    .layout_mode = meta_monitor_manager_get_default_layout_mode (monitor_manager),
   };
 
   return config_key;
@@ -1521,8 +1522,9 @@ meta_logical_monitor_config_free (MetaLogicalMonitorConfig *logical_monitor_conf
 }
 
 static MetaMonitorsConfigKey *
-meta_monitors_config_key_new (GList *logical_monitor_configs,
-                              GList *disabled_monitor_specs)
+meta_monitors_config_key_new (GList                        *logical_monitor_configs,
+                              GList                        *disabled_monitor_specs,
+                              MetaLogicalMonitorLayoutMode  layout_mode)
 {
   MetaMonitorsConfigKey *config_key;
   GList *monitor_specs;
@@ -1557,7 +1559,8 @@ meta_monitors_config_key_new (GList *logical_monitor_configs,
 
   config_key = g_new0 (MetaMonitorsConfigKey, 1);
   *config_key = (MetaMonitorsConfigKey) {
-    .monitor_specs = monitor_specs
+    .monitor_specs = monitor_specs,
+    .layout_mode = layout_mode
   };
 
   return config_key;
@@ -1578,7 +1581,7 @@ meta_monitors_config_key_hash (gconstpointer data)
   GList *l;
   unsigned long hash;
 
-  hash = 0;
+  hash = config_key->layout_mode;
   for (l = config_key->monitor_specs; l; l = l->next)
     {
       MetaMonitorSpec *monitor_spec = l->data;
@@ -1599,6 +1602,9 @@ meta_monitors_config_key_equal (gconstpointer data_a,
   const MetaMonitorsConfigKey *config_key_a = data_a;
   const MetaMonitorsConfigKey *config_key_b = data_b;
   GList *l_a, *l_b;
+
+  if (config_key_a->layout_mode != config_key_b->layout_mode)
+    return FALSE;
 
   for (l_a = config_key_a->monitor_specs, l_b = config_key_b->monitor_specs;
        l_a && l_b;
@@ -1651,9 +1657,11 @@ meta_monitors_config_new_full (GList                        *logical_monitor_con
   config = g_object_new (META_TYPE_MONITORS_CONFIG, NULL);
   config->logical_monitor_configs = logical_monitor_configs;
   config->disabled_monitor_specs = disabled_monitor_specs;
-  config->key = meta_monitors_config_key_new (logical_monitor_configs,
-                                              disabled_monitor_specs);
   config->layout_mode = layout_mode;
+  config->key = meta_monitors_config_key_new (logical_monitor_configs,
+                                              disabled_monitor_specs,
+                                              layout_mode);
+
   config->flags = flags;
   config->switch_config = META_MONITOR_SWITCH_CONFIG_UNKNOWN;
 

src/backends/meta-monitor-config-manager.h
@@ -49,6 +49,7 @@ typedef struct _MetaLogicalMonitorConfig
 typedef struct _MetaMonitorsConfigKey
 {
   GList *monitor_specs;
+  MetaLogicalMonitorLayoutMode layout_mode;
 } MetaMonitorsConfigKey;
 
 enum _MetaMonitorsConfigFlag

src/tests/monitor-configs/detached-groups.xml
@@ -11,6 +11,7 @@
        +-----+-----+           +-----+-----+
   -->
   <configuration>
+    <layoutmode>logical</layoutmode>
     <logicalmonitor>
       <x>0</x>
       <y>0</y>

src/tests/monitor-configs/first-rotated.xml
@@ -1,5 +1,6 @@
 <monitors version="2">
   <configuration>
+    <layoutmode>logical</layoutmode>
     <logicalmonitor>
       <x>0</x>
       <y>0</y>

src/tests/monitor-configs/fractional-scale.xml
@@ -1,5 +1,6 @@
 <monitors version="2">
   <configuration>
+    <layoutmode>logical</layoutmode>
     <logicalmonitor>
       <x>0</x>
       <y>0</y>

src/tests/monitor-configs/full-hd-fractional-scale-1.25.xml
@@ -1,5 +1,6 @@
 <monitors version="2">
   <configuration>
+    <layoutmode>logical</layoutmode>
     <logicalmonitor>
       <x>0</x>
       <y>0</y>

src/tests/monitor-configs/full-hd-fractional-scale-1.5.xml
@@ -1,5 +1,6 @@
 <monitors version="2">
   <configuration>
+    <layoutmode>logical</layoutmode>
     <logicalmonitor>
       <x>0</x>
       <y>0</y>

commit 3eebe2592aca0f74be9becac2e1cf05b7b931020

monitor-config-store: Fix typos in <layoutmode> written to monitors.xml
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3635
Fixes: 076b3664a3 ("monitor-config-store: Store and load layout mode for each configuration")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3976>

src/backends/meta-monitor-config-store.c
@@ -2297,10 +2297,10 @@ generate_config_xml (MetaMonitorConfigStore *config_store)
       switch (config->layout_mode)
         {
         case META_LOGICAL_MONITOR_LAYOUT_MODE_LOGICAL:
-          g_string_append (buffer, "    <layout_mode>logical</layout_mode>\n");
+          g_string_append (buffer, "    <layoutmode>logical</layoutmode>\n");
           break;
         case META_LOGICAL_MONITOR_LAYOUT_MODE_PHYSICAL:
-          g_string_append (buffer, "    <layout_mode>physical</layout_mode>\n");
+          g_string_append (buffer, "    <layoutmode>physical</layoutmode>\n");
           break;
         }
 