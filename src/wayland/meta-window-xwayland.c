GNOME/mutter

commit 6e8c7c5f8420716d8559ca6dcd123559aed55c6a

Add experimental mode to use native scaling of Xwayland clients
Allow scale-aware Xwayland clients to scale by an integer scale
themselves, instead of letting them render them at 1x scale and then
scaling up the texture, making it look blurry.

When monitor framebuffers are scaled, this special cases Xwayland and
sends output regions in a way that Xwayland think everything is N times
as large as the logical region, where N is the ceil of the max monitor
scale.

This is done by introducing a "stage" vs "protocol" coordinate space for
X11, where the "protocol" coordinate space is "stage" multiplied by a
scaling factor.

Xwayland thus will have its own "effective scale", sent via
wl_output.scale. The effective Xwayland scale is also used for the
internal MetaWaylandSurface scale internally, unless there is a viewport
dst size set on the same surface, in which case the scale is still set
to 1, to not interfere with wp_viewport semantics.

We're guarding this behind a new experimental feature
"xwayland-native-scaling", which can only come into effect when enabled
together with "scale-monitor-framebuffer".

[v2]:

Move stage_to_protocol/protocol_to_stage to generic window class.

This means parts that aren't aware of any windowing system specific
logic, only that coordinates originate from there for a given window,
can still get their coordinates properly handled.

In particular, this means coordinates from IBus that originates from the
client, can be scaled properly when that client is using X11.

Also make them properly introspected.

[v3]:

Split up coordinate transform API.

Make it one that takes a MtkRectangle, and another that takes a point.

This means the rounding strategy becames explicit when transforming a
point, while when it's a rectangle, it's still always "grow".

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3567>

src/wayland/meta-window-xwayland.c
@@ -27,8 +27,9 @@
 #include "x11/window-x11-private.h"
 #include "x11/xprops.h"
 #include "wayland/meta-window-xwayland.h"
-#include "wayland/meta-wayland.h"
+#include "wayland/meta-wayland-private.h"
 #include "wayland/meta-wayland-surface-private.h"
+#include "wayland/meta-xwayland.h"
 
 enum
 {
@@ -316,6 +317,69 @@ meta_window_xwayland_process_property_notify (MetaWindow     *window,
 }
 
 static void
+meta_window_xwayland_stage_to_protocol (MetaWindow *window,
+                                        int         stage_x,
+                                        int         stage_y,
+                                        int        *protocol_x,
+                                        int        *protocol_y)
+{
+  MetaDisplay *display = meta_window_get_display (window);
+  MetaContext *context = meta_display_get_context (display);
+  MetaWaylandCompositor *wayland_compositor =
+    meta_context_get_wayland_compositor (context);
+  MetaXWaylandManager *xwayland_manager = &wayland_compositor->xwayland_manager;
+  int scale;
+
+  scale = meta_xwayland_get_effective_scale (xwayland_manager);
+  if (protocol_x)
+    *protocol_x = stage_x * scale;
+  if (protocol_y)
+    *protocol_y = stage_y * scale;
+}
+
+static void
+meta_window_xwayland_protocol_to_stage (MetaWindow          *window,
+                                        int                  protocol_x,
+                                        int                  protocol_y,
+                                        int                 *stage_x,
+                                        int                 *stage_y,
+                                        MtkRoundingStrategy  rounding_strategy)
+{
+  MetaDisplay *display = meta_window_get_display (window);
+  MetaContext *context = meta_display_get_context (display);
+  MetaWaylandCompositor *wayland_compositor =
+    meta_context_get_wayland_compositor (context);
+  MetaXWaylandManager *xwayland_manager = &wayland_compositor->xwayland_manager;
+  int xwayland_scale;
+  float scale;
+
+  xwayland_scale = meta_xwayland_get_effective_scale (xwayland_manager);
+  scale = 1.0f / xwayland_scale;
+
+  switch (rounding_strategy)
+    {
+    case MTK_ROUNDING_STRATEGY_SHRINK:
+      if (stage_x)
+        *stage_x = (int) floorf (protocol_x * scale);
+      if (stage_y)
+        *stage_y = (int) floorf (protocol_y * scale);
+      break;
+    case MTK_ROUNDING_STRATEGY_GROW:
+      if (stage_x)
+        *stage_x = (int) ceilf (protocol_x * scale);
+      if (stage_y)
+        *stage_y = (int) ceilf (protocol_y * scale);
+      break;
+    case MTK_ROUNDING_STRATEGY_ROUND:
+      if (stage_x)
+        *stage_x = (int) roundf (protocol_x * scale);
+      if (stage_y)
+        *stage_y = (int) roundf (protocol_y * scale);
+      break;
+    }
+}
+
+static void
 meta_window_xwayland_class_init (MetaWindowXwaylandClass *klass)
 {
   MetaWindowClass *window_class = META_WINDOW_CLASS (klass);
@@ -326,6 +390,8 @@ meta_window_xwayland_class_init (MetaWindowXwaylandClass *klass)
   window_class->force_restore_shortcuts = meta_window_xwayland_force_restore_shortcuts;
   window_class->shortcuts_inhibited = meta_window_xwayland_shortcuts_inhibited;
   window_class->get_wayland_surface = meta_window_xwayland_get_wayland_surface;
+  window_class->stage_to_protocol = meta_window_xwayland_stage_to_protocol;
+  window_class->protocol_to_stage = meta_window_xwayland_protocol_to_stage;
 
   window_x11_class->freeze_commits = meta_window_xwayland_freeze_commits;
   window_x11_class->thaw_commits = meta_window_xwayland_thaw_commits;

src/compositor/meta-window-actor-x11.c
@@ -671,14 +671,21 @@ meta_window_actor_x11_process_damage (MetaWindowActorX11 *actor_x11,
                                       XDamageNotifyEvent *event)
 {
   MetaSurfaceActor *surface;
-  MtkRectangle area = MTK_RECTANGLE_INIT (event->area.x,
-                                          event->area.y,
-                                          event->area.width,
-                                          event->area.height);
 
   surface = meta_window_actor_get_surface (META_WINDOW_ACTOR (actor_x11));
   if (surface)
-    meta_surface_actor_process_damage (surface, &area);
+    {
+      MetaWindow *window =
+        meta_window_actor_get_meta_window (META_WINDOW_ACTOR (actor_x11));
+      MtkRectangle area;
+
+      area = MTK_RECTANGLE_INIT (event->area.x,
+                                 event->area.y,
+                                 event->area.width,
+                                 event->area.height);
+      meta_window_protocol_to_stage_rect (window, &area, &area);
+      meta_surface_actor_process_damage (surface, &area);
+    }
 
   meta_window_actor_notify_damaged (META_WINDOW_ACTOR (actor_x11));
 }

src/core/window-private.h
@@ -616,6 +616,18 @@ struct _MetaWindowClass
 
   gboolean (*set_transient_for) (MetaWindow *window,
                                  MetaWindow *parent);
+
+  void (*stage_to_protocol) (MetaWindow *window,
+                             int         stage_x,
+                             int         stage_y,
+                             int        *protocol_x,
+                             int        *protocol_y);
+  void (*protocol_to_stage) (MetaWindow          *window,
+                             int                  protocol_x,
+                             int                  protocol_y,
+                             int                 *stage_x,
+                             int                 *stage_y,
+                             MtkRoundingStrategy  rounding_strategy);
 };
 
 /* These differ from window->has_foo_func in that they consider
@@ -884,3 +896,16 @@ meta_window_should_attach_to_parent (MetaWindow *window);
  */
 void meta_window_set_normal_hints (MetaWindow    *window,
                                    MetaSizeHints *hints);
+
+void meta_window_stage_to_protocol_point (MetaWindow *window,
+                                          int         stage_x,
+                                          int         stage_y,
+                                          int        *protocol_x,
+                                          int        *protocol_y);
+
+void meta_window_protocol_to_stage_point (MetaWindow          *window,
+                                          int                  protocol_x,
+                                          int                  protocol_y,
+                                          int                 *stage_x,
+                                          int                 *stage_y,
+                                          MtkRoundingStrategy  rounding_strategy);

src/core/window.c
@@ -8037,3 +8037,103 @@ meta_window_set_normal_hints (MetaWindow    *window,
        */
     }
 }
+
+/**
+ * meta_window_stage_to_protocol_rect:
+ * @window: A #MetaWindow
+ * @stage_rect: x #MtkRectangle in stage coordinate space
+ * @protocol_rect: (out): x #MtkRectangle in protocol coordinate space
+ *
+ * Transform the coordinates from stage coordinates to protocol coordinates
+ */
+void
+meta_window_stage_to_protocol_rect (MetaWindow         *window,
+                                    const MtkRectangle *stage_rect,
+                                    MtkRectangle       *protocol_rect)
+{
+  MetaWindowClass *klass = META_WINDOW_GET_CLASS (window);
+
+  klass->stage_to_protocol (window,
+                            stage_rect->x, stage_rect->y,
+                            &protocol_rect->x, &protocol_rect->y);
+  klass->stage_to_protocol (window,
+                            stage_rect->width, stage_rect->height,
+                            &protocol_rect->width, &protocol_rect->height);
+}
+
+/**
+ * meta_window_stage_to_protocol_point:
+ * @window: A #MetaWindow
+ * @stage_x: x cordinate in stage coordinate space
+ * @stage_y: y cordinate in stage coordinate space
+ * @protocol_x: (out): x cordinate in protocol coordinate space
+ * @protocol_y: (out): y cordinate in protocol coordinate space
+ *
+ * Transform the coordinates from stage coordinates to protocol coordinates
+ */
+void
+meta_window_stage_to_protocol_point (MetaWindow *window,
+                                     int         stage_x,
+                                     int         stage_y,
+                                     int        *protocol_x,
+                                     int        *protocol_y)
+{
+  MetaWindowClass *klass = META_WINDOW_GET_CLASS (window);
+
+  klass->stage_to_protocol (window,
+                            stage_x, stage_y,
+                            protocol_x, protocol_y);
+}
+
+/**
+ * meta_window_protocol_to_stage_rect:
+ * @window: A #MetaWindow
+ * @protocol_rect: rectangle in protocol coordinate space
+ * @stage_rect: (out): rect in stage coordinate space
+ *
+ * Transform the coordinates from protocol coordinates to coordinates expected
+ * by the stage and internal window management logic.
+ */
+void
+meta_window_protocol_to_stage_rect (MetaWindow *window,
+                                    const MtkRectangle *protocol_rect,
+                                    MtkRectangle       *stage_rect)
+{
+  MetaWindowClass *klass = META_WINDOW_GET_CLASS (window);
+
+  klass->protocol_to_stage (window,
+                            protocol_rect->x, protocol_rect->y,
+                            &stage_rect->x, &stage_rect->y,
+                            MTK_ROUNDING_STRATEGY_SHRINK);
+  klass->protocol_to_stage (window,
+                            protocol_rect->width, protocol_rect->height,
+                            &stage_rect->width, &stage_rect->height,
+                            MTK_ROUNDING_STRATEGY_GROW);
+}
+
+/**
+ * meta_window_protocol_to_stage_point:
+ * @window: A #MetaWindow
+ * @protocol_x: x cordinate in protocol coordinate space
+ * @protocol_y: y cordinate in protocol coordinate space
+ * @stage_x: (out): x cordinate in stage coordinate space
+ * @stage_y: (out): y cordinate in stage coordinate space
+ *
+ * Transform the coordinates from protocol coordinates to coordinates expected
+ * by the stage and internal window management logic.
+ */
+void
+meta_window_protocol_to_stage_point (MetaWindow          *window,
+                                     int                  protocol_x,
+                                     int                  protocol_y,
+                                     int                 *stage_x,
+                                     int                 *stage_y,
+                                     MtkRoundingStrategy  rounding_strategy)
+{
+  MetaWindowClass *klass = META_WINDOW_GET_CLASS (window);
+
+  klass->protocol_to_stage (window,
+                            protocol_x, protocol_y,
+                            stage_x, stage_y,
+                            rounding_strategy);
+}

src/meta/window.h
@@ -425,3 +425,13 @@ MetaWindowClientType meta_window_get_client_type (MetaWindow *window);
 
 META_EXPORT
 gboolean meta_window_has_pointer (MetaWindow *window);
+
+META_EXPORT
+void meta_window_stage_to_protocol_rect (MetaWindow         *window,
+                                         const MtkRectangle *stage_rect,
+                                         MtkRectangle       *protocol_rect);
+
+META_EXPORT
+void meta_window_protocol_to_stage_rect (MetaWindow *window,
+                                         const MtkRectangle *protocol_rect,
+                                         MtkRectangle       *stage_rect);

src/wayland/meta-wayland-cursor-surface.c
@@ -101,24 +101,20 @@ cursor_sprite_prepare_at (MetaCursorSprite         *cursor_sprite,
 
   logical_monitor =
     meta_monitor_manager_get_logical_monitor_at (monitor_manager, x, y);
-
-  if (!meta_wayland_surface_is_xwayland (surface))
+  if (logical_monitor)
     {
-      if (logical_monitor)
-        {
-          int surface_scale = surface->applied_state.scale;
-          float texture_scale;
+      int surface_scale = surface->applied_state.scale;
+      float texture_scale;
 
-          if (meta_backend_is_stage_views_scaled (backend))
-            texture_scale = 1.0f / surface_scale;
-          else
-            texture_scale = (meta_logical_monitor_get_scale (logical_monitor) /
-                             surface_scale);
+      if (meta_backend_is_stage_views_scaled (backend))
+        texture_scale = 1.0f / surface_scale;
+      else
+        texture_scale = (meta_logical_monitor_get_scale (logical_monitor) /
+                         surface_scale);
 
-          meta_cursor_sprite_set_texture_scale (cursor_sprite, texture_scale);
-          meta_cursor_sprite_set_texture_transform (cursor_sprite,
-                                                    surface->buffer_transform);
-        }
+      meta_cursor_sprite_set_texture_scale (cursor_sprite, texture_scale);
+      meta_cursor_sprite_set_texture_transform (cursor_sprite,
+                                                surface->buffer_transform);
     }
 
   meta_wayland_surface_set_main_monitor (surface, logical_monitor);

src/wayland/meta-wayland-outputs.c
@@ -31,6 +31,10 @@
 #include "backends/meta-monitor-manager-private.h"
 #include "wayland/meta-wayland-private.h"
 
+#ifdef HAVE_XWAYLAND
+#include "wayland/meta-xwayland.h"
+#endif
+
 #include "xdg-output-unstable-v1-server-protocol.h"
 
 /* Wayland protocol headers list new additions, not deprecations */
@@ -49,6 +53,8 @@ static guint signals[LAST_SIGNAL];
 struct _MetaWaylandOutput
 {
   GObject parent;
+
+  MetaWaylandCompositor *compositor;
 
   struct wl_global *global;
   GList *resources;
@@ -157,6 +163,37 @@ wl_output_transform_from_transform (MtkMonitorTransform transform)
   g_assert_not_reached ();
 }
 
+#ifdef HAVE_XWAYLAND
+static gboolean
+is_xwayland_resource (MetaWaylandOutput  *wayland_output,
+                      struct wl_resource *resource)
+{
+  MetaXWaylandManager *manager = &wayland_output->compositor->xwayland_manager;
+
+  return resource && wl_resource_get_client (resource) == manager->client;
+}
+#endif
+
+static void
+maybe_scale_for_xwayland (MetaWaylandOutput  *wayland_output,
+                          struct wl_resource *resource,
+                          int                *x,
+                          int                *y)
+{
+#ifdef HAVE_XWAYLAND
+  if (is_xwayland_resource (wayland_output, resource))
+    {
+      MetaXWaylandManager *xwayland_manager =
+        &wayland_output->compositor->xwayland_manager;
+      int xwayland_scale;
+
+      xwayland_scale = meta_xwayland_get_effective_scale (xwayland_manager);
+      *x *= xwayland_scale;
+      *y *= xwayland_scale;
+    }
+#endif
+}
+
 static void
 send_output_events (struct wl_resource *resource,
                     MetaWaylandOutput  *wayland_output,
@@ -242,6 +279,9 @@ send_output_events (struct wl_resource *resource,
 
       wl_transform = wl_output_transform_from_transform (transform);
 
+      maybe_scale_for_xwayland (wayland_output, resource,
+                                &layout.x,
+                                &layout.y);
       wl_output_send_geometry (resource,
                                layout.x,
                                layout.y,
@@ -424,6 +464,7 @@ meta_wayland_output_new (MetaWaylandCompositor *compositor,
   MetaWaylandOutput *wayland_output;
 
   wayland_output = g_object_new (META_TYPE_WAYLAND_OUTPUT, NULL);
+  wayland_output->compositor = compositor;
   wayland_output->global = wl_global_create (compositor->wayland_display,
                                              &wl_output_interface,
                                              META_WL_OUTPUT_VERSION,
@@ -618,6 +659,7 @@ send_xdg_output_events (struct wl_resource *resource,
   if (need_all_events ||
       old_layout.x != layout.x || old_layout.y != layout.y)
     {
+      maybe_scale_for_xwayland (wayland_output, resource, &layout.x, &layout.y);
       zxdg_output_v1_send_logical_position (resource, layout.x, layout.y);
       need_done = TRUE;
     }
@@ -625,6 +667,7 @@ send_xdg_output_events (struct wl_resource *resource,
   if (need_all_events ||
       old_layout.width != layout.width || old_layout.height != layout.height)
     {
+      maybe_scale_for_xwayland (wayland_output, resource, &layout.width, &layout.height);
       zxdg_output_v1_send_logical_size (resource, layout.width, layout.height);
       need_done = TRUE;
     }
@@ -747,7 +790,7 @@ meta_wayland_outputs_init (MetaWaylandCompositor *compositor)
   MetaMonitorManager *monitor_manager =
     meta_backend_get_monitor_manager (backend);
 
-  g_signal_connect (monitor_manager, "monitors-changed-internal",
+  g_signal_connect (monitor_manager, "monitors-changed",
                     G_CALLBACK (on_monitors_changed), compositor);
 
   compositor->outputs =

src/wayland/meta-wayland-pointer.c
@@ -1250,6 +1250,20 @@ pointer_set_cursor (struct wl_client *client,
       cursor_surface = META_WAYLAND_CURSOR_SURFACE (surface->role);
       meta_wayland_cursor_surface_set_renderer (cursor_surface,
                                                 cursor_renderer);
+
+#ifdef HAVE_XWAYLAND
+      if (meta_wayland_surface_is_xwayland (surface))
+        {
+          MetaXWaylandManager *xwayland_manager =
+            &surface->compositor->xwayland_manager;
+          int scale;
+
+          scale = meta_xwayland_get_effective_scale (xwayland_manager);
+          hot_x = (int32_t) round (hot_x / (double) scale);
+          hot_y = (int32_t) round (hot_y / (double) scale);
+        }
+#endif
+
       meta_wayland_cursor_surface_set_hotspot (cursor_surface,
                                                hot_x, hot_y);
 

commit 2d64965a55f818d40fef0fe04bf3fad70fae29f5

window/xwayland: Handle arithmetics close to the int limits
`(int) (1.0f * (float) INT_MAX)` doesn't necessarily result in INT_MAX
due to how floating point arithmetics. Handle this better by setting
INT_MIN/MAX explicitly, when the floating point value post scaling
exceeds the corresponding limit.

This fixes resizing of electron windows.

Fixes: 6e8c7c5f84 ("Add experimental mode to use native scaling of Xwayland clients")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3997>

src/wayland/meta-window-xwayland.c
@@ -337,6 +337,22 @@ meta_window_xwayland_stage_to_protocol (MetaWindow *window,
     *protocol_y = stage_y * scale;
 }
 
+static int
+scale_and_handle_overflow (int      protocol,
+                           float    scale,
+                           float (* rounding_function) (float value))
+{
+  float value;
+
+  value = rounding_function (protocol * scale);
+  if (value >= (float) INT_MAX)
+    return INT_MAX;
+  else if (value <= (float) INT_MIN)
+    return INT_MIN;
+  else
+    return (int) value;
+}
+
 static void
 meta_window_xwayland_protocol_to_stage (MetaWindow          *window,
                                         int                  protocol_x,
@@ -360,21 +376,21 @@ meta_window_xwayland_protocol_to_stage (MetaWindow          *window,
     {
     case MTK_ROUNDING_STRATEGY_SHRINK:
       if (stage_x)
-        *stage_x = (int) floorf (protocol_x * scale);
+        *stage_x = scale_and_handle_overflow (protocol_x, scale, floorf);
       if (stage_y)
-        *stage_y = (int) floorf (protocol_y * scale);
+        *stage_y = scale_and_handle_overflow (protocol_y, scale, floorf);
       break;
     case MTK_ROUNDING_STRATEGY_GROW:
       if (stage_x)
-        *stage_x = (int) ceilf (protocol_x * scale);
+        *stage_x = scale_and_handle_overflow (protocol_x, scale, ceilf);
       if (stage_y)
-        *stage_y = (int) ceilf (protocol_y * scale);
+        *stage_y = scale_and_handle_overflow (protocol_y, scale, ceilf);
       break;
     case MTK_ROUNDING_STRATEGY_ROUND:
       if (stage_x)
-        *stage_x = (int) roundf (protocol_x * scale);
+        *stage_x = scale_and_handle_overflow (protocol_x, scale, roundf);
       if (stage_y)
-        *stage_y = (int) roundf (protocol_y * scale);
+        *stage_y = scale_and_handle_overflow (protocol_y, scale, roundf);
       break;
     }
 }