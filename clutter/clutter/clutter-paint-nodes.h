52f4196b939a01fc9e20fade22041c7d694327fb
Bilal Elmoussaoui <belmouss@redhat.com>
2024-09-02 21:58:42 +0200

    clutter: Remove TextNode
    As that was only used for the max render time debug flag.
    GNOME Shell, could implement that using a PipelineNode, to be experimented
    with later

--- clutter/clutter/clutter-paint-nodes.h
+++ clutter/clutter/clutter-paint-nodes.h
@@ -88,20 +88,6 @@ GType clutter_pipeline_node_get_type (void) G_GNUC_CONST;
 CLUTTER_EXPORT
 ClutterPaintNode *      clutter_pipeline_node_new       (CoglPipeline          *pipeline);
 
-#define CLUTTER_TYPE_TEXT_NODE                  (clutter_text_node_get_type ())
-#define CLUTTER_TEXT_NODE(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), CLUTTER_TYPE_TEXT_NODE, ClutterTextNode))
-#define CLUTTER_IS_TEXT_NODE(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CLUTTER_TYPE_TEXT_NODE))
-
-typedef struct _ClutterTextNode                 ClutterTextNode;
-typedef struct _ClutterTextNodeClass            ClutterTextNodeClass;
-
-CLUTTER_EXPORT
-GType clutter_text_node_get_type (void) G_GNUC_CONST;
-
-CLUTTER_EXPORT
-ClutterPaintNode *      clutter_text_node_new           (PangoLayout           *layout,
-                                                         const CoglColor       *color);
-
 #define CLUTTER_TYPE_ACTOR_NODE                 (clutter_actor_node_get_type ())
 #define CLUTTER_ACTOR_NODE(obj)                 (G_TYPE_CHECK_INSTANCE_CAST ((obj), CLUTTER_TYPE_ACTOR_NODE, ClutterActorNode))
 #define CLUTTER_IS_ACTOR_NODE(obj)              (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CLUTTER_TYPE_ACTOR_NODE))
