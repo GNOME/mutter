fd481182dcd609d0136dea827fb73195aaeec016
Bilal Elmoussaoui <belmouss@redhat.com>
2024-09-03 00:51:04 +0200

    clutter: Move unichar_direction helper to where it is used
    Making the fribidi dependency specific to the x11 backend

--- src/backends/x11/meta-keymap-x11.c
+++ src/backends/x11/meta-keymap-x11.c
@@ -23,6 +23,7 @@
 
 #include "config.h"
 
+#include <fribidi.h>
 #include <X11/Xatom.h>
 #include <X11/XKBlib.h>
 
@@ -257,8 +258,19 @@ get_direction (XkbDescPtr xkb,
     {
       int level = 0;
       KeySym sym = XkbKeySymEntry (xkb, code, level, group);
-      ClutterTextDirection dir =
-        clutter_unichar_direction (clutter_keysym_to_unicode (sym));
+      FriBidiCharType fribidi_ch_type;
+      ClutterTextDirection dir;
+
+      G_STATIC_ASSERT (sizeof (FriBidiChar) == sizeof (gunichar));
+
+      fribidi_ch_type = fribidi_get_bidi_type (clutter_keysym_to_unicode (sym));
+
+      if (!FRIBIDI_IS_STRONG (fribidi_ch_type))
+        dir = CLUTTER_TEXT_DIRECTION_DEFAULT;
+      else if (FRIBIDI_IS_RTL (fribidi_ch_type))
+        dir = CLUTTER_TEXT_DIRECTION_RTL;
+      else
+        dir = CLUTTER_TEXT_DIRECTION_LTR;
 
       switch (dir)
         {
