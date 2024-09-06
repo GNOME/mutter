GNOME/mutter

commit da828c2fccac89fedb7c85bcb4554608839e3dbf

x11/input-device: Stop using deprecated libwacom API
libwacom_has_ring/ring2 was deprecated. Switch to the replacement
function where it is available.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3974>

src/backends/x11/meta-input-device-x11.c
@@ -195,6 +195,20 @@ meta_input_device_x11_get_property (GObject    *object,
     }
 }
 
+#ifndef HAVE_LIBWACOM_GET_NUM_RINGS
+static int
+libwacom_get_num_rings (WacomDevice *device)
+{
+  if (libwacom_has_ring2 (device))
+    return 2;
+
+  if (libwacom_has_ring (device))
+    return 1;
+
+  return 0;
+}
+#endif
+
 static int
 meta_input_device_x11_get_group_n_modes (ClutterInputDevice *device,
                                          int                 group)
@@ -208,14 +222,14 @@ meta_input_device_x11_get_group_n_modes (ClutterInputDevice *device,
     {
       if (group == 0)
         {
-          if (libwacom_has_ring (wacom_device))
+          if (libwacom_get_num_rings (wacom_device) >= 1)
             return libwacom_get_ring_num_modes (wacom_device);
           else if (libwacom_get_num_strips (wacom_device) >= 1)
             return libwacom_get_strips_num_modes (wacom_device);
         }
       else if (group == 1)
         {
-          if (libwacom_has_ring2 (wacom_device))
+          if (libwacom_get_num_rings (wacom_device) >= 2)
             return libwacom_get_ring2_num_modes (wacom_device);
           else if (libwacom_get_num_strips (wacom_device) >= 2)
             return libwacom_get_strips_num_modes (wacom_device);

config.h.meson
@@ -48,6 +48,9 @@
 /* Building with libwacom for advanced tablet management */
 #mesondefine HAVE_LIBWACOM
 
+/* libwacom has get_num_rings() */
+#mesondefine HAVE_LIBWACOM_GET_NUM_RINGS
+
 /* Building with libsystemd */
 #mesondefine HAVE_LIBSYSTEMD
 

meson.build
@@ -298,6 +298,10 @@ endif
 have_libwacom = get_option('libwacom')
 if have_libwacom
   libwacom_dep = dependency('libwacom', version: libwacom_req)
+  have_libwacom_get_num_rings = cc.has_function('libwacom_get_num_rings',
+    dependencies: libwacom_dep)
+else
+  have_libwacom_get_num_rings = false
 endif
 
 have_pango_ft2 = get_option('pango_ft2')
@@ -553,6 +557,7 @@ cdata.set('HAVE_EGL_DEVICE', have_egl_device)
 cdata.set('HAVE_WAYLAND_EGLSTREAM', have_wayland_eglstream)
 cdata.set('HAVE_LIBGUDEV', have_libgudev)
 cdata.set('HAVE_LIBWACOM', have_libwacom)
+cdata.set('HAVE_LIBWACOM_GET_NUM_RINGS', have_libwacom_get_num_rings)
 cdata.set('HAVE_SM', have_sm)
 cdata.set('HAVE_STARTUP_NOTIFICATION', have_startup_notification)
 cdata.set('HAVE_INTROSPECTION', have_introspection)

commit 148ab84b833d954d37c1351be4109b88444978e0

Fix build when libwacom is disabled
Fixes: da828c2fc ("x11/input-device: Stop using deprecated libwacom API")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3996>

src/backends/x11/meta-input-device-x11.c
@@ -195,6 +195,7 @@ meta_input_device_x11_get_property (GObject    *object,
     }
 }
 
+#ifdef HAVE_LIBWACOM
 #ifndef HAVE_LIBWACOM_GET_NUM_RINGS
 static int
 libwacom_get_num_rings (WacomDevice *device)
@@ -207,6 +208,7 @@ libwacom_get_num_rings (WacomDevice *device)
 
   return 0;
 }
+#endif
 #endif
 
 static int