83102a70639297bf3e1c179484874c80b5dd8fa7
Sebastian Wick <sebastian.wick@redhat.com>
2024-09-19 22:23:47 +0200

    clutter/paint-nodes: Avoid useless copying of the LayerNode pipeline
    The constructor already copies the pipeline which means we can add a
    snippet safely without affecting the parent pipeline, without creating
    another copy.
    
    Fixes: 6b07141f1a ("clutter/paint-nodes: Make paint nodes handle color transformations")
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4037>
    (cherry picked from commit 4938987d97bef9a74787840e72779f21f7b12d10)

--- clutter/clutter/clutter-paint-nodes.c
+++ clutter/clutter/clutter-paint-nodes.c
@@ -1228,7 +1228,6 @@ clutter_layer_node_post_draw (ClutterPaintNode    *node,
 {
   ClutterLayerNode *lnode = CLUTTER_LAYER_NODE (node);
   CoglFramebuffer *fb;
-  g_autoptr (CoglPipeline) pipeline = NULL;
   guint i;
 
   /* switch to the previous framebuffer */
@@ -1240,9 +1239,8 @@ clutter_layer_node_post_draw (ClutterPaintNode    *node,
     return;
 
   fb = clutter_paint_context_get_framebuffer (paint_context);
-  pipeline = cogl_pipeline_copy (lnode->pipeline);
 
-  if (!cogl_pipeline_has_capability (pipeline,
+  if (!cogl_pipeline_has_capability (lnode->pipeline,
                                      CLUTTER_PIPELINE_CAPABILITY,
                                      CLUTTER_PIPELINE_CAPABILITY_COLOR_STATE))
     {
@@ -1255,7 +1253,7 @@ clutter_layer_node_post_draw (ClutterPaintNode    *node,
         clutter_paint_context_get_target_color_state (paint_context);
       clutter_color_state_add_pipeline_transform (color_state,
                                                   target_color_state,
-                                                  pipeline);
+                                                  lnode->pipeline);
     }
 
   for (i = 0; i < node->operations->len; i++)
