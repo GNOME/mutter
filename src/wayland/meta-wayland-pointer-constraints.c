GNOME/mutter

commit e994fbf02ce8873985634b0b19778d1d39d4ebb4

wayland/pointer-constraints: Warp pointer before destroying resource
We're retrieving the context from the constraint instance, and the
wl_resource_destroy() call indirectly destroys the instance, so warp
earlier to still have a valid instance while warping.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3649
Fixes: 6a694d64f4 ("wayland/seat: Keep a back pointer to the ClutterSeat")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4003>

src/wayland/meta-wayland-pointer-constraints.c
@@ -958,10 +958,11 @@ locked_pointer_destroy (struct wl_client   *client,
       warp_x = (int) x;
       warp_y = (int) y;
     }
-  wl_resource_destroy (resource);
 
   if (warp_pointer)
     clutter_seat_warp_pointer (constraint->seat->clutter_seat, warp_x, warp_y);
+
+  wl_resource_destroy (resource);
 }
 
 static void

commit cb34fafd5727db5684d32b462368c34adf89cca3

wayland/pointer-constraints: Warp pointer after destroying resource
e994fbf02 moved warping the pointer to before the destruction of the
resource to prevent dereferencing the constraint after destruction.
This however meant that the constraint was still active when the motion
event caused by the warp is handled, which would constrain the pointer
back again to its original position.

This moves the warping of the pointer back to after the destruction of
the resource and instead just retrieves the seat earlier while the
constraint is still valid.

Fixes: e994fbf02 ("wayland/pointer-constraints: Warp pointer before destroying resource")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3696
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4098>

src/wayland/meta-wayland-pointer-constraints.c
@@ -938,6 +938,7 @@ locked_pointer_destroy (struct wl_client   *client,
 {
   MetaWaylandPointerConstraint *constraint =
     wl_resource_get_user_data (resource);
+  ClutterSeat *seat;
   gboolean warp_pointer = FALSE;
   int warp_x, warp_y;
 
@@ -957,12 +958,12 @@ locked_pointer_destroy (struct wl_client   *client,
       warp_pointer = TRUE;
       warp_x = (int) x;
       warp_y = (int) y;
+      seat = constraint->seat->clutter_seat;
     }
+  wl_resource_destroy (resource);
 
   if (warp_pointer)
-    clutter_seat_warp_pointer (constraint->seat->clutter_seat, warp_x, warp_y);
-
-  wl_resource_destroy (resource);
+    clutter_seat_warp_pointer (seat, warp_x, warp_y);
 }
 
 static void