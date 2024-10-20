dca5f8282c50a90c1143f034c320bf631c904e90
Bilal Elmoussaoui <belmouss@redhat.com>
2024-10-20 14:06:13 +0200

    cogl: Add a copy of GDK_DECLARE_INTERNAL_TYPE
    Allows subclassing types internally only.
    Until something similar gets added to glib.

--- cogl/cogl/cogl-macros.h
+++ cogl/cogl/cogl-macros.h
@@ -65,3 +65,43 @@
 
 #define COGL_EXPORT __attribute__((visibility("default"))) extern
 #define COGL_EXPORT_TEST __attribute__((visibility("default"))) extern
+
+
+/*<private>
+ * COGL_DECLARE_INTERNAL_TYPE:
+ * @ModuleObjName: The name of the new type, in camel case (like CoglTexture)
+ * @module_obj_name: The name of the new type in lowercase, with words
+ *  separated by '_' (like 'cogl_texture')
+ * @MODULE: The name of the module, in all caps (like 'COGL')
+ * @OBJ_NAME: The bare name of the type, in all caps (like 'TEXTURE')
+ * @ParentName: the name of the parent type, in camel case (like CoglTexture)
+ *
+ * A convenience macro for emitting the usual declarations in the
+ * header file for a type which is intended to be subclassed only
+ * by internal consumers.
+ *
+ * This macro differs from %G_DECLARE_DERIVABLE_TYPE and %G_DECLARE_FINAL_TYPE
+ * by declaring a type that is only derivable internally. Internal users can
+ * derive this type, assuming they have access to the instance and class
+ * structures; external users will not be able to subclass this type.
+ */
+#define COGL_DECLARE_INTERNAL_TYPE(ModuleObjName, module_obj_name, MODULE, OBJ_NAME, ParentName) \
+        GType module_obj_name##_get_type (void);                                                               \
+        G_GNUC_BEGIN_IGNORE_DEPRECATIONS                                                                       \
+        typedef struct _##ModuleObjName ModuleObjName;                                                         \
+        typedef struct _##ModuleObjName##Class ModuleObjName##Class;                                           \
+                                                                                                         \
+        _GLIB_DEFINE_AUTOPTR_CHAINUP (ModuleObjName, ParentName)                                               \
+        G_DEFINE_AUTOPTR_CLEANUP_FUNC (ModuleObjName##Class, g_type_class_unref)                               \
+                                                                                                         \
+        G_GNUC_UNUSED static inline ModuleObjName *MODULE##_##OBJ_NAME (gpointer ptr) {                       \
+          return G_TYPE_CHECK_INSTANCE_CAST (ptr, module_obj_name##_get_type (), ModuleObjName); }             \
+        G_GNUC_UNUSED static inline ModuleObjName##Class *MODULE##_##OBJ_NAME##_CLASS (gpointer ptr) {        \
+          return G_TYPE_CHECK_CLASS_CAST (ptr, module_obj_name##_get_type (), ModuleObjName##Class); }         \
+        G_GNUC_UNUSED static inline gboolean MODULE##_IS_##OBJ_NAME (gpointer ptr) {                           \
+          return G_TYPE_CHECK_INSTANCE_TYPE (ptr, module_obj_name##_get_type ()); }                            \
+        G_GNUC_UNUSED static inline gboolean MODULE##_IS_##OBJ_NAME##_CLASS (gpointer ptr) {                   \
+          return G_TYPE_CHECK_CLASS_TYPE (ptr, module_obj_name##_get_type ()); }                               \
+        G_GNUC_UNUSED static inline ModuleObjName##Class *MODULE##_##OBJ_NAME##_GET_CLASS (gpointer ptr) {    \
+          return G_TYPE_INSTANCE_GET_CLASS (ptr, module_obj_name##_get_type (), ModuleObjName##Class); }       \
+        G_GNUC_END_IGNORE_DEPRECATIONS
