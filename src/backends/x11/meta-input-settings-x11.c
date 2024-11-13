265a541b656bbfecab08556d5a5a1cadf684620e
Bilal Elmoussaoui <belmouss@redhat.com>
2024-11-13 10:27:35 +0100

    x11: Mark meta_XFree macro as private
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4131>

--- src/backends/x11/meta-input-settings-x11.c
+++ src/backends/x11/meta-input-settings-x11.c
@@ -30,7 +30,7 @@
 
 #include "backends/x11/meta-backend-x11.h"
 #include "backends/x11/meta-input-device-x11.h"
-#include "meta/meta-x11-display.h"
+#include "x11/meta-x11-display-private.h"
 #include "mtk/mtk-x11.h"
 
 typedef struct
