GNOME/mutter

commit 2693cac83a5c264a77b52a7b158bab2a9565e16a

clutter/color-manager: Add a method to get the default color state
This avoids re-creating new color states objects to the default state.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3897>

clutter/clutter/clutter-color-manager.c
@@ -46,6 +46,7 @@ struct _ClutterColorManager
 
   GHashTable *snippet_cache;
   unsigned int id_counter;
+  ClutterColorState *default_color_state;
 };
 
 G_DEFINE_FINAL_TYPE (ClutterColorManager, clutter_color_manager, G_TYPE_OBJECT)
@@ -138,6 +139,20 @@ unsigned int
 clutter_color_manager_get_next_id (ClutterColorManager *color_manager)
 {
   return ++color_manager->id_counter;
+}
+
+ClutterColorState *
+clutter_color_manager_get_default_color_state (ClutterColorManager *color_manager)
+{
+  if (!color_manager->default_color_state)
+    {
+      color_manager->default_color_state =
+        clutter_color_state_new (color_manager->context,
+                                 CLUTTER_COLORSPACE_DEFAULT,
+                                 CLUTTER_TRANSFER_FUNCTION_DEFAULT);
+    }
+
+  return color_manager->default_color_state;
 }
 
 CoglSnippet *

clutter/clutter/clutter-actor.c
@@ -17845,13 +17845,13 @@ clutter_actor_get_content_repeat (ClutterActor *self)
 }
 
 static ClutterColorState *
-create_default_color_state (ClutterActor *self)
+get_default_color_state (ClutterActor *self)
 {
-  ClutterColorState *color_state;
-
-  color_state = clutter_color_state_new (clutter_actor_get_context (self),
-                                         CLUTTER_COLORSPACE_DEFAULT,
-                                         CLUTTER_TRANSFER_FUNCTION_DEFAULT);
+  ClutterContext *context = clutter_actor_get_context (self);
+  ClutterColorManager *color_manager =
+    clutter_context_get_color_manager (context);
+  ClutterColorState *color_state =
+    clutter_color_manager_get_default_color_state (color_manager);
 
   return color_state;
 }
@@ -17875,11 +17875,11 @@ clutter_actor_set_color_state_internal (ClutterActor      *self,
 void
 clutter_actor_unset_color_state (ClutterActor *self)
 {
-  g_autoptr (ClutterColorState) default_color_state = NULL;
+  ClutterColorState *default_color_state;
 
   g_return_if_fail (CLUTTER_IS_ACTOR (self));
 
-  default_color_state = create_default_color_state (self);
+  default_color_state = get_default_color_state (self);
   clutter_actor_set_color_state_internal (self, default_color_state);
 }
 

clutter/clutter/clutter-color-manager.h
@@ -32,3 +32,9 @@
 CLUTTER_EXPORT
 G_DECLARE_FINAL_TYPE (ClutterColorManager, clutter_color_manager,
                       CLUTTER, COLOR_MANAGER, GObject)
+
+/**
+ * clutter_color_manager_get_default_color_state: (skip)
+ */
+CLUTTER_EXPORT
+ClutterColorState * clutter_color_manager_get_default_color_state (ClutterColorManager *color_manager);

src/backends/meta-cursor-sprite-xcursor.c
@@ -383,13 +383,15 @@ ensure_xcursor_color_state (MetaCursorTracker *cursor_tracker)
         meta_cursor_tracker_get_backend (cursor_tracker);
       ClutterContext *clutter_context =
         meta_backend_get_clutter_context (backend);
+      ClutterColorManager *color_manager =
+        clutter_context_get_color_manager (clutter_context);
 
-      color_state = clutter_color_state_new (clutter_context,
-                                             CLUTTER_COLORSPACE_DEFAULT,
-                                             CLUTTER_TRANSFER_FUNCTION_DEFAULT);
+      color_state = clutter_color_manager_get_default_color_state (color_manager);
+
       g_object_set_qdata_full (G_OBJECT (cursor_tracker),
                                GPOINTER_TO_INT (quark_once.retval),
-                               color_state, g_object_unref);
+                               g_object_ref (color_state),
+                               g_object_unref);
     }
 
   return color_state;

src/wayland/meta-cursor-sprite-wayland.c
@@ -82,13 +82,15 @@ ensure_default_color_state (MetaCursorTracker *cursor_tracker)
         meta_cursor_tracker_get_backend (cursor_tracker);
       ClutterContext *clutter_context =
         meta_backend_get_clutter_context (backend);
+      ClutterColorManager *color_manager =
+        clutter_context_get_color_manager (clutter_context);
 
-      color_state = clutter_color_state_new (clutter_context,
-                                             CLUTTER_COLORSPACE_DEFAULT,
-                                             CLUTTER_TRANSFER_FUNCTION_DEFAULT);
+      color_state = clutter_color_manager_get_default_color_state (color_manager);
+
       g_object_set_qdata_full (G_OBJECT (cursor_tracker),
                                GPOINTER_TO_INT (quark_once.retval),
-                               color_state, g_object_unref);
+                               g_object_ref (color_state),
+                               g_object_unref);
     }
 
   return color_state;

commit 57f3e172ca0d9f2f109405231669b2b2302de6e7

clutter/color-manager: Clear default_color_state in finalize
Fixes leak:

==5763== 96 (32 direct, 64 indirect) bytes in 1 blocks are definitely lost in loss record 10,901 of 13,065
==5763==    at 0x4F81D57: g_type_create_instance (gtype.c:1929)
==5763==    by 0x4F64ABF: g_object_new_internal.part.0 (gobject.c:2606)
==5763==    by 0x4F66ADD: g_object_new_internal (gobject.c:2603)
==5763==    by 0x4F66ADD: g_object_new_with_properties (gobject.c:2769)
==5763==    by 0x4F67A30: g_object_new (gobject.c:2415)
==5763==    by 0x52F7C46: clutter_color_state_new_full (clutter-color-state.c:339)
==5763==    by 0x52F7C03: clutter_color_state_new (clutter-color-state.c:312)
==5763==    by 0x52F7110: clutter_color_manager_get_default_color_state (clutter-color-manager.c:150)
==5763==    by 0x52E7543: get_default_color_state (clutter-actor.c:17836)
==5763==    by 0x52E765D: clutter_actor_unset_color_state (clutter-actor.c:17864)
==5763==    by 0x52CF056: clutter_actor_constructor (clutter-actor.c:5646)
==5763==    by 0x4F64DD3: g_object_new_with_custom_constructor (gobject.c:2524)
==5763==    by 0x4F67275: g_object_new_internal (gobject.c:2604)
==5763==    by 0x4F67275: g_object_new_valist (gobject.c:2945)

Fixes: 2693cac83a5c ("clutter/color-manager: Add a method to get the default color state")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4149>

clutter/clutter/clutter-color-manager.c
@@ -57,6 +57,8 @@ clutter_color_manager_finalize (GObject *object)
 {
   ClutterColorManager *color_manager = CLUTTER_COLOR_MANAGER (object);
 
+  g_clear_object (&color_manager->default_color_state);
+
   g_clear_pointer (&color_manager->snippet_cache, g_hash_table_unref);
 
   G_OBJECT_CLASS (clutter_color_manager_parent_class)->finalize (object);