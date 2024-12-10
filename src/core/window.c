GNOME/mutter

commit 3047b2ce26162f14349a3e0f64668bbadd4bc2ab

wayland/xdg-toplevel-drag: Add MetaWindowActor tied_to_drag state
- Skip placement constraints when it is set.
- Required to properly support about-to-be-mapped windows, which get
  "detached" when dragged out of its original owning window.

Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4107>

src/core/window.c
@@ -2298,6 +2298,13 @@ meta_window_update_visibility (MetaWindow  *window)
   implement_showing (window, meta_window_should_be_showing (window));
 }
 
+static gboolean
+meta_window_is_tied_to_drag (MetaWindow *window)
+{
+  MetaWindowActor *window_actor = meta_window_actor_from_window (window);
+  return window_actor && meta_window_actor_is_tied_to_drag (window_actor);
+}
+
 static void
 meta_window_show (MetaWindow *window)
 {
@@ -4025,7 +4032,10 @@ meta_window_move_resize_internal (MetaWindow          *window,
 
   constrained_rect = unconstrained_rect;
   temporary_rect = rect;
-  if (flags & META_MOVE_RESIZE_CONSTRAIN && window->monitor)
+  /* Do not constrain if it is tied to an ongoing window drag. */
+  if ((flags & META_MOVE_RESIZE_CONSTRAIN) &&
+      window->monitor &&
+      !meta_window_is_tied_to_drag (window))
     {
       MtkRectangle old_rect;
 

src/compositor/meta-window-actor-private.h
@@ -114,3 +114,8 @@ void meta_window_actor_add_surface_actor (MetaWindowActor  *window_actor,
 
 void meta_window_actor_remove_surface_actor (MetaWindowActor  *window_actor,
                                              MetaSurfaceActor *surface_actor);
+
+void meta_window_actor_set_tied_to_drag (MetaWindowActor *window_actor,
+                                         gboolean         tied_to_drag);
+
+gboolean meta_window_actor_is_tied_to_drag (MetaWindowActor *window_actor);

src/compositor/meta-window-actor.c
@@ -82,6 +82,9 @@ typedef struct _MetaWindowActorPrivate
 
   guint             updates_frozen         : 1;
   guint             first_frame_state      : 2; /* FirstFrameState */
+
+  /* whether the associated window was created during a window drag */
+  unsigned int tied_to_drag : 1;
 } MetaWindowActorPrivate;
 
 enum
@@ -1807,4 +1810,21 @@ meta_window_actor_paint_to_content (MetaWindowActor  *self,
 out:
   clutter_actor_uninhibit_culling (actor);
   return content;
+}
+
+void
+meta_window_actor_set_tied_to_drag (MetaWindowActor *window_actor,
+                                    gboolean         tied_to_drag)
+{
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+  priv->tied_to_drag = tied_to_drag;
+}
+
+gboolean
+meta_window_actor_is_tied_to_drag (MetaWindowActor *window_actor)
+{
+  MetaWindowActorPrivate *priv =
+    meta_window_actor_get_instance_private (window_actor);
+  return priv->tied_to_drag;
 }

commit 0265fa34be05386cb2b3092c1e46da631be156dc

window: Initialize rect
As far as I can see this doesn't happen in practice, but theoretically
the uninitialized values can be used if:

 1. `did_placement` is TRUE
 2. `flags` contains both `META_MOVE_RESIZE_RESIZE_ACTION` and
    also `META_MOVE_RESIZE_MOVE_ACTION`
 3. `!meta_window_is_tied_to_drag (window)` is FALSE

In that case, the `frame_rect` variable (with uninitialized values) is
passed to `unconstrained_rect`, then passed to `constrained_rect`,
then finally the (uninitialized) X and Y values are read in the
`if (did_placement)` branch.

This is probably a regression from 3047b2ce261. I don't know if this is
the appropriate fix.

Coverity CID: #1511378

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4175>

src/core/window.c
@@ -4375,14 +4375,14 @@ adjust_size_for_tile_match (MetaWindow *window,
 }
 
 void
-meta_window_resize_frame_with_gravity (MetaWindow *window,
+meta_window_resize_frame_with_gravity (MetaWindow  *window,
                                        gboolean     user_op,
                                        int          w,
                                        int          h,
                                        MetaGravity  gravity)
 {
   MetaMoveResizeFlags flags;
-  MtkRectangle rect;
+  MtkRectangle rect = { 0, };
 
   rect.width = w;
   rect.height = h;