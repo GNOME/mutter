GNOME/mutter

commit e06592d3dfc57f42bb54cd31b2563d732feb8fba

util: Drop duplicated Clutter debug flags helpers
Clutter exposes those functions already.
So there is literally 0 point in duplicating them.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4131>

clutter/clutter/clutter-main.c
@@ -598,6 +598,12 @@ clutter_debug_set_max_render_time_constant (int max_render_time_constant_us)
   clutter_max_render_time_constant_us = max_render_time_constant_us;
 }
 
+/**
+ * clutter_get_debug_flags:
+ * @debug_flags: (out) (optional): return location for debug flags
+ * @draw_flags: (out) (optional): return location for draw debug flags
+ * @pick_flags: (out) (optional): return location for pick debug flags
+ */
 void
 clutter_get_debug_flags (ClutterDebugFlag     *debug_flags,
                          ClutterDrawDebugFlag *draw_flags,

src/backends/meta-stage-impl.c
@@ -541,7 +541,7 @@ meta_stage_impl_redraw_view_primary (MetaStageImpl    *stage_impl,
                                              buffer_age);
     }
 
-  meta_get_clutter_debug_flags (NULL, &paint_debug_flags, NULL);
+  clutter_get_debug_flags (NULL, &paint_debug_flags, NULL);
 
   use_clipped_redraw =
     should_use_clipped_redraw (is_full_redraw,

src/core/util.c
@@ -531,37 +531,6 @@ meta_generate_random_id (GRand *rand,
   return id;
 }
 
-
-void
-meta_add_clutter_debug_flags (ClutterDebugFlag     debug_flags,
-                              ClutterDrawDebugFlag draw_flags,
-                              ClutterPickDebugFlag pick_flags)
-{
-  clutter_add_debug_flags (debug_flags, draw_flags, pick_flags);
-}
-
-void
-meta_remove_clutter_debug_flags (ClutterDebugFlag     debug_flags,
-                                 ClutterDrawDebugFlag draw_flags,
-                                 ClutterPickDebugFlag pick_flags)
-{
-  clutter_remove_debug_flags (debug_flags, draw_flags, pick_flags);
-}
-
-/**
- * meta_get_clutter_debug_flags:
- * @debug_flags: (out) (optional): return location for debug flags
- * @draw_flags: (out) (optional): return location for draw debug flags
- * @pick_flags: (out) (optional): return location for pick debug flags
- */
-void
-meta_get_clutter_debug_flags (ClutterDebugFlag     *debug_flags,
-                              ClutterDrawDebugFlag *draw_flags,
-                              ClutterPickDebugFlag *pick_flags)
-{
-  clutter_get_debug_flags (debug_flags, draw_flags, pick_flags);
-}
-
 void
 meta_add_debug_paint_flag (MetaDebugPaintFlag flag)
 {

src/meta/util.h
@@ -94,21 +94,6 @@ META_EXPORT
 char* meta_g_utf8_strndup (const gchar *src, gsize n);
 
 META_EXPORT
-void meta_add_clutter_debug_flags (ClutterDebugFlag     debug_flags,
-                                   ClutterDrawDebugFlag draw_flags,
-                                   ClutterPickDebugFlag pick_flags);
-
-META_EXPORT
-void meta_remove_clutter_debug_flags (ClutterDebugFlag     debug_flags,
-                                      ClutterDrawDebugFlag draw_flags,
-                                      ClutterPickDebugFlag pick_flags);
-
-META_EXPORT
-void meta_get_clutter_debug_flags (ClutterDebugFlag     *debug_flags,
-                                   ClutterDrawDebugFlag *draw_flags,
-                                   ClutterPickDebugFlag *pick_flags);
-
-META_EXPORT
 void meta_add_debug_paint_flag (MetaDebugPaintFlag flag);
 
 META_EXPORT

commit 623c6c6cd37d6ab80aa4fedd0ebc405f5c44c1ac

clutter: Make debug helpers functions introspected
Fixes: e06592d3d ("util: Drop duplicated Clutter debug flags helpers")
Closes https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8118

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4197>

clutter/clutter/clutter-main.c
@@ -563,7 +563,7 @@ _clutter_clear_events_queue (void)
 }
 
 /**
- * clutter_add_debug_flags: (skip)
+ * clutter_add_debug_flags:
  *
  * Adds the debug flags passed to the list of debug flags.
  */
@@ -578,7 +578,7 @@ clutter_add_debug_flags (ClutterDebugFlag     debug_flags,
 }
 
 /**
- * clutter_remove_debug_flags: (skip)
+ * clutter_remove_debug_flags:
  *
  * Removes the debug flags passed from the list of debug flags.
  */

clutter/clutter/clutter-main.h
@@ -120,6 +120,11 @@ CLUTTER_EXPORT
 ClutterTextDirection    clutter_get_default_text_direction      (void);
 
 CLUTTER_EXPORT
+void                    clutter_get_debug_flags                 (ClutterDebugFlag     *debug_flags,
+                                                                 ClutterDrawDebugFlag *draw_flags,
+                                                                 ClutterPickDebugFlag *pick_flags);
+
+CLUTTER_EXPORT
 void                    clutter_add_debug_flags                 (ClutterDebugFlag     debug_flags,
                                                                  ClutterDrawDebugFlag draw_flags,
                                                                  ClutterPickDebugFlag pick_flags);

clutter/clutter/clutter-mutter.h
@@ -107,11 +107,6 @@ gboolean clutter_stage_get_device_coords (ClutterStage         *stage,
                                           graphene_point_t     *coords);
 
 CLUTTER_EXPORT
-void clutter_get_debug_flags (ClutterDebugFlag     *debug_flags,
-                              ClutterDrawDebugFlag *draw_flags,
-                              ClutterPickDebugFlag *pick_flags);
-
-CLUTTER_EXPORT
 void clutter_actor_notify_transform_invalid (ClutterActor *self);
 
 CLUTTER_EXPORT