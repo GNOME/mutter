GNOME/mutter

commit 20c7653d495a4fcc63761a5f5d28660cad8534e6

compositor-view/native: Don't scan out surface with color state mismatch
If the color state of a surface doesn't match the output, don't try to
scan it out, as it needs to pass through color state transformations to
look correct.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3433>

src/compositor/meta-compositor-view-native.c
@@ -159,6 +159,8 @@ find_scanout_candidate (MetaCompositorView  *compositor_view,
   ClutterActorBox actor_box;
   MetaSurfaceActor *surface_actor;
   MetaSurfaceActorWayland *surface_actor_wayland;
+  ClutterColorState *view_color_state;
+  ClutterColorState *surface_color_state;
   MetaWaylandSurface *surface;
 
   if (meta_compositor_is_unredirect_inhibited (compositor))
@@ -282,6 +284,18 @@ find_scanout_candidate (MetaCompositorView  *compositor_view,
       meta_topic (META_DEBUG_RENDER,
                   "No direct scanout candidate: window-actor has no scanout "
                   "candidate");
+      return FALSE;
+    }
+
+  view_color_state =
+    clutter_stage_view_get_color_state (CLUTTER_STAGE_VIEW (view));
+  surface_color_state =
+    clutter_actor_get_color_state (CLUTTER_ACTOR (surface_actor));
+  if (!clutter_color_state_equals (view_color_state, surface_color_state))
+    {
+      meta_topic (META_DEBUG_RENDER,
+                  "No direct scanout candidate: "
+                  "surface color state doesn't match the outputs");
       return FALSE;
     }
 

clutter/clutter/clutter-stage-view-private.h
@@ -81,5 +81,3 @@ CLUTTER_EXPORT
 void clutter_stage_view_notify_ready (ClutterStageView *view);
 
 void clutter_stage_view_invalidate_input_devices (ClutterStageView *view);
-
-ClutterColorState * clutter_stage_view_get_color_state (ClutterStageView *view);

clutter/clutter/clutter-stage-view.c
@@ -1322,6 +1322,9 @@ clutter_stage_view_get_default_paint_flags (ClutterStageView *view)
     return CLUTTER_PAINT_FLAG_NONE;
 }
 
+/**
+ * clutter_stage_view_get_color_state: (skip)
+ */
 ClutterColorState *
 clutter_stage_view_get_color_state (ClutterStageView *view)
 {

clutter/clutter/clutter-stage-view.h
@@ -94,3 +94,6 @@ void clutter_stage_view_schedule_update_now (ClutterStageView *view);
 
 CLUTTER_EXPORT
 ClutterPaintFlag clutter_stage_view_get_default_paint_flags (ClutterStageView *view);
+
+CLUTTER_EXPORT
+ClutterColorState * clutter_stage_view_get_color_state (ClutterStageView *view);

commit aea1aee79e0fff2ff78e1f4fc4b7650c51c8f14f

compositor-view/native: Check output color state for direct scanout
The stage view color state is not the output color state but usually a
linear version of it. For direct scanout, we need the view color state
to match the output color state instead.

Fixes: 20c7653d49 ("compositor-view/native: Don't scan out surface with color state mismatch")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4196>

src/compositor/meta-compositor-view-native.c
@@ -159,7 +159,7 @@ find_scanout_candidate (MetaCompositorView  *compositor_view,
   ClutterActorBox actor_box;
   MetaSurfaceActor *surface_actor;
   MetaSurfaceActorWayland *surface_actor_wayland;
-  ClutterColorState *view_color_state;
+  ClutterColorState *output_color_state;
   ClutterColorState *surface_color_state;
   MetaWaylandSurface *surface;
 
@@ -290,11 +290,11 @@ find_scanout_candidate (MetaCompositorView  *compositor_view,
       return FALSE;
     }
 
-  view_color_state =
-    clutter_stage_view_get_color_state (CLUTTER_STAGE_VIEW (view));
+  output_color_state =
+    clutter_stage_view_get_output_color_state (CLUTTER_STAGE_VIEW (view));
   surface_color_state =
     clutter_actor_get_color_state (CLUTTER_ACTOR (surface_actor));
-  if (!clutter_color_state_equals (view_color_state, surface_color_state))
+  if (!clutter_color_state_equals (output_color_state, surface_color_state))
     {
       meta_topic (META_DEBUG_RENDER,
                   "No direct scanout candidate: "