GNOME/mutter

commit 23c2c0853c70b08bccede2796fa9a99135361e9d

launcher: Move from backends/native/ to backends/
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4130>

src/meson.build
@@ -242,6 +242,8 @@ mutter_sources = [
   'backends/meta-input-settings-dummy.h',
   'backends/meta-keymap-utils.c',
   'backends/meta-keymap-utils.h',
+  'backends/meta-launcher.c',
+  'backends/meta-launcher.h',
   'backends/meta-logical-monitor.c',
   'backends/meta-logical-monitor.h',
   'backends/meta-monitor.c',
@@ -802,8 +804,6 @@ if have_native_backend
     'backends/native/meta-input-settings-native.h',
     'backends/native/meta-keymap-native.c',
     'backends/native/meta-keymap-native.h',
-    'backends/native/meta-launcher.c',
-    'backends/native/meta-launcher.h',
     'backends/native/meta-monitor-manager-native.c',
     'backends/native/meta-monitor-manager-native.h',
     'backends/native/meta-output-kms.c',
@@ -984,6 +984,11 @@ dbus_interfaces = [
     'interface': 'org.gnome.Mutter.X11.xml',
     'prefix': 'org.gnome.Mutter',
   },
+  {
+    'name': 'meta-dbus-login1',
+    'interface': 'org.freedesktop.login1.xml',
+    'prefix': 'org.freedesktop.',
+  },
 ]
 
 if have_profiler
@@ -1011,14 +1016,6 @@ if have_native_backend
     command: [gen_default_modes, '@OUTPUT@']
   )
   mutter_built_sources += default_modes_h
-
-  dbus_interfaces += [
-    {
-      'name': 'meta-dbus-login1',
-      'interface': 'org.freedesktop.login1.xml',
-      'prefix': 'org.freedesktop.',
-    },
-  ]
 endif
 
 if have_remote_desktop

src/backends/meta-backend-types.h
@@ -95,3 +95,5 @@ typedef struct _MetaInputCaptureSession MetaInputCaptureSession;
 
 typedef struct _MetaEis MetaEis;
 typedef struct _MetaEisClient MetaEisClient;
+
+typedef struct _MetaLauncher MetaLauncher;

src/backends/meta-launcher.c
@@ -17,7 +17,7 @@
 
 #include "config.h"
 
-#include "backends/native/meta-launcher.h"
+#include "backends/meta-launcher.h"
 
 #include <gio/gunixfdlist.h>
 #include <sys/types.h>

src/backends/native/meta-backend-native.c
@@ -52,7 +52,6 @@
 #include "backends/native/meta-device-pool-private.h"
 #include "backends/native/meta-kms.h"
 #include "backends/native/meta-kms-device.h"
-#include "backends/native/meta-launcher.h"
 #include "backends/native/meta-monitor-manager-native.h"
 #include "backends/native/meta-render-device-gbm.h"
 #include "backends/native/meta-renderer-native.h"

src/backends/native/meta-backend-native.h
@@ -23,9 +23,9 @@
 #pragma once
 
 #include "backends/meta-backend-private.h"
+#include "backends/meta-launcher.h"
 #include "backends/native/meta-clutter-backend-native.h"
 #include "backends/native/meta-kms-types.h"
-#include "backends/native/meta-launcher.h"
 #include "backends/native/meta-udev.h"
 
 #define META_BACKEND_TEST_INPUT_SEAT "meta-test-seat0"

src/backends/native/meta-device-pool-private.h
@@ -21,7 +21,6 @@
 
 #include "backends/native/meta-backend-native-types.h"
 #include "backends/native/meta-device-pool.h"
-#include "backends/native/meta-launcher.h"
 
 #define META_TYPE_DEVICE_POOL (meta_device_pool_get_type ())
 G_DECLARE_FINAL_TYPE (MetaDevicePool, meta_device_pool,

src/backends/native/meta-device-pool.c
@@ -25,8 +25,8 @@
 #include <sys/sysmacros.h>
 #include <sys/types.h>
 
+#include "backends/meta-launcher.h"
 #include "backends/native/meta-backend-native.h"
-#include "backends/native/meta-launcher.h"
 #include "meta/meta-backend.h"
 #include "meta/util.h"
 

commit ef0ff602b50252d6edf05a52b5875da2e46e0c2f

backends: Only build and use MetaLauncher if we have logind
Fixes: 23c2c0853c ("launcher: Move from backends/native/ to backends/")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3878
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4223>

src/backends/meta-backend-private.h
@@ -162,8 +162,10 @@ MetaIdleManager * meta_backend_get_idle_manager (MetaBackend *backend);
 META_EXPORT_TEST
 MetaColorManager * meta_backend_get_color_manager (MetaBackend *backend);
 
+#ifdef HAVE_LOGIND
 META_EXPORT_TEST
 MetaLauncher * meta_backend_get_launcher (MetaBackend *backend);
+#endif
 
 #ifdef HAVE_LIBGUDEV
 META_EXPORT_TEST

src/backends/meta-backend.c
@@ -62,7 +62,6 @@
 #include "backends/meta-input-capture.h"
 #include "backends/meta-input-mapper-private.h"
 #include "backends/meta-input-settings-private.h"
-#include "backends/meta-launcher.h"
 #include "backends/meta-logical-monitor.h"
 #include "backends/meta-monitor-manager-dummy.h"
 #include "backends/meta-remote-access-controller-private.h"
@@ -93,6 +92,10 @@
 
 #ifdef HAVE_WAYLAND
 #include "wayland/meta-wayland.h"
+#endif
+
+#ifdef HAVE_LOGIND
+#include "backends/meta-launcher.h"
 #endif
 
 #ifdef HAVE_LIBGUDEV
@@ -149,7 +152,9 @@ struct _MetaBackendPrivate
   MetaIdleManager *idle_manager;
   MetaRenderer *renderer;
   MetaColorManager *color_manager;
+#ifdef HAVE_LOGIND
   MetaLauncher *launcher;
+#endif
 #ifdef HAVE_LIBGUDEV
   MetaUdev *udev;
 #endif
@@ -265,7 +270,9 @@ meta_backend_finalize (GObject *object)
   g_cancellable_cancel (priv->cancellable);
   g_clear_object (&priv->cancellable);
 
+#ifdef HAVE_LOGIND
   g_clear_object (&priv->launcher);
+#endif
 
 #ifdef HAVE_LIBGUDEV
   g_clear_object (&priv->udev);
@@ -964,6 +971,7 @@ meta_backend_class_init (MetaBackendClass *klass)
                   G_TYPE_NONE, 0);
 }
 
+#ifdef HAVE_LOGIND
 static void
 meta_backend_pause (MetaBackend *backend)
 {
@@ -1018,6 +1026,7 @@ meta_backend_create_launcher (MetaBackend   *backend,
   *launcher_out = g_steal_pointer (&launcher);
   return ret;
 }
+#endif
 
 static MetaMonitorManager *
 meta_backend_create_monitor_manager (MetaBackend *backend,
@@ -1326,8 +1335,10 @@ meta_backend_initable_init (GInitable     *initable,
              system_bus_gotten_cb,
              backend);
 
+#ifdef HAVE_LOGIND
   if (!meta_backend_create_launcher (backend, &priv->launcher, error))
       return FALSE;
+#endif
 
 #ifdef HAVE_LIBGUDEV
   priv->udev = meta_udev_new (backend);
@@ -1524,6 +1535,7 @@ meta_backend_get_color_manager (MetaBackend *backend)
   return priv->color_manager;
 }
 
+#ifdef HAVE_LOGIND
 MetaLauncher *
 meta_backend_get_launcher (MetaBackend *backend)
 {
@@ -1531,6 +1543,7 @@ meta_backend_get_launcher (MetaBackend *backend)
 
   return priv->launcher;
 }
+#endif
 
 #ifdef HAVE_LIBGUDEV
 MetaUdev *

src/backends/x11/meta-backend-x11.c
@@ -43,7 +43,6 @@
 #include "backends/meta-color-manager.h"
 #include "backends/meta-idle-monitor-private.h"
 #include "backends/meta-keymap-utils.h"
-#include "backends/meta-launcher.h"
 #include "backends/meta-stage-private.h"
 #include "backends/x11/meta-barrier-x11.h"
 #include "backends/x11/meta-clutter-backend-x11.h"
@@ -60,6 +59,10 @@
 #include "meta/util.h"
 #include "mtk/mtk-x11.h"
 #include "x11/window-x11.h"
+
+#ifdef HAVE_LOGIND
+#include "backends/meta-launcher.h"
+#endif
 
 typedef struct _MetaBackendX11Private
 {
@@ -612,6 +615,7 @@ meta_backend_x11_init_post (MetaBackend  *backend,
   return TRUE;
 }
 
+#ifdef HAVE_LOGIND
 static gboolean
 meta_backend_x11_create_launcher (MetaBackend   *backend,
                                   MetaLauncher **launcher_out,
@@ -636,6 +640,7 @@ meta_backend_x11_create_launcher (MetaBackend   *backend,
   *launcher_out = g_steal_pointer (&launcher);
   return TRUE;
 }
+#endif
 
 static ClutterBackend *
 meta_backend_x11_create_clutter_backend (MetaBackend    *backend,
@@ -1083,7 +1088,9 @@ meta_backend_x11_class_init (MetaBackendX11Class *klass)
   backend_class->init_render = meta_backend_x11_init_render;
   backend_class->init_post = meta_backend_x11_init_post;
 
+#ifdef HAVE_LOGIND
   backend_class->create_launcher = meta_backend_x11_create_launcher;
+#endif
   backend_class->create_clutter_backend = meta_backend_x11_create_clutter_backend;
   backend_class->create_color_manager = meta_backend_x11_create_color_manager;
   backend_class->create_default_seat = meta_backend_x11_create_default_seat;

src/meson.build
@@ -240,8 +240,6 @@ mutter_sources = [
   'backends/meta-input-settings-dummy.h',
   'backends/meta-keymap-utils.c',
   'backends/meta-keymap-utils.h',
-  'backends/meta-launcher.c',
-  'backends/meta-launcher.h',
   'backends/meta-logical-monitor.c',
   'backends/meta-logical-monitor.h',
   'backends/meta-monitor.c',
@@ -919,6 +917,13 @@ if have_libgudev
   mutter_sources += [
     'backends/meta-udev.c',
     'backends/meta-udev.h',
+  ]
+endif
+
+if have_logind
+  mutter_sources += [
+    'backends/meta-launcher.c',
+    'backends/meta-launcher.h',
   ]
 endif
 