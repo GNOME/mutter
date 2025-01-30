GNOME/mutter

commit 6bd76eec0c32bd5d56e9ed33ace3ac8b55b1bf7a

ci: Use common-dependencies script to install argcomplete
It is a dependency of the new gdctl tool, and therefore no longer
confined to tests. Install it via the common-dependencies script,
so that the dependency is also satisfied for gnome-os and systemd
system extensions.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4233>

.gitlab-ci/install-common-dependencies.sh
@@ -35,6 +35,21 @@ pkgconf() {
   env "${ENV[@]}" pkgconf --env-only "$@"
 }
 
+pip_install() {
+  local pkg=$1
+
+  for destdir in "${DESTDIRS[@]}"; do
+    local pypaths=($destdir/usr/lib*/python3*/site-packages)
+    if ! pip3 list "${pypaths[@]/#/--path=}" | grep $pkg >/dev/null
+    then
+      pip3 install --ignore-installed \
+        --root-user-action ignore \
+        --prefix $destdir/usr \
+        $pkg
+    fi
+  done
+}
+
 TEMP=$(getopt \
   --name=$(basename $0) \
   --options='h' \
@@ -85,3 +100,5 @@ then
       https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas.git \
       master
 fi
+
+pip_install argcomplete

.gitlab-ci.yml
@@ -106,7 +106,7 @@ variables:
     - .skip-git-clone
   variables:
     FDO_DISTRIBUTION_VERSION: 41
-    BASE_TAG: '2025-01-29.0'
+    BASE_TAG: '2025-01-30.0'
     MUTTER_USER: 'meta-user'
     FDO_DISTRIBUTION_PACKAGES:
       clang
@@ -125,7 +125,6 @@ variables:
       zenity
       python3-dbusmock
       gnome-desktop-testing
-      python3-argcomplete
       ruff
 
     FDO_DISTRIBUTION_EXEC: |

commit 1463ddde66f22bbb7c68d749fac7e40b28b79fce

ci: Use sudo to run `pip install`
We can't assume the script is run as root, so request the required
privileges where needed.

Fixes: 6bd76eec0c ("ci: Use common-dependencies script to install argcomplete")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4234>

.gitlab-ci/install-common-dependencies.sh
@@ -42,7 +42,7 @@ pip_install() {
     local pypaths=($destdir/usr/lib*/python3*/site-packages)
     if ! pip3 list "${pypaths[@]/#/--path=}" | grep $pkg >/dev/null
     then
-      pip3 install --ignore-installed \
+      sudo pip3 install --ignore-installed \
         --root-user-action ignore \
         --prefix $destdir/usr \
         $pkg