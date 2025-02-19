GNOME/mutter

commit 163520c177f1cc746341a540c4ebdbfb77eae656

onscreen/native: Do not use modifiers if surface should be sharable
Importing a buffer allocated with a modifier into another GPU isn't
handled correctly yet in mutter. We'll want to fix that in the future,
for now just don't use modifiers in this case though, falling back to
GBM_BO_USE_LINEAR, to prevent issues due to mis-matched tiling
parameters.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3791
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4219>
(cherry picked from commit a211ed553bfeb9c527ff38f22c8c737765202a9f)

src/backends/native/meta-onscreen-native.c
@@ -2248,6 +2248,7 @@ create_surfaces_gbm (CoglOnscreen        *onscreen,
   struct gbm_device *gbm_device;
   struct gbm_surface *new_gbm_surface = NULL;
   EGLNativeWindowType egl_native_window;
+  gboolean should_be_sharable;
   EGLSurface new_egl_surface;
   EGLConfig egl_config;
   uint32_t format;
@@ -2259,6 +2260,8 @@ create_surfaces_gbm (CoglOnscreen        *onscreen,
   render_device_gbm = META_RENDER_DEVICE_GBM (renderer_gpu_data->render_device);
   gbm_device = meta_render_device_gbm_get_gbm_device (render_device_gbm);
 
+  should_be_sharable = should_surface_be_sharable (onscreen);
+
   if (!(cogl_renderer_egl->private_features &
         COGL_EGL_WINSYS_FEATURE_NO_CONFIG_CONTEXT) ||
       !choose_onscreen_egl_config (onscreen, &egl_config, error))
@@ -2268,7 +2271,8 @@ create_surfaces_gbm (CoglOnscreen        *onscreen,
                                     cogl_renderer_egl->edpy,
                                     egl_config);
 
-  if (meta_renderer_native_use_modifiers (renderer_native))
+  if (!should_be_sharable &&
+      meta_renderer_native_use_modifiers (renderer_native))
     modifiers = get_supported_modifiers (onscreen, format);
   else
     modifiers = NULL;
@@ -2287,7 +2291,7 @@ create_surfaces_gbm (CoglOnscreen        *onscreen,
     {
       uint32_t flags = GBM_BO_USE_SCANOUT | GBM_BO_USE_RENDERING;
 
-      if (should_surface_be_sharable (onscreen))
+      if (should_be_sharable)
         flags |= GBM_BO_USE_LINEAR;
 
       new_gbm_surface = gbm_surface_create (gbm_device,

commit 045342eb5a81228f6ab81d32b716abd1bcea14d9

onscreen/native: Prefer gbm_surface_create_with_modifiers even for linear
The Nvidia driver doesn't implement `gbm_surface_create` properly,
which was first mentioned in 9fee3a9ec1. So using `gbm_surface_create`
as a fallback for the linear case was failing with ENOSYS (Function not
implemented).

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3918

Fixes: 163520c177 ("onscreen/native: Do not use modifiers if surface should be sharable")

See also: https://forums.developer.nvidia.com/t/gbm-surface-create-fails-if-flags-0/279951

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4284>

src/backends/native/meta-onscreen-native.c
@@ -2407,11 +2407,23 @@ create_surfaces_gbm (CoglOnscreen        *onscreen,
                                     cogl_renderer_egl->edpy,
                                     egl_config);
 
-  if (!should_be_sharable &&
-      meta_renderer_native_use_modifiers (renderer_native))
-    modifiers = get_supported_modifiers (onscreen, format);
+  if (meta_renderer_native_use_modifiers (renderer_native))
+    {
+      if (should_be_sharable)
+        {
+          modifiers = g_array_sized_new (FALSE, FALSE, sizeof (uint64_t), 1);
+          g_array_set_size (modifiers, 1);
+          ((uint64_t *) modifiers->data)[0] = DRM_FORMAT_MOD_LINEAR;
+        }
+      else
+        {
+          modifiers = get_supported_modifiers (onscreen, format);
+        }
+    }
   else
-    modifiers = NULL;
+    {
+      modifiers = NULL;
+    }
 
   if (modifiers)
     {