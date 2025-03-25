GNOME/mutter

commit 005b9692277e49aa15a2fd17a1d1d1fe7271b280

wayland: Implement the cursor_shape_v1 protocol
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3668>

src/wayland/meta-wayland-pointer.c
@@ -101,6 +101,8 @@ struct _MetaWaylandPointer
   MetaWaylandSurface *cursor_surface;
   gulong cursor_surface_destroy_id;
 
+  MetaCursor cursor_shape;
+
   guint32 grab_button;
   guint32 grab_serial;
   guint32 grab_time;
@@ -1150,14 +1152,24 @@ meta_wayland_pointer_update_cursor_surface (MetaWaylandPointer *pointer)
 
   if (surface)
     {
-      MetaCursorSprite *cursor_sprite = NULL;
+      g_autoptr (MetaCursorSprite) cursor_sprite = NULL;
 
       if (pointer->cursor_surface)
         {
           MetaWaylandCursorSurface *cursor_surface =
             META_WAYLAND_CURSOR_SURFACE (pointer->cursor_surface->role);
+          MetaCursorSprite *sprite;
 
-          cursor_sprite = meta_wayland_cursor_surface_get_sprite (cursor_surface);
+          sprite = meta_wayland_cursor_surface_get_sprite (cursor_surface);
+          cursor_sprite = g_object_ref (sprite);
+        }
+      else if (pointer->cursor_shape != META_CURSOR_INVALID)
+        {
+          MetaCursorSpriteXcursor *sprite;
+
+          sprite = meta_cursor_sprite_xcursor_new (pointer->cursor_shape,
+                                                   cursor_tracker);
+          cursor_sprite = META_CURSOR_SPRITE (sprite);
         }
 
       meta_cursor_tracker_set_window_cursor (cursor_tracker, cursor_sprite);
@@ -1191,6 +1203,7 @@ meta_wayland_pointer_set_cursor_surface (MetaWaylandPointer *pointer,
     return;
 
   pointer->cursor_surface = cursor_surface;
+  pointer->cursor_shape = META_CURSOR_INVALID;
 
   if (prev_cursor_surface)
     {
@@ -1206,6 +1219,23 @@ meta_wayland_pointer_set_cursor_surface (MetaWaylandPointer *pointer,
                                   G_CALLBACK (ensure_update_cursor_surface),
                                   pointer);
     }
+
+  meta_wayland_pointer_update_cursor_surface (pointer);
+}
+
+void
+meta_wayland_pointer_set_cursor_shape (MetaWaylandPointer *pointer,
+                                       MetaCursor          shape)
+{
+  if (pointer->cursor_surface)
+    {
+      meta_wayland_surface_update_outputs (pointer->cursor_surface);
+      g_clear_signal_handler (&pointer->cursor_surface_destroy_id,
+                              pointer->cursor_surface);
+    }
+
+  pointer->cursor_surface = NULL;
+  pointer->cursor_shape = shape;
 
   meta_wayland_pointer_update_cursor_surface (pointer);
 }

src/meson.build
@@ -604,6 +604,8 @@ if have_wayland
     'wayland/meta-wayland-color-management.c',
     'wayland/meta-wayland-color-management.h',
     'wayland/meta-wayland-commit-timing.c',
+    'wayland/meta-wayland-cursor-shape.c',
+    'wayland/meta-wayland-cursor-shape.h',
     'wayland/meta-wayland-fifo.c',
     'wayland/meta-wayland-cursor-surface.c',
     'wayland/meta-wayland-cursor-surface.h',
@@ -1107,6 +1109,7 @@ if have_wayland
   #  - protocol stability ('private', 'stable' or 'unstable')
   #  - protocol version (if stability is 'unstable')
   wayland_protocols = [
+    ['cursor-shape-v1', 'private', ],
     ['drm-lease', 'staging', 'v1', ],
     ['fractional-scale', 'staging', 'v1', ],
     ['gtk-shell', 'private', ],

src/wayland/meta-wayland-cursor-shape.c
@@ -1,0 +1,330 @@
+/*
+ * Copyright 2024 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "config.h"
+
+#include "wayland/meta-wayland-cursor-shape.h"
+
+#include "core/meta-debug-control-private.h"
+#include "core/window-private.h"
+#include "wayland/meta-wayland-private.h"
+#include "wayland/meta-wayland-xdg-shell.h"
+#include "wayland/meta-wayland-versions.h"
+
+#include "cursor-shape-v1-server-protocol.h"
+
+typedef enum _MetaWaylandCursorShapeDeviceType
+{
+  META_WAYLAND_CURSOR_SHAPE_DEVICE_TYPE_POINTER,
+  META_WAYLAND_CURSOR_SHAPE_DEVICE_TYPE_TOOL,
+} MetaWaylandCursorShapeDeviceType;
+
+typedef struct _MetaWaylandCursorShapeDevice
+{
+  MetaWaylandCursorShapeDeviceType type;
+  union {
+    MetaWaylandPointer *pointer;
+    MetaWaylandTabletTool *tool;
+  };
+} MetaWaylandCursorShapeDevice;
+
+static const struct
+{
+  enum wp_cursor_shape_device_v1_shape shape;
+  MetaCursor cursor;
+} shape_map[] = {
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_DEFAULT, META_CURSOR_DEFAULT },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_CONTEXT_MENU, META_CURSOR_CONTEXT_MENU },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_HELP, META_CURSOR_HELP },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_POINTER, META_CURSOR_POINTER },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_PROGRESS, META_CURSOR_PROGRESS },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_WAIT, META_CURSOR_WAIT },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_CELL, META_CURSOR_CELL },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_CROSSHAIR, META_CURSOR_CROSSHAIR },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_TEXT, META_CURSOR_TEXT },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_VERTICAL_TEXT, META_CURSOR_VERTICAL_TEXT },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_ALIAS, META_CURSOR_ALIAS },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_COPY, META_CURSOR_COPY },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_MOVE, META_CURSOR_MOVE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NO_DROP, META_CURSOR_NO_DROP },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NOT_ALLOWED, META_CURSOR_NOT_ALLOWED },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_GRAB, META_CURSOR_GRAB },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_GRABBING, META_CURSOR_GRABBING },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_E_RESIZE, META_CURSOR_E_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_N_RESIZE, META_CURSOR_N_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NE_RESIZE, META_CURSOR_NE_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NW_RESIZE, META_CURSOR_NW_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_S_RESIZE, META_CURSOR_S_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_SE_RESIZE, META_CURSOR_SE_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_SW_RESIZE, META_CURSOR_SW_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_W_RESIZE, META_CURSOR_W_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_EW_RESIZE, META_CURSOR_EW_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NS_RESIZE, META_CURSOR_NS_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NESW_RESIZE, META_CURSOR_NESW_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NWSE_RESIZE, META_CURSOR_NWSE_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_COL_RESIZE, META_CURSOR_COL_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_ROW_RESIZE, META_CURSOR_ROW_RESIZE },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_ALL_SCROLL, META_CURSOR_ALL_SCROLL },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_ZOOM_IN, META_CURSOR_ZOOM_IN },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_ZOOM_OUT, META_CURSOR_ZOOM_OUT },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_DND_ASK, META_CURSOR_DND_ASK },
+  { WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_ALL_RESIZE, META_CURSOR_ALL_RESIZE },
+};
+
+static MetaCursor
+cursor_from_shape (enum wp_cursor_shape_device_v1_shape shape,
+                   int                                  version)
+{
+  if (shape < WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_DEFAULT)
+    return META_CURSOR_INVALID;
+
+  if (version <= 1 && shape > WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_ZOOM_OUT)
+    return META_CURSOR_INVALID;
+
+  if (shape >= G_N_ELEMENTS (shape_map))
+    return META_CURSOR_INVALID;
+
+  return shape_map[shape].cursor;
+}
+
+static MetaWaylandCursorShapeDevice *
+meta_wayland_cursor_shape_device_new_pointer (MetaWaylandPointer *pointer)
+{
+  MetaWaylandCursorShapeDevice *cursor_shape_device =
+    g_new0 (MetaWaylandCursorShapeDevice, 1);
+
+  cursor_shape_device->type = META_WAYLAND_CURSOR_SHAPE_DEVICE_TYPE_POINTER;
+  cursor_shape_device->pointer = pointer;
+  g_object_add_weak_pointer (G_OBJECT (pointer),
+                             (gpointer *) &cursor_shape_device->pointer);
+
+  return cursor_shape_device;
+}
+
+
+static MetaWaylandCursorShapeDevice *
+meta_wayland_cursor_shape_device_new_tool (MetaWaylandTabletTool *tool)
+{
+  MetaWaylandCursorShapeDevice *cursor_shape_device =
+    g_new0 (MetaWaylandCursorShapeDevice, 1);
+
+  cursor_shape_device->type = META_WAYLAND_CURSOR_SHAPE_DEVICE_TYPE_TOOL;
+  cursor_shape_device->tool = tool;
+  g_object_add_weak_pointer (G_OBJECT (tool),
+                             (gpointer *) &cursor_shape_device->tool);
+
+  return cursor_shape_device;
+}
+
+static void
+meta_wayland_cursor_shape_device_free (MetaWaylandCursorShapeDevice *cursor_shape_device)
+{
+  if (cursor_shape_device->type == META_WAYLAND_CURSOR_SHAPE_DEVICE_TYPE_POINTER)
+    g_clear_weak_pointer ((gpointer *) &cursor_shape_device->pointer);
+  if (cursor_shape_device->type == META_WAYLAND_CURSOR_SHAPE_DEVICE_TYPE_TOOL)
+    g_clear_weak_pointer ((gpointer *) &cursor_shape_device->tool);
+
+  g_free (cursor_shape_device);
+}
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (MetaWaylandCursorShapeDevice,
+                               meta_wayland_cursor_shape_device_free)
+
+static void
+cursor_shape_device_destructor (struct wl_resource *resource)
+{
+  MetaWaylandCursorShapeDevice *cursor_shape_device =
+    wl_resource_get_user_data (resource);
+
+  meta_wayland_cursor_shape_device_free (cursor_shape_device);
+}
+
+static void
+cursor_shape_device_destroy (struct wl_client   *client,
+                             struct wl_resource *resource)
+{
+  wl_resource_destroy (resource);
+}
+
+static void
+cursor_shape_device_set_shape (struct wl_client                     *client,
+                               struct wl_resource                   *resource,
+                               uint32_t                              serial,
+                               enum wp_cursor_shape_device_v1_shape  shape)
+{
+  MetaWaylandCursorShapeDevice *cursor_shape_device =
+    wl_resource_get_user_data (resource);
+  MetaCursor cursor = cursor_from_shape (shape,
+                                         wl_resource_get_version (resource));
+
+  if (cursor == META_CURSOR_INVALID)
+    {
+      wl_resource_post_error (resource,
+                              WP_CURSOR_SHAPE_DEVICE_V1_ERROR_INVALID_SHAPE,
+                              "wp_cursor_shape_device_v1@%d: "
+                              "the specified shape value is invalid",
+                              wl_resource_get_id (resource));
+      return;
+    }
+
+  if (cursor_shape_device->type == META_WAYLAND_CURSOR_SHAPE_DEVICE_TYPE_POINTER &&
+      cursor_shape_device->pointer)
+    {
+      MetaWaylandPointer *pointer = cursor_shape_device->pointer;
+
+      if (!meta_wayland_pointer_check_focus_serial (pointer, client, serial))
+        return;
+
+      meta_wayland_pointer_set_cursor_shape (pointer, cursor);
+    }
+
+  if (cursor_shape_device->type == META_WAYLAND_CURSOR_SHAPE_DEVICE_TYPE_TOOL &&
+      cursor_shape_device->tool)
+    {
+      MetaWaylandTabletTool *tool = cursor_shape_device->tool;
+
+      if (!meta_wayland_tablet_tool_check_focus_serial (tool, client, serial))
+        return;
+
+      meta_wayland_tablet_tool_set_cursor_shape (tool, cursor);
+    }
+}
+
+static const struct wp_cursor_shape_device_v1_interface cursor_shape_device_interface = {
+  cursor_shape_device_destroy,
+  cursor_shape_device_set_shape,
+};
+
+static void
+cursor_manager_destroy (struct wl_client   *client,
+                        struct wl_resource *resource)
+{
+  wl_resource_destroy (resource);
+}
+
+static void
+cursor_manager_get_pointer (struct wl_client   *client,
+                            struct wl_resource *resource,
+                            uint32_t            id,
+                            struct wl_resource *pointer_resource)
+{
+  MetaWaylandPointer *pointer = wl_resource_get_user_data (pointer_resource);
+  g_autoptr (MetaWaylandCursorShapeDevice) cursor_shape_device = NULL;
+  struct wl_resource *shape_device_resource;
+
+  cursor_shape_device = meta_wayland_cursor_shape_device_new_pointer (pointer);
+
+  shape_device_resource =
+    wl_resource_create (client,
+                        &wp_cursor_shape_device_v1_interface,
+                        wl_resource_get_version (resource),
+                        id);
+  wl_resource_set_implementation (shape_device_resource,
+                                  &cursor_shape_device_interface,
+                                  g_steal_pointer (&cursor_shape_device),
+                                  cursor_shape_device_destructor);
+}
+
+static void
+cursor_manager_get_tablet_tool_v2 (struct wl_client   *client,
+                                   struct wl_resource *resource,
+                                   uint32_t            id,
+                                   struct wl_resource *pointer_resource)
+{
+  MetaWaylandTabletTool *tool = wl_resource_get_user_data (resource);
+  g_autoptr (MetaWaylandCursorShapeDevice) cursor_shape_device = NULL;
+  struct wl_resource *shape_device_resource;
+
+  cursor_shape_device = meta_wayland_cursor_shape_device_new_tool (tool);
+
+  shape_device_resource =
+    wl_resource_create (client,
+                        &wp_cursor_shape_device_v1_interface,
+                        wl_resource_get_version (resource),
+                        id);
+  wl_resource_set_implementation (shape_device_resource,
+                                  &cursor_shape_device_interface,
+                                  g_steal_pointer (&cursor_shape_device),
+                                  cursor_shape_device_destructor);
+}
+
+static const struct wp_cursor_shape_manager_v1_interface cursor_shape_manager_interface = {
+  cursor_manager_destroy,
+  cursor_manager_get_pointer,
+  cursor_manager_get_tablet_tool_v2,
+};
+
+static void
+bind_cursor_shape (struct wl_client *client,
+                   void             *data,
+                   uint32_t          version,
+                   uint32_t          id)
+{
+  struct wl_resource *resource;
+
+  resource = wl_resource_create (client, &wp_cursor_shape_manager_v1_interface,
+                                 version, id);
+  wl_resource_set_implementation (resource, &cursor_shape_manager_interface,
+                                  NULL, NULL);
+}
+
+static void
+update_enabled (MetaWaylandCompositor *compositor)
+{
+  MetaDebugControl *debug_control =
+    meta_context_get_debug_control (compositor->context);
+  gboolean is_enabled =
+    meta_debug_control_is_cursor_shape_protocol_enabled (debug_control);
+  struct wl_global *global;
+
+  global = g_object_get_data (G_OBJECT (compositor),
+                              "-meta-wayland-cursor-shape");
+
+  if (is_enabled && global == NULL)
+    {
+
+      global = wl_global_create (compositor->wayland_display,
+                                 &wp_cursor_shape_manager_v1_interface,
+                                 META_WP_CURSOR_SHAPE_VERSION,
+                                 NULL, bind_cursor_shape);
+      if (global == NULL)
+        g_error ("Failed to register a global cursor-shape object");
+    }
+  else if (!is_enabled)
+    {
+      g_clear_pointer (&global, wl_global_destroy);
+    }
+
+  g_object_set_data (G_OBJECT (compositor),
+                     "-meta-wayland-cursor-shape",
+                     global);
+}
+
+void
+meta_wayland_init_cursor_shape (MetaWaylandCompositor *compositor)
+{
+  MetaDebugControl *debug_control =
+    meta_context_get_debug_control (compositor->context);
+
+  g_signal_connect_data (debug_control, "notify::cursor-shape-protocol",
+                         G_CALLBACK (update_enabled),
+                         compositor, NULL,
+                         G_CONNECT_SWAPPED | G_CONNECT_AFTER);
+
+  update_enabled (compositor);
+}

src/wayland/meta-wayland-cursor-shape.h
@@ -1,0 +1,23 @@
+/*
+ * Copyright 2024 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include "wayland/meta-wayland.h"
+
+void meta_wayland_init_cursor_shape (MetaWaylandCompositor *compositor);

src/wayland/meta-wayland-pointer.h
@@ -103,6 +103,9 @@ MetaWaylandSurface * meta_wayland_pointer_get_implicit_grab_surface (MetaWayland
 
 MetaWaylandPointerClient * meta_wayland_pointer_get_focus_client (MetaWaylandPointer *pointer);
 
+void meta_wayland_pointer_set_cursor_shape (MetaWaylandPointer *pointer,
+                                            MetaCursor          shape);
+
 gboolean meta_wayland_pointer_check_focus_serial (MetaWaylandPointer *pointer,
                                                   struct wl_client   *client,
                                                   uint32_t            serial);

src/wayland/meta-wayland-tablet-tool.c
@@ -53,6 +53,8 @@ struct _MetaWaylandTabletTool
   MetaCursorRenderer *cursor_renderer;
   MetaCursorSpriteXcursor *default_sprite;
 
+  MetaCursor cursor_shape;
+
   MetaWaylandSurface *current;
   guint32 pressed_buttons;
   guint32 button_count;
@@ -115,7 +117,10 @@ move_resources_for_client (struct wl_list   *destination,
 static void
 meta_wayland_tablet_tool_update_cursor_surface (MetaWaylandTabletTool *tool)
 {
-  MetaCursorSprite *cursor = NULL;
+  MetaBackend *backend = backend_from_tool (tool);
+  MetaCursorTracker *cursor_tracker =
+    meta_backend_get_cursor_tracker (backend);
+  g_autoptr (MetaCursorSprite) cursor_sprite = NULL;
 
   if (tool->cursor_renderer == NULL)
     return;
@@ -127,18 +132,29 @@ meta_wayland_tablet_tool_update_cursor_surface (MetaWaylandTabletTool *tool)
         {
           MetaWaylandCursorSurface *cursor_surface =
             META_WAYLAND_CURSOR_SURFACE (tool->cursor_surface->role);
+          MetaCursorSprite *sprite;
 
-          cursor = meta_wayland_cursor_surface_get_sprite (cursor_surface);
+          sprite = meta_wayland_cursor_surface_get_sprite (cursor_surface);
+          cursor_sprite = g_object_ref (sprite);
         }
-      else
-        cursor = NULL;
+      else if (tool->cursor_shape != META_CURSOR_INVALID)
+        {
+          MetaCursorSpriteXcursor *sprite;
+
+          sprite = meta_cursor_sprite_xcursor_new (tool->cursor_shape,
+                                                   cursor_tracker);
+          cursor_sprite = META_CURSOR_SPRITE (sprite);
+        }
     }
   else if (tool->current_tablet)
-    cursor = META_CURSOR_SPRITE (tool->default_sprite);
-  else
-    cursor = NULL;
+    {
+      MetaCursorSprite *sprite;
 
-  meta_cursor_renderer_set_cursor (tool->cursor_renderer, cursor);
+      sprite = META_CURSOR_SPRITE (tool->default_sprite);
+      cursor_sprite = g_object_ref (sprite);
+    }
+
+  meta_cursor_renderer_set_cursor (tool->cursor_renderer, cursor_sprite);
 }
 
 static void
@@ -160,6 +176,7 @@ meta_wayland_tablet_tool_set_cursor_surface (MetaWaylandTabletTool *tool,
     }
 
   tool->cursor_surface = surface;
+  tool->cursor_shape = META_CURSOR_INVALID;
 
   if (tool->cursor_surface)
     {
@@ -167,6 +184,27 @@ meta_wayland_tablet_tool_set_cursor_surface (MetaWaylandTabletTool *tool,
       wl_resource_add_destroy_listener (tool->cursor_surface->resource,
                                         &tool->cursor_surface_destroy_listener);
     }
+
+  meta_wayland_tablet_tool_update_cursor_surface (tool);
+}
+
+void
+meta_wayland_tablet_tool_set_cursor_shape (MetaWaylandTabletTool *tool,
+                                           MetaCursor             shape)
+{
+  if (tool->cursor_surface)
+    {
+      MetaWaylandCursorSurface *cursor_surface;
+
+      cursor_surface = META_WAYLAND_CURSOR_SURFACE (tool->cursor_surface->role);
+      meta_wayland_cursor_surface_set_renderer (cursor_surface, NULL);
+
+      meta_wayland_surface_update_outputs (tool->cursor_surface);
+      wl_list_remove (&tool->cursor_surface_destroy_listener.link);
+    }
+
+  tool->cursor_surface = NULL;
+  tool->cursor_shape = shape;
 
   meta_wayland_tablet_tool_update_cursor_surface (tool);
 }

src/wayland/meta-wayland-tablet-tool.h
@@ -65,6 +65,9 @@ MetaWaylandSurface * meta_wayland_tablet_tool_get_current_surface (MetaWaylandTa
 void meta_wayland_tablet_tool_focus_surface (MetaWaylandTabletTool *tool,
                                              MetaWaylandSurface    *surface);
 
+void meta_wayland_tablet_tool_set_cursor_shape (MetaWaylandTabletTool *tool,
+                                                MetaCursor             shape);
+
 gboolean meta_wayland_tablet_tool_check_focus_serial (MetaWaylandTabletTool *tool,
                                                       struct wl_client      *client,
                                                       uint32_t               serial);

src/wayland/meta-wayland-versions.h
@@ -65,3 +65,4 @@
 #define META_XDG_TOPLEVEL_DRAG_VERSION 1
 #define META_WP_COMMIT_TIMING_V1_VERSION 1
 #define META_WP_FIFO_V1_VERSION 1
+#define META_WP_CURSOR_SHAPE_VERSION 2

commit c4b0e2e93307baa6d53ddeb142f78256a49f33ea

wayland: Allow changing from a cursor shape to a NULL surface
Changing to a cursor shape would set the cursor surface to NULL, so
trying to disable the cursor by setting the cursor surface to NULL was
detected as no-change. This commit fixes the check by taking into
account if the cursor shape is currently set.

Also adds a ref-test for it.

Fixes: 005b969227 ("wayland: Implement the cursor_shape_v1 protocol")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/3997
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4358>

src/wayland/meta-wayland-pointer.c
@@ -1199,7 +1199,8 @@ meta_wayland_pointer_set_cursor_surface (MetaWaylandPointer *pointer,
 
   prev_cursor_surface = pointer->cursor_surface;
 
-  if (prev_cursor_surface == cursor_surface)
+  if (prev_cursor_surface == cursor_surface &&
+      pointer->cursor_shape == META_CURSOR_INVALID)
     return;
 
   pointer->cursor_surface = cursor_surface;

src/tests/wayland-test-clients/cursor-shape.c
@@ -128,6 +128,9 @@ main (int    argc,
                                            WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_DEFAULT);
       wp_cursor_shape_device_v1_destroy (cursor_shape_device);
       wait_for_view_verified (display, 1);
+      /* make sure disabling the cursor works */
+      wl_pointer_set_cursor (pointer, serial, NULL, 0, 0);
+      wait_for_view_verified (display, 3);
 
       return EXIT_SUCCESS;
     }