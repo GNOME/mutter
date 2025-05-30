GNOME/mutter

commit 4d194b8e1da555c14e1dc092879e9580f25b6986

frames/window-tracker: Handle override-redirect attribute change.
If a client creates windows with override-redirect attribute, but later
changes it to allow its windows to be managed, the window tracker will
still not be tracking _MUTTER_NEEDS_FRAME property changes and any
decoration change will be broken.

This reproduces with Wine, if a window is first created as a popup, to
later change to a framed window. Wine always creates its windows with
override-redirect, and only later decides to let them be managed.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4287>

src/frames/meta-window-tracker.c
@@ -263,6 +263,21 @@ on_xevent (GdkDisplay *display,
       xwindow = xevent->xcreatewindow.window;
       listen_set_up_frame (window_tracker, xwindow);
     }
+  else if (xevent->type == ConfigureNotify &&
+           xevent->xconfigure.event == xroot)
+    {
+      gboolean has_frame;
+
+      xwindow = xevent->xconfigure.window;
+      has_frame =
+        g_hash_table_contains (window_tracker->frames,
+                               GUINT_TO_POINTER (xwindow));
+
+      if (!xevent->xconfigure.override_redirect && !has_frame)
+        listen_set_up_frame (window_tracker, xwindow);
+      else if (xevent->xconfigure.override_redirect && has_frame)
+        remove_frame (window_tracker, xwindow);
+    }
   else if (xevent->type == DestroyNotify)
     {
       xwindow = xevent->xdestroywindow.window;

commit a1d6e5771bbd704c65c8079310102637a5aab071

frames/window-tracker: Move frame existence check inside of set_up_frame
Avoiding creating frames multiple times when listen_set_up_frame is
called from a ConfigureNotify event after the frame has already been
created.

Fixes: 4d194b8e1 ("frames/window-tracker: Handle override-redirect attribute change.")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4138
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4465>

src/frames/meta-window-tracker.c
@@ -125,6 +125,11 @@ set_up_frame (MetaWindowTracker *window_tracker,
   unsigned long data[1];
   GtkWidget *frame;
 
+  frame = g_hash_table_lookup (window_tracker->client_windows,
+                               GUINT_TO_POINTER (xwindow));
+  if (frame)
+    return;
+
   /* Double check it's not a request for a frame of our own. */
   if (g_hash_table_contains (window_tracker->frames,
                              GUINT_TO_POINTER (xwindow)))
@@ -287,9 +292,7 @@ on_xevent (GdkDisplay *display,
            xevent->xproperty.atom ==
            gdk_x11_get_xatom_by_name_for_display (display, "_MUTTER_NEEDS_FRAME"))
     {
-      if (xevent->xproperty.state == PropertyNewValue &&
-          !g_hash_table_contains (window_tracker->client_windows,
-                                  GUINT_TO_POINTER (xwindow)))
+      if (xevent->xproperty.state == PropertyNewValue)
         set_up_frame (window_tracker, xwindow);
       else if (xevent->xproperty.state == PropertyDelete)
         remove_frame (window_tracker, xwindow);