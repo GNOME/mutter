GNOME/mutter

commit 863b31cae9be5e8e0467f1a9f93e7348d41f7e88

output/kms: Add meta_output_kms_from_kms_connector()
Add a function that returns a MetaOutputKms (if any) from a
MetaKmsConnector.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4112>

src/backends/native/meta-output-kms.c
@@ -47,6 +47,8 @@ struct _MetaOutputKms
 
 G_DEFINE_TYPE (MetaOutputKms, meta_output_kms, META_TYPE_OUTPUT_NATIVE)
 
+static GQuark kms_connector_output_kms_quark;
+
 MetaKmsConnector *
 meta_output_kms_get_kms_connector (MetaOutputKms *output_kms)
 {
@@ -88,6 +90,13 @@ meta_output_kms_can_clone (MetaOutputKms *output_kms,
     return FALSE;
 
   return TRUE;
+}
+
+MetaOutputKms *
+meta_output_kms_from_kms_connector (MetaKmsConnector *connector)
+{
+  return g_object_get_qdata (G_OBJECT (connector),
+                             kms_connector_output_kms_quark);
 }
 
 static GBytes *
@@ -533,6 +542,16 @@ meta_output_kms_new (MetaGpuKms        *gpu_kms,
     {
       meta_output_unassign_crtc (output);
     }
+
+  if (!kms_connector_output_kms_quark)
+    {
+      kms_connector_output_kms_quark =
+        g_quark_from_static_string ("kms-connector-output-kms-quark");
+    }
+
+  g_object_set_qdata (G_OBJECT (kms_connector),
+                      kms_connector_output_kms_quark,
+                      output_kms);
 
   return output_kms;
 }

src/backends/native/meta-output-kms.h
@@ -39,6 +39,8 @@ MetaKmsConnector * meta_output_kms_get_kms_connector (MetaOutputKms *output_kms)
 
 uint32_t meta_output_kms_get_connector_id (MetaOutputKms *output_kms);
 
+MetaOutputKms * meta_output_kms_from_kms_connector (MetaKmsConnector *connector);
+
 MetaOutputKms * meta_output_kms_new (MetaGpuKms        *gpu_kms,
                                      MetaKmsConnector  *kms_connector,
                                      MetaOutput        *old_output,

commit eca90c0b38ea761ce9011e6ae5ffeac532c00887

kms: Unlink outputs from unused connectors in update_outputs
Fixes meta_output_kms_from_kms_connector returning a stale pointer to an
already-destroyed MetaOutputKms object after a monitor was disconnected.

Fixes: 863b31ca ("output/kms: Add meta_output_kms_from_kms_connector()")

v2:
* Handle it from update_outputs. (Jonas Ådahl)
v3:
* Rename to meta_unlink_kms_connector. (Jonas Ådahl)
* Always call meta_unlink_kms_connector first thing in the
  update_outputs loop. (Jonas Ådahl)
* Bail from meta_unlink_kms_connector if the quark isn't initialized yet
  (before the first time meta_output_kms_new is called), to avoid
  g_object_set_qdata logging a crtitical warning.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4474>

src/backends/native/meta-gpu-kms.c
@@ -351,6 +351,8 @@ update_outputs (MetaGpuKms *gpu_kms)
       MetaOutput *old_output;
       GError *error = NULL;
 
+      meta_unlink_kms_connector (kms_connector);
+
       if (!meta_kms_connector_get_current_state (kms_connector))
         continue;
 

src/backends/native/meta-output-kms.c
@@ -99,6 +99,17 @@ meta_output_kms_from_kms_connector (MetaKmsConnector *connector)
                              kms_connector_output_kms_quark);
 }
 
+void
+meta_unlink_kms_connector (MetaKmsConnector *connector)
+{
+  if (!kms_connector_output_kms_quark)
+    return;
+
+  g_object_set_qdata (G_OBJECT (connector),
+                      kms_connector_output_kms_quark,
+                      NULL);
+}
+
 static GBytes *
 meta_output_kms_read_edid (MetaOutputNative *output_native)
 {

src/backends/native/meta-output-kms.h
@@ -41,6 +41,8 @@ uint32_t meta_output_kms_get_connector_id (MetaOutputKms *output_kms);
 
 MetaOutputKms * meta_output_kms_from_kms_connector (MetaKmsConnector *connector);
 
+void meta_unlink_kms_connector (MetaKmsConnector *connector);
+
 MetaOutputKms * meta_output_kms_new (MetaGpuKms        *gpu_kms,
                                      MetaKmsConnector  *kms_connector,
                                      MetaOutput        *old_output,