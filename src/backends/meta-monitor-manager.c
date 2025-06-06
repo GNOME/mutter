GNOME/mutter

commit e52e5aee53a86c3bf110148926898fe91c873358

monitor-manager: Add input sanity checking to public functions
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4437>

src/backends/meta-monitor-manager.c
@@ -2103,6 +2103,9 @@ restore_previous_config (MetaMonitorManager *manager)
 int
 meta_monitor_manager_get_display_configuration_timeout (MetaMonitorManager *manager)
 {
+  g_return_val_if_fail (META_IS_MONITOR_MANAGER (manager),
+                        DEFAULT_DISPLAY_CONFIGURATION_TIMEOUT);
+
   return DEFAULT_DISPLAY_CONFIGURATION_TIMEOUT;
 }
 
@@ -4407,12 +4410,15 @@ meta_monitor_manager_get_monitor_for_connector (MetaMonitorManager *manager,
 {
   GList *l;
 
+  g_return_val_if_fail (META_IS_MONITOR_MANAGER (manager), -1);
+  g_return_val_if_fail (connector != NULL, -1);
+
   for (l = manager->monitors; l; l = l->next)
     {
       MetaMonitor *monitor = l->data;
 
       if (meta_monitor_is_active (monitor) &&
-          g_str_equal (connector, meta_monitor_get_connector (monitor)))
+          g_strcmp0 (connector, meta_monitor_get_connector (monitor)))
         return meta_monitor_get_logical_monitor (monitor)->number;
     }
 
@@ -4510,6 +4516,7 @@ meta_monitor_manager_switch_config (MetaMonitorManager          *manager,
     meta_monitor_manager_get_instance_private (manager);
   SwitchConfigData *data;
 
+  g_return_if_fail (META_IS_MONITOR_MANAGER (manager));
   g_return_if_fail (config_type != META_MONITOR_SWITCH_CONFIG_UNKNOWN);
 
   data = g_new0 (SwitchConfigData, 1);
@@ -4526,6 +4533,8 @@ meta_monitor_manager_switch_config (MetaMonitorManager          *manager,
 gboolean
 meta_monitor_manager_can_switch_config (MetaMonitorManager *manager)
 {
+  g_return_val_if_fail (META_IS_MONITOR_MANAGER (manager), FALSE);
+
   return (!meta_backend_is_lid_closed (manager->backend) &&
           g_list_length (manager->monitors) > 1);
 }
@@ -4533,6 +4542,9 @@ meta_monitor_manager_can_switch_config (MetaMonitorManager *manager)
 MetaMonitorSwitchConfigType
 meta_monitor_manager_get_switch_config (MetaMonitorManager *manager)
 {
+  g_return_val_if_fail (META_IS_MONITOR_MANAGER (manager),
+                        META_MONITOR_SWITCH_CONFIG_UNKNOWN);
+
   return manager->current_switch_config;
 }
 

commit 5d2df522ac0a7e43fdf056ca3657357dc1a8ac7e

monitor-manager: Fix regression in get_monitor_for_connector()
Commit e52e5aee5 replaced g_str_equal() with g_strcmp0(), however the
former returns true if the strings are equal whereas the latter will
return a non-zero value if they differ, causing a regression in
meta_monitor_manager_get_monitor_for_connector().

Fix the issue by simply g_strcmp0() == 0 which would be the expected
equivalent to g_str_equal().

Fixes: e52e5aee5 - monitor-manager: Add input sanity checking to public
                   functions

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4162
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4488>

src/backends/meta-monitor-manager.c
@@ -4304,7 +4304,7 @@ meta_monitor_manager_get_monitor_for_connector (MetaMonitorManager *manager,
       MetaMonitor *monitor = l->data;
 
       if (meta_monitor_is_active (monitor) &&
-          g_strcmp0 (connector, meta_monitor_get_connector (monitor)))
+          g_strcmp0 (connector, meta_monitor_get_connector (monitor)) == 0)
         return meta_monitor_get_logical_monitor (monitor)->number;
     }
 