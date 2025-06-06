fbde58d1183dc3fbf21209d19b880018b4de1754
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2025-06-06 12:58:29 +0200

    Add a speed limit to Visual alerts to comply with EAA
    The European Accessibility Act, in its Annex I, Section I, 2.J,
    specifies that products "shall avoid triggering photosensitive
    seizures".
    
    According to the Web Content Accessibility Guidelines (WCAG),
    any element that flashes in the screen must have a maximum
    period of 3Hz to avoid the risk of Photosensitivity Seizures.
    
    When the Visual Alerts option is enabled in Mutter/Gnome Shell,
    any system alarm (including the terminal bell) will produce a
    flash in either the whole screen or the specific window that
    triggered the alarm.
    
    The problem is that there is no limit to the number of alarms
    per second that can be triggered, so it is possible that a
    large number of alarms could produce a flicker faster than
    the afforementioned 3Hz. Just a simple BASH script like the
    following one produces a flicker of 8-9Hz, well inside the
    dangerous range.
    
    WARNING: THIS TEST WILL PRODUCE FAST FLICKERING IN YOUR
    SCREEN. DON'T RUN IT IF YOU ARE PHOTOSENSITIVE.
    
    Enable Visual alerts in the Accessibility -> Auditory section
    in Gnome Control Center. Then, open a terminal and type:
    
        while [ 1 ] ; do tput bel; done
    
    The screen will flash at about 8-9Hz.
    
    To stop this test, just press Ctrl+C
    
    This patch ensures that no more than two flashes per second are
    produced. The limit is global, to avoid fast patterns between
    different screens/windows.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4487>
    (cherry picked from commit cc2643762019900bb59175501e0c95c0ccbd8e9c)

--- src/compositor/compositor.c
+++ src/compositor/compositor.c
@@ -1282,7 +1282,8 @@ flash_out_completed (ClutterTimeline *timeline,
 
 void
 meta_compositor_flash_display (MetaCompositor *compositor,
-                               MetaDisplay    *display)
+                               MetaDisplay    *display,
+                               int             n_flashes)
 {
   MetaBackend *backend;
   ClutterActor *stage;
@@ -1308,7 +1309,7 @@ meta_compositor_flash_display (MetaCompositor *compositor,
 
   transition = clutter_actor_get_transition (flash, "opacity");
   clutter_timeline_set_auto_reverse (CLUTTER_TIMELINE (transition), TRUE);
-  clutter_timeline_set_repeat_count (CLUTTER_TIMELINE (transition), 2);
+  clutter_timeline_set_repeat_count (CLUTTER_TIMELINE (transition), n_flashes);
 
   g_signal_connect (transition, "stopped",
                     G_CALLBACK (flash_out_completed), flash);
@@ -1327,7 +1328,8 @@ window_flash_out_completed (ClutterTimeline *timeline,
 
 void
 meta_compositor_flash_window (MetaCompositor *compositor,
-                              MetaWindow     *window)
+                              MetaWindow     *window,
+                              int             n_flashes)
 {
   ClutterActor *window_actor =
     CLUTTER_ACTOR (meta_window_actor_from_window (window));
@@ -1356,7 +1358,7 @@ meta_compositor_flash_window (MetaCompositor *compositor,
   if (transition)
     {
       clutter_timeline_set_auto_reverse (CLUTTER_TIMELINE (transition), TRUE);
-      clutter_timeline_set_repeat_count (CLUTTER_TIMELINE (transition), 2);
+      clutter_timeline_set_repeat_count (CLUTTER_TIMELINE (transition), n_flashes);
 
       g_signal_connect (transition, "stopped",
                         G_CALLBACK (window_flash_out_completed), flash);
