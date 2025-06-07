d8e6293390a576867a242ebb1e74a36b8d4218c5
voidanix <voidanix@keyedlimepie.org>
2025-06-07 22:01:01 +0200

    backend: Advertise Broadcast RGB support
    This would allow for changes to the RGB quantization range.
    
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4489>

--- src/backends/meta-monitor-manager.c
+++ src/backends/meta-monitor-manager.c
@@ -2240,6 +2240,7 @@ meta_monitor_manager_handle_get_current_state (MetaDBusDisplayConfig *skeleton,
       gboolean is_builtin;
       gboolean is_for_lease;
       MetaColorMode color_mode;
+      MetaOutputRGBRange rgb_range;
       const char *display_name;
 
       current_mode = meta_monitor_get_current_mode (monitor);
@@ -2374,6 +2375,11 @@ meta_monitor_manager_handle_get_current_state (MetaDBusDisplayConfig *skeleton,
       g_variant_builder_add (&monitor_properties_builder, "{sv}",
                              "supported-color-modes",
                              generate_color_modes_variant (monitor));
+
+      rgb_range = meta_monitor_get_rgb_range (monitor);
+      g_variant_builder_add (&monitor_properties_builder, "{sv}",
+                             "rgb-range",
+                             g_variant_new_uint32 (rgb_range));
 
       g_variant_builder_add (&monitors_builder, MONITOR_FORMAT,
                              monitor_spec->connector,
@@ -2671,6 +2677,8 @@ create_monitor_config_from_variant (MetaMonitorManager *manager,
   gboolean set_underscanning = FALSE;
   MetaColorMode color_mode = META_COLOR_MODE_DEFAULT;
   uint32_t color_mode_value;
+  MetaOutputRGBRange rgb_range = META_OUTPUT_RGB_RANGE_UNKNOWN;
+  uint32_t rgb_range_value;
 
   g_variant_get (monitor_config_variant, "(ss@a{sv})",
                  &connector,
@@ -2710,6 +2718,10 @@ create_monitor_config_from_variant (MetaMonitorManager *manager,
                         &color_mode_value))
     color_mode = color_mode_value;
 
+  if (g_variant_lookup (properties_variant, "rgb-range", "u",
+                        &rgb_range_value))
+    rgb_range = rgb_range_value;
+
   monitor_spec = meta_monitor_spec_clone (meta_monitor_get_spec (monitor));
 
   monitor_mode_spec = g_new0 (MetaMonitorModeSpec, 1);
@@ -2721,6 +2733,7 @@ create_monitor_config_from_variant (MetaMonitorManager *manager,
     .mode_spec = monitor_mode_spec,
     .enable_underscanning = enable_underscanning,
     .color_mode = color_mode,
+    .rgb_range = rgb_range,
   };
 
   return monitor_config;
