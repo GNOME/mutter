GNOME/mutter

commit 4d194b8e1da555c14e1dc092879e9580f25b6986

frames/window-tracker: Handle override-redirect attribute change.
If a client creates windows with override-redirect attribute, but later
changes it to allow its windows to be managed, the window tracker will
still not be tracking _MUTTER_NEEDS_FRAME property changes and any
decoration change will be broken.

This reproduces with Wine, if a window is first created as a popup, to
later change to a framed window. Wine always creates its windows with
override-redirect, and only later decides to let them be managed.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4287>

src/frames/meta-window-tracker.c
@@ -263,6 +263,21 @@ on_xevent (GdkDisplay *display,
       xwindow = xevent->xcreatewindow.window;
       listen_set_up_frame (window_tracker, xwindow);
     }
+  else if (xevent->type == ConfigureNotify &&
+           xevent->xconfigure.event == xroot)
+    {
+      gboolean has_frame;
+
+      xwindow = xevent->xconfigure.window;
+      has_frame =
+        g_hash_table_contains (window_tracker->frames,
+                               GUINT_TO_POINTER (xwindow));
+
+      if (!xevent->xconfigure.override_redirect && !has_frame)
+        listen_set_up_frame (window_tracker, xwindow);
+      else if (xevent->xconfigure.override_redirect && has_frame)
+        remove_frame (window_tracker, xwindow);
+    }
   else if (xevent->type == DestroyNotify)
     {
       xwindow = xevent->xdestroywindow.window;

commit 6a9b2e0be02725267bcda696e1044383f9f81e63

frames/window-tracker: Ignore ConfigureNotify on the root window
Based on a patch from Sebastian Keller

Fixes: 4d194b8e1 ("frames/window-tracker: Handle override-redirect attribute change.")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4472>

src/frames/meta-window-tracker.c
@@ -269,7 +269,8 @@ on_xevent (GdkDisplay *display,
       listen_set_up_frame (window_tracker, xwindow);
     }
   else if (xevent->type == ConfigureNotify &&
-           xevent->xconfigure.event == xroot)
+           xevent->xconfigure.event == xroot &&
+           xevent->xconfigure.window != xroot)
     {
       gboolean has_frame;
 