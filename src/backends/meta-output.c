GNOME/mutter

commit 5a3e1f900965b4fd9783c7ffac895dab2346bb19

gpu: Move output/ctrt/modes lifetime management to backend
This allows more dynamic management that also more closely follows how
the actual backend works.

For example, CRTC's of a KMS device never change, only outputs come and
go. Make the associated MetaCrtcKms instances reflect this. Outputs
represents the end point of a connector, so they are still regenerated.

This means we also need to unassign disposed outputs, which before
wasn't needed due to the MetaCrtcKms instance being recreated every
time.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4430>

src/backends/meta-output.c
@@ -448,6 +448,8 @@ meta_output_dispose (GObject *object)
   MetaOutput *output = META_OUTPUT (object);
   MetaOutputPrivate *priv = meta_output_get_instance_private (output);
 
+  meta_output_unassign_crtc (output);
+  priv->monitor = NULL;
   g_clear_object (&priv->crtc);
 
   G_OBJECT_CLASS (meta_output_parent_class)->dispose (object);

src/backends/native/meta-gpu-kms.c
@@ -236,7 +236,7 @@ setup_output_clones (MetaGpu *gpu)
 }
 
 static void
-init_modes (MetaGpuKms *gpu_kms)
+update_modes (MetaGpuKms *gpu_kms)
 {
   MetaGpu *gpu = META_GPU (gpu_kms);
   MetaKmsDevice *kms_device = gpu_kms->kms_device;
@@ -333,7 +333,7 @@ init_crtcs (MetaGpuKms *gpu_kms)
 }
 
 static void
-init_outputs (MetaGpuKms *gpu_kms)
+update_outputs (MetaGpuKms *gpu_kms)
 {
   MetaGpu *gpu = META_GPU (gpu_kms);
   GList *old_outputs;
@@ -389,14 +389,8 @@ meta_gpu_kms_read_current (MetaGpu  *gpu,
 {
   MetaGpuKms *gpu_kms = META_GPU_KMS (gpu);
 
-  /* Note: we must not free the public structures (output, crtc, monitor
-     mode and monitor info) here, they must be kept alive until the API
-     users are done with them after we emit monitors-changed, and thus
-     are freed by the platform-independent layer. */
-
-  init_modes (gpu_kms);
-  init_crtcs (gpu_kms);
-  init_outputs (gpu_kms);
+  update_modes (gpu_kms);
+  update_outputs (gpu_kms);
 
   return TRUE;
 }
@@ -419,6 +413,8 @@ meta_gpu_kms_new (MetaBackendNative  *backend_native,
                           NULL);
 
   gpu_kms->kms_device = kms_device;
+
+  init_crtcs (gpu_kms);
 
   meta_gpu_kms_read_current (META_GPU (gpu_kms), NULL);
 

src/backends/meta-gpu.c
@@ -68,24 +68,7 @@ gboolean
 meta_gpu_read_current (MetaGpu  *gpu,
                        GError  **error)
 {
-  MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
-  gboolean ret;
-  GList *old_outputs;
-  GList *old_crtcs;
-  GList *old_modes;
-
-  /* TODO: Get rid of this when objects incref:s what they need instead */
-  old_outputs = priv->outputs;
-  old_crtcs = priv->crtcs;
-  old_modes = priv->modes;
-
-  ret = META_GPU_GET_CLASS (gpu)->read_current (gpu, error);
-
-  g_list_free_full (old_outputs, g_object_unref);
-  g_list_free_full (old_modes, g_object_unref);
-  g_list_free_full (old_crtcs, g_object_unref);
-
-  return ret;
+  return META_GPU_GET_CLASS (gpu)->read_current (gpu, error);
 }
 
 MetaBackend *
@@ -122,19 +105,21 @@ meta_gpu_get_modes (MetaGpu *gpu)
 
 void
 meta_gpu_take_outputs (MetaGpu *gpu,
-                       GList   *outputs)
+                          GList   *outputs)
 {
   MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
 
+  g_clear_list (&priv->outputs, g_object_unref);
   priv->outputs = outputs;
 }
 
 void
 meta_gpu_take_crtcs (MetaGpu *gpu,
-                    GList   *crtcs)
+                     GList   *crtcs)
 {
   MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
 
+  g_clear_list (&priv->crtcs, g_object_unref);
   priv->crtcs = crtcs;
 }
 
@@ -144,6 +129,7 @@ meta_gpu_take_modes (MetaGpu *gpu,
 {
   MetaGpuPrivate *priv = meta_gpu_get_instance_private (gpu);
 
+  g_clear_list (&priv->modes, g_object_unref);
   priv->modes = modes;
 }
 

commit 8e4f5812fcb41c4037ad1262dfb79388ab437e44

gpu/kms: Dispose old outputs when replacing
We must disassociate the CRTC and old output when we're replacing
outputs on hotplug. A new association will be set up when applying
the new assignments from the to-be applied configuration. We can't rely
on the last reference to be released until it's too late because
MetaOnscreenNative holds on to it until an async page flip callback.

This also needs a warning about trying to unset a output's monitor if
there wasn't one, because disposed monitor's will try to do this on the
defunct output ref it has before releasing them.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4177
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4148
Fixes: 5a3e1f9009 ("gpu: Move output/ctrt/modes lifetime management to backend")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4500>

src/backends/meta-output.c
@@ -149,8 +149,6 @@ meta_output_unset_monitor (MetaOutput *output)
 {
   MetaOutputPrivate *priv = meta_output_get_instance_private (output);
 
-  g_warn_if_fail (priv->monitor);
-
   priv->monitor = NULL;
 }
 

src/backends/native/meta-gpu-kms.c
@@ -377,6 +377,8 @@ update_outputs (MetaGpuKms *gpu_kms)
         }
     }
 
+  g_list_foreach (old_outputs, (GFunc) g_object_run_dispose, NULL);
+
   /* Sort the outputs for easier handling in MetaMonitorConfig */
   outputs = g_list_sort (outputs, compare_outputs);
   meta_gpu_take_outputs (gpu, outputs);