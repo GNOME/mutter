GNOME/mutter

commit 213200a766837d2ed1f2161c7db3ae25137b918b

ci: Install glycin-2 if necessary
This is too new to be present in F42, so install it from
git if necessary.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4554>

.gitlab-ci/install-common-dependencies.sh
@@ -149,6 +149,14 @@ then
       2.85.2
 fi
 
+if ! pkgconf --atleast-version 2.0i.beta.2 glycin-2
+then
+    ./$SCRIPTS_DIR/install-meson-project.sh \
+      "${OPTIONS[@]}" \
+      https://gitlab.gnome.org/GNOME/glycin.git \
+      2.0.beta.2
+fi
+
 if ! check_gsettings_key org.gnome.desktop.screensaver restart-enabled
 then
     ./$SCRIPTS_DIR/install-meson-project.sh \

.gitlab-ci.yml
@@ -111,7 +111,7 @@ variables:
     - .skip-git-clone
   variables:
     FDO_DISTRIBUTION_VERSION: 42
-    BASE_TAG: '2025-08-08.0.fixate-versions'
+    BASE_TAG: '2025-08-08.1'
     MUTTER_USER: 'meta-user'
     FDO_DISTRIBUTION_PACKAGES:
       accountsservice-devel
@@ -134,6 +134,10 @@ variables:
       ruff
       mypy
       umockdev-devel
+      libheif-devel
+      libjxl-devel
+      lcms2-devel
+      libseccomp-devel
 
     FDO_DISTRIBUTION_EXEC: |
       set -e

commit c6c2cd117ddfecf90b3e55cc543af2a657dde900

ci: Fix glycin-2 version check
The spare "i" that sneaked into the version means glycin
is built whenever the script is run, whoops.

Fixes: 213200a766 ("ci: Install glycin-2 if necessary")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4609>

.gitlab-ci/install-common-dependencies.sh
@@ -149,7 +149,7 @@ then
       2.85.2
 fi
 
-if ! pkgconf --atleast-version 2.0i.beta.2 glycin-2
+if ! pkgconf --atleast-version 2.0.beta.2 glycin-2
 then
     ./$SCRIPTS_DIR/install-meson-project.sh \
       "${OPTIONS[@]}" \