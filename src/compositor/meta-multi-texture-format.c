GNOME/mutter

commit 775b605566dfac8ec2e8ea6a775b5e5f70779e5e

multi-texture-format: Add support for 10/12/16 bit SW decoder YUV formats
Which are used for e.g. HDR10 video by software decoders like dav1d,
libav, libaom and libvpx.

Having native support for these formats - and thus usually better
performance - helps in various use-cases. It can facilitate HDR10
adoption on slow/old hardware or make CI testing easier. The 422/444
variants are often not covered by hardware decoders but are used e.g.
in post production with codecs like Apple ProRes.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4348>

src/compositor/meta-multi-texture-format.c
@@ -233,6 +233,30 @@ static const char y_u_v_shader[] =
   "  return yuva;\n"
   "}\n";
 
+/* Shader for 1 Y-plane, 1 U-plane and 1 V-plane, shifted by 6 bits (2^6=64) */
+static const char y_u_v_shader_10bit_lsb[] =
+  "vec4 sample_y_u_v_10bit_lsb(vec4 unused)\n"
+  "{\n"
+  "  vec4 yuva;\n"
+  "  yuva.a = 1.0;\n"
+  "  yuva.x = texture2D(cogl_sampler0, cogl_tex_coord0_in.st).x * 64.0;\n"
+  "  yuva.y = texture2D(cogl_sampler1, cogl_tex_coord0_in.st).x * 64.0;\n"
+  "  yuva.z = texture2D(cogl_sampler2, cogl_tex_coord0_in.st).x * 64.0;\n"
+  "  return yuva;\n"
+  "}\n";
+
+/* Shader for 1 Y-plane, 1 U-plane and 1 V-plane, shifted by 4 bits (2^4=16) */
+static const char y_u_v_shader_12bit_lsb[] =
+  "vec4 sample_y_u_v_12bit_lsb(vec4 unused)\n"
+  "{\n"
+  "  vec4 yuva;\n"
+  "  yuva.a = 1.0;\n"
+  "  yuva.x = texture2D(cogl_sampler0, cogl_tex_coord0_in.st).x * 16.0;\n"
+  "  yuva.y = texture2D(cogl_sampler1, cogl_tex_coord0_in.st).x * 16.0;\n"
+  "  yuva.z = texture2D(cogl_sampler2, cogl_tex_coord0_in.st).x * 16.0;\n"
+  "  return yuva;\n"
+  "}\n";
+
 typedef struct _MetaMultiTextureFormatFullInfo
 {
   MetaMultiTextureFormatInfo info;
@@ -342,6 +366,132 @@ static MetaMultiTextureFormatFullInfo multi_format_table[] = {
     .info = {
       .n_planes = 3,
       .subformats = { COGL_PIXEL_FORMAT_R_8, COGL_PIXEL_FORMAT_R_8, COGL_PIXEL_FORMAT_R_8 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 1, 1 },
+      .vsub = { 1, 1, 1 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S010] = {
+    .name = "S010",
+    .snippet = {
+      .source = y_u_v_shader_10bit_lsb,
+      .name = "sample_y_u_v_10bit_lsb",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 2, 2 },
+      .vsub = { 1, 2, 2 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S210] = {
+    .name = "S210",
+    .snippet = {
+      .source = y_u_v_shader_10bit_lsb,
+      .name = "sample_y_u_v_10bit_lsb",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 2, 2 },
+      .vsub = { 1, 1, 1 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S410] = {
+    .name = "S410",
+    .snippet = {
+      .source = y_u_v_shader_10bit_lsb,
+      .name = "sample_y_u_v_10bit_lsb",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 1, 1 },
+      .vsub = { 1, 1, 1 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S012] = {
+    .name = "S012",
+    .snippet = {
+      .source = y_u_v_shader_12bit_lsb,
+      .name = "sample_y_u_v_12bit_lsb",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 2, 2 },
+      .vsub = { 1, 2, 2 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S212] = {
+    .name = "S212",
+    .snippet = {
+      .source = y_u_v_shader_12bit_lsb,
+      .name = "sample_y_u_v_12bit_lsb",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 2, 2 },
+      .vsub = { 1, 1, 1 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S412] = {
+    .name = "S412",
+    .snippet = {
+      .source = y_u_v_shader_12bit_lsb,
+      .name = "sample_y_u_v_12bit_lsb",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 1, 1 },
+      .vsub = { 1, 1, 1 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S016] = {
+    .name = "S016",
+    .snippet = {
+      .source = y_u_v_shader,
+      .name = "sample_y_u_v",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 2, 2 },
+      .vsub = { 1, 2, 2 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S216] = {
+    .name = "S216",
+    .snippet = {
+      .source = y_u_v_shader,
+      .name = "sample_y_u_v",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
+      .plane_indices = { 0, 1, 2 },
+      .hsub = { 1, 2, 2 },
+      .vsub = { 1, 1, 1 },
+    },
+  },
+  [META_MULTI_TEXTURE_FORMAT_S416] = {
+    .name = "S416",
+    .snippet = {
+      .source = y_u_v_shader,
+      .name = "sample_y_u_v",
+    },
+    .info = {
+      .n_planes = 3,
+      .subformats = { COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16, COGL_PIXEL_FORMAT_R_16 },
       .plane_indices = { 0, 1, 2 },
       .hsub = { 1, 1, 1 },
       .vsub = { 1, 1, 1 },

src/common/meta-cogl-drm-formats.h
@@ -27,6 +27,35 @@
 #include "cogl/cogl.h"
 #include "meta/meta-multi-texture-format.h"
 
+/* Included in libdrm 2.4.125 */
+#ifndef DRM_FORMAT_S010
+#define DRM_FORMAT_S010 fourcc_code ('S', '0', '1', '0')
+#endif
+#ifndef DRM_FORMAT_S210
+#define DRM_FORMAT_S210 fourcc_code ('S', '2', '1', '0')
+#endif
+#ifndef DRM_FORMAT_S410
+#define DRM_FORMAT_S410 fourcc_code ('S', '4', '1', '0')
+#endif
+#ifndef DRM_FORMAT_S012
+#define DRM_FORMAT_S012 fourcc_code ('S', '0', '1', '2')
+#endif
+#ifndef DRM_FORMAT_S212
+#define DRM_FORMAT_S212 fourcc_code ('S', '2', '1', '2')
+#endif
+#ifndef DRM_FORMAT_S412
+#define DRM_FORMAT_S412 fourcc_code ('S', '4', '1', '2')
+#endif
+#ifndef DRM_FORMAT_S016
+#define DRM_FORMAT_S016 fourcc_code ('S', '0', '1', '6')
+#endif
+#ifndef DRM_FORMAT_S216
+#define DRM_FORMAT_S216 fourcc_code ('S', '2', '1', '6')
+#endif
+#ifndef DRM_FORMAT_S416
+#define DRM_FORMAT_S416 fourcc_code ('S', '4', '1', '6')
+#endif
+
 G_BEGIN_DECLS
 
 typedef struct _MetaFormatInfo
@@ -67,6 +96,15 @@ static const MetaFormatInfo meta_format_info[] = {
   { DRM_FORMAT_YUV420, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_YUV420 },
   { DRM_FORMAT_YUV422, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_YUV422 },
   { DRM_FORMAT_YUV444, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_YUV444 },
+  { DRM_FORMAT_S010, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S010 },
+  { DRM_FORMAT_S210, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S210 },
+  { DRM_FORMAT_S410, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S410 },
+  { DRM_FORMAT_S012, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S012 },
+  { DRM_FORMAT_S212, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S212 },
+  { DRM_FORMAT_S412, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S412 },
+  { DRM_FORMAT_S016, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S016 },
+  { DRM_FORMAT_S216, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S216 },
+  { DRM_FORMAT_S416, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_ANY, META_MULTI_TEXTURE_FORMAT_S416 },
 #elif G_BYTE_ORDER == G_BIG_ENDIAN
   { DRM_FORMAT_RGBX8888, DRM_FORMAT_INVALID, COGL_PIXEL_FORMAT_RGBX_8888, META_MULTI_TEXTURE_FORMAT_SIMPLE },
   { DRM_FORMAT_RGBA8888, DRM_FORMAT_RGBX8888, COGL_PIXEL_FORMAT_RGBA_8888_PRE, META_MULTI_TEXTURE_FORMAT_SIMPLE },

src/meta/meta-multi-texture-format.h
@@ -50,6 +50,15 @@ typedef enum _MetaMultiTextureFormat
   META_MULTI_TEXTURE_FORMAT_YUV420,
   META_MULTI_TEXTURE_FORMAT_YUV422,
   META_MULTI_TEXTURE_FORMAT_YUV444,
+  META_MULTI_TEXTURE_FORMAT_S010,
+  META_MULTI_TEXTURE_FORMAT_S210,
+  META_MULTI_TEXTURE_FORMAT_S410,
+  META_MULTI_TEXTURE_FORMAT_S012,
+  META_MULTI_TEXTURE_FORMAT_S212,
+  META_MULTI_TEXTURE_FORMAT_S412,
+  META_MULTI_TEXTURE_FORMAT_S016,
+  META_MULTI_TEXTURE_FORMAT_S216,
+  META_MULTI_TEXTURE_FORMAT_S416,
 } MetaMultiTextureFormat;
 
 typedef enum _MetaMultiTextureAlphaMode

commit a5c8ee797f78f8f42579e8b6830e7bcce63f7a9d

multi-texture-format: Fixup shader for 10 and 12 bit SW decoder YUV formats
The highest possible values that can be represented with
16/12/10 bits are 65535/4095/1023, not 65536/4096/1024.
In order to ensure 1023 maps to 65535 in the Sx10 case
we thus need to multiply by 65535 / 1023 ~= 64.06158
instead of 64.

Fixes: 775b605566 ("multi-texture-format: Add support for 10/12/16 bit SW decoder YUV formats")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4611>

src/compositor/meta-multi-texture-format.c
@@ -289,27 +289,29 @@ static const char y_v_u_shader[] =
   "  return yuva;\n"
   "}\n";
 
-/* Shader for 1 Y-plane, 1 U-plane and 1 V-plane, shifted by 6 bits (2^6=64) */
+/* Shader for 1 Y-plane, 1 U-plane and 1 V-plane, shifted by 6 bits */
 static const char y_u_v_shader_10bit_lsb[] =
   "vec4 sample_y_u_v_10bit_lsb(vec4 unused)\n"
   "{\n"
   "  vec4 yuva;\n"
+  "  float mult = 65535.0 / 1023.0;\n"
   "  yuva.a = 1.0;\n"
-  "  yuva.x = texture2D(cogl_sampler0, cogl_tex_coord0_in.st).r * 64.0;\n"
-  "  yuva.y = texture2D(cogl_sampler1, cogl_tex_coord0_in.st).r * 64.0;\n"
-  "  yuva.z = texture2D(cogl_sampler2, cogl_tex_coord0_in.st).r * 64.0;\n"
+  "  yuva.x = texture2D(cogl_sampler0, cogl_tex_coord0_in.st).r * mult;\n"
+  "  yuva.y = texture2D(cogl_sampler1, cogl_tex_coord0_in.st).r * mult;\n"
+  "  yuva.z = texture2D(cogl_sampler2, cogl_tex_coord0_in.st).r * mult;\n"
   "  return yuva;\n"
   "}\n";
 
-/* Shader for 1 Y-plane, 1 U-plane and 1 V-plane, shifted by 4 bits (2^4=16) */
+/* Shader for 1 Y-plane, 1 U-plane and 1 V-plane, shifted by 4 bits */
 static const char y_u_v_shader_12bit_lsb[] =
   "vec4 sample_y_u_v_12bit_lsb(vec4 unused)\n"
   "{\n"
   "  vec4 yuva;\n"
+  "  float mult = 65535.0 / 4095.0;\n"
   "  yuva.a = 1.0;\n"
-  "  yuva.x = texture2D(cogl_sampler0, cogl_tex_coord0_in.st).r * 16.0;\n"
-  "  yuva.y = texture2D(cogl_sampler1, cogl_tex_coord0_in.st).r * 16.0;\n"
-  "  yuva.z = texture2D(cogl_sampler2, cogl_tex_coord0_in.st).r * 16.0;\n"
+  "  yuva.x = texture2D(cogl_sampler0, cogl_tex_coord0_in.st).r * mult;\n"
+  "  yuva.y = texture2D(cogl_sampler1, cogl_tex_coord0_in.st).r * mult;\n"
+  "  yuva.z = texture2D(cogl_sampler2, cogl_tex_coord0_in.st).r * mult;\n"
   "  return yuva;\n"
   "}\n";
 