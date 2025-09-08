6e13c5edd79a94872b1c99380366ce43f30ad090
Bilal Elmoussaoui <belmouss@redhat.com>
2025-09-08 11:59:16 +0200

    cogl: Expose CoglDriver
    As that would be necessary for dropping indirection within CoglContext.

--- cogl/cogl/cogl-context-private.h
+++ cogl/cogl/cogl-context-private.h
@@ -248,5 +248,3 @@ _cogl_context_set_current_modelview_entry (CoglContext *context,
 
 void
 _cogl_context_update_sync (CoglContext *context);
-
-CoglDriver * cogl_context_get_driver (CoglContext *context);
