GNOME/mutter

commit b5ca2a46d5294bda1420bb1b4ea9ad916cd206a3

compositor,x11: Use per-view frame counter for MetaSyncCounter
Instead of the global one.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4341>

src/compositor/meta-window-actor-x11.c
@@ -185,12 +185,11 @@ queue_send_frame_messages_timeout (MetaWindowActorX11 *actor_x11)
 }
 
 static void
-assign_frame_counter_to_frames (MetaWindowActorX11 *actor_x11)
+assign_frame_counter_to_frames (MetaWindowActorX11 *actor_x11,
+                                ClutterFrame       *clutter_frame)
 {
   MetaWindow *window =
     meta_window_actor_get_meta_window (META_WINDOW_ACTOR (actor_x11));
-  MetaCompositor *compositor = window->display->compositor;
-  ClutterStage *stage = meta_compositor_get_stage (compositor);
   MetaFrame *frame;
   MetaSyncCounter *sync_counter;
 
@@ -202,13 +201,13 @@ assign_frame_counter_to_frames (MetaWindowActorX11 *actor_x11)
 
   sync_counter = meta_window_x11_get_sync_counter (window);
   meta_sync_counter_assign_counter_to_frames (sync_counter,
-                                              clutter_stage_get_frame_counter (stage));
+                                              clutter_frame->frame_count);
   frame = meta_window_x11_get_frame (window);
   if (frame)
     {
       sync_counter = meta_frame_get_sync_counter (frame);
       meta_sync_counter_assign_counter_to_frames (sync_counter,
-                                                  clutter_stage_get_frame_counter (stage));
+                                                  clutter_frame->frame_count);
     }
 }
 
@@ -1219,7 +1218,7 @@ meta_window_actor_x11_before_paint (MetaWindowActor  *actor,
 
   handle_updates (actor_x11);
 
-  assign_frame_counter_to_frames (actor_x11);
+  assign_frame_counter_to_frames (actor_x11, frame);
 }
 
 static void
@@ -1237,8 +1236,11 @@ meta_window_actor_x11_paint (ClutterActor        *actor,
   * and send the completion events normally */
   if (actor_x11->send_frame_messages_timer != 0)
     {
+      ClutterFrame *frame;
+
       remove_frame_messages_timer (actor_x11);
-      assign_frame_counter_to_frames (actor_x11);
+      frame = clutter_paint_context_get_frame (paint_context);
+      assign_frame_counter_to_frames (actor_x11, frame);
     }
 
   window = meta_window_actor_get_meta_window (META_WINDOW_ACTOR (actor_x11));

src/x11/meta-sync-counter.c
@@ -519,7 +519,7 @@ meta_sync_counter_complete_frame (MetaSyncCounter  *sync_counter,
     {
       GList *l_next = l->next;
       FrameData *frame = l->data;
-      int64_t frame_counter = frame_info->global_frame_counter;
+      int64_t frame_counter = frame_info->view_frame_counter;
 
       if (frame->frame_counter != -1 && frame->frame_counter <= frame_counter)
         {

commit 96f429b5cb4bea0d715195870ab31f151518ba8b

compositor: Don't assume there's an active ClutterFrame during X11 paint
The paint_context created in `create_framebuffer_from_window_actor` is
too short lived to have a `ClutterFrame` associated with it. That only
exists during a view paint. Since we're clearly not synchronised when
this happens, don't suggest we know the frame counter.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4254
Fixes: b5ca2a46d52 ("compositor,x11: Use per-view frame counter ...")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4612>

src/compositor/meta-window-actor-x11.c
@@ -1240,7 +1240,8 @@ meta_window_actor_x11_paint (ClutterActor        *actor,
 
       remove_frame_messages_timer (actor_x11);
       frame = clutter_paint_context_get_frame (paint_context);
-      assign_frame_counter_to_frames (actor_x11, frame);
+      if (frame)
+        assign_frame_counter_to_frames (actor_x11, frame);
     }
 
   window = meta_window_actor_get_meta_window (META_WINDOW_ACTOR (actor_x11));