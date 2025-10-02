GNOME/mutter

commit 2f0895df090479894d2a29bbbe20d2a58e882021

window: Add move/resize flag for invalid target rects
Sometimes the passed rectangle is not valid, e.g. an nonsensical /
non-placed unconstrained rectangle. Pass this information down the line
to meta_window_move_resize_internal() so that using the target rectangle
can be limited.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4413>

src/core/window.c
@@ -2869,6 +2869,8 @@ meta_window_maximize (MetaWindow        *window,
   if ((maximize_horizontally && !was_maximized_horizontally) ||
       (maximize_vertically   && !was_maximized_vertically))
     {
+      MetaMoveResizeFlags flags;
+
       /* if the window hasn't been placed yet, we'll maximize it then
        */
       if (!window->placed)
@@ -2905,12 +2907,14 @@ meta_window_maximize (MetaWindow        *window,
                                           META_SIZE_CHANGE_MAXIMIZE,
                                           &old_frame_rect, &old_buffer_rect);
 
-      meta_window_move_resize (window,
-                               (META_MOVE_RESIZE_MOVE_ACTION |
-                                META_MOVE_RESIZE_RESIZE_ACTION |
-                                META_MOVE_RESIZE_STATE_CHANGED |
-                                META_MOVE_RESIZE_CONSTRAIN),
-                               window->unconstrained_rect);
+      flags = (META_MOVE_RESIZE_MOVE_ACTION |
+               META_MOVE_RESIZE_RESIZE_ACTION |
+               META_MOVE_RESIZE_STATE_CHANGED |
+               META_MOVE_RESIZE_CONSTRAIN);
+      if (!window->unconstrained_rect_valid)
+        flags |= META_MOVE_RESIZE_RECT_INVALID;
+
+      meta_window_move_resize (window, flags, window->unconstrained_rect);
     }
 }
 
@@ -3559,6 +3563,7 @@ meta_window_make_fullscreen (MetaWindow  *window)
   if (!meta_window_is_fullscreen (window))
     {
       MtkRectangle old_frame_rect, old_buffer_rect;
+      MetaMoveResizeFlags flags;
 
       meta_window_get_frame_rect (window, &old_frame_rect);
       meta_window_get_buffer_rect (window, &old_buffer_rect);
@@ -3568,12 +3573,14 @@ meta_window_make_fullscreen (MetaWindow  *window)
                                           &old_frame_rect, &old_buffer_rect);
 
       meta_window_make_fullscreen_internal (window);
-      meta_window_move_resize (window,
-                               (META_MOVE_RESIZE_MOVE_ACTION |
-                                META_MOVE_RESIZE_RESIZE_ACTION |
-                                META_MOVE_RESIZE_STATE_CHANGED |
-                                META_MOVE_RESIZE_CONSTRAIN),
-                               window->unconstrained_rect);
+
+      flags = (META_MOVE_RESIZE_MOVE_ACTION |
+               META_MOVE_RESIZE_RESIZE_ACTION |
+               META_MOVE_RESIZE_STATE_CHANGED |
+               META_MOVE_RESIZE_CONSTRAIN);
+      if (!window->unconstrained_rect_valid)
+        flags |= META_MOVE_RESIZE_RECT_INVALID;
+      meta_window_move_resize (window, flags, window->unconstrained_rect);
     }
 }
 
@@ -4512,11 +4519,19 @@ meta_window_resize_frame (MetaWindow *window,
 void
 meta_window_idle_move_resize (MetaWindow *window)
 {
-  meta_window_move_resize_frame (window, FALSE,
-                                 window->unconstrained_rect.x,
-                                 window->unconstrained_rect.y,
-                                 window->unconstrained_rect.width,
-                                 window->unconstrained_rect.height);
+  MetaMoveResizeFlags flags;
+
+  flags = (META_MOVE_RESIZE_MOVE_ACTION |
+           META_MOVE_RESIZE_RESIZE_ACTION |
+           META_MOVE_RESIZE_CONSTRAIN);
+  if (!window->unconstrained_rect_valid)
+    flags |= META_MOVE_RESIZE_RECT_INVALID;
+  meta_window_move_resize (window,
+                           META_MOVE_RESIZE_MOVE_ACTION |
+                           META_MOVE_RESIZE_RESIZE_ACTION |
+                           META_MOVE_RESIZE_CONSTRAIN |
+                           META_MOVE_RESIZE_RECT_INVALID,
+                           window->unconstrained_rect);
 }
 
 gboolean

src/core/window-private.h
@@ -73,6 +73,7 @@ typedef enum
   META_MOVE_RESIZE_PLACEMENT_CHANGED = 1 << 11,
   META_MOVE_RESIZE_WAYLAND_CLIENT_RESIZE = 1 << 12,
   META_MOVE_RESIZE_CONSTRAIN = 1 << 13,
+  META_MOVE_RESIZE_RECT_INVALID = 1 << 14,
 } MetaMoveResizeFlags;
 
 typedef enum _MetaPlaceFlag

src/wayland/meta-window-wayland.c
@@ -1210,6 +1210,7 @@ meta_window_wayland_finish_move_resize (MetaWindow              *window,
                                         MetaWaylandSurfaceState *pending)
 {
   MetaWindowWayland *wl_window = META_WINDOW_WAYLAND (window);
+  gboolean has_position = FALSE;
   MetaDisplay *display = window->display;
   MetaWaylandSurface *surface = wl_window->surface;
   int dx, dy;
@@ -1297,8 +1298,16 @@ meta_window_wayland_finish_move_resize (MetaWindow              *window,
               if (acked_configuration->is_fullscreen)
                 flags |= META_MOVE_RESIZE_CONSTRAIN;
               if (acked_configuration->has_position)
-                calculate_position (acked_configuration, &new_geom, &rect);
+                {
+                  has_position = TRUE;
+                  calculate_position (acked_configuration, &new_geom, &rect);
+                }
             }
+        }
+      else
+        {
+          if (window->placed)
+            has_position = TRUE;
         }
     }
   else
@@ -1306,6 +1315,9 @@ meta_window_wayland_finish_move_resize (MetaWindow              *window,
       if (acked_configuration && acked_configuration->has_position)
         calculate_position (acked_configuration, &new_geom, &rect);
     }
+
+  if (!has_position)
+    flags |= META_MOVE_RESIZE_RECT_INVALID;
 
   toplevel_drag = get_toplevel_drag (window);
   if (toplevel_drag && !is_window_being_resized && !window->mapped &&

src/x11/window-x11.c
@@ -688,6 +688,7 @@ meta_window_x11_initialize_state (MetaWindow *window)
       MtkRectangle rect;
       MetaMoveResizeFlags flags;
       MetaGravity gravity = window->size_hints.win_gravity;
+      MetaPlaceFlag place_flags = META_PLACE_FLAG_NONE;
 
       rect.x = window->size_hints.x;
       rect.y = window->size_hints.y;
@@ -699,9 +700,13 @@ meta_window_x11_initialize_state (MetaWindow *window)
                META_MOVE_RESIZE_RESIZE_ACTION |
                META_MOVE_RESIZE_CONSTRAIN);
 
+      if (!(window->size_hints.flags & META_SIZE_HINTS_USER_POSITION))
+        flags |= META_MOVE_RESIZE_RECT_INVALID;
+
       adjust_for_gravity (window, TRUE, gravity, &rect);
       meta_window_client_rect_to_frame_rect (window, &rect, &rect);
-      meta_window_move_resize (window, flags, rect);
+
+      meta_window_move_resize_internal (window, flags, place_flags, rect);
     }
 
   meta_window_x11_update_shape_region (window);

commit 5a51898f6f00fbc7802631f4139e6a50522e143c

window: Fixup flagging META_MOVE_RESIZE_RECT_INVALID only when invalid
Fixes: 2f0895df09 ("window: Add move/resize flag for invalid target rects")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4694>

src/core/window.c
@@ -4719,12 +4719,7 @@ meta_window_idle_move_resize (MetaWindow *window)
            META_MOVE_RESIZE_CONSTRAIN);
   if (!window->unconstrained_rect_valid)
     flags |= META_MOVE_RESIZE_RECT_INVALID;
-  meta_window_move_resize (window,
-                           META_MOVE_RESIZE_MOVE_ACTION |
-                           META_MOVE_RESIZE_RESIZE_ACTION |
-                           META_MOVE_RESIZE_CONSTRAIN |
-                           META_MOVE_RESIZE_RECT_INVALID,
-                           window->unconstrained_rect);
+  meta_window_move_resize (window, flags, window->unconstrained_rect);
 }
 
 gboolean