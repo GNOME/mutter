GNOME/mutter

commit aa5b7e61f3537bdc9de36e91c228de3f7b6df832

xwayland/dnd: Use meta_window_stage_to_protocol_point/rect
Instead of meta_xwayland_stage_to_protocol_point/rect directly.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4689>

src/wayland/meta-xwayland-dnd.c
@@ -340,13 +340,13 @@ xdnd_send_position (MetaXWaylandDnd *dnd,
                     int              y)
 {
   MetaWaylandCompositor *compositor = dnd->manager->compositor;
-  MetaXWaylandManager *manager = &compositor->xwayland_manager;
   MetaWaylandDataSource *source = compositor->seat->data_device.dnd_data_source;
   MetaX11Display *x11_display = x11_display_from_dnd (dnd);
   Display *xdisplay = x11_display->xdisplay;
   uint32_t action = 0, user_action, actions;
   int protocol_x, protocol_y;
   XEvent xev = { 0 };
+  MetaWindow *window;
 
   user_action = meta_wayland_data_source_get_user_action (source);
   meta_wayland_data_source_get_actions (source, &actions);
@@ -361,9 +361,11 @@ xdnd_send_position (MetaXWaylandDnd *dnd,
   xev.xclient.format = 32;
   xev.xclient.window = dest;
 
-  meta_xwayland_stage_to_protocol_point (manager,
-                                         x, y,
-                                         &protocol_x, &protocol_y);
+  window = meta_x11_display_lookup_x_window (x11_display,
+                                             x11_display->selection.xwindow);
+  meta_window_stage_to_protocol_point (window,
+                                       x, y,
+                                       &protocol_x, &protocol_y);
 
   xev.xclient.data.l[0] = x11_display->selection.xwindow;
   xev.xclient.data.l[1] = 0;
@@ -803,7 +805,6 @@ repick_drop_surface (MetaWaylandCompositor *compositor,
                      const ClutterEvent    *event)
 {
   MetaXWaylandDnd *dnd = compositor->xwayland_manager.dnd;
-  MetaXWaylandManager *manager = &compositor->xwayland_manager;
   MetaX11Display *x11_display = x11_display_from_dnd (dnd);
   Display *xdisplay = meta_x11_display_get_xdisplay (x11_display);
   MetaWaylandSurface *focus = NULL;
@@ -832,9 +833,9 @@ repick_drop_surface (MetaWaylandCompositor *compositor,
       XMapRaised (xdisplay, dnd_window);
 
       frame_rect = meta_window_config_get_rect (focus_window->config);
-      meta_xwayland_stage_to_protocol_rect (manager,
-                                            &frame_rect,
-                                            &frame_rect);
+      meta_window_stage_to_protocol_rect (focus_window,
+                                          &frame_rect,
+                                          &frame_rect);
 
       XMoveResizeWindow (xdisplay, dnd_window,
                          frame_rect.x,

commit fc2548c7825465547abc2ccbcb5d63bb5889ee22

xwayland/dnd: Pass dest to meta_x11_display_lookup_x_window
That shouldn't return NULL, and might result in better protocol_x/y for
a destination window using viewport scaling.

If meta_x11_display_lookup_x_window unexpectedly returns NULL, bail and
log a warning instead of crashing.

Suggested by

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4400
Fixes: aa5b7e61f353 ("xwayland/dnd: Use meta_window_stage_to_protocol_point/rect")

v2:
* Use g_return_if_fail instead of falling back to
  meta_xwayland_stage_to_protocol_point.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4720>

src/wayland/meta-xwayland-dnd.c
@@ -348,6 +348,9 @@ xdnd_send_position (MetaXWaylandDnd *dnd,
   XEvent xev = { 0 };
   MetaWindow *window;
 
+  window = meta_x11_display_lookup_x_window (x11_display, dest);
+  g_return_if_fail (window);
+
   user_action = meta_wayland_data_source_get_user_action (source);
   meta_wayland_data_source_get_actions (source, &actions);
 
@@ -361,8 +364,6 @@ xdnd_send_position (MetaXWaylandDnd *dnd,
   xev.xclient.format = 32;
   xev.xclient.window = dest;
 
-  window = meta_x11_display_lookup_x_window (x11_display,
-                                             x11_display->selection.xwindow);
   meta_window_stage_to_protocol_point (window,
                                        x, y,
                                        &protocol_x, &protocol_y);