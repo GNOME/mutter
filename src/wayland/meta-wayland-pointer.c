GNOME/mutter

commit d4247714396ba6217265cd0f9895885cdb16f9d6

wayland: Change implicit pointer grab behavior
In order to honor a bit better the "owner_events lookalike" spirit of
the protocol in describing the interaction of popup grabs with client
events, make the implicit grab surface only effective outside of the
client. Inside the client, enter and leave events will be emitted as
normal between child surfaces.

This is a behavioral change that requires changes to the
popup-focus.metatest stacking test. While at it, test a bit further
the behavior inside popups (checking that focus changes to these), and
outside the implicit grab client (checking that focus returns to the
implicit grab surface).

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4703>

src/wayland/meta-wayland-pointer.c
@@ -93,6 +93,7 @@ struct _MetaWaylandPointer
   GHashTable *pointer_clients;
 
   MetaWaylandSurface *focus_surface;
+  MetaWaylandSurface *implicit_grab_surface;
   gulong focus_surface_destroyed_handler_id;
   gulong focus_surface_alive_notify_id;
   guint32 focus_serial;
@@ -113,6 +114,7 @@ struct _MetaWaylandPointer
   ClutterSprite *sprite;
   MetaWaylandSurface *current;
   gulong current_surface_destroyed_handler_id;
+  gulong implicit_grab_surface_destroyed_handler_id;
 
   guint32 button_count;
 };
@@ -126,6 +128,10 @@ meta_wayland_pointer_set_current (MetaWaylandPointer *pointer,
 
 static void meta_wayland_pointer_set_focus (MetaWaylandPointer *pointer,
                                             MetaWaylandSurface *surface);
+
+static void
+meta_wayland_pointer_set_implicit_grab_surface (MetaWaylandPointer *pointer,
+                                                MetaWaylandSurface *surface);
 
 static MetaBackend *
 backend_from_pointer (MetaWaylandPointer *pointer)
@@ -586,6 +592,38 @@ meta_wayland_pointer_set_current (MetaWaylandPointer *pointer,
 }
 
 static void
+implicit_grab_surface_destroyed (MetaWaylandSurface *surface,
+                                 MetaWaylandPointer *pointer)
+{
+  meta_wayland_pointer_set_implicit_grab_surface (pointer, NULL);
+}
+
+static void
+meta_wayland_pointer_set_implicit_grab_surface (MetaWaylandPointer *pointer,
+                                                MetaWaylandSurface *surface)
+{
+  if (pointer->implicit_grab_surface == surface)
+    return;
+
+  if (pointer->implicit_grab_surface)
+    {
+      g_clear_signal_handler (&pointer->implicit_grab_surface_destroyed_handler_id,
+                              pointer->implicit_grab_surface);
+      pointer->implicit_grab_surface = NULL;
+    }
+
+  if (surface)
+    {
+      pointer->implicit_grab_surface = surface;
+      pointer->implicit_grab_surface_destroyed_handler_id =
+        g_signal_connect (surface, "destroy",
+                          G_CALLBACK (implicit_grab_surface_destroyed),
+                          pointer);
+    }
+}
+
+
+static void
 repick_for_event (MetaWaylandPointer *pointer,
                   const ClutterEvent *for_event)
 {
@@ -675,7 +713,13 @@ meta_wayland_pointer_update (MetaWaylandPointer *pointer,
       event_type == CLUTTER_BUTTON_PRESS ||
       event_type == CLUTTER_BUTTON_RELEASE)
     {
+      if (event_type == CLUTTER_BUTTON_PRESS && pointer->button_count == 0)
+        meta_wayland_pointer_set_implicit_grab_surface (pointer, pointer->current);
+
       pointer->button_count = count_buttons (event);
+
+      if (event_type == CLUTTER_BUTTON_RELEASE && pointer->button_count == 0)
+        meta_wayland_pointer_set_implicit_grab_surface (pointer, NULL);
     }
 }
 
@@ -1604,8 +1648,12 @@ meta_wayland_pointer_get_focus_surface (MetaWaylandPointer *pointer)
 MetaWaylandSurface *
 meta_wayland_pointer_get_implicit_grab_surface (MetaWaylandPointer *pointer)
 {
-  if (pointer->button_count > 0)
-    return pointer->focus_surface;
+  if (pointer->button_count > 0 &&
+      pointer->implicit_grab_surface &&
+      (!pointer->current ||
+       wl_resource_get_client (pointer->implicit_grab_surface->resource) !=
+       wl_resource_get_client (pointer->current->resource)))
+    return pointer->implicit_grab_surface;
 
   return NULL;
 }

src/tests/stacking/popup-focus.metatest
@@ -34,7 +34,7 @@ assert_pointer_focus none
 
 dismiss w/p1
 
-# Check that implicit pointer grab is transfered to the popup
+# Check that focus can move to a popup despite a implicit pointer grab
 move_cursor_to 150 150
 assert_pointer_focus w/1
 
@@ -43,12 +43,18 @@ popup_at w/p2 1 right 100 100 grab
 assert_position w/p2 200 100
 assert_size w/p2 100 100
 assert_keyboard_focus w/p2
-assert_pointer_focus w/p2
+assert_pointer_focus w/1
 
-move_cursor_to 120 120
+# Move inside the popup, focus should move there
+move_cursor_to 220 120
+assert_pointer_focus w/p2
+# Move outside the client, focus should move to the implicit grab surface
+move_cursor_to 420 420
+assert_pointer_focus w/1
+# Move focus back to popup
+move_cursor_to 220 120
 assert_pointer_focus w/p2
 release_click
-assert_pointer_focus w/1
 
 dismiss w/p2
 
@@ -65,7 +71,7 @@ assert_pointer_focus w/1
 
 move_cursor_to 250 150
 assert_keyboard_focus w/1
-assert_pointer_focus w/1
+assert_pointer_focus w/p3
 
 release_click
 dismiss w/p3

commit 5fef274d26cb873b3e75f8feeb2015f8c9973941

wayland: Confine implicit grab inter-surface crossing within toplevel
Commit d424771439 attempted to make the implicit grab behavior able to cater
for click/drag/release patterns to select items in menus. For this we need
to keep the pointer able to cross between surfaces despite the implicit grab
on the parent surface of the popup.

However this check breaks Xwayland expectations that the implicit grab must
stay in the same window, breaking some clients like xterm due to the unexpected
crossing events.

In order to keep our cake and eat it, make the implicit grab ability to cross
between surfaces apply within the surface tree of a toplevel, instead of
across any surface of the same client.

Notably, menus in Xwayland are not driven by xdg_popup, and are seen as
toplevels themselves. This allows us to keep Xwayland happy, and to keep a
sensible implicit grab behavior without having to think yet again from scratch
how the click/drag/release pattern could ever work without pointer crossing.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4409
Fixes: d424771439 ("wayland: Change implicit pointer grab behavior")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4729>

src/wayland/meta-wayland-pointer.c
@@ -1651,8 +1651,8 @@ meta_wayland_pointer_get_implicit_grab_surface (MetaWaylandPointer *pointer)
   if (pointer->button_count > 0 &&
       pointer->implicit_grab_surface &&
       (!pointer->current ||
-       wl_resource_get_client (pointer->implicit_grab_surface->resource) !=
-       wl_resource_get_client (pointer->current->resource)))
+       meta_wayland_surface_get_toplevel (pointer->implicit_grab_surface) !=
+       meta_wayland_surface_get_toplevel (pointer->current)))
     return pointer->implicit_grab_surface;
 
   return NULL;