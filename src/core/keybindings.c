GNOME/mutter

commit c0fd2d95568e36a9c96011ff98d6c5beba2c8879

core: Let key presses of special modifiers through
When handling the key press of special modifier keys (Super for all
our compositor level keybindings, Control for "locate pointer"), let
this event go through, and possibly be forwarded to Wayland clients.

Rationale is: On X11, we had the passive grab mechanism that possibly
replayed the full sequence of shortcut keystrokes to the client this
allowed clients to seamlessly handle keystrokes that had these special
modifiers. On Wayland, we do nothing of that, except (oh-oh) consuming
the event. This could leave Wayland clients' keyboard tracking unbalanced,
and was compensated at MetaWaylandKeyboard by tracking modifier changes
during update(). We don't need to fully replicate the passive grab mechanism
on Wayland and we can steal the focus at any time, so we may as well send
the key press, and wl_keyboard.leave will clean up when we steal focus away.

Letting this event go through on X11 should have no ill effects, the
keyboard remains passively grabbed by the WM, so allowing further event
propagation only makes more places of the shell able to maybe handle the
Super key press event. Passive grabs are still the mechanism to lend back
control to clients, as opposed to something that happens inside GNOME Shell.

While at it, change to CLUTTER_EVENT_* defines that are clearer at the
intention.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4686>

src/core/keybindings.c
@@ -1548,6 +1548,8 @@ process_special_modifier_key (MetaDisplay          *display,
                * per-window key bindings or to the application */
               meta_compositor_handle_event (compositor, event, window,
                                             META_EVENT_MODE_REPLAY);
+
+              return CLUTTER_EVENT_PROPAGATE;
             }
         }
       else if (clutter_event_type (event) == CLUTTER_KEY_RELEASE)
@@ -1564,7 +1566,7 @@ process_special_modifier_key (MetaDisplay          *display,
 
       meta_compositor_handle_event (compositor, event, window,
                                     META_EVENT_MODE_THAW);
-      return TRUE;
+      return CLUTTER_EVENT_STOP;
     }
   else if (clutter_event_type (event) == CLUTTER_KEY_PRESS &&
            ((modifiers & ~(IGNORED_MODIFIERS)) & CLUTTER_MODIFIER_MASK) == 0 &&
@@ -1576,10 +1578,10 @@ process_special_modifier_key (MetaDisplay          *display,
       meta_compositor_handle_event (compositor, event, window,
                                     META_EVENT_MODE_KEEP_FROZEN);
 
-      return TRUE;
+      return CLUTTER_EVENT_PROPAGATE;
     }
   else
-    return FALSE;
+    return CLUTTER_EVENT_PROPAGATE;
 }
 
 

commit e39c6a1a776520926108ed4350ebfb7308193027

core: Avoid processing twice a same key event during keybindings
The changes in commit c0fd2d9556 to let wayland clients follow more
closely the pressed modifiers resulted in combinations of these special
modifiers with other keys maybe going through process_event() twice
in MetaKeybindings, once when attempted as a fallback when detecting
this situation, and then again because that commit now lets these
events through. This seems to result in inconsistent state and
seemingly repeated actions.

Since we do have good reasons to let these events maybe go past
keybindings (e.g. the keycombo not being managed by the compositor
after all, but the app), it sounds better to let the single
process_event() call happen through the fallbacks. This again results
in the action being handled once.

In X11, we used to replay events in those paths, as we freeze event
processing first when receiving the special modifier key. This has
been shuffled, so that the intent is preserved, and events get
replayed if they were not meant for keybindings.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4368
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4404
Fixes: c0fd2d9556 ("core: Let key presses of special modifiers through")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4724>

src/core/keybindings.c
@@ -1537,20 +1537,10 @@ process_special_modifier_key (MetaDisplay          *display,
            * work, since global keybindings won't be activated ("this
            * time, however, the function ignores any passive grabs at
            * above (toward the root of) the grab_window of the grab
-           * just released.") So, we first explicitly check for one of
-           * our global keybindings, and if not found, we then replay
-           * the event. Other clients with global grabs will be out of
-           * luck.
+           * just released.") So, it must be delayed until we know
+           * it is not handled by our global keybindings.
            */
-          if (!process_event (display, window, event))
-            {
-              /* Replay the event so it gets delivered to our
-               * per-window key bindings or to the application */
-              meta_compositor_handle_event (compositor, event, window,
-                                            META_EVENT_MODE_REPLAY);
-
-              return CLUTTER_EVENT_PROPAGATE;
-            }
+          return CLUTTER_EVENT_PROPAGATE;
         }
       else if (clutter_event_type (event) == CLUTTER_KEY_RELEASE)
         {
@@ -1684,11 +1674,21 @@ process_key_event (MetaDisplay     *display,
   if (process_iso_next_group (display, event))
     return TRUE;
 
-  meta_compositor_handle_event (compositor, event, window,
-                                META_EVENT_MODE_THAW);
-
   /* Do the normal keybindings */
-  return process_event (display, window, event);
+  if (process_event (display, window, event))
+    {
+      meta_compositor_handle_event (compositor, event, window,
+                                    META_EVENT_MODE_THAW);
+      return TRUE;
+    }
+  else
+    {
+      /* Replay the event so it gets delivered to our
+       * per-window key bindings or to the application */
+      meta_compositor_handle_event (compositor, event, window,
+                                    META_EVENT_MODE_REPLAY);
+      return FALSE;
+    }
 }
 
 /* Handle a key event. May be called recursively: some key events cause