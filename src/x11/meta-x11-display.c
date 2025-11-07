GNOME/mutter

commit 99fd5567fd974c3d8e0b950079bb2b0f268e3a2d

Drop restarting capability
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4505>

src/x11/meta-x11-display.c
@@ -796,33 +796,16 @@ static Window
 take_manager_selection (MetaX11Display *x11_display,
                         Window          xroot,
                         Atom            manager_atom,
-                        int             timestamp,
-                        gboolean        should_replace)
+                        int             timestamp)
 {
   Window current_owner, new_owner;
 
   current_owner = XGetSelectionOwner (x11_display->xdisplay, manager_atom);
   if (current_owner != None)
     {
-      XSetWindowAttributes attrs;
-
-      if (should_replace)
-        {
-          /* We want to find out when the current selection owner dies */
-          mtk_x11_error_trap_push (x11_display->xdisplay);
-          attrs.event_mask = StructureNotifyMask;
-          XChangeWindowAttributes (x11_display->xdisplay, current_owner, CWEventMask, &attrs);
-          if (mtk_x11_error_trap_pop_with_return (x11_display->xdisplay) != Success)
-            current_owner = None; /* don't wait for it to die later on */
-        }
-      else
-        {
-          g_warning (_("Display “%s” already has a window manager; "
-                       "try using the --replace option to replace "
-                       "the current window manager."),
-                     x11_display->name);
-          return None;
-        }
+      g_warning (_("Display “%s” already has a window manager"),
+                 x11_display->name);
+      return None;
     }
 
   /* We need SelectionClear and SelectionRequest events on the new owner,
@@ -1343,14 +1326,10 @@ meta_x11_display_new (MetaDisplay  *display,
   Window xroot;
   int i, number;
   Window new_wm_sn_owner;
-  gboolean replace_current_wm;
   Atom wm_sn_atom;
   Atom wm_cm_atom;
   char buf[128];
   guint32 timestamp;
-  Atom atom_restart_helper;
-  Window restart_helper_window = None;
-  gboolean is_restart = FALSE;
 
   /* A list of all atom names, so that we can intern them in one go. */
   const char *atom_names[] = {
@@ -1374,9 +1353,6 @@ meta_x11_display_new (MetaDisplay  *display,
     }
 #endif
 
-  replace_current_wm =
-    meta_context_is_replacing (meta_backend_get_context (backend));
-
   number = DefaultScreen (xdisplay);
 
   xroot = RootWindow (xdisplay, number);
@@ -1397,14 +1373,6 @@ meta_x11_display_new (MetaDisplay  *display,
     }
 
   xscreen = ScreenOfDisplay (xdisplay, number);
-
-  atom_restart_helper = XInternAtom (xdisplay, "_MUTTER_RESTART_HELPER", False);
-  restart_helper_window = XGetSelectionOwner (xdisplay, atom_restart_helper);
-  if (restart_helper_window)
-    {
-      is_restart = TRUE;
-      meta_set_is_restart (TRUE);
-    }
 
   x11_display = g_object_new (META_TYPE_X11_DISPLAY, NULL);
   x11_display->display = display;
@@ -1510,11 +1478,6 @@ meta_x11_display_new (MetaDisplay  *display,
   if (!meta_is_wayland_compositor ())
     x11_display->composite_overlay_window = XCompositeGetOverlayWindow (xdisplay, xroot);
 
-  /* Now that we've gotten taken a reference count on the COW, we
-   * can close the helper that is holding on to it */
-  if (is_restart)
-    XSetSelectionOwner (xdisplay, atom_restart_helper, None, META_CURRENT_TIME);
-
   /* Handle creating a no_focus_window for this screen */
   x11_display->no_focus_window =
     meta_x11_display_create_offscreen_window (x11_display,
@@ -1601,8 +1564,7 @@ meta_x11_display_new (MetaDisplay  *display,
   new_wm_sn_owner = take_manager_selection (x11_display,
                                             xroot,
                                             wm_sn_atom,
-                                            timestamp,
-                                            replace_current_wm);
+                                            timestamp);
   if (new_wm_sn_owner == None)
     {
       g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
@@ -1620,8 +1582,7 @@ meta_x11_display_new (MetaDisplay  *display,
   wm_cm_atom = XInternAtom (x11_display->xdisplay, buf, False);
 
   x11_display->wm_cm_selection_window =
-    take_manager_selection (x11_display, xroot, wm_cm_atom, timestamp,
-                            replace_current_wm);
+    take_manager_selection (x11_display, xroot, wm_cm_atom, timestamp);
 
   if (x11_display->wm_cm_selection_window == None)
     {
@@ -2571,17 +2532,11 @@ void
 meta_x11_display_redirect_windows (MetaX11Display *x11_display,
                                    MetaDisplay    *display)
 {
-  MetaContext *context = meta_display_get_context (display);
   Display *xdisplay = meta_x11_display_get_xdisplay (x11_display);
   Window xroot = meta_x11_display_get_xroot (x11_display);
   int screen_number = meta_x11_display_get_screen_number (x11_display);
   guint n_retries;
-  guint max_retries;
-
-  if (meta_context_is_replacing (context))
-    max_retries = 5;
-  else
-    max_retries = 1;
+  guint max_retries = 1;
 
   n_retries = 0;
 

doc/man/mutter.1
@@ -19,22 +19,19 @@
 MUTTER \- Clutter based compositing GTK2 Window Manager
 .SH SYNOPSIS
 .B mutter
-[\-\-display=\fIDISPLAY\fP] [\-\-replace] [\-\-sm\-client\-id=\fIID\fP] [\-\-sm\-disable] [\-\-sm\-save\-file=\fIFILENAME\fP] [\-\-version] [\-\-help] [[\-\-] command [argument...]]
+[\-\-display=\fIDISPLAY\fP] [\-\-sm\-client\-id=\fIID\fP] [\-\-sm\-disable] [\-\-sm\-save\-file=\fIFILENAME\fP] [\-\-version] [\-\-help] [[\-\-] command [argument...]]
 .SH DESCRIPTION
 This manual page documents briefly
 .B mutter\fP.
 .PP
 .\" TeX users may be more comfortable with the \fB<whatever>\fP and
-.\" \fI<whatever>\fP escape sequences to invode bold face and italics, 
+.\" \fI<whatever>\fP escape sequences to invode bold face and italics,
 .\" respectively.
 \fBmutter\fP is a minimal X window manager aimed at nontechnical users and is designed to integrate well with the GNOME desktop.  \fBmutter\fP lacks some features that may be expected by traditional UNIX or other technical users; these users may want to investigate other available window managers for use with GNOME or standalone.
 .SH OPTIONS
 .TP
 .B \-\-display=DISPLAY
 Connect to X display \fIDISPLAY\fP.
-.TP
-.B \-\-replace
-a window manager which is running is replaced by \fBmutter\fP.  Users are encouraged to change the GNOME window manager by running the new WM with the --replace or -replace option, and subsequently saving the session.
 .TP
 .B \-\-sm\-client\-id=ID
 Specify a session management \fIID\fP.

src/backends/meta-a11y-manager.c
@@ -394,10 +394,6 @@ static void
 meta_a11y_manager_constructed (GObject *object)
 {
   MetaA11yManager *a11y_manager = META_A11Y_MANAGER (object);
-  MetaContext *context;
-
-  g_assert (a11y_manager->backend);
-  context = meta_backend_get_context (a11y_manager->backend);
 
   a11y_manager->grabbed_keypresses = g_hash_table_new (NULL, NULL);
   a11y_manager->all_grabbed_modifiers = g_hash_table_new (NULL, NULL);
@@ -406,9 +402,7 @@ meta_a11y_manager_constructed (GObject *object)
     g_bus_own_name (G_BUS_TYPE_SESSION,
                     "org.freedesktop.a11y.Manager",
                     G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
-                    (meta_context_is_replacing (context) ?
-                     G_BUS_NAME_OWNER_FLAGS_REPLACE :
-                     G_BUS_NAME_OWNER_FLAGS_NONE),
+                    G_BUS_NAME_OWNER_FLAGS_NONE,
                     on_bus_acquired,
                     on_name_acquired,
                     on_name_lost,

src/backends/meta-idle-manager.c
@@ -275,7 +275,6 @@ meta_idle_manager_reset_idle_time (MetaIdleManager *idle_manager)
 MetaIdleManager *
 meta_idle_manager_new (MetaBackend *backend)
 {
-  MetaContext *context = meta_backend_get_context (backend);
   MetaIdleManager *idle_manager;
 
   idle_manager = g_new0 (MetaIdleManager, 1);
@@ -285,9 +284,7 @@ meta_idle_manager_new (MetaBackend *backend)
     g_bus_own_name (G_BUS_TYPE_SESSION,
                     "org.gnome.Mutter.IdleMonitor",
                     G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
-                    (meta_context_is_replacing (context) ?
-                     G_BUS_NAME_OWNER_FLAGS_REPLACE :
-                     G_BUS_NAME_OWNER_FLAGS_NONE),
+                    G_BUS_NAME_OWNER_FLAGS_NONE,
                     on_bus_acquired,
                     on_name_acquired,
                     on_name_lost,

src/backends/meta-monitor-manager.c
@@ -3647,15 +3647,11 @@ on_name_lost (GDBusConnection *connection,
 static void
 initialize_dbus_interface (MetaMonitorManager *manager)
 {
-  MetaContext *context = meta_backend_get_context (manager->backend);
-
   manager->dbus_name_id =
     g_bus_own_name (G_BUS_TYPE_SESSION,
                     "org.gnome.Mutter.DisplayConfig",
                     G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
-                    (meta_context_is_replacing (context) ?
-                     G_BUS_NAME_OWNER_FLAGS_REPLACE :
-                     G_BUS_NAME_OWNER_FLAGS_NONE),
+                    G_BUS_NAME_OWNER_FLAGS_NONE,
                     on_bus_acquired,
                     on_name_acquired,
                     on_name_lost,

src/core/display-private.h
@@ -261,17 +261,11 @@ void meta_display_remove_pending_pings_for_window (MetaDisplay *display,
 
 MetaGestureTracker * meta_display_get_gesture_tracker (MetaDisplay *display);
 
-gboolean meta_display_show_restart_message (MetaDisplay *display,
-                                            const char  *message);
-gboolean meta_display_request_restart      (MetaDisplay *display);
-
 gboolean meta_display_show_resize_popup (MetaDisplay  *display,
                                          gboolean      show,
                                          MtkRectangle *rect,
                                          int           display_w,
                                          int           display_h);
-
-void meta_set_is_restart (gboolean whether);
 
 void meta_display_cancel_touch (MetaDisplay *display);
 

src/core/display.c
@@ -157,8 +157,6 @@ enum
   WINDOW_MARKED_URGENT,
   GRAB_OP_BEGIN,
   GRAB_OP_END,
-  SHOW_RESTART_MESSAGE,
-  RESTART,
   SHOW_RESIZE_POPUP,
   GL_VIDEO_MEMORY_PURGED,
   SHOW_PAD_OSD,
@@ -407,62 +405,6 @@ meta_display_class_init (MetaDisplayClass *klass)
                   G_TYPE_NONE, 2,
                   META_TYPE_WINDOW,
                   META_TYPE_GRAB_OP);
-
-  /**
-   * MetaDisplay::show-restart-message:
-   * @display: the #MetaDisplay instance
-   * @message: (allow-none): The message to display, or %NULL
-   *  to clear a previous restart message.
-   *
-   * The signal will be emitted to indicate that the compositor
-   * should show a message during restart.
-   *
-   * This is emitted when [func@Meta.restart] is called, either by Mutter
-   * internally or by the embedding compositor. The message should be
-   * immediately added to the Clutter stage in its final form -
-   * [signal@Meta.Display::restart] will be emitted to exit the application and leave the
-   * stage contents frozen as soon as the the stage is painted again.
-   *
-   * On case of failure to restart, this signal will be emitted again
-   * with %NULL for @message.
-   *
-   * Returns: %TRUE means the message was added to the stage; %FALSE
-   *   indicates that the compositor did not show the message.
-   */
-  display_signals[SHOW_RESTART_MESSAGE] =
-    g_signal_new ("show-restart-message",
-                  G_TYPE_FROM_CLASS (klass),
-                  G_SIGNAL_RUN_LAST,
-                  0,
-                  g_signal_accumulator_true_handled,
-                  NULL, NULL,
-                  G_TYPE_BOOLEAN, 1,
-                  G_TYPE_STRING);
-
-  /**
-   * MetaDisplay::restart:
-   * @display: the #MetaDisplay instance
-   *
-   * The signal is emitted to indicate that compositor
-   * should reexec the process.
-   *
-   * This is emitted when [func@Meta.restart] is called,
-   * either by Mutter internally or by the embedding compositor.
-   *
-   * See also [signal@Meta.Display::show-restart-message].
-   *
-   * Returns: %FALSE to indicate that the compositor could not
-   *  be restarted. When the compositor is restarted, the signal
-   *  should not return.
-   */
-  display_signals[RESTART] =
-    g_signal_new ("restart",
-                  G_TYPE_FROM_CLASS (klass),
-                  G_SIGNAL_RUN_LAST,
-                  0,
-                  g_signal_accumulator_true_handled,
-                  NULL, NULL,
-                  G_TYPE_BOOLEAN, 0);
 
   display_signals[SHOW_RESIZE_POPUP] =
     g_signal_new ("show-resize-popup",
@@ -2512,31 +2454,6 @@ MetaGestureTracker *
 meta_display_get_gesture_tracker (MetaDisplay *display)
 {
   return display->gesture_tracker;
-}
-
-gboolean
-meta_display_show_restart_message (MetaDisplay *display,
-                                   const char  *message)
-{
-  gboolean result = FALSE;
-
-  g_signal_emit (display,
-                 display_signals[SHOW_RESTART_MESSAGE], 0,
-                 message, &result);
-
-  return result;
-}
-
-gboolean
-meta_display_request_restart (MetaDisplay *display)
-{
-  gboolean result = FALSE;
-
-  g_signal_emit (display,
-                 display_signals[RESTART], 0,
-                 &result);
-
-  return result;
 }
 
 gboolean

src/core/meta-context-main.c
@@ -50,9 +50,6 @@
 
 typedef struct _MetaContextMainOptions
 {
-  struct {
-    gboolean replace;
-  } x11;
 #ifdef HAVE_WAYLAND
   gboolean wayland;
   gboolean no_x11;
@@ -166,14 +163,6 @@ meta_context_main_get_x11_display_policy (MetaContext *context)
   else
 #endif
   return META_X11_DISPLAY_POLICY_ON_DEMAND;
-}
-
-static gboolean
-meta_context_main_is_replacing (MetaContext *context)
-{
-  MetaContextMain *context_main = META_CONTEXT_MAIN (context);
-
-  return context_main->options.x11.replace;
 }
 
 #ifdef HAVE_NATIVE_BACKEND
@@ -508,7 +497,6 @@ meta_context_main_class_init (MetaContextMainClass *klass)
   context_class->configure = meta_context_main_configure;
   context_class->get_x11_display_policy =
     meta_context_main_get_x11_display_policy;
-  context_class->is_replacing = meta_context_main_is_replacing;
   context_class->setup = meta_context_main_setup;
   context_class->create_backend = meta_context_main_create_backend;
   context_class->notify_ready = meta_context_main_notify_ready;

commit f6283813a911283d7d1a885ce8b5162ebc5cb2f2

x11/display: Remove some dead code
Since the replace capability was removed, we fail early when
the selection already has an owner and never get to the point
where we have to wait for the previous owner to release it.

CID: #1667192
Fixes: 99fd5567fd ("Drop restarting capability")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4750>

src/x11/meta-x11-display.c
@@ -801,19 +801,6 @@ take_manager_selection (MetaX11Display *x11_display,
     XSendEvent (x11_display->xdisplay, xroot, False, StructureNotifyMask, (XEvent *) &ev);
   }
 
-  /* Wait for old window manager to go away */
-  if (current_owner != None)
-    {
-      XEvent event;
-
-      /* We sort of block infinitely here which is probably lame. */
-
-      meta_topic (META_DEBUG_X11, "Waiting for old window manager to exit");
-      do
-        XWindowEvent (x11_display->xdisplay, current_owner, StructureNotifyMask, &event);
-      while (event.type != DestroyNotify);
-    }
-
   return new_owner;
 }
 