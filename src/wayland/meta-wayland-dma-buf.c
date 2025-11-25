GNOME/mutter

commit 78b2889df47e7837ea611522f18f265da849562c

wayland/dma-buf: Check plane formats before testing scanout
Bail early for known unsupported DRM formats, avoiding some
allocations, cpu cycles and context switches.

Further more having information about planes and CRTCs available
here will come in handy in follow-up work.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4383>

src/wayland/meta-wayland-dma-buf.c
@@ -642,11 +642,15 @@ crtc_supports_modifier (MetaCrtcKms *crtc_kms,
 CoglScanout *
 meta_wayland_dma_buf_try_acquire_scanout (MetaWaylandBuffer     *buffer,
                                           CoglOnscreen          *onscreen,
+                                          ClutterStageView      *stage_view,
                                           const graphene_rect_t *src_rect,
                                           const MtkRectangle    *dst_rect)
 {
 #ifdef HAVE_NATIVE_BACKEND
   MetaWaylandDmaBufBuffer *dma_buf;
+  MetaRendererView *renderer_view = META_RENDERER_VIEW (stage_view);
+  MetaCrtc *crtc;
+  MetaCrtcKms *crtc_kms;
   MetaContext *context;
   MetaBackend *backend;
   MetaRenderer *renderer;
@@ -664,6 +668,20 @@ meta_wayland_dma_buf_try_acquire_scanout (MetaWaylandBuffer     *buffer,
   dma_buf = meta_wayland_dma_buf_from_buffer (buffer);
   if (!dma_buf)
     return NULL;
+
+  crtc = meta_renderer_view_get_crtc (renderer_view);
+  g_return_val_if_fail (META_IS_CRTC_KMS (crtc), NULL);
+  crtc_kms = META_CRTC_KMS (crtc);
+  if (!crtc_supports_modifier (crtc_kms,
+                               dma_buf->drm_format,
+                               dma_buf->drm_modifier))
+    {
+      meta_topic (META_DEBUG_RENDER,
+                  "DRM format 0x%x (0x%lx) not supported by primary plane",
+                  dma_buf->drm_format,
+                  dma_buf->drm_modifier);
+      return NULL;
+    }
 
   context = meta_wayland_compositor_get_context (dma_buf->manager->compositor);
   backend = meta_context_get_backend (context);

src/wayland/meta-wayland-buffer.c
@@ -961,6 +961,7 @@ scanout_destroyed (gpointer  data,
 CoglScanout *
 meta_wayland_buffer_try_acquire_scanout (MetaWaylandBuffer     *buffer,
                                          CoglOnscreen          *onscreen,
+                                         ClutterStageView      *stage_view,
                                          const graphene_rect_t *src_rect,
                                          const MtkRectangle    *dst_rect)
 {
@@ -1002,6 +1003,7 @@ meta_wayland_buffer_try_acquire_scanout (MetaWaylandBuffer     *buffer,
       {
         scanout = meta_wayland_dma_buf_try_acquire_scanout (buffer,
                                                             onscreen,
+                                                            stage_view,
                                                             src_rect,
                                                             dst_rect);
         break;

src/wayland/meta-wayland-buffer.h
@@ -104,6 +104,7 @@ void                    meta_wayland_buffer_process_damage      (MetaWaylandBuff
                                                                  MtkRegion             *region);
 CoglScanout *           meta_wayland_buffer_try_acquire_scanout (MetaWaylandBuffer     *buffer,
                                                                  CoglOnscreen          *onscreen,
+                                                                 ClutterStageView      *stage_view,
                                                                  const graphene_rect_t *src_rect,
                                                                  const MtkRectangle    *dst_rect);
 

src/wayland/meta-wayland-dma-buf.h
@@ -73,5 +73,6 @@ meta_wayland_drm_syncobj_create_source (MetaWaylandBuffer                *buffer
 CoglScanout *
 meta_wayland_dma_buf_try_acquire_scanout (MetaWaylandBuffer     *buffer,
                                           CoglOnscreen          *onscreen,
+                                          ClutterStageView      *stage_view,
                                           const graphene_rect_t *src_rect,
                                           const MtkRectangle    *dst_rect);

src/wayland/meta-wayland-surface.c
@@ -2473,6 +2473,7 @@ meta_wayland_surface_try_acquire_scanout (MetaWaylandSurface *surface,
 
   return meta_wayland_buffer_try_acquire_scanout (surface->buffer,
                                                   onscreen,
+                                                  stage_view,
                                                   src_rect_ptr,
                                                   &crtc_dst_rect);
 }

commit 64655517589e7bb5636bd5fe3576fef41e67b022

wayland/dma-buf: Fix direct scanout on drivers without explicit modifiers.
The crtc_supports_modifier() check is meant to allow direct scanout on
kms planes if the drm_modifier is DRM_FORMAT_MOD_INVALID, ie. no
explicit modifier, as implemented by commit 0f452107a4ff.

It currently fails to do so if the underlying display hardware kms plane
does not report any explicit modifiers. This prevents direct scanout
on all AMD gpu's before the Gfx 9 Vega gpu family, ie. relatively recent
Polaris and earlier, whereas older versions of Mutter before v48.2 allowed
efficient direct scanout on these gpu's. This wasn't a problem before commit
78b2889df47e ("wayland/dma-buf: Check plane formats before testing scanout")
introduced crtc_supports_modifier() into the direct scanout check for
Mutter 48.2, but now it is.

Refine the check a bit: If the plane supports the drm_format, and the
modifier is DRM_FORMAT_MOD_INVALID, allow direct scanout, even if the
plane does not support explicit modifiers at all. Only require explicit
plane modifiers if we are actually testing against an explicit modifier.

Restores direct scanout, as successfully tested on AMD Polaris.

Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
Fixes: 0f452107a4ff ("wayland/dma-buf: Assume KMS planes support the invalid modifier")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4773>

src/wayland/meta-wayland-dma-buf.c
@@ -631,12 +631,15 @@ crtc_supports_modifier (MetaCrtcKms *crtc_kms,
 
   g_return_val_if_fail (plane, FALSE);
 
-  crtc_modifiers = meta_kms_plane_get_modifiers_for_format (plane, drm_format);
-  if (!crtc_modifiers)
+  if (!meta_kms_plane_is_format_supported (plane, drm_format))
     return FALSE;
 
   if (drm_modifier == DRM_FORMAT_MOD_INVALID)
     return TRUE;
+
+  crtc_modifiers = meta_kms_plane_get_modifiers_for_format (plane, drm_format);
+  if (!crtc_modifiers)
+    return FALSE;
 
   return has_modifier (crtc_modifiers, drm_modifier);
 }