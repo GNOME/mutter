GNOME/mutter

commit a48f6117df32e0b89af6b2d8ef668b8ce78d8831

monitor-manager: Add and use 'monitors-changing' signal
There are places that needs to happen before the rest of the backend
handles monitors changed (via the 'monitors-changed-internal' signal),
for example the MetaWaylandOutput needs to be updated before views are
recreated, so that Wayland surface actors receives the views-changed
signal with already valid MetaWaylandOutput's.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4434>

src/wayland/meta-wayland-outputs.c
@@ -575,8 +575,8 @@ meta_wayland_compositor_update_outputs (MetaWaylandCompositor *compositor,
 }
 
 static void
-on_monitors_changed (MetaMonitorManager    *monitors,
-                     MetaWaylandCompositor *compositor)
+on_monitors_changing (MetaMonitorManager    *monitors,
+                      MetaWaylandCompositor *compositor)
 {
   compositor->outputs = meta_wayland_compositor_update_outputs (compositor, monitors);
 }
@@ -787,7 +787,7 @@ meta_wayland_outputs_finalize (MetaWaylandCompositor *compositor)
   MetaMonitorManager *monitor_manager =
     meta_backend_get_monitor_manager (backend);
 
-  g_signal_handlers_disconnect_by_func (monitor_manager, on_monitors_changed,
+  g_signal_handlers_disconnect_by_func (monitor_manager, on_monitors_changing,
                                         compositor);
 
   g_hash_table_destroy (compositor->outputs);
@@ -801,8 +801,8 @@ meta_wayland_outputs_init (MetaWaylandCompositor *compositor)
   MetaMonitorManager *monitor_manager =
     meta_backend_get_monitor_manager (backend);
 
-  g_signal_connect (monitor_manager, "monitors-changed",
-                    G_CALLBACK (on_monitors_changed), compositor);
+  g_signal_connect (monitor_manager, "monitors-changing",
+                    G_CALLBACK (on_monitors_changing), compositor);
 
   compositor->outputs =
     meta_wayland_compositor_update_outputs (compositor, monitor_manager);

src/backends/meta-monitor-manager.c
@@ -88,6 +88,7 @@ enum
 {
   MONITORS_CHANGED,
   MONITORS_CHANGED_INTERNAL,
+  MONITORS_CHANGING,
   POWER_SAVE_MODE_CHANGED,
   CONFIRM_DISPLAY_CHANGE,
   MONITOR_PRIVACY_SCREEN_CHANGED,
@@ -1360,6 +1361,7 @@ meta_monitor_manager_notify_monitors_changed (MetaMonitorManager *manager)
   update_has_external_monitor (manager);
   update_backlight (manager, TRUE);
 
+  g_signal_emit (manager, signals[MONITORS_CHANGING], 0);
   meta_backend_monitors_changed (manager->backend);
 
   g_signal_emit (manager, signals[MONITORS_CHANGED_INTERNAL], 0);
@@ -1589,6 +1591,14 @@ meta_monitor_manager_class_init (MetaMonitorManagerClass *klass)
 
   signals[MONITORS_CHANGED_INTERNAL] =
     g_signal_new ("monitors-changed-internal",
+                  G_TYPE_FROM_CLASS (object_class),
+                  G_SIGNAL_RUN_LAST,
+                  0,
+                  NULL, NULL, NULL,
+                  G_TYPE_NONE, 0);
+
+  signals[MONITORS_CHANGING] =
+    g_signal_new ("monitors-changing",
                   G_TYPE_FROM_CLASS (object_class),
                   G_SIGNAL_RUN_LAST,
                   0,

commit 31d6cb7d184d558cb78d81e4a39eccf8f6d35f7e

xwayland: Update highest monitor scale before sending output events
Otherwise the logical layout in xdg_output events is based on the
previous highest monitor scale, resulting in Xwayland's layout being
inconsistent with mutter's.

Fixes: a48f6117df32 ("monitor-manager: Add and use 'monitors-changing' signal")
Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4379
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4731>

src/wayland/meta-wayland-outputs.c
@@ -804,8 +804,8 @@ meta_wayland_outputs_init (MetaWaylandCompositor *compositor)
   MetaMonitorManager *monitor_manager =
     meta_backend_get_monitor_manager (backend);
 
-  g_signal_connect (monitor_manager, "monitors-changing",
-                    G_CALLBACK (on_monitors_changing), compositor);
+  g_signal_connect_after (monitor_manager, "monitors-changing",
+                          G_CALLBACK (on_monitors_changing), compositor);
 
   meta_wayland_compositor_update_outputs (compositor, monitor_manager);
 

src/wayland/meta-xwayland.c
@@ -1153,7 +1153,7 @@ meta_xwayland_init (MetaXWaylandManager    *manager,
   /* Xwayland specific protocol, needs to be filtered out for all other clients */
   meta_xwayland_grab_keyboard_init (compositor);
 
-  g_signal_connect_swapped (monitor_manager, "monitors-changed-internal",
+  g_signal_connect_swapped (monitor_manager, "monitors-changing",
                             G_CALLBACK (update_highest_monitor_scale), manager);
   update_highest_monitor_scale (manager);
 