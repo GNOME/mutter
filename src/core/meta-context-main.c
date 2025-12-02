GNOME/mutter

commit 820a7ad813a38b30dc038e7ad234cbaf69443d09

build: Allow building xwayland without x11
Co-authored-by: Jonas Ådahl <jadahl@gmail.com>
Closes https://gitlab.gnome.org/GNOME/mutter/-/issues/3553

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3853>

src/x11/meta-x11-display.c
@@ -1242,7 +1242,9 @@ meta_x11_display_new (MetaDisplay  *display,
   if (!xdisplay)
     return NULL;
 
+#ifdef HAVE_X11
   XSynchronize (xdisplay, meta_context_is_x11_sync (context));
+#endif
 
 #ifdef HAVE_XWAYLAND
   if (meta_is_wayland_compositor ())
@@ -1340,9 +1342,8 @@ meta_x11_display_new (MetaDisplay  *display,
                            x11_display,
                            G_CONNECT_SWAPPED);
 
-#ifdef HAVE_XWAYLAND
+#ifdef HAVE_X11
   if (!meta_is_wayland_compositor ())
-#endif
     {
       ClutterStage *stage =
         CLUTTER_STAGE (meta_get_stage_for_display (display));
@@ -1353,6 +1354,7 @@ meta_x11_display_new (MetaDisplay  *display,
                                x11_display,
                                G_CONNECT_SWAPPED);
     }
+#endif
 
   x11_display->xids = g_hash_table_new (meta_unsigned_long_hash,
                                         meta_unsigned_long_equal);
@@ -1754,6 +1756,7 @@ update_cursor_theme (MetaX11Display *x11_display)
   set_cursor_theme (x11_display->xdisplay, backend);
   schedule_reload_x11_cursor (x11_display);
 
+#ifdef HAVE_X11
   if (META_IS_BACKEND_X11 (backend))
     {
       MetaBackendX11 *backend_x11 = META_BACKEND_X11 (backend);
@@ -1762,6 +1765,7 @@ update_cursor_theme (MetaX11Display *x11_display)
       set_cursor_theme (xdisplay, backend);
       meta_backend_x11_reload_cursor (backend_x11);
     }
+#endif
 }
 
 MetaWindow *
@@ -1883,27 +1887,27 @@ create_guard_window (MetaX11Display *x11_display)
   /* https://bugzilla.gnome.org/show_bug.cgi?id=710346 */
   XStoreName (x11_display->xdisplay, guard_window, "mutter guard window");
 
-  {
-    if (!meta_is_wayland_compositor ())
-      {
-        MetaBackendX11 *backend =
-          META_BACKEND_X11 (backend_from_x11_display (x11_display));
-        Display *backend_xdisplay = meta_backend_x11_get_xdisplay (backend);
-        unsigned char mask_bits[XIMaskLen (XI_LASTEVENT)] = { 0 };
-        XIEventMask mask = { XIAllMasterDevices, sizeof (mask_bits), mask_bits };
+#ifdef HAVE_X11
+  if (!meta_is_wayland_compositor ())
+    {
+      MetaBackendX11 *backend =
+        META_BACKEND_X11 (backend_from_x11_display (x11_display));
+      Display *backend_xdisplay = meta_backend_x11_get_xdisplay (backend);
+      unsigned char mask_bits[XIMaskLen (XI_LASTEVENT)] = { 0 };
+      XIEventMask mask = { XIAllMasterDevices, sizeof (mask_bits), mask_bits };
 
-        XISetMask (mask.mask, XI_ButtonPress);
-        XISetMask (mask.mask, XI_ButtonRelease);
-        XISetMask (mask.mask, XI_Motion);
+      XISetMask (mask.mask, XI_ButtonPress);
+      XISetMask (mask.mask, XI_ButtonRelease);
+      XISetMask (mask.mask, XI_Motion);
 
-        /* Sync on the connection we created the window on to
-         * make sure it's created before we select on it on the
-         * backend connection. */
-        XSync (x11_display->xdisplay, False);
+      /* Sync on the connection we created the window on to
+        * make sure it's created before we select on it on the
+        * backend connection. */
+      XSync (x11_display->xdisplay, False);
 
-        XISelectEvents (backend_xdisplay, guard_window, &mask, 1);
-      }
-  }
+      XISelectEvents (backend_xdisplay, guard_window, &mask, 1);
+    }
+#endif
 
   meta_stack_tracker_record_add (x11_display->display->stack_tracker,
                                  guard_window,
@@ -2429,6 +2433,7 @@ meta_x11_display_set_stage_input_region (MetaX11Display *x11_display,
                                          XRectangle     *rects,
                                          int             n_rects)
 {
+#ifdef HAVE_X11
   Display *xdisplay = x11_display->xdisplay;
   MetaBackend *backend = backend_from_x11_display (x11_display);
   ClutterStage *stage = CLUTTER_STAGE (meta_backend_get_stage (backend));
@@ -2447,6 +2452,7 @@ meta_x11_display_set_stage_input_region (MetaX11Display *x11_display,
   XFixesSetWindowShapeRegion (xdisplay,
                               x11_display->composite_overlay_window,
                               ShapeInput, 0, 0, x11_display->stage_input_region);
+#endif
 }
 
 /**

cogl/meson.build
@@ -34,7 +34,7 @@ if have_egl
   ]
 endif
 
-if have_x11_client
+if have_x11
   cogl_pkg_deps += [
     x11_dep,
   ]

meson.build
@@ -192,14 +192,14 @@ if have_egl
   egl_dep = dependency('egl')
 endif
 
-have_glx = get_option('glx') and have_x11_client
+have_glx = get_option('glx') and have_x11
 if have_glx
   if not have_gl
     error('GLX support requires OpenGL to be enabled')
   endif
 endif
 
-have_egl_xlib = have_egl and have_x11_client
+have_egl_xlib = have_egl and have_x11
 
 have_gles2 = get_option('gles2')
 if have_gles2

mtk/meson.build
@@ -33,7 +33,7 @@ mtk_pkg_deps = [
   pixman_dep,
 ]
 
-if have_x11
+if have_x11_client
   mtk_pkg_deps += x11_dep
 endif
 

mtk/mtk/meson.build
@@ -12,7 +12,7 @@ mtk_sources = [
   'mtk-region.c',
 ]
 
-if have_x11
+if have_x11_client
   mtk_sources += 'mtk-x11-errors.c'
   mtk_headers += [
     'mtk-x11-errors.h',

src/compositor/meta-window-actor-x11.c
@@ -437,6 +437,7 @@ has_shadow (MetaWindowActorX11 *actor_x11)
   return TRUE;
 }
 
+#ifdef HAVE_X11
 gboolean
 meta_window_actor_x11_should_unredirect (MetaWindowActorX11 *actor_x11)
 {
@@ -478,6 +479,7 @@ meta_window_actor_x11_set_unredirected (MetaWindowActorX11 *actor_x11,
   surface_x11 = META_SURFACE_ACTOR_X11 (surface);
   meta_surface_actor_x11_set_unredirected (surface_x11, unredirected);
 }
+#endif /* HAVE_X11 */
 
 static const char *
 get_shadow_class (MetaWindowActorX11 *actor_x11)
@@ -1013,9 +1015,11 @@ is_actor_maybe_transparent (MetaWindowActorX11 *actor_x11)
   if (!surface)
     return TRUE;
 
+#ifdef HAVE_X11
   if (META_IS_SURFACE_ACTOR_X11 (surface) &&
       meta_surface_actor_x11_is_unredirected (META_SURFACE_ACTOR_X11 (surface)))
     return FALSE;
+#endif
 
   stex = meta_surface_actor_get_texture (surface);
   if (!meta_shaped_texture_has_alpha (stex))
@@ -1148,9 +1152,11 @@ handle_updates (MetaWindowActorX11 *actor_x11)
     meta_window_actor_get_surface (META_WINDOW_ACTOR (actor_x11));
   MetaWindow *window;
 
+#ifdef HAVE_X11
   if (META_IS_SURFACE_ACTOR_X11 (surface) &&
       meta_surface_actor_x11_is_unredirected (META_SURFACE_ACTOR_X11 (surface)))
     return;
+#endif
 
   window = meta_window_actor_get_meta_window (META_WINDOW_ACTOR (actor_x11));
   if (meta_window_actor_is_frozen (META_WINDOW_ACTOR (actor_x11)))
@@ -1168,6 +1174,7 @@ handle_updates (MetaWindowActorX11 *actor_x11)
       return;
     }
 
+#ifdef HAVE_X11
   if (META_IS_SURFACE_ACTOR_X11 (surface))
     {
       MetaSurfaceActorX11 *surface_x11 = META_SURFACE_ACTOR_X11 (surface);
@@ -1178,6 +1185,7 @@ handle_updates (MetaWindowActorX11 *actor_x11)
   if (META_IS_SURFACE_ACTOR_X11 (surface) &&
       !meta_surface_actor_x11_is_visible (META_SURFACE_ACTOR_X11 (surface)))
     return;
+#endif /* HAVE_X11 */
 
   update_frame_bounds (actor_x11);
   check_needs_reshape (actor_x11);

src/compositor/meta-window-actor-x11.h
@@ -20,6 +20,8 @@
 
 #pragma once
 
+#include <X11/extensions/Xdamage.h>
+
 #include "compositor/meta-window-actor-private.h"
 
 #define META_TYPE_WINDOW_ACTOR_X11 (meta_window_actor_x11_get_type())
@@ -31,10 +33,12 @@ G_DECLARE_FINAL_TYPE (MetaWindowActorX11,
 void meta_window_actor_x11_process_x11_damage (MetaWindowActorX11 *actor_x11,
                                                XDamageNotifyEvent *event);
 
+#ifdef HAVE_X11
 gboolean meta_window_actor_x11_should_unredirect (MetaWindowActorX11 *actor_x11);
 
 void meta_window_actor_x11_set_unredirected (MetaWindowActorX11 *actor_x11,
                                              gboolean            unredirected);
+#endif
 
 void meta_window_actor_x11_update_shape (MetaWindowActorX11 *actor_x11);
 

src/compositor/meta-window-actor.c
@@ -495,7 +495,7 @@ init_surface_actor (MetaWindowActor *self)
   MetaWindow *window = priv->window;
   MetaSurfaceActor *surface_actor = NULL;
 
-#ifdef HAVE_X11_CLIENT
+#ifdef HAVE_X11
   if (!meta_is_wayland_compositor ())
     {
       surface_actor = meta_surface_actor_x11_new (window);
@@ -953,7 +953,7 @@ meta_window_actor_sync_actor_geometry (MetaWindowActor *self,
 
   /* When running as a Wayland compositor we catch size changes when new
    * buffers are attached */
-#ifdef HAVE_X11_CLIENT
+#ifdef HAVE_X11
   if (META_IS_SURFACE_ACTOR_X11 (priv->surface))
     meta_surface_actor_x11_set_size (META_SURFACE_ACTOR_X11 (priv->surface),
                                      actor_rect.width, actor_rect.height);

commit cb29a10bfd8709b7dee4f0e370074969d905d1fd

x11: Build is_x11_sync for Xwayland as well
It's not specific to the X11 backend.

Fixes: 820a7ad813a3 ("build: Allow building xwayland without x11")
Fixes: fdde7e0f3706 ("x11: Let X11 connection owners call XSynchronize() themselves")

v2: (Bilal Elmoussaoui)
* Use HAVE_X11_CLIENT.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4789>

src/core/meta-context-main.c
@@ -560,7 +560,7 @@ meta_context_main_notify_ready (MetaContext *context)
     g_critical ("Could not create session manager: %s", error->message);
 }
 
-#ifdef HAVE_X11
+#ifdef HAVE_X11_CLIENT
 static gboolean
 meta_context_main_is_x11_sync (MetaContext *context)
 {
@@ -790,7 +790,7 @@ meta_context_main_class_init (MetaContextMainClass *klass)
   context_class->setup = meta_context_main_setup;
   context_class->create_backend = meta_context_main_create_backend;
   context_class->notify_ready = meta_context_main_notify_ready;
-#ifdef HAVE_X11
+#ifdef HAVE_X11_CLIENT
   context_class->is_x11_sync = meta_context_main_is_x11_sync;
 #endif
   context_class->get_session_manager = meta_context_main_get_session_manager;

src/core/meta-context-private.h
@@ -54,7 +54,7 @@ struct _MetaContextClass
 
   void (* notify_ready) (MetaContext *context);
 
-#ifdef HAVE_X11
+#ifdef HAVE_X11_CLIENT
   gboolean (* is_x11_sync) (MetaContext *context);
 #endif
 
@@ -79,7 +79,7 @@ MetaServiceChannel * meta_context_get_service_channel (MetaContext *context);
 
 MetaX11DisplayPolicy meta_context_get_x11_display_policy (MetaContext *context);
 
-#ifdef HAVE_X11
+#ifdef HAVE_X11_CLIENT
 META_EXPORT_TEST
 gboolean meta_context_is_x11_sync (MetaContext *context);
 #endif

src/core/meta-context.c
@@ -331,7 +331,7 @@ meta_context_get_x11_display_policy (MetaContext *context)
   return META_CONTEXT_GET_CLASS (context)->get_x11_display_policy (context);
 }
 
-#ifdef HAVE_X11
+#ifdef HAVE_X11_CLIENT
 gboolean
 meta_context_is_x11_sync (MetaContext *context)
 {

src/x11/meta-x11-display.c
@@ -1432,9 +1432,7 @@ meta_x11_display_new (MetaDisplay  *display,
   if (!xdisplay)
     return NULL;
 
-#ifdef HAVE_X11
   XSynchronize (xdisplay, meta_context_is_x11_sync (context));
-#endif
 
 #ifdef HAVE_XWAYLAND
   if (meta_is_wayland_compositor ())

src/tests/meta-context-test.c
@@ -277,7 +277,7 @@ meta_context_test_get_session_manager (MetaContext *context)
   return priv->session_manager;
 }
 
-#ifdef HAVE_X11
+#ifdef HAVE_X11_CLIENT
 static gboolean
 meta_context_test_is_x11_sync (MetaContext *context)
 {
@@ -445,7 +445,7 @@ meta_context_test_class_init (MetaContextTestClass *klass)
   context_class->setup = meta_context_test_setup;
   context_class->create_backend = meta_context_test_create_backend;
   context_class->notify_ready = meta_context_test_notify_ready;
-#ifdef HAVE_X11
+#ifdef HAVE_X11_CLIENT
   context_class->is_x11_sync = meta_context_test_is_x11_sync;
 #endif
   context_class->get_session_manager = meta_context_test_get_session_manager;