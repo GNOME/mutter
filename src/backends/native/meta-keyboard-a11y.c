GNOME/mutter

commit 34bb61aeedb68d0fbae62fc0234cd04cf6655d89

backends/native: Update keymap after applying a11y modifiers
meta_input_device_native_process_kbd_a11y_event_in_impl may modify the seat
xkb state if sticky keys are enabled.

meta_keymap_native_update_in_impl, which reads the seat xkb state to set the
clutter keymap accordingly, should be run after processing sticky keys.

To implement this we call process_kbd_a11y_event_in_impl first, store the
potentially rewritten event, then update the keymap, and finally enqueue the
event.

This doesn't actually fix any bugs that I'm aware of, because the clutter
keymap would have gotten updated just before enqueueing the next key event
and that's enough to keep sticky keys working, but it does make the logic
easier to follow and may prevent future bugs.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4730>

src/backends/native/meta-keyboard-a11y.c
@@ -373,7 +373,7 @@ update_internal_xkb_state (MetaKeyboardA11y *keyboard_a11y,
   g_rw_lock_writer_unlock (&seat_impl->state_lock);
 }
 
-static void
+static ClutterEvent *
 rewrite_stickykeys_event (ClutterEvent     *event,
                           MetaKeyboardA11y *keyboard_a11y,
                           xkb_mod_mask_t    new_latched_mask,
@@ -409,7 +409,7 @@ rewrite_stickykeys_event (ClutterEvent     *event,
                            clutter_event_get_key_code (event),
                            clutter_event_get_key_unicode (event));
 
-  _clutter_event_push (rewritten_event, FALSE);
+  return rewritten_event;
 }
 
 static void
@@ -464,7 +464,7 @@ set_slowkeys_on (MetaKeyboardA11y *keyboard_a11y)
                                                         META_A11Y_SLOW_KEYS_ENABLED);
 }
 
-static gboolean
+static ClutterEvent *
 handle_stickykeys_press (ClutterEvent     *event,
                          MetaKeyboardA11y *keyboard_a11y)
 {
@@ -472,14 +472,13 @@ handle_stickykeys_press (ClutterEvent     *event,
   struct xkb_state *xkb_state;
 
   if (!key_event_is_modifier (event))
-    return FALSE;
+    return NULL;
 
   if (keyboard_a11y->stickykeys_depressed_mask &&
       (keyboard_a11y->a11y_flags & META_A11Y_STICKY_KEYS_TWO_KEY_OFF))
     {
       set_stickykeys_off (keyboard_a11y);
-      rewrite_stickykeys_event (event, keyboard_a11y, 0, 0);
-      return TRUE;
+      return rewrite_stickykeys_event (event, keyboard_a11y, 0, 0);
     }
 
   xkb_state = meta_seat_impl_get_xkb_state_in_impl (seat_impl);
@@ -491,10 +490,10 @@ handle_stickykeys_press (ClutterEvent     *event,
    * to some other modifier which can be sticky.
    */
   keyboard_a11y->stickykeys_depressed_mask &= ~CLUTTER_LOCK_MASK;
-  return FALSE;
+  return NULL;
 }
 
-static gboolean
+static ClutterEvent *
 handle_stickykeys_release (ClutterEvent     *event,
                            MetaKeyboardA11y *keyboard_a11y)
 {
@@ -512,7 +511,7 @@ handle_stickykeys_release (ClutterEvent     *event,
   if (key_event_is_modifier (event))
     {
       if (!depressed_mods)
-        return FALSE;
+        return NULL;
 
       new_latched_mask = keyboard_a11y->stickykeys_latched_mask;
       new_locked_mask = keyboard_a11y->stickykeys_locked_mask;
@@ -537,15 +536,14 @@ handle_stickykeys_release (ClutterEvent     *event,
   else
     {
       if (!keyboard_a11y->stickykeys_latched_mask)
-        return FALSE;
+        return NULL;
 
       new_latched_mask = 0;
       new_locked_mask = keyboard_a11y->stickykeys_locked_mask;
     }
 
-  rewrite_stickykeys_event (event, keyboard_a11y,
-                            new_latched_mask, new_locked_mask);
-  return TRUE;
+  return rewrite_stickykeys_event (event, keyboard_a11y,
+                                   new_latched_mask, new_locked_mask);
 }
 
 static gboolean
@@ -1177,8 +1175,9 @@ meta_keyboard_a11y_maybe_notify_toggle_keys_in_impl (MetaKeyboardA11y *keyboard_
 }
 
 gboolean
-meta_keyboard_a11y_process_event_in_impl (MetaKeyboardA11y *keyboard_a11y,
-                                          ClutterEvent     *event)
+meta_keyboard_a11y_process_event_in_impl (MetaKeyboardA11y  *keyboard_a11y,
+                                          ClutterEvent      *event,
+                                          ClutterEvent     **out_event)
 {
   ClutterEventType event_type;
 
@@ -1229,9 +1228,15 @@ meta_keyboard_a11y_process_event_in_impl (MetaKeyboardA11y *keyboard_a11y,
   if (keyboard_a11y->a11y_flags & META_A11Y_STICKY_KEYS_ENABLED)
     {
       if (event_type == CLUTTER_KEY_PRESS)
-        return handle_stickykeys_press (event, keyboard_a11y);
+        {
+          *out_event = handle_stickykeys_press (event, keyboard_a11y);
+          return out_event != NULL;
+        }
       else if (event_type == CLUTTER_KEY_RELEASE)
-        return handle_stickykeys_release (event, keyboard_a11y);
+        {
+          *out_event = handle_stickykeys_release (event, keyboard_a11y);
+          return out_event != NULL;
+        }
     }
 
   return FALSE;

src/backends/native/meta-keyboard-a11y.h
@@ -31,5 +31,6 @@ void meta_keyboard_a11y_apply_settings_in_impl (MetaKeyboardA11y    *keyboard_a1
 
 void meta_keyboard_a11y_maybe_notify_toggle_keys_in_impl (MetaKeyboardA11y *keyboard_a11y);
 
-gboolean meta_keyboard_a11y_process_event_in_impl (MetaKeyboardA11y *keyboard_a11y,
-                                                   ClutterEvent     *event);
+gboolean meta_keyboard_a11y_process_event_in_impl (MetaKeyboardA11y  *keyboard_a11y,
+                                                   ClutterEvent      *event,
+                                                   ClutterEvent     **out_event);

src/backends/native/meta-seat-impl.c
@@ -586,11 +586,13 @@ meta_seat_impl_notify_key_in_impl (MetaSeatImpl       *seat_impl,
                                    gboolean            update_keys)
 {
   ClutterEvent *event = NULL;
+  ClutterEvent *rewritten_event = NULL;
   ClutterEventFlags flags = CLUTTER_EVENT_NONE;
   enum xkb_state_component changed_state;
   uint32_t keycode;
   uint32_t keysym;
   gboolean should_ignore;
+  gboolean event_swallowed;
 
   if (state != AUTOREPEAT_VALUE)
     {
@@ -635,16 +637,28 @@ meta_seat_impl_notify_key_in_impl (MetaSeatImpl       *seat_impl,
                                             state ? XKB_KEY_DOWN : XKB_KEY_UP);
     }
 
+  event_swallowed =
+    meta_keyboard_a11y_process_event_in_impl (seat_impl->keyboard_a11y,
+                                              event,
+                                              &rewritten_event);
+
   if (update_keys)
     {
       meta_keymap_native_update_in_impl (seat_impl->keymap,
                                          seat_impl->xkb);
     }
 
-  if (!meta_keyboard_a11y_process_event_in_impl (seat_impl->keyboard_a11y, event))
-    queue_event (seat_impl, event);
+  if (event_swallowed)
+    {
+      clutter_event_free (event);
+      if (rewritten_event)
+        queue_event (seat_impl, rewritten_event);
+    }
   else
-    clutter_event_free (event);
+    {
+      queue_event (seat_impl, event);
+    }
+
 
   if (update_keys && (changed_state & XKB_STATE_LEDS))
     {

commit 8de10aebbd5112e43a3c7b6435b1bcb2653f5415

backends/native: Fixup input with sticky keys enabled
Fixes: 34bb61aeed ("backends/native: Update keymap after applying a11y modifiers")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4790>

src/backends/native/meta-keyboard-a11y.c
@@ -1230,12 +1230,12 @@ meta_keyboard_a11y_process_event_in_impl (MetaKeyboardA11y  *keyboard_a11y,
       if (event_type == CLUTTER_KEY_PRESS)
         {
           *out_event = handle_stickykeys_press (event, keyboard_a11y);
-          return out_event != NULL;
+          return *out_event != NULL;
         }
       else if (event_type == CLUTTER_KEY_RELEASE)
         {
           *out_event = handle_stickykeys_release (event, keyboard_a11y);
-          return out_event != NULL;
+          return *out_event != NULL;
         }
     }
 