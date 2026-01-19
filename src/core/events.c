GNOME/mutter

commit 52974792dec42448fc2ff48d8f8b5811be7c5447

Merge have_x11_client with have_xwayland
By keeping only have_xwayland as that is the only possible option with
the dropped X11 backend.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4505>

src/core/events.c
@@ -33,13 +33,10 @@
 #include "core/display-private.h"
 #include "core/window-private.h"
 #include "meta/meta-backend.h"
+#include "wayland/meta-wayland-private.h"
 
 #ifdef HAVE_NATIVE_BACKEND
 #include "backends/native/meta-backend-native.h"
-#endif
-
-#ifdef HAVE_WAYLAND
-#include "wayland/meta-wayland-private.h"
 #endif
 
 #define IS_KEY_EVENT(et) ((et) == CLUTTER_KEY_PRESS || \
@@ -176,19 +173,12 @@ meta_display_handle_event (MetaDisplay        *display,
   gboolean a11y_grabbed;
   MetaTabletActionMapper *mapper;
   MetaEventMode mode_hint;
-#ifdef HAVE_WAYLAND
   MetaWaylandCompositor *wayland_compositor;
   MetaWaylandTextInput *wayland_text_input = NULL;
-#endif
 
-#ifdef HAVE_WAYLAND
   wayland_compositor = meta_context_get_wayland_compositor (context);
-  if (wayland_compositor)
-    {
-      wayland_text_input =
-        meta_wayland_compositor_get_text_input (wayland_compositor);
-    }
-#endif
+  wayland_text_input =
+    meta_wayland_compositor_get_text_input (wayland_compositor);
 
   COGL_TRACE_BEGIN_SCOPED (MetaDisplayHandleEvent,
                            "Meta::Display::handle_event()");
@@ -235,15 +225,12 @@ meta_display_handle_event (MetaDisplay        *display,
         }
     }
 
-#ifdef HAVE_WAYLAND
   if (wayland_text_input &&
       !meta_compositor_get_current_window_drag (compositor) &&
       meta_wayland_text_input_update (wayland_text_input, event))
     return CLUTTER_EVENT_STOP;
 
-  if (wayland_compositor)
-    meta_wayland_compositor_update (wayland_compositor, event);
-#endif
+  meta_wayland_compositor_update (wayland_compositor, event);
 
   if (event_type == CLUTTER_PAD_BUTTON_PRESS ||
       event_type == CLUTTER_PAD_BUTTON_RELEASE ||
@@ -397,20 +384,15 @@ meta_display_handle_event (MetaDisplay        *display,
       mode_hint = META_EVENT_MODE_KEEP_FROZEN;
     }
 
-#ifdef HAVE_WAYLAND
-  if (wayland_compositor)
-    {
-      uint32_t time_ms;
+    uint32_t time_ms;
 
-      time_ms = clutter_event_get_time (event);
-      if (window && event_type == CLUTTER_MOTION &&
-          time_ms != CLUTTER_CURRENT_TIME)
-        meta_window_check_alive_on_event (window, time_ms);
+    time_ms = clutter_event_get_time (event);
+    if (window && event_type == CLUTTER_MOTION &&
+        time_ms != CLUTTER_CURRENT_TIME)
+      meta_window_check_alive_on_event (window, time_ms);
 
-      if (meta_wayland_compositor_handle_event (wayland_compositor, event))
-        return CLUTTER_EVENT_STOP;
-    }
-#endif
+    if (meta_wayland_compositor_handle_event (wayland_compositor, event))
+      return CLUTTER_EVENT_STOP;
 
   return meta_compositor_handle_event (compositor, event, window, mode_hint);
 }

.gitlab-ci.yml
@@ -492,7 +492,6 @@ build-wayland-only@x86_64:
         --werror
         --wrap-mode nofallback
         -Dbuildtype=debugoptimized
-        -Dwayland=true
         -Dxwayland=false
         -Dtests=disabled
         -Dintrospection=false

cogl/meson.build
@@ -9,6 +9,7 @@ cogl_pkg_deps = [
   gio_dep,
   gobject_dep,
   graphene_dep,
+  wayland_server_dep,
 ]
 
 cogl_pkg_private_deps = [
@@ -19,12 +20,6 @@ cogl_pkg_private_deps = [
 if have_profiler
   cogl_pkg_private_deps += [
     libsysprof_capture_dep,
-  ]
-endif
-
-if have_wayland
-  cogl_pkg_deps += [
-    wayland_server_dep,
   ]
 endif
 

config.h.meson
@@ -52,14 +52,8 @@
 /* Define if you want to enable the native (KMS) backend based on systemd */
 #mesondefine HAVE_NATIVE_BACKEND
 
-/* Define if you want to enable Wayland support */
-#mesondefine HAVE_WAYLAND
-
 /* Define if you want to enable XWayland support */
 #mesondefine HAVE_XWAYLAND
-
-/* Define if either XWayland or X11 backend are enabled */
-#mesondefine HAVE_X11_CLIENT
 
 /* Defined if screen cast and remote desktop support is enabled */
 #mesondefine HAVE_REMOTE_DESKTOP

data/meson.build
@@ -26,13 +26,8 @@ keybinding_xml_files = [
   '50-mutter-navigation.xml',
   '50-mutter-system.xml',
   '50-mutter-windows.xml',
+  '50-mutter-wayland.xml',
 ]
-
-if have_wayland
-  keybinding_xml_files += [
-    '50-mutter-wayland.xml',
-  ]
-endif
 
 install_data(keybinding_xml_files,
   install_dir: gnome_keybindings_keysdir,

meson.build
@@ -139,18 +139,9 @@ libeis_dep = dependency('libeis-1.0', version: libei_req)
 libei_dep = dependency('libei-1.0', version: libei_req)
 gvdb_dep = dependency('gvdb')
 libdisplay_info_dep = dependency('libdisplay-info', version: libdisplay_info_req)
+libdrm_dep = dependency('libdrm', version: libdrm_req)
 
-have_wayland = get_option('wayland')
 have_xwayland = get_option('xwayland')
-have_x11_client = have_xwayland
-
-if have_xwayland and not have_wayland
-  error('XWayland support requires Wayland support enabled')
-endif
-
-if not have_wayland
-  error('A Wayland backend must be enabled')
-endif
 
 have_fonts = get_option('fonts')
 
@@ -164,7 +155,7 @@ if have_fonts
   fribidi_dep = dependency('fribidi', version: fribidi_req)
 endif
 
-if have_x11_client
+if have_xwayland
   if not gtk4_dep.found()
     error('X11 client support requires gtk4 @0@'.format(gtk4_req))
   endif
@@ -219,23 +210,21 @@ if not have_gl and not have_gles2
   error('Neither GLES2 or OpenGL was enabled')
 endif
 
-if have_wayland
-  if meson.version().version_compare('>= 1.8.0')
-    wl_mod = import('wayland')
-  else
-    wl_mod = import('unstable-wayland')
-  endif
+if meson.version().version_compare('>= 1.8.0')
+  wl_mod = import('wayland')
+else
+  wl_mod = import('unstable-wayland')
+endif
 
-  wayland_server_dep = dependency('wayland-server', version: wayland_server_req)
-  wayland_client_dep = dependency('wayland-client', version: wayland_server_req)
-  wayland_cursor_dep = dependency('wayland-cursor')
-  wayland_protocols_dep = dependency('wayland-protocols',
-                                     version: wayland_protocols_req)
-  wayland_egl_dep = dependency('wayland-egl')
+wayland_server_dep = dependency('wayland-server', version: wayland_server_req)
+wayland_client_dep = dependency('wayland-client', version: wayland_server_req)
+wayland_cursor_dep = dependency('wayland-cursor')
+wayland_protocols_dep = dependency('wayland-protocols',
+                                    version: wayland_protocols_req)
+wayland_egl_dep = dependency('wayland-egl')
 
-  if not have_egl
-    error('Wayland support requires EGL to be enabled')
-  endif
+if not have_egl
+  error('Wayland support requires EGL to be enabled')
 endif
 
 have_logind = get_option('logind')
@@ -289,14 +278,9 @@ if have_native_backend
   endif
 endif
 
-if have_wayland or have_native_backend
-  libdrm_dep = dependency('libdrm', version: libdrm_req)
-  have_drm_plane_size_hint = cc.has_type('struct drm_plane_size_hint',
-                                         dependencies: libdrm_dep,
-                                         prefix: '#include <drm_mode.h>')
-else
-  have_drm_plane_size_hint = false
-endif
+have_drm_plane_size_hint = cc.has_type('struct drm_plane_size_hint',
+                                        dependencies: libdrm_dep,
+                                        prefix: '#include <drm_mode.h>')
 
 have_egl_device = get_option('egl_device')
 
@@ -305,9 +289,6 @@ if have_wayland_eglstream
   wayland_eglstream_protocols_dep = dependency('wayland-eglstream-protocols')
   dl_dep = cc.find_library('dl', required: true)
 
-  if not have_wayland
-    error('Wayland EGLStream support requires Wayland to be enabled')
-  endif
 endif
 
 
@@ -320,7 +301,7 @@ else
   have_libwacom_get_num_rings = false
 endif
 
-have_startup_notification = get_option('startup_notification') and have_x11_client
+have_startup_notification = get_option('startup_notification') and have_xwayland
 if have_startup_notification
   libstartup_notification_dep = dependency('libstartup-notification-1.0',
                                            version: libstartup_notification_req)
@@ -399,10 +380,8 @@ if have_tests
 
   have_mutter_tests = get_option('mutter_tests')
   if have_mutter_tests
-    if not have_wayland
-      error('Tests require Wayland to be enabled')
-    endif
-    if not have_x11_client
+
+    if not have_xwayland
       error('Tests requires an X11 client')
     endif
     if not dbusmock_dep.found()
@@ -571,9 +550,7 @@ cdata.set_quoted('PACKAGE_VERSION', meson.project_version())
 cdata.set('HAVE_EGL', have_egl)
 cdata.set('HAVE_GL', have_gl)
 cdata.set('HAVE_GLES2', have_gles2)
-cdata.set('HAVE_WAYLAND', have_wayland)
 cdata.set('HAVE_XWAYLAND', have_xwayland)
-cdata.set('HAVE_X11_CLIENT', have_x11_client)
 cdata.set('HAVE_LOGIND', have_logind)
 cdata.set('HAVE_NATIVE_BACKEND', have_native_backend)
 cdata.set('HAVE_REMOTE_DESKTOP', have_remote_desktop)
@@ -597,7 +574,7 @@ cdata.set('HAVE_XKBCOMMON_KANA_COMPOSE_LEDS',
 cdata.set('HAVE_EIS_EVENT_REF',
   libeis_dep.version().version_compare('>= 1.5.0'))
 
-if have_x11_client
+if have_xwayland
   xkb_base = xkeyboard_config_dep.get_variable('xkb_base')
   cdata.set_quoted('XKB_BASE', xkb_base)
 endif
@@ -762,7 +739,6 @@ summary('OpenGL', have_gl, section: 'Rendering APIs')
 summary('GLES2', have_gles2, section: 'Rendering APIs')
 summary('EGL', have_egl, section: 'Rendering APIs')
 
-summary('Wayland', have_wayland, section: 'Options')
 summary('Wayland EGLStream', have_wayland_eglstream, section: 'Options')
 summary('XWayland', have_xwayland, section: 'Options')
 summary('Native Backend', have_native_backend, section: 'Options')

meson.options
@@ -28,12 +28,6 @@ option('egl',
   description: 'Enable EGL support'
 )
 
-option('wayland',
-  type: 'boolean',
-  value: true,
-  description: 'Enable Wayland support'
-)
-
 option('xwayland',
   type: 'boolean',
   value: true,

mtk/meson.build
@@ -21,7 +21,7 @@ mtk_pkg_deps = [
   pixman_dep,
 ]
 
-if have_x11_client
+if have_xwayland
   mtk_pkg_deps += x11_dep
 endif
 

commit 8e9f4a4e1bfc664af1cb46a52ec3ad1a98c140ba

core: Code style fixes
Fallout from commit 52974792d. Move the variable declaration to the
top and fix indenting.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4829>

src/core/events.c
@@ -132,6 +132,7 @@ meta_display_handle_event (MetaDisplay        *display,
   MetaEventMode mode_hint;
   MetaWaylandCompositor *wayland_compositor;
   MetaWaylandTextInput *wayland_text_input = NULL;
+  uint32_t time_ms;
 
   wayland_compositor = meta_context_get_wayland_compositor (context);
   wayland_text_input =
@@ -301,15 +302,13 @@ meta_display_handle_event (MetaDisplay        *display,
       mode_hint = META_EVENT_MODE_KEEP_FROZEN;
     }
 
-    uint32_t time_ms;
+  time_ms = clutter_event_get_time (event);
+  if (window && event_type == CLUTTER_MOTION &&
+      time_ms != CLUTTER_CURRENT_TIME)
+    meta_window_check_alive_on_event (window, time_ms);
 
-    time_ms = clutter_event_get_time (event);
-    if (window && event_type == CLUTTER_MOTION &&
-        time_ms != CLUTTER_CURRENT_TIME)
-      meta_window_check_alive_on_event (window, time_ms);
-
-    if (meta_wayland_compositor_handle_event (wayland_compositor, event))
-      return CLUTTER_EVENT_STOP;
+  if (meta_wayland_compositor_handle_event (wayland_compositor, event))
+    return CLUTTER_EVENT_STOP;
 
   return meta_compositor_handle_event (compositor, event, window, mode_hint);
 }