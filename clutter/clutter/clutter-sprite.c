GNOME/mutter

commit be703baafaf6bca3f963310c6a6890196247fab9

clutter: Add "target" event propagation phase
Matching the at-target event phase in the CSS specs, this
phase is run at the target actor after capture, before bubbling.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4830>

clutter/clutter/clutter-sprite.c
@@ -372,6 +372,7 @@ create_event_emission_chain (ClutterSprite *sprite,
                              ClutterActor  *deepmost)
 {
   ClutterSpritePrivate *priv = clutter_sprite_get_instance_private (sprite);
+  const GList *l;
   int i;
 
   g_assert (priv->cur_event_actors->len == 0);
@@ -380,7 +381,6 @@ create_event_emission_chain (ClutterSprite *sprite,
   for (i = priv->cur_event_actors->len - 1; i >= 0; i--)
     {
       ClutterActor *actor = g_ptr_array_index (priv->cur_event_actors, i);
-      const GList *l;
 
       for (l = clutter_actor_peek_actions (actor); l; l = l->next)
         {
@@ -394,10 +394,18 @@ create_event_emission_chain (ClutterSprite *sprite,
       add_actor_to_event_emission_chain (chain, actor, CLUTTER_PHASE_CAPTURE);
     }
 
+  for (l = clutter_actor_peek_actions (topmost); l; l = l->next)
+    {
+      ClutterAction *action = l->data;
+
+      if (clutter_actor_meta_get_enabled (CLUTTER_ACTOR_META (action)) &&
+          clutter_action_get_phase (action) == CLUTTER_PHASE_TARGET)
+        add_action_to_event_emission_chain (chain, action);
+    }
+
   for (i = 0; i < priv->cur_event_actors->len; i++)
     {
       ClutterActor *actor = g_ptr_array_index (priv->cur_event_actors, i);
-      const GList *l;
 
       for (l = clutter_actor_peek_actions (actor); l; l = l->next)
         {

clutter/clutter/clutter-actor.c
@@ -13831,7 +13831,8 @@ clutter_actor_add_action_full (ClutterActor      *self,
   g_return_if_fail (name != NULL);
   g_return_if_fail (CLUTTER_IS_ACTION (action));
   g_return_if_fail (phase == CLUTTER_PHASE_BUBBLE ||
-                    phase == CLUTTER_PHASE_CAPTURE);
+                    phase == CLUTTER_PHASE_CAPTURE ||
+                    phase == CLUTTER_PHASE_TARGET);
 
   clutter_actor_meta_set_name (CLUTTER_ACTOR_META (action), name);
   clutter_actor_add_action_internal (self, action, phase);

clutter/clutter/clutter-enums.h
@@ -1113,6 +1113,7 @@ typedef enum
 typedef enum
 {
   CLUTTER_PHASE_CAPTURE,
+  CLUTTER_PHASE_TARGET,
   CLUTTER_PHASE_BUBBLE,
 } ClutterEventPhase;
 

commit 9ef151d4bcbbb48258dba6d773767515a5cbb70c

clutter: Fix thinko in what is considered the "target" actor
Per the terminology in use here, this would be the "deepmost" actor,
from the root, not the topmost from the user perspective.

Fixes: be703baafa ("clutter: Add "target" event propagation phase")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4854>

clutter/clutter/clutter-sprite.c
@@ -394,7 +394,7 @@ create_event_emission_chain (ClutterSprite *sprite,
       add_actor_to_event_emission_chain (chain, actor, CLUTTER_PHASE_CAPTURE);
     }
 
-  for (l = clutter_actor_peek_actions (topmost); l; l = l->next)
+  for (l = clutter_actor_peek_actions (deepmost); l; l = l->next)
     {
       ClutterAction *action = l->data;
 