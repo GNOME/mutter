GNOME/mutter

commit 8e81bf686afd16007143d1d720cd90951db58b0f

backend: Change to the new backlight interface
This does three different things

* Removes the backlight API from MetaMonitor and instead adds a
  MetaBacklight getter to the MetaMonitor
* Creates MetaBacklights in MetaOutput specific vfuncs for a specific
  MetaMonitor with a complete display config. This allows us to even
  create the tricky ref-white backlight.
* Adjusts the users of the MetaMonitor backlight API to the
  MetaBacklight API

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4014>

src/meta/meta-monitor.h
@@ -49,3 +49,6 @@ gboolean meta_monitor_is_builtin (MetaMonitor *monitor);
 
 META_EXPORT
 gboolean meta_monitor_is_virtual (MetaMonitor *monitor);
+
+META_EXPORT
+MetaBacklight * meta_monitor_get_backlight (MetaMonitor *monitor);

src/backends/meta-monitor-manager.c
@@ -1406,9 +1406,21 @@ ensure_monitor_color_devices (MetaMonitorManager *manager)
 }
 
 static void
+ensure_monitor_backlights (MetaMonitorManager *manager)
+{
+  for (GList *l = manager->monitors; l; l = l->next)
+    {
+      MetaMonitor *monitor = l->data;
+
+      meta_monitor_create_backlight (monitor);
+    }
+}
+
+static void
 meta_monitor_manager_notify_monitors_changed (MetaMonitorManager *manager)
 {
   ensure_monitor_color_devices (manager);
+  ensure_monitor_backlights (manager);
 
   update_has_external_monitor (manager);
   update_backlight (manager, TRUE);
@@ -1802,24 +1814,55 @@ meta_monitor_manager_maybe_emit_privacy_screen_change (MetaMonitorManager *manag
 }
 
 static int
-normalize_backlight (MetaOutput *output,
-                     int         value)
+normalize_brightness (MetaBacklight *backlight,
+                      int            value)
 {
-  const MetaOutputInfo *output_info = meta_output_get_info (output);
+  int brightness_min, brightness_max;
 
-  return (int) round ((double) (value - output_info->backlight_min) /
-                      (output_info->backlight_max - output_info->backlight_min) * 100.0);
+  meta_backlight_get_brightness_info (backlight,
+                                      &brightness_min, &brightness_max);
+
+  return (int) round ((double) (value - brightness_min) /
+                      (brightness_max - brightness_min) * 100.0);
 }
 
 static int
-denormalize_backlight (MetaOutput *output,
-                       int         normalized_value)
+denormalize_brightness (MetaBacklight *backlight,
+                        int            normalized_value)
 {
-  const MetaOutputInfo *output_info = meta_output_get_info (output);
+  int brightness_min, brightness_max;
+
+  meta_backlight_get_brightness_info (backlight,
+                                      &brightness_min, &brightness_max);
 
   return (int) round (((double) normalized_value / 100.0 *
-                      (output_info->backlight_max - output_info->backlight_min)) +
-                      output_info->backlight_min);
+                       (brightness_max - brightness_min)) + brightness_min);
+}
+
+static int
+get_min_brightness_step (MetaBacklight *backlight)
+{
+  int brightness_min, brightness_max;
+
+  meta_backlight_get_brightness_info (backlight,
+                                      &brightness_min, &brightness_max);
+
+  if (brightness_max - brightness_min != 0)
+    return 100 / (brightness_max - brightness_min);
+
+  return -1;
+}
+
+static MetaBacklight *
+get_backlight_from_output (MetaOutput *output)
+{
+  MetaMonitor *monitor;
+
+  monitor = meta_output_get_monitor (output);
+  if (!monitor)
+    return NULL;
+
+  return meta_monitor_get_backlight (monitor);
 }
 
 static gboolean
@@ -1900,9 +1943,7 @@ meta_monitor_manager_handle_get_resources (MetaDBusDisplayConfig *skeleton,
       GBytes *edid;
       MetaCrtc *crtc;
       int crtc_index;
-      int backlight;
-      int normalized_backlight;
-      int min_backlight_step;
+      MetaBacklight *backlight;
       gboolean is_primary;
       gboolean is_presentation;
       const char * connector_type_name;
@@ -1942,12 +1983,6 @@ meta_monitor_manager_handle_get_resources (MetaDBusDisplayConfig *skeleton,
           g_variant_builder_add (&clones, "u", possible_clone_index);
         }
 
-      backlight = meta_output_get_backlight (output);
-      normalized_backlight = normalize_backlight (output, backlight);
-      min_backlight_step =
-        output_info->backlight_max - output_info->backlight_min
-        ? 100 / (output_info->backlight_max - output_info->backlight_min)
-        : -1;
       is_primary = meta_output_is_primary (output);
       is_presentation = meta_output_is_presentation (output);
       is_underscanning = meta_output_is_underscanning (output);
@@ -1972,10 +2007,6 @@ meta_monitor_manager_handle_get_resources (MetaDBusDisplayConfig *skeleton,
                              g_variant_new_int32 (output_info->height_mm));
       g_variant_builder_add (&properties, "{sv}", "display-name",
                              g_variant_new_string (output_info->name));
-      g_variant_builder_add (&properties, "{sv}", "backlight",
-                             g_variant_new_int32 (normalized_backlight));
-      g_variant_builder_add (&properties, "{sv}", "min-backlight-step",
-                             g_variant_new_int32 (min_backlight_step));
       g_variant_builder_add (&properties, "{sv}", "primary",
                              g_variant_new_boolean (is_primary));
       g_variant_builder_add (&properties, "{sv}", "presentation",
@@ -1988,6 +2019,22 @@ meta_monitor_manager_handle_get_resources (MetaDBusDisplayConfig *skeleton,
                              g_variant_new_boolean (supports_underscanning));
       g_variant_builder_add (&properties, "{sv}", "supports-color-transform",
                              g_variant_new_boolean (supports_color_transform));
+
+
+      backlight = get_backlight_from_output (output);
+      if (backlight)
+        {
+          int brightness, normalized_brightness, min_brightness_step;
+
+          brightness = meta_backlight_get_brightness (backlight);
+          normalized_brightness = normalize_brightness (backlight, brightness);
+          min_brightness_step = get_min_brightness_step (backlight);
+
+          g_variant_builder_add (&properties, "{sv}", "backlight",
+                                 g_variant_new_int32 (normalized_brightness));
+          g_variant_builder_add (&properties, "{sv}", "min-backlight-step",
+                                 g_variant_new_int32 (min_brightness_step));
+        }
 
       edid = manager_class->read_edid (manager, output);
       if (edid)
@@ -3176,7 +3223,7 @@ meta_monitor_manager_handle_change_backlight  (MetaDBusDisplayConfig *skeleton,
 {
   GList *combined_outputs;
   MetaOutput *output;
-  const MetaOutputInfo *output_info;
+  MetaBacklight *backlight;
   int value;
   int renormalized_value;
 
@@ -3209,10 +3256,8 @@ meta_monitor_manager_handle_change_backlight  (MetaDBusDisplayConfig *skeleton,
       return G_DBUS_METHOD_INVOCATION_HANDLED;
     }
 
-  output_info = meta_output_get_info (output);
-  if (meta_output_get_backlight (output) == -1 ||
-      (output_info->backlight_min == 0 &&
-       output_info->backlight_max == 0))
+  backlight = get_backlight_from_output (output);
+  if (!backlight)
     {
       g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR,
                                              G_DBUS_ERROR_INVALID_ARGS,
@@ -3220,9 +3265,9 @@ meta_monitor_manager_handle_change_backlight  (MetaDBusDisplayConfig *skeleton,
       return G_DBUS_METHOD_INVOCATION_HANDLED;
     }
 
-  value = denormalize_backlight (output, normalized_value);
-  meta_output_set_backlight (output, value);
-  renormalized_value = normalize_backlight (output, value);
+  value = denormalize_brightness (backlight, normalized_value);
+  meta_backlight_set_brightness (backlight, value);
+  renormalized_value = normalize_brightness (backlight, value);
 
   G_GNUC_BEGIN_IGNORE_DEPRECATIONS
   meta_dbus_display_config_complete_change_backlight (skeleton,
@@ -3247,6 +3292,7 @@ meta_monitor_manager_handle_set_backlight (MetaDBusDisplayConfig *skeleton,
   MetaMonitorManagerPrivate *priv =
     meta_monitor_manager_get_instance_private (monitor_manager);
   MetaMonitor *monitor;
+  MetaBacklight *backlight;
   int backlight_min;
   int backlight_max;
 
@@ -3267,16 +3313,16 @@ meta_monitor_manager_handle_set_backlight (MetaDBusDisplayConfig *skeleton,
       return G_DBUS_METHOD_INVOCATION_HANDLED;
     }
 
-  if (!meta_monitor_get_backlight_info (monitor,
-                                        &backlight_min,
-                                        &backlight_max))
+  backlight = meta_monitor_get_backlight (monitor);
+  if (!backlight)
     {
       g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR,
                                              G_DBUS_ERROR_INVALID_ARGS,
-                                             "Monitor doesn't support changing backlight");
+                                             "Monitor doesn't support changing the backlight");
       return G_DBUS_METHOD_INVOCATION_HANDLED;
     }
 
+  meta_backlight_get_brightness_info (backlight, &backlight_min, &backlight_max);
   if (value < backlight_min || value > backlight_max)
     {
       g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR,
@@ -3285,7 +3331,7 @@ meta_monitor_manager_handle_set_backlight (MetaDBusDisplayConfig *skeleton,
       return G_DBUS_METHOD_INVOCATION_HANDLED;
     }
 
-  meta_monitor_set_backlight (monitor, value);
+  meta_backlight_set_brightness (backlight, value);
 
   meta_dbus_display_config_complete_set_backlight (skeleton, invocation);
 
@@ -4107,7 +4153,7 @@ update_backlight (MetaMonitorManager *manager,
     meta_monitor_manager_get_instance_private (manager);
   GList *l;
   GVariantBuilder backlight_builder;
-  g_autoptr (GVariant) backlight = NULL;
+  g_autoptr (GVariant) backlight_variant = NULL;
 
   if (bump_serial)
     priv->backlight_serial++;
@@ -4120,45 +4166,44 @@ update_backlight (MetaMonitorManager *manager,
   for (l = manager->monitors; l; l = l->next)
     {
       MetaMonitor *monitor = META_MONITOR (l->data);
+      MetaBacklight *backlight;
       const char *connector;
       gboolean active;
+      int min, max;
       int value;
 
-      if (!meta_monitor_is_builtin (monitor))
+      backlight = meta_monitor_get_backlight (monitor);
+      if (!backlight)
         continue;
+
+      connector = meta_monitor_get_connector (monitor);
+      active = meta_monitor_is_active (monitor);
+      meta_backlight_get_brightness_info (backlight, &min, &max);
+      value = meta_backlight_get_brightness (backlight);
 
       g_variant_builder_open (&backlight_builder,
                               G_VARIANT_TYPE_VARDICT);
 
-      connector = meta_monitor_get_connector (monitor);
-      active = meta_monitor_is_active (monitor);
       g_variant_builder_add (&backlight_builder, "{sv}",
                              "connector", g_variant_new_string (connector));
       g_variant_builder_add (&backlight_builder, "{sv}",
                              "active", g_variant_new_boolean (active));
-
-      if (meta_monitor_get_backlight (monitor, &value))
-        {
-          int min, max;
-
-          meta_monitor_get_backlight_info (monitor, &min, &max);
-          g_variant_builder_add (&backlight_builder, "{sv}",
-                                 "min", g_variant_new_int32 (min));
-          g_variant_builder_add (&backlight_builder, "{sv}",
-                                 "max", g_variant_new_int32 (max));
-          g_variant_builder_add (&backlight_builder, "{sv}",
-                                 "value", g_variant_new_int32 (value));
-        }
+      g_variant_builder_add (&backlight_builder, "{sv}",
+                             "min", g_variant_new_int32 (min));
+      g_variant_builder_add (&backlight_builder, "{sv}",
+                             "max", g_variant_new_int32 (max));
+      g_variant_builder_add (&backlight_builder, "{sv}",
+                             "value", g_variant_new_int32 (value));
 
       g_variant_builder_close (&backlight_builder);
     }
 
   g_variant_builder_close (&backlight_builder);
 
-  backlight = g_variant_builder_end (&backlight_builder);
+  backlight_variant = g_variant_builder_end (&backlight_builder);
 
   meta_dbus_display_config_set_backlight (manager->display_config,
-                                          g_steal_pointer (&backlight));
+                                          g_steal_pointer (&backlight_variant));
 }
 
 static void
@@ -4275,6 +4320,8 @@ meta_monitor_manager_rebuild (MetaMonitorManager *manager,
 
   meta_monitor_manager_update_monitor_modes (manager, config);
 
+  ensure_privacy_screen_settings (manager);
+
   if (manager->in_init)
     return;
 
@@ -4282,8 +4329,6 @@ meta_monitor_manager_rebuild (MetaMonitorManager *manager,
 
   meta_monitor_manager_update_logical_state (manager, config);
   meta_monitor_manager_update_for_lease_state (manager, config);
-
-  ensure_privacy_screen_settings (manager);
 
   meta_monitor_manager_notify_monitors_changed (manager);
 

src/backends/meta-monitor-private.h
@@ -320,17 +320,7 @@ gboolean meta_parse_monitor_mode (const char *string,
                                   float      *out_refresh_rate,
                                   float       fallback_refresh_rate);
 
-META_EXPORT_TEST
-gboolean meta_monitor_get_backlight_info (MetaMonitor *monitor,
-                                          int         *backlight_min,
-                                          int         *backlight_max);
-
-void meta_monitor_set_backlight (MetaMonitor *monitor,
-                                 int          value);
-
-META_EXPORT_TEST
-gboolean meta_monitor_get_backlight (MetaMonitor *monitor,
-                                     int         *value);
+void meta_monitor_create_backlight (MetaMonitor *monitor);
 
 void meta_monitor_set_for_lease (MetaMonitor *monitor,
                                  gboolean     for_lease);

src/backends/meta-monitor.c
@@ -27,6 +27,7 @@
 #include "backends/meta-monitor-manager-private.h"
 #include "backends/meta-settings-private.h"
 #include "backends/meta-output.h"
+#include "backends/meta-backlight-private.h"
 #include "core/boxes-private.h"
 
 #define SCALE_FACTORS_PER_INTEGER 4
@@ -81,6 +82,8 @@ typedef struct _MetaMonitorPrivate
   gboolean is_for_lease;
 
   GList *color_modes;
+
+  MetaBacklight *backlight;
 } MetaMonitorPrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE (MetaMonitor, meta_monitor, G_TYPE_OBJECT)
@@ -682,6 +685,8 @@ meta_monitor_dispose (GObject *object)
       priv->outputs = NULL;
     }
 
+  g_clear_object (&priv->backlight);
+
   G_OBJECT_CLASS (meta_monitor_parent_class)->dispose (object);
 }
 
@@ -852,6 +857,40 @@ meta_monitor_set_gamma_lut (MetaMonitor        *monitor,
                                   set_gamma_lut,
                                   (gpointer) lut,
                                   NULL);
+}
+
+void
+meta_monitor_create_backlight (MetaMonitor *monitor)
+{
+  MetaMonitorPrivate *priv = meta_monitor_get_instance_private (monitor);
+  MetaOutput *main_output = meta_monitor_get_main_output (monitor);
+  g_autoptr (MetaBacklight) backlight = NULL;
+  g_autoptr (GError) error = NULL;
+
+  backlight = meta_output_create_backlight (main_output, &error);
+  if (!backlight)
+    {
+      if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED))
+        {
+          meta_topic (META_DEBUG_BACKEND,
+                      "No backlight support for monitor %s",
+                      meta_monitor_get_display_name (monitor));
+        }
+      else
+        {
+          g_warning ("Failed creating backlight for %s: %s",
+                     meta_monitor_get_display_name (monitor),
+                     error->message);
+        }
+    }
+  else
+    {
+      meta_topic (META_DEBUG_BACKEND,
+                  "Created backlight for monitor %s",
+                  meta_monitor_get_display_name (monitor));
+    }
+
+  g_set_object (&priv->backlight, backlight);
 }
 
 static void
@@ -2702,62 +2741,25 @@ out:
   return TRUE;
 }
 
-gboolean
-meta_monitor_get_backlight_info (MetaMonitor *monitor,
-                                 int         *backlight_min,
-                                 int         *backlight_max)
+/**
+ * meta_monitor_get_backlight:
+ * @monitor: A #MetaMonitor object
+ *
+ * Returns the [class@Meta.Backlight] of the monitor, or NULL if it has no
+ * controllable backlight.
+ *
+ * Returns: (transfer none) (nullable): The [class@Meta.Backlight].
+ */
+MetaBacklight *
+meta_monitor_get_backlight (MetaMonitor *monitor)
 {
-  MetaOutput *main_output;
-  int value;
+  MetaMonitorPrivate *priv;
 
-  main_output = meta_monitor_get_main_output (monitor);
-  value = meta_output_get_backlight (main_output);
-  if (value >= 0)
-    {
-      const MetaOutputInfo *output_info = meta_output_get_info (main_output);
-      if (backlight_min)
-        *backlight_min = output_info->backlight_min;
-      if (backlight_max)
-        *backlight_max = output_info->backlight_max;
-      return TRUE;
-    }
-  else
-    {
-      return FALSE;
-    }
-}
+  g_return_val_if_fail (META_IS_MONITOR (monitor), NULL);
 
-void
-meta_monitor_set_backlight (MetaMonitor *monitor,
-                            int          value)
-{
-  MetaMonitorPrivate *priv = meta_monitor_get_instance_private (monitor);
-  GList *l;
+  priv = meta_monitor_get_instance_private (monitor);
 
-  for (l = priv->outputs; l; l = l->next)
-    {
-      MetaOutput *output = l->data;
-
-      meta_output_set_backlight (output, value);
-    }
-}
-
-gboolean
-meta_monitor_get_backlight (MetaMonitor *monitor,
-                            int         *value)
-{
-  if (meta_monitor_get_backlight_info (monitor, NULL, NULL))
-    {
-      MetaOutput *main_output;
-
-      main_output = meta_monitor_get_main_output (monitor);
-      *value = meta_output_get_backlight (main_output);
-      return TRUE;
-    }
-  else
-    {
-      return FALSE;
-    }
+  return priv->backlight;
 }
 
 void

src/backends/meta-output.c
@@ -37,15 +37,6 @@ enum
 
 static GParamSpec *obj_props[N_PROPS];
 
-enum
-{
-  BACKLIGHT_CHANGED,
-
-  N_SIGNALS
-};
-
-static guint signals[N_SIGNALS];
-
 typedef struct _MetaOutputPrivate
 {
   uint64_t id;
@@ -66,8 +57,6 @@ typedef struct _MetaOutputPrivate
 
   gboolean has_max_bpc;
   unsigned int max_bpc;
-
-  int backlight;
 
   MetaPrivacyScreenState privacy_screen_state;
   gboolean is_privacy_screen_enabled;
@@ -209,26 +198,20 @@ meta_output_get_max_bpc (MetaOutput   *output,
   return priv->has_max_bpc;
 }
 
-void
-meta_output_set_backlight (MetaOutput *output,
-                           int         backlight)
+MetaBacklight *
+meta_output_create_backlight (MetaOutput  *output,
+                              GError     **error)
 {
-  MetaOutputPrivate *priv = meta_output_get_instance_private (output);
+  MetaOutputClass *output_class = META_OUTPUT_GET_CLASS (output);
 
-  g_return_if_fail (backlight >= priv->info->backlight_min);
-  g_return_if_fail (backlight <= priv->info->backlight_max);
+  if (!output_class->create_backlight)
+    {
+      g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
+                           "Output does not support creating a backlight");
+      return NULL;
+    }
 
-  priv->backlight = backlight;
-
-  g_signal_emit (output, signals[BACKLIGHT_CHANGED], 0);
-}
-
-int
-meta_output_get_backlight (MetaOutput *output)
-{
-  MetaOutputPrivate *priv = meta_output_get_instance_private (output);
-
-  return priv->backlight;
+  return output_class->create_backlight (output, error);
 }
 
 void
@@ -619,7 +602,6 @@ meta_output_init (MetaOutput *output)
 {
   MetaOutputPrivate *priv = meta_output_get_instance_private (output);
 
-  priv->backlight = -1;
   priv->is_primary = FALSE;
   priv->is_presentation = FALSE;
   priv->is_underscanning = FALSE;
@@ -662,14 +644,6 @@ meta_output_class_init (MetaOutputClass *klass)
                           G_PARAM_READWRITE |
                           G_PARAM_STATIC_STRINGS);
   g_object_class_install_properties (object_class, N_PROPS, obj_props);
-
-  signals[BACKLIGHT_CHANGED] =
-    g_signal_new ("backlight-changed",
-                  G_TYPE_FROM_CLASS (klass),
-                  G_SIGNAL_RUN_LAST,
-                  0,
-                  NULL, NULL, NULL,
-                  G_TYPE_NONE, 0);
 }
 
 gboolean

src/backends/meta-output.h
@@ -22,6 +22,7 @@
 
 #include "backends/edid.h"
 #include "backends/meta-backend-types.h"
+#include "backends/meta-backlight-private.h"
 #include "backends/meta-connector.h"
 #include "backends/meta-gpu.h"
 #include "core/util-private.h"
@@ -134,9 +135,6 @@ typedef struct _MetaOutputInfo
   MetaOutput **possible_clones;
   unsigned int n_possible_clones;
 
-  int backlight_min;
-  int backlight_max;
-
   gboolean supports_underscanning;
   gboolean supports_color_transform;
   gboolean supports_privacy_screen;
@@ -204,6 +202,9 @@ struct _MetaOutputClass
   GObjectClass parent_class;
 
   MetaPrivacyScreenState (* get_privacy_screen_state) (MetaOutput *output);
+
+  MetaBacklight * (* create_backlight) (MetaOutput  *output,
+                                        GError     **error);
 };
 
 META_EXPORT_TEST
@@ -236,11 +237,8 @@ gboolean meta_output_get_max_bpc (MetaOutput   *output,
                                   unsigned int *max_bpc);
 
 META_EXPORT_TEST
-void meta_output_set_backlight (MetaOutput *output,
-                                int         backlight);
-
-META_EXPORT_TEST
-int meta_output_get_backlight (MetaOutput *output);
+MetaBacklight * meta_output_create_backlight (MetaOutput  *output,
+                                              GError     **error);
 
 MetaPrivacyScreenState meta_output_get_privacy_screen_state (MetaOutput *output);
 

src/backends/native/meta-output-kms.c
@@ -26,15 +26,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "backends/meta-backlight-sysfs-private.h"
+#include "backends/meta-backlight-ref-white-private.h"
+#include "backends/meta-color-device.h"
+#include "backends/meta-color-manager.h"
 #include "backends/meta-crtc.h"
+#include "backends/meta-monitor-private.h"
+#include "backends/native/meta-crtc-kms.h"
+#include "backends/native/meta-crtc-mode-kms.h"
 #include "backends/native/meta-kms.h"
 #include "backends/native/meta-kms-connector.h"
 #include "backends/native/meta-kms-device.h"
 #include "backends/native/meta-kms-mode.h"
 #include "backends/native/meta-kms-update.h"
 #include "backends/native/meta-kms-utils.h"
-#include "backends/native/meta-crtc-kms.h"
-#include "backends/native/meta-crtc-mode-kms.h"
 
 #define SYNC_TOLERANCE_HZ 0.001f
 
@@ -371,6 +376,63 @@ meta_kms_connector_type_from_drm (uint32_t drm_connector_type)
   return (MetaConnectorType) drm_connector_type;
 }
 
+static MetaBacklight *
+meta_output_kms_create_backlight (MetaOutput  *output,
+                                  GError     **error)
+{
+  MetaMonitor *monitor = meta_output_get_monitor (output);
+  MetaBackend *backend = meta_monitor_get_backend (monitor);
+  MetaColorManager *color_manager = meta_backend_get_color_manager (backend);
+  MetaColorDevice *color_device =
+    meta_color_manager_get_color_device (color_manager, monitor);
+  const MetaOutputInfo *output_info = meta_output_get_info (output);
+  MetaColorMode color_mode = meta_output_get_color_mode (output);
+  MetaBacklight *old_backlight = meta_monitor_get_backlight (monitor);
+  float orig_ref_white;
+  g_autoptr (MetaBacklightSysfs) backlight_sysfs = NULL;
+  g_autoptr (GError) local_error = NULL;
+
+  if (META_IS_BACKLIGHT_REF_WHITE (old_backlight))
+    {
+      orig_ref_white = meta_backlight_ref_white_get_original_ref_white (
+        META_BACKLIGHT_REF_WHITE (old_backlight));
+    }
+  else
+    {
+      orig_ref_white =
+        meta_color_device_get_reference_luminance_factor (color_device);
+    }
+
+  backlight_sysfs = meta_backlight_sysfs_new (backend,
+                                              output_info,
+                                              &local_error);
+  if (!backlight_sysfs &&
+      g_error_matches (local_error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED) &&
+      color_mode == META_COLOR_MODE_BT2100)
+    {
+
+      meta_topic (META_DEBUG_BACKEND,
+                  "Creating reference-white software backlight control for %s, "
+                  "because sysfs based backlight is not supported and HDR is active.",
+                  output_info->name);
+
+      return META_BACKLIGHT (meta_backlight_ref_white_new (backend,
+                                                           monitor,
+                                                           orig_ref_white));
+    }
+
+  meta_color_device_set_reference_luminance_factor (color_device,
+                                                    orig_ref_white);
+
+  if (!backlight_sysfs)
+    {
+      g_propagate_error (error, g_steal_pointer (&local_error));
+      return NULL;
+    }
+
+  return META_BACKLIGHT (g_steal_pointer (&backlight_sysfs));
+}
+
 MetaOutputKms *
 meta_output_kms_new (MetaGpuKms        *gpu_kms,
                      MetaKmsConnector  *kms_connector,
@@ -578,6 +640,7 @@ meta_output_kms_class_init (MetaOutputKmsClass *klass)
 
   output_class->get_privacy_screen_state =
     meta_output_kms_get_privacy_screen_state;
+  output_class->create_backlight = meta_output_kms_create_backlight;
 
   output_native_class->read_edid = meta_output_kms_read_edid;
 

commit a9ad3a1a4359947acbbaa744cbc3f34705f226cb

monitor: Fix build with the C header
The "meta-monitor.h" header definition now uses the MetaBacklight but
does not actually include the corresponding header.

As a result, a C program using "meta/meta-monitor.h" will fail to build
without explicitly including "meta/meta-backlight.h" first. Not ideal.

Fix the issue by adding the required opaque definition of MetaBacklight
to the generic "meta/types.h" and use that from "meta/meta-monitor.h" so
that a code which includes only "meta/meta-monitor.h" can build.

Fixes: 8e81bf686 ("backend: Change to the new backlight interface")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4861>

src/meta/meta-monitor.h
@@ -19,6 +19,8 @@
 
 #include <glib-object.h>
 
+#include "meta/types.h"
+
 #define META_TYPE_MONITOR (meta_monitor_get_type ())
 META_EXPORT
 G_DECLARE_DERIVABLE_TYPE (MetaMonitor, meta_monitor, META, MONITOR, GObject)

src/meta/types.h
@@ -38,3 +38,4 @@ typedef struct _MetaSelection MetaSelection;
 typedef struct _MetaDebugControl MetaDebugControl;
 typedef struct _MetaWindowConfig MetaWindowConfig;
 typedef struct _MetaExternalConstraint MetaExternalConstraint;
+typedef struct _MetaBacklight MetaBacklight;