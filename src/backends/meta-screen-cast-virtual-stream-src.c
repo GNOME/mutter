GNOME/mutter

commit 1769af1922ae971efcdc3aad80d6176925b9e690

screen-cast/stream-src: Add layout property
It describes the position and size of the stream in the logical
coordinate system.

v2:
* When the property value changes, add the new layout to the accumulated
  redraw clip, to ensure the next recorded frame will have full damage.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4793>

src/backends/meta-screen-cast-virtual-stream-src.c
@@ -59,6 +59,7 @@ struct _MetaScreenCastVirtualStreamSrc
 
   MetaStageWatch *paint_watch;
   MetaStageWatch *skipped_watch;
+  GBinding *layout_binding;
 
   gulong position_invalidated_handler_id;
   gulong cursor_changed_handler_id;
@@ -256,6 +257,10 @@ setup_view (MetaScreenCastVirtualStreamSrc *virtual_src,
                            on_skipped_paint,
                            virtual_src);
 
+  virtual_src->layout_binding = g_object_bind_property (view, "layout",
+                                                        src, "layout",
+                                                        G_BINDING_SYNC_CREATE);
+
   if (meta_screen_cast_stream_src_is_enabled (src) &&
       !meta_screen_cast_stream_src_is_driving (src))
     make_frame_clock_passive (virtual_src, view);
@@ -365,6 +370,8 @@ meta_screen_cast_virtual_stream_src_disable (MetaScreenCastStreamSrc *src)
       meta_stage_remove_watch (META_STAGE (stage), virtual_src->skipped_watch);
       virtual_src->skipped_watch = NULL;
     }
+
+  g_clear_object (&virtual_src->layout_binding);
 
   g_clear_signal_handler (&virtual_src->position_invalidated_handler_id,
                           cursor_tracker);

src/backends/meta-screen-cast-area-stream-src.c
@@ -712,8 +712,11 @@ MetaScreenCastAreaStreamSrc *
 meta_screen_cast_area_stream_src_new (MetaScreenCastAreaStream  *area_stream,
                                       GError                   **error)
 {
+  MtkRectangle *area = meta_screen_cast_area_stream_get_area (area_stream);
+
   return g_initable_new (META_TYPE_SCREEN_CAST_AREA_STREAM_SRC, NULL, error,
                          "stream", area_stream,
+                         "layout", area,
                          NULL);
 }
 

src/backends/meta-screen-cast-monitor-stream-src.c
@@ -449,6 +449,17 @@ static void
 on_monitors_changed (MetaMonitorManager             *monitor_manager,
                      MetaScreenCastMonitorStreamSrc *monitor_src)
 {
+  MetaScreenCastStreamSrc *src = META_SCREEN_CAST_STREAM_SRC (monitor_src);
+  MetaScreenCastStream *stream = meta_screen_cast_stream_src_get_stream (src);
+  MetaScreenCastMonitorStream *monitor_stream =
+    META_SCREEN_CAST_MONITOR_STREAM (stream);
+  MetaMonitor *monitor =
+    meta_screen_cast_monitor_stream_get_monitor (monitor_stream);
+  MetaLogicalMonitor *logical_monitor =
+    meta_monitor_get_logical_monitor (monitor);
+  MtkRectangle layout = meta_logical_monitor_get_layout (logical_monitor);
+
+  g_object_set (G_OBJECT (src), "layout", &layout, NULL);
   reattach_watches (monitor_src);
 }
 
@@ -909,8 +920,15 @@ MetaScreenCastMonitorStreamSrc *
 meta_screen_cast_monitor_stream_src_new (MetaScreenCastMonitorStream  *monitor_stream,
                                          GError                      **error)
 {
+  MetaMonitor *monitor =
+    meta_screen_cast_monitor_stream_get_monitor (monitor_stream);
+  MetaLogicalMonitor *logical_monitor =
+    meta_monitor_get_logical_monitor (monitor);
+  MtkRectangle layout = meta_logical_monitor_get_layout (logical_monitor);
+
   return g_initable_new (META_TYPE_SCREEN_CAST_MONITOR_STREAM_SRC, NULL, error,
                          "stream", monitor_stream,
+                         "layout", &layout,
                          NULL);
 }
 

src/backends/meta-screen-cast-stream-src.c
@@ -80,6 +80,7 @@ enum
 
   PROP_STREAM,
   PROP_MUST_DRIVE,
+  PROP_LAYOUT,
 
   N_PROPS
 };
@@ -146,6 +147,7 @@ typedef struct _MetaScreenCastStreamSrcPrivate
    */
   GList *dequeued_buffers;
 
+  MtkRectangle layout;
   MtkRegion *redraw_clip;
 
   GHashTable *modifiers;
@@ -2324,6 +2326,7 @@ meta_screen_cast_stream_src_set_property (GObject      *object,
   MetaScreenCastStreamSrc *src = META_SCREEN_CAST_STREAM_SRC (object);
   MetaScreenCastStreamSrcPrivate *priv =
     meta_screen_cast_stream_src_get_instance_private (src);
+  MtkRectangle *layout;
 
   switch (prop_id)
     {
@@ -2332,6 +2335,17 @@ meta_screen_cast_stream_src_set_property (GObject      *object,
       break;
     case PROP_MUST_DRIVE:
       priv->must_drive = g_value_get_boolean (value);
+      break;
+    case PROP_LAYOUT:
+      layout = g_value_get_boxed (value);
+      if (!mtk_rectangle_equal (&priv->layout, layout))
+        {
+          g_autoptr (MtkRegion) region = NULL;
+
+          priv->layout = *layout;
+          region = mtk_region_create_rectangle (layout);
+          meta_screen_cast_stream_src_accumulate_redraw_clip (src, region);
+        }
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -2406,6 +2420,11 @@ meta_screen_cast_stream_src_class_init (MetaScreenCastStreamSrcClass *klass)
                           G_PARAM_WRITABLE |
                           G_PARAM_CONSTRUCT_ONLY |
                           G_PARAM_STATIC_STRINGS);
+  obj_props[PROP_LAYOUT] =
+    g_param_spec_boxed ("layout", NULL, NULL,
+                        MTK_TYPE_RECTANGLE,
+                        G_PARAM_WRITABLE |
+                        G_PARAM_STATIC_STRINGS);
   g_object_class_install_properties (object_class,
                                      N_PROPS,
                                      obj_props);

commit 24b7ed58da226ac2b258f069abb2a80abb540a70

virtual-stream-src: Don't unref object without ownership
g_object_bind_property() is transfer-none, meaning we shoudn't clear it,
unless we also ref:ed it. Fix that.

Fixes: 1769af1922 ("screen-cast/stream-src: Add layout property")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4727>

src/backends/meta-screen-cast-virtual-stream-src.c
@@ -257,9 +257,10 @@ setup_view (MetaScreenCastVirtualStreamSrc *virtual_src,
                            on_skipped_paint,
                            virtual_src);
 
-  virtual_src->layout_binding = g_object_bind_property (view, "layout",
-                                                        src, "layout",
-                                                        G_BINDING_SYNC_CREATE);
+  g_set_object (&virtual_src->layout_binding,
+                g_object_bind_property (view, "layout",
+                                        src, "layout",
+                                        G_BINDING_SYNC_CREATE));
 
   if (meta_screen_cast_stream_src_is_enabled (src) &&
       !meta_screen_cast_stream_src_is_driving (src))