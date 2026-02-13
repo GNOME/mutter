GNOME/mutter

commit b10fbda8cb340a612b09b3770301e622d210fde8

backends: Avoid disabling touchscreen outside of touch mode
The reasons to disable touchscreen on dpms off apply mostly to
devices where the touch mode is useful, and not so much outside of
those.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4414>

src/backends/meta-input-mapper.c
@@ -697,6 +697,7 @@ input_mapper_power_save_mode_changed_cb (MetaMonitorManager        *monitor_mana
                                          MetaInputMapper           *mapper)
 {
   ClutterInputDevice *device;
+  ClutterSeat *seat;
   MetaLogicalMonitor *logical_monitor;
   MetaMonitor *builtin;
   MetaPowerSave power_save_mode;
@@ -719,6 +720,10 @@ input_mapper_power_save_mode_changed_cb (MetaMonitorManager        *monitor_mana
                                                   logical_monitor,
                                                   CLUTTER_TOUCHSCREEN_DEVICE);
   if (!device)
+    return;
+
+  seat = clutter_input_device_get_seat (device);
+  if (!clutter_seat_get_touch_mode (seat))
     return;
 
   g_signal_emit (mapper, signals[DEVICE_ENABLED], 0, device, on);

commit c6ec4786b329b3d952f627b4698f599de6297dec

backends/input-mapper: Re-enable touchscreen when exiting from power save mode
Commit b10fbda8 introduced logic to avoid disabling
touchscreens on dpms off, but this caused mutter
to not re-enable this devices when exiting from
power save mode while not in tablet mode.

Fix the issue by checking the "on" status.

Fixes: b10fbda8cb ("backends: Avoid disabling touchscreen outside of
                    touch mode")

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4570
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4899>

src/backends/meta-input-mapper.c
@@ -726,7 +726,7 @@ input_mapper_power_save_mode_changed_cb (MetaMonitorManager        *monitor_mana
     return;
 
   seat = clutter_input_device_get_seat (device);
-  if (!clutter_seat_get_touch_mode (seat))
+  if (!on && !clutter_seat_get_touch_mode (seat))
     return;
 
   g_signal_emit (mapper, signals[DEVICE_ENABLED], 0, device, on);