GNOME/mutter

commit 23f285f972ae06fb8558bd36d3871f7e6129f9ba

clutter/frame-clock: Take previous target presentation time into account
A previously dispatched frame might already target a later refresh
cycle, take that into account when possible.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4821>

clutter/clutter/clutter-frame-clock.c
@@ -873,13 +873,37 @@ calculate_next_update_time_us (ClutterFrameClock *frame_clock,
     case CLUTTER_FRAME_CLOCK_STATE_DISPATCHED_ONE_AND_SCHEDULED:
     case CLUTTER_FRAME_CLOCK_STATE_DISPATCHED_ONE_AND_SCHEDULED_NOW:
     case CLUTTER_FRAME_CLOCK_STATE_DISPATCHED_ONE_AND_SCHEDULED_LATER:
-      next_smooth_presentation_time_us = last_presentation_time_us +
-                                         2 * refresh_interval_us;
+      if (frame_clock->next_presentation->target_presentation_time_us > 0)
+        {
+          next_smooth_presentation_time_us =
+            frame_clock->next_presentation->target_presentation_time_us +
+            refresh_interval_us;
+        }
+      else
+        {
+          next_smooth_presentation_time_us = last_presentation_time_us +
+                                             2 * refresh_interval_us;
+        }
       break;
     case CLUTTER_FRAME_CLOCK_STATE_DISPATCHED_TWO:
       g_warn_if_reached ();  /* quad buffering would be a bug */
-      next_smooth_presentation_time_us = last_presentation_time_us +
-                                         3 * refresh_interval_us;
+      if (frame_clock->next_next_presentation->target_presentation_time_us > 0)
+        {
+          next_smooth_presentation_time_us =
+            frame_clock->next_next_presentation->target_presentation_time_us +
+            refresh_interval_us;
+        }
+      else if (frame_clock->next_presentation->target_presentation_time_us > 0)
+        {
+          next_smooth_presentation_time_us =
+            frame_clock->next_presentation->target_presentation_time_us +
+            2 * refresh_interval_us;
+        }
+      else
+        {
+          next_smooth_presentation_time_us = last_presentation_time_us +
+                                             3 * refresh_interval_us;
+        }
       break;
     }
 

commit e961cd2bb7b6ef4d1afb207ee93f3dcb1485c3c0

clutter/frame-clock: Keep next_presentation_time_us aligned
To the actual refresh cycle.

Because the actual refresh cycle duration varies around the nominal
refresh interval, it could previously gradually drift away from the
real presentation times when constantly updating, which could result
in spuriously missing refresh cycles.

Fixes: 23f285f972ae ("clutter/frame-clock: Take previous target presentation time into account")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4874>

clutter/clutter/clutter-frame-clock.c
@@ -978,8 +978,10 @@ calculate_next_update_time_us (ClutterFrameClock *frame_clock,
    * next presentation = last presentation + N * refresh interval.
    */
   next_presentation_time_us =
-    mtk_extrapolate_next_interval_boundary (next_smooth_presentation_time_us,
-                                            now_us,
+    mtk_extrapolate_next_interval_boundary (last_presentation_time_us,
+                                            MAX (next_smooth_presentation_time_us -
+                                                 refresh_interval_us / 2,
+                                                 now_us),
                                             refresh_interval_us);
 
   if (should_update_now (frame_clock,