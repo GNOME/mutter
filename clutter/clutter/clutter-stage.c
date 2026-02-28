GNOME/mutter

commit 9199a83a0e0582e38e00c2878bdff1cbc8733ef9

clutter/stage: Pass color state when painting to something
In the paint_to_(buffer,framebuffer,content) functions, pass a color
state instead of using the stage color state. This will allow the caller
to control the target color state used for compositing. Passing NULL
will fall back to the same color state as the stage itself.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4928>

clutter/clutter/clutter-stage.c
@@ -2621,12 +2621,12 @@ clutter_stage_paint_to_framebuffer (ClutterStage       *stage,
                                     CoglFramebuffer    *framebuffer,
                                     const MtkRectangle *rect,
                                     float               scale,
+                                    ClutterColorState  *color_state,
                                     ClutterPaintFlag    paint_flags)
 {
   ClutterStagePrivate *priv = clutter_stage_get_instance_private (stage);
   ClutterPaintContext *paint_context;
   g_autoptr (MtkRegion) redraw_clip = NULL;
-  ClutterColorState *color_state;
 
   COGL_TRACE_BEGIN_SCOPED (PaintToFramebuffer,
                            "Clutter::Stage::paint_to_framebuffer()");
@@ -2640,8 +2640,10 @@ clutter_stage_paint_to_framebuffer (ClutterStage       *stage,
     }
 
   redraw_clip = mtk_region_create_rectangle (rect);
-  color_state =
-    clutter_actor_get_color_state (CLUTTER_ACTOR (stage));
+
+  if (!color_state)
+    color_state = clutter_actor_get_color_state (CLUTTER_ACTOR (stage));
+
   paint_context =
     clutter_paint_context_new_for_framebuffer (framebuffer,
                                                redraw_clip,
@@ -2685,6 +2687,7 @@ clutter_stage_paint_to_buffer (ClutterStage        *stage,
                                uint8_t             *data,
                                int                  stride,
                                CoglPixelFormat      format,
+                               ClutterColorState   *color_state,
                                ClutterPaintFlag     paint_flags,
                                GError             **error)
 {
@@ -2721,7 +2724,9 @@ clutter_stage_paint_to_buffer (ClutterStage        *stage,
     return FALSE;
 
   clutter_stage_paint_to_framebuffer (stage, framebuffer,
-                                      rect, scale, paint_flags);
+                                      rect, scale,
+                                      color_state,
+                                      paint_flags);
 
   bitmap = cogl_bitmap_new_for_data (cogl_context,
                                      texture_width, texture_height,
@@ -2756,6 +2761,7 @@ ClutterContent *
 clutter_stage_paint_to_content (ClutterStage        *stage,
                                 const MtkRectangle  *rect,
                                 float                scale,
+                                ClutterColorState   *color_state,
                                 ClutterPaintFlag     paint_flags,
                                 GError             **error)
 {
@@ -2791,7 +2797,9 @@ clutter_stage_paint_to_content (ClutterStage        *stage,
     return NULL;
 
   clutter_stage_paint_to_framebuffer (stage, framebuffer,
-                                      rect, scale, paint_flags);
+                                      rect, scale,
+                                      color_state,
+                                      paint_flags);
 
   return clutter_texture_content_new_from_texture (cogl_offscreen_get_texture (offscreen),
                                                    NULL);

clutter/clutter/clutter-stage.h
@@ -182,6 +182,7 @@ void clutter_stage_paint_to_framebuffer (ClutterStage       *stage,
                                          CoglFramebuffer    *framebuffer,
                                          const MtkRectangle *rect,
                                          float               scale,
+                                         ClutterColorState  *color_state,
                                          ClutterPaintFlag    paint_flags);
 
 CLUTTER_EXPORT
@@ -191,6 +192,7 @@ gboolean clutter_stage_paint_to_buffer (ClutterStage        *stage,
                                         uint8_t             *data,
                                         int                  stride,
                                         CoglPixelFormat      format,
+                                        ClutterColorState   *color_state,
                                         ClutterPaintFlag     paint_flags,
                                         GError             **error);
 
@@ -198,6 +200,7 @@ CLUTTER_EXPORT
 ClutterContent * clutter_stage_paint_to_content (ClutterStage        *stage,
                                                  const MtkRectangle  *rect,
                                                  float                scale,
+                                                 ClutterColorState   *color_state,
                                                  ClutterPaintFlag     paint_flags,
                                                  GError             **error);
 

src/backends/meta-screen-cast-area-stream-src.c
@@ -487,6 +487,7 @@ meta_screen_cast_area_stream_src_record_to_buffer (MetaScreenCastStreamSrc   *sr
                                       data,
                                       stride,
                                       COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
+                                      NULL,
                                       paint_flags,
                                       error))
     return FALSE;
@@ -526,6 +527,7 @@ meta_screen_cast_area_stream_src_record_to_framebuffer (MetaScreenCastStreamSrc 
     }
   clutter_stage_paint_to_framebuffer (stage, framebuffer,
                                       area, scale,
+                                      NULL,
                                       paint_flags);
 
   return TRUE;

src/backends/meta-screen-cast-monitor-stream-src.c
@@ -594,6 +594,7 @@ meta_screen_cast_monitor_stream_src_record_to_buffer (MetaScreenCastStreamSrc   
                                       data,
                                       stride,
                                       COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
+                                      NULL,
                                       paint_flags,
                                       error))
     return FALSE;
@@ -732,6 +733,7 @@ stage_paint:
                                           framebuffer,
                                           &logical_monitor_layout,
                                           view_scale,
+                                          NULL,
                                           paint_flags);
     }
 

src/backends/meta-screen-cast-virtual-stream-src.c
@@ -450,6 +450,7 @@ meta_screen_cast_virtual_stream_src_record_to_buffer (MetaScreenCastStreamSrc   
                                       data,
                                       stride,
                                       COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
+                                      NULL,
                                       paint_flags,
                                       error))
     return FALSE;

commit 8088f43e94273015722443e29a7311729ed5b415

clutter/stage: Add missing color state annotation
Otherwise it will not be possible to pass null from JS code.

Fixes: 9199a83a0e ("clutter/stage: Pass color state when painting to something")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4935>

clutter/clutter/clutter-stage.c
@@ -2673,6 +2673,7 @@ clutter_stage_paint_to_framebuffer (ClutterStage       *stage,
  * @data: (array) (element-type guint8): a pointer to the data
  * @stride: stride of the image surface
  * @format: the pixel format
+ * @color_state: (nullable): the #ClutterColorState
  * @paint_flags: the #ClutterPaintFlag
  * @error: the error
  *
@@ -2750,6 +2751,7 @@ clutter_stage_paint_to_buffer (ClutterStage        *stage,
  * @stage: a #ClutterStage actor
  * @rect: a rectangle
  * @scale: the scale
+ * @color_state: (nullable): the #ClutterColorState
  * @paint_flags: the #ClutterPaintFlag
  * @error: the error
  *