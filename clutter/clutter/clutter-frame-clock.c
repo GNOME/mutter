GNOME/mutter

commit c25ad31af18d063f80335994439b9c3af7ac1a93

clutter/frame-clock: Track target presentation time explicitly
Separately from the expected presentation time.

Always set target presentation time if any time constraints have been
cleared for the expected presentation time. Otherwise do not set it when
starting the next dispatch ASAP.

v2:
* Set target presentation time in on_before_update if any time
  constraints have been cleared.
v3:
* Rename presentation_time -> expected_presentation_time. (Jonas Ådahl)
* Rename has_target_presentation_time -> is_target_presentation_time.
* Move unrelated change in calculate_next_variable_update_time_us to
  separate commit.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4821>

clutter/clutter/clutter-frame-clock.c
@@ -129,6 +129,7 @@ struct _ClutterFrameClock
   Frame *prev_presentation;
 
   gboolean is_next_presentation_time_valid;
+  gboolean is_target_presentation_time;
   int64_t next_presentation_time_us;
 
   gboolean has_next_frame_deadline;
@@ -801,6 +802,7 @@ static void
 calculate_next_update_time_us (ClutterFrameClock *frame_clock,
                                int64_t           *out_next_update_time_us,
                                int64_t           *out_next_presentation_time_us,
+                               gboolean          *out_is_target_presentation_time,
                                int64_t           *out_next_frame_deadline_us)
 {
   const Frame *last_presentation = frame_clock->prev_presentation;
@@ -898,7 +900,11 @@ calculate_next_update_time_us (ClutterFrameClock *frame_clock,
                          next_presentation_time_us,
                          next_smooth_presentation_time_us))
     {
-      next_update_time_us = now_us;
+      *out_next_update_time_us = now_us;
+      *out_next_presentation_time_us = next_presentation_time_us;
+      if (out_is_target_presentation_time)
+        *out_is_target_presentation_time = FALSE;
+      *out_next_frame_deadline_us = 0;
     }
   else
     {
@@ -908,11 +914,14 @@ calculate_next_update_time_us (ClutterFrameClock *frame_clock,
       next_update_time_us = next_presentation_time_us - max_update_time_estimate_us;
       if (next_update_time_us < now_us)
         next_update_time_us = now_us;
-    }
 
-  *out_next_update_time_us = next_update_time_us;
-  *out_next_presentation_time_us = next_presentation_time_us;
-  *out_next_frame_deadline_us = next_presentation_time_us - frame_clock->vblank_duration_us;
+      *out_next_update_time_us = next_update_time_us;
+      *out_next_presentation_time_us = next_presentation_time_us;
+      if (out_is_target_presentation_time)
+        *out_is_target_presentation_time = TRUE;
+      *out_next_frame_deadline_us =
+        next_presentation_time_us - frame_clock->vblank_duration_us;
+    }
 }
 
 static void
@@ -1144,6 +1153,7 @@ clutter_frame_clock_schedule_update_now (ClutterFrameClock *frame_clock)
     case CLUTTER_FRAME_CLOCK_MODE_FIXED:
       next_update_time_us = g_get_monotonic_time ();
       frame_clock->is_next_presentation_time_valid = FALSE;
+      frame_clock->is_target_presentation_time = FALSE;
       frame_clock->has_next_frame_deadline = FALSE;
       break;
     case CLUTTER_FRAME_CLOCK_MODE_VARIABLE:
@@ -1153,6 +1163,7 @@ clutter_frame_clock_schedule_update_now (ClutterFrameClock *frame_clock)
                                               &frame_clock->next_frame_deadline_us);
       frame_clock->is_next_presentation_time_valid =
         (frame_clock->next_presentation_time_us != 0);
+      frame_clock->is_target_presentation_time = FALSE;
       frame_clock->has_next_frame_deadline =
         (frame_clock->next_frame_deadline_us != 0);
       break;
@@ -1227,6 +1238,7 @@ clutter_frame_clock_schedule_update (ClutterFrameClock *frame_clock)
       calculate_next_update_time_us (frame_clock,
                                      &next_update_time_us,
                                      &frame_clock->next_presentation_time_us,
+                                     &frame_clock->is_target_presentation_time,
                                      &frame_clock->next_frame_deadline_us);
       frame_clock->is_next_presentation_time_valid =
         (frame_clock->next_presentation_time_us != 0);
@@ -1237,6 +1249,7 @@ clutter_frame_clock_schedule_update (ClutterFrameClock *frame_clock)
       calculate_next_variable_update_timeout_us (frame_clock,
                                                  &next_update_time_us);
       frame_clock->is_next_presentation_time_valid = FALSE;
+      frame_clock->is_target_presentation_time = FALSE;
       frame_clock->has_next_frame_deadline = FALSE;
       break;
     case CLUTTER_FRAME_CLOCK_MODE_PASSIVE:
@@ -1312,6 +1325,7 @@ clutter_frame_clock_schedule_update_later (ClutterFrameClock *frame_clock,
       calculate_next_update_time_us (frame_clock,
                                      &next_update_time_us,
                                      &next_presentation_time_us,
+                                     NULL,
                                      &next_frame_deadline_us);
       break;
     case CLUTTER_FRAME_CLOCK_MODE_VARIABLE:
@@ -1563,13 +1577,22 @@ clutter_frame_clock_dispatch (ClutterFrameClock *frame_clock,
     frame = clutter_frame_new (ClutterFrame, NULL);
 
   frame->frame_count = frame_count;
-  frame->has_target_presentation_time = frame_clock->is_next_presentation_time_valid;
-  frame->target_presentation_time_us = frame_clock->next_presentation_time_us;
+  frame->has_expected_presentation_time =
+    frame_clock->is_next_presentation_time_valid;
+  frame->expected_presentation_time_us =
+    frame_clock->next_presentation_time_us;
+  frame->is_target_presentation_time =
+    frame_clock->is_target_presentation_time;
 
-  if (frame->has_target_presentation_time)
-    this_dispatch->target_presentation_time_us = frame->target_presentation_time_us;
+  if (frame->is_target_presentation_time)
+    {
+      this_dispatch->target_presentation_time_us =
+        frame->expected_presentation_time_us;
+    }
   else
-    this_dispatch->target_presentation_time_us = 0;
+    {
+      this_dispatch->target_presentation_time_us = 0;
+    }
 
   frame->has_frame_deadline = frame_clock->has_next_frame_deadline;
   frame->frame_deadline_us = frame_clock->next_frame_deadline_us;
@@ -1900,6 +1923,7 @@ clutter_frame_clock_set_passive (ClutterFrameClock       *frame_clock,
                 frame_clock->output_name);
   frame_clock->mode = CLUTTER_FRAME_CLOCK_MODE_PASSIVE;
   frame_clock->is_next_presentation_time_valid = FALSE;
+  frame_clock->is_target_presentation_time = FALSE;
   frame_clock->has_next_frame_deadline = FALSE;
 
   g_set_object (&frame_clock->driver, driver);

clutter/clutter/clutter-frame-private.h
@@ -28,8 +28,9 @@ struct _ClutterFrame
 
   int64_t frame_count;
 
-  gboolean has_target_presentation_time;
-  int64_t target_presentation_time_us;
+  gboolean has_expected_presentation_time;
+  gboolean is_target_presentation_time;
+  int64_t expected_presentation_time_us;
 
   gboolean has_frame_deadline;
   int64_t frame_deadline_us;

clutter/clutter/clutter-frame.c
@@ -63,12 +63,12 @@ clutter_frame_get_count (ClutterFrame *frame)
 }
 
 gboolean
-clutter_frame_get_target_presentation_time (ClutterFrame *frame,
-                                            int64_t      *target_presentation_time_us)
+clutter_frame_get_expected_presentation_time (ClutterFrame *frame,
+                                              int64_t      *expected_presentation_time_us)
 {
-  if (frame->has_target_presentation_time)
+  if (frame->has_expected_presentation_time)
     {
-      *target_presentation_time_us = frame->target_presentation_time_us;
+      *expected_presentation_time_us = frame->expected_presentation_time_us;
       return TRUE;
     }
   else

clutter/clutter/clutter-frame.h
@@ -40,8 +40,8 @@ CLUTTER_EXPORT
 int64_t clutter_frame_get_count (ClutterFrame *frame);
 
 CLUTTER_EXPORT
-gboolean clutter_frame_get_target_presentation_time (ClutterFrame *frame,
-                                                     int64_t      *target_presentation_time_us);
+gboolean clutter_frame_get_expected_presentation_time (ClutterFrame *frame,
+                                                       int64_t      *expected_presentation_time_us);
 
 CLUTTER_EXPORT
 gboolean clutter_frame_get_frame_deadline (ClutterFrame *frame,

src/backends/meta-screen-cast-monitor-stream-src.c
@@ -176,7 +176,8 @@ stage_painted (MetaStage        *stage,
   if (monitor_src->maybe_record_idle_id)
     return;
 
-  if (!clutter_frame_get_target_presentation_time (frame, &presentation_time_us))
+  if (!clutter_frame_get_expected_presentation_time (frame,
+                                                     &presentation_time_us))
     presentation_time_us = g_get_monotonic_time ();
 
   if (meta_screen_cast_stream_src_uses_dma_bufs (src))
@@ -227,7 +228,8 @@ before_stage_painted (MetaStage        *stage,
   if (!clutter_stage_view_peek_scanout (view))
     return;
 
-  if (!clutter_frame_get_target_presentation_time (frame, &presentation_time_us))
+  if (!clutter_frame_get_expected_presentation_time (frame,
+                                                     &presentation_time_us))
     presentation_time_us = g_get_monotonic_time ();
 
   flags = META_SCREEN_CAST_RECORD_FLAG_NONE;

src/tests/clutter/conform/frame-clock-timeline.c
@@ -17,8 +17,8 @@ timeline_frame_clock_frame (ClutterFrameClock *frame_clock,
     .sequence = 0,
   };
 
-  if (!clutter_frame_get_target_presentation_time (frame,
-                                                   &frame_info.presentation_time))
+  if (!clutter_frame_get_expected_presentation_time (frame,
+                                                     &frame_info.presentation_time))
     frame_info.presentation_time = g_get_monotonic_time ();
 
   clutter_frame_clock_notify_presented (frame_clock, &frame_info);

src/tests/clutter/conform/frame-clock.c
@@ -191,7 +191,7 @@ immediate_frame_clock_frame (ClutterFrameClock *frame_clock,
                              gpointer           user_data)
 {
   GMainLoop *main_loop = user_data;
-  int64_t target_presentation_time_us;
+  int64_t presentation_time_us;
   ClutterFrameInfo frame_info;
 
   g_assert_cmpint (clutter_frame_get_count (frame), ==, expected_frame_count);
@@ -206,11 +206,11 @@ immediate_frame_clock_frame (ClutterFrameClock *frame_clock,
 
   test_frame_count--;
 
-  if (!clutter_frame_get_target_presentation_time (frame,
-                                                   &target_presentation_time_us))
-    target_presentation_time_us = g_get_monotonic_time ();
+  if (!clutter_frame_get_expected_presentation_time (frame,
+                                                     &presentation_time_us))
+    presentation_time_us = g_get_monotonic_time ();
 
-  init_frame_info (&frame_info, target_presentation_time_us);
+  init_frame_info (&frame_info, presentation_time_us);
   clutter_frame_clock_notify_presented (frame_clock, &frame_info);
   g_idle_add (schedule_update_idle, frame_clock);
 

src/wayland/meta-wayland.c
@@ -364,20 +364,24 @@ ensure_source_for_stage_view (MetaWaylandCompositor *compositor,
 }
 #endif /* HAVE_NATIVE_BACKEND */
 
-static void
+static gboolean
 clear_time_constraints_for_stage_view_transactions (MetaWaylandCompositor *compositor,
                                                     ClutterStageView      *stage_view,
                                                     int64_t                target_time_us)
 {
   MetaWaylandTransaction *transaction;
+  gboolean cleared = FALSE;
 
   while ((transaction = g_queue_peek_head (compositor->timed_transactions)))
     {
-      if (meta_wayland_transaction_unblock_timed (transaction, target_time_us))
-        g_queue_pop_head (compositor->timed_transactions);
-      else
+      if (!meta_wayland_transaction_unblock_timed (transaction, target_time_us))
         break;
+
+      g_queue_pop_head (compositor->timed_transactions);
+      cleared = TRUE;
     }
+
+  return cleared;
 }
 
 static void
@@ -416,12 +420,16 @@ on_before_update (ClutterStage          *stage,
                   ClutterFrame          *frame,
                   MetaWaylandCompositor *compositor)
 {
-  int64_t target_time_us;
+  int64_t expected_presentation_time_us;
 
-  if (!clutter_frame_get_target_presentation_time (frame, &target_time_us))
-    target_time_us = g_get_monotonic_time ();
+  if (!clutter_frame_get_expected_presentation_time (frame,
+                                                     &expected_presentation_time_us))
+    expected_presentation_time_us = g_get_monotonic_time ();
 
-  clear_time_constraints_for_stage_view_transactions (compositor, stage_view, target_time_us);
+  if (clear_time_constraints_for_stage_view_transactions (compositor,
+                                                          stage_view,
+                                                          expected_presentation_time_us))
+    frame->is_target_presentation_time = TRUE;
 }
 
 static void

commit 031affe5df5e2337d2e1fc22ff0034506cba31d8

clutter/frame-clock: Set non-0 deadline again when starting ASAP
If we set 0 for the deadline, emit_frame_callbacks_for_stage_view is
called even if the frame clock dispatch doesn't result in presenting
any update, which may result in clients using "empty" frame events
running unthrottled.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4664
Fixes: c25ad31af18d ("clutter/frame-clock: Track target presentation time explicitly")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4948>

clutter/clutter/clutter-frame-clock.c
@@ -992,7 +992,6 @@ calculate_next_update_time_us (ClutterFrameClock *frame_clock,
       *out_next_presentation_time_us = next_presentation_time_us;
       if (out_is_target_presentation_time)
         *out_is_target_presentation_time = FALSE;
-      *out_next_frame_deadline_us = 0;
     }
   else
     {
@@ -1020,9 +1019,10 @@ calculate_next_update_time_us (ClutterFrameClock *frame_clock,
       *out_next_presentation_time_us = next_presentation_time_us;
       if (out_is_target_presentation_time)
         *out_is_target_presentation_time = TRUE;
-      *out_next_frame_deadline_us =
-        next_presentation_time_us - frame_clock->vblank_duration_us;
     }
+
+  *out_next_frame_deadline_us =
+    next_presentation_time_us - frame_clock->vblank_duration_us;
 }
 
 static void