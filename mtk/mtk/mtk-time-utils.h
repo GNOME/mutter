095c142f3669b7a83173f08a0dfacd56baaa3b42
Bilal Elmoussaoui <belmouss@redhat.com>
2026-03-12 10:54:32 +0100

    mtk: Remove wrong header comment
    A copy-paste failure.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4172>

--- mtk/mtk/mtk-time-utils.h
+++ mtk/mtk/mtk-time-utils.h
@@ -5,8 +5,6 @@
  *
  * Copyright (C) 2015-2025 Red Hat
  *
- * The implementation is heavily inspired by cairo_region_t.
- *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
