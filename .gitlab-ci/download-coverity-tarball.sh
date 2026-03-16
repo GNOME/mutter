GNOME/mutter

commit 136e3436544146b43fdbb9fde833288ab9027c01

ci: Fix download script for coverity build tarball
Commit 965689f0 changed using a file set in gitlab CI environment
for the coverity token, but missed updating the download script so
the environment variable is used when updating/downloading the
tarball.

Fixes: 965689f0f1 ("ci: Use token file for coverity token")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4969>

.gitlab-ci/download-coverity-tarball.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # We need a coverity token to fetch the tarball
-if [ -x $COVERITY_TOKEN ]
+if [ -x $COVERITY_TOKEN_FILE ]
 then
   echo "No coverity token. Run this job from a protected branch."
   exit -1
@@ -11,7 +11,7 @@ mkdir -p coverity
 
 # Download and check MD5 first
 curl https://scan.coverity.com/download/linux64 \
-  --data "token=$COVERITY_TOKEN&project=mutter&md5=1" \
+  --data "token=$(cat '${COVERITY_TOKEN_FILE}')&project=mutter&md5=1" \
   --output /tmp/coverity_tool.md5
 
 diff /tmp/coverity_tool.md5 coverity/coverity_tool.md5 >/dev/null 2>&1
@@ -24,7 +24,7 @@ fi
 
 # Download and extract coverity tarball
 curl https://scan.coverity.com/download/linux64 \
-  --data "token=$COVERITY_TOKEN&project=mutter" \
+  --data "token=$(cat '${COVERITY_TOKEN_FILE}')&project=mutter" \
   --output /tmp/coverity_tool.tgz
 
 rm -rf ./coverity/cov-analysis*

commit 81e7b529f3d99428daf521dc4b3426d9b7a28ef6

ci: Fix command expansion in string
Confusingly, the rule that ${} only expands in double-quoted strings
also applies even if it happens within a string that is itself
double-quoted. Prefer to use double quotes over no quotes, there's
a low chance the file path might contain spaces, but it's not zero.

Fixes: 136e343654 ("ci: Fix download script for coverity build tarball")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4976>

.gitlab-ci/download-coverity-tarball.sh
@@ -11,7 +11,7 @@ mkdir -p coverity
 
 # Download and check MD5 first
 curl https://scan.coverity.com/download/linux64 \
-  --data "token=$(cat '${COVERITY_TOKEN_FILE}')&project=mutter&md5=1" \
+  --data "token=$(cat "${COVERITY_TOKEN_FILE}")&project=mutter&md5=1" \
   --output /tmp/coverity_tool.md5
 
 diff /tmp/coverity_tool.md5 coverity/coverity_tool.md5 >/dev/null 2>&1
@@ -24,7 +24,7 @@ fi
 
 # Download and extract coverity tarball
 curl https://scan.coverity.com/download/linux64 \
-  --data "token=$(cat '${COVERITY_TOKEN_FILE}')&project=mutter" \
+  --data "token=$(cat "${COVERITY_TOKEN_FILE}")&project=mutter" \
   --output /tmp/coverity_tool.tgz
 
 rm -rf ./coverity/cov-analysis*