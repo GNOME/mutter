GNOME/mutter

commit 5181e5b37070b56b105e8af79d371da75cb2fab6

monitor-manager: Auto clean errors in handle_apply_monitors_config()
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4843>

src/backends/meta-monitor-manager.c
@@ -3122,7 +3122,7 @@ meta_monitor_manager_handle_apply_monitors_config (MetaDBusDisplayConfig *skelet
   GList *logical_monitor_configs = NULL;
   GList *disabled_monitor_specs = NULL;
   GList *for_lease_monitor_specs = NULL;
-  GError *error = NULL;
+  g_autoptr (GError) error = NULL;
 
   if (serial != manager->serial)
     {
@@ -3226,7 +3226,6 @@ meta_monitor_manager_handle_apply_monitors_config (MetaDBusDisplayConfig *skelet
       g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR,
                                              G_DBUS_ERROR_INVALID_ARGS,
                                              "%s", error->message);
-      g_error_free (error);
       g_object_unref (config);
       return G_DBUS_METHOD_INVOCATION_HANDLED;
     }
@@ -3236,7 +3235,6 @@ meta_monitor_manager_handle_apply_monitors_config (MetaDBusDisplayConfig *skelet
       g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR,
                                              G_DBUS_ERROR_INVALID_ARGS,
                                              "%s", error->message);
-      g_error_free (error);
       g_object_unref (config);
       return G_DBUS_METHOD_INVOCATION_HANDLED;
     }
@@ -3255,7 +3253,6 @@ meta_monitor_manager_handle_apply_monitors_config (MetaDBusDisplayConfig *skelet
       g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR,
                                              G_DBUS_ERROR_INVALID_ARGS,
                                              "%s", error->message);
-      g_error_free (error);
       g_object_unref (config);
       return G_DBUS_METHOD_INVOCATION_HANDLED;
     }

commit 24b583db801c7f48f83f4eb7bc695b6534aeb9bf

monitor-manager: Remove left-over g_error_free()
Fixes: 5181e5b370 ("monitor-manager: Auto clean errors in handle_apply_monitors_config()")
CID: #1670339
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4979>

src/backends/meta-monitor-manager.c
@@ -2997,7 +2997,6 @@ meta_monitor_manager_handle_apply_monitors_config (MetaDBusDisplayConfig *skelet
           g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR,
                                                  G_DBUS_ERROR_INVALID_ARGS,
                                                  "%s", error->message);
-          g_error_free (error);
           g_list_free_full (logical_monitor_configs,
                             (GDestroyNotify) meta_logical_monitor_config_free);
           return G_DBUS_METHOD_INVOCATION_HANDLED;