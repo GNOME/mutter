8a7600df448607dc8529e1e562fbe81fb9db43e6
Jonas Ådahl <jadahl@gmail.com>
2026-04-19 22:12:32 +0200

    tests/wayland-client-utils: Allow setting input region
    This means for these surfaces, pointer focus can be gained.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5035>
    (cherry picked from commit 7767523c462effd406fd5be84162d7d8bfefb539)

--- src/tests/wayland-test-clients/wayland-test-client-utils.h
+++ src/tests/wayland-test-clients/wayland-test-client-utils.h
@@ -121,6 +121,7 @@ struct _WaylandSurface
 
   gboolean manual_paint;
   gboolean has_alpha;
+  gboolean set_input_region;
 };
 
 #define WAYLAND_TYPE_SURFACE (wayland_surface_get_type ())
@@ -212,3 +213,5 @@ void wayland_display_set_selection (WaylandDisplay *display,
 
 char * wayland_display_read_selection (WaylandDisplay *display,
                                        const char     *mime_type);
+
+void wayland_surface_set_input_region (WaylandSurface *surface);
