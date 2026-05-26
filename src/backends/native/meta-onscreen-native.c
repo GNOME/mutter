GNOME/mutter

commit bebaf434be5ad137c0832a028d20e1bc4b4852c2

onscreen/native: Move nvidia deadlock prevention to assign_next_frame
This is better for multiple reasons:

meta_onscreen_native_swap_buffers_with_damage can no longer fail to
swap.

If maybe_post_next_frame leaves onscreen_native->next_frame non-NULL
(mostly due to power save mode), this will move it to
onscreen_native->superseded_frame.

This also does the deadlock prevention in
meta_onscreen_native_direct_scanout, which can get called before the
last non-direct-scanout frame was posted. Previously, that frame would
have been moved to onscreen_native->superseded_frame instead,
essentially dropping it on the floor.

maybe_post_next_frame calls render_source_remove_frame when needed, so
assign_next_frame no longer needs to.

Closes: https://gitlab.gnome.org/GNOME/mutter/-/issues/4658

v2:
* Remove superfluous return statement as well. (Daniel van Vugt)
* Remove superseded frame handling from assign_next_frame, handled in
  maybe_post_next_frame now.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5021>

src/backends/native/meta-onscreen-native.c
@@ -1740,11 +1740,20 @@ assign_next_frame (MetaOnscreenNative *onscreen_native,
 
   if (onscreen_native->next_frame != NULL)
     {
-      render_source_remove_frame (onscreen_native->render_source,
-                                  onscreen_native->next_frame);
-      clear_superseded_frame (onscreen);
-      onscreen_native->superseded_frame =
-        g_steal_pointer (&onscreen_native->next_frame);
+      if (!onscreen_native->render_source)
+        g_warning_once ("Unexpected non-NULL onscreen_native->next_frame");
+
+      /* This is needed to make sure we lock the correct front buffer for the
+       * new frame, as eglSwapBuffers() swaps what is the active front buffer.
+       *
+       * On Nvidia this will result in potentially synchronously waiting for
+       * pending GPU work.
+       */
+      cogl_framebuffer_flush (COGL_FRAMEBUFFER (onscreen));
+      maybe_post_next_frame (onscreen);
+
+      if (onscreen_native->next_frame != NULL)
+        g_warning_once ("maybe_post_next_frame didn't clear next frame");
     }
 
   onscreen_native->next_frame = clutter_frame_ref (frame);
@@ -1780,24 +1789,6 @@ meta_onscreen_native_swap_buffers_with_damage (CoglOnscreen    *onscreen,
 
   COGL_TRACE_BEGIN_SCOPED (MetaRendererNativeSwapBuffers,
                            "Meta::OnscreenNative::swap_buffers_with_damage()");
-
-  if (onscreen_native->next_frame != NULL)
-    {
-      if (!onscreen_native->render_source)
-        g_warning_once ("Unexpected non-NULL onscreen_native->next_frame");
-
-      /* This is needed to make sure we lock the correct front buffer for the
-       * new frame, as eglSwapBuffers() swaps what is the active front buffer.
-       *
-       * On Nvidia this will result in potentially synchronously waiting for
-       * pending GPU work.
-       */
-      cogl_framebuffer_flush (framebuffer);
-      maybe_post_next_frame (onscreen);
-
-      if (onscreen_native->next_frame != NULL)
-        goto swap_failed;
-    }
 
   assign_next_frame (onscreen_native, frame);
 
@@ -1837,11 +1828,6 @@ meta_onscreen_native_swap_buffers_with_damage (CoglOnscreen    *onscreen,
     {
       maybe_post_next_frame (onscreen);
     }
-
-  return;
-
-swap_failed:
-  clutter_frame_set_result (frame, CLUTTER_FRAME_RESULT_IDLE);
 }
 
 static void

commit 03e8cdcb15cdaefe7fd1846dd3182b0aa923d934

onscreen/native: Move next_frame to superseded_frame when shutting down
This should prevent hitting the "Unexpected non-NULL
onscreen_native->next_frame" warning in assign_next_frame during
shutdown.

Fixes: bebaf434be5a ("onscreen/native: Move nvidia deadlock prevention to assign_next_frame")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5040>

src/backends/native/meta-onscreen-native.c
@@ -1881,8 +1881,7 @@ maybe_post_next_frame (CoglOnscreen *onscreen)
                               onscreen_native->next_frame);
 
   if (onscreen_native->posted_frame != NULL ||
-      onscreen_native->view == NULL ||
-      meta_kms_is_shutting_down (kms))
+      onscreen_native->view == NULL)
     return;
 
   frame = g_steal_pointer (&onscreen_native->next_frame);
@@ -1990,6 +1989,11 @@ maybe_post_next_frame (CoglOnscreen *onscreen)
     {
       meta_renderer_native_queue_power_save_page_flip (renderer_native,
                                                        onscreen);
+    }
+
+  if (power_save_mode != META_POWER_SAVE_ON ||
+      meta_kms_is_shutting_down (kms))
+    {
       onscreen_native->superseded_frame = g_steal_pointer (&frame);
       return;
     }