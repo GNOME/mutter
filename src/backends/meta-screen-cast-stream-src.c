GNOME/mutter

commit 93a178eaf501c881994e86b1f5246b9a96d3581e

screen-cast/stream-src: Add meta_screen_cast_stream_src_paint_to_buffer
It centralizes code which was previously duplicated in multiple places.

It also re-uses the intermediate framebuffer, instead of creating an
ephemeral one each time. This eliminates some CPU overhead.

v2:
* Drop stage parameter. (Jonas Ådahl)
* Leave meta_screen_cast_virtual_stream_src_record_to_buffer view
  assignment unmodified.
v3:
* Use clutter_stage_paint_to_framebuffer_clipped.
v4:
* Call cogl_framebuffer_allocate from ensure_framebuffer.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5046>

src/backends/meta-screen-cast-stream-src.c
@@ -153,6 +153,7 @@ typedef struct _MetaScreenCastStreamSrcPrivate
    */
   GList *dequeued_buffers;
 
+  CoglFramebuffer *framebuffer;
   MtkRectangle layout;
   MtkRegion *damage;
 
@@ -398,6 +399,107 @@ meta_screen_cast_stream_src_get_videocrop (MetaScreenCastStreamSrc *src,
     return klass->get_videocrop (src, crop_rect);
 
   return FALSE;
+}
+
+static CoglFramebuffer *
+ensure_framebuffer (MetaScreenCastStreamSrc *src,
+                    CoglContext             *cogl_context,
+                    int                      width,
+                    int                      height,
+                    GError                 **error)
+{
+  MetaScreenCastStreamSrcPrivate *priv =
+    meta_screen_cast_stream_src_get_instance_private (src);
+
+  if (!priv->framebuffer)
+    {
+      CoglTexture *texture;
+      CoglOffscreen *offscreen;
+
+      texture = cogl_texture_2d_new_with_size (cogl_context,
+                                               width,
+                                               height);
+      if (!texture)
+        {
+          g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                       "Failed to create %dx%d texture",
+                       width, height);
+          return NULL;
+        }
+
+      offscreen = cogl_offscreen_new_with_texture (texture);
+      priv->framebuffer = COGL_FRAMEBUFFER (offscreen);
+
+      if (!cogl_framebuffer_allocate (priv->framebuffer, error))
+        g_clear_object (&priv->framebuffer);
+    }
+
+  return priv->framebuffer;
+}
+
+gboolean
+meta_screen_cast_stream_src_paint_to_buffer (MetaScreenCastStreamSrc   *src,
+                                             ClutterColorState         *color_state,
+                                             MtkRectangle              *area,
+                                             float                      scale,
+                                             int                        width,
+                                             int                        height,
+                                             int                        stride,
+                                             uint8_t                   *data,
+                                             CoglPixelFormat            format,
+                                             MtkRegion                 *damage,
+                                             GError                   **error)
+{
+  MetaScreenCastStream *stream = meta_screen_cast_stream_src_get_stream (src);
+  MetaScreenCastSession *session = meta_screen_cast_stream_get_session (stream);
+  MetaScreenCast *screen_cast =
+    meta_screen_cast_session_get_screen_cast (session);
+  MetaBackend *backend = meta_screen_cast_get_backend (screen_cast);
+  ClutterStage *stage = CLUTTER_STAGE (meta_backend_get_stage (backend));
+  ClutterContext *context =
+    clutter_actor_get_context (CLUTTER_ACTOR (stage));
+  ClutterBackend *clutter_backend = clutter_context_get_backend (context);
+  CoglContext *cogl_context =
+    clutter_backend_get_cogl_context (clutter_backend);
+  g_autoptr (CoglBitmap) bitmap = NULL;
+  CoglFramebuffer *framebuffer;
+  ClutterPaintFlag paint_flags;
+
+  paint_flags = CLUTTER_PAINT_FLAG_NONE;
+  switch (meta_screen_cast_stream_get_cursor_mode (stream))
+    {
+    case META_SCREEN_CAST_CURSOR_MODE_METADATA:
+    case META_SCREEN_CAST_CURSOR_MODE_HIDDEN:
+      paint_flags |= CLUTTER_PAINT_FLAG_NO_CURSORS;
+      break;
+    case META_SCREEN_CAST_CURSOR_MODE_EMBEDDED:
+      paint_flags |= CLUTTER_PAINT_FLAG_FORCE_CURSORS;
+      break;
+    }
+
+  framebuffer = ensure_framebuffer (src, cogl_context, width, height, error);
+  if (!framebuffer)
+    return FALSE;
+
+  clutter_stage_paint_to_framebuffer_clipped (stage,
+                                              framebuffer,
+                                              area,
+                                              scale,
+                                              color_state,
+                                              damage,
+                                              paint_flags);
+
+  bitmap = cogl_bitmap_new_for_data (cogl_context,
+                                     width, height,
+                                     format,
+                                     stride,
+                                     data);
+
+  cogl_framebuffer_read_pixels_into_bitmap (framebuffer,
+                                            0, 0,
+                                            COGL_READ_PIXELS_COLOR_BUFFER,
+                                            bitmap);
+  return TRUE;
 }
 
 static gboolean
@@ -2847,6 +2949,7 @@ meta_screen_cast_stream_src_dispose (GObject *object)
   g_clear_pointer (&priv->pipewire_source, g_source_destroy);
   g_clear_pointer (&priv->damage, mtk_region_unref);
   g_clear_object (&priv->color_state);
+  g_clear_object (&priv->framebuffer);
 
   g_clear_handle_id (&priv->negotiate_with_device_handle_id, g_source_remove);
 
@@ -2884,6 +2987,7 @@ meta_screen_cast_stream_src_set_property (GObject      *object,
           priv->layout = *layout;
           region = mtk_region_create_rectangle (layout);
           meta_screen_cast_stream_src_accumulate_damage (src, flag, region);
+          g_clear_object (&priv->framebuffer);
         }
       break;
     default:

src/backends/meta-screen-cast-area-stream-src.c
@@ -463,40 +463,25 @@ meta_screen_cast_area_stream_src_record_to_buffer (MetaScreenCastStreamSrc   *sr
                                                    uint8_t                   *data,
                                                    GError                   **error)
 {
-  MetaScreenCastAreaStreamSrc *area_src =
-    META_SCREEN_CAST_AREA_STREAM_SRC (src);
   MetaScreenCastStream *stream = meta_screen_cast_stream_src_get_stream (src);
   MetaScreenCastAreaStream *area_stream = META_SCREEN_CAST_AREA_STREAM (stream);
-  ClutterStage *stage;
   MtkRectangle *area;
   float scale;
-  ClutterPaintFlag paint_flags = CLUTTER_PAINT_FLAG_CLEAR;
 
-  stage = get_stage (area_src);
   area = meta_screen_cast_area_stream_get_area (area_stream);
   scale = meta_screen_cast_area_stream_get_scale (area_stream);
 
-  switch (meta_screen_cast_stream_get_cursor_mode (stream))
-    {
-    case META_SCREEN_CAST_CURSOR_MODE_METADATA:
-    case META_SCREEN_CAST_CURSOR_MODE_HIDDEN:
-      paint_flags |= CLUTTER_PAINT_FLAG_NO_CURSORS;
-      break;
-    case META_SCREEN_CAST_CURSOR_MODE_EMBEDDED:
-      paint_flags |= CLUTTER_PAINT_FLAG_FORCE_CURSORS;
-      break;
-    }
-
-  if (!clutter_stage_paint_to_buffer (stage, area, scale,
-                                      data,
-                                      stride,
-                                      COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
-                                      NULL,
-                                      paint_flags,
-                                      error))
-    return FALSE;
-
-  return TRUE;
+  return meta_screen_cast_stream_src_paint_to_buffer (src,
+                                                      NULL,
+                                                      area,
+                                                      scale,
+                                                      width,
+                                                      height,
+                                                      stride,
+                                                      data,
+                                                      COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
+                                                      NULL,
+                                                      error);
 }
 
 static gboolean

src/backends/meta-screen-cast-monitor-stream-src.c
@@ -573,47 +573,33 @@ meta_screen_cast_monitor_stream_src_record_to_buffer (MetaScreenCastStreamSrc   
 {
   MetaScreenCastMonitorStreamSrc *monitor_src =
     META_SCREEN_CAST_MONITOR_STREAM_SRC (src);
-  MetaScreenCastStream *stream = meta_screen_cast_stream_src_get_stream (src);
   MetaBackend *backend = get_backend (monitor_src);
-  ClutterStage *stage;
   MetaMonitor *monitor;
   MetaLogicalMonitor *logical_monitor;
   float scale;
-  ClutterPaintFlag paint_flags = CLUTTER_PAINT_FLAG_CLEAR;
   ClutterColorState *color_state;
 
   monitor = get_monitor (monitor_src);
   logical_monitor = meta_monitor_get_logical_monitor (monitor);
-  stage = get_stage (monitor_src);
 
   if (meta_backend_is_stage_views_scaled (backend))
     scale = meta_logical_monitor_get_scale (logical_monitor);
   else
     scale = 1.0;
 
-  switch (meta_screen_cast_stream_get_cursor_mode (stream))
-    {
-    case META_SCREEN_CAST_CURSOR_MODE_METADATA:
-    case META_SCREEN_CAST_CURSOR_MODE_HIDDEN:
-      paint_flags |= CLUTTER_PAINT_FLAG_NO_CURSORS;
-      break;
-    case META_SCREEN_CAST_CURSOR_MODE_EMBEDDED:
-      paint_flags |= CLUTTER_PAINT_FLAG_FORCE_CURSORS;
-      break;
-    }
-
   color_state = meta_screen_cast_stream_src_get_color_state (src);
 
-  if (!clutter_stage_paint_to_buffer (stage, &logical_monitor->rect, scale,
-                                      data,
-                                      stride,
-                                      COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
-                                      color_state,
-                                      paint_flags,
-                                      error))
-    return FALSE;
-
-  return TRUE;
+  return meta_screen_cast_stream_src_paint_to_buffer (src,
+                                                      color_state,
+                                                      &logical_monitor->rect,
+                                                      scale,
+                                                      width,
+                                                      height,
+                                                      stride,
+                                                      data,
+                                                      COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
+                                                      NULL,
+                                                      error);
 }
 
 static gboolean

src/backends/meta-screen-cast-stream-src.h
@@ -153,6 +153,18 @@ void meta_screen_cast_stream_src_request_process (MetaScreenCastStreamSrc *src);
 
 MetaScreenCastStream * meta_screen_cast_stream_src_get_stream (MetaScreenCastStreamSrc *src);
 
+gboolean meta_screen_cast_stream_src_paint_to_buffer (MetaScreenCastStreamSrc   *src,
+                                                      ClutterColorState         *color_state,
+                                                      MtkRectangle              *area,
+                                                      float                      scale,
+                                                      int                        width,
+                                                      int                        height,
+                                                      int                        stride,
+                                                      uint8_t                   *data,
+                                                      CoglPixelFormat            format,
+                                                      MtkRegion                 *damage,
+                                                      GError                   **error);
+
 gboolean meta_screen_cast_stream_src_draw_cursor_into (MetaScreenCastStreamSrc  *src,
                                                        CoglTexture              *cursor_texture,
                                                        int                       width,

src/backends/meta-screen-cast-virtual-stream-src.c
@@ -421,41 +421,25 @@ meta_screen_cast_virtual_stream_src_record_to_buffer (MetaScreenCastStreamSrc   
                                                       uint8_t                   *data,
                                                       GError                   **error)
 {
-  MetaScreenCastStream *stream;
-  ClutterPaintFlag paint_flags;
   ClutterStageView *view;
   MtkRectangle view_rect;
   float scale;
 
-  stream = meta_screen_cast_stream_src_get_stream (src);
   view = view_from_src (src);
   scale = clutter_stage_view_get_scale (view);
   clutter_stage_view_get_layout (view, &view_rect);
 
-  paint_flags = CLUTTER_PAINT_FLAG_CLEAR;
-  switch (meta_screen_cast_stream_get_cursor_mode (stream))
-    {
-    case META_SCREEN_CAST_CURSOR_MODE_METADATA:
-    case META_SCREEN_CAST_CURSOR_MODE_HIDDEN:
-      paint_flags |= CLUTTER_PAINT_FLAG_NO_CURSORS;
-      break;
-    case META_SCREEN_CAST_CURSOR_MODE_EMBEDDED:
-      paint_flags |= CLUTTER_PAINT_FLAG_FORCE_CURSORS;
-      break;
-    }
-
-  if (!clutter_stage_paint_to_buffer (stage_from_src (src),
-                                      &view_rect,
-                                      scale,
-                                      data,
-                                      stride,
-                                      COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
-                                      NULL,
-                                      paint_flags,
-                                      error))
-    return FALSE;
-
-  return TRUE;
+  return meta_screen_cast_stream_src_paint_to_buffer (src,
+                                                      NULL,
+                                                      &view_rect,
+                                                      scale,
+                                                      width,
+                                                      height,
+                                                      stride,
+                                                      data,
+                                                      COGL_PIXEL_FORMAT_CAIRO_ARGB32_COMPAT,
+                                                      NULL,
+                                                      error);
 }
 
 static gboolean

commit ee939cd3190ba7b9ac05c9f0d6fa69cc1779dc83

screen-cast/stream-src: Convert damage to redraw clip in _paint_to_buffer
The damage_to_redraw_clip helper is the inverse of
redraw_clip_to_damage.

Fixes gnome-shell screen recording without dma-bufs.

Fixes: 93a178eaf501 ("screen-cast/stream-src: Add meta_screen_cast_stream_src_paint_to_buffer")

v2:
* Fix condition for mtk_region_copy fast path. (Jonas Ådahl)

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5119>

src/backends/meta-screen-cast-stream-src.c
@@ -472,6 +472,35 @@ ensure_framebuffer (MetaScreenCastStreamSrc *src,
   return priv->framebuffer;
 }
 
+static MtkRegion *
+damage_to_redraw_clip (MetaScreenCastStreamSrc *src,
+                       const MtkRegion         *damage)
+{
+  MetaScreenCastStreamSrcPrivate *priv =
+    meta_screen_cast_stream_src_get_instance_private (src);
+  const MtkRectangle *layout = &priv->layout;
+  graphene_rect_t src_rect;
+
+  if (mtk_region_is_empty (damage))
+    return mtk_region_create ();
+
+  if (mtk_rectangle_is_empty (layout) ||
+      (layout->x == 0 && layout->y == 0 &&
+       layout->width == priv->video_format.size.width &&
+       layout->height == priv->video_format.size.height))
+    return mtk_region_copy (damage);
+
+  src_rect.origin.x = roundf ((float) layout->x *
+                              layout->width / priv->video_format.size.width);
+  src_rect.origin.y = roundf ((float) layout->y *
+                              layout->height / priv->video_format.size.height);
+  src_rect.size.width = layout->width;
+  src_rect.size.height = layout->height;
+  return mtk_region_crop_and_scale ((MtkRegion *) damage, &src_rect,
+                                    priv->video_format.size.width,
+                                    priv->video_format.size.height);
+}
+
 gboolean
 meta_screen_cast_stream_src_paint_to_buffer (MetaScreenCastStreamSrc   *src,
                                              ClutterColorState         *color_state,
@@ -516,16 +545,19 @@ meta_screen_cast_stream_src_paint_to_buffer (MetaScreenCastStreamSrc   *src,
 
   if (!framebuffer)
     {
+      g_autoptr (MtkRegion) redraw_clip = NULL;
+
       framebuffer = ensure_framebuffer (src, cogl_context, width, height, error);
       if (!framebuffer)
         return FALSE;
 
+      redraw_clip = damage_to_redraw_clip (src, damage);
       clutter_stage_paint_to_framebuffer_clipped (stage,
                                                   framebuffer,
                                                   area,
                                                   scale,
                                                   color_state,
-                                                  damage,
+                                                  redraw_clip,
                                                   paint_flags);
     }
 