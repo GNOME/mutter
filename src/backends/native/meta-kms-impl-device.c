6161872c466b5b75d902db12d95fb9f7f148856b
Michel Dänzer <mdaenzer@redhat.com>
2026-07-09 09:52:31 +0200

    kms/impl-device: Handle NULL crtc_frames in disarm_all_frame_sources
    Prevents a crash in that case.
    
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5170>
    (cherry picked from commit 5816af5d657af6bcbf0fd6bd830bc7e34611dcfb)
    (cherry picked from commit ade2439b213609552667336f701de2cc1a28ab23)

--- src/backends/native/meta-kms-impl-device.c
+++ src/backends/native/meta-kms-impl-device.c
@@ -2145,6 +2145,9 @@ disarm_all_frame_sources (MetaKmsImplDevice *impl_device)
   GHashTableIter iter;
   CrtcFrame *crtc_frame;
 
+  if (!priv->crtc_frames)
+    return;
+
   g_hash_table_iter_init (&iter, priv->crtc_frames);
   while (g_hash_table_iter_next (&iter, NULL, (gpointer *) &crtc_frame))
     {
