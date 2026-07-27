GNOME/mutter

commit e9ab34262ed07849a1cc655eb1392ade407e3eeb

clutter/color-state-params: Use output colorimetry & EOTF for blending
Exceptions being HDR and forced linear blending.

Preparation for next commit.

v2: (Sebastian Wick)
* Use PQ transfer instead of BT.2020 colorspace to check for HDR.
* Use blending_eotf on both sides of the comparison.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4900>

clutter/clutter/clutter-color-state-params.c
@@ -2150,22 +2150,25 @@ clutter_color_state_params_get_blending (ClutterColorState *color_state,
   ClutterEOTF blending_eotf;
   ClutterLuminance luminance, blending_luminance;
 
-  blending_eotf.type = CLUTTER_EOTF_TYPE_NAMED;
+  blending_colorimetry = color_state_params->colorimetry;
+  blending_eotf = color_state_params->eotf;
 
   if (force_linear)
     {
-      blending_colorimetry = color_state_params->colorimetry;
+      blending_eotf.type = CLUTTER_EOTF_TYPE_NAMED;
       blending_eotf.tf_name = CLUTTER_TRANSFER_FUNCTION_LINEAR;
     }
-  else
+  else if (blending_eotf.type == CLUTTER_EOTF_TYPE_NAMED &&
+           blending_eotf.tf_name == CLUTTER_TRANSFER_FUNCTION_PQ)
     {
-      blending_colorimetry.type = CLUTTER_COLORIMETRY_TYPE_COLORSPACE;
       blending_colorimetry.colorspace = CLUTTER_COLORSPACE_SRGB;
+      blending_eotf.type = CLUTTER_EOTF_TYPE_NAMED;
       blending_eotf.tf_name = CLUTTER_TRANSFER_FUNCTION_SRGB;
     }
 
-  if (color_state_params->eotf.type == CLUTTER_EOTF_TYPE_NAMED &&
-      color_state_params->eotf.tf_name == blending_eotf.tf_name)
+  if (eotf_equal (&blending_eotf, &color_state_params->eotf) &&
+      colorimetry_equal (&blending_colorimetry,
+                         &color_state_params->colorimetry))
     return g_object_ref (color_state);
 
   luminance = *clutter_color_state_params_get_luminance (color_state_params);

commit 98ab49c1caea784c558b67a7a8c5d997f85d787e

clutter/color-state-params: Set colorimetry type tag when overriding to sRGB
The PQ blending path set blending_colorimetry.colorspace without
updating the type tag from PRIMARIES to COLORSPACE. Since
ClutterColorimetry is a tagged union, writing the colorspace enum
clobbered only the lower bytes of the primaries pointer, causing a
use-after-free crash in clutter_primaries_equal.

Fixes: e9ab34262e ("clutter/color-state-params: Use output colorimetry & EOTF for blending")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5176>

clutter/clutter/clutter-color-state-params.c
@@ -2207,6 +2207,7 @@ clutter_color_state_params_get_blending (ClutterColorState *color_state,
   else if (blending_eotf.type == CLUTTER_EOTF_TYPE_NAMED &&
            blending_eotf.tf_name == CLUTTER_TRANSFER_FUNCTION_PQ)
     {
+      blending_colorimetry.type = CLUTTER_COLORIMETRY_TYPE_COLORSPACE;
       blending_colorimetry.colorspace = CLUTTER_COLORSPACE_SRGB;
       blending_eotf.type = CLUTTER_EOTF_TYPE_NAMED;
       blending_eotf.tf_name = CLUTTER_TRANSFER_FUNCTION_GAMMA22;