95b8442c6fe06a98a54bdfc7da1ff05f87e05456
Sebastian Wick <sebastian.wick@redhat.com>
2026-07-30 01:15:15 +0200

    clutter/color-utils: Add clutter_colorimetry_clear and g_auto support
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5208>
    (cherry picked from commit 940fcf5361ddeadd439b492b6b733deb4bd8ac58)

--- clutter/clutter/clutter-color-state-params.c
+++ clutter/clutter/clutter-color-state-params.c
@@ -29,6 +29,7 @@
 #include "clutter/clutter-color-state-params.h"
 
 #include "clutter/clutter-color-state-private.h"
+#include "clutter/clutter-mutter.h"
 
 #define UNIFORM_NAME_GAMMA_EXP "gamma_exp"
 #define UNIFORM_NAME_INV_GAMMA_EXP "inv_gamma_exp"
@@ -434,8 +435,7 @@ clutter_color_state_params_finalize (GObject *object)
   ClutterColorStateParams *color_state_params =
     CLUTTER_COLOR_STATE_PARAMS (object);
 
-  if (color_state_params->colorimetry.type == CLUTTER_COLORIMETRY_TYPE_PRIMARIES)
-    g_clear_pointer (&color_state_params->colorimetry.primaries, g_free);
+  clutter_colorimetry_clear (&color_state_params->colorimetry);
 
   G_OBJECT_CLASS (clutter_color_state_params_parent_class)->finalize (object);
 }
@@ -2464,4 +2464,11 @@ clutter_color_state_params_new_from_cicp (ClutterContext     *context,
                                                          colorimetry,
                                                          eotf,
                                                          lum);
+}
+
+void
+clutter_colorimetry_clear (ClutterColorimetry *colorimetry)
+{
+  if (colorimetry->type == CLUTTER_COLORIMETRY_TYPE_PRIMARIES)
+    g_clear_pointer (&colorimetry->primaries, g_free);
 }
