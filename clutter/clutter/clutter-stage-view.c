2405a1fb47fc5d014f5ebe0d4d05681cd724f672
Sebastian Wick <sebastian.wick@redhat.com>
2026-07-30 20:05:39 +0200

    clutter/stage-view: Invalidate viewport when invalidating offscreen
    Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5208>
    (cherry picked from commit 525644ab1950c274dc462b651d35209d93db9776)

--- clutter/clutter/clutter-stage-view.c
+++ clutter/clutter/clutter-stage-view.c
@@ -300,6 +300,8 @@ clutter_stage_view_invalidate_offscreen (ClutterStageView *view)
       !clutter_color_state_needs_mapping (priv->color_state,
                                           priv->output_color_state))
     {
+      clutter_stage_view_invalidate_viewport (view);
+      clutter_stage_view_invalidate_projection (view);
       g_clear_object (&priv->offscreen_pipeline);
       g_clear_object (&priv->offscreen);
       g_clear_handle_id (&priv->ensure_offscreen_idle_id, g_source_remove);
