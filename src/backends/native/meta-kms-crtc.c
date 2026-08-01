GNOME/mutter

commit b3179d4ae635e0737595ff011d19cbce28a609d9

kms/crtc: Dispatch ASAP on missing deadline within constant margin
One purpose of the margin is to compensate for the deadline timer firing
later than planned. If we missed the deadline for setting the timer, but
are still within the margin, we can start dispatch ASAP and hit the next
refresh cycle at least some of the time, instead of always skipping it.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5116>

src/backends/native/meta-kms-crtc.c
@@ -975,6 +975,23 @@ meta_kms_crtc_determine_deadline (MetaKmsCrtc  *crtc,
           next_deadline_us = now_us;
         }
     }
+  else if (now_us > next_deadline_us &&
+           now_us - next_deadline_us <= DEADLINE_EVASION_CONSTANT_US &&
+           (!target_presentation_time_us ||
+            mtk_find_nearest_interval_boundary (next_presentation_us,
+                                                target_presentation_time_us,
+                                                refresh_interval_us) ==
+            next_presentation_us))
+    {
+      if (target_presentation_time_us)
+        {
+          meta_topic (META_DEBUG_KMS_DEADLINE,
+                      "CRTC %d missed deadline by %3"G_GINT64_FORMAT "µs, "
+                      "starting ASAP for FRR",
+                      meta_kms_crtc_get_id (crtc),
+                      now_us - next_deadline_us);
+        }
+    }
   else if (now_us > next_deadline_us)
     {
       int64_t skip_us;

commit 2b4be6c0027937a718b76fd224aedcdb03b579f6

kms/crtc: Check for ASAP dispatch consistently with dispatch code
Otherwise it could happen that meta_kms_crtc_determine_deadline
decided to start dispatch ASAP, but then crtc_frame_deadline_dispatch
decided to wait for the next cycle, resulting in ping-pong between
them.

Fixes: b3179d4ae635 ("kms/crtc: Dispatch ASAP on missing deadline within constant margin")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5156>

src/backends/native/meta-kms-crtc.c
@@ -983,10 +983,8 @@ meta_kms_crtc_determine_deadline (MetaKmsCrtc    *crtc,
            crtc->deadline_evasion_margin.current_us +
            DEADLINE_EVASION_CONSTANT_US &&
            (!target_presentation_time_us ||
-            mtk_find_nearest_interval_boundary (next_presentation_us,
-                                                target_presentation_time_us,
-                                                refresh_interval_us) ==
-            next_presentation_us))
+            !meta_kms_crtc_target_is_after_expected (next_presentation_us,
+                                                     target_presentation_time_us)))
     {
       if (target_presentation_time_us)
         {