GNOME/mutter

commit b0dde2b618c5b40123b8982c8d3972f3832bf6cc

core/stack-tracker: Misc refactor/reformat
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5014>

src/core/stack-tracker.c
@@ -564,20 +564,16 @@ drop_x11_windows (MetaDisplay      *display,
 
   g_array_unref (tracker->verified_stack);
   tracker->verified_stack = new_stack;
-  l = tracker->unverified_predictions->head;
 
-  while (l)
+  for (l = tracker->unverified_predictions->head; l; l = l->next)
     {
       MetaStackOp *op = l->data;
-      GList *next = l->next;
 
       if (META_STACK_ID_IS_X11 (op->any.window))
         {
           g_queue_remove (tracker->unverified_predictions, op);
           meta_stack_op_free (op);
         }
-
-      l = next;
     }
 }
 #endif /* HAVE_XWAYLAND */
@@ -704,10 +700,11 @@ meta_stack_tracker_free (MetaStackTracker *tracker)
 {
   if (tracker->sync_stack_later)
     {
-      MetaCompositor *compositor =
-        meta_display_get_compositor (tracker->display);
-      MetaLaters *laters = meta_compositor_get_laters (compositor);
+      MetaCompositor *compositor;
+      MetaLaters *laters;
 
+      compositor = meta_display_get_compositor (tracker->display);
+      laters = meta_compositor_get_laters (compositor);
       meta_laters_remove (laters, tracker->sync_stack_later);
     }
 
@@ -1108,8 +1105,7 @@ meta_stack_tracker_sync_stack (MetaStackTracker *tracker)
                                        meta_display_lookup_stamp (tracker->display, window));
     }
 
-  meta_compositor_sync_stack (tracker->display->compositor,
-                              meta_windows);
+  meta_compositor_sync_stack (tracker->display->compositor, meta_windows);
   g_list_free (meta_windows);
 
   meta_display_restacked (tracker->display);

commit 31df6384dc8c6608b3b9909c60674f3a35acd64e

core/stack-tracker: Fix use-after-free when dropping X11 windows
drop_x11_windows() removes operations from the unverified_predictions
queue while iterating over it. g_queue_remove() deletes the link
holding the operation, so advancing to l->next afterwards dereferences
freed memory.

The loop saved the next link before removing, until it was converted to
a for loop and the temporary was dropped along the way. Restore it.

This is reachable whenever an X11 operation is still queued as Xwayland
shuts down, and shows up as a heap-use-after-free under AddressSanitizer.

Fixes: b0dde2b618 ("core/stack-tracker: Misc refactor/reformat")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5216>

src/core/stack-tracker.c
@@ -578,15 +578,19 @@ drop_x11_windows (MetaDisplay      *display,
   g_array_unref (tracker->verified_stack);
   tracker->verified_stack = new_stack;
 
-  for (l = tracker->unverified_predictions->head; l; l = l->next)
+  l = tracker->unverified_predictions->head;
+  while (l)
     {
       MetaStackOp *op = l->data;
+      GList *next = l->next;
 
       if (META_STACK_ID_IS_X11 (op->any.window))
         {
           g_queue_remove (tracker->unverified_predictions, op);
           meta_stack_op_free (op);
         }
+
+      l = next;
     }
 
   meta_stack_tracker_dump (tracker);