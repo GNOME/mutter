GNOME/mutter

commit 7131442be4d8288654dcf31d629c1af8871dd968

onscreen/native: Remove blit_source_quark
I don't know of any reason why the primary GPU MetaDrmBuffer would need
to stay alive as long as the secondary GPU one.

This will allow secondary GPU MetaDrmBuffers to stay alive indefinitely
without affecting the primary GPU.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4908>

src/backends/native/meta-onscreen-native.c
@@ -170,8 +170,6 @@ struct _MetaOnscreenNative
 G_DEFINE_TYPE (MetaOnscreenNative, meta_onscreen_native,
                COGL_TYPE_ONSCREEN_EGL)
 
-static GQuark blit_source_quark = 0;
-
 static void
 maybe_post_next_frame (CoglOnscreen *onscreen);
 
@@ -1400,11 +1398,6 @@ copy_shared_framebuffer_gpu (CoglOnscreen                         *onscreen,
       goto done;
     }
 
-  g_object_set_qdata_full (G_OBJECT (buffer_gbm),
-                           blit_source_quark,
-                           g_object_ref (primary_gpu_fb),
-                           g_object_unref);
-
 done:
   if (egl_sync != EGL_NO_SYNC)
     {
@@ -1548,11 +1541,6 @@ copy_shared_framebuffer_primary_gpu (CoglOnscreen                        *onscre
             }
         }
     }
-
-  g_object_set_qdata_full (G_OBJECT (buffer),
-                           blit_source_quark,
-                           g_steal_pointer (&dmabuf_fb),
-                           g_object_unref);
 
   secondary_gpu_state->cpu.current_dumb_fb = buffer_dumb;
 
@@ -3545,8 +3533,6 @@ meta_onscreen_native_class_init (MetaOnscreenNativeClass *klass)
     meta_onscreen_native_swap_buffers_with_damage;
   onscreen_class->direct_scanout = meta_onscreen_native_direct_scanout;
   onscreen_class->get_window_handles = meta_onscreen_native_get_window_handles;
-
-  blit_source_quark = g_quark_from_static_string ("Blit source");
 }
 
 MetaCrtc *

commit 2e0b99b1610b6855b26796744639ac08f19cd606

onscreen/native: Release temporary framebuffers after copying
Right now, the primary GPU copy path creates a temporary framebuffer for
each copy and releases its reference when blitting fails.

A successful copy leaves that framebuffer referenced, so framebuffers
accumulate during rendering.

This commit uses automatic cleanup to release the reference on both
success and failure.

Fixes: 7131442be4d8 ("onscreen/native: Remove blit_source_quark")

src/backends/native/meta-onscreen-native.c
@@ -1289,7 +1289,7 @@ copy_shared_framebuffer_primary_gpu (CoglOnscreen                        *onscre
   MetaDrmBufferDumb *buffer_dumb;
   MetaDrmBuffer *buffer;
   int width, height;
-  CoglFramebuffer *dmabuf_fb;
+  g_autoptr (CoglFramebuffer) dmabuf_fb = NULL;
   CoglContext *cogl_context;
   CoglDisplay *cogl_display;
   CoglRenderer *cogl_renderer;
@@ -1375,10 +1375,7 @@ copy_shared_framebuffer_primary_gpu (CoglOnscreen                        *onscre
                                      region,
                                      0, 0,
                                      &error))
-    {
-      g_object_unref (dmabuf_fb);
-      return NULL;
-    }
+    return NULL;
 
   secondary_gpu_state->cpu.current_dumb_fb = buffer_dumb;
 