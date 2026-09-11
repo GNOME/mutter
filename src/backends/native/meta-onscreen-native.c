GNOME/mutter

commit 6a76fe5e94f5ccb93c485b8f0d262354f972d932

onscreen/native: Use FBOs for secondary GPU rendering
Instead of EGL surfaces.

This avoids meta_drm_buffer_gbm_new_lock_front blocking with the nvidia
driver, works fine with other drivers as well, and is a net reduction
in code.

v2:
* Also swap y1 & y2 in vertices in paint_egl_image, fixes drawing upside
  down.
v3:
* Remove TODO comment in maybe_init_render_source which is now obsolete.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4908>

src/backends/native/meta-onscreen-native.c
@@ -87,10 +87,9 @@ typedef struct _MetaOnscreenNativeSecondaryGpuState
   MetaGpuKms *gpu_kms;
   MetaRendererNativeGpuData *renderer_gpu_data;
 
-  EGLSurface egl_surface;
-
   struct {
-    struct gbm_surface *surface;
+    MetaDrmBufferGbm *buffer_gbm[2];
+    int buffer_index;
   } gbm;
 
   struct {
@@ -1077,34 +1076,16 @@ static void
 secondary_gpu_state_free (MetaOnscreenNativeSecondaryGpuState *secondary_gpu_state)
 {
   unsigned int i;
-  MetaGpu *gpu = META_GPU (secondary_gpu_state->gpu_kms);
-  MetaBackend *backend = meta_gpu_get_backend (gpu);
-  MetaEgl *egl = meta_backend_get_egl (backend);
 
+  g_clear_object (&secondary_gpu_state->gbm.buffer_gbm[0]);
+  g_clear_object (&secondary_gpu_state->gbm.buffer_gbm[1]);
   g_clear_object (&secondary_gpu_state->source_framebuffer);
-
-  if (secondary_gpu_state->egl_surface != EGL_NO_SURFACE)
-    {
-      MetaRendererNativeGpuData *renderer_gpu_data;
-      MetaRenderDevice *render_device;
-      EGLDisplay egl_display;
-
-      renderer_gpu_data = secondary_gpu_state->renderer_gpu_data;
-      render_device = renderer_gpu_data->render_device;
-      egl_display = meta_render_device_get_egl_display (render_device);
-      meta_egl_destroy_surface (egl,
-                                egl_display,
-                                secondary_gpu_state->egl_surface,
-                                NULL);
-    }
 
   for (i = 0; i < MAX_SECONDARY_GPU_BUFFER_AGE; i++)
     {
       g_clear_pointer (&secondary_gpu_state->damage_regions[i],
                        mtk_region_unref);
     }
-
-  g_clear_pointer (&secondary_gpu_state->gbm.surface, gbm_surface_destroy);
 
   secondary_gpu_release_dumb (secondary_gpu_state);
 
@@ -1213,43 +1194,25 @@ build_secondary_gpu_damage_region (MetaOnscreenNativeSecondaryGpuState *secondar
   return g_steal_pointer (&region);
 }
 
-static int
-get_secondary_gpu_buffer_age (MetaOnscreenNativeSecondaryGpuState *secondary_gpu_state,
-                              MetaRendererNativeGpuData           *renderer_gpu_data)
+static MetaDrmBufferGbm *
+get_secondary_gpu_buffer_and_age (MetaOnscreenNativeSecondaryGpuState *secondary_gpu_state,
+                                  int                                 *out_buffer_age)
 {
-  MetaRendererNative *renderer_native = renderer_gpu_data->renderer_native;
-  MetaEgl *egl = meta_renderer_native_get_egl (renderer_native);
-  MetaRenderDevice *render_device;
-  EGLDisplay egl_display;
-  int buffer_age;
-  g_autoptr (GError) error = NULL;
+  int buffer_index = secondary_gpu_state->gbm.buffer_index;
 
-  render_device = renderer_gpu_data->render_device;
-  egl_display = meta_render_device_get_egl_display (render_device);
-
-  if (!meta_egl_query_surface (egl, egl_display,
-                               secondary_gpu_state->egl_surface,
-                               EGL_BUFFER_AGE_EXT, &buffer_age,
-                               &error))
+  if (buffer_index < 0)
     {
-      g_warning ("Failed to query age of surface, ignoring damage "
-                 "rectangles and fully redrawing, which may cause increased "
-                 "GPU power consumption: %s", error->message);
-
-      return 0;
+      buffer_index += 2;
+      secondary_gpu_state->gbm.buffer_index++;
+      *out_buffer_age = 0;
+    }
+  else
+    {
+      secondary_gpu_state->gbm.buffer_index = 1 - buffer_index;
+      *out_buffer_age = 2;
     }
 
-  if (buffer_age > MAX_SECONDARY_GPU_BUFFER_AGE)
-    {
-      g_warning ("Secondary GPU provides buffers of age %i, which is "
-                 "older than supported; ignoring damage rectangles and fully "
-                 "redrawing which may cause increased GPU power consumption",
-                 buffer_age);
-
-      return 0;
-    }
-
-  return buffer_age;
+  return g_object_ref (secondary_gpu_state->gbm.buffer_gbm[buffer_index]);
 }
 
 static MetaDrmBuffer *
@@ -1269,15 +1232,12 @@ copy_shared_framebuffer_gpu (CoglOnscreen                         *onscreen,
   CoglDisplay *cogl_display = cogl_context_get_display (cogl_context);
   MetaRenderDevice *render_device;
   EGLDisplay egl_display;
-  gboolean use_modifiers;
-  MetaDeviceFile *device_file;
-  MetaDrmBufferFlags flags;
-  MetaDrmBufferGbm *buffer_gbm = NULL;
-  struct gbm_bo *bo;
+  MetaDrmBufferGbm *dst_buffer_gbm = NULL, *src_buffer_gbm;
+  struct gbm_bo *dst_bo, *src_bo;
   EGLSync egl_sync = EGL_NO_SYNC;
   g_autofd int sync_fd = -1;
-  EGLImageKHR egl_image;
-  int buffer_age = 0;
+  EGLImageKHR dst_egl_image, src_egl_image;
+  int buffer_age;
   g_autoptr (MtkRegion) blit_region = NULL;
   g_autoptr (MtkRegion) region_to_push = NULL;
 
@@ -1292,8 +1252,8 @@ copy_shared_framebuffer_gpu (CoglOnscreen                         *onscreen,
 
   if (!meta_egl_make_current (egl,
                               egl_display,
-                              secondary_gpu_state->egl_surface,
-                              secondary_gpu_state->egl_surface,
+                              EGL_NO_SURFACE,
+                              EGL_NO_SURFACE,
                               renderer_gpu_data->secondary.egl_context,
                               error))
     {
@@ -1331,42 +1291,41 @@ copy_shared_framebuffer_gpu (CoglOnscreen                         *onscreen,
         }
     }
 
-  buffer_gbm = META_DRM_BUFFER_GBM (primary_gpu_fb);
-  bo = meta_drm_buffer_gbm_get_bo (buffer_gbm);
-  egl_image = meta_egl_ensure_gbm_bo_egl_image (egl, egl_display, bo, error);
+  src_buffer_gbm = META_DRM_BUFFER_GBM (primary_gpu_fb);
+  src_bo = meta_drm_buffer_gbm_get_bo (src_buffer_gbm);
+  src_egl_image = meta_egl_ensure_gbm_bo_egl_image (egl, egl_display, src_bo, error);
 
-  if (!egl_image)
+  if (!src_egl_image)
     {
       g_prefix_error (error, "Failed to create EGL image from buffer object for secondary GPU: ");
       goto done;
     }
 
-  push_secondary_gpu_damage_rectangles (secondary_gpu_state, bo, region);
+  dst_buffer_gbm = get_secondary_gpu_buffer_and_age (secondary_gpu_state,
+                                                     &buffer_age);
+  dst_bo = meta_drm_buffer_gbm_get_bo (dst_buffer_gbm);
+  dst_egl_image = meta_egl_ensure_gbm_bo_egl_image (egl, egl_display, dst_bo, error);
+  if (!dst_egl_image)
+    {
+      g_prefix_error (error, "Failed to create EGL image from buffer object for secondary GPU: ");
+      goto done;
+    }
 
-  buffer_age = get_secondary_gpu_buffer_age (secondary_gpu_state,
-                                             renderer_gpu_data);
+  push_secondary_gpu_damage_rectangles (secondary_gpu_state, src_bo, region);
   blit_region = build_secondary_gpu_damage_region (secondary_gpu_state,
-                                                   bo, buffer_age);
+                                                   src_bo, buffer_age);
 
   if (!meta_renderer_native_gles3_blit_shared_bo (egl,
                                                   gles3,
                                                   egl_display,
                                                   renderer_gpu_data->secondary.egl_context,
-                                                  egl_image,
-                                                  bo,
+                                                  dst_egl_image,
+                                                  src_egl_image,
+                                                  src_bo,
                                                   blit_region,
                                                   error))
     {
       g_prefix_error (error, "Failed to blit shared framebuffer: ");
-      goto done;
-    }
-
-  if (!meta_egl_swap_buffers (egl,
-                              egl_display,
-                              secondary_gpu_state->egl_surface,
-                              error))
-    {
-      g_prefix_error (error, "Failed to swap buffers: ");
       goto done;
     }
 
@@ -1378,24 +1337,6 @@ copy_shared_framebuffer_gpu (CoglOnscreen                         *onscreen,
     }
 
   meta_frame_native_set_sync_fd (frame_native, g_steal_fd (&sync_fd));
-
-  use_modifiers = meta_renderer_native_use_modifiers (renderer_native);
-  device_file = meta_render_device_get_device_file (render_device);
-
-  flags = META_DRM_BUFFER_FLAG_NONE;
-  if (!use_modifiers)
-    flags |= META_DRM_BUFFER_FLAG_DISABLE_MODIFIERS;
-
-  buffer_gbm =
-    meta_drm_buffer_gbm_new_lock_front (device_file,
-                                        secondary_gpu_state->gbm.surface,
-                                        flags,
-                                        error);
-  if (!buffer_gbm)
-    {
-      g_prefix_error (error, "meta_drm_buffer_gbm_new_lock_front failed: ");
-      goto done;
-    }
 
 done:
   if (egl_sync != EGL_NO_SYNC)
@@ -1411,7 +1352,7 @@ done:
 
   _cogl_winsys_egl_ensure_current (cogl_display);
 
-  return buffer_gbm ? META_DRM_BUFFER (buffer_gbm) : NULL;
+  return dst_buffer_gbm ? META_DRM_BUFFER (dst_buffer_gbm) : NULL;
 }
 
 static MetaDrmBufferDumb *
@@ -3116,18 +3057,19 @@ init_secondary_gpu_state_gpu_copy_mode (MetaRendererNative         *renderer_nat
   MetaOnscreenNative *onscreen_native = META_ONSCREEN_NATIVE (onscreen);
   MetaEgl *egl = meta_onscreen_native_get_egl (onscreen_native);
   MetaRenderDevice *render_device;
+  MetaDeviceFile *device_file;
   MetaRenderDeviceGbm *render_device_gbm;
   struct gbm_device *gbm_device;
   EGLDisplay egl_display;
   int width, height;
-  EGLNativeWindowType egl_native_window;
-  struct gbm_surface *gbm_surface;
-  EGLSurface egl_surface;
   MetaOnscreenNativeSecondaryGpuState *secondary_gpu_state;
+  MetaDrmBufferFlags flags = META_DRM_BUFFER_FLAG_NONE;
   MetaGpuKms *gpu_kms;
   uint32_t format;
+  int i;
 
   render_device = renderer_gpu_data->render_device;
+  device_file = meta_render_device_get_device_file (render_device);
   egl_display = meta_render_device_get_egl_display (render_device);
   width = cogl_framebuffer_get_width (framebuffer);
   height = cogl_framebuffer_get_height (framebuffer);
@@ -3137,47 +3079,47 @@ init_secondary_gpu_state_gpu_copy_mode (MetaRendererNative         *renderer_nat
 
   render_device_gbm = META_RENDER_DEVICE_GBM (render_device);
   gbm_device = meta_render_device_gbm_get_gbm_device (render_device_gbm);
-  gbm_surface = gbm_surface_create (gbm_device,
-                                    width, height,
-                                    format,
-                                    GBM_BO_USE_SCANOUT | GBM_BO_USE_RENDERING);
-
-  if (!gbm_surface)
-    {
-      gbm_surface = gbm_surface_create (gbm_device,
-                                        width, height,
-                                        format,
-                                        0);
-    }
-
-  if (!gbm_surface)
-    {
-      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
-                   "Failed to create gbm_surface: %s", g_strerror (errno));
-      return FALSE;
-    }
-
-  egl_native_window = (EGLNativeWindowType) gbm_surface;
-  egl_surface =
-    meta_egl_create_window_surface (egl,
-                                    egl_display,
-                                    renderer_gpu_data->secondary.egl_config,
-                                    egl_native_window,
-                                    NULL,
-                                    error);
-  if (egl_surface == EGL_NO_SURFACE)
-    {
-      gbm_surface_destroy (gbm_surface);
-      return FALSE;
-    }
 
   secondary_gpu_state = g_new0 (MetaOnscreenNativeSecondaryGpuState, 1);
 
   gpu_kms = META_GPU_KMS (meta_crtc_get_gpu (onscreen_native->crtc));
   secondary_gpu_state->gpu_kms = gpu_kms;
   secondary_gpu_state->renderer_gpu_data = renderer_gpu_data;
-  secondary_gpu_state->gbm.surface = gbm_surface;
-  secondary_gpu_state->egl_surface = egl_surface;
+  secondary_gpu_state->gbm.buffer_index = -2;
+
+  if (!meta_renderer_native_use_modifiers (renderer_native))
+    flags = META_DRM_BUFFER_FLAG_DISABLE_MODIFIERS;
+
+  for (i = 0; i < 2; i++)
+    {
+      struct gbm_bo *gbm_bo;
+
+      gbm_bo = gbm_bo_create (gbm_device,
+                              width,
+                              height,
+                              format,
+                              GBM_BO_USE_SCANOUT | GBM_BO_USE_RENDERING);
+      if (!gbm_bo)
+        {
+          gbm_bo_destroy (gbm_bo);
+          g_free (secondary_gpu_state);
+          g_set_error (error,  G_IO_ERROR, G_IO_ERROR_FAILED,
+                       "gbm_bo_create failed for secondary GPU: %s",
+                       strerror (errno));
+          return FALSE;
+        }
+
+      secondary_gpu_state->gbm.buffer_gbm[i] =
+        meta_drm_buffer_gbm_new_take (device_file, gbm_bo, flags, error);
+      if (!secondary_gpu_state->gbm.buffer_gbm[i])
+        {
+          gbm_bo_destroy (gbm_bo);
+          g_free (secondary_gpu_state);
+          g_prefix_error (error,
+                          "meta_drm_buffer_gbm_new_take failed for secondary GPU: ");
+          return FALSE;
+        }
+    }
 
   onscreen_native->secondary_gpu_state = secondary_gpu_state;
 
@@ -3282,7 +3224,6 @@ init_secondary_gpu_state_cpu_copy_mode (MetaRendererNative         *renderer_nat
   secondary_gpu_state = g_new0 (MetaOnscreenNativeSecondaryGpuState, 1);
   secondary_gpu_state->renderer_gpu_data = renderer_gpu_data;
   secondary_gpu_state->gpu_kms = gpu_kms;
-  secondary_gpu_state->egl_surface = EGL_NO_SURFACE;
 
   for (i = 0; i < G_N_ELEMENTS (secondary_gpu_state->cpu.dumb_fbs); i++)
     {

src/backends/native/meta-renderer-native-gles3.c
@@ -293,26 +293,20 @@ blit_egl_image (MetaGles3        *gles3,
   GLBAS (gles3, glFramebufferTexture2D, (GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                                          GL_TEXTURE_2D, texture, 0));
 
-  GLBAS (gles3, glBindFramebuffer, (GL_READ_FRAMEBUFFER, framebuffer));
-
   for (i = 0; i < n_rectangles; ++i)
     {
       MtkRectangle rectangle;
       GLint x1, y1, x2, y2;
-      GLint src_y1, src_y2;
 
       rectangle = mtk_region_get_rectangle (region, i);
 
       x1 = rectangle.x;
-      y1 = height - rectangle.y - rectangle.height;
+      y1 = rectangle.y;
       x2 = x1 + rectangle.width;
       y2 = y1 + rectangle.height;
 
-      src_y1 = rectangle.y;
-      src_y2 = src_y1 + rectangle.height;
-
-      GLBAS (gles3, glBlitFramebuffer, (x1, src_y2, x2, src_y1,
-                                        x1, height - rectangle.y - rectangle.height, x2, y2,
+      GLBAS (gles3, glBlitFramebuffer, (x1, y1, x2, y2,
+                                        x1, y1, x2, y2,
                                         GL_COLOR_BUFFER_BIT,
                                         GL_NEAREST));
     }
@@ -347,7 +341,6 @@ paint_egl_image (ContextData      *context_data,
 
   for (i = 0; i < n_rectangles; ++i)
     {
-      int reversed_rect_y;
       GLint x1, y1, x2, y2;
       GLint u1, v1, u2, v2;
       MtkRectangle rectangle;
@@ -355,12 +348,11 @@ paint_egl_image (ContextData      *context_data,
 
       rectangle = mtk_region_get_rectangle (region, i);
       rectangle_vertices = &vertices[i * COMPONENTS_PER_RECTANGLE];
-      reversed_rect_y = height - rectangle.y - rectangle.height;
 
       x1 = rectangle.x;
-      y1 = reversed_rect_y;
+      y1 = rectangle.y;
       x2 = rectangle.x + rectangle.width;
-      y2 = reversed_rect_y + rectangle.height;
+      y2 = rectangle.y + rectangle.height;
 
       u1 = rectangle.x;
       v1 = rectangle.y;
@@ -368,32 +360,32 @@ paint_egl_image (ContextData      *context_data,
       v2 = rectangle.y + rectangle.height;
 
       rectangle_vertices[0] = x1;
-      rectangle_vertices[1] = y2;
+      rectangle_vertices[1] = y1;
       rectangle_vertices[2] = u1;
       rectangle_vertices[3] = v1;
 
       rectangle_vertices[4] = x2;
-      rectangle_vertices[5] = y2;
+      rectangle_vertices[5] = y1;
       rectangle_vertices[6] = u2;
       rectangle_vertices[7] = v1;
 
       rectangle_vertices[8] = x1;
-      rectangle_vertices[9] = y1;
+      rectangle_vertices[9] = y2;
       rectangle_vertices[10] = u1;
       rectangle_vertices[11] = v2;
 
       rectangle_vertices[12] = x1;
-      rectangle_vertices[13] = y1;
+      rectangle_vertices[13] = y2;
       rectangle_vertices[14] = u1;
       rectangle_vertices[15] = v2;
 
       rectangle_vertices[16] = x2;
-      rectangle_vertices[17] = y2;
+      rectangle_vertices[17] = y1;
       rectangle_vertices[18] = u2;
       rectangle_vertices[19] = v1;
 
       rectangle_vertices[20] = x2;
-      rectangle_vertices[21] = y1;
+      rectangle_vertices[21] = y2;
       rectangle_vertices[22] = u2;
       rectangle_vertices[23] = v2;
     }
@@ -471,7 +463,8 @@ meta_renderer_native_gles3_blit_shared_bo (MetaEgl          *egl,
                                            MetaGles3        *gles3,
                                            EGLDisplay        egl_display,
                                            EGLContext        egl_context,
-                                           EGLImageKHR       egl_image,
+                                           EGLImageKHR       dst_egl_image,
+                                           EGLImageKHR       src_egl_image,
                                            struct gbm_bo    *shared_bo,
                                            const MtkRegion  *region,
                                            GError          **error)
@@ -480,6 +473,7 @@ meta_renderer_native_gles3_blit_shared_bo (MetaEgl          *egl,
   unsigned int height;
   GQuark context_data_quark;
   ContextData *context_data;
+  GLuint dst_texture, dst_framebuffer;
   gboolean can_blit;
 
   context_data_quark = get_quark_for_egl_context (egl_context);
@@ -504,11 +498,22 @@ meta_renderer_native_gles3_blit_shared_bo (MetaEgl          *egl,
   width = gbm_bo_get_width (shared_bo);
   height = gbm_bo_get_height (shared_bo);
 
-  if (can_blit)
-    blit_egl_image (gles3, egl_image, width, height, region);
-  else
-    paint_egl_image (context_data, gles3, egl_image, width, height, region);
+  GLBAS (gles3, glGenFramebuffers, (1, &dst_framebuffer));
+  GLBAS (gles3, glBindFramebuffer, (GL_DRAW_FRAMEBUFFER, dst_framebuffer));
+  GLBAS (gles3, glActiveTexture, (GL_TEXTURE0));
+  GLBAS (gles3, glGenTextures, (1, &dst_texture));
+  GLBAS (gles3, glBindTexture, (GL_TEXTURE_2D, dst_texture));
+  GLEXT (gles3, glEGLImageTargetTexture2DOES, (GL_TEXTURE_2D, dst_egl_image));
+  GLBAS (gles3, glFramebufferTexture2D, (GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                                         GL_TEXTURE_2D, dst_texture, 0));
 
+  if (can_blit)
+    blit_egl_image (gles3, src_egl_image, width, height, region);
+  else
+    paint_egl_image (context_data, gles3, src_egl_image, width, height, region);
+
+  GLBAS (gles3, glDeleteTextures, (1, &dst_texture));
+  GLBAS (gles3, glDeleteFramebuffers, (1, &dst_framebuffer));
   return TRUE;
 }
 

src/backends/native/meta-renderer-native-gles3.h
@@ -31,7 +31,8 @@ gboolean meta_renderer_native_gles3_blit_shared_bo (MetaEgl          *egl,
                                                     MetaGles3        *gles3,
                                                     EGLDisplay        egl_display,
                                                     EGLContext        egl_context,
-                                                    EGLImageKHR       egl_image,
+                                                    EGLImageKHR       dst_egl_image,
+                                                    EGLImageKHR       src_egl_image,
                                                     struct gbm_bo    *shared_bo,
                                                     const MtkRegion  *region,
                                                     GError          **error);

commit e17757554963aeebb1279c34b8143360d0a186da

onscreen/native: Reject failed secondary GPU copies
Right now, the secondary GPU copy path selects a destination before
preparing its EGL image and blitting.

If either operation fails, the common exit path still returns the
selected buffer, even though it has not been filled. A non-NULL result
therefore does not reliably indicate a successful copy.

This commit tracks whether the blit succeeded and releases the destination
reference on failure, so those failures return NULL.

Fixes: 6a76fe5e94f5 ("onscreen/native: Use FBOs for secondary GPU rendering")

src/backends/native/meta-onscreen-native.c
@@ -1137,6 +1137,7 @@ copy_shared_framebuffer_gpu (CoglOnscreen                         *onscreen,
   int buffer_age;
   g_autoptr (MtkRegion) blit_region = NULL;
   g_autoptr (MtkRegion) region_to_push = NULL;
+  gboolean buffer_copied = FALSE;
 
   COGL_TRACE_BEGIN_SCOPED (CopySharedFramebufferSecondaryGpu,
                            "copy_shared_framebuffer_gpu()");
@@ -1228,6 +1229,7 @@ copy_shared_framebuffer_gpu (CoglOnscreen                         *onscreen,
       g_prefix_error (error, "Failed to blit shared framebuffer: ");
       goto done;
     }
+  buffer_copied = TRUE;
 
   sync_fd = cogl_renderer_egl_create_sync_fd (renderer_egl, error);
   if (sync_fd < 0)
@@ -1250,6 +1252,9 @@ done:
     }
 
   cogl_display_egl_ensure_current (COGL_DISPLAY_EGL (cogl_display));
+
+  if (!buffer_copied)
+    g_clear_object (&dst_buffer_gbm);
 
   return dst_buffer_gbm ? META_DRM_BUFFER (dst_buffer_gbm) : NULL;
 }