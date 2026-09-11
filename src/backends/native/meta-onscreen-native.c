GNOME/mutter

commit 753805bcca7abe96f234170534d512f7a5e6bf65

cogl: Add cogl_framebuffer_blit_region
And use it instead of cogl_framebuffer_blit where useful.

v2:
* Adapt to cogl_framebuffer_blit staying unmodified.
v3:
* Fix indentation.
v4: (Jonas Ådahl)
* Refactor cogl_framebuffer_blit_setup & cogl_framebuffer_blit_box
  helpers, cogl_framebuffer_blit no longer needs to allocate memory.
* Keep using cogl_framebuffer_blit in clutter_blit_node_draw.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5046>

src/backends/native/meta-onscreen-native.c
@@ -1357,35 +1357,19 @@ copy_shared_framebuffer_primary_gpu (CoglOnscreen                        *onscre
     }
 
   n_rectangles = mtk_region_num_rectangles (region);
-  if (n_rectangles == 0 || n_rectangles > MAX_DAMAGE_RECTANGLES)
+  if (((n_rectangles == 0 || n_rectangles > MAX_DAMAGE_RECTANGLES) &&
+       !cogl_framebuffer_blit (framebuffer,
+                               COGL_FRAMEBUFFER (dmabuf_fb),
+                               0, 0, 0, 0,
+                               width, height,
+                               &error)) ||
+      !cogl_framebuffer_blit_region (framebuffer, COGL_FRAMEBUFFER (dmabuf_fb),
+                                     region,
+                                     0, 0,
+                                     &error))
     {
-      if (!cogl_framebuffer_blit (framebuffer, COGL_FRAMEBUFFER (dmabuf_fb),
-                                  0, 0, 0, 0,
-                                  width, height,
-                                  &error))
-        {
-          g_object_unref (dmabuf_fb);
-          return NULL;
-        }
-    }
-  else
-    {
-      int i;
-
-      for (i = 0; i < n_rectangles; ++i)
-        {
-          MtkRectangle rectangle = mtk_region_get_rectangle (region, i);
-
-          if (!cogl_framebuffer_blit (framebuffer, COGL_FRAMEBUFFER (dmabuf_fb),
-                                      rectangle.x, rectangle.y,
-                                      rectangle.x, rectangle.y,
-                                      rectangle.width, rectangle.height,
-                                      &error))
-            {
-              g_object_unref (dmabuf_fb);
-              return NULL;
-            }
-        }
+      g_object_unref (dmabuf_fb);
+      return NULL;
     }
 
   secondary_gpu_state->cpu.current_dumb_fb = buffer_dumb;

clutter/clutter/clutter-stage-view.c
@@ -538,8 +538,9 @@ copy_shadowfb_to_onscreen (ClutterStageView *view,
 {
   ClutterStageViewPrivate *priv =
     clutter_stage_view_get_instance_private (view);
+  CoglFramebuffer *shadowfb = COGL_FRAMEBUFFER (priv->shadow.framebuffer);
   g_autoptr (MtkRegion) damage_region = NULL;
-  int i;
+  g_autoptr (GError) error = NULL;
 
   if (mtk_region_is_empty (swap_region))
     {
@@ -554,25 +555,12 @@ copy_shadowfb_to_onscreen (ClutterStageView *view,
       damage_region = mtk_region_copy (swap_region);
     }
 
-  for (i = 0; i < mtk_region_num_rectangles (damage_region); i++)
-    {
-      CoglFramebuffer *shadowfb = COGL_FRAMEBUFFER (priv->shadow.framebuffer);
-      g_autoptr (GError) error = NULL;
-      MtkRectangle rect;
-
-      rect = mtk_region_get_rectangle (damage_region, i);
-
-      if (!cogl_framebuffer_blit (shadowfb,
-                                  priv->framebuffer,
-                                  rect.x, rect.y,
-                                  rect.x, rect.y,
-                                  rect.width, rect.height,
-                                  &error))
-        {
-          g_warning ("Failed to blit shadow buffer: %s", error->message);
-          return;
-        }
-    }
+  if (!cogl_framebuffer_blit_region (shadowfb,
+                                     priv->framebuffer,
+                                     damage_region,
+                                     0, 0,
+                                     &error))
+    g_warning ("Failed to blit shadow buffer: %s", error->message);
 }
 
 void

cogl/cogl/cogl-framebuffer.c
@@ -1441,16 +1441,10 @@ cogl_can_blit_between_formats (CoglPixelFormat src_format,
   return TRUE;
 }
 
-gboolean
-cogl_framebuffer_blit (CoglFramebuffer *framebuffer,
-                       CoglFramebuffer *dst,
-                       int src_x,
-                       int src_y,
-                       int dst_x,
-                       int dst_y,
-                       int width,
-                       int height,
-                       GError **error)
+static gboolean
+cogl_framebuffer_blit_setup (CoglFramebuffer  *framebuffer,
+                             CoglFramebuffer  *dst,
+                             GError          **error)
 {
   CoglFramebufferPrivate *priv =
     cogl_framebuffer_get_instance_private (framebuffer);
@@ -1458,8 +1452,6 @@ cogl_framebuffer_blit (CoglFramebuffer *framebuffer,
     cogl_framebuffer_get_instance_private (dst);
   CoglContext *ctx = cogl_framebuffer_get_context (framebuffer);
   CoglDriver *driver = cogl_context_get_driver (ctx);
-  int src_x1, src_y1, src_x2, src_y2;
-  int dst_x1, dst_y1, dst_x2, dst_y2;
 
   if (!cogl_driver_has_feature (driver, COGL_FEATURE_ID_BLIT_FRAMEBUFFER))
     {
@@ -1503,44 +1495,108 @@ cogl_framebuffer_blit (CoglFramebuffer *framebuffer,
    * as changed */
   cogl_context_add_current_draw_buffer_changes (ctx, COGL_FRAMEBUFFER_STATE_CLIP);
 
+  return TRUE;
+}
+
+static void
+cogl_framebuffer_blit_box (CoglFramebuffer *framebuffer,
+                           CoglFramebuffer *dst,
+                           int              src_x1,
+                           int              src_y1,
+                           int              src_x2,
+                           int              src_y2,
+                           int              dst_x_offset,
+                           int              dst_y_offset)
+{
+  CoglContext *ctx = cogl_framebuffer_get_context (framebuffer);
+  CoglDriver *driver = cogl_context_get_driver (ctx);
+  int dst_x1, dst_y1, dst_x2, dst_y2;
+
+  dst_x1 = src_x1 + dst_x_offset;
+  dst_x2 = src_x2 + dst_x_offset;
+
   /* Offscreens we do the normal way, onscreens need an y-flip. Even if
    * we consider offscreens to be rendered upside-down, the offscreen
    * orientation is in this function's API. */
-  if (cogl_framebuffer_is_y_flipped (framebuffer))
-    {
-      src_x1 = src_x;
-      src_y1 = src_y;
-      src_x2 = src_x + width;
-      src_y2 = src_y + height;
-    }
-  else
-    {
-      src_x1 = src_x;
-      src_y1 = cogl_framebuffer_get_height (framebuffer) - src_y;
-      src_x2 = src_x + width;
-      src_y2 = src_y1 - height;
-    }
-
   if (cogl_framebuffer_is_y_flipped (dst))
     {
-      dst_x1 = dst_x;
-      dst_y1 = dst_y;
-      dst_x2 = dst_x + width;
-      dst_y2 = dst_y + height;
+      dst_y1 = src_y1 + dst_y_offset;
+      dst_y2 = src_y2 + dst_y_offset;
     }
   else
     {
-      dst_x1 = dst_x;
-      dst_y1 = cogl_framebuffer_get_height (dst) - dst_y;
-      dst_x2 = dst_x + width;
-      dst_y2 = dst_y1 - height;
+      dst_y1 = cogl_framebuffer_get_height (dst) - src_y1 - dst_y_offset;
+      dst_y2 = dst_y1 - (src_y2 - src_y1);
+    }
+
+  if (!cogl_framebuffer_is_y_flipped (framebuffer))
+    {
+      src_y1 = cogl_framebuffer_get_height (framebuffer) - src_y1;
+      src_y2 = src_y1 - (dst_y2 - dst_y1);
     }
 
   GE (driver, glBlitFramebuffer (src_x1, src_y1, src_x2, src_y2,
                                  dst_x1, dst_y1, dst_x2, dst_y2,
                                  GL_COLOR_BUFFER_BIT,
                                  GL_NEAREST));
+}
 
+gboolean
+cogl_framebuffer_blit_region (CoglFramebuffer  *framebuffer,
+                              CoglFramebuffer  *dst,
+                              const MtkRegion  *src_region,
+                              int               dst_x_offset,
+                              int               dst_y_offset,
+                              GError          **error)
+{
+  int num_rects, i;
+
+  if (!cogl_framebuffer_blit_setup (framebuffer, dst, error))
+    return FALSE;
+
+  num_rects = mtk_region_num_rectangles (src_region);
+  for (i = 0; i < num_rects; i++)
+    {
+      int x1, y1, x2, y2;
+
+      mtk_region_get_box (src_region, i, &x1, &y1, &x2, &y2);
+      cogl_framebuffer_blit_box (framebuffer,
+                                 dst,
+                                 x1,
+                                 y1,
+                                 x2,
+                                 y2,
+                                 dst_x_offset,
+                                 dst_y_offset);
+    }
+
+  return TRUE;
+}
+
+gboolean
+cogl_framebuffer_blit (CoglFramebuffer  *framebuffer,
+                       CoglFramebuffer  *dst,
+                       int               src_x,
+                       int               src_y,
+                       int               dst_x,
+                       int               dst_y,
+                       int               width,
+                       int               height,
+                       GError          **error)
+{
+  g_autoptr (MtkRegion) src_region = NULL;
+
+  if (!cogl_framebuffer_blit_setup (framebuffer, dst, error))
+    return FALSE;
+
+  cogl_framebuffer_blit_box (framebuffer,
+                             dst,
+                             src_x,
+                             src_y,
+                             src_x + width,
+                             src_y + height,
+                             dst_x - src_x,
+                             dst_y - src_y);
   return TRUE;
 }
 

cogl/cogl/cogl-framebuffer.h
@@ -1048,6 +1048,63 @@ cogl_can_blit_between_formats (CoglPixelFormat src_format,
                                CoglPixelFormat dst_format);
 
 /**
+ * cogl_framebuffer_blit_region:
+ * @framebuffer: The source #CoglFramebuffer
+ * @dst: The destination #CoglFramebuffer
+ * @src_region: Source region
+ * @dst_x_offset: Destination x offset
+ * @dst_y_offset: Destination y offset
+ * @error: optional error object
+ *
+ * @return FALSE for an immediately detected error, TRUE otherwise.
+ *
+ * This blits a region of the color buffer of the source buffer
+ * to the destination buffer. This function should only be
+ * called if the COGL_FEATURE_ID_BLIT_FRAMEBUFFER feature is
+ * advertised.
+ *
+ * The source region and destination offsets are defined in offscreen
+ * framebuffer orientation. When copying between an offscreen and
+ * onscreen framebuffers, the image is y-flipped accordingly.
+ *
+ * The two buffers must have the same value types (e.g. floating-point,
+ * unsigned int, signed int, or fixed-point), but color formats do not
+ * need to match. This limitation comes from OpenGL ES 3.0 definition
+ * of glBlitFramebuffer.
+ *
+ * Note that this function differs a lot from the glBlitFramebuffer
+ * function provided by the GL_EXT_framebuffer_blit extension. Notably
+ * it doesn't support having different sizes for the source and
+ * destination region. This doesn't seem
+ * like a particularly useful feature. If the application wanted to
+ * scale the results it may make more sense to draw a primitive
+ * instead.
+ *
+ * The GL function is documented to be affected by the scissor. This
+ * function therefore ensure that an empty clip stack is flushed
+ * before performing the blit which means the scissor is effectively
+ * ignored.
+ *
+ * The function also doesn't support specifying the buffers to copy
+ * and instead only the color buffer is copied. When copying the depth
+ * or stencil buffers the extension on GLES2.0 only supports copying
+ * the full buffer which would be awkward to document with this
+ * API. If we wanted to support that feature it may be better to have
+ * a separate function to copy the entire buffer for a given mask.
+ *
+ * The @c error argument is optional, it can be NULL. If it is not NULL
+ * and this function returns FALSE, an error object with a code from
+ * COGL_SYSTEM_ERROR will be created.
+ */
+COGL_EXPORT gboolean
+cogl_framebuffer_blit_region (CoglFramebuffer  *framebuffer,
+                              CoglFramebuffer  *dst,
+                              const MtkRegion  *src_region,
+                              int               dst_x_offset,
+                              int               dst_y_offset,
+                              GError          **error);
+
+/**
  * cogl_framebuffer_blit:
  * @framebuffer: The source #CoglFramebuffer
  * @dst: The destination #CoglFramebuffer

commit 5f0030fc1c3b9c1052b10ec6c805a396bba161de

onscreen/native: Avoid copying primary GPU damage twice
Right now, the primary GPU copy path falls back to a full-frame blit if
the number of damage rectangles is above a threshold.

After blitting the whole frame, it also blits the original region.
The pixels in that region are therefore copied twice, and the full-frame
blit can copy unchanged pixels outside the damaged region's extents.

This commit uses the supplied region or its bounding rectangle for the
blit, avoiding duplicate and unnecessary work.

Fixes: 753805bcca7a ("cogl: Add cogl_framebuffer_blit_region")

src/backends/native/meta-onscreen-native.c
@@ -1300,7 +1300,7 @@ copy_shared_framebuffer_primary_gpu (CoglOnscreen                        *onscre
   uint32_t offset;
   uint32_t drm_format;
   uint64_t modifier;
-  int n_rectangles;
+  g_autoptr (MtkRegion) blit_region = NULL;
 
   COGL_TRACE_BEGIN_SCOPED (CopySharedFramebufferPrimaryGpu,
                            "copy_shared_framebuffer_primary_gpu()");
@@ -1364,15 +1364,17 @@ copy_shared_framebuffer_primary_gpu (CoglOnscreen                        *onscre
       return NULL;
     }
 
-  n_rectangles = mtk_region_num_rectangles (region);
-  if ((n_rectangles > MAX_DAMAGE_RECTANGLES &&
-       !cogl_framebuffer_blit (framebuffer,
-                               COGL_FRAMEBUFFER (dmabuf_fb),
-                               0, 0, 0, 0,
-                               width, height,
-                               &error)) ||
-      !cogl_framebuffer_blit_region (framebuffer, COGL_FRAMEBUFFER (dmabuf_fb),
-                                     region,
+  if (mtk_region_num_rectangles (region) > MAX_DAMAGE_RECTANGLES)
+    {
+      MtkRectangle extents = mtk_region_get_extents (region);
+
+      blit_region = mtk_region_create_rectangle (&extents);
+    }
+  else
+    blit_region = mtk_region_copy (region);
+  if (!cogl_framebuffer_blit_region (framebuffer,
+                                     dmabuf_fb,
+                                     blit_region,
                                      0, 0,
                                      &error))
     return NULL;