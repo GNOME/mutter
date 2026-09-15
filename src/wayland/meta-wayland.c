GNOME/mutter

commit b676782bfd4bb75605ca2445463a31e371f92260

wayland/compositor: Switch to `prepare-compositor-shutdown` signal
Now that we have the signal, the compositor itself can use it
instead of exposing a dedicated method to the context.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5327>

src/wayland/meta-wayland.c
@@ -779,9 +779,12 @@ on_client_created (struct wl_listener *listener,
   wayland_client = meta_wayland_client_new_from_wl (context, client);
 }
 
-void
-meta_wayland_compositor_prepare_shutdown (MetaWaylandCompositor *compositor)
+static void
+on_prepare_compositor_shutdown (MetaContext *context,
+                                gpointer     user_data)
 {
+  MetaWaylandCompositor *compositor = user_data;
+
   g_signal_emit (compositor, signals[PREPARE_SHUTDOWN], 0, NULL);
 
   if (compositor->wayland_display)
@@ -947,6 +950,9 @@ meta_wayland_compositor_new (MetaContext *context)
 
   compositor = g_object_new (META_TYPE_WAYLAND_COMPOSITOR, NULL);
   compositor->context = context;
+
+  g_signal_connect (context, "prepare-compositor-shutdown",
+                    G_CALLBACK (on_prepare_compositor_shutdown), compositor);
 
   wl_display_set_default_max_buffer_size (compositor->wayland_display,
                                           1024 * 1024);

src/core/meta-context.c
@@ -800,10 +800,7 @@ meta_context_dispose (GObject *object)
   g_clear_object (&priv->service_channel);
 
   if (priv->wayland_compositor)
-    {
-      g_signal_emit (context, signals[PREPARE_COMPOSITOR_SHUTDOWN], 0);
-      meta_wayland_compositor_prepare_shutdown (priv->wayland_compositor);
-    }
+    g_signal_emit (context, signals[PREPARE_COMPOSITOR_SHUTDOWN], 0);
 
   g_signal_emit (context, signals[PREPARE_SHUTDOWN], 0);
 

src/wayland/meta-wayland.h
@@ -33,8 +33,6 @@ void                    meta_wayland_override_display_name (const char *display_
 
 MetaWaylandCompositor * meta_wayland_compositor_new             (MetaContext *context);
 
-void                    meta_wayland_compositor_prepare_shutdown (MetaWaylandCompositor *compositor);
-
 void                    meta_wayland_compositor_update          (MetaWaylandCompositor *compositor,
                                                                  const ClutterEvent    *event);
 

commit eb74a9962fb97dca9a4cf23f86ebdab888eef361

wayland/compositor: Prepare shutdown after other signal handlers
The idea behind !5327 was that the plugin manager stops starting effects
before the compositor is getting shut down, but since the compositor is
connected before the plugin, it was still getting shut down first.

This now ensures the this only happens after all the other signal
handlers.

Fixes: b676782bfd ("wayland/compositor: Switch to `prepare-compositor-shutdown` signal")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5336>

src/wayland/meta-wayland.c
@@ -951,8 +951,9 @@ meta_wayland_compositor_new (MetaContext *context)
   compositor = g_object_new (META_TYPE_WAYLAND_COMPOSITOR, NULL);
   compositor->context = context;
 
-  g_signal_connect (context, "prepare-compositor-shutdown",
-                    G_CALLBACK (on_prepare_compositor_shutdown), compositor);
+  g_signal_connect_after (context, "prepare-compositor-shutdown",
+                          G_CALLBACK (on_prepare_compositor_shutdown),
+                          compositor);
 
   wl_display_set_default_max_buffer_size (compositor->wayland_display,
                                           1024 * 1024);